046c6ba348b1bb7ff89dcf1a68d383fc0c4b31ee
xuweng <2028403305@qq.com>
2019-09-08 13:24:47 +0800

    创建人际关系

--- algorithm-study/src/main/java/com/algorithm/study/graph/Graph.java
+++ algorithm-study/src/main/java/com/algorithm/study/graph/Graph.java
@@ -1,5 +1,8 @@
 package com.algorithm.study.graph;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 图算法
  * 散列表是一对一映射,图是一对多映射
@@ -15,5 +18,35 @@ public class Graph {
      */
     public static Integer getIntimacy() {
         return 0;
+    }
+
+    /**
+     * 创建人际关系
+     * graph["longGe"] = ["friend1", "friend2", "friend3"]
+     * graph["friend1"] = ["friend11", "friend12", "friend13"]
+     * graph["friend2"] = ["friend21", "friend22", "friend23"]
+     * graph["friend3"] = ["friend31", "friend32", "friend33"]
+     *
+     * @return
+     */
+    public static Map<String, String[]> createRelationship() {
+        Map<String, String[]> map = new HashMap<>();
+
+        String longGe = "longGe";
+        String[] longGeFriends = {"friend1", "friend2", "friend3"};
+        map.put(longGe, longGeFriends);
+
+        String friend1 = "friend1";
+        String[] friend1Friends = {"friend11", "friend12", "friend13"};
+        map.put(friend1, friend1Friends);
+
+        String friend2 = "friend2";
+        String[] friend2Friends = {"friend21", "friend22", "friend23"};
+        map.put(friend2, friend2Friends);
+
+        String friend3 = "friend3";
+        String[] friend3Friends = {"friend31", "friend32", "friend33"};
+        map.put(friend3, friend3Friends);
+        return map;
     }
 }
