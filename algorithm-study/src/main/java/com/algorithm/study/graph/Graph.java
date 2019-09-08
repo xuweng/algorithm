39f6932da52b09b7cbfffd1111df9da230fbda8c
xuweng <2028403305@qq.com>
2019-09-08 12:57:18 +0800

    图算法

---
+++ algorithm-study/src/main/java/com/algorithm/study/graph/Graph.java
@@ -0,0 +1,18 @@
+package com.algorithm.study.graph;
+
+/**
+ * 图算法
+ */
+public class Graph {
+    /**
+     * 获取亲密度
+     * 朋友是一度关系，朋友的朋友是二度关系。
+     * 一度关系胜过二度关系，二度关系胜过三度关系，以此类推。因此，你应先在一
+     * 度关系中搜索，确定其中没有芒果销售商后，才在二度关系中搜索
+     *
+     * @return
+     */
+    public static Integer getIntimacy() {
+        return 0;
+    }
+}
