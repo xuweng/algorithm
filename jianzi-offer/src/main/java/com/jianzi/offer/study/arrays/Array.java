c7eab41da6aa09fe75f675e987e38e23b793a2e1
xuweng <2028403305@qq.com>
2019-09-20 23:49:25 +0800

    1:输入校验永远是第一步 2:控制好条件范围,不要越界 3:指针太多,变量太多,容易混淆

--- jianzi-offer/src/main/java/com/jianzi/offer/study/arrays/Array.java
+++ jianzi-offer/src/main/java/com/jianzi/offer/study/arrays/Array.java
@@ -40,6 +40,45 @@ public class Array {
     }
 
     /**
+     * 合并两个有序数组
+     * <p>
+     * 假设a1比较长
+     *
+     * @param array1 有序数组1
+     * @param array2 有序数组2
+     * @return 合并的数组
+     */
+    public static Integer[] mergeArray(Integer[] array1, Integer[] array2) {
+        //输入校验,放在一起
+        Objects.requireNonNull(array1);
+        Objects.requireNonNull(array2);
+        if (array1.length < array2.length) {
+            throw new IllegalArgumentException();
+        }
+
+        int oLength = 0;
+        for (int i = 0; i < array1.length && array1[i] != 0; i++) {
+            oLength++;
+        }
+        //指针太多,变量太多,容易混淆
+        int newLength = oLength + array2.length;
+
+        int indexOfOlength = oLength;
+        int indexOfNewLength = newLength;
+
+        int newIndex = array2.length - 1;
+        for (int i = newIndex; i <= 0; i--) {
+            if (array2[newIndex] >= array1[indexOfOlength]) {
+                array1[indexOfNewLength--] = array2[newIndex--];
+            } else {
+                array1[indexOfNewLength--] = array1[indexOfOlength--];
+            }
+        }
+        //两个数组的长度?如何遍历?条件范围?
+        return array1;
+    }
+
+    /**
      * 生成顺序二维数组
      *
      * @return 二维数组
--- jianzi-offer/src/test/java/com/jianzi/offer/study/ArrayTests.java
+++ jianzi-offer/src/test/java/com/jianzi/offer/study/ArrayTests.java
@@ -1,11 +1,14 @@
 package com.jianzi.offer.study;
 
 import com.jianzi.offer.study.arrays.Array;
+import com.jianzi.offer.study.common.ArrayUtils;
 import com.jianzi.offer.study.common.annotation.ExceptionUserCase;
 import com.jianzi.offer.study.common.annotation.NormalUserCase;
 import com.jianzi.offer.study.linkedlist.LinkedLists;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -44,5 +47,23 @@ public class ArrayTests {
 
             assertTrue(isFound);
         });
+    }
+
+    @NormalUserCase
+    @Test
+    public void mergeArraySuccess() {
+        Integer[] array1 = new Integer[20];
+        Integer[] array2 = new Integer[2];
+        Arrays.fill(array1, 0);
+        for (int i = 1; i <= 10; i++) {
+            array1[i - 1] = i;
+        }
+        array1[10] = 0;
+        array2[0] = 11;
+        array2[1] = 12;
+
+        Integer[] array = Array.mergeArray(array1, array2);
+
+        ArrayUtils.println(array);
     }
 }
