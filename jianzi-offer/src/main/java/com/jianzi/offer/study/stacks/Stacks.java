c3c57ccc1f4dd393d574e565fa314668f7ec4759
xuweng <2028403305@qq.com>
2019-09-22 09:49:12 +0800

    栈

--- jianzi-offer/src/main/java/com/jianzi/offer/study/stacks/Stacks.java
+++ jianzi-offer/src/main/java/com/jianzi/offer/study/stacks/Stacks.java
@@ -6,7 +6,9 @@ import java.util.Stack;
  * 栈
  */
 public class Stacks {
+    //stack1用来入队
     private static final Stack<Integer> stack1 = new Stack<Integer>();
+    //stack2用来出队,存放最早的元素
     private static final Stack<Integer> stack2 = new Stack<Integer>();
 
     /**
@@ -28,6 +30,7 @@ public class Stacks {
      * @return
      */
     public static Integer deleteHead() {
+        //stack2一定为空才入队,使最先的元素出队,防止被覆盖
         if (stack2.isEmpty()) {
             while (!stack1.isEmpty()) {
                 stack2.push(stack1.pop());
