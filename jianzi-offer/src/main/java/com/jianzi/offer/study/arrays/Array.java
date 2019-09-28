9e7d84e1b9bf017da624abcb5faedda4a19992c3
xuweng <2028403305@qq.com>
2019-09-28 11:34:18 +0800

    * 学到什么      * <p>      * 二分查找思想      * 大神分析过程      * 多种情况      * 注意条件表达式      * 只完成最基本的功能?      * 算法太考验思维

--- jianzi-offer/src/main/java/com/jianzi/offer/study/arrays/Array.java
+++ jianzi-offer/src/main/java/com/jianzi/offer/study/arrays/Array.java
@@ -79,6 +79,63 @@ public class Array {
     }
 
     /**
+     * 旋转数组：两个递增子数组
+     * <p>
+     * 学到什么
+     * <p>
+     * 二分查找思想
+     * 大神分析过程
+     * 多种情况
+     * 注意条件表达式
+     * 只完成最基本的功能?
+     * 算法太考验思维
+     *
+     * @param array
+     */
+    public static int rotateBinarySearch(int[] array) {
+        Objects.requireNonNull(array);
+
+        int index1 = 0;
+        int index2 = array.length - 1;
+        int indexMid = index1;//初始化
+        while (array[index1] >= array[index2]) {
+            if (index2 - index1 == 1) {
+                indexMid = index2;
+                break;
+            }
+            indexMid = (index1 + index2) / 2;
+            if (array[index1] == array[index2] && array[index1] == array[indexMid]) {
+                return rotateInOrder(array, index1, index2);
+            }
+            if (array[indexMid] >= array[index1]) {
+                index1 = indexMid;
+            } else if (array[indexMid] <= array[index2]) {
+                index2 = indexMid;
+            }
+
+        }
+        return array[indexMid];
+    }
+
+    /**
+     * 顺序查找最小值
+     *
+     * @param array
+     * @param index1
+     * @param index2
+     * @return
+     */
+    private static int rotateInOrder(int[] array, int index1, int index2) {
+        int result = array[index1];
+        for (int i = index1 + 1; i <= index2; i++) {
+            if (result > array[i]) {
+                result = array[i];
+            }
+        }
+        return result;
+    }
+
+    /**
      * 生成顺序二维数组
      *
      * @return 二维数组
--- jianzi-offer/src/test/java/com/jianzi/offer/study/ArrayTests.java
+++ jianzi-offer/src/test/java/com/jianzi/offer/study/ArrayTests.java
@@ -5,6 +5,7 @@ import com.jianzi.offer.study.common.ArrayUtils;
 import com.jianzi.offer.study.common.annotation.ExceptionUserCase;
 import com.jianzi.offer.study.common.annotation.NormalUserCase;
 import com.jianzi.offer.study.linkedlist.LinkedLists;
+import com.jianzi.offer.study.strings.Strings;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -26,6 +27,31 @@ public class ArrayTests {
         boolean isFound = Array.findTwoArray(array, 3, 3, 7);
 
         assertTrue(isFound);
+    }
+
+    @NormalUserCase
+    @Test
+    public void rotateBinarySearchSuccess() {
+        //功能测试
+        int[] array1 = {3, 4, 5, 1, 2};
+        int[] array2 = {1, 0, 1, 1, 1};
+        int[] array3 = {1, 1, 1, 0, 1};
+
+        assertEquals(1, Array.rotateBinarySearch(array1));
+        assertEquals(0, Array.rotateBinarySearch(array2));
+        assertEquals(0, Array.rotateBinarySearch(array3));
+
+        //边界值测试
+        int[] array4 = {1, 2, 3, 4, 5};
+        int[] array5 = {1};
+
+        assertEquals(1, Array.rotateBinarySearch(array4));
+        assertEquals(1, Array.rotateBinarySearch(array5));
+
+        //特殊输入测试
+        Throwable exception = assertThrows(NullPointerException.class, () -> {
+            Array.rotateBinarySearch(null);
+        });
     }
 
     @NormalUserCase
