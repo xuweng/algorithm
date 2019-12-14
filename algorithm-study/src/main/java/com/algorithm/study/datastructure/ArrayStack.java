d6cd29dcc3b15cced50efb14fac58fb2cbeab2b1
xuweng <2028403305@qq.com>
2019-12-14 18:07:33 +0800

    栈顶指针

--- algorithm-study/src/main/java/com/algorithm/study/datastructure/ArrayStack.java
+++ algorithm-study/src/main/java/com/algorithm/study/datastructure/ArrayStack.java
@@ -5,6 +5,7 @@ package com.algorithm.study.datastructure;
 public class ArrayStack {
     private String[] items;  // 数组
     private int size;       // 栈中元素个数
+    private int top;       // 栈顶指针,数组下标
     private int capacity;    //容量
 
     // 初始化数组，申请一个大小为n的数组空间
@@ -12,6 +13,7 @@ public class ArrayStack {
         this.items = new String[capacity];
         this.capacity = capacity;
         this.size = 0;
+        this.top = -1;
     }
 
     // 入栈操作
@@ -20,8 +22,8 @@ public class ArrayStack {
         if (isFull()) {
             return false;
         }
-        // 将item放到下标为count的位置，并且count加一
-        items[size] = item;
+        // 将item放到下标为top的位置
+        items[++top] = item;
         ++size;
         return true;
     }
@@ -33,7 +35,7 @@ public class ArrayStack {
             return null;
         }
         // 返回下标为count-1的数组元素，并且栈中元素个数count减一
-        String tmp = items[size - 1];
+        String tmp = items[top];
         --size;
         return tmp;
     }
