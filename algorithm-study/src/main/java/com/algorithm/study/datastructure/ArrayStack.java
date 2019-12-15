d07d5f6f7a7e9d375b150df4c8da30611a1e6395
xuweng <2028403305@qq.com>
2019-12-15 15:11:55 +0800

    类库栈学习

--- algorithm-study/src/main/java/com/algorithm/study/datastructure/ArrayStack.java
+++ algorithm-study/src/main/java/com/algorithm/study/datastructure/ArrayStack.java
@@ -5,16 +5,16 @@ import java.util.Objects;
 
 // 基于数组实现的顺序栈
 public class ArrayStack {
-    private String[] items;  // 数组
-    private int size;       // 栈中元素个数
+    private String[] elementData;  // 数组
+    private int elementCount;       // 栈中元素个数
     private int top;       // 栈顶指针,数组下标
     private int capacity;    //容量
 
     // 初始化数组，申请一个大小为n的数组空间
     public ArrayStack(int capacity) {
-        this.items = new String[capacity];
+        this.elementData = new String[capacity];
         this.capacity = capacity;
-        this.size = 0;
+        this.elementCount = 0;
         this.top = -1;
     }
 
@@ -26,8 +26,8 @@ public class ArrayStack {
             return false;
         }
         // 将item放到下标为top的位置
-        items[++top] = item;
-        ++size;
+        elementData[++top] = item;
+        ++elementCount;
         return true;
     }
 
@@ -37,9 +37,42 @@ public class ArrayStack {
         if (isEmpty()) {
             return null;
         }
-        --size;
         // 返回下标为top的数组元素，并且栈中元素个数减一
-        return items[top--];
+        String result = peek();
+        removeElementAt(top);
+        top--;
+        return result;
+    }
+
+    public String peek() {
+        // 栈为空，则直接返回null
+        if (isEmpty()) {
+            return null;
+        }
+
+        return elementData[top];
+    }
+
+    /**
+     * 删除元素
+     *
+     * @param index 数组下标 0<=index<size
+     * @return
+     */
+    public void removeElementAt(int index) {
+        if (index >= elementCount) {
+            throw new ArrayIndexOutOfBoundsException(index + " >= " +
+                    elementCount);
+        } else if (index < 0) {
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
+        //j在中间
+        int j = elementCount - index - 1;
+        if (j > 0) {
+            System.arraycopy(elementData, index + 1, elementData, index, j);
+        }
+        //清空最后一个元素
+        elementData[--elementCount] = null; /* to let gc do its work */
     }
 
     /**
@@ -48,7 +81,29 @@ public class ArrayStack {
      * @return
      */
     private boolean isEmpty() {
-        return size == 0;
+        return elementCount == 0;
+    }
+
+    public boolean empty() {
+        return size() == 0;
+    }
+
+    /**
+     * 元素个数
+     *
+     * @return
+     */
+    public int size() {
+        return elementCount;
+    }
+
+    /**
+     * 容量
+     *
+     * @return
+     */
+    public int capacity() {
+        return elementData.length;
     }
 
     /**
@@ -57,6 +112,23 @@ public class ArrayStack {
      * @return
      */
     private boolean isFull() {
-        return size >= capacity;
+        return elementCount >= capacity;
+    }
+
+    /**
+     * 清空所有元素
+     */
+    public void clear() {
+        removeAllElements();
+    }
+
+    public void removeAllElements() {
+        // Let gc do its work
+        for (int i = 0; i < elementCount; i++) {
+            elementData[i] = null;
+        }
+
+        elementCount = 0;
+        top = -1;
     }
 }
