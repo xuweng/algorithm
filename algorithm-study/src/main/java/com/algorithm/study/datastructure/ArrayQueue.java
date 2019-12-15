c800110adbe33d3ed19768ddf952568fd465fe3b
xuweng <2028403305@qq.com>
2019-12-15 22:35:05 +0800

    将 head 到 tail 之间的数据，整体搬移到数组中 0 到 tail-head 的位置 注意细节 不要数组越界 不要下标越界

--- algorithm-study/src/main/java/com/algorithm/study/datastructure/ArrayQueue.java
+++ algorithm-study/src/main/java/com/algorithm/study/datastructure/ArrayQueue.java
@@ -17,12 +17,31 @@ public class ArrayQueue {
         n = capacity;
     }
 
-    // 入队
+    /**
+     * 实际上，我们在出队时可以不用搬移数据。如果没有空闲空间了，我们只需要在入队时，再集中触发一次数据的搬移操作
+     * <p>
+     * 当队列的 tail 指针移动到数组的最右边后，如果有新的数据入队，我们可以将 head 到 tail 之间的数据，
+     * 整体搬移到数组中 0 到 tail-head 的位置。
+     *
+     * @param item
+     * @return
+     */
     public boolean enqueue(String item) {
-        // 如果tail == n 表示队列已经满了
+        // tail == n表示队列末尾没有空间了
         if (isFull()) {
-            return false;
+            // tail ==n && head==0，表示整个队列都占满了,不用搬移
+            if (head == 0) {
+                return false;
+            }
+            // 数据搬移
+            for (int i = head; i < tail; ++i) {
+                items[i - head] = items[i];
+            }
+            // 搬移完之后重新更新head和tail
+            tail -= head;
+            head = 0;
         }
+
         items[tail] = item;
         ++tail;
         return true;
