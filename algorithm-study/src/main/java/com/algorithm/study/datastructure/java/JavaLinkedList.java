b74ce2178238ed644594a5010f2ef908f2538656
xuweng <2028403305@qq.com>
2019-12-15 21:55:19 +0800

    类库LinkedList学习 first一定在last前面 从左往右新增结点 从右往左新增结点 没有index,只能在first或者last操作

--- algorithm-study/src/main/java/com/algorithm/study/datastructure/java/JavaLinkedList.java
+++ algorithm-study/src/main/java/com/algorithm/study/datastructure/java/JavaLinkedList.java
@@ -3,6 +3,7 @@ package com.algorithm.study.datastructure.java;
 
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
+import java.util.Queue;
 
 /**
  * 类库LinkedList
@@ -222,6 +223,8 @@ public class JavaLinkedList<E> {
     }
 
     /**
+     * 删除第一个结点
+     * <p>
      * first和last在两端
      * <p>
      * 删除first
@@ -240,6 +243,8 @@ public class JavaLinkedList<E> {
     }
 
     /**
+     * 删除最后一个结点
+     * <p>
      * first和last在两端
      * <p>
      * 删除last
@@ -255,6 +260,270 @@ public class JavaLinkedList<E> {
             throw new NoSuchElementException();
         }
         return unlinkLast(l);
+    }
+
+    /**
+     * 从右往左新增结点
+     * <p>
+     * Inserts the specified element at the beginning of this list.
+     *
+     * @param e the element to add
+     */
+    public void addFirst(E e) {
+        linkFirst(e);
+    }
+
+    /**
+     * 从左往右新增结点
+     * <p>
+     * Appends the specified element to the end of this list.
+     *
+     * <p>This method is equivalent to {@link #add}.
+     *
+     * @param e the element to add
+     */
+    public void addLast(E e) {
+        linkLast(e);
+    }
+
+    /**
+     * Returns the number of elements in this list.
+     *
+     * @return the number of elements in this list
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * 不能通过索引新增元素
+     * <p>
+     * 新增一个元素都是尾插
+     * <p>
+     * Appends the specified element to the end of this list.
+     *
+     * <p>This method is equivalent to {@link #addLast}.
+     *
+     * @param e element to be appended to this list
+     * @return {@code true} (as specified by {@link Collection#add})
+     */
+    public boolean add(E e) {
+        linkLast(e);
+        return true;
+    }
+
+    /**
+     * 删除一个元素
+     * 从first到last遍历
+     *
+     * @param o
+     * @return
+     */
+    public boolean remove(Object o) {
+        if (o == null) {
+            for (Node<E> x = first; x != null; x = x.next) {
+                if (x.item == null) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        } else {
+            for (Node<E> x = first; x != null; x = x.next) {
+                if (o.equals(x.item)) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 清空
+     */
+    public void clear() {
+        // Clearing all of the links between nodes is "unnecessary", but:
+        // - helps a generational GC if the discarded nodes inhabit
+        //   more than one generation
+        // - is sure to free memory even if there is a reachable Iterator
+        for (Node<E> x = first; x != null; ) {
+            Node<E> next = x.next;
+            x.item = null;
+            x.next = null;
+            x.prev = null;
+            x = next;
+        }
+        first = last = null;
+        size = 0;
+        modCount++;
+    }
+
+    public E get(int index) {
+        checkElementIndex(index);
+        return node(index).item;
+    }
+
+    /**
+     * 覆盖
+     *
+     * @param index
+     * @param element
+     * @return
+     */
+    public E set(int index, E element) {
+        checkElementIndex(index);
+        Node<E> x = node(index);
+        E oldVal = x.item;
+        x.item = element;
+        return oldVal;
+    }
+
+    public void add(int index, E element) {
+        checkPositionIndex(index);
+
+        if (index == size) {
+            linkLast(element);
+        } else {
+            linkBefore(element, node(index));
+        }
+    }
+
+    public E remove(int index) {
+        checkElementIndex(index);
+        return unlink(node(index));
+    }
+
+    /**
+     * *检索但不删除由表示的队列的头
+     *       *此双端队列（换句话说，此双端队列的第一个元素），或
+     *       *如果此双端队列为空，则返回{@code null}。
+     *       *
+     *       * <p>此方法等效于{@link #peekFirst（）}。
+     *       *
+     *       * @返回此双端队列代表的队列的头，或者
+     *       * {@code null}如果此双端队列为空
+     * <p>
+     * Retrieves, but does not remove, the head of the queue represented by
+     * this deque (in other words, the first element of this deque), or
+     * returns {@code null} if this deque is empty.
+     *
+     * <p>This method is equivalent to {@link #peekFirst()}.
+     *
+     * @return the head of the queue represented by this deque, or
+     * {@code null} if this deque is empty
+     */
+    public E peek() {
+        final Node<E> f = first;
+        return (f == null) ? null : f.item;
+    }
+
+    /**
+     * Retrieves, but does not remove, the head (first element) of this list.
+     *
+     * @return the head of this list
+     * @throws NoSuchElementException if this list is empty
+     * @since 1.5
+     */
+    public E element() {
+        return getFirst();
+    }
+
+    /**
+     * / **
+     *       *检索并删除此列表的标题（第一个元素）。
+     *       *
+     *       * @返回此列表的开头，如果此列表为空，则返回{@code null}
+     *       * @自1.5起
+     *       * /
+     * <p>
+     * Retrieves and removes the head (first element) of this list.
+     *
+     * @return the head of this list, or {@code null} if this list is empty
+     * @since 1.5
+     */
+    public E poll() {
+        final Node<E> f = first;
+        return (f == null) ? null : unlinkFirst(f);
+    }
+
+    /**
+     * Retrieves and removes the head (first element) of this list.
+     *
+     * @return the head of this list
+     * @throws NoSuchElementException if this list is empty
+     * @since 1.5
+     */
+    public E remove() {
+        return removeFirst();
+    }
+
+    /**
+     * 尾插
+     * <p>
+     * 没有index,只能在first或者last操作
+     *
+     * <p>
+     * Adds the specified element as the tail (last element) of this list.
+     *
+     * @param e the element to add
+     * @return {@code true} (as specified by {@link Queue#offer})
+     * @since 1.5
+     */
+    public boolean offer(E e) {
+        return add(e);
+    }
+
+    private void checkPositionIndex(int index) {
+        if (!isPositionIndex(index)) {
+            //索引越界异常
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+        }
+    }
+
+    private boolean isPositionIndex(int index) {
+        return index >= 0 && index <= size;
+    }
+
+    private void checkElementIndex(int index) {
+        if (!isElementIndex(index)) {
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+        }
+    }
+
+    private String outOfBoundsMsg(int index) {
+        return "Index: " + index + ", Size: " + size;
+    }
+
+    /**
+     * Tells if the argument is the index of an existing element.
+     */
+    private boolean isElementIndex(int index) {
+        return index >= 0 && index < size;
+    }
+
+    /**
+     * 通过index获取结点
+     * <p>
+     * Returns the (non-null) Node at the specified element index.
+     */
+    Node<E> node(int index) {
+        // assert isElementIndex(index);
+
+        if (index < (size >> 1)) {
+            //索引在左半部分,从first开始遍历
+            Node<E> x = first;
+            for (int i = 0; i < index; i++) {
+                x = x.next;
+            }
+            return x;
+        } else {
+            //索引在右部分,从last开始遍历
+            Node<E> x = last;
+            for (int i = size - 1; i > index; i--) {
+                x = x.prev;
+            }
+            return x;
+        }
     }
 
     /**
