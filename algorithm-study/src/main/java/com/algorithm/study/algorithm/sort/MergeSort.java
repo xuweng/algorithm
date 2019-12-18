c2ab9e752cfc32740934a3db547825207c7f7e12
xuweng <2028403305@qq.com>
2019-12-18 22:31:54 +0800

    代码模板

---
+++ algorithm-study/src/main/java/com/algorithm/study/algorithm/sort/MergeSort.java
@@ -0,0 +1,62 @@
+package com.algorithm.study.algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * 代码模板
+ * <p>
+ * 多看漂亮的代码
+ */
+public class MergeSort {
+
+    public static void merge(int[] a, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+        int i = low;// 左指针
+        int j = mid + 1;// 右指针
+        int k = 0;
+        // 把较小的数先移到新数组中
+        //两个判断条件
+        while (i <= mid && j <= high) {
+            if (a[i] < a[j]) {
+                temp[k++] = a[i++];
+            } else {
+                temp[k++] = a[j++];
+            }
+        }
+        // 把左边剩余的数移入数组
+        while (i <= mid) {
+            temp[k++] = a[i++];
+        }
+        // 把右边边剩余的数移入数组
+        while (j <= high) {
+            temp[k++] = a[j++];
+        }
+        // 把新数组中的数覆盖nums数组
+        if (temp.length >= 0) {
+            System.arraycopy(temp, 0, a, low, temp.length);
+        }
+    }
+
+    /**
+     * 左右指针
+     *
+     * @param a
+     * @param low
+     * @param high
+     */
+    public static void mergeSort(int[] a, int low, int high) {
+//        int mid = (low + high) / 2;
+        int mid = low + (high - low) / 2;
+        //开始递归
+        if (low < high) {
+            // 左边
+            mergeSort(a, low, mid);
+            // 右边
+            mergeSort(a, mid + 1, high);
+            // 左右归并
+            merge(a, low, mid, high);
+            System.out.println(Arrays.toString(a));
+        }
+
+    }
+}
