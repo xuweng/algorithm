8bbb6aae859d37c8a45e0c50984fc58c0d9b2405
xuweng <2028403305@qq.com>
2019-12-21 17:12:26 +0800

    希尔排序

--- algorithm-study/src/main/java/com/algorithm/study/algorithm/sort/insert/InsertSort.java
+++ algorithm-study/src/main/java/com/algorithm/study/algorithm/sort/insert/InsertSort.java
@@ -1,6 +1,9 @@
 package com.algorithm.study.algorithm.sort.insert;
 
 /**
+ * 直接插入排序
+ * 二分查找插入排序---------一样需要移动元素。没什么优化。
+ * <p>
  * 插入排序优化
  */
 public class InsertSort {
@@ -44,6 +47,36 @@ public class InsertSort {
 
             // 将 arr[i] 放到正确位置上
             arr[j + 1] = temp;
+        }
+    }
+
+    /**
+     * 一个更好理解的希尔排序实现：将数组列在一个表中并对列排序（用插入排序）。
+     * 重复这过程，不过每次用更长的列来进行。最后整个表就只有一列了。
+     * 将数组转换至表是为了更好地理解这算法，算法本身仅仅对原数组进行排序
+     * （通过增加索引的步长，例如是用i += step_size而不是i++ ）。
+     * <p>
+     * Donald Shell最初建议步长选择为 n/2 并且对步长取半直到步长达到1
+     * <p>
+     * 5列---------->3列---------->1列。
+     * <p>
+     * 希尔排序
+     *
+     * @param arr
+     */
+    public static void shellSort(int[] arr) {
+        int length = arr.length;
+        int temp;
+        for (int step = length / 2; step >= 1; step /= 2) {
+            for (int i = step; i < length; i++) {
+                temp = arr[i];
+                int j = i - step;
+                while (j >= 0 && arr[j] > temp) {
+                    arr[j + step] = arr[j];
+                    j -= step;
+                }
+                arr[j + step] = temp;
+            }
         }
     }
 }
