2326d1f1eabd858c1d5efbb1b1e2c06b2de4d491
xuweng <2028403305@qq.com>
2019-12-21 12:28:41 +0800

    两两交换。先进分区函数。

--- algorithm-study/src/main/java/com/algorithm/study/algorithm/sort/quicksort/one/QuickSort.java
+++ algorithm-study/src/main/java/com/algorithm/study/algorithm/sort/quicksort/one/QuickSort.java
@@ -1,5 +1,7 @@
 package com.algorithm.study.algorithm.sort.quicksort.one;
 
+import com.algorithm.study.common.ArrayUtils;
+
 /**
  * 快排---------大于等于的元素分区比例----------左子树:右子树=1:n-1
  * 归并----------左子树:右子树=1:1
@@ -127,6 +129,56 @@ public class QuickSort {
      * @return 基准元素下标
      */
     public static int selectPartitionRandom(int[] arry, int left, int right) {
-        return 0;
+        ArrayUtils.checkIndex(arry.length, left);
+        ArrayUtils.checkIndex(arry.length, right);
+
+        return (left + right) / 2;
     }
+
+    /**
+     * 先进分区函数
+     * <p>
+     * 画图分析
+     * <p>
+     * 一开始将小于v和大于v的元素放在数组的两端,等于v稍后再处理
+     * arr[l+1...l-1]<v
+     * arr[j...r]>v
+     *
+     * @param arr   数据
+     * @param left  左边界。数组下标。
+     * @param right 右边界。数组下标。
+     * @return
+     */
+    public static int advancedPartition(int arr[], int left, int right) {
+        //swap(arr[left],arr[rand()%(r-l+1)+l]);//随机取参考值的大小
+        //arr[left]是一个随机选择数
+        //v是一个随机选择数。应付高度有序。
+        arr[left] = selectPartitionRandom(arr, left, right);
+        int v = arr[left];
+        int i = left + 1, j = right;
+        while (true) {
+            //合并
+            //当i的元素小于v的时候继续向后扫描，直到碰到了arr[i]>=v
+            while (arr[i] < v && i <= right) {
+                i++;
+            }
+            //直到碰到某个元素arr[i]<=v
+            while (j >= left + 1 && arr[j] > v) {
+                j--;
+            }
+
+            if (i > j) {
+                break;
+            }
+            //i，j两两交换。两个数相等时也会交换
+            ArrayUtils.swap(arr, i, j);
+            i++;
+            j--;
+        }
+        //设置基准元素
+        ArrayUtils.swap(arr, left, j);
+
+        return j;
+    }
+
 }
