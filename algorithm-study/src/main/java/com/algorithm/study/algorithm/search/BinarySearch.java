8321461bcb9afcbb9ee106c4ebee60f8910e808c
xuweng <2028403305@qq.com>
2019-12-22 00:17:23 +0800

    二分查找的递归实现

--- algorithm-study/src/main/java/com/algorithm/study/algorithm/search/BinarySearch.java
+++ algorithm-study/src/main/java/com/algorithm/study/algorithm/search/BinarySearch.java
@@ -102,4 +102,32 @@ public class BinarySearch {
 
         return -1;
     }
+
+
+    /**
+     * 二分查找的递归实现
+     *
+     * @param a
+     * @param n
+     * @param val
+     * @return
+     */
+    public static int bsearchRecursive(int[] a, int n, int val) {
+        return bsearchInternally(a, 0, n - 1, val);
+    }
+
+    private static int bsearchInternally(int[] a, int low, int high, int value) {
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = low + ((high - low) >> 1);
+        if (a[mid] == value) {
+            return mid;
+        } else if (a[mid] < value) {
+            return bsearchInternally(a, mid + 1, high, value);
+        } else {
+            return bsearchInternally(a, low, mid - 1, value);
+        }
+    }
 }
