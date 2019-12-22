caaa0a70bd6abd70a3474107c32acc6717c16bd2
xuweng <2028403305@qq.com>
2019-12-22 11:27:08 +0800

    查找最后一个值等于给定值的元素

--- algorithm-study/src/main/java/com/algorithm/study/algorithm/search/two/BinarySearch.java
+++ algorithm-study/src/main/java/com/algorithm/study/algorithm/search/two/BinarySearch.java
@@ -23,7 +23,7 @@ public class BinarySearch {
      * @param value
      * @return
      */
-    public int bsearch(int[] a, int n, int value) {
+    public int searchFirstEqual(int[] a, int n, int value) {
         int low = 0;
         int high = n - 1;
         while (low <= high) {
@@ -38,6 +38,39 @@ public class BinarySearch {
                     return mid;
                 } else {
                     high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 变体二：查找最后一个值等于给定值的元素
+     * <p>
+     * 如果 a[mid] 这个元素已经是数组中的最后一个元素了，那它肯定是我们要找的；
+     * 如果 a[mid] 的后一个元素 a[mid+1] 不等于 value，
+     * 那也说明 a[mid] 就是我们要找的最后一个值等于给定值的元素。
+     *
+     * @param a
+     * @param n
+     * @param value
+     * @return
+     */
+    public int searchLastEqual(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                //注意下标越界
+                if ((mid == n - 1) || (a[mid + 1] != value)) {
+                    return mid;
+                } else {
+                    low = mid + 1;
                 }
             }
         }
