478b12e4f38adbe0fd6e3f6b1db66a18effc598e
xuweng <2028403305@qq.com>
2019-12-24 21:45:58 +0800

    单调数据结构

---
+++ algorithm-study/src/main/java/com/algorithm/study/datastructure/monotonous/MonotonicQueue.java
@@ -0,0 +1,7 @@
+package com.algorithm.study.datastructure.monotonous;
+
+/**
+ * 单调队列。单调递增、单调递减。用双端队列实现。
+ */
+public class MonotonicQueue {
+}
---
+++ algorithm-study/src/main/java/com/algorithm/study/datastructure/monotonous/MonotonicStack.java
@@ -0,0 +1,7 @@
+package com.algorithm.study.datastructure.monotonous;
+
+/**
+ * 单调栈。单调递增、单调递减。
+ */
+public class MonotonicStack {
+}
