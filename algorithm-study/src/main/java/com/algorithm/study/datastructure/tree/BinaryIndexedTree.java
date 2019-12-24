33aa7f1158a2d950273815f95f97c9af0e89bcda
xuweng <2028403305@qq.com>
2019-12-24 22:40:12 +0800

    树状数组

--- algorithm-study/src/main/java/com/algorithm/study/datastructure/tree/BinaryIndexedTree.java
+++ algorithm-study/src/main/java/com/algorithm/study/datastructure/tree/BinaryIndexedTree.java
@@ -1,6 +1,30 @@
 package com.algorithm.study.datastructure.tree;
 
 /**
+ * C[1] = C[0001] = A[1];
+ * <p>
+ *         C[2] = C[0010] = A[1]+A[2];
+ * <p>
+ *         C[3] = C[0011] = A[3];
+ * <p>
+ *         C[4] = C[0100] = A[1]+A[2]+A[3]+A[4];
+ * <p>
+ *         C[5] = C[0101] = A[5];
+ * <p>
+ *         C[6] = C[0110] = A[5]+A[6];
+ * <p>
+ *         C[7] = C[0111] = A[7];
+ * <p>
+ *         C[8] = C[1000] = A[1]+A[2]+A[3]+A[4]+A[5]+A[6]+A[7]+A[8];
+ * <p>
+ * 对照式子可以发现  C[i]=A[i-2^k+1]+A[i-2^k+2]+......A[i]; （k为i的二进制中从最低位到高位连续零的长度）例如i=8(1000)时，k=3;
+ * <p>
+ * C[8] = A[8-2^3+1]+A[8-2^3+2]+......+A[8]
+ * ————————————————
+ * 版权声明：本文为CSDN博主「bestsort」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+ * 原文链接：https://blog.csdn.net/bestsort/article/details/80796531
+ * <p>
+ * <p>
  * 树状数组的Java版本，created by 曹艳丰  2016.07.09
  * 原理参考：http://www.hawstein.com/posts/binary-indexed-trees.html
  * 或：https://www.topcoder.com/community/data-science/data-science-tutorials/binary-indexed-trees/
