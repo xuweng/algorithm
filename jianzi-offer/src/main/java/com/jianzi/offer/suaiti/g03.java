294d01f169ee31bc4a59371f259968c1aa47fbad
xuweng <2028403305@qq.com>
2020-03-27 16:07:10 +0800

    官方答案：面试题03. 数组中重复的数字

---
+++ jianzi-offer/src/main/java/com/jianzi/offer/suaiti/g03.java
@@ -0,0 +1,26 @@
+package com.jianzi.offer.suaiti;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 作者：LeetCode-Solution
+ * 链接：https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/solution/mian-shi-ti-03-shu-zu-zhong-zhong-fu-de-shu-zi-b-4/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class g03 {
+    public int findRepeatNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int repeat = -1;
+        for (int num : nums) {
+            if (!set.add(num)) {
+                repeat = num;
+                break;
+            }
+        }
+        return repeat;
+    }
+
+
+}
