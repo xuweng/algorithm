02a13b1be04e2de5a38eee1f7e84cd5425f18f58
xuweng <2028403305@qq.com>
2020-03-31 16:43:29 +0800

    面试题10- II. 青蛙跳台阶问题

---
+++ jianzi-offer/src/main/java/com/jianzi/offer/suati/s102.java
@@ -0,0 +1,26 @@
+package com.jianzi.offer.suati;
+
+/**
+ * 面试题10- II. 青蛙跳台阶问题
+ */
+public class s102 {
+    /**
+     * 作者：jyd
+     * 链接：https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof/solution/mian-shi-ti-10-ii-qing-wa-tiao-tai-jie-wen-ti-dong/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param n
+     * @return
+     */
+    public int numWays(int n) {
+        int a = 1, b = 1, sum;
+        for (int i = 0; i < n; i++) {
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+
+}
