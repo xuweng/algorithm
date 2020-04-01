40934eabcc0a2801e5ddcff079430814b47170cb
xuweng <2028403305@qq.com>
2020-04-01 10:17:28 +0800

    面试题11. 旋转数组的最小数字

---
+++ jianzi-offer/src/main/java/com/jianzi/offer/suati/s11.java
@@ -0,0 +1,27 @@
+package com.jianzi.offer.suati;
+
+/**
+ * 面试题11. 旋转数组的最小数字
+ * <p>
+ * 作者：jyd
+ * 链接：https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/solution/mian-shi-ti-11-xuan-zhuan-shu-zu-de-zui-xiao-shu-3/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class s11 {
+    public int minArray(int[] numbers) {
+        int i = 0, j = numbers.length - 1;
+        while (i < j) {
+            int m = (i + j) / 2;
+            if (numbers[m] > numbers[j]) {
+                i = m + 1;
+            } else if (numbers[m] < numbers[j]) {
+                j = m;
+            } else {
+                j--;
+            }
+        }
+        return numbers[i];
+    }
+
+}
