b304604bd22ddfa2c66d7db8a4bab2e0d36808e0
xuweng <2028403305@qq.com>
2020-04-01 17:48:03 +0800

    面试题12. 矩阵中的路径 dfs。递归。剪纸

---
+++ jianzi-offer/src/main/java/com/jianzi/offer/suati/g12.java
@@ -0,0 +1,84 @@
+package com.jianzi.offer.suati;
+
+/**
+ * 官方
+ * <p>
+ * 面试题12. 矩阵中的路径
+ */
+public class g12 {
+    private int len;
+    private int row;
+    private int col;
+
+    public boolean exist(char[][] board, String word) {
+        len = word.length();
+        if (len == 0) {
+            return false;
+        }
+        row = board.length;
+        col = board[0].length;
+        char c = word.charAt(0);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == c) {
+                    board[i][j] = 0;
+                    if (check(board, word, 1, i, j)) {
+                        return true;
+                    }
+                    board[i][j] = c;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param board 矩阵
+     * @param word  搜索word
+     * @param pos   当前word位置
+     * @param i     当前横坐标
+     * @param j     当前纵坐标
+     * @return
+     */
+    public boolean check(char[][] board, String word, int pos, int i, int j) {
+        if (pos >= len) {
+            return true;
+        }
+        char c = word.charAt(pos);
+
+        //向上
+        if (i > 0 && board[i - 1][j] == c) {
+            board[i - 1][j] = 0;
+            if (check(board, word, pos + 1, i - 1, j)) {
+                return true;
+            }
+            board[i - 1][j] = c;
+        }
+        //向下
+        if (i < row - 1 && board[i + 1][j] == c) {
+            board[i + 1][j] = 0;
+            if (check(board, word, pos + 1, i + 1, j)) {
+                return true;
+            }
+            board[i + 1][j] = c;
+        }
+        //向左
+        if (j > 0 && board[i][j - 1] == c) {
+            board[i][j - 1] = 0;
+            if (check(board, word, pos + 1, i, j - 1)) {
+                return true;
+            }
+            board[i][j - 1] = c;
+        }
+        //向右
+        if (j < col - 1 && board[i][j + 1] == c) {
+            board[i][j + 1] = 0;
+            if (check(board, word, pos + 1, i, j + 1)) {
+                return true;
+            }
+            board[i][j + 1] = c;
+        }
+
+        return false;
+    }
+}
---
+++ jianzi-offer/src/main/java/com/jianzi/offer/suati/s12.java
@@ -0,0 +1,43 @@
+package com.jianzi.offer.suati;
+
+/**
+ * 面试题12. 矩阵中的路径
+ * <p>
+ * 能想到解题方法很厉害
+ * <p>
+ * 本问题是典型的矩阵搜索问题，可使用 深度优先搜索（DFS）+ 剪枝 解决。
+ * <p>
+ * 作者：jyd
+ * 链接：https://leetcode-cn.com/problems/ju-zhen-zhong-de-lu-jing-lcof/solution/mian-shi-ti-12-ju-zhen-zhong-de-lu-jing-shen-du-yo/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class s12 {
+    public boolean exist(char[][] board, String word) {
+        char[] words = word.toCharArray();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(board, words, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    boolean dfs(char[][] board, char[] word, int i, int j, int k) {
+        if (i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word[k]) {
+            return false;
+        }
+        if (k == word.length - 1) {
+            return true;
+        }
+        char tmp = board[i][j];
+        board[i][j] = '/';
+        boolean res = dfs(board, word, i + 1, j, k + 1) || dfs(board, word, i - 1, j, k + 1) ||
+                dfs(board, word, i, j + 1, k + 1) || dfs(board, word, i, j - 1, k + 1);
+        board[i][j] = tmp;
+        return res;
+    }
+
+}
