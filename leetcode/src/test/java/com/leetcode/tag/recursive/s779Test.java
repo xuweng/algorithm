d49a52136912fec233b3215a3b102ea206c05bd7
xuweng <2028403305@qq.com>
2020-04-30 23:31:21 +0800

    779. 第K个语法符号

--- leetcode/src/test/java/com/leetcode/tag/recursive/s779Test.java
+++ leetcode/src/test/java/com/leetcode/tag/recursive/s779Test.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class s779Test {
   @Test
   public void replaceSpaceTest() {
-    int[][] array = {{1, 1}, {2, 1}, {2, 2}, {4, 5}};
+    int[][] array = {{1, 1}, {2, 1}, {2, 2}, {4, 5}, {15, 100}, {30, 434991989}};
     s779 s779 = new s779();
     for (int i = 0; i < array.length; i++) {
       System.out.println(s779.kthGrammar(array[i][0], array[i][1]));
