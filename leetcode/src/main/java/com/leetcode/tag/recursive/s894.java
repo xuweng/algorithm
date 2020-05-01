e7194b4b05f17a6875d383fd58d8043283a621ef
xuweng <2028403305@qq.com>
2020-05-01 18:27:33 +0800

    数据规模缩小多少?

---
+++ leetcode/src/main/java/com/leetcode/tag/recursive/s894.java
@@ -0,0 +1,28 @@
+package com.leetcode.tag.recursive;
+
+import java.util.List;
+
+/**
+ * 新题型.新题型.新题型.新题型.
+ *
+ * <p>数组数据规模.链表数据规模.数数据规模.图数据规模.这些数据结构数据规模缩小?
+ *
+ * <p>返回一个list.不是返回一个数.
+ *
+ * <p>894. 所有可能的满二叉树
+ */
+public class s894 {
+  public List<TreeNode> allPossibleFBT(int N) {
+    return null;
+  }
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+}
