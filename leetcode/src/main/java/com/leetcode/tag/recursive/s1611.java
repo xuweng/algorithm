e5a22196c1b56e8f1ac52974051908d08651c37a
xuweng <2028403305@qq.com>
2020-05-02 13:11:22 +0800

    面试题 16.11. 跳水板 用list判断重复太耗时

--- leetcode/src/main/java/com/leetcode/tag/recursive/s1611.java
+++ leetcode/src/main/java/com/leetcode/tag/recursive/s1611.java
@@ -1,8 +1,8 @@
 package com.leetcode.tag.recursive;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 /**
  * 面试题 16.11. 跳水板
@@ -19,30 +19,38 @@ public class s1611 {
     return arrays;
   }
 
-  public Set<Integer> re(int shorter, int longer, int k) {
-    Set<Integer> set = new HashSet<>();
+  public List<Integer> re(int shorter, int longer, int k) {
+    List<Integer> list = new ArrayList<>();
 
     if (k == 1) {
       // 取shorter
-      set.add(shorter);
+      if (!list.contains(shorter)) {
+        list.add(shorter);
+      }
       // 取longer
-      set.add(longer);
-      return set;
+      if (!list.contains(longer)) {
+        list.add(longer);
+      }
+
+      return list;
     }
     // 子问题
-    Set<Integer> s = re(shorter, longer, k - 1);
-    for (int i : s) {
-      set.add(i + shorter);
-      set.add(i + longer);
+    List<Integer> l = re(shorter, longer, k - 1);
+    for (int i : l) {
+      if (!list.contains(i + shorter)) {
+        list.add(i + shorter);
+      }
+      if (!list.contains(i + longer)) {
+        list.add(i + longer);
+      }
     }
-    return set;
+    return list;
   }
 
-  public int[] toArray(Set<Integer> set) {
-    int[] arrays = new int[set.size()];
-    Integer[] s = set.toArray(new Integer[0]);
-    for (int i = 0; i < s.length; i++) {
-      arrays[i] = s[i];
+  public int[] toArray(List<Integer> list) {
+    int[] arrays = new int[list.size()];
+    for (int i = 0; i < list.size(); i++) {
+      arrays[i] = list.get(i);
     }
 
     return arrays;
