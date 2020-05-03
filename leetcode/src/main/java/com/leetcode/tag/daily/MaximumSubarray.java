c9839caaa9c54ab95c3b5981c37e557324361c64
xuweng <2028403305@qq.com>
2020-05-03 15:53:19 +0800

    最大子序和 暴力法和dp和分治法 分治法:划分子问题包含全部,不要漏掉某个子问题

--- leetcode/src/main/java/com/leetcode/tag/daily/MaximumSubarray.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/MaximumSubarray.java
@@ -63,4 +63,67 @@ public class MaximumSubarray {
 
     return max;
   }
+
+  /**
+   * 分治法
+   *
+   * @param nums
+   * @return
+   */
+  public int fen(int[] nums) {
+    if (nums == null || nums.length <= 0) {
+      return 0;
+    }
+
+    return re(nums, 0, nums.length - 1);
+  }
+
+  /**
+   * 分治法.
+   *
+   * <p>划分子问题包含全部,不要漏掉某个子问题
+   *
+   * <p>3部分:左,右,夸过
+   *
+   * <p>4部分:左,中间,右,夸过
+   *
+   * @param nums
+   * @param start
+   * @param end
+   * @return
+   */
+  private int re(int[] nums, int start, int end) {
+    if (start >= end) {
+      return nums[start];
+    }
+    int middle = start + (end - start) / 2;
+    // 左
+    int left = re(nums, start, middle);
+    // 右
+    int right = re(nums, middle + 1, end);
+    // 夸过middle.需要计算?
+    int m = prSum(nums, start, middle) + houSum(nums, middle + 1, end);
+
+    return Math.max(Math.max(left, right), m);
+  }
+
+  private int prSum(int[] nums, int start, int end) {
+    int sum = 0;
+    int max = Integer.MIN_VALUE;
+    for (int i = end; i >= start; i--) {
+      sum += nums[i];
+      max = Math.max(max, sum);
+    }
+    return max;
+  }
+
+  private int houSum(int[] nums, int start, int end) {
+    int sum = 0;
+    int max = Integer.MIN_VALUE;
+    for (int i = start; i <= end; i++) {
+      sum += nums[i];
+      max = Math.max(max, sum);
+    }
+    return max;
+  }
 }
