55c39c2a8fc582b169df1543fe230dcfdcbf3a79
xuweng <2028403305@qq.com>
2020-05-05 16:56:44 +0800

    注意递归调用顺序

--- leetcode/src/main/java/com/leetcode/tag/recursive/s698.java
+++ leetcode/src/main/java/com/leetcode/tag/recursive/s698.java
@@ -23,24 +23,40 @@ public class s698 {
         return result.get();
     }
 
+    /**
+     * 把树放入list
+     *
+     * <p>把整个树放入list;把左子树放入list;把右子树放入list
+     *
+     * <p>把前n放入map
+     *
+     * @param nums
+     * @param n
+     * @param map
+     */
     private void re(int[] nums, int n, Map<Integer, Integer> map) {
         if (n <= 0) {
             map.put(nums[n], 1);
             return;
         }
-        if (map.containsKey(nums[n])) {
-            map.put(nums[n], map.get(nums[n]) + 1);
-        }
-
+        // 注意递归调用顺序.逻辑清晰
+        // 递归处理子问题.先把前n-1放入map,再处理n
+        re(nums, n - 1, map);
+        // 处理n.知道哪里错误
+        // 包含nums[n]如何计算?不包含nums[n]如何计算?
         map.forEach(
                 (k, v) -> {
-                    if (k != nums[n]) {
-                        map.put(nums[n] + k, 1);
+                    int key = nums[n] + k;
+                    if (map.containsKey(key)) {
+                        map.put(key, map.get(key) + 1);
+                    } else {
+                        map.put(key, 1);
                     }
                 });
-        if (!map.containsKey(nums[n])) {
+        if (map.containsKey(nums[n])) {
+            map.put(nums[n], map.get(nums[n]) + 1);
+        } else {
             map.put(nums[n], 1);
         }
-        re(nums, n - 1, map);
     }
 }
