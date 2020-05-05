ddf043bcfb57f6bf256d186514cd438efa2f54c6
xuweng <2028403305@qq.com>
2020-05-05 22:00:52 +0800

    整体思路正确就,修修补补 小数据规模验证

--- leetcode/src/main/java/com/leetcode/tag/recursive/s938.java
+++ leetcode/src/main/java/com/leetcode/tag/recursive/s938.java
@@ -18,6 +18,35 @@ public class s938 {
     return sum;
   }
 
+  /**
+   * 整体思路正确就,修修补补
+   *
+   * <p>小数据规模验证
+   *
+   * @param root
+   * @param L
+   * @param R
+   * @return
+   */
+  public int re(TreeNode root, int L, int R) {
+    if (root == null) {
+      return 0;
+    }
+    if (R < root.val) {
+      return re(root.left, L, R);
+    }
+    if (R == root.val) {
+      return re(root.left, L, R) + root.val;
+    }
+    if (L == root.val) {
+      return re(root.right, L, R) + root.val;
+    }
+    if (L > root.val) {
+      return re(root.right, L, R);
+    }
+    return re(root.left, L, R) + re(root.right, L, R) + root.val;
+  }
+
   public class TreeNode {
     int val;
     TreeNode left;
