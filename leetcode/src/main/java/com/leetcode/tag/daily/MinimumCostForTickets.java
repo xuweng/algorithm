11d6d744209c469030cd20a479613667e3f05fdf
xuweng <2028403305@qq.com>
2020-05-06 19:53:59 +0800

    动态规划不是很明显

--- leetcode/src/main/java/com/leetcode/tag/daily/MinimumCostForTickets.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/MinimumCostForTickets.java
@@ -1,5 +1,8 @@
 package com.leetcode.tag.daily;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 983. 最低票价
  *
@@ -62,5 +65,37 @@ public class MinimumCostForTickets {
       i--;
     }
     return i + 1;
+  }
+
+  class Solution {
+    int[] costs;
+    Integer[] memo;
+    Set<Integer> dayset;
+
+    public int mincostTickets(int[] days, int[] costs) {
+      this.costs = costs;
+      memo = new Integer[366];
+      dayset = new HashSet();
+      for (int d : days) {
+        dayset.add(d);
+      }
+      return dp(1);
+    }
+
+    public int dp(int i) {
+      if (i > 365) {
+        return 0;
+      }
+      if (memo[i] != null) {
+        return memo[i];
+      }
+      if (dayset.contains(i)) {
+        memo[i] =
+                Math.min(Math.min(dp(i + 1) + costs[0], dp(i + 7) + costs[1]), dp(i + 30) + costs[2]);
+      } else {
+        memo[i] = dp(i + 1);
+      }
+      return memo[i];
+    }
   }
 }
