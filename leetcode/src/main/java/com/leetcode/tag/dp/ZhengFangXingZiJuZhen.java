e066a3d10862727530d5dbffb8317455e7ee9c30
xuweng <2028403305@qq.com>
2020-05-08 21:52:01 +0800

    统计全为 1 的正方形子矩阵

---
+++ leetcode/src/main/java/com/leetcode/tag/dp/ZhengFangXingZiJuZhen.java
@@ -0,0 +1,48 @@
+package com.leetcode.tag.dp;
+
+/**
+ * 递推式：
+ *
+ * <p>按照递推式写代码
+ *
+ * <p>f[i][j]= ⎩ ⎪ ⎨ ⎪ ⎧ ​
+ *
+ * <p>matrix[i][j] 0 min(f[i][j−1],f[i−1][j],f[i−1][j−1])+1 ​
+ *
+ * <p>,if i==0 or j==0 ,if matrix[i][j]==0 ,otherwise ​
+ *
+ * <p>作者：LeetCode-Solution
+ * 链接：https://leetcode-cn.com/problems/count-square-submatrices-with-all-ones/solution/tong-ji-quan-wei-1-de-zheng-fang-xing-zi-ju-zhen-2/
+ * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。 统计全为 1 的正方形子矩阵
+ */
+public class ZhengFangXingZiJuZhen {
+  /**
+   * 统计全为 1 的正方形子矩阵
+   *
+   * @param matrix
+   * @return
+   */
+  public int countSquares(int[][] matrix) {
+    if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+      return 0;
+    }
+    int rows = matrix.length;
+    int columns = matrix[0].length;
+    int[][] dp = new int[rows][columns];
+    int sum = 0;
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < columns; j++) {
+        if (i == 0 || j == 0) {
+          dp[i][j] = matrix[i][j];
+        } else if (matrix[i][j] == 0) {
+          dp[i][j] = 0;
+        } else {
+          dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+        }
+        sum += dp[i][j];
+      }
+    }
+
+    return sum;
+  }
+}
