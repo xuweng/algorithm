0d4ed3b59f7a193371dca1ef65cf9a44dec81152
xuweng <2028403305@qq.com>
2020-05-09 23:46:43 +0800

    官方

--- leetcode/src/main/java/com/leetcode/tag/divide/SmallestK.java
+++ leetcode/src/main/java/com/leetcode/tag/divide/SmallestK.java
@@ -1,5 +1,6 @@
 package com.leetcode.tag.divide;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 /**
@@ -29,5 +30,98 @@ public class SmallestK {
       result[i] = maxHeap.poll();
     }
     return result;
+  }
+
+  class Solution {
+    public int[] smallestK(int[] arr, int k) {
+      if (k >= arr.length) {
+        return arr;
+      }
+      if (k == 0) {
+        return Arrays.copyOf(arr, k);
+      }
+      int left = 0;
+      int right = arr.length - 1;
+      int partition = getPartition(arr, left, right);
+
+      while (partition != k - 1) {
+        if (partition > k - 1) {
+          right = partition - 1;
+        } else {
+          left = partition + 1;
+        }
+        partition = getPartition(arr, left, right);
+      }
+      return Arrays.copyOf(arr, k);
+    }
+
+    public int getPartition(int[] arr, int left, int right) {
+      int pivot = arr[left];
+      int leftr = left;
+      int rightr = right + 1;
+      while (leftr < rightr) {
+        while (arr[++leftr] < pivot) {
+          if (leftr == right) {
+            break;
+          }
+        }
+        while (arr[--rightr] > pivot) {
+          if (rightr == left) {
+            break;
+          }
+        }
+        if (leftr >= rightr) {
+          break;
+        }
+        int temp = arr[leftr];
+        arr[leftr] = arr[rightr];
+        arr[rightr] = temp;
+      }
+      int temp = arr[rightr];
+      arr[rightr] = pivot;
+      arr[left] = temp;
+      return rightr;
+    }
+  }
+
+  class Solution2 {
+    public int[] smallestK(int[] arr, int k) {
+      if (k == 0 || arr.length == 0) {
+        return new int[0];
+      }
+
+      int low = 0;
+      int high = arr.length - 1;
+      while (low < high) {
+        int pos = partition(arr, low, high);
+        if (pos == k - 1) {
+          break;
+        } else if (pos < k - 1) {
+          low = pos + 1;
+        } else {
+          high = pos - 1;
+        }
+      }
+
+      int[] dest = new int[k];
+      System.arraycopy(arr, 0, dest, 0, k);
+      return dest;
+    }
+
+    private int partition(int[] arr, int low, int high) {
+      int pivot = arr[low];
+      while (low < high) {
+        while (low < high && arr[high] >= pivot) {
+          high--;
+        }
+        arr[low] = arr[high];
+        while (low < high && arr[low] <= pivot) {
+          low++;
+        }
+        arr[high] = arr[low];
+      }
+      arr[low] = pivot;
+      return low;
+    }
   }
 }
