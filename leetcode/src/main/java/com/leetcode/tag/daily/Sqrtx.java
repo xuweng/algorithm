2a3a1f1f96966eff868c84c582ec1a73edcb3a54
xuweng <2028403305@qq.com>
2020-05-09 10:24:10 +0800

    x的平方根

--- leetcode/src/main/java/com/leetcode/tag/daily/Sqrtx.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/Sqrtx.java
@@ -39,4 +39,39 @@ public class Sqrtx {
       return re(low, (int) middle, x);
     }
   }
+
+  class Solution {
+    /**
+     * 袖珍计算器算法
+     *
+     * @param x
+     * @return
+     */
+    public int mySqrt(int x) {
+      if (x == 0) {
+        return 0;
+      }
+      int ans = (int) Math.exp(0.5 * Math.log(x));
+      return (long) (ans + 1) * (ans + 1) <= x ? ans + 1 : ans;
+    }
+  }
+
+  /**
+   * 二分查找
+   */
+  class Solution2 {
+    public int mySqrt(int x) {
+      int l = 0, r = x, ans = -1;
+      while (l <= r) {
+        int mid = l + (r - l) / 2;
+        if ((long) mid * mid <= x) {
+          ans = mid;
+          l = mid + 1;
+        } else {
+          r = mid - 1;
+        }
+      }
+      return ans;
+    }
+  }
 }
