f63187fddc626cd5a0d4858314b79643a7c3b80a
xuweng <2028403305@qq.com>
2020-05-09 09:45:39 +0800

    x的平方根

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/Sqrtx.java
@@ -0,0 +1,25 @@
+package com.leetcode.tag.daily;
+
+/**
+ * 69. x 的平方根
+ */
+public class Sqrtx {
+  public int mySqrt(int x) {
+    return re(0, x);
+  }
+
+  public int re(int start, int x) {
+    if (x - start <= 1) {
+      return start;
+    }
+    int middle = start + (x - start) >> 1;
+    int ping = middle * middle;
+    if (ping == x) {
+      return middle;
+    } else if (ping < x) {
+      return re(middle, x);
+    } else {
+      return re(start, middle);
+    }
+  }
+}
---
+++ leetcode/src/test/java/com/leetcode/tag/daily/SqrtxTest.java
@@ -0,0 +1,12 @@
+package com.leetcode.tag.daily;
+
+import org.junit.Test;
+
+public class SqrtxTest {
+  @Test
+  public void replaceSpaceTest() {
+    Sqrtx sqrtx = new Sqrtx();
+
+    sqrtx.mySqrt(5);
+  }
+}
