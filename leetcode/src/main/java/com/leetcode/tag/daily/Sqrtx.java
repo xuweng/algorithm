4654d4406e492e8eaa21127dc482a406dd13a779
xuweng <2028403305@qq.com>
2020-05-09 10:17:27 +0800

    x的平方根

--- leetcode/src/main/java/com/leetcode/tag/daily/Sqrtx.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/Sqrtx.java
@@ -5,25 +5,38 @@ package com.leetcode.tag.daily;
  */
 public class Sqrtx {
   public int mySqrt(int x) {
-    return re(0, x);
+    return re(0, x, x);
   }
 
-  public int re(int start, int x) {
+  /**
+   * 因为leetcode的test case x=2147395599，
+   *
+   * <p>在算mid*mid的时候造成溢出，所以mid不能使用int型来接，要使用long型防止溢出
+   *
+   * <p>（Java中Integer型的范围：-2147483648 到2147483648）
+   *
+   * @param low
+   * @param high
+   * @param x
+   * @return
+   */
+  public int re(int low, int high, int x) {
     if (x <= 1) {
       return x;
     }
     // 递归终止条件
-    if (x - start <= 1) {
-      return start;
+    if (high - low <= 1) {
+      return low;
     }
-    int middle = start + (x - start) / 2;
-    int ping = middle * middle;
+    // 在算mid*mid的时候造成溢出，所以mid不能使用int型来接，要使用long型防止溢出
+    long middle = low + (high - low) / 2;
+    long ping = middle * middle;
     if (ping == x) {
-      return middle;
+      return (int) middle;
     } else if (ping < x) {
-      return re(middle, x);
+      return re((int) middle, high, x);
     } else {
-      return re(start, middle);
+      return re(low, (int) middle, x);
     }
   }
 }
