e409d09c7b3c6c428390921b130ec95c5d831437
xuweng <2028403305@qq.com>
2020-05-09 11:46:31 +0800

    理解后算法很简单

--- leetcode/src/main/java/com/leetcode/tag/dp/JumpGame.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/JumpGame.java
@@ -22,4 +22,28 @@ public class JumpGame {
 
     return dp[nums.length - 1];
   }
+
+  /**
+   * 贪心
+   *
+   * <p>自己理解
+   *
+   * <p>理解后算法很简单
+   *
+   * @param nums
+   * @return
+   */
+  public boolean tan(int[] nums) {
+    // 最远位置下标
+    int maxIndex = nums[0];
+    for (int i = 1; i < nums.length && maxIndex < nums.length - 1; i++) {
+      // 从i位置下标最远可以跳到位置
+      int index = i + nums[i];
+      if (i <= maxIndex && index > maxIndex) {
+        maxIndex = index;
+      }
+    }
+
+    return maxIndex >= nums.length - 1;
+  }
 }
