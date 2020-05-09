1aadc2f54fd26a94787c54159958809e2edaefb6
xuweng <2028403305@qq.com>
2020-05-09 09:59:32 +0800

    x的平方根

--- leetcode/src/main/java/com/leetcode/tag/daily/Sqrtx.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/Sqrtx.java
@@ -9,10 +9,14 @@ public class Sqrtx {
   }
 
   public int re(int start, int x) {
+    if (x <= 1) {
+      return x;
+    }
+    // 递归终止条件
     if (x - start <= 1) {
       return start;
     }
-    int middle = start + (x - start) >> 1;
+    int middle = start + (x - start) / 2;
     int ping = middle * middle;
     if (ping == x) {
       return middle;
