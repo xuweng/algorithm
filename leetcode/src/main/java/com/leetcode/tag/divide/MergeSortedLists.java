0c6ec188eec10ed83cc2a294a9a5724d11665878
xuweng <2028403305@qq.com>
2020-05-09 13:01:14 +0800

    标准分治

--- leetcode/src/main/java/com/leetcode/tag/divide/MergeSortedLists.java
+++ leetcode/src/main/java/com/leetcode/tag/divide/MergeSortedLists.java
@@ -1,5 +1,7 @@
 package com.leetcode.tag.divide;
 
+import java.util.PriorityQueue;
+
 /**
  * 标准分治
  *
@@ -58,6 +60,64 @@ public class MergeSortedLists {
     }
 
     return (l1.val <= l2.val) ? l1 : l2;
+  }
+
+  /**
+   * 优先级队列
+   *
+   * <p>时间复杂度：O(n*log(k))，n 是所有链表中元素的总和，k 是链表个数。
+   *
+   * <p>作者：powcai
+   * 链接：https://leetcode-cn.com/problems/merge-k-sorted-lists/solution/leetcode-23-he-bing-kge-pai-xu-lian-biao-by-powcai/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution {
+    /**
+     * 纵向看lists
+     *
+     * @param lists
+     * @return
+     */
+    public ListNode mergeKLists(ListNode[] lists) {
+      if (lists == null || lists.length == 0) {
+        return null;
+      }
+      // 优先队列
+      // 最小堆
+      PriorityQueue<ListNode> queue =
+              new PriorityQueue<>(
+                      lists.length,
+                      (o1, o2) -> {
+                        if (o1.val < o2.val) {
+                          return -1;
+                        } else if (o1.val == o2.val) {
+                          return 0;
+                        } else {
+                          return 1;
+                        }
+                      });
+
+      for (ListNode node : lists) {
+        if (node != null) {
+          queue.add(node);
+        }
+      }
+      // 最小值并且出队
+      // 头结点.一定要新建一个头结点
+      ListNode head = new ListNode(0);
+      // 尾结点
+      ListNode tail = head;
+      while (!queue.isEmpty()) {
+        // 最小值并且出队
+        ListNode node = queue.poll();
+        tail.next = node;
+        tail = node;
+        if (node.next != null) {
+          queue.add(node.next);
+        }
+      }
+      return head.next;
+    }
   }
 
   public class ListNode {
