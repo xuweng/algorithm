e6a92cdce0a1b9063c522fad466ac2f420a12bb2
xuweng <2028403305@qq.com>
2020-05-10 22:46:10 +0800

    子问题依赖

--- leetcode/src/main/java/com/leetcode/tag/divide/BurstBalloons.java
+++ leetcode/src/main/java/com/leetcode/tag/divide/BurstBalloons.java
@@ -74,7 +74,19 @@ public class BurstBalloons {
     }
 
     /**
-     * 区间dp
+     * 我们一开始的想法是先假设第一个被戳爆的气球为x，则x两边的气球则产生了依赖；相邻问题
+     *
+     * <p>每当你戳破一个气球 i 时，你可以获得 nums[left] * nums[i] * nums[right]
+     *
+     * <p>i-1,i,i+1.相邻问题.左边和右边相互依赖
+     *
+     * <p>子问题依赖
+     *
+     * <p>那我们假设不戳爆x，则x两边的气球就没有了依赖关系！这个气球x，我们可以放在最后戳爆它。
+     *
+     * <p>作者：mei-tian-you-ni
+     * 链接：https://leetcode-cn.com/problems/burst-balloons/solution/jie-ti-guan-jian-xu-ni-qi-qiu-hui-su-fen-zhi-dong-/
+     * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。 区间dp
      *
      * <p>定义方法 dp，使其返回开区间 (left, right) 中所能得到的最大金币数
      *
@@ -103,7 +115,7 @@ public class BurstBalloons {
                         ans,
                         nums[left] * nums[i] * nums[right]
                                 + dp(memo, nums, left, i)
-                                + dp(memo, nums, i, right));
+                    + dp(memo, nums, i, right));
       }
       // add to the cache
       memo[left][right] = ans;
@@ -112,7 +124,17 @@ public class BurstBalloons {
   }
 
   /**
-   * 方法二：动态规划（自底向上）
+   * 问题都搞不清楚
+   *
+   * <p>搞清楚问题
+   *
+   * <p>我们在第一次戳气球的时候选择编号为k1（k1 >=1 and k1 <= n）的气球戳破；此时原问题被划分为 [1, k1)和 (k1, n]这两个子问题
+   *
+   * <p>假设气球的编号范围为 [i, j]，我们选择编号为k1的气球最后戳爆
+   *
+   * <p>即[1, k1) 这个子问题的答案和(k1, n]这个子问题的答案是互相依赖的
+   *
+   * <p>方法二：动态规划（自底向上）
    */
   class Solution2 {
     /**
