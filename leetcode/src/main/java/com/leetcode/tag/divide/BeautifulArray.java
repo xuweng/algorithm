b616a868cb5ff0312dd7f71242ae1e2107588435
xuweng <2028403305@qq.com>
2020-05-10 17:22:53 +0800

    932. 漂亮数组 新题型

---
+++ leetcode/src/main/java/com/leetcode/tag/divide/BeautifulArray.java
@@ -0,0 +1,20 @@
+package com.leetcode.tag.divide;
+
+/**
+ * 932. 漂亮数组
+ */
+public class BeautifulArray {
+  /**
+   * 递推方程?怎么递推? n-1推出n?
+   *
+   * <p>-1还是两两划分?
+   *
+   * <p>新题型?新题型?新题型
+   *
+   * @param N
+   * @return
+   */
+  public int[] beautifulArray(int N) {
+    return null;
+  }
+}
