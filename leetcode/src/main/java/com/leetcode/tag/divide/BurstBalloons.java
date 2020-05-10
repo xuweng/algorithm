099d73e6c0af10f156b47a736e7ef78cc1c71591
xuweng <2028403305@qq.com>
2020-05-10 18:24:16 +0800

    区间dp

--- leetcode/src/main/java/com/leetcode/tag/divide/BurstBalloons.java
+++ leetcode/src/main/java/com/leetcode/tag/divide/BurstBalloons.java
@@ -31,4 +31,65 @@ public class BurstBalloons {
   public int maxCoins(int[] nums) {
     return 0;
   }
+
+  /**
+   * 作者：LeetCode
+   * 链接：https://leetcode-cn.com/problems/burst-balloons/solution/chuo-qi-qiu-by-leetcode/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution {
+    public int maxCoins(int[] nums) {
+
+      // reframe the problem
+      int n = nums.length + 2;
+      int[] new_nums = new int[n];
+
+      System.arraycopy(nums, 0, new_nums, 1, nums.length);
+
+      new_nums[0] = new_nums[n - 1] = 1;
+
+      // cache the results of dp
+      int[][] memo = new int[n][n];
+
+      // find the maximum number of coins obtained from adding all balloons from (0, len(nums) - 1)
+      return dp(memo, new_nums, 0, n - 1);
+    }
+
+    /**
+     * 区间dp
+     *
+     * <p>定义方法 dp，使其返回开区间 (left, right) 中所能得到的最大金币数
+     *
+     * @param memo
+     * @param nums
+     * @param left
+     * @param right
+     * @return
+     */
+    public int dp(int[][] memo, int[] nums, int left, int right) {
+      // no more balloons can be added
+      if (left + 1 == right) {
+        return 0;
+      }
+
+      // we've already seen this, return from cache
+      if (memo[left][right] > 0) {
+        return memo[left][right];
+      }
+
+      // add each balloon on the interval and return the maximum score
+      int ans = 0;
+      for (int i = left + 1; i < right; ++i) {
+        ans =
+                Math.max(
+                        ans,
+                        nums[left] * nums[i] * nums[right]
+                                + dp(memo, nums, left, i)
+                                + dp(memo, nums, i, right));
+      }
+      // add to the cache
+      memo[left][right] = ans;
+      return ans;
+    }
+  }
 }
