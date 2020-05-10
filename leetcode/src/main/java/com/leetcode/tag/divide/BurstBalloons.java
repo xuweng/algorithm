3ee257d17242ce79e9141b84abe0bd47b7000ba1
xuweng <2028403305@qq.com>
2020-05-11 00:13:04 +0800

    看答案通过 子问题依赖

--- leetcode/src/main/java/com/leetcode/tag/divide/BurstBalloons.java
+++ leetcode/src/main/java/com/leetcode/tag/divide/BurstBalloons.java
@@ -6,6 +6,7 @@ package com.leetcode.tag.divide;
  * <p>-1、两两划分
  */
 public class BurstBalloons {
+  int[][] me;
   /**
    * 想不出方程
    *
@@ -29,7 +30,46 @@ public class BurstBalloons {
    * @return
    */
   public int maxCoins(int[] nums) {
-    return 0;
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+    if (nums.length == 1) {
+      return nums[0];
+    }
+    int newLength = nums.length + 2;
+    me = new int[newLength][newLength];
+
+    int[] newNums = new int[newLength];
+    newNums[0] = 1;
+    newNums[newNums.length - 1] = 1;
+    System.arraycopy(nums, 0, newNums, 1, nums.length);
+    return re(newNums, 0, newNums.length - 1);
+  }
+
+  /**
+   * 第low和第high都是不可戳
+   *
+   * @param nums
+   * @param low
+   * @param high
+   * @return
+   */
+  public int re(int[] nums, int low, int high) {
+    if (me[low][high] != 0) {
+      return me[low][high];
+    }
+    // 递归终止条件
+    // 终止条件特别麻烦
+    if (high - low <= 1) {
+      return 0;
+    }
+    int max = Integer.MIN_VALUE;
+    // i的范围?low<i<high
+    for (int i = low + 1; i <= high - 1; i++) {
+      max = Math.max(max, re(nums, low, i) + re(nums, i, high) + nums[low] * nums[i] * nums[high]);
+    }
+    me[low][high] = max;
+    return max;
   }
 
   /**
