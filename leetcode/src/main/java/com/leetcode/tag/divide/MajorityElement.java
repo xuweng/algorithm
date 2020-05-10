02c0c09cf937cc5b5f3e7cf085571e622b358238
xuweng <2028403305@qq.com>
2020-05-10 16:56:39 +0800

    分治套路

--- leetcode/src/main/java/com/leetcode/tag/divide/MajorityElement.java
+++ leetcode/src/main/java/com/leetcode/tag/divide/MajorityElement.java
@@ -40,4 +40,67 @@ public class MajorityElement {
 
     return 0;
   }
+
+  /**
+   * 作者：LeetCode-Solution
+   * 链接：https://leetcode-cn.com/problems/majority-element/solution/duo-shu-yuan-su-by-leetcode-solution/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution {
+    private int countInRange(int[] nums, int num, int lo, int hi) {
+      int count = 0;
+      for (int i = lo; i <= hi; i++) {
+        if (nums[i] == num) {
+          count++;
+        }
+      }
+      return count;
+    }
+
+    /**
+     * 我们就可以使用分治法解决这个问题：将数组分成左右两部分，
+     *
+     * <p>分别求出左半部分的众数 a1 以及右半部分的众数 a2，随后在 a1 和 a2 中选出正确的众数。
+     *
+     * <p>我们使用经典的分治算法递归求解，直到所有的子问题都是长度为 1 的数组。
+     *
+     * <p>长度为 1 的子数组中唯一的数显然是众数，直接返回即可。如果回溯后某区间的长度大于
+     *
+     * <p>1，我们必须将左右子区间的值合并。如果它们的众数相同，那么显然这一段区间的众数是它们相同的值。
+     *
+     * <p>否则，我们需要比较两个众数在整个区间内出现的次数来决定该区间的众数。
+     *
+     * @param nums
+     * @param lo
+     * @param hi
+     * @return
+     */
+    private int majorityElementRec(int[] nums, int lo, int hi) {
+      // base case; the only element in an array of size 1 is the majority
+      // element.
+      if (lo == hi) {
+        return nums[lo];
+      }
+
+      // recurse on left and right halves of this slice.
+      int mid = (hi - lo) / 2 + lo;
+      int left = majorityElementRec(nums, lo, mid);
+      int right = majorityElementRec(nums, mid + 1, hi);
+
+      // if the two halves agree on the majority element, return it.
+      if (left == right) {
+        return left;
+      }
+
+      // otherwise, count each element and return the "winner".
+      int leftCount = countInRange(nums, left, lo, mid);
+      int rightCount = countInRange(nums, right, mid + 1, hi);
+
+      return leftCount > rightCount ? left : right;
+    }
+
+    public int majorityElement(int[] nums) {
+      return majorityElementRec(nums, 0, nums.length - 1);
+    }
+  }
 }
