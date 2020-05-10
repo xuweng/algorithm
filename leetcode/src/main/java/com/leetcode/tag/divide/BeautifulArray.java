c76230b1c9d74c5d7121fa0e9b565daf2616c185
xuweng <2028403305@qq.com>
2020-05-10 17:43:03 +0800

    官方

--- leetcode/src/main/java/com/leetcode/tag/divide/BeautifulArray.java
+++ leetcode/src/main/java/com/leetcode/tag/divide/BeautifulArray.java
@@ -1,7 +1,12 @@
 package com.leetcode.tag.divide;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 932. 漂亮数组
+ *
+ * <p>想不出就看答案
  */
 public class BeautifulArray {
   /**
@@ -18,5 +23,42 @@ public class BeautifulArray {
    */
   public int[] beautifulArray(int N) {
     return null;
+  }
+
+  /**
+   * 这题技巧性强，不容易想到
+   *
+   * <p>分治算法，如何划分出子问题是个能力啊
+   */
+  class Solution {
+    Map<Integer, int[]> memo;
+
+    public int[] beautifulArray(int N) {
+      memo = new HashMap();
+      return f(N);
+    }
+
+    public int[] f(int N) {
+      if (memo.containsKey(N)) {
+        return memo.get(N);
+      }
+
+      int[] ans = new int[N];
+      if (N == 1) {
+        ans[0] = 1;
+      } else {
+        int t = 0;
+        for (int x : f((N + 1) / 2)) // odds
+        {
+          ans[t++] = 2 * x - 1;
+        }
+        for (int x : f(N / 2)) // evens
+        {
+          ans[t++] = 2 * x;
+        }
+      }
+      memo.put(N, ans);
+      return ans;
+    }
   }
 }
