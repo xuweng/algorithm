5cbe1453f94d06fe22ec3aa8b76c9107b836e94b
xuweng <2028403305@qq.com>
2020-05-11 10:31:10 +0800

    两个参数,递归终止条件比较麻烦 数组大小限制

--- leetcode/src/main/java/com/leetcode/tag/daily/PowxN.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/PowxN.java
@@ -1,32 +1,38 @@
 package com.leetcode.tag.daily;
 
+import java.util.Arrays;
+
 /**
  * 实现 pow(x, n) ，即计算 x 的 n 次幂函数。
  */
 public class PowxN {
+  double[] me;
+
   public double myPow(double x, int n) {
+    me = new double[Math.abs(n) + 1];
+    Arrays.fill(me, Double.MIN_VALUE);
+
+    return (n >= 0) ? re(x, n) : 1 / re(x, -n);
+  }
+
+  public double re(double x, int n) {
+    if (me[n] != Double.MIN_VALUE) {
+      return me[n];
+    }
     // 递归终止条件
     // 两个参数,递归终止条件比较麻烦
-    if (n == 0) {
+    if (n == 0.0 || x == 1.0) {
       return 1.0;
-    }
-    if (x == 1) {
-      return 1;
     }
     if (x == -1) {
       return n % 2 == 0 ? 1 : -1;
     }
     if (n == 1) {
-      return x;
+      me[n] = x;
+      return me[n];
     }
-    if (n < 0) {
-      return 1 / myPow(x, -n);
-    }
-    double mid = myPow(x, n >> 1);
-    if (n % 2 == 0) {
-      return mid * mid;
-    } else {
-      return mid * mid * x;
-    }
+    double mid = re(x, n >> 1);
+    me[n] = n % 2 == 0 ? mid * mid : mid * mid * x;
+    return me[n];
   }
 }
