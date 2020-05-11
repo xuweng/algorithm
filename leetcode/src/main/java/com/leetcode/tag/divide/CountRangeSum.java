720da96d37586fa0e515c51fcb94ef735cf84481
xuweng <2028403305@qq.com>
2020-05-11 21:56:25 +0800

    优化内循环

--- leetcode/src/main/java/com/leetcode/tag/divide/CountRangeSum.java
+++ leetcode/src/main/java/com/leetcode/tag/divide/CountRangeSum.java
@@ -4,6 +4,16 @@ package com.leetcode.tag.divide;
  * 327. 区间和的个数
  */
 public class CountRangeSum {
+  /**
+   * 最直观的算法复杂度是 O(n2) ，请在此基础上优化你的算法。
+   *
+   * <p>优化内循环?
+   *
+   * @param nums
+   * @param lower
+   * @param upper
+   * @return
+   */
   public int countRangeSum(int[] nums, int lower, int upper) {
     if (nums == null || nums.length == 0) {
       return 0;
