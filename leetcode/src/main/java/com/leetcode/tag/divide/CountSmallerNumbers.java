d1e558d371f8b9b60ece07eed03399ebbb793967
xuweng <2028403305@qq.com>
2020-05-11 18:04:31 +0800

    线段树

--- leetcode/src/main/java/com/leetcode/tag/divide/CountSmallerNumbers.java
+++ leetcode/src/main/java/com/leetcode/tag/divide/CountSmallerNumbers.java
@@ -49,9 +49,9 @@ public class CountSmallerNumbers {
    *
    * <p>反常思维.反常思维.反常思维.反常思维.反常思维.反常思维.反常思维.反常思维.反常思维
    *
-   * <p>左----->右
+   * <p>左----.右
    *
-   * <p>右----->左
+   * <p>右----.左
    *
    * <p>搞破.不搞破
    *
@@ -90,4 +90,155 @@ public class CountSmallerNumbers {
 
     return i + 1;
   }
+
+  /**
+   * 线段树
+   *
+   * <p>线段范围
+   *
+   * <p>start
+   *
+   * <p>end
+   *
+   * <p>线段范围内有几个元素
+   *
+   * <p>count
+   *
+   * <p>指向左右结点的指针
+   *
+   * <p>left（左结点的线段范围是[start, start+(end-start)/2]）
+   *
+   * <p>right(右结点的线段范围是[start+(end-start)/2+1, end])
+   */
+  class SegmentTreeNode {
+    int start;
+    int end;
+    // 线段范围内有几个元素
+    int count;
+
+    SegmentTreeNode left;
+    SegmentTreeNode right;
+
+    public SegmentTreeNode() {
+    }
+
+    SegmentTreeNode(int start, int end) {
+      count = 0;
+      this.start = start;
+      this.end = end;
+    }
+
+    /**
+     * 构造线段树 找出数组中最小和最大的元素，做为根节点的线段范围
+     *
+     * @param start 最小值
+     * @param end   最大值
+     * @return
+     */
+    public SegmentTreeNode build(int start, int end) {
+      // 非法
+      if (start > end) {
+        return null;
+      }
+
+      SegmentTreeNode root = new SegmentTreeNode(start, end);
+      if (start == end) {
+        root.count = 0;
+        return root;
+      }
+      int mid = start + (end - start) / 2;
+      root.left = build(start, mid);
+      root.right = build(mid + 1, end);
+      return root;
+    }
+
+    /**
+     * 返回[start, end]的count值
+     *
+     * @param root
+     * @param start
+     * @param end
+     * @return
+     */
+    int count(SegmentTreeNode root, int start, int end) {
+      if (root == null || start > end) {
+        return 0;
+      }
+      if (start == root.start && end == root.end) {
+        return root.count;
+      }
+      int mid = root.start + (root.end - root.start) / 2;
+      int leftcount = 0, rightcount = 0;
+
+      if (start <= mid) {
+        if (mid < end) {
+          leftcount = count(root.left, start, mid);
+        } else {
+          leftcount = count(root.left, start, end);
+        }
+      }
+
+      if (mid < end) {
+        if (start <= mid) {
+          rightcount = count(root.right, mid + 1, end);
+        } else {
+          rightcount = count(root.right, start, end);
+        }
+      }
+
+      return (leftcount + rightcount);
+    }
+
+    /**
+     * 插入一个元素，并更新线段树的count值
+     *
+     * @param root
+     * @param index
+     * @param val
+     */
+    void insert(SegmentTreeNode root, int index, int val) {
+      if (root.start == index && root.end == index) {
+        root.count += val;
+        return;
+      }
+
+      int mid = root.start + (root.end - root.start) / 2;
+      if (index >= root.start && index <= mid) {
+        insert(root.left, index, val);
+      }
+      if (index > mid && index <= root.end) {
+        insert(root.right, index, val);
+      }
+
+      // 左子树和右子树搞完后,更新root的count
+      root.count = root.left.count + root.right.count;
+    }
+  }
+
+  ;
+
+  /**
+   * 作者：Dufre
+   * 链接：https://leetcode-cn.com/problems/count-of-smaller-numbers-after-self/solution/c-xian-duan-shu-jie-fa-by-dufre/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+      if (nums == null || nums.length == 0) {
+        return new ArrayList<>();
+      }
+      Integer[] res = new Integer[nums.length];
+      int min = Arrays.stream(nums).min().getAsInt();
+      int max = Arrays.stream(nums).max().getAsInt();
+
+      SegmentTreeNode root = new SegmentTreeNode().build(min, max);
+
+      for (int i = nums.length - 1; i >= 0; i--) {
+        res[i] = root.count(root, min, nums[i] - 1);
+        root.insert(root, nums[i], 1);
+      }
+
+      return Arrays.asList(res);
+    }
+  };
 }
