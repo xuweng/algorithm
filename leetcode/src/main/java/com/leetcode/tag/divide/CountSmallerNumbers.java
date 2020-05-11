fd259a6c359938ad7c4420e069e9374bf23d07e2
xuweng <2028403305@qq.com>
2020-05-11 19:18:54 +0800

    树状数组

--- leetcode/src/main/java/com/leetcode/tag/divide/CountSmallerNumbers.java
+++ leetcode/src/main/java/com/leetcode/tag/divide/CountSmallerNumbers.java
@@ -240,4 +240,56 @@ public class CountSmallerNumbers {
       return Arrays.asList(res);
     }
   }
+
+  /**
+   * 树状数组
+   *
+   * <p>作者：liweiwei1419
+   * 链接：https://leetcode-cn.com/problems/count-of-smaller-numbers-after-self/solution/shu-zhuang-shu-zu-by-liweiwei1419/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  public static class FenwickTree {
+    /** 预处理数组 */
+    private int[] tree;
+
+    private int len;
+
+    public FenwickTree(int n) {
+      this.len = n;
+      tree = new int[n + 1];
+    }
+
+    /**
+     * 单点更新
+     *
+     * @param i 原始数组索引 i
+     * @param delta 变化值 = 更新以后的值 - 原始值
+     */
+    public void update(int i, int delta) {
+      // 从下到上更新，注意，预处理数组，比原始数组的 len 大 1，故 预处理索引的最大值为 len
+      while (i <= len) {
+        tree[i] += delta;
+        i += lowbit(i);
+      }
+    }
+
+    /**
+     * 查询前缀和
+     *
+     * @param i 前缀的最大索引，即查询区间 [0, i] 的所有元素之和
+     */
+    public int query(int i) {
+      // 从右到左查询
+      int sum = 0;
+      while (i > 0) {
+        sum += tree[i];
+        i -= lowbit(i);
+      }
+      return sum;
+    }
+
+    public static int lowbit(int x) {
+      return x & (-x);
+    }
+  }
 }
