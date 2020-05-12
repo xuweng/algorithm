eedf41331caee4c83231e714697d7853ded4b442
xuweng <2028403305@qq.com>
2020-05-12 10:51:28 +0800

    最小栈

--- leetcode/src/main/java/com/leetcode/tag/divide/MinStack.java
+++ leetcode/src/main/java/com/leetcode/tag/divide/MinStack.java
@@ -1,8 +1,6 @@
 package com.leetcode.tag.divide;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.Stack;
 
 /**
  * 155. 最小栈
@@ -10,46 +8,49 @@ import java.util.List;
  * <p>设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
  */
 class MinStack {
-  private List<Integer> list;
-  private int min;
+  // 栈
+  private Stack<Integer> stack;
+  // 每个元素 a 与其相应的最小值 m 时刻保持一一对应。因此我们可以使用一个辅助栈，与元素栈同步插入与删除，用于存储与每个元素对应的最小值
+  private Stack<Integer> minStack;
 
   /**
    * initialize your data structure here.
    */
   public MinStack() {
-    min = Integer.MAX_VALUE;
-    list = new ArrayList<>();
+    stack = new Stack<>();
+    minStack = new Stack<>();
+    minStack.push(Integer.MAX_VALUE);
   }
 
+  /**
+   * 当一个元素要入栈时，我们取当前辅助栈的栈顶存储的最小值，与当前元素比较得出最小值，将这个最小值插入辅助栈中；
+   *
+   * <p>当一个元素要出栈时，我们把辅助栈的栈顶元素也一并弹出；
+   *
+   * <p>在任意一个时刻，栈内元素的最小值就存储在辅助栈的栈顶元素中。
+   *
+   * <p>作者：LeetCode-Solution
+   * 链接：https://leetcode-cn.com/problems/min-stack/solution/zui-xiao-zhan-by-leetcode-solution/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   *
+   * @param x
+   */
   public void push(int x) {
-    list.add(x);
-    min = Math.min(min, x);
+    stack.push(x);
+    minStack.push(Math.min(x, minStack.peek()));
   }
 
   public void pop() {
-    if (list.isEmpty()) {
-      return;
-    }
-    // remove后min会发生改变
-    int o = list.remove(list.size() - 1);
-    if (min != o) {
-      return;
-    }
-    min = list.stream().min(Comparator.comparing(Integer::valueOf)).orElse(Integer.MAX_VALUE);
+    stack.pop();
+    minStack.pop();
   }
 
   public int top() {
-    if (list.isEmpty()) {
-      return 0;
-    }
-    return list.get(list.size() - 1);
+    return stack.peek();
   }
 
   public int getMin() {
-    if (list.isEmpty()) {
-      return 0;
-    }
-    return min;
+    return minStack.peek();
   }
 }
 
