6d3a69bb029087c4500765b8b0080533bade4482
xuweng <2028403305@qq.com>
2020-05-12 22:16:43 +0800

    函数表达式 最值和求和

--- leetcode/src/main/java/com/leetcode/tag/divide/MaxSubArray.java
+++ leetcode/src/main/java/com/leetcode/tag/divide/MaxSubArray.java
@@ -1,5 +1,7 @@
 package com.leetcode.tag.divide;
 
+import java.util.stream.IntStream;
+
 /**
  * 面试题42. 连续子数组的最大和
  */
@@ -16,5 +18,38 @@ public class MaxSubArray {
       max = Math.max(max, dp[i]);
     }
     return max;
+  }
+
+  public int divide(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+
+    return re(nums, 0, nums.length - 1);
+  }
+
+  /**
+   * 理解错误
+   *
+   * @param nums
+   * @param low
+   * @param high
+   * @return
+   */
+  private int re(int[] nums, int low, int high) {
+    if (low > high) {
+      return 0;
+    }
+    if (low == high) {
+      return nums[low];
+    }
+    int mid = low + (high - low) / 2;
+    int left = re(nums, low, mid);
+    int right = re(nums, mid + 1, high);
+
+    int l = IntStream.rangeClosed(low, mid).map(i -> nums[i]).sum();
+    int r = IntStream.rangeClosed(mid + 1, high).map(i -> nums[i]).sum();
+
+    return Math.max(Math.max(left, right), l + r);
   }
 }
