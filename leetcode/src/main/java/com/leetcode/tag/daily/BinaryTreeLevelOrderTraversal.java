4288d86893f063da39f62f19a708a1824e1775d3
xuweng <2028403305@qq.com>
2020-05-13 23:19:22 +0800

    102. 二叉树的层序遍历 自定义数据结构

--- leetcode/src/main/java/com/leetcode/tag/daily/BinaryTreeLevelOrderTraversal.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/BinaryTreeLevelOrderTraversal.java
@@ -1,7 +1,6 @@
 package com.leetcode.tag.daily;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 102. 二叉树的层序遍历
@@ -34,15 +33,35 @@ public class BinaryTreeLevelOrderTraversal {
    * @param height
    * @param lists
    */
-  public void levelOrder(TreeNode root, int height, List<List<Integer>> lists) {
-    if (height == 0) {
-      return;
+  public List<List<Integer>> levelOrder(TreeNode root) {
+    if (root == null) {
+      return new ArrayList<>();
     }
-    List<Integer> list = new ArrayList<>();
-    list.add(root.val);
-    lists.add(list);
-    levelOrder(root.left, height - 1, lists);
-    levelOrder(root.right, height - 1, lists);
+    List<List<Integer>> lists = new ArrayList<>();
+    Map<Integer, List<Integer>> map = new TreeMap<>();
+    MyNode myNode = new MyNode(1, root);
+    Queue<MyNode> queue = new LinkedList<>();
+    queue.offer(myNode);
+    while (!queue.isEmpty()) {
+      MyNode treeNode = queue.poll();
+      List<Integer> list = map.getOrDefault(treeNode.ceng, new ArrayList<>());
+      list.add(treeNode.node.val);
+      map.put(treeNode.ceng, list);
+      if (treeNode.node.left != null) {
+        MyNode node = new MyNode(treeNode.ceng + 1, treeNode.node.left);
+        queue.offer(node);
+      }
+      if (treeNode.node.right != null) {
+        MyNode node = new MyNode(treeNode.ceng + 1, treeNode.node.right);
+        queue.offer(node);
+      }
+    }
+    map.forEach(
+            (k, v) -> {
+              lists.add(v);
+            });
+
+    return lists;
   }
 
   /**
@@ -58,7 +77,20 @@ public class BinaryTreeLevelOrderTraversal {
     return Math.max(height(root.left), height(root.right)) + 1;
   }
 
-  public class TreeNode {
+  /**
+   * 自定义数据结构
+   */
+  public class MyNode {
+    int ceng;
+    TreeNode node;
+
+    public MyNode(int ceng, TreeNode node) {
+      this.ceng = ceng;
+      this.node = node;
+    }
+  }
+
+  public static class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
