b53d93db2fdfd3636cd6088b05ecfcb8e6266648
xuweng <2028403305@qq.com>
2020-05-14 10:14:19 +0800

    3个位运算 非,与,异或

--- leetcode/src/main/java/com/leetcode/tag/bit/SingleNumberII.java
+++ leetcode/src/main/java/com/leetcode/tag/bit/SingleNumberII.java
@@ -65,4 +65,47 @@ public class SingleNumberII {
               .orElse(-1);
     }
   }
+
+  /**
+   * 使用位运算符可以实现 \mathcal{O}(1)O(1) 的空间复杂度。
+   *
+   * <p>∼x表示位运算 NOT
+   *
+   * <p>x&y表示位运算 AND
+   *
+   * <p>x⊕y表示位运算 XOR
+   *
+   * <p>作者：LeetCode
+   * 链接：https://leetcode-cn.com/problems/single-number-ii/solution/zhi-chu-xian-yi-ci-de-shu-zi-ii-by-leetcode/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution3 {
+    /**
+     * 非,与,异或
+     *
+     * @param nums
+     * @return
+     */
+    public int singleNumber(int[] nums) {
+      int seenOnce = 0, seenTwice = 0;
+
+      for (int num : nums) {
+        // first appearence:
+        // add num to seen_once
+        // don't add to seen_twice because of presence in seen_once
+
+        // second appearance:
+        // remove num from seen_once
+        // add num to seen_twice
+
+        // third appearance:
+        // don't add to seen_once because of presence in seen_twice
+        // remove num from seen_twice
+        seenOnce = ~seenTwice & (seenOnce ^ num);
+        seenTwice = ~seenOnce & (seenTwice ^ num);
+      }
+
+      return seenOnce;
+    }
+  }
 }
