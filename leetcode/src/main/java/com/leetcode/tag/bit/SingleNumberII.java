e92f7c68af6a51f4b660261be85f5282a8d39394
xuweng <2028403305@qq.com>
2020-05-14 10:03:29 +0800

    函数表达式代码简洁 位运算

--- leetcode/src/main/java/com/leetcode/tag/bit/SingleNumberII.java
+++ leetcode/src/main/java/com/leetcode/tag/bit/SingleNumberII.java
@@ -1,5 +1,6 @@
 package com.leetcode.tag.bit;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -22,7 +23,9 @@ public class SingleNumberII {
   }
 
   /**
-   * 方法一：HashSet 将输入数组存储到 HashSet，然后使用 HashSet 中数字和的三倍与数组之和比较。
+   * 方法一：HashSet
+   *
+   * <p>将输入数组存储到 HashSet，然后使用 HashSet 中数字和的三倍与数组之和比较。
    *
    * <p>3×(a+b+c)−(a+a+a+b+b+b+c)=2c
    */
@@ -36,6 +39,30 @@ public class SingleNumberII {
       }
       long sumSet = set.stream().mapToLong(s -> s).sum();
       return (int) ((3 * sumSet - sumArray) / 2);
+    }
+  }
+
+  /**
+   * 方法二：HashMap
+   *
+   * <p>遍历输入数组，统计每个数字出现的次数，最后返回出现次数为 1 的数字。
+   *
+   * <p>作者：LeetCode
+   * 链接：https://leetcode-cn.com/problems/single-number-ii/solution/zhi-chu-xian-yi-ci-de-shu-zi-ii-by-leetcode/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution2 {
+    public int singleNumber(int[] nums) {
+      HashMap<Integer, Integer> hashmap = new HashMap<>();
+      for (int num : nums) {
+        hashmap.put(num, hashmap.getOrDefault(num, 0) + 1);
+      }
+
+      return hashmap.keySet().stream()
+              .mapToInt(k -> k)
+              .filter(k -> hashmap.get(k) == 1)
+              .findFirst()
+              .orElse(-1);
     }
   }
 }
