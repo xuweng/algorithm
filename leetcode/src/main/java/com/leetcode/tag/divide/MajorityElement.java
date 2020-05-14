ed26209936f7a43e9cf40b09f4e9093a8c81903a
xuweng <2028403305@qq.com>
2020-05-14 15:15:27 +0800

    面试题39. 数组中出现次数超过一半的数字

--- leetcode/src/main/java/com/leetcode/tag/divide/MajorityElement.java
+++ leetcode/src/main/java/com/leetcode/tag/divide/MajorityElement.java
@@ -14,11 +14,8 @@ public class MajorityElement {
 
     Map<Integer, Integer> map = new HashMap<>();
     for (int i : nums) {
-      if (map.containsKey(i)) {
-        map.put(i, map.get(i) + 1);
-      } else {
-        map.put(i, 1);
-      }
+      map.put(i, map.getOrDefault(i, 0) + 1);
+
       if (map.get(i) > nums.length / 2) {
         return i;
       }
