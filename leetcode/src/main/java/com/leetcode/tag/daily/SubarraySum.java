dc0cf339d8704f35e314e320d6dae51ea34c9273
xuweng <2028403305@qq.com>
2020-05-15 22:20:26 +0800

    只会枚举没有用 优化枚举

--- leetcode/src/main/java/com/leetcode/tag/daily/SubarraySum.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/SubarraySum.java
@@ -4,6 +4,25 @@ package com.leetcode.tag.daily;
  * 560. 和为K的子数组
  */
 public class SubarraySum {
+  /**
+   * 方法一：枚举
+   *
+   * <p>优化枚举
+   *
+   * <p>优化枚举
+   *
+   * <p>优化枚举
+   *
+   * <p>优化枚举
+   *
+   * <p>只会枚举没有用
+   *
+   * <p>只会枚举没有用
+   *
+   * @param nums
+   * @param k
+   * @return
+   */
   public int subarraySum(int[] nums, int k) {
     if (nums == null || nums.length == 0) {
       return 0;
