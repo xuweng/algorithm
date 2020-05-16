abc5671b4a62e651495678a897f0e2027720ba75
xuweng <2028403305@qq.com>
2020-05-16 22:53:09 +0800

    左右划分

--- leetcode/src/main/java/com/leetcode/tag/divide/DiffWaysToCompute.java
+++ leetcode/src/main/java/com/leetcode/tag/divide/DiffWaysToCompute.java
@@ -1,5 +1,6 @@
 package com.leetcode.tag.divide;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -32,7 +33,75 @@ import java.util.List;
  * <p>241. 为运算表达式设计优先级
  */
 public class DiffWaysToCompute {
+
   public List<Integer> diffWaysToCompute(String input) {
-    return null;
+    return re(input, 0, input.length() - 1);
+  }
+
+  /**
+   * 左右划分
+   *
+   * <p>不是按照mid划分
+   *
+   * <p>是按照运算符划分
+   *
+   * <p>一看到题就觉得有点复杂，可以考虑一下递归的方式，去寻找子问题和原问题解的关系。
+   *
+   * <p>可以通过运算符把整个式子分成两部分，两部分再利用递归解决。
+   *
+   * <p>作者：windliang
+   * 链接：https://leetcode-cn.com/problems/different-ways-to-add-parentheses/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-5-5/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   *
+   * @param input
+   * @return
+   */
+  public List<Integer> re(String input, int low, int high) {
+    if (input == null || input.length() == 0 || low > high) {
+      return new ArrayList<>();
+    }
+    // 考虑当前层
+    List<Integer> result = new ArrayList<>();
+    if (low == high) {
+      result.add(Integer.valueOf(String.valueOf(input.charAt(low))));
+      return result;
+    }
+
+    for (int i = 0; i < input.length(); i++) {
+      // 通过运算符将字符串分成两部分
+      if (!isOperation(input.charAt(i))) {
+        continue;
+      }
+      List<Integer> left = re(input, low, i - 1);
+      List<Integer> right = re(input, i + 1, high);
+      // 将两个结果依次运算
+      for (Integer l : left) {
+        for (Integer r : right) {
+          result.add(caculate(l, input.charAt(i), r));
+        }
+      }
+    }
+    return result;
+  }
+
+  private int caculate(int num1, char c, int num2) {
+    if (c == '+') {
+      return num1 + num2;
+    } else if (c == '-') {
+      return num1 - num2;
+    } else if (c == '*') {
+      return num1 * num2;
+    }
+    return -1;
+  }
+
+  /**
+   * 是否是运算符
+   *
+   * @param c
+   * @return
+   */
+  private boolean isOperation(char c) {
+    return c == '+' || c == '-' || c == '*';
   }
 }
