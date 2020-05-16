687dbb3bd1ff979f12de2591ac968952077c4196
xuweng <2028403305@qq.com>
2020-05-16 22:15:35 +0800

    如何划分子问题

---
+++ leetcode/src/main/java/com/leetcode/tag/divide/DiffWaysToCompute.java
@@ -0,0 +1,28 @@
+package com.leetcode.tag.divide;
+
+import java.util.List;
+
+/**
+ * 明确问题
+ *
+ * <p>明确问题
+ *
+ * <p>添加括号和求值
+ *
+ * <p>结果只需要求值
+ *
+ * <p>常规分治?常规分治?
+ *
+ * <p>f(n)--------->f(n-1).这样划分目测很麻烦?
+ *
+ * <p>f(n)------------>f(n/2).这样划分?
+ *
+ * <p>怎么划分子问题?
+ *
+ * <p>241. 为运算表达式设计优先级
+ */
+public class DiffWaysToCompute {
+  public List<Integer> diffWaysToCompute(String input) {
+    return null;
+  }
+}
