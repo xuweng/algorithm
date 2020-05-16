ea392c2e8f45256b282937378ab0f5207da30ae3
xuweng <2028403305@qq.com>
2020-05-16 23:47:09 +0800

    dp.数据预处理 数据预处理

--- leetcode/src/main/java/com/leetcode/tag/divide/DiffWaysToCompute.java
+++ leetcode/src/main/java/com/leetcode/tag/divide/DiffWaysToCompute.java
@@ -98,6 +98,72 @@ public class DiffWaysToCompute {
     return result;
   }
 
+  /**
+   * 最巧妙的地方就是做一个预处理，把每个数字提前转为 int 然后存起来，同时把运算符也都存起来。
+   *
+   * <p>这样的话我们就有了两个 list，一个保存了所有数字，一个保存了所有运算符。
+   *
+   * <p>dp[i][j] 也比较好定义了，含义是第 i 到第 j 个数字（从 0 开始计数）范围内的表达式的所有解。
+   *
+   * <p>作者：windliang
+   * 链接：https://leetcode-cn.com/problems/different-ways-to-add-parentheses/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-5-5/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   *
+   * @param input
+   * @return
+   */
+  public List<Integer> dp(String input) {
+    List<Integer> numList = new ArrayList<>();
+    List<Character> opList = new ArrayList<>();
+    char[] array = input.toCharArray();
+    int num = 0;
+    for (char c : array) {
+      if (isOperation(c)) {
+        numList.add(num);
+        num = 0;
+        opList.add(c);
+        continue;
+      }
+      num = num * 10 + c - '0';
+    }
+    numList.add(num);
+    int N = numList.size(); // 数字的个数
+
+    // 一个数字
+    ArrayList<Integer>[][] dp = (ArrayList<Integer>[][]) new ArrayList[N][N];
+    for (int i = 0; i < N; i++) {
+      ArrayList<Integer> result = new ArrayList<>();
+      result.add(numList.get(i));
+      dp[i][i] = result;
+    }
+    // 2 个数字到 N 个数字
+    for (int n = 2; n <= N; n++) {
+      // 开始下标
+      for (int i = 0; i < N; i++) {
+        // 结束下标
+        int j = i + n - 1;
+        if (j >= N) {
+          break;
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        // 分成 i ~ s 和 s+1 ~ j 两部分
+        for (int s = i; s < j; s++) {
+          ArrayList<Integer> result1 = dp[i][s];
+          ArrayList<Integer> result2 = dp[s + 1][j];
+          for (Integer integer : result1) {
+            for (Integer value : result2) {
+              // 第 s 个数字下标对应是第 s 个运算符
+              char op = opList.get(s);
+              result.add(caculate(integer, op, value));
+            }
+          }
+        }
+        dp[i][j] = result;
+      }
+    }
+    return dp[0][N - 1];
+  }
+
   private int caculate(int num1, char c, int num2) {
     if (c == '+') {
       return num1 + num2;
