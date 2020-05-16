072bb1ea543e8617f8f3796a156ca993e411e9d2
xuweng <2028403305@qq.com>
2020-05-16 15:22:49 +0800

    递归考虑当前层 只能测试才能发现问题 推导,问题转换

--- leetcode/src/main/java/com/leetcode/tag/daily/ReverseKGroup.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/ReverseKGroup.java
@@ -1,22 +1,65 @@
 package com.leetcode.tag.daily;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * 25. K 个一组翻转链表
+ * 递归考虑当前层
+ *
+ * <p>只能测试才能发现问题
+ *
+ * <p>推导,问题转换 只能测试发现问题
+ *
+ * <p>只能测试才能发现问题
+ *
+ * <p>25. K 个一组翻转链表
  */
 public class ReverseKGroup {
   /**
+   * 考虑当前层
    *
+   * <p>连接k个断开的链表
+   *
+   * <p>连接k个断开的链表
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>递归只需要考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
    *
    * @param head
    * @param k
    * @return
    */
   public ListNode reverseKGroup(ListNode head, int k) {
-    if (k <= 0) {
+    if (head == null || k <= 0) {
       return null;
+    }
+    if (head.next == null) {
+      return head;
     }
     ListNode result = head;
     ListNode node = head;
+    List<ListNode[]> listNodeList = new ArrayList<>();
     while (node != null) {
       ListNode tail = node;
       for (int i = 0; i < k - 1 && tail != null; i++) {
@@ -28,25 +71,60 @@ public class ReverseKGroup {
       if (tail == null) {
         break;
       }
+      ListNode[] listNodes = new ListNode[2];
+      listNodes[0] = node;
+      listNodes[1] = tail;
+      listNodeList.add(listNodes);
       // 记录next
       ListNode nodeNext = tail.next;
-      re(node, tail);
+      re(node, tail).next = null;
       node = nodeNext;
     }
+    for (int i = 0; i < listNodeList.size() - 1; i++) {
+      listNodeList.get(i)[0].next = listNodeList.get(i + 1)[1];
+    }
+    listNodeList.get(listNodeList.size() - 1)[0].next = node;
 
     return result;
   }
 
+  /**
+   * 考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * <p>考虑当前层
+   *
+   * @param head
+   * @param tail
+   * @return
+   */
   public ListNode re(ListNode head, ListNode tail) {
     if (head == tail) {
       return head;
     }
 
     re(head.next, tail).next = head;
-    return tail;
+    return head;
   }
 
-  public class ListNode {
+  public static class ListNode {
     int val;
     ListNode next;
 
---
+++ leetcode/src/test/java/com/leetcode/tag/daily/ReverseKGroupTest.java
@@ -0,0 +1,25 @@
+package com.leetcode.tag.daily;
+
+import org.junit.Test;
+
+public class ReverseKGroupTest {
+  @Test
+  public void reverseKGroupTest() {
+    ReverseKGroup reverseKGroup = new ReverseKGroup();
+    ReverseKGroup.ListNode node1 = new ReverseKGroup.ListNode(1);
+    ReverseKGroup.ListNode node2 = new ReverseKGroup.ListNode(2);
+    ReverseKGroup.ListNode node3 = new ReverseKGroup.ListNode(3);
+    ReverseKGroup.ListNode node4 = new ReverseKGroup.ListNode(4);
+    ReverseKGroup.ListNode node5 = new ReverseKGroup.ListNode(5);
+
+    node1.next = node2;
+    node2.next = node3;
+    node3.next = node4;
+    node4.next = node5;
+    node5.next = null;
+
+    int k = 2;
+
+    reverseKGroup.reverseKGroup(node1, k);
+  }
+}
