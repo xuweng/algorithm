bef8751354a4565ef2fee361d3c9d4cd83393d5d
xuweng <2028403305@qq.com>
2020-05-17 22:16:19 +0800

    相加或者相乘导致大数溢出

--- leetcode/src/test/java/com/leetcode/tag/divide/ReversePairsTest.java
+++ leetcode/src/test/java/com/leetcode/tag/divide/ReversePairsTest.java
@@ -8,6 +8,10 @@ public class ReversePairsTest {
     //    int[] nums = {1, 3, 2, 3, 1};
     int[] nums = {2147483647, 2147483647, 2147483647, 2147483647, 2147483647, 2147483647};
     ReversePairs reversePairs = new ReversePairs();
+    System.out.println(2147483646 > 2 * 2147483647);
+    System.out.println(2147483646 > (long) 2 * 2147483647);
+    System.out.println(1 > 2 * 2147483647);
+    System.out.println(1 > (long) 2 * 2147483647);
     System.out.println(reversePairs.reversePairs(nums));
   }
 }
