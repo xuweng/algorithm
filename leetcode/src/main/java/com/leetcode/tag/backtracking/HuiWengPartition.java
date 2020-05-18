4195071ff800fe1e98a04e8b672d6f4904597687
xuweng <2028403305@qq.com>
2020-05-18 22:23:27 +0800

    递归

--- leetcode/src/main/java/com/leetcode/tag/backtracking/HuiWengPartition.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/HuiWengPartition.java
@@ -120,9 +120,7 @@ public class HuiWengPartition {
           // 直接清空导致把root也干掉
           // 假设2个分支,搜索完一个分支就把这个分支的结点删除,但是不要把root也删除,还要继续搜索第二个分支
           //          temp.clear();
-          String root = temp.get(0);
-          temp.clear();
-          temp.add(root);
+          temp.remove(temp.size() - 1);
         }
       }
     }
