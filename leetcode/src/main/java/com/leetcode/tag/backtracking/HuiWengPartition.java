c905d9372f5478c7e1eebe01d16eb79a6276e993
xuweng <2028403305@qq.com>
2020-05-18 16:55:44 +0800

    能不能两两划分,就看能不能计算横跨mid这部分

---
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/HuiWengPartition.java
@@ -0,0 +1,36 @@
+package com.leetcode.tag.backtracking;
+
+import java.util.List;
+
+/**
+ * 所有方案?最优解?
+ *
+ * <p>所有可能的分割方案?
+ *
+ * <p>分割最少次数?
+ *
+ * <p>所有方案
+ *
+ * <p>分割最少次数?
+ *
+ * <p>不是求最优解
+ *
+ * <p>如何划分?
+ *
+ * <p>一次两两划分?
+ *
+ * <p>多次划分?
+ *
+ * <p>划分后需要递归?
+ *
+ * <p>131. 分割回文串
+ *
+ * <p>两两划分后,发现横跨mid的字符串就是原问题,横跨mid的字符串难求
+ *
+ * <p>能不能两两划分,就看能不能计算横跨mid这部分
+ */
+public class HuiWengPartition {
+  public List<List<String>> partition(String s) {
+    return null;
+  }
+}
