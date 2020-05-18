5a25991fcc11abf718b7ae4c7bb40a6bffa7039b
xuweng <2028403305@qq.com>
2020-05-18 18:33:59 +0800

    许多复杂的，规模较大的问题都可以使用回溯法，有“通用解题方法”的美称。

--- leetcode/src/main/java/com/leetcode/tag/backtracking/HuiWengPartition.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/HuiWengPartition.java
@@ -1,6 +1,8 @@
 package com.leetcode.tag.backtracking;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 /**
@@ -136,6 +138,74 @@ public class HuiWengPartition {
         }
         i++;
         j--;
+      }
+      return true;
+    }
+  }
+
+  /**
+   * 作者：liweiwei1419
+   * 链接：https://leetcode-cn.com/problems/palindrome-partitioning/solution/hui-su-you-hua-jia-liao-dong-tai-gui-hua-by-liweiw/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution1 {
+    List<List<String>> res = new ArrayList<>();
+    // Stack 这个类 Java 的文档里推荐写成 Deque<Integer> stack = new ArrayDeque<Integer>();
+    // 注意：只使用 stack 相关的接口
+    Deque<String> stack = new ArrayDeque<>();
+
+    public List<List<String>> partition(String s) {
+      int len = s.length();
+      if (len == 0) {
+        return res;
+      }
+
+      backtracking(s, 0, len);
+      return res;
+    }
+
+    /**
+     * @param s
+     * @param start 起始字符的索引
+     * @param len   字符串 s 的长度，可以设置为全局变量
+     * @param path  记录从根结点到叶子结点的路径
+     * @param res   记录所有的结果
+     */
+    private void backtracking(String s, int start, int len) {
+      if (start == len) {
+        res.add(new ArrayList<>(stack));
+        return;
+      }
+
+      for (int i = start; i < len; i++) {
+        // 因为截取字符串是消耗性能的，因此，采用传子串索引的方式判断一个子串是否是回文子串
+        // 不是的话，剪枝
+        if (!checkPalindrome(s, start, i)) {
+          continue;
+        }
+
+        stack.addLast(s.substring(start, i + 1));
+        backtracking(s, i + 1, len);
+        stack.removeLast();
+      }
+    }
+
+    /**
+     * 这一步的时间复杂度是 O(N)，因此，可以采用动态规划先把回文子串的结果记录在一个表格里
+     *
+     * @param str
+     * @param left  子串的左边界，可以取到
+     * @param right 子串的右边界，可以取到
+     * @return
+     */
+    private boolean checkPalindrome(String str, int left, int right) {
+      // 严格小于即可
+      while (left < right) {
+        if (str.charAt(left) != str.charAt(right)) {
+          return false;
+        }
+        left++;
+        right--;
       }
       return true;
     }
