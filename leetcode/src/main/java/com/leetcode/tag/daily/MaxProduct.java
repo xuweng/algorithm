c46e98e1f1767b9352f17a48903e18720d569f68
xuweng <2028403305@qq.com>
2020-05-18 11:07:23 +0800

    递归方程错误

--- leetcode/src/main/java/com/leetcode/tag/daily/MaxProduct.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/MaxProduct.java
@@ -18,7 +18,7 @@ public class MaxProduct {
     dp[0] = nums[0];
     int max = nums[0];
     for (int i = 1; i < nums.length; i++) {
-      dp[i] = dp[i - 1] <= 0 ? nums[i] : dp[i - 1] * nums[i];
+      dp[i] = dp[i - 1] * nums[i] <= 0 ? nums[i] : dp[i - 1] * nums[i];
       max = Math.max(max, dp[i]);
     }
 
