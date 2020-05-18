4f9ec524b3a893ff8b5812e4d0d9cf8f7af41fde
xuweng <2028403305@qq.com>
2020-05-18 15:45:20 +0800

    经验和举一反三

--- leetcode/src/main/java/com/leetcode/tag/daily/MaxProduct.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/MaxProduct.java
@@ -173,4 +173,45 @@ public class MaxProduct {
     }
     return (int) max;
   }
+
+  /**
+   * 动态规划」通常不关心过程，只关心「阶段结果」，这个「阶段结果」就是我们设计的「状态」。
+   *
+   * <p>什么算法关心过程呢？「回溯算法」，「回溯算法」需要记录过程，复杂度通常较高。
+   *
+   * <p>链接：https://leetcode-cn.com/problems/maximum-product-subarray/solution/dong-tai-gui-hua-li-jie-wu-hou-xiao-xing-by-liweiw/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  public class Solution1 {
+    public int maxProduct(int[] nums) {
+      int len = nums.length;
+      if (len == 0) {
+        return 0;
+      }
+
+      // dp[i][0]：以 nums[i] 结尾的连续子数组的最小值
+      // dp[i][1]：以 nums[i] 结尾的连续子数组的最大值
+      int[][] dp = new int[len][2];
+      // 最大正数
+      dp[0][0] = nums[0];
+      // 最小负数
+      dp[0][1] = nums[0];
+      for (int i = 1; i < len; i++) {
+        if (nums[i] >= 0) {
+          dp[i][0] = Math.min(nums[i], nums[i] * dp[i - 1][0]);
+          dp[i][1] = Math.max(nums[i], nums[i] * dp[i - 1][1]);
+        } else {
+          dp[i][0] = Math.min(nums[i], nums[i] * dp[i - 1][1]);
+          dp[i][1] = Math.max(nums[i], nums[i] * dp[i - 1][0]);
+        }
+      }
+
+      // 只关心最大值，需要遍历
+      int res = dp[0][1];
+      for (int i = 1; i < len; i++) {
+        res = Math.max(res, dp[i][1]);
+      }
+      return res;
+    }
+  }
 }
