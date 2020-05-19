e0d65edd599857fd69a0423ea167a42702070c3c
xuweng <2028403305@qq.com>
2020-05-19 10:57:06 +0800

    680. 验证回文字符串 Ⅱ

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/ValidPalindrome.java
@@ -0,0 +1,56 @@
+package com.leetcode.tag.daily;
+
+/**
+ * 680. 验证回文字符串 Ⅱ
+ */
+public class ValidPalindrome {
+  public boolean validPalindrome(String s) {
+    if (s == null || s.length() == 0) {
+      return false;
+    }
+    boolean valid = false;
+    String s1 = s;
+    for (int i = 0; i < s.length() && !valid; i++) {
+      valid = valid || isHui(s1, 0, s1.length() - 1);
+      s1 = delete(s, i);
+    }
+
+    return valid;
+  }
+
+  public boolean isHui(String s, int low, int high) {
+    if (s == null || low > high) {
+      return false;
+    }
+    // 递归终止条件
+    if (low == high) {
+      return true;
+    }
+    boolean b = s.charAt(low) == s.charAt(high);
+    if (high - low == 1) {
+      return b;
+    }
+
+    return b && isHui(s, low + 1, high - 1);
+  }
+
+  public String delete(String s, int index) {
+    if (s.length() == 1) {
+      return "";
+    }
+    // 删除最后一个字符
+    if (index == s.length() - 1) {
+      return s.substring(0, index);
+    }
+    char[] chars = s.toCharArray();
+    if (s.length() - 1 - index >= 0) {
+      System.arraycopy(chars, index + 1, chars, index, s.length() - 1 - index);
+    }
+    char[] result = new char[s.length() - 1];
+    if (chars.length - 1 >= 0) {
+      System.arraycopy(chars, 0, result, 0, chars.length - 1);
+    }
+
+    return new String(result);
+  }
+}
