c72801751ffa53dc909d47558193b90e20a31da5
xuweng <2028403305@qq.com>
2020-05-19 16:36:32 +0800

    证明算法正确性

--- leetcode/src/main/java/com/leetcode/tag/backtracking/RegularExpressionMatch.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/RegularExpressionMatch.java
@@ -108,4 +108,52 @@ public class RegularExpressionMatch {
       }
     }
   }
+
+  enum Result {
+    TRUE,
+    FALSE
+  }
+
+  /**
+   * 作者：LeetCode
+   * 链接：https://leetcode-cn.com/problems/regular-expression-matching/solution/zheng-ze-biao-da-shi-pi-pei-by-leetcode/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution1 {
+    Result[][] memo;
+
+    /**
+     * dp[i][j]就是s的前i个元素是否可以被p的前j个元素所匹配。
+     *
+     * @param text
+     * @param pattern
+     * @return
+     */
+    public boolean isMatch(String text, String pattern) {
+      memo = new Result[text.length() + 1][pattern.length() + 1];
+      return dp(0, 0, text, pattern);
+    }
+
+    public boolean dp(int i, int j, String text, String pattern) {
+      if (memo[i][j] != null) {
+        return memo[i][j] == Result.TRUE;
+      }
+      boolean ans;
+      if (j == pattern.length()) {
+        ans = i == text.length();
+      } else {
+        boolean firstMatch =
+                (i < text.length()
+                        && (pattern.charAt(j) == text.charAt(i) || pattern.charAt(j) == '.'));
+
+        if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
+          ans = (dp(i, j + 2, text, pattern) || firstMatch && dp(i + 1, j, text, pattern));
+        } else {
+          ans = firstMatch && dp(i + 1, j + 1, text, pattern);
+        }
+      }
+      memo[i][j] = ans ? Result.TRUE : Result.FALSE;
+      return ans;
+    }
+  }
 }
