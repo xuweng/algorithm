d02a870651cf81d6a6fce0a7b8b391a10ae28d37
xuweng <2028403305@qq.com>
2020-05-19 14:48:00 +0800

    正则表达式匹配

--- leetcode/src/main/java/com/leetcode/tag/backtracking/RegularExpressionMatch.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/RegularExpressionMatch.java
@@ -14,6 +14,17 @@ public class RegularExpressionMatch {
    * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
    */
   class Solution {
+    /**
+     * 如果没有星号（正则表达式中的 * ），问题会很简单——我们只需要从左到右检查匹配串 s 是否能匹配模式串 p 的每一个字符。
+     *
+     * <p>当模式串中有星号时，我们需要检查匹配串 s 中的不同后缀，以判断它们是否能匹配模式串剩余的部分。
+     *
+     * <p>一个直观的解法就是用回溯的方法来体现这种关系。
+     *
+     * @param text
+     * @param pattern
+     * @return
+     */
     public boolean isMatch(String text, String pattern) {
       if (pattern.isEmpty()) {
         return text.isEmpty();
