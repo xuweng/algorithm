9d0102928b95e85062dcf446657b95924666dca4
xuweng <2028403305@qq.com>
2020-05-19 11:48:30 +0800

    算法正确性

---
+++ leetcode/src/test/java/com/leetcode/tag/daily/ValidPalindromeTest.java
@@ -0,0 +1,14 @@
+package com.leetcode.tag.daily;
+
+import org.junit.Test;
+
+public class ValidPalindromeTest {
+  @Test
+  public void replaceSpaceTest() {
+    ValidPalindrome validPalindrome = new ValidPalindrome();
+
+    String s =
+            "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga";
+    validPalindrome.validPalindrome1(s);
+  }
+}
