969df4367a7d0990fca3ab70da61dc4cbf031a83
xuweng <2028403305@qq.com>
2020-05-20 22:38:20 +0800

    不会就debug debug吧,不要乱猜

--- algorithm-study/src/main/java/com/algorithm/study/datastructure/tree/bit/BinaryIndexedTree3.java
+++ algorithm-study/src/main/java/com/algorithm/study/datastructure/tree/bit/BinaryIndexedTree3.java
@@ -1,7 +1,7 @@
 package com.algorithm.study.datastructure.tree.bit;
 
 /**
- * C[i] = A[i - 2k+1] + A[i - 2k+2] + ... + A[i]; //k为i的二进制中从最低位到高位连续零的长度
+ * C[i] = A[i - lowbit(i)+1] + A[i - lowbit(i)+2] + ... + A[i]; //k为i的二进制中从最低位到高位连续零的长度
  *
  * <p>例如i = 8(1000)时候，k = 3，可自行验证。
  */
