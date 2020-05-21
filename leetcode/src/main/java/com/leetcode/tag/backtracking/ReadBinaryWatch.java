4dfede0819575d31150c1e2ace92a24b748eaaa7
xuweng <2028403305@qq.com>
2020-05-21 19:14:59 +0800

    选择策略

---
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/ReadBinaryWatch.java
@@ -0,0 +1,64 @@
+package com.leetcode.tag.backtracking;
+
+import java.util.List;
+
+/**
+ * 代码模板
+ *
+ * <p>代码模板
+ *
+ * <p>递归树
+ *
+ * <p>代码模板
+ *
+ * <p>代码模板
+ *
+ * <p>代码模板
+ *
+ * <p>递归树
+ *
+ * <p>递归树
+ *
+ * <p>递归树
+ *
+ * <p>递归树
+ *
+ * <p>递归树
+ *
+ * <p>递归树
+ *
+ * <p>代码模板
+ *
+ * <p>代码模板
+ *
+ * <p>代码模板
+ *
+ * <p>代码模板
+ *
+ * <p>代码模板
+ *
+ * <p>代码模板
+ *
+ * <p>401. 二进制手表
+ */
+public class ReadBinaryWatch {
+  /**
+   * 条件太多?
+   *
+   * <p>基础上添加添加?
+   *
+   * <p>选择策略
+   *
+   * <p>选择策略
+   *
+   * <p>选择策略
+   *
+   * <p>选择策略
+   *
+   * @param num
+   * @return
+   */
+  public List<String> readBinaryWatch(int num) {
+    return null;
+  }
+}
