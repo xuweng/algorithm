316e294607db76481d6e366abf22e6fedeeec56e
xuweng <2028403305@qq.com>
2020-05-21 19:37:16 +0800

    选择策略

--- leetcode/src/main/java/com/leetcode/tag/backtracking/ReadBinaryWatch.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/ReadBinaryWatch.java
@@ -1,5 +1,6 @@
 package com.leetcode.tag.backtracking;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -60,5 +61,43 @@ public class ReadBinaryWatch {
    */
   public List<String> readBinaryWatch(int num) {
     return null;
+  }
+
+  /**
+   * 选多少个时?
+   *
+   * <p>选多少个分?
+   *
+   * @param num
+   * @param list
+   * @return
+   */
+  public List<Integer> re(int num, List<Integer> list) {
+    for (int i = 0; i < list.size(); i++) {
+      List<Integer> list1 = new ArrayList<>(list);
+      list1.remove(i);
+      List<Integer> list2 = re(num - 1, list1);
+    }
+
+    return null;
+  }
+
+  public List<Integer> init() {
+    List<Integer> list = new ArrayList<>();
+
+    // 时
+    list.add(8);
+    list.add(4);
+    list.add(2);
+    list.add(1);
+    // 分
+    list.add(32);
+    list.add(16);
+    list.add(8);
+    list.add(4);
+    list.add(2);
+    list.add(1);
+
+    return list;
   }
 }
