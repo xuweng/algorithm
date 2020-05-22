5a3b565de03c7b1fbcc4476f5f62e32b15193161
xuweng <2028403305@qq.com>
2020-05-22 21:13:48 +0800

    寻找所有分支

--- leetcode/src/main/java/com/leetcode/tag/backtracking/GenerateParenthesis.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/GenerateParenthesis.java
@@ -77,9 +77,7 @@ public class GenerateParenthesis {
     return result;
   }
 
-  /**
-   * 我们可以生成所有 2^2n 个 '(' 和 ')' 字符构成的序列，然后我们检查每一个是否有效即可。
-   */
+  /** 我们可以生成所有 2^2n 个 '(' 和 ')' 字符构成的序列，然后我们检查每一个是否有效即可。 */
   class Solution {
     public List<String> generateParenthesis(int n) {
       List<String> combinations = new ArrayList<>();
@@ -123,6 +121,58 @@ public class GenerateParenthesis {
         }
       }
       return (balance == 0);
+    }
+  }
+
+  /**
+   * 作者：liweiwei1419
+   * 链接：https://leetcode-cn.com/problems/generate-parentheses/solution/hui-su-suan-fa-by-liweiwei1419/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  public class Solution1 {
+    // 做减法
+    public List<String> generateParenthesis(int n) {
+      List<String> res = new ArrayList<>();
+      // 特判
+      if (n == 0) {
+        return res;
+      }
+
+      // 执行深度优先遍历，搜索可能的结果
+      dfs("", n, n, res);
+      return res;
+    }
+
+    /**
+     * 递归树
+     *
+     * @param curStr 当前递归得到的结果
+     * @param left 左括号还有几个可以使用
+     * @param right 右括号还有几个可以使用
+     * @param res 结果集
+     */
+    private void dfs(String curStr, int left, int right, List<String> res) {
+      // 因为每一次尝试，都使用新的字符串变量，所以无需回溯
+      // 在递归终止的时候，直接把它添加到结果集即可，注意与「力扣」第 46 题、第 39 题区分
+      if (left == 0 && right == 0) {
+        res.add(curStr);
+        return;
+      }
+
+      // 剪枝（如图，左括号可以使用的个数严格大于右括号可以使用的个数，才剪枝，注意这个细节）
+      if (left > right) {
+        return;
+      }
+
+      // 每个结点最多两个分支
+      // 使用一个左括号
+      if (left > 0) {
+        dfs(curStr + "(", left - 1, right, res);
+      }
+      // 使用一个右括号
+      if (right > 0) {
+        dfs(curStr + ")", left, right - 1, res);
+      }
     }
   }
 }
