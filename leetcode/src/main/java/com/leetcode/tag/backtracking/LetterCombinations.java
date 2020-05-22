1ac30c320664c67daa8c620054ff8455e9d2a6e0
xuweng <2028403305@qq.com>
2020-05-22 14:45:04 +0800

    用一个全局list保存结果 每个子问题都用一个list保存结果 空间复杂度

--- leetcode/src/main/java/com/leetcode/tag/backtracking/LetterCombinations.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/LetterCombinations.java
@@ -21,6 +21,17 @@ public class LetterCombinations {
     return re(digits, digits.length() - 1);
   }
 
+  /**
+   * 每个子问题用一个list保存,空间复杂度高
+   *
+   * <p>假设子问题解决
+   *
+   * <p>假设n-1解决,然后推导n
+   *
+   * @param digits
+   * @param length
+   * @return
+   */
   public List<String> re(String digits, int length) {
     if (length < 0) {
       return new ArrayList<>();
@@ -60,5 +71,65 @@ public class LetterCombinations {
     map.put('7', "pqrs");
     map.put('8', "tuv");
     map.put('9', "wxyz");
+  }
+
+  class Solution {
+    Map<String, String> phone =
+            new HashMap<String, String>() {
+              {
+                put("2", "abc");
+                put("3", "def");
+                put("4", "ghi");
+                put("5", "jkl");
+                put("6", "mno");
+                put("7", "pqrs");
+                put("8", "tuv");
+                put("9", "wxyz");
+              }
+            };
+
+    List<String> output = new ArrayList<>();
+
+    /**
+     * 只需要一个list保存结果.空间复杂度低
+     *
+     * <p>寻找所有分支
+     *
+     * <p>寻找所有子问题
+     *
+     * <p>递归解决所有分支
+     *
+     * <p>递归解决所有分支
+     *
+     * @param combination
+     * @param nextDigits
+     */
+    public void backtrack(String combination, String nextDigits) {
+      // if there is no more digits to check
+      if (nextDigits.length() == 0) {
+        // the combination is done
+        output.add(combination);
+      }
+      // if there are still digits to check
+      else {
+        // iterate over all letters which map
+        // the next available digit
+        String digit = nextDigits.substring(0, 1);
+        String letters = phone.get(digit);
+        for (int i = 0; i < letters.length(); i++) {
+          String letter = phone.get(digit).substring(i, i + 1);
+          // append the current letter to the combination
+          // and proceed to the next digits
+          backtrack(combination + letter, nextDigits.substring(1));
+        }
+      }
+    }
+
+    public List<String> letterCombinations(String digits) {
+      if (digits.length() != 0) {
+        backtrack("", digits);
+      }
+      return output;
+    }
   }
 }
