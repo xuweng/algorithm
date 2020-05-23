51cd85dd169dbea071fea15ab9a0d2cbb76a5863
xuweng <2028403305@qq.com>
2020-05-23 11:33:05 +0800

    76. 最小覆盖子串

--- leetcode/src/main/java/com/leetcode/tag/daily/MinimumWindowSubstring.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/MinimumWindowSubstring.java
@@ -1,10 +1,16 @@
 package com.leetcode.tag.daily;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 76. 最小覆盖子串
  */
 public class MinimumWindowSubstring {
+  Map<Character, Integer> tMap;
+
   public String minWindow(String s, String t) {
+    initTMap(t);
     int min = Integer.MAX_VALUE;
     int low = -1, high = -1;
     for (int i = 0; i < s.length(); i++) {
@@ -24,15 +30,26 @@ public class MinimumWindowSubstring {
   }
 
   public boolean contain(String s, String t) {
-    if (s.length() < t.length()) {
-      return false;
+    Map<Character, Integer> sMap = new HashMap<>();
+
+    for (int i = 0; i < s.length(); i++) {
+      sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
     }
+
     for (int i = 0; i < t.length(); i++) {
-      if (!s.contains(String.valueOf(t.charAt(i)))) {
+      if (sMap.getOrDefault(t.charAt(i), -1) < tMap.get(t.charAt(i))) {
         return false;
       }
     }
 
     return true;
+  }
+
+  public Map<Character, Integer> initTMap(String t) {
+    tMap = new HashMap<>();
+    for (int i = 0; i < t.length(); i++) {
+      tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
+    }
+    return tMap;
   }
 }
