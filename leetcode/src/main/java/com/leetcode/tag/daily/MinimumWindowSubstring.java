4f80a1c6bfaaff99918bc6f0d6fe05767f2176f2
xuweng <2028403305@qq.com>
2020-05-23 12:00:19 +0800

    滑动窗口

--- leetcode/src/main/java/com/leetcode/tag/daily/MinimumWindowSubstring.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/MinimumWindowSubstring.java
@@ -52,4 +52,64 @@ public class MinimumWindowSubstring {
     }
     return tMap;
   }
+
+  /**
+   * 作者：LeetCode-Solution
+   * 链接：https://leetcode-cn.com/problems/minimum-window-substring/solution/zui-xiao-fu-gai-zi-chuan-by-leetcode-solution/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution {
+    Map<Character, Integer> ori = new HashMap<>();
+    Map<Character, Integer> cnt = new HashMap<>();
+
+    /**
+     * 滑动窗口
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public String minWindow(String s, String t) {
+      int tLen = t.length();
+      for (int i = 0; i < tLen; i++) {
+        char c = t.charAt(i);
+        ori.put(c, ori.getOrDefault(c, 0) + 1);
+      }
+      // 左边界:l
+      // 右边界:r
+      // 滑动窗口:l-----r
+      int l = 0, r = -1;
+      int len = Integer.MAX_VALUE, ansL = -1, ansR = -1;
+      int sLen = s.length();
+      while (r < sLen) {
+        ++r;
+        if (r < sLen && ori.containsKey(s.charAt(r))) {
+          cnt.put(s.charAt(r), cnt.getOrDefault(s.charAt(r), 0) + 1);
+        }
+        while (check() && l <= r) {
+          if (r - l + 1 < len) {
+            len = r - l + 1;
+            ansL = l;
+            ansR = l + len;
+          }
+          if (ori.containsKey(s.charAt(l))) {
+            cnt.put(s.charAt(l), cnt.getOrDefault(s.charAt(l), 0) - 1);
+          }
+          ++l;
+        }
+      }
+      return ansL == -1 ? "" : s.substring(ansL, ansR);
+    }
+
+    public boolean check() {
+      for (Map.Entry<Character, Integer> characterIntegerEntry : ori.entrySet()) {
+        Character key = characterIntegerEntry.getKey();
+        Integer val = characterIntegerEntry.getValue();
+        if (cnt.getOrDefault(key, 0) < val) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
 }
