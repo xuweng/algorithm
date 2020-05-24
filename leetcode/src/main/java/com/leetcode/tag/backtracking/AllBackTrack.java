5547430e8ee0bb9e6825a6e2044535d0ed50e8fe
xuweng <2028403305@qq.com>
2020-05-24 18:46:05 +0800

    回溯算法题目集合

--- leetcode/src/main/java/com/leetcode/tag/backtracking/AllBackTrack.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/AllBackTrack.java
@@ -21,9 +21,35 @@ public class AllBackTrack {
     void backtrack(int[] nums, int start) {
       res.add(path);
       for (int i = start; i < nums.length; i++) {
-        path.push(nums[i]); // 做出选择
-        backtrack(nums, i + 1); // 递归进入下一层，注意i+1，标识下一个选择列表的开始位置，最重要的一步
+        // 做出选择
+        path.push(nums[i]);
+        // 递归进入下一层，注意i+1，标识下一个选择列表的开始位置，最重要的一步
+        backtrack(nums, i + 1);
         path.pop(); // 撤销选择
+      }
+    }
+
+    void backtrack1(int[] nums, int start) {
+      res.add(path);
+      for (int i = start; i < nums.length; i++) {
+        // 剪枝去重
+        if (i > start && nums[i] == nums[i - 1]) {
+          continue;
+        }
+        path.push(nums[i]);
+        backtrack(nums, i + 1);
+        path.pop();
+      }
+    }
+
+    void backtrack(int[] nums, int start, int sum, int target) {
+      for (int i = start; i < nums.length; i++) {
+        if (sum > target) {
+          continue;
+        }
+        path.push(nums[i]);
+        backtrack(nums, i, sum + nums[i], target); // 更新i和当前状态的sum
+        path.pop();
       }
     }
   }
