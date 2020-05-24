5a8a1454910902cef59d1000ef5976459377dc82
xuweng <2028403305@qq.com>
2020-05-24 18:36:57 +0800

    回溯算法题目集合

---
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/AllBackTrack.java
@@ -0,0 +1,29 @@
+package com.leetcode.tag.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 回溯算法题目集合
+ */
+public class AllBackTrack {
+  /**
+   * 作者：show-me-the-code-2
+   * 链接：https://leetcode-cn.com/problems/subsets/solution/c-zong-jie-liao-hui-su-wen-ti-lei-xing-dai-ni-gao-/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class S {
+    List<List<Integer>> res = new ArrayList<>();
+    Stack<Integer> path = new Stack<>();
+
+    void backtrack(int[] nums, int start) {
+      res.add(path);
+      for (int i = start; i < nums.length; i++) {
+        path.push(nums[i]); // 做出选择
+        backtrack(nums, i + 1); // 递归进入下一层，注意i+1，标识下一个选择列表的开始位置，最重要的一步
+        path.pop(); // 撤销选择
+      }
+    }
+  }
+}
