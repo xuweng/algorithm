c7b57de563672be4219d73f04187a2096f94e21e
xuweng <2028403305@qq.com>
2020-05-25 16:08:25 +0800

    39. 组合总和

--- leetcode/src/main/java/com/leetcode/tag/backtracking/CombinationSum.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/CombinationSum.java
@@ -12,12 +12,12 @@ public class CombinationSum {
   List<List<Integer>> result = new ArrayList<>();
 
   public List<List<Integer>> combinationSum(int[] candidates, int target) {
-    backTrack(candidates, 0, target);
+    backTrack(candidates, 0, 0, target);
 
     return result;
   }
 
-  public void backTrack(int[] candidates, int sum, int target) {
+  public void backTrack(int[] candidates, int select, int sum, int target) {
     if (sum > target) {
       return;
     }
@@ -27,9 +27,9 @@ public class CombinationSum {
       result.add(new ArrayList<>(stack));
       return;
     }
-    for (int i : candidates) {
-      stack.push(i);
-      backTrack(candidates, i + sum, target);
+    for (int i = select; i < candidates.length; i++) {
+      stack.push(candidates[i]);
+      backTrack(candidates, i, candidates[i] + sum, target);
       stack.pop();
     }
   }
