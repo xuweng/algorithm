cceb57239a2ca32f6803659182f6225a75eefa57
xuweng <2028403305@qq.com>
2020-05-25 16:03:15 +0800

    39. 组合总和

---
+++ leetcode/src/test/java/com/leetcode/tag/backtracking/CombinationSumTest.java
@@ -0,0 +1,13 @@
+package com.leetcode.tag.backtracking;
+
+import org.junit.Test;
+
+public class CombinationSumTest {
+  @Test
+  public void replaceSpaceTest() {
+    CombinationSum combinationSum = new CombinationSum();
+    int[] candidates = new int[]{2, 3, 6, 7};
+    int target = 7;
+    combinationSum.combinationSum(candidates, target);
+  }
+}
