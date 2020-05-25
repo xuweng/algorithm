f8ed15f61c52cd6e20ce766e68729754cce1cb93
xuweng <2028403305@qq.com>
2020-05-25 21:11:54 +0800

    1239. 串联字符串的最大长度

---
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/ConcatenatedMaxLength.java
@@ -0,0 +1,41 @@
+package com.leetcode.tag.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 1239. 串联字符串的最大长度
+ */
+public class ConcatenatedMaxLength {
+  List<String> result = new ArrayList<>();
+
+  public int maxLength(List<String> arr) {
+    backTrack(arr, "", 0);
+
+    int maxLength = 0;
+    for (String s : result) {
+      maxLength = Math.max(maxLength, s.length());
+    }
+    return maxLength;
+  }
+
+  public void backTrack(List<String> arr, String temp, int start) {
+    result.add(temp);
+
+    for (int i = start; i < arr.size(); i++) {
+      if (contains(temp, arr.get(i))) {
+        continue;
+      }
+      backTrack(arr, temp + arr.get(i), i + 1);
+    }
+  }
+
+  private boolean contains(String s, String s1) {
+    for (int i = 0; i < s.length(); i++) {
+      if (s1.contains(String.valueOf(s.charAt(i)))) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
