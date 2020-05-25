41599762dcbb8e890027511106bcfa9d08b29a6b
xuweng <2028403305@qq.com>
2020-05-25 21:53:49 +0800

    44. 通配符匹配

--- leetcode/src/main/java/com/leetcode/tag/backtracking/WildcardMatching.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/WildcardMatching.java
@@ -11,12 +11,10 @@ public class WildcardMatching {
     if (p.isEmpty()) {
       return s.isEmpty();
     }
-    boolean headMatch = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
-    // 下一个是否是*
-    boolean pNext = p.length() >= 2 && p.charAt(1) == '*';
+    boolean headMatch = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '?');
 
-    if (pNext) {
-      return isMatch(s, p.substring(2)) || (headMatch && isMatch(s.substring(1), p));
+    if (p.charAt(0) == '*') {
+      return isMatch(s, p.substring(1)) || isMatch(s.substring(1), p);
     } else {
       return headMatch && isMatch(s.substring(1), p.substring(1));
     }
