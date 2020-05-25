cf12a6957e0d3e978af05cc73d62dc8c652ceb14
xuweng <2028403305@qq.com>
2020-05-25 22:11:33 +0800

    44. 通配符匹配

--- leetcode/src/main/java/com/leetcode/tag/backtracking/WildcardMatching.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/WildcardMatching.java
@@ -14,7 +14,10 @@ public class WildcardMatching {
     boolean headMatch = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '?');
 
     if (p.charAt(0) == '*') {
-      return isMatch(s, p.substring(1)) || isMatch(s.substring(1), p);
+      if (s.length() >= 2 && p.length() >= 2 && s.charAt(1) == p.charAt(1)) {
+        return isMatch(s.substring(2), p.substring(2));
+      }
+      return isMatch(s, p.substring(1)) || s.length() >= 1 && isMatch(s.substring(1), p);
     } else {
       return headMatch && isMatch(s.substring(1), p.substring(1));
     }
