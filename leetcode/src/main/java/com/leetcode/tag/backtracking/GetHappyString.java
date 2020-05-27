ad95c57053eaf7ef9698e9be99e331aede1b24f4
xuweng <2028403305@qq.com>
2020-05-27 15:49:11 +0800

    1415. 长度为 n 的开心字符串中字典序第 k 小的字符串
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/GetHappyString.java
@@ -0,0 +1,29 @@
+package com.leetcode.tag.backtracking;
+
+/**
+ * 1415. 长度为 n 的开心字符串中字典序第 k 小的字符串
+ */
+public class GetHappyString {
+  int count;
+
+  public String getHappyString(int n, int k) {
+    char[] chars = {'a', 'b', 'c'};
+
+    return backTrack(chars, n, k, ' ', "");
+  }
+
+  public String backTrack(char[] chars, int n, int K, char select, String temp) {
+    if (n == 0) {
+      count++;
+      return count == K ? temp : "";
+    }
+
+    for (char c : chars) {
+      if (select == c) {
+        continue;
+      }
+      backTrack(chars, n - 1, K, c, temp + c);
+    }
+    return temp;
+  }
+}
