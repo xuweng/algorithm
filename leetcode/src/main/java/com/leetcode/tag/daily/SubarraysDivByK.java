ee93538fb9512e180e39d41312f8ea963d72e488
xuweng <2028403305@qq.com>
2020-05-27 11:36:48 +0800

    前缀和

--- leetcode/src/main/java/com/leetcode/tag/daily/SubarraysDivByK.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/SubarraysDivByK.java
@@ -1,7 +1,16 @@
 package com.leetcode.tag.daily;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * 滑动窗口?
+ * 连续子数组问题的时候，我们使用前缀和来解决。
+ *
+ * <p>我们令 P[i] = A[0] + A[1] + ... + A[i]。
+ *
+ * <p>那么每个连续子数组的和 sum(i,j) 就可以写成 P[j] - P[i-1]
+ *
+ * <p>滑动窗口?
  *
  * <p>区间和?线段树?区间数组?
  *
@@ -73,6 +82,54 @@ public class SubarraysDivByK {
         return preSum[j];
       }
       return preSum[j] - preSum[i - 1];
+    }
+  }
+
+  /**
+   * 作者：LeetCode-Solution
+   * 链接：https://leetcode-cn.com/problems/subarray-sums-divisible-by-k/solution/he-ke-bei-k-zheng-chu-de-zi-shu-zu-by-leetcode-sol/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution {
+    /**
+     * 方法一：哈希表 + 逐一统计
+     *
+     * @param A
+     * @param K
+     * @return
+     */
+    public int subarraysDivByK(int[] A, int K) {
+      Map<Integer, Integer> record = new HashMap<>();
+      record.put(0, 1);
+      int sum = 0, ans = 0;
+      for (int elem : A) {
+        sum += elem;
+        // 注意 Java 取模的特殊性，当被除数为负数时取模结果为负数，需要纠正
+        int modulus = (sum % K + K) % K;
+        int same = record.getOrDefault(modulus, 0);
+        ans += same;
+        record.put(modulus, same + 1);
+      }
+      return ans;
+    }
+  }
+
+  class S1 {
+    class Solution {
+      public int subarraysDivByK(int[] A, int K) {
+        int sum = 0, counter = 0;
+        int[] remainders = new int[K];
+        remainders[0] = 1;
+
+        for (int a : A) {
+          sum += a;
+          int remainder = Math.floorMod(sum, K);
+          counter += remainders[remainder];
+          ++remainders[remainder];
+        }
+
+        return counter;
+      }
     }
   }
 }
