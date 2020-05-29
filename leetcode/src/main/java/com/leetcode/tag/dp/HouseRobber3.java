7f2dd507fb08f9e17d3283a8584b1d68d90834e9
xuweng <2028403305@qq.com>
2020-05-29 21:00:47 +0800

    递归记忆化
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/HouseRobber3.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/HouseRobber3.java
@@ -1,9 +1,6 @@
 package com.leetcode.tag.dp;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
 /**
  * 线性结构.用线性dp。
@@ -122,6 +119,42 @@ public class HouseRobber3 {
       }
 
       return Math.max(money, rob(root.left) + rob(root.right));
+    }
+  }
+
+  /**
+   * 记忆化 - 解决重复子问题
+   *
+   * <p>作者：reals
+   * 链接：https://leetcode-cn.com/problems/house-robber-iii/solution/san-chong-fang-fa-jie-jue-shu-xing-dong-tai-gui-hu/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class S2 {
+
+    public int rob(TreeNode root) {
+      HashMap<TreeNode, Integer> memo = new HashMap<>();
+      return robInternal(root, memo);
+    }
+
+    public int robInternal(TreeNode root, HashMap<TreeNode, Integer> memo) {
+      if (root == null) {
+        return 0;
+      }
+      if (memo.containsKey(root)) {
+        return memo.get(root);
+      }
+      int money = root.val;
+
+      if (root.left != null) {
+        money += (robInternal(root.left.left, memo) + robInternal(root.left.right, memo));
+      }
+      if (root.right != null) {
+        money += (robInternal(root.right.left, memo) + robInternal(root.right.right, memo));
+      }
+      int result = Math.max(money, robInternal(root.left, memo) + robInternal(root.right, memo));
+      memo.put(root, result);
+
+      return result;
     }
   }
 }
