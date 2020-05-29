d15ffb0b8bf20f6012b45ef0d3927808a7117670
xuweng <2028403305@qq.com>
2020-05-29 11:38:03 +0800

    213. 打家劫舍 II
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/HouseRobber2.java
@@ -0,0 +1,32 @@
+package com.leetcode.tag.daily;
+
+/**
+ * 213. 打家劫舍 II
+ */
+public class HouseRobber2 {
+  /**
+   * 这意味着第一个房屋和最后一个房屋是紧挨着的
+   *
+   * @param nums
+   * @return
+   */
+  public int rob(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+    if (nums.length == 1) {
+      return nums[0];
+    }
+    if (nums.length <= 3) {
+      return Math.max(nums[0], nums[1]);
+    }
+    int[] dp = new int[nums.length];
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+
+    for (int i = 2; i < nums.length - 1; i++) {
+      dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+    }
+    return dp[nums.length - 2];
+  }
+}
