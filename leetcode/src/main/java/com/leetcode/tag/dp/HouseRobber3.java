3f997ad2b1d6ea05f8d26f241fbff4c253abad17
xuweng <2028403305@qq.com>
2020-05-29 21:06:20 +0800

    dp
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/HouseRobber3.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/HouseRobber3.java
@@ -157,4 +157,55 @@ public class HouseRobber3 {
       return result;
     }
   }
+
+  /**
+   * dp
+   *
+   * <p>作者：reals
+   * 链接：https://leetcode-cn.com/problems/house-robber-iii/solution/san-chong-fang-fa-jie-jue-shu-xing-dong-tai-gui-hu/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class S3 {
+    /**
+     * 每个节点可选择偷或者不偷两种状态，根据题目意思，相连节点不能一起偷
+     *
+     * <p>当前节点选择偷时，那么两个孩子节点就不能选择偷了
+     *
+     * <p>当前节点选择不偷时，两个孩子节点只需要拿最多的钱出来就行(两个孩子节点偷不偷没关系)
+     *
+     * <p>我们使用一个大小为 2 的数组来表示
+     *
+     * <p>int[] res = new int[2]
+     *
+     * <p>0 代表不偷，1 代表偷 任何一个节点能偷到的最大钱的状态可以定义为
+     *
+     * <p>当前节点选择不偷：当前节点能偷到的最大钱数 = 左孩子能偷到的钱 + 右孩子能偷到的钱
+     *
+     * <p>当前节点选择偷：当前节点能偷到的最大钱数 = 左孩子选择自己不偷时能得到的钱 +
+     *
+     * <p>右孩子选择不偷时能得到的钱 + 当前节点的钱数
+     *
+     * @param root
+     * @return
+     */
+    public int rob(TreeNode root) {
+      int[] result = robInternal(root);
+      return Math.max(result[0], result[1]);
+    }
+
+    public int[] robInternal(TreeNode root) {
+      if (root == null) {
+        return new int[2];
+      }
+      int[] result = new int[2];
+
+      int[] left = robInternal(root.left);
+      int[] right = robInternal(root.right);
+
+      result[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+      result[1] = left[0] + right[0] + root.val;
+
+      return result;
+    }
+  }
 }
