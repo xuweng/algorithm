1f278a11e40aa54a3be37f894466451058b9fc4a
xuweng <2028403305@qq.com>
2020-05-30 22:18:08 +0800

    78. 子集
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/Subsets2.java
@@ -0,0 +1,30 @@
+package com.leetcode.tag.backtracking;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 78. 子集
+ */
+public class Subsets2 {
+  public List<List<Integer>> subsets(int[] nums) {
+    Deque<Integer> deque = new ArrayDeque<>();
+    List<List<Integer>> result = new ArrayList<>();
+
+    backTrack(nums, 0, deque, result);
+
+    return result;
+  }
+
+  public void backTrack(int[] nums, int begin, Deque<Integer> deque, List<List<Integer>> result) {
+    result.add(new ArrayList<>(deque));
+
+    for (int i = begin; i < nums.length; i++) {
+      deque.push(nums[i]);
+      backTrack(nums, i + 1, deque, result);
+      deque.pop();
+    }
+  }
+}
