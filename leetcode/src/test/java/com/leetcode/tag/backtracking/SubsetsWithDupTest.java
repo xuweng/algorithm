5af96fa8d63a5bf6ff4beee1b8698a3a65e2106a
xuweng <2028403305@qq.com>
2020-05-31 01:16:29 +0800

    如何剪枝
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/test/java/com/leetcode/tag/backtracking/SubsetsWithDupTest.java
@@ -0,0 +1,13 @@
+package com.leetcode.tag.backtracking;
+
+import org.junit.Test;
+
+public class SubsetsWithDupTest {
+  @Test
+  public void test() {
+    SubsetsWithDup subsetsWithDup = new SubsetsWithDup();
+    int[] nums = {1, 2, 2};
+
+    subsetsWithDup.subsetsWithDup(nums);
+  }
+}
