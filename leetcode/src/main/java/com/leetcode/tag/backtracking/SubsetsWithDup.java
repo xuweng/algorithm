6083286dadd27ec605f7947978ce8acfe08ccf13
xuweng <2028403305@qq.com>
2020-05-30 23:53:34 +0800

    每条路径可以有重复 分支不能有重复
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/SubsetsWithDup.java
@@ -0,0 +1,48 @@
+package com.leetcode.tag.backtracking;
+
+import java.util.*;
+
+/**
+ * 90. 子集 II
+ *
+ * <p>说明：解集不能包含重复的子集。
+ */
+public class SubsetsWithDup {
+
+  public List<List<Integer>> subsetsWithDup(int[] nums) {
+    Deque<Integer> deque = new ArrayDeque<>();
+    List<List<Integer>> result = new ArrayList<>();
+    Set<Integer> set = new HashSet<>();
+
+    backTrack(nums, 0, deque, set, result);
+
+    return result;
+  }
+
+  /**
+   * 每条路径可以有重复
+   *
+   * <p>分支不能有重复
+   *
+   * @param nums
+   * @param begin
+   * @param deque
+   * @param set
+   * @param result
+   */
+  public void backTrack(
+          int[] nums, int begin, Deque<Integer> deque, Set<Integer> set, List<List<Integer>> result) {
+
+    result.add(new ArrayList<>(deque));
+    for (int i = begin; i < nums.length; i++) {
+      if (set.contains(nums[i])) {
+        continue;
+      }
+      deque.push(nums[i]);
+      backTrack(nums, i + 1, deque, set, result);
+      deque.pop();
+
+      set.add(nums[begin]);
+    }
+  }
+}
