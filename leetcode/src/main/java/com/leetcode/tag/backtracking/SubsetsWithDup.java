a073f1095e8d5abd9499667969e0d064dc1a2847
xuweng <2028403305@qq.com>
2020-05-31 01:28:54 +0800

    如何剪枝
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/backtracking/SubsetsWithDup.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/SubsetsWithDup.java
@@ -15,8 +15,9 @@ public class SubsetsWithDup {
   public List<List<Integer>> subsetsWithDup(int[] nums) {
     Deque<Integer> deque = new ArrayDeque<>();
     List<List<Integer>> result = new ArrayList<>();
+    List<List<Integer>> levels = new ArrayList<>();
 
-    backTrack(nums, 0, 0, deque, result);
+    backTrack(nums, 0, 0, deque, levels, result);
 
     return result;
   }
@@ -24,7 +25,7 @@ public class SubsetsWithDup {
   /**
    * 每条路径可以有重复
    *
-   * <p>分支不能有重复
+   * <p>分支不能有重复。每层不能有重复。
    *
    * @param nums
    * @param begin
@@ -32,15 +33,28 @@ public class SubsetsWithDup {
    * @param result
    */
   public void backTrack(
-          int[] nums, int count, int begin, Deque<Integer> deque, List<List<Integer>> result) {
+          int[] nums,
+          int count,
+          int begin,
+          Deque<Integer> deque,
+          List<List<Integer>> levels,
+          List<List<Integer>> result) {
+
+    if (count == levels.size()) {
+      levels.add(new ArrayList<>());
+    }
 
     // 越界也要计算一个结果
     result.add(new ArrayList<>(deque));
     // 越界不用进入循环
     for (int i = begin; i < nums.length; i++) {
       // 如何剪枝?如何剪枝?如何剪枝?如何剪枝?
+      if (levels.get(count).contains(nums[i])) {
+        continue;
+      }
       deque.push(nums[i]);
-      backTrack(nums, count + 1, i + 1, deque, result);
+      levels.get(count).add(nums[i]);
+      backTrack(nums, count + 1, i + 1, deque, levels, result);
       // 以下部分都属于回溯
       deque.pop();
     }
