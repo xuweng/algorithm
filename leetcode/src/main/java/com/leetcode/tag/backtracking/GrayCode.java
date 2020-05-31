47d5dc0e5a010ebc73d8cfc83dfa3bdb10732d64
xuweng <2028403305@qq.com>
2020-05-31 18:09:12 +0800

    技巧性题目
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/backtracking/GrayCode.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/GrayCode.java
@@ -76,4 +76,28 @@ public class GrayCode {
       return res;
     }
   }
+
+  /**
+   * 规律题目
+   *
+   * <p>技巧性题目
+   *
+   * <p>作者：windliang
+   * 链接：https://leetcode-cn.com/problems/gray-code/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by--12/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class S {
+    public List<Integer> grayCode(int n) {
+      List<Integer> gray = new ArrayList<>();
+      gray.add(0); // 初始化 n = 0 的解
+      for (int i = 0; i < n; i++) {
+        int add = 1 << i; // 要加的数
+        // 倒序遍历，并且加上一个值添加到结果中
+        for (int j = gray.size() - 1; j >= 0; j--) {
+          gray.add(gray.get(j) + add);
+        }
+      }
+      return gray;
+    }
+  }
 }
