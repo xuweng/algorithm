75fed90e27ece475092611a6fe92559065b42228
xuweng <2028403305@qq.com>
2020-05-31 17:49:11 +0800

    十分钟看答案
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/backtracking/GrayCode.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/GrayCode.java
@@ -52,4 +52,28 @@ public class GrayCode {
       backTrack(n - 1, temp + i, result);
     }
   }
+
+  /**
+   * 作者：jyd
+   * 链接：https://leetcode-cn.com/problems/gray-code/solution/gray-code-jing-xiang-fan-she-fa-by-jyd/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution {
+    public List<Integer> grayCode(int n) {
+      List<Integer> res =
+              new ArrayList<Integer>() {
+                {
+                  add(0);
+                }
+              };
+      int head = 1;
+      for (int i = 0; i < n; i++) {
+        for (int j = res.size() - 1; j >= 0; j--) {
+          res.add(head + res.get(j));
+        }
+        head <<= 1;
+      }
+      return res;
+    }
+  }
 }
