0048d99b7c63a36e7e74b6af5d74ccfa87c4dbde
xuweng <2028403305@qq.com>
2020-06-07 10:21:15 +0800

    算法模板 递归参数统计
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/backtracking/Combine.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/Combine.java
@@ -42,28 +42,24 @@ public class Combine {
   public List<List<Integer>> combine(int n, int k) {
     List<List<Integer>> result = new ArrayList<>();
     Deque<Integer> deque = new ArrayDeque<>();
-    boolean[] used = new boolean[n + 1];
 
-    backTrack(n, 0, k, 1, used, deque, result);
+    backTrack(n, 0, k, 1, deque, result);
     return result;
   }
 
   /**
-   * 一条路径不能有重复
+   * 算法模板
+   *
+   * <p>递归参数统计
+   *
+   * <p>一条路径不能有重复
    *
    * @param n
    * @param k
    * @param start
-   * @param used
    */
   public void backTrack(
-          int n,
-          int count,
-          int k,
-          int start,
-          boolean[] used,
-          Deque<Integer> deque,
-          List<List<Integer>> result) {
+          int n, int count, int k, int start, Deque<Integer> deque, List<List<Integer>> result) {
     if (count == k) {
       result.add(new ArrayList<>(deque));
       return;
@@ -71,15 +67,10 @@ public class Combine {
 
     // 候选集
     for (int i = start; i <= n; i++) {
-      if (used[i]) {
-        continue;
-      }
       // 当前选择i
-      used[i] = true;
       deque.push(i);
       // 回溯。从i+1开始选择。
-      backTrack(n, count + 1, k, i + 1, used, deque, result);
-      used[i] = false;
+      backTrack(n, count + 1, k, i + 1, deque, result);
       deque.pop();
     }
   }
