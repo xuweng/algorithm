7ff95f599c4cdfd90d03ae1c25fe2845d08cf2c6
xuweng <2028403305@qq.com>
2020-06-11 23:42:54 +0800

    使用前缀树的回溯
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/backtracking/FindWords.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/FindWords.java
@@ -1,6 +1,7 @@
 package com.leetcode.tag.backtracking;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -120,5 +121,102 @@ public class FindWords {
       visited[row][col] = false;
     }
     return false;
+  }
+
+  /**
+   * 方法一：使用前缀树的回溯
+   *
+   * <p>十分钟看题解
+   *
+   * <p>十分钟看题解
+   *
+   * <p>十分钟看题解
+   *
+   * <p>作者：LeetCode
+   * 链接：https://leetcode-cn.com/problems/word-search-ii/solution/dan-ci-sou-suo-ii-by-leetcode/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class TrieNode {
+    HashMap<Character, TrieNode> children = new HashMap<>();
+    String word = null;
+
+    public TrieNode() {
+    }
+  }
+
+  class Solution {
+    char[][] _board = null;
+    ArrayList<String> _result = new ArrayList<String>();
+
+    public List<String> findWords(char[][] board, String[] words) {
+
+      // Step 1). Construct the Trie
+      TrieNode root = new TrieNode();
+      for (String word : words) {
+        TrieNode node = root;
+
+        for (Character letter : word.toCharArray()) {
+          if (node.children.containsKey(letter)) {
+            node = node.children.get(letter);
+          } else {
+            TrieNode newNode = new TrieNode();
+            node.children.put(letter, newNode);
+            node = newNode;
+          }
+        }
+        node.word = word; // store words in Trie
+      }
+
+      this._board = board;
+      // Step 2). Backtracking starting for each cell in the board
+      for (int row = 0; row < board.length; ++row) {
+        for (int col = 0; col < board[row].length; ++col) {
+          if (root.children.containsKey(board[row][col])) {
+            backtracking(row, col, root);
+          }
+        }
+      }
+
+      return this._result;
+    }
+
+    private void backtracking(int row, int col, TrieNode parent) {
+      Character letter = this._board[row][col];
+      TrieNode currNode = parent.children.get(letter);
+
+      // check if there is any match
+      if (currNode.word != null) {
+        this._result.add(currNode.word);
+        currNode.word = null;
+      }
+
+      // mark the current letter before the EXPLORATION
+      this._board[row][col] = '#';
+
+      // explore neighbor cells in around-clock directions: up, right, down, left
+      int[] rowOffset = {-1, 0, 1, 0};
+      int[] colOffset = {0, 1, 0, -1};
+      for (int i = 0; i < 4; ++i) {
+        int newRow = row + rowOffset[i];
+        int newCol = col + colOffset[i];
+        if (newRow < 0
+                || newRow >= this._board.length
+                || newCol < 0
+                || newCol >= this._board[0].length) {
+          continue;
+        }
+        if (currNode.children.containsKey(this._board[newRow][newCol])) {
+          backtracking(newRow, newCol, currNode);
+        }
+      }
+
+      // End of EXPLORATION, restore the original letter in the board.
+      this._board[row][col] = letter;
+
+      // Optimization: incrementally remove the leaf nodes
+      if (currNode.children.isEmpty()) {
+        parent.children.remove(letter);
+      }
+    }
   }
 }
