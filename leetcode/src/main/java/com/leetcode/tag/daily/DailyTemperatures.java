fa58c88543b91ccbd86368c57212d355ad3befed
xuweng <2028403305@qq.com>
2020-06-11 23:06:17 +0800

    考查数据结构
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/DailyTemperatures.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/DailyTemperatures.java
@@ -1,7 +1,14 @@
 package com.leetcode.tag.daily;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
- * 739. 每日温度
+ * 数据结构
+ *
+ * <p>考查数据结构
+ *
+ * <p>739. 每日温度
  */
 public class DailyTemperatures {
   public int[] dailyTemperatures(int[] T) {
@@ -18,5 +25,29 @@ public class DailyTemperatures {
       result[i] = (j >= T.length) ? 0 : j - i;
     }
     return result;
+  }
+
+  /**
+   * 方法二：单调栈
+   *
+   * <p>作者：LeetCode-Solution
+   * 链接：https://leetcode-cn.com/problems/daily-temperatures/solution/mei-ri-wen-du-by-leetcode-solution/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution {
+    public int[] dailyTemperatures(int[] T) {
+      int length = T.length;
+      int[] ans = new int[length];
+      Deque<Integer> stack = new LinkedList<Integer>();
+      for (int i = 0; i < length; i++) {
+        int temperature = T[i];
+        while (!stack.isEmpty() && temperature > T[stack.peek()]) {
+          int prevIndex = stack.pop();
+          ans[prevIndex] = i - prevIndex;
+        }
+        stack.push(i);
+      }
+      return ans;
+    }
   }
 }
