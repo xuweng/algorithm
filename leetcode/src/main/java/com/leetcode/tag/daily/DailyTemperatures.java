b6a91155a4c2fca0304181c73ec0a55b32b72284
xuweng <2028403305@qq.com>
2020-06-11 23:18:43 +0800

    单调递减栈
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/DailyTemperatures.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/DailyTemperatures.java
@@ -42,7 +42,7 @@ public class DailyTemperatures {
     public int[] dailyTemperatures(int[] T) {
       int length = T.length;
       int[] ans = new int[length];
-      Deque<Integer> stack = new LinkedList<Integer>();
+      Deque<Integer> stack = new LinkedList<>();
       for (int i = 0; i < length; i++) {
         int temperature = T[i];
         while (!stack.isEmpty() && temperature > T[stack.peek()]) {
