a869025d882063e9b8694c638944183a89901e5c
xuweng <2028403305@qq.com>
2020-06-12 23:49:44 +0800

    15. 三数之和
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/ThreeSum.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/ThreeSum.java
@@ -1,12 +1,19 @@
 package com.leetcode.tag.daily;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 15. 三数之和
+ *
+ * <p>十分钟看答案
+ *
+ * <p>十分钟看答案
+ *
+ * <p>十分钟看答案
+ *
+ * <p>十分钟看答案
+ *
+ * <p>十分钟看答案
  */
 public class ThreeSum {
   public List<List<Integer>> threeSum(int[] nums) {
@@ -39,5 +46,54 @@ public class ThreeSum {
     }
 
     return result;
+  }
+
+  /**
+   * 方法一：排序 + 双指针
+   *
+   * <p>作者：LeetCode-Solution
+   * 链接：https://leetcode-cn.com/problems/3sum/solution/san-shu-zhi-he-by-leetcode-solution/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+      int n = nums.length;
+      Arrays.sort(nums);
+      List<List<Integer>> ans = new ArrayList<>();
+      // 枚举 a
+      for (int first = 0; first < n; ++first) {
+        // 需要和上一次枚举的数不相同
+        if (first > 0 && nums[first] == nums[first - 1]) {
+          continue;
+        }
+        // c 对应的指针初始指向数组的最右端
+        int third = n - 1;
+        int target = -nums[first];
+        // 枚举 b
+        for (int second = first + 1; second < n; ++second) {
+          // 需要和上一次枚举的数不相同
+          if (second > first + 1 && nums[second] == nums[second - 1]) {
+            continue;
+          }
+          // 需要保证 b 的指针在 c 的指针的左侧
+          while (second < third && nums[second] + nums[third] > target) {
+            --third;
+          }
+          // 如果指针重合，随着 b 后续的增加
+          // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+          if (second == third) {
+            break;
+          }
+          if (nums[second] + nums[third] == target) {
+            List<Integer> list = new ArrayList<Integer>();
+            list.add(nums[first]);
+            list.add(nums[second]);
+            list.add(nums[third]);
+            ans.add(list);
+          }
+        }
+      }
+      return ans;
+    }
   }
 }
