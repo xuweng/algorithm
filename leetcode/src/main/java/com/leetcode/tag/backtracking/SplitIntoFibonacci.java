fadd33645612406e50668d169c2169f77107789c
xuweng <2028403305@qq.com>
2020-06-14 17:30:53 +0800

    官方 遍历数据范围内的数据
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/backtracking/SplitIntoFibonacci.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/SplitIntoFibonacci.java
@@ -75,4 +75,59 @@ public class SplitIntoFibonacci {
 
     return true;
   }
+
+  /**
+   * 方法 1：暴力
+   *
+   * <p>作者：LeetCode
+   * 链接：https://leetcode-cn.com/problems/split-array-into-fibonacci-sequence/solution/jiang-shu-zu-chai-fen-cheng-fei-bo-na-qi-xu-lie-by/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution {
+    public List<Integer> splitIntoFibonacci(String S) {
+      int N = S.length();
+      for (int i = 0; i < Math.min(10, N); ++i) {
+        if (S.charAt(0) == '0' && i > 0) {
+          break;
+        }
+        long a = Long.parseLong(S.substring(0, i + 1));
+        if (a >= Integer.MAX_VALUE) {
+          break;
+        }
+
+        search:
+        for (int j = i + 1; j < Math.min(i + 10, N); ++j) {
+          if (S.charAt(i + 1) == '0' && j > i + 1) {
+            break;
+          }
+          long b = Long.parseLong(S.substring(i + 1, j + 1));
+          if (b >= Integer.MAX_VALUE) {
+            break;
+          }
+
+          List<Integer> fib = new ArrayList<>();
+          fib.add((int) a);
+          fib.add((int) b);
+
+          int k = j + 1;
+          while (k < N) {
+            long nxt = fib.get(fib.size() - 2) + fib.get(fib.size() - 1);
+            String nxtS = String.valueOf(nxt);
+
+            if (nxt <= Integer.MAX_VALUE && S.substring(k).startsWith(nxtS)) {
+              k += nxtS.length();
+              fib.add((int) nxt);
+            } else {
+              continue search;
+            }
+          }
+          if (fib.size() >= 3) {
+            return fib;
+          }
+        }
+      }
+
+      return new ArrayList<>();
+    }
+  }
 }
