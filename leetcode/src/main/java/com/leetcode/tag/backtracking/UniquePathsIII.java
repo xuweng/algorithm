bc023956f7b48efade828f5ddaa815f5867a0b3f
xuweng <2028403305@qq.com>
2020-06-14 15:03:01 +0800

    行和列一一对应
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/backtracking/UniquePathsIII.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/UniquePathsIII.java
@@ -109,4 +109,76 @@ public class UniquePathsIII {
       deque.pop();
     }
   }
+
+  /**
+   * 思路差不多
+   *
+   * <p>作者：LeetCode
+   * 链接：https://leetcode-cn.com/problems/unique-paths-iii/solution/bu-tong-lu-jing-iii-by-leetcode/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution {
+    int ans;
+    int[][] grid;
+    int tr, tc;
+    // 行和列一一对应。
+    // 行。上下左右。
+    int[] dr = new int[]{0, -1, 0, 1};
+    // 列。上下左右。
+    int[] dc = new int[]{1, 0, -1, 0};
+    int R, C;
+
+    public int uniquePathsIII(int[][] grid) {
+      this.grid = grid;
+      R = grid.length;
+      C = grid[0].length;
+
+      int todo = 0;
+      int sr = 0, sc = 0;
+      for (int r = 0; r < R; ++r) {
+        for (int c = 0; c < C; ++c) {
+          if (grid[r][c] != -1) {
+            todo++;
+          }
+
+          if (grid[r][c] == 1) {
+            sr = r;
+            sc = c;
+          } else if (grid[r][c] == 2) {
+            tr = r;
+            tc = c;
+          }
+        }
+      }
+
+      ans = 0;
+      dfs(sr, sc, todo);
+      return ans;
+    }
+
+    public void dfs(int r, int c, int todo) {
+      todo--;
+      if (todo < 0) {
+        return;
+      }
+      if (r == tr && c == tc) {
+        if (todo == 0) {
+          ans++;
+        }
+        return;
+      }
+
+      grid[r][c] = 3;
+      for (int k = 0; k < 4; ++k) {
+        int nr = r + dr[k];
+        int nc = c + dc[k];
+        if (0 <= nr && nr < R && 0 <= nc && nc < C) {
+          if (grid[nr][nc] % 2 == 0) {
+            dfs(nr, nc, todo);
+          }
+        }
+      }
+      grid[r][c] = 0;
+    }
+  }
 }
