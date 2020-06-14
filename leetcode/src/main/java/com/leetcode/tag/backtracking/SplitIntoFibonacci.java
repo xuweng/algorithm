0e36dad703124fa1a82d8388a20ef22538e842dc
xuweng <2028403305@qq.com>
2020-06-14 17:33:54 +0800

    用参数来计算
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/backtracking/SplitIntoFibonacci.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/SplitIntoFibonacci.java
@@ -130,4 +130,65 @@ public class SplitIntoFibonacci {
       return new ArrayList<>();
     }
   }
+
+  /**
+   * 作者：jangbao
+   * 链接：https://leetcode-cn.com/problems/split-array-into-fibonacci-sequence/solution/java-dfs-jia-jian-zhi-ji-bai-liao-9306-by-capta1n/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution1 {
+    List<Integer> ans;
+
+    public List<Integer> splitIntoFibonacci(String S) {
+
+      ans = new ArrayList<>();
+      return dfs(0, S, 0, 0, 0) ? ans : new ArrayList<>();
+    }
+
+    /**
+     * 用参数来计算
+     *
+     * <p>用参数来计算
+     *
+     * <p>用参数来计算
+     *
+     * @p : 当前指针指向数组的索引
+     * @s : 字符串
+     * @pre1 : 前面隔一个的数
+     * @pre2 : 前一个数
+     * @deep : 当前是第几个数
+     */
+    public boolean dfs(int p, String s, int pre1, int pre2, int deep) {
+      int length = s.length();
+      if (p == length) {
+        return deep >= 3;
+      }
+
+      for (int i = 1; i <= 11; i++) {
+        // 超出长度或者以0开头直接break;
+        if (p + i > length || (s.charAt(p) == '0' && i > 1)) {
+          break;
+        }
+        // 截取字符串
+        String sub = s.substring(p, p + i);
+
+        long numL = Long.parseLong(sub);
+        // 判断是否超出范围,或者deep不是0,1却大于他的前两个数之和
+        if (numL > Integer.MAX_VALUE || (deep != 0 && deep != 1 && numL > (pre1 + pre2))) {
+          break;
+        }
+        // 转成int
+        Integer num = (int) numL;
+        // 满足条件的数,递归加回溯
+        if (deep == 0 || deep == 1 || num.equals(pre1 + pre2)) {
+          ans.add(num);
+          if (dfs(p + i, s, pre2, num, deep + 1)) {
+            return true;
+          }
+          ans.remove(num);
+        }
+      }
+      return false;
+    }
+  }
 }
