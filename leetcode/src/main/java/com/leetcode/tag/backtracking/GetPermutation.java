8be10783036622c20586aea7968a830cba973245
xuweng <2028403305@qq.com>
2020-06-14 18:21:58 +0800

    60. 第k个排列 代码模板
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/backtracking/GetPermutation.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/GetPermutation.java
@@ -1,5 +1,8 @@
 package com.leetcode.tag.backtracking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 抄代码
  *
@@ -36,6 +39,69 @@ public class GetPermutation {
       used[i] = true;
       backTrack(n, k, used, temp + i);
       used[i] = false;
+    }
+  }
+
+  /**
+   * 优秀代码
+   *
+   * <p>简洁代码
+   *
+   * <p>代码模板
+   *
+   * <p>代码模板
+   *
+   * <p>代码模板
+   *
+   * <p>优秀代码
+   *
+   * <p>优秀代码
+   *
+   * <p>优秀代码
+   *
+   * <p>优秀代码
+   *
+   * <p>优秀代码
+   *
+   * <p>优秀代码
+   *
+   * <p>代码模板
+   *
+   * <p>代码模板
+   *
+   * <p>代码模板
+   */
+  class Solution {
+    public String getPermutation(int n, int k) {
+      int[] factorials = new int[n];
+      List<Integer> nums =
+              new ArrayList() {
+                {
+                  add(1);
+                }
+              };
+
+      factorials[0] = 1;
+      for (int i = 1; i < n; ++i) {
+        // generate factorial system bases 0!, 1!, ..., (n - 1)!
+        factorials[i] = factorials[i - 1] * i;
+        // generate nums 1, 2, ..., n
+        nums.add(i + 1);
+      }
+
+      // fit k in the interval 0 ... (n! - 1)
+      --k;
+
+      // compute factorial representation of k
+      StringBuilder sb = new StringBuilder();
+      for (int i = n - 1; i > -1; --i) {
+        int idx = k / factorials[i];
+        k -= idx * factorials[i];
+
+        sb.append(nums.get(idx));
+        nums.remove(idx);
+      }
+      return sb.toString();
     }
   }
 }
