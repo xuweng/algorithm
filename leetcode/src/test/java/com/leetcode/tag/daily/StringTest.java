a917ec8e3689cc91f41a32d2f8af94d7ff5692e4
xuweng <2028403305@qq.com>
2020-06-19 00:36:42 +0800

    string测试
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/test/java/com/leetcode/tag/daily/StringTest.java
+++ leetcode/src/test/java/com/leetcode/tag/daily/StringTest.java
@@ -2,14 +2,34 @@ package com.leetcode.tag.daily;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class StringTest {
   @Test
   public void test() {
     String str = "1-2--3--4-5--6--7";
 
+    String token = "-";
+
+    System.out.println(Arrays.toString(getIndex(str.substring(token.length() + 1), token)));
+
     System.out.println(str.indexOf("-"));
     System.out.println(str.indexOf("--"));
     System.out.println(str.lastIndexOf("-"));
     System.out.println(str.lastIndexOf("--"));
+  }
+
+  private int[] getIndex(String s, String token) {
+    char[] chars = s.toCharArray();
+    int preIndex = 0;
+    for (int i = 1; i < chars.length; i++) {
+      if (Character.isDigit(chars[i])) {
+        if (token.equals(s.substring(preIndex + 1, i))) {
+          return new int[]{preIndex, i};
+        }
+        preIndex = i;
+      }
+    }
+    return null;
   }
 }
