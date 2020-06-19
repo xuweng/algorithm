c065ab38823fa8d9a1f47d3d289278a9c6e520cd
xuweng <2028403305@qq.com>
2020-06-20 00:26:44 +0800

    递归
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/test/java/com/leetcode/tag/daily/RecoverFromPreorderTest.java
+++ leetcode/src/test/java/com/leetcode/tag/daily/RecoverFromPreorderTest.java
@@ -7,7 +7,8 @@ public class RecoverFromPreorderTest {
   public void test() {
     RecoverFromPreorder.Solution recoverFromPreorder = new RecoverFromPreorder.Solution();
 
-    String S = "1-2--3--4-5--6--7";
+    //    String S = "1-2--3--4-5--6--7";
+    String S = "1-2--3---4-5--6---7";
     RecoverFromPreorder.TreeNode treeNode = recoverFromPreorder.recoverFromPreorder(S);
 
     System.out.println();
