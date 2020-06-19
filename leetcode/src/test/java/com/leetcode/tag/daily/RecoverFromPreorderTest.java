64af93dfa85157fe02bf897b6e68335b75f86946
xuweng <2028403305@qq.com>
2020-06-20 00:52:17 +0800

    单个字符数字可以。多个字符数字不可以。
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/test/java/com/leetcode/tag/daily/RecoverFromPreorderTest.java
+++ leetcode/src/test/java/com/leetcode/tag/daily/RecoverFromPreorderTest.java
@@ -10,6 +10,7 @@ public class RecoverFromPreorderTest {
     //    String S = "1-2--3--4-5--6--7";
     //    String S = "1-2--3---4-5--6---7";
     // 这个测试用例有问题
+    // 单个字符数字可以。多个字符数字不可以。
     String S = "1-401--349---90--88";
     RecoverFromPreorder.TreeNode treeNode = recoverFromPreorder.recoverFromPreorder(S);
 
