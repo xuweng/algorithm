17e0791a5f1cac9c3680e54f3201e31da8a94b4e
xuweng <2028403305@qq.com>
2020-06-19 23:55:50 +0800

    递归
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/RecoverFromPreorder.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/RecoverFromPreorder.java
@@ -4,20 +4,6 @@ package com.leetcode.tag.daily;
  * 1028. 从先序遍历还原二叉树
  */
 public class RecoverFromPreorder {
-  private int[] getIndex(String s, String token) {
-    char[] chars = s.toCharArray();
-    int preIndex = chars.length - 1;
-    for (int i = chars.length - 2; i >= 0; i--) {
-      if (Character.isDigit(chars[i])) {
-        if (token.equals(s.substring(i + 1, preIndex))) {
-          return new int[]{i, preIndex};
-        }
-        preIndex = i;
-      }
-    }
-    return null;
-  }
-
   class Solution {
     public TreeNode recoverFromPreorder(String S) {
       return re(S, "-");
@@ -34,6 +20,20 @@ public class RecoverFromPreorder {
       treeNode.right = re(S.substring(indexes[1]), token + "-");
 
       return treeNode;
+    }
+
+    private int[] getIndex(String s, String token) {
+      char[] chars = s.toCharArray();
+      int preIndex = chars.length - 1;
+      for (int i = chars.length - 2; i >= 0; i--) {
+        if (Character.isDigit(chars[i])) {
+          if (token.equals(s.substring(i + 1, preIndex))) {
+            return new int[]{i, preIndex};
+          }
+          preIndex = i;
+        }
+      }
+      return null;
     }
   }
 
