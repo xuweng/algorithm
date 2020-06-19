ad4aa5c864cc24ee3780e9f772c381cade0c60c5
xuweng <2028403305@qq.com>
2020-06-20 00:51:23 +0800

    不能通过所有测试用例
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/test/java/com/leetcode/tag/daily/RecoverFromPreorderTest.java
+++ leetcode/src/test/java/com/leetcode/tag/daily/RecoverFromPreorderTest.java
@@ -9,6 +9,7 @@ public class RecoverFromPreorderTest {
 
     //    String S = "1-2--3--4-5--6--7";
     //    String S = "1-2--3---4-5--6---7";
+    // 这个测试用例有问题
     String S = "1-401--349---90--88";
     RecoverFromPreorder.TreeNode treeNode = recoverFromPreorder.recoverFromPreorder(S);
 
