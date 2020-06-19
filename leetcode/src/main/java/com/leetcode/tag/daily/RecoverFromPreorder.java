208bf2447c35a8d0608f058e5edf03e52dc38544
xuweng <2028403305@qq.com>
2020-06-19 23:53:40 +0800

    递归
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/RecoverFromPreorder.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/RecoverFromPreorder.java
@@ -4,26 +4,6 @@ package com.leetcode.tag.daily;
  * 1028. 从先序遍历还原二叉树
  */
 public class RecoverFromPreorder {
-  class Solution {
-    public TreeNode recoverFromPreorder(String S) {
-      return re(S, "-");
-    }
-
-    public TreeNode re(String S, String token) {
-      int root = Integer.parseInt(String.valueOf(S.charAt(0)));
-      int index = S.indexOf(token);
-      int lastIndex = S.lastIndexOf(token);
-      if (index == -1 || lastIndex == -1) {
-        return null;
-      }
-      TreeNode treeNode = new TreeNode(root);
-      treeNode.left = re(S.substring(index + 1, lastIndex), token + "-");
-      treeNode.right = re(S.substring(lastIndex + 1), token + "-");
-
-      return treeNode;
-    }
-  }
-
   private int[] getIndex(String s, String token) {
     char[] chars = s.toCharArray();
     int preIndex = chars.length - 1;
@@ -36,6 +16,30 @@ public class RecoverFromPreorder {
       }
     }
     return null;
+  }
+
+  class Solution {
+    public TreeNode recoverFromPreorder(String S) {
+      return re(S, "-");
+    }
+
+    public TreeNode re(String S, String token) {
+      int root = Integer.parseInt(String.valueOf(S.charAt(0)));
+      int[] indexes = getIndex(S, token);
+      if (indexes == null) {
+        return null;
+      }
+      int index = S.indexOf(token);
+      int lastIndex = S.lastIndexOf(token);
+      if (index == -1 || lastIndex == -1) {
+        return null;
+      }
+      TreeNode treeNode = new TreeNode(root);
+      treeNode.left = re(S.substring(1 + token.length(), indexes[0]), token + "-");
+      treeNode.right = re(S.substring(indexes[1]), token + "-");
+
+      return treeNode;
+    }
   }
 
   public class TreeNode {
