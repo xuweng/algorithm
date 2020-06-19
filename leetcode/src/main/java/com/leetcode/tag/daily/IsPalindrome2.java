c571e37f4bc270947fa45cd330a876213608c15b
xuweng <2028403305@qq.com>
2020-06-19 23:26:36 +0800

    125. 验证回文串
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/IsPalindrome2.java
@@ -0,0 +1,24 @@
+package com.leetcode.tag.daily;
+
+/**
+ * 审题
+ *
+ * <p>搞清楚题目
+ *
+ * <p>搞清楚题目
+ *
+ * <p>搞清楚题目
+ *
+ * <p>搞清楚题目
+ *
+ * <p>搞清楚题目
+ *
+ * <p>我们将空字符串定义为有效的回文串
+ *
+ * <p>125. 验证回文串
+ */
+public class IsPalindrome2 {
+  public boolean isPalindrome(String s) {
+    return true;
+  }
+}
