fdeb5ec3a7dce80f3863383c558a63e5ce65982f
xuweng <2028403305@qq.com>
2020-06-21 00:04:00 +0800

    剑指 Offer 38. 字符串的排列
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/Permutation2.java
@@ -0,0 +1,14 @@
+package com.leetcode.tag.backtracking;
+
+/**
+ * 递归深度
+ *
+ * <p>重复计算
+ *
+ * <p>剑指 Offer 38. 字符串的排列
+ */
+public class Permutation2 {
+  public String[] permutation(String s) {
+    return null;
+  }
+}
