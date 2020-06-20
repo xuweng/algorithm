e5560c2bb17f975ae9d940960352c73b2c7787c5
xuweng <2028403305@qq.com>
2020-06-21 00:10:42 +0800

    剑指 Offer 38. 字符串的排列
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/backtracking/Permutation2.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/Permutation2.java
@@ -1,5 +1,8 @@
 package com.leetcode.tag.backtracking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 递归深度
  *
@@ -9,6 +12,27 @@ package com.leetcode.tag.backtracking;
  */
 public class Permutation2 {
   public String[] permutation(String s) {
-    return null;
+
+    boolean[] used = new boolean[s.length()];
+    List<String> result = new ArrayList<>();
+
+    backTrack(s, used, "", result);
+
+    return result.toArray(new String[0]);
+  }
+
+  private void backTrack(String s, boolean[] used, String temp, List<String> result) {
+    if (temp.length() == s.length()) {
+      result.add(temp);
+      return;
+    }
+    for (int i = 0; i < s.length(); i++) {
+      if (used[i]) {
+        continue;
+      }
+      used[i] = true;
+      backTrack(s, used, temp + s.charAt(i), result);
+      used[i] = false;
+    }
   }
 }
