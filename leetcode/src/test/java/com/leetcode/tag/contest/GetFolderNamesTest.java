b86a0238bfc3526a3584aad56ac311781dd0af6d
xuweng <2028403305@qq.com>
2020-06-21 12:20:33 +0800

    特殊测试用例
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/test/java/com/leetcode/tag/contest/GetFolderNamesTest.java
@@ -0,0 +1,22 @@
+package com.leetcode.tag.contest;
+
+import org.junit.Test;
+
+public class GetFolderNamesTest {
+  @Test
+  public void test() {
+    /**
+     * 我的输入 ["o(1)","o","o(1)","o","o(4)","o"]
+     *
+     * <p>我的答案 ["o(1)","o","o(1)(1)","o(3)","o(4)","o(2)"]
+     *
+     * <p>预期答案 ["o(1)","o","o(1)(1)","o(2)","o(4)","o(3)"]
+     */
+    GetFolderNames getFolderNames = new GetFolderNames();
+
+    // 特殊测试用例
+    String[] strings = {"o(1)", "o", "o(1)", "o", "o(4)", "o"};
+
+    getFolderNames.getFolderNames(strings);
+  }
+}
