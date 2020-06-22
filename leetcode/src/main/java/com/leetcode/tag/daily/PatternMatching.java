42824d167bf01753176671c6250f3dc6a406a080
xuweng <2028403305@qq.com>
2020-06-22 23:53:00 +0800

    面试题 16.18. 模式匹配
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/PatternMatching.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/PatternMatching.java
@@ -1,17 +1,22 @@
 package com.leetcode.tag.daily;
 
+import java.util.Map;
+
 /**
  * 面试题 16.18. 模式匹配
  */
 public class PatternMatching {
+  private Map<Character, String> map;
+
   public boolean patternMatching(String pattern, String value) {
     return true;
   }
 
-  private void backTrack(String pattern, String value, int index) {
+  private void backTrack(String pattern, String value, int patternIndex, int valueIndex) {
     for (int i = 0; i < value.length(); i++) {
       String str = value.substring(0, i + 1);
-      backTrack(pattern, value.substring(i + 1), i);
+      map.putIfAbsent(pattern.charAt(patternIndex), str);
+      backTrack(pattern, value.substring(i + 1), i, valueIndex);
     }
   }
 }
