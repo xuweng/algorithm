b3d8f44d2db201f3aeeb680813e95e5d6c96bdf8
xuweng <2028403305@qq.com>
2020-06-23 00:24:06 +0800

    面试题 16.18. 模式匹配
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/test/java/com/leetcode/tag/daily/PatternMatchingTest.java
+++ leetcode/src/test/java/com/leetcode/tag/daily/PatternMatchingTest.java
@@ -7,8 +7,11 @@ public class PatternMatchingTest {
   public void test() {
     PatternMatching patternMatching = new PatternMatching();
 
+    //    String pattern = "abba";
+    //    String value = "dogcatcatdog";
+
     String pattern = "abba";
-    String value = "dogcatcatdog";
+    String value = "dogcatcatfish";
     patternMatching.patternMatching(pattern, value);
   }
 }
