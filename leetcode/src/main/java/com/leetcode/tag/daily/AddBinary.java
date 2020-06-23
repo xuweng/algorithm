11b8fa4e42347b66af639ba0c3a66899621b7451
xuweng <2028403305@qq.com>
2020-06-23 23:17:55 +0800

    在十进制的计算中「逢十进一」，二进制中我们需要「逢二进一」。
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/AddBinary.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/AddBinary.java
@@ -25,4 +25,37 @@ public class AddBinary {
       return Integer.toBinaryString(Integer.parseInt(a, 2) + Integer.parseInt(b, 2));
     }
   }
+
+  /**
+   * 作者：LeetCode-Solution
+   * 链接：https://leetcode-cn.com/problems/add-binary/solution/er-jin-zhi-qiu-he-by-leetcode-solution/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution1 {
+    /**
+     * 在十进制的计算中「逢十进一」，二进制中我们需要「逢二进一」。
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public String addBinary(String a, String b) {
+      StringBuffer ans = new StringBuffer();
+
+      int n = Math.max(a.length(), b.length()), carry = 0;
+      for (int i = 0; i < n; ++i) {
+        carry += i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
+        carry += i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0;
+        ans.append((char) (carry % 2 + '0'));
+        carry /= 2;
+      }
+
+      if (carry > 0) {
+        ans.append('1');
+      }
+      ans.reverse();
+
+      return ans.toString();
+    }
+  }
 }
