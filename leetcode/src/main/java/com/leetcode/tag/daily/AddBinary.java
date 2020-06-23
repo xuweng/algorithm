732146a980dc7c734e78589572d791de277a0e40
xuweng <2028403305@qq.com>
2020-06-23 23:19:17 +0800

    方法一：模拟
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/AddBinary.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/AddBinary.java
@@ -27,7 +27,9 @@ public class AddBinary {
   }
 
   /**
-   * 作者：LeetCode-Solution
+   * 方法一：模拟
+   *
+   * <p>作者：LeetCode-Solution
    * 链接：https://leetcode-cn.com/problems/add-binary/solution/er-jin-zhi-qiu-he-by-leetcode-solution/
    * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
    */
