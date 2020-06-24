05b38c0f3d3a563af04639ab834f9fdf18dfcbe5
xuweng <2028403305@qq.com>
2020-06-25 00:58:02 +0800

    如果以前有记录就不能删除
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/test/java/com/leetcode/tag/daily/PatternMatchingTest.java
+++ leetcode/src/test/java/com/leetcode/tag/daily/PatternMatchingTest.java
@@ -5,19 +5,19 @@ import org.junit.Test;
 public class PatternMatchingTest {
   @Test
   public void test() {
-      PatternMatching patternMatching = new PatternMatching();
+    PatternMatching patternMatching = new PatternMatching();
 
-      String pattern = "abba";
-      String value = "dogcatcatdog";
+    //      String pattern = "abba";
+    //      String value = "dogcatcatdog";
 
-      //    String pattern = "abba";
-      //    String value = "dogcatcatfish";
+    //    String pattern = "abba";
+    //    String value = "dogcatcatfish";
 
-      //    String pattern = "bbbaa";
-      //    // 重复字符不通过
-      //    String value = "xxxxxxy";
-      boolean result = patternMatching.patternMatching(pattern, value);
+    String pattern = "bbbaa";
+    // 重复字符不通过
+    String value = "xxxxxxy";
+    boolean result = patternMatching.patternMatching(pattern, value);
 
-      System.out.println();
+    System.out.println();
   }
 }
