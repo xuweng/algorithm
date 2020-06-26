39d9c7c87dd05a170d84c462552e4ddc61d58102
xuweng <2028403305@qq.com>
2020-06-26 16:52:43 +0800

    搜索所有答案.不是搜索到一个答案就返回。
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/test/java/com/leetcode/tag/backtracking/TotalNQueensTest.java
@@ -0,0 +1,13 @@
+package com.leetcode.tag.backtracking;
+
+import org.junit.Test;
+
+public class TotalNQueensTest {
+  TotalNQueens totalNQueens = new TotalNQueens();
+
+  @Test
+  public void test() {
+    int n = 4;
+    totalNQueens.totalNQueens(n);
+  }
+}
