600ac148e1466259b857c78159ce5d1e3e7d88d0
xuweng <2028403305@qq.com>
2020-06-26 12:58:10 +0800

    String+Set去重
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/backtracking/NumSquarefulPerms.java
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/NumSquarefulPerms.java
@@ -2,7 +2,9 @@ package com.leetcode.tag.backtracking;
 
 import com.leetcode.tag.util.Square;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 996. 正方形数组的数目
@@ -12,39 +14,37 @@ public class NumSquarefulPerms {
     Arrays.sort(A);
 
     boolean[] used = new boolean[A.length];
-    List<List<Integer>> result = new ArrayList<>();
-    Deque<Integer> stack = new LinkedList<>();
+    Set<String> result = new HashSet<>();
 
-    backTrack(A, used, result, stack);
+    backTrack(A, used, result, "");
 
     return (int) result.stream().filter(this::isSquare).count();
   }
 
-  private void backTrack(
-          int[] array, boolean[] used, List<List<Integer>> result, Deque<Integer> stack) {
-    if (stack.size() == array.length) {
-      result.add(new ArrayList<>(stack));
+  private void backTrack(int[] array, boolean[] used, Set<String> result, String temp) {
+    if (temp.length() == array.length) {
+      result.add(temp);
       return;
     }
     for (int i = 0; i < array.length; i++) {
       // 是否重复分支
-      boolean fen = (stack.isEmpty() && i > 0 && array[i] == array[i - 1]);
+      boolean fen = (temp.isEmpty() && i > 0 && array[i] == array[i - 1]);
       if (used[i] || fen) {
         continue;
       }
       // 不会选择重复分支，但是第一个分支会选择重复数据,需要处理第一个分支
       used[i] = true;
-      stack.push(array[i]);
-      backTrack(array, used, result, stack);
+      backTrack(array, used, result, temp + array[i]);
       used[i] = false;
-      stack.pop();
     }
   }
 
-  private boolean isSquare(List<Integer> list) {
+  private boolean isSquare(String str) {
     int pre = 0;
-    for (int i = 1; i < list.size(); i++) {
-      if (!Square.isPerfectSquare(list.get(pre) + list.get(i))) {
+    for (int i = 1; i < str.length(); i++) {
+      if (!Square.isPerfectSquare(
+              Integer.parseInt(String.valueOf(str.charAt(pre)))
+                      + Integer.parseInt(String.valueOf(str.charAt(i))))) {
         return false;
       }
       pre = i;
