c365ebc48fe625152c47babe6eeae0a42e3bf747
xuweng <2028403305@qq.com>
2020-06-27 16:08:02 +0800

    1307. 口算难题
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/VerbalArithmeticPuzzle.java
@@ -0,0 +1,88 @@
+package com.leetcode.tag.backtracking;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * 经典题目
+ *
+ * <p>简单实用
+ *
+ * <p>十分钟
+ *
+ * <p>搞懂所有示例
+ *
+ * <p>搞懂所有示例
+ *
+ * <p>搞懂所有示例
+ *
+ * <p>搞懂所有示例
+ *
+ * <p>1307. 口算难题
+ */
+public class VerbalArithmeticPuzzle {
+  public boolean isSolvable(String[] words, String result) {
+    Set<Character> set = new TreeSet<>();
+    for (String word : words) {
+      for (char c : word.toCharArray()) {
+        set.add(c);
+      }
+    }
+    for (char c : result.toCharArray()) {
+      set.add(c);
+    }
+    StringBuilder stringBuilder = new StringBuilder();
+    set.forEach(stringBuilder::append);
+
+    return backTrack(stringBuilder.toString(), 0, words, result, new HashMap<>());
+  }
+
+  /**
+   * 算法框架
+   *
+   * <p>算法框架
+   *
+   * <p>算法框架
+   *
+   * <p>算法框架
+   *
+   * <p>算法框架
+   *
+   * @param result
+   * @return
+   */
+  public boolean backTrack(
+          String word, int start, String[] words, String result, Map<Character, Integer> map) {
+    if (start >= word.length()) {
+      int sum = 0;
+      for (String s : words) {
+        sum += Integer.parseInt(s);
+      }
+
+      return sum == Integer.parseInt(result);
+    }
+    for (int i = 0; i <= 9; i++) {
+      char c = word.charAt(start);
+      if (map.containsKey(c) || map.containsValue(i)) {
+        continue;
+      }
+      map.put(c, i);
+      for (String w : words) {
+        w.replaceAll(String.valueOf(c), String.valueOf(i));
+      }
+      result.replaceAll(String.valueOf(c), String.valueOf(i));
+      if (backTrack(word, start + 1, words, result, map)) {
+        return true;
+      }
+      for (String w : words) {
+        w.replaceAll(String.valueOf(c), String.valueOf(c));
+      }
+      result.replaceAll(String.valueOf(c), String.valueOf(c));
+      map.remove(c);
+    }
+
+    return false;
+  }
+}
