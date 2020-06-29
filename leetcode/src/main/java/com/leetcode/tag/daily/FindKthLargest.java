3d21ceaf686469bb035665917ff87116613cba21
xuweng <2028403305@qq.com>
2020-06-29 23:27:11 +0800

    215. 数组中的第K个最大元素
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/FindKthLargest.java
@@ -0,0 +1,41 @@
+package com.leetcode.tag.daily;
+
+/**
+ * 215. 数组中的第K个最大元素
+ */
+public class FindKthLargest {
+    class Solution {
+        public int findKthLargest(int[] nums, int k) {
+            return quickSort(nums, 0, nums.length - 1, k);
+        }
+
+        private int quickSort(int[] nums, int l, int r, int k) {
+            if (l >= r) return nums[l];
+            //-1?,+1?
+            int i = l - 1, j = r + 1;
+            int x = nums[l + r >> 1];
+            while (i < j) {
+                do {
+                    i++;
+                } while (nums[i] < x);
+                do {
+                    j--;
+                } while (nums[j] > x);
+                if (i < j) {
+                    swap(nums, i, j);
+                }
+            }
+
+            if (r - j >= k) {
+                return quickSort(nums, j + 1, r, k);
+            }
+            return quickSort(nums, l, j, k - r + j);
+        }
+
+        private void swap(int[] nums, int a, int b) {
+            int tmp = nums[a];
+            nums[a] = nums[b];
+            nums[b] = tmp;
+        }
+    }
+}
