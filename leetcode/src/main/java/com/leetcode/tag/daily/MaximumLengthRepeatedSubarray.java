fae2471844bd438a2a393ee882e40a2d77908c88
xuweng <2028403305@qq.com>
2020-07-01 23:06:36 +0800

    718. 最长重复子数组
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/MaximumLengthRepeatedSubarray.java
@@ -0,0 +1,22 @@
+package com.leetcode.tag.daily;
+
+/**
+ * 718. 最长重复子数组
+ *
+ * <p>是否连续?
+ *
+ * <p>是否连续？
+ *
+ * <p>是否连续？
+ *
+ * <p>子数组一定连续。子串一定连续。
+ */
+public class MaximumLengthRepeatedSubarray {
+  public int findLength(int[] A, int[] B) {
+    // 排序不对。改变原来的顺序。不是子数组。
+    //    Arrays.sort(A);
+    //    Arrays.sort(B);
+
+    return 0;
+  }
+}
