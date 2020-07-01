3106f067701545a3b38d4f6a3ef64d41cad2e9a9
xuweng <2028403305@qq.com>
2020-07-01 23:39:47 +0800

    状态定义
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/MaximumLengthRepeatedSubarray.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/MaximumLengthRepeatedSubarray.java
@@ -54,4 +54,35 @@ public class MaximumLengthRepeatedSubarray {
     int[][] dpMax = new int[A.length][B.length];
     return dpMax[A.length - 1][B.length - 1];
   }
+
+  /**
+   * 作者：LeetCode-Solution
+   * 链接：https://leetcode-cn.com/problems/maximum-length-of-repeated-subarray/solution/zui-chang-zhong-fu-zi-shu-zu-by-leetcode-solution/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution {
+    /**
+     * 不是前i个?
+     *
+     * <p>A[i:] 表示数组 A 中索引 i 到数组末尾的范围对应的子数组
+     *
+     * <p>令 dp[i][j] 表示 A[i:] 和 B[j:] 的最长公共前缀，那么答案即为所有 dp[i][j] 中的最大值
+     *
+     * @param A
+     * @param B
+     * @return
+     */
+    public int findLength(int[] A, int[] B) {
+      int n = A.length, m = B.length;
+      int[][] dp = new int[n + 1][m + 1];
+      int ans = 0;
+      for (int i = n - 1; i >= 0; i--) {
+        for (int j = m - 1; j >= 0; j--) {
+          dp[i][j] = A[i] == B[j] ? dp[i + 1][j + 1] + 1 : 0;
+          ans = Math.max(ans, dp[i][j]);
+        }
+      }
+      return ans;
+    }
+  }
 }
