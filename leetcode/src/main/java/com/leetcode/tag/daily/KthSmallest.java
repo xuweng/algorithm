ed2fd9b00ee7bd45db55b17aa1ed0830758690fe
xuweng <2028403305@qq.com>
2020-07-03 00:01:18 +0800

    二分查找 图解
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/KthSmallest.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/KthSmallest.java
@@ -75,4 +75,58 @@ public class KthSmallest {
       return pq.poll()[0];
     }
   }
+
+  /**
+   * 方法三：二分查找
+   *
+   * <p>作者：LeetCode-Solution
+   * 链接：https://leetcode-cn.com/problems/kth-smallest-element-in-a-sorted-matrix/solution/you-xu-ju-zhen-zhong-di-kxiao-de-yuan-su-by-leetco/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution3 {
+    /**
+     * 图解。图解。图解。图解。图解。图解。图解
+     *
+     * <p>matrix[0][0] 为最小值，matrix[n−1][n−1] 为最大值，现在我们将其分别记作 l和 r。
+     *
+     * <p>可以发现一个性质：任取一个数 mid 满足 l< mid < r，
+     *
+     * <p>作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/kth-smallest-element-in-a-sorted-matrix/solution/you-xu-ju-zhen-zhong-di-kxiao-de-yuan-su-by-leetco/
+     * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param matrix
+     * @param k
+     * @return
+     */
+    public int kthSmallest(int[][] matrix, int k) {
+      int n = matrix.length;
+      int left = matrix[0][0];
+      int right = matrix[n - 1][n - 1];
+      while (left < right) {
+        int mid = left + ((right - left) >> 1);
+        if (check(matrix, mid, k, n)) {
+          right = mid;
+        } else {
+          left = mid + 1;
+        }
+      }
+      return left;
+    }
+
+    public boolean check(int[][] matrix, int mid, int k, int n) {
+      int i = n - 1;
+      int j = 0;
+      int num = 0;
+      while (i >= 0 && j < n) {
+        if (matrix[i][j] <= mid) {
+          num += i + 1;
+          j++;
+        } else {
+          i--;
+        }
+      }
+      return num >= k;
+    }
+  }
 }
