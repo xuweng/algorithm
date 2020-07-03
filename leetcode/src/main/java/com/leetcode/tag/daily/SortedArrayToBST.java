720ab365b6ccb502d30635a03dc1d090a6da537f
xuweng <2028403305@qq.com>
2020-07-03 23:20:27 +0800

    方法一：中序遍历，总是选择中间位置左边的数字作为根节点
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/SortedArrayToBST.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/SortedArrayToBST.java
@@ -33,6 +33,33 @@ public class SortedArrayToBST {
     return rootNode;
   }
 
+  /**
+   * 方法一：中序遍历，总是选择中间位置左边的数字作为根节点
+   *
+   * <p>作者：LeetCode-Solution
+   * 链接：https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/solution/jiang-you-xu-shu-zu-zhuan-huan-wei-er-cha-sou-s-33/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+      return helper(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode helper(int[] nums, int left, int right) {
+      if (left > right) {
+        return null;
+      }
+
+      // 总是选择中间位置左边的数字作为根节点
+      int mid = (left + right) / 2;
+
+      TreeNode root = new TreeNode(nums[mid]);
+      root.left = helper(nums, left, mid - 1);
+      root.right = helper(nums, mid + 1, right);
+      return root;
+    }
+  }
+
   public class TreeNode {
     int val;
     TreeNode left;
