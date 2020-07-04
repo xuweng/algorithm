3bf30951e8434a6030c191e8388d48a8d6e40d95
xuweng <2028403305@qq.com>
2020-07-04 23:50:00 +0800

    官方dp
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/LongestValidParentheses.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/LongestValidParentheses.java
@@ -45,4 +45,37 @@ public class LongestValidParentheses {
 
     return stack.isEmpty();
   }
+
+  /**
+   * 作者：LeetCode-Solution
+   * 链接：https://leetcode-cn.com/problems/longest-valid-parentheses/solution/zui-chang-you-xiao-gua-hao-by-leetcode-solution/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  public class Solution {
+    /**
+     * 我们定义 dp[i] 表示以下标 i 字符结尾的最长有效括号的长度。我们将 dp 数组全部初始化为 0 。
+     *
+     * <p>显然有效的子串一定以‘)’ 结尾，因此我们可以知道以 ‘(’ 结尾的子串对应的 dp 值必定为 0 ，
+     *
+     * <p>我们只需要求解‘)’ 在dp 数组中对应位置的值。
+     *
+     * @param s
+     * @return
+     */
+    public int longestValidParentheses(String s) {
+      int maxans = 0;
+      int dp[] = new int[s.length()];
+      for (int i = 1; i < s.length(); i++) {
+        if (s.charAt(i) == ')') {
+          if (s.charAt(i - 1) == '(') {
+            dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+          } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+            dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+          }
+          maxans = Math.max(maxans, dp[i]);
+        }
+      }
+      return maxans;
+    }
+  }
 }
