aa3a81a38f596adab6a616bd4e529c50f36d51ea
xuweng <2028403305@qq.com>
2020-07-04 23:28:24 +0800

    32. 最长有效括号
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/LongestValidParentheses.java
@@ -0,0 +1,47 @@
+package com.leetcode.tag.daily;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 32. 最长有效括号
+ *
+ * <p>最长?dp?
+ *
+ * <p>枚举所有子串?
+ */
+public class LongestValidParentheses {
+  public int longestValidParentheses(String s) {
+    if (s == null || s.isEmpty()) {
+      return 0;
+    }
+    int max = Integer.MIN_VALUE;
+    for (int i = 0; i < s.length(); i++) {
+      for (int j = i; j < s.length(); j++) {
+        String str = s.substring(i, j + 1);
+        if (check(str)) {
+          max = Math.max(max, str.length());
+        }
+      }
+    }
+    return max;
+  }
+
+  public boolean check(String s) {
+    if (s.charAt(0) == ')') {
+      return false;
+    }
+    Deque<Character> stack = new LinkedList<>();
+    for (char c : s.toCharArray()) {
+      if (c == '(') {
+        stack.push(c);
+      } else {
+        if (!stack.isEmpty()) {
+          stack.pop();
+        }
+      }
+    }
+
+    return stack.isEmpty();
+  }
+}
