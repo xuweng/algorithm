3f788fd0632d5bebeba6f7b7426356c7a20d84d8
xuweng <2028403305@qq.com>
2020-07-05 11:47:16 +0800

    用图来理解
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/NumSubmat.java
@@ -0,0 +1,32 @@
+package com.leetcode.tag.contest;
+
+/**
+ * 5454. 统计全 1 子矩形
+ *
+ * <p>连通性。用图来理解。
+ *
+ * <p>用图来理解。
+ *
+ * <p>用图来理解。
+ *
+ * <p>用图来理解。
+ *
+ * <p>用图来理解
+ *
+ * <p>用图来理解
+ */
+public class NumSubmat {
+  /**
+   * 这道题很直观
+   *
+   * <p>这道题很直观
+   *
+   * <p>这道题很直观
+   *
+   * @param mat
+   * @return
+   */
+  public int numSubmat(int[][] mat) {
+    return 0;
+  }
+}
