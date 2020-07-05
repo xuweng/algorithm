a734eea868048fea681448247b047a31c3e742e4
xuweng <2028403305@qq.com>
2020-07-05 10:24:17 +0800

    两个字符串的状态定义
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/WildcardMatching.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/WildcardMatching.java
@@ -36,11 +36,24 @@ public class WildcardMatching {
    *
    * <p>星号 ∗，可以匹配任意字符串，可以为空，也就是匹配零或任意多个小写字母。
    *
+   * <p>其中「小写字母」和「问号」的匹配是确定的，而「星号」的匹配是不确定的，因此我们需要枚举所有的匹配情况。
+   *
+   * <p>为了减少重复枚举，我们可以使用动态规划来解决本题。
+   *
    * <p>作者：LeetCode-Solution
    * 链接：https://leetcode-cn.com/problems/wildcard-matching/solution/tong-pei-fu-pi-pei-by-leetcode-solution/
    * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
    */
   class Solution1 {
+    /**
+     * 两个字符串的状态定义
+     *
+     * <p>我们用dp[i][j] 表示字符串 s 的前 i 个字符和模式 p 的前 j 个字符是否能匹配
+     *
+     * @param s
+     * @param p
+     * @return
+     */
     public boolean isMatch(String s, String p) {
       int m = s.length();
       int n = p.length();
