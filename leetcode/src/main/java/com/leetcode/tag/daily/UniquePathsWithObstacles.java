56577138f1e26172786c1431d061b849165c2060
xuweng <2028403305@qq.com>
2020-07-06 23:25:54 +0800

    63. 不同路径 II
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/UniquePathsWithObstacles.java
@@ -0,0 +1,40 @@
+package com.leetcode.tag.daily;
+
+/**
+ * 63. 不同路径 II
+ */
+public class UniquePathsWithObstacles {
+  private int count;
+
+  public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    boolean[][] used = new boolean[obstacleGrid.length][obstacleGrid[0].length];
+    backTrack(obstacleGrid, used, 0, 0);
+
+    return count;
+  }
+
+  public void backTrack(int[][] obstacleGrid, boolean[][] used, int row, int col) {
+    if (row >= obstacleGrid.length) {
+      return;
+    }
+    if (col >= obstacleGrid[0].length) {
+      return;
+    }
+    if (row == obstacleGrid.length - 1 && col == obstacleGrid[0].length - 1) {
+      count++;
+      return;
+    }
+    // 向右
+    if (!used[row][col + 1] && obstacleGrid[row][col + 1] != 1) {
+      used[row][col + 1] = true;
+      backTrack(obstacleGrid, used, row, col + 1);
+      used[row][col + 1] = false;
+    }
+    // 向下
+    if (!used[row + 1][col] && obstacleGrid[row + 1][col] != 1) {
+      used[row + 1][col] = true;
+      backTrack(obstacleGrid, used, row + 1, col);
+      used[row + 1][col] = false;
+    }
+  }
+}
