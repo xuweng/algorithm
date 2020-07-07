c8dd18f36713c134c7a9a0ec4ee0a6cf9ff5f6b3
xuweng <2028403305@qq.com>
2020-07-07 23:25:36 +0800

    递归
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/HasPathSum.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/HasPathSum.java
@@ -86,6 +86,25 @@ public class HasPathSum {
     }
   }
 
+  /**
+   * 方法二：递归
+   *
+   * <p>作者：LeetCode-Solution
+   * 链接：https://leetcode-cn.com/problems/path-sum/solution/lu-jing-zong-he-by-leetcode-solution/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution1 {
+    public boolean hasPathSum(TreeNode root, int sum) {
+      if (root == null) {
+        return false;
+      }
+      if (root.left == null && root.right == null) {
+        return sum == root.val;
+      }
+      return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+  }
+
   class TreeNode {
     int val;
     TreeNode left;
