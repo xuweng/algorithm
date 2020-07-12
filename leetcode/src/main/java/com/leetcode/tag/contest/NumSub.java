8195e8df2bc5442fda94baa8fecbade5f4a32db9
xuweng <2028403305@qq.com>
2020-07-12 11:13:32 +0800

    5461. 仅含 1 的子串数
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/NumSub.java
@@ -0,0 +1,38 @@
+package com.leetcode.tag.contest;
+
+import java.util.stream.IntStream;
+
+/**
+ * 5461. 仅含 1 的子串数
+ */
+public class NumSub {
+  /**
+   * s[i] == '0' 或 s[i] == '1'
+   *
+   * <p>1 <= s.length <= 10^5
+   */
+  class Solution {
+    public int numSub(String s) {
+      if (s == null || s.length() == 0) {
+        return 0;
+      }
+      long result = 0;
+      for (int i = 0; i < s.length(); i++) {
+        if (s.charAt(i) == '0') {
+          continue;
+        }
+        int j = i;
+        while (j < s.length() && s.charAt(j) == '1') {
+          j++;
+        }
+        result += sum(j - i);
+        i = j;
+      }
+      return (int) (result % (Math.pow(10, 9) + 7));
+    }
+
+    private long sum(int count) {
+      return IntStream.rangeClosed(1, count).sum();
+    }
+  }
+}
