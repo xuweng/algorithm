d0bf5547a0f5c4d3965eed904b3388b1de047a1e
xuweng <2028403305@qq.com>
2020-07-13 00:02:20 +0800

    每次只能向右或者向下移动一步。让人很容易想到该题使用动态规划的方法
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/CalculateMinimumHP.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/CalculateMinimumHP.java
@@ -1,7 +1,15 @@
 package com.leetcode.tag.daily;
 
+import java.util.Arrays;
+
 /**
- * 174. 地下城游戏
+ * 十分钟看答案
+ *
+ * <p>十分钟看答案
+ *
+ * <p>十分钟看答案
+ *
+ * <p>174. 地下城游戏
  */
 public class CalculateMinimumHP {
   /**
@@ -35,6 +43,35 @@ public class CalculateMinimumHP {
       }
 
       return dp[dungeon.length - 1][dungeon[0].length - 1];
+    }
+  }
+
+  /**
+   * 方法一：动态规划
+   */
+  class Solution1 {
+    /**
+     * 方法一：动态规划 思路及算法
+     *
+     * <p>几个要素：「M×N 的网格」「每次只能向右或者向下移动一步」。让人很容易想到该题使用动态规划的方法。
+     *
+     * @param dungeon
+     * @return
+     */
+    public int calculateMinimumHP(int[][] dungeon) {
+      int n = dungeon.length, m = dungeon[0].length;
+      int[][] dp = new int[n + 1][m + 1];
+      for (int i = 0; i <= n; ++i) {
+        Arrays.fill(dp[i], Integer.MAX_VALUE);
+      }
+      dp[n][m - 1] = dp[n - 1][m] = 1;
+      for (int i = n - 1; i >= 0; --i) {
+        for (int j = m - 1; j >= 0; --j) {
+          int minn = Math.min(dp[i + 1][j], dp[i][j + 1]);
+          dp[i][j] = Math.max(minn - dungeon[i][j], 1);
+        }
+      }
+      return dp[0][0];
     }
   }
 }
