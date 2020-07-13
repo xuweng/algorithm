ca2c3965c87b9114aefdd3e9208fbb07092738b4
xuweng <2028403305@qq.com>
2020-07-13 23:35:26 +0800

    hashmap计数还能这么使用
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/Intersect.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/Intersect.java
@@ -47,6 +47,27 @@ public class Intersect {
    * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
    */
   class Solution {
+    /**
+     * hashmap计数还能这么使用
+     *
+     * <p>时间复杂度：O(m+n)，其中 m 和 n 分别是两个数组的长度。需要遍历两个数组并对哈希表进行操作，哈希表操作的时间复杂度是
+     *
+     * <p>O(1)，因此总时间复杂度与两个数组的长度和呈线性关系。
+     *
+     * <p>空间复杂度O(min(m,n))，其中 m 和 n
+     *
+     * <p>分别是两个数组的长度。对较短的数组进行哈希表的操作，哈希表的大小不会超过较短的数组的长度。
+     *
+     * <p>为返回值创建一个数组 intersection，其长度为较短的数组的长度。
+     *
+     * <p>作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/solution/liang-ge-shu-zu-de-jiao-ji-ii-by-leetcode-solution/
+     * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
     public int[] intersect(int[] nums1, int[] nums2) {
       if (nums1.length > nums2.length) {
         return intersect(nums2, nums1);
@@ -60,17 +81,18 @@ public class Intersect {
       int index = 0;
       for (int num : nums2) {
         int count = map.getOrDefault(num, 0);
+        if (count <= 0) {
+          continue;
+        }
+        // 存在哈希表
+        intersection[index++] = num;
+        count--;
         if (count > 0) {
-          // 存在哈希表
-          intersection[index++] = num;
-          count--;
-          if (count > 0) {
-            // 重新放入map
-            map.put(num, count);
-          } else {
-            // 删除
-            map.remove(num);
-          }
+          // 重新放入map
+          map.put(num, count);
+        } else {
+          // 删除
+          map.remove(num);
         }
       }
       return Arrays.copyOfRange(intersection, 0, index);
