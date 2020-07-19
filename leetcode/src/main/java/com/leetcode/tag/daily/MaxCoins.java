ea720ed120cc7ed0e2beed15df5a6825d369ca5a
xuweng <2028403305@qq.com>
2020-07-19 09:06:50 +0800

    倒过来。换位思考
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/MaxCoins.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/MaxCoins.java
@@ -1,5 +1,7 @@
 package com.leetcode.tag.daily;
 
+import java.util.Arrays;
+
 /**
  * 312. 戳气球
  *
@@ -37,10 +39,10 @@ public class MaxCoins {
 
     /**
      * 戳气球的操作，发现这会导致两个气球从不相邻变成相邻，使得后续操作难以处理。
-     * <p>
-     * 于是我们倒过来看这些操作，将全过程看作是每次添加一个气球。
-     * <p>
-     * 第low和第high都是不可戳
+     *
+     * <p>于是我们倒过来看这些操作，将全过程看作是每次添加一个气球。
+     *
+     * <p>第low和第high都是不可戳
      *
      * @param nums
      * @param low
@@ -59,11 +61,51 @@ public class MaxCoins {
       int max = Integer.MIN_VALUE;
       // i的范围?low<i<high
       for (int i = low; i <= high; i++) {
+        // 将全过程看作是每次添加一个气球
         max =
                 Math.max(max, re(nums, low, i) + re(nums, i, high) + nums[low] * nums[i] * nums[high]);
       }
       me[low][high] = max;
       return max;
+    }
+  }
+
+  /**
+   * 作者：LeetCode-Solution
+   * 链接：https://leetcode-cn.com/problems/burst-balloons/solution/chuo-qi-qiu-by-leetcode-solution/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution1 {
+    public int[][] rec;
+    public int[] val;
+
+    public int maxCoins(int[] nums) {
+      int n = nums.length;
+      val = new int[n + 2];
+      for (int i = 1; i <= n; i++) {
+        val[i] = nums[i - 1];
+      }
+      val[0] = val[n + 1] = 1;
+      rec = new int[n + 2][n + 2];
+      for (int i = 0; i <= n + 1; i++) {
+        Arrays.fill(rec[i], -1);
+      }
+      return solve(0, n + 1);
+    }
+
+    public int solve(int left, int right) {
+      if (left >= right - 1) {
+        return 0;
+      }
+      if (rec[left][right] != -1) {
+        return rec[left][right];
+      }
+      for (int i = left + 1; i < right; i++) {
+        int sum = val[left] * val[i] * val[right];
+        sum += solve(left, i) + solve(i, right);
+        rec[left][right] = Math.max(rec[left][right], sum);
+      }
+      return rec[left][right];
     }
   }
 }
