a97b3943dcb0b253f1d02b0f9f44563f93c53651
xuweng <2028403305@qq.com>
2020-07-19 12:17:06 +0800

    构建树的map 构建图的map
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/contest/CountSubTrees.java
+++ leetcode/src/main/java/com/leetcode/tag/contest/CountSubTrees.java
@@ -26,19 +26,17 @@ public class CountSubTrees {
   class Solution {
     Map<Integer, List<Integer>> map = new HashMap<>();
     int count;
-    boolean[] visited;
 
     public int[] countSubTrees(int n, int[][] edges, String labels) {
+      // 构建map错误
+      // 构建树的map
+      // 不是构建图的map
       for (int[] a : edges) {
         List<Integer> list = map.getOrDefault(a[0], new ArrayList<>());
         list.add(a[1]);
         map.put(a[0], list);
-
-        List<Integer> list1 = map.getOrDefault(a[1], new ArrayList<>());
-        list1.add(a[0]);
-        map.put(a[1], list1);
       }
-      visited = new boolean[n];
+
       char[] chars = labels.toCharArray();
       int[] result = new int[n];
       Arrays.fill(result, 1);
@@ -47,7 +45,6 @@ public class CountSubTrees {
         if (list == null) {
           continue;
         }
-        visited[i] = true;
         re(chars[i], i, chars);
         result[i] = result[i] + count;
         count = 0;
@@ -62,15 +59,10 @@ public class CountSubTrees {
         return;
       }
       for (int j : list) {
-        if (visited[j]) {
-          continue;
-        }
         if (c == chars[j]) {
           count++;
         }
-        visited[j] = true;
         re(c, j, chars);
-        visited[j] = false;
       }
     }
   }
