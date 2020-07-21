fb93bf45f6d1cbb21af5e573e2d11baa2c80a9cb
xuweng <2028403305@qq.com>
2020-07-21 23:24:50 +0800

    95. 不同的二叉搜索树 II
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/GenerateTrees.java
@@ -0,0 +1,95 @@
+package com.leetcode.tag.daily;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 95. 不同的二叉搜索树 II
+ *
+ * <p>十分钟看答案
+ *
+ * <p>十分钟看答案
+ *
+ * <p>十分钟看答案
+ *
+ * <p>十分钟看答案
+ *
+ * <p>十分钟看答案
+ *
+ * <p>十分钟看答案
+ *
+ * <p>十分钟看答案
+ */
+public class GenerateTrees {
+  /**
+   * 二叉搜索树关键的性质是根节点的值大于左子树所有节点的值，小于右子树所有节点的值，且左子树和右子树也同样为二叉搜索树
+   *
+   * <p>如果我们枚举根节点的值为 i，那么根据二叉搜索树的性质我们可以知道左子树的节点值的集合为[1…i−1]，右子树的节点值的集合为 [i+1…n]
+   */
+  class Solution {
+    public List<TreeNode> generateTrees(int n) {
+      return null;
+    }
+  }
+
+  /**
+   * 作者：LeetCode-Solution
+   * 链接：https://leetcode-cn.com/problems/unique-binary-search-trees-ii/solution/bu-tong-de-er-cha-sou-suo-shu-ii-by-leetcode-solut/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution1 {
+    public List<TreeNode> generateTrees(int n) {
+      if (n == 0) {
+        return new LinkedList<>();
+      }
+      return generateTrees(1, n);
+    }
+
+    public List<TreeNode> generateTrees(int start, int end) {
+      List<TreeNode> allTrees = new LinkedList<>();
+      if (start > end) {
+        allTrees.add(null);
+        return allTrees;
+      }
+
+      // 枚举可行根节点
+      for (int i = start; i <= end; i++) {
+        // 获得所有可行的左子树集合
+        List<TreeNode> leftTrees = generateTrees(start, i - 1);
+
+        // 获得所有可行的右子树集合
+        List<TreeNode> rightTrees = generateTrees(i + 1, end);
+
+        // 从左子树集合中选出一棵左子树，从右子树集合中选出一棵右子树，拼接到根节点上
+        for (TreeNode left : leftTrees) {
+          for (TreeNode right : rightTrees) {
+            TreeNode currTree = new TreeNode(i);
+            currTree.left = left;
+            currTree.right = right;
+            allTrees.add(currTree);
+          }
+        }
+      }
+      return allTrees;
+    }
+  }
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+}
