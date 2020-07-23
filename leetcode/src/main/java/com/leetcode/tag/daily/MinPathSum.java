d05e69dc16fa45350562e219a754ac7d0d6f1122
xuweng <2028403305@qq.com>
2020-07-23 23:09:01 +0800

    64. 最小路径和
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/MinPathSum.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/MinPathSum.java
@@ -25,4 +25,32 @@ public class MinPathSum {
       return dp[grid.length - 1][grid[0].length - 1];
     }
   }
+
+  /**
+   * 作者：LeetCode-Solution
+   * 链接：https://leetcode-cn.com/problems/minimum-path-sum/solution/zui-xiao-lu-jing-he-by-leetcode-solution/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution1 {
+    public int minPathSum(int[][] grid) {
+      if (grid == null || grid.length == 0 || grid[0].length == 0) {
+        return 0;
+      }
+      int rows = grid.length, columns = grid[0].length;
+      int[][] dp = new int[rows][columns];
+      dp[0][0] = grid[0][0];
+      for (int i = 1; i < rows; i++) {
+        dp[i][0] = dp[i - 1][0] + grid[i][0];
+      }
+      for (int j = 1; j < columns; j++) {
+        dp[0][j] = dp[0][j - 1] + grid[0][j];
+      }
+      for (int i = 1; i < rows; i++) {
+        for (int j = 1; j < columns; j++) {
+          dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+        }
+      }
+      return dp[rows - 1][columns - 1];
+    }
+  }
 }
