ed991b91cae053257f8df5ebef4130fa08fdcfcb
xuweng <2028403305@qq.com>
2020-07-26 00:09:13 +0800

    有点腰
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/SplitArray.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/SplitArray.java
@@ -78,6 +78,7 @@ public class SplitArray {
         // 即我们可以枚举 k，其中前 k 个数被分割为 j−1 段，而第 k+1 到第 i 个数为第 j 段
         for (int j = 1; j <= Math.min(i, m); j++) {
           for (int k = 0; k < i; k++) {
+            // m 个子数组各自和的最大值最小。有点腰。和的最大值。和的最大值。
             f[i][j] = Math.min(f[i][j], Math.max(f[k][j - 1], sub[i] - sub[k]));
           }
         }
