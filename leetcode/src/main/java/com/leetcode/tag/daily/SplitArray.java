b5da2c4162d9469b3af0fd6148817eeb8f8b195c
xuweng <2028403305@qq.com>
2020-07-26 00:16:41 +0800

    方法二：二分查找 + 贪心
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/SplitArray.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/SplitArray.java
@@ -86,4 +86,56 @@ public class SplitArray {
       return f[n][m];
     }
   }
+
+  /**
+   * 方法二：二分查找 + 贪心
+   *
+   * <p>思路及算法
+   *
+   * <p>「使……最大值尽可能小」是二分搜索题目常见的问法。
+   *
+   * <p>本题中，我们注意到：当我们选定一个值 x，我们可以线性地验证是否存在一种分割方案，满足其最大分割子数组和不超过 x。策略如下：
+   *
+   * <p>贪心地模拟分割的过程，从前到后遍历数组，用 sum 表示当前分割子数组的和，cnt 表示已经分割出的子数组的数量（包括当前子数组），
+   *
+   * <p>那么每当sum 加上当前值超过x，我们就把当前取的值作为新的一段分割子数组的开头，并将 cnt 加 1。遍历结束后验证是否 cnt 不超过 m。
+   *
+   * <p>作者：LeetCode-Solution
+   * 链接：https://leetcode-cn.com/problems/split-array-largest-sum/solution/fen-ge-shu-zu-de-zui-da-zhi-by-leetcode-solution/
+   * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   */
+  class Solution2 {
+    public int splitArray(int[] nums, int m) {
+      int left = 0, right = 0;
+      for (int num : nums) {
+        right += num;
+        if (left < num) {
+          left = num;
+        }
+      }
+      while (left < right) {
+        int mid = (right - left) / 2 + left;
+        if (check(nums, mid, m)) {
+          right = mid;
+        } else {
+          left = mid + 1;
+        }
+      }
+      return left;
+    }
+
+    public boolean check(int[] nums, int x, int m) {
+      int sum = 0;
+      int cnt = 1;
+      for (int num : nums) {
+        if (sum + num > x) {
+          cnt++;
+          sum = num;
+        } else {
+          sum += num;
+        }
+      }
+      return cnt <= m;
+    }
+  }
 }
