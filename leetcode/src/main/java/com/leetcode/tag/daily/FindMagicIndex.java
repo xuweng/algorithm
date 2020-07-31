115cb8f8817860064df1abadc9bee6b9aedd512f
xuweng <2028403305@qq.com>
2020-07-31 23:31:42 +0800

    面试题 08.03. 魔术索引

--- leetcode/src/main/java/com/leetcode/tag/daily/FindMagicIndex.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/FindMagicIndex.java
@@ -17,4 +17,31 @@ public class FindMagicIndex {
             return -1;
         }
     }
+
+    /**
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/magic-index-lcci/solution/mo-zhu-suo-yin-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int findMagicIndex(int[] nums) {
+            return getAnswer(nums, 0, nums.length - 1);
+        }
+
+        public int getAnswer(int[] nums, int left, int right) {
+            if (left > right) {
+                return -1;
+            }
+            int mid = (right - left) / 2 + left;
+            int leftAnswer = getAnswer(nums, left, mid - 1);
+            if (leftAnswer != -1) {
+                return leftAnswer;
+            } else if (nums[mid] == mid) {
+                return mid;
+            }
+            return getAnswer(nums, mid + 1, right);
+        }
+    }
+
 }
