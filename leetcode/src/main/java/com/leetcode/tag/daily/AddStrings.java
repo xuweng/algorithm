7e497c4e6de6df8038f61deb05aa7f44ee77d577
xuweng <2028403305@qq.com>
2020-08-03 23:08:03 +0800

    415. 字符串相加
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/AddStrings.java
@@ -0,0 +1,19 @@
+package com.leetcode.tag.daily;
+
+/**
+ * 415. 字符串相加
+ */
+public class AddStrings {
+    class Solution {
+        /**
+         * 给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。
+         *
+         * @param num1 定两个字符串形式的非负整数
+         * @param num2 定两个字符串形式的非负整数
+         * @return 计算它们的和
+         */
+        public String addStrings(String num1, String num2) {
+            return String.valueOf(Integer.parseInt(num1) + Integer.parseInt(num2));
+        }
+    }
+}
