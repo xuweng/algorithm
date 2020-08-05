99f29d08ae6adcf1657972199a8c0e449e4a753f
xuweng <2028403305@qq.com>
2020-08-05 23:49:47 +0800

    337. 打家劫舍 III
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/HouseRobberIii.java
@@ -0,0 +1,47 @@
+package com.leetcode.tag.daily;
+
+/**
+ * 337. 打家劫舍 III
+ * <p>
+ * 2个示例已经很明显。以root为标准。
+ */
+public class HouseRobberIii {
+    class Solution {
+        /**
+         * 写完代码后在脑海里执行所有的测试用例
+         *
+         * @param root
+         * @return
+         */
+        public int rob(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            if (root.left == null && root.right == null) {
+                return root.val;
+            }
+            int leftMax = 0;
+            int rightMax = 0;
+            if (root.left != null) {
+                leftMax = Math.max(rob(root.left.left), rob(root.left.right));
+            }
+            if (root.right != null) {
+                rightMax = Math.max(rob(root.right.left), rob(root.right.right));
+            }
+            int max1 = rob(root.left) + rob(root.right);
+            int max2 = root.val + leftMax + rightMax;
+
+            return Math.max(max1, max2);
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
