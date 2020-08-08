b040326edc8319cc1f3db8849b0562c33bcb46ab
xuweng <2028403305@qq.com>
2020-08-08 23:59:05 +0800

    99. 恢复二叉搜索树
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/RecoverTree.java
@@ -0,0 +1,90 @@
+package com.leetcode.tag.daily;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 99. 恢复二叉搜索树
+ */
+public class RecoverTree {
+    /**
+     * 想到中序遍历
+     */
+    class Solution {
+        public void recoverTree(TreeNode root) {
+
+        }
+    }
+
+    /**
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/recover-binary-search-tree/solution/hui-fu-er-cha-sou-suo-shu-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public void recoverTree(TreeNode root) {
+            List<Integer> nums = new ArrayList<>();
+            inorder(root, nums);
+            int[] swapped = findTwoSwapped(nums);
+            recover(root, 2, swapped[0], swapped[1]);
+        }
+
+        public void inorder(TreeNode root, List<Integer> nums) {
+            if (root == null) {
+                return;
+            }
+            inorder(root.left, nums);
+            nums.add(root.val);
+            inorder(root.right, nums);
+        }
+
+        public int[] findTwoSwapped(List<Integer> nums) {
+            int n = nums.size();
+            int x = -1, y = -1;
+            for (int i = 0; i < n - 1; ++i) {
+                if (nums.get(i + 1) < nums.get(i)) {
+                    y = nums.get(i + 1);
+                    if (x == -1) {
+                        x = nums.get(i);
+                    } else {
+                        break;
+                    }
+                }
+            }
+            return new int[]{x, y};
+        }
+
+        public void recover(TreeNode root, int count, int x, int y) {
+            if (root != null) {
+                if (root.val == x || root.val == y) {
+                    root.val = root.val == x ? y : x;
+                    if (--count == 0) {
+                        return;
+                    }
+                }
+                recover(root.right, count, x, y);
+                recover(root.left, count, x, y);
+            }
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
