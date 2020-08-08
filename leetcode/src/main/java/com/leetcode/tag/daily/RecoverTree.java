dd8c02a7bc0081b0aab29aadefd325aabaaed87d
xuweng <2028403305@qq.com>
2020-08-09 00:06:47 +0800

    复杂算法拆分成多个函数
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/RecoverTree.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/RecoverTree.java
@@ -25,6 +25,11 @@ public class RecoverTree {
      * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
      */
     class Solution1 {
+        /**
+         * 复杂算法拆分成多个函数
+         *
+         * @param root
+         */
         public void recoverTree(TreeNode root) {
             //中序遍历结果
             List<Integer> nums = new ArrayList<>();
@@ -33,6 +38,12 @@ public class RecoverTree {
             recover(root, 2, swapped[0], swapped[1]);
         }
 
+        /**
+         * 中序遍历
+         *
+         * @param root
+         * @param nums
+         */
         public void inorder(TreeNode root, List<Integer> nums) {
             if (root == null) {
                 return;
@@ -42,33 +53,49 @@ public class RecoverTree {
             inorder(root.right, nums);
         }
 
+        /**
+         * 找到两个没有排序的数字
+         *
+         * @param nums
+         * @return
+         */
         public int[] findTwoSwapped(List<Integer> nums) {
             int n = nums.size();
             int x = -1, y = -1;
             for (int i = 0; i < n - 1; ++i) {
-                if (nums.get(i + 1) < nums.get(i)) {
-                    y = nums.get(i + 1);
-                    if (x == -1) {
-                        x = nums.get(i);
-                    } else {
-                        break;
-                    }
+                if (nums.get(i + 1) >= nums.get(i)) {
+                    continue;
+                }
+                y = nums.get(i + 1);
+                if (x == -1) {
+                    x = nums.get(i);
+                } else {
+                    break;
                 }
             }
             return new int[]{x, y};
         }
 
+        /**
+         * 先序遍历来交换两个数字
+         *
+         * @param root
+         * @param count
+         * @param x
+         * @param y
+         */
         public void recover(TreeNode root, int count, int x, int y) {
-            if (root != null) {
-                if (root.val == x || root.val == y) {
-                    root.val = root.val == x ? y : x;
-                    if (--count == 0) {
-                        return;
-                    }
-                }
-                recover(root.right, count, x, y);
-                recover(root.left, count, x, y);
+            if (root == null) {
+                return;
             }
+            if (root.val == x || root.val == y) {
+                root.val = root.val == x ? y : x;
+                if (--count == 0) {
+                    return;
+                }
+            }
+            recover(root.right, count, x, y);
+            recover(root.left, count, x, y);
         }
     }
 
