52fbbec5c305aa092590726d680e659fd544bcfa
xuweng <2028403305@qq.com>
2020-08-09 10:22:45 +0800

    看思路后看代码
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/RestoreIpAddresses.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/RestoreIpAddresses.java
@@ -22,13 +22,19 @@ public class RestoreIpAddresses {
      * <p>
      * 回溯
      * <p>
-     * 题解的思路与算法
+     * 题解的思路与算法。
+     * <p>
+     * 看思路后看代码。
      * <p>
      * 递归函数。递归函数。递归函数。递归函数。递归函数。递归函数。递归函数
      * <p>
      * 递归函数dfs(segId,segStart) 表示我们正在从s[segStart] 的位置开始，搜索 IP 地址中的第 segId 段，
      * <p>
      * 其中segId∈{0,1,2,3}。最多3段。最多3段。最多3段。最多3段。最多3段。
+     * <p>
+     * 由于 IP 地址的每一段必须是 [0,255] 中的整数，因此我们从 segStart 开始，从小到大依次枚举当前这一段 IP 地址的结束位置segEnd
+     * <p>
+     * 如果满足要求，就递归地进行下一段搜索，调用递归函数 dfs(segId+1,segEnd+1)。
      */
     class Solution {
         public List<String> restoreIpAddresses(String s) {
