39b41119845917a402cd10f3ed5af5a15116a8bd
xuweng <2028403305@qq.com>
2020-08-09 11:40:28 +0800

    5471. 和为目标值的最大数目不重叠非空子数组数目
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/MaxNonOverlapping.java
@@ -0,0 +1,18 @@
+package com.leetcode.tag.contest;
+
+/**
+ * 5471. 和为目标值的最大数目不重叠非空子数组数目
+ * <p>
+ * 通过示例搞懂题意
+ * <p>
+ * 通过示例搞懂题意
+ * <p>
+ * 通过示例搞懂题意
+ */
+public class MaxNonOverlapping {
+    class Solution {
+        public int maxNonOverlapping(int[] nums, int target) {
+            return 0;
+        }
+    }
+}
