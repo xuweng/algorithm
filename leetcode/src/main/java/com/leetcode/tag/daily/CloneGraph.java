a4299d6b26d323c2826bbb5c8c28180c08f3040b
xuweng <2028403305@qq.com>
2020-08-12 23:22:45 +0800

    克隆邻接列表后重新加入当前结点
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/CloneGraph.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/CloneGraph.java
@@ -1,6 +1,7 @@
 package com.leetcode.tag.daily;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -50,4 +51,38 @@ public class CloneGraph {
             neighbors = _neighbors;
         }
     }
+
+    /**
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/clone-graph/solution/ke-long-tu-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        private HashMap<Node, Node> visited = new HashMap<>();
+
+        public Node cloneGraph(Node node) {
+            if (node == null) {
+                return node;
+            }
+
+            // 如果该节点已经被访问过了，则直接从哈希表中取出对应的克隆节点返回
+            if (visited.containsKey(node)) {
+                return visited.get(node);
+            }
+
+            // 克隆节点，注意到为了深拷贝我们不会克隆它的邻居的列表
+            Node cloneNode = new Node(node.val, new ArrayList<>());
+            // 哈希表存储
+            visited.put(node, cloneNode);
+
+            // 遍历该节点的邻居并更新克隆节点的邻居列表
+            for (Node neighbor : node.neighbors) {
+                //克隆邻接列表后重新加入当前结点
+                cloneNode.neighbors.add(cloneGraph(neighbor));
+            }
+            return cloneNode;
+        }
+    }
+
 }
