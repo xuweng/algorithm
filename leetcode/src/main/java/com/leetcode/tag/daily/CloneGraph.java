261fc68817f4e72e88716d2e6a4ad6a24a5b68df
xuweng <2028403305@qq.com>
2020-08-12 23:27:45 +0800

    先搞定一个分支，再搞定另外一个分支
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/CloneGraph.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/CloneGraph.java
@@ -2,6 +2,7 @@ package com.leetcode.tag.daily;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -53,6 +54,8 @@ public class CloneGraph {
     }
 
     /**
+     * 方法一：深度优先搜索
+     * <p>
      * 作者：LeetCode-Solution
      * 链接：https://leetcode-cn.com/problems/clone-graph/solution/ke-long-tu-by-leetcode-solution/
      * 来源：力扣（LeetCode）
@@ -84,5 +87,57 @@ public class CloneGraph {
             return cloneNode;
         }
     }
+
+    /**
+     * 方法二：广度优先遍历
+     * <p>
+     * 方法一与方法二的区别仅在于搜索的方式。
+     * <p>
+     * 深度优先搜索以深度优先，广度优先搜索以广度优先。
+     * <p>
+     * 先搞定一个分支，再搞定另外一个分支。
+     * <p>
+     * 这两种方法都需要借助哈希表记录被克隆过的节点来避免陷入死循环。
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/clone-graph/solution/ke-long-tu-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public Node cloneGraph(Node node) {
+            if (node == null) {
+                return node;
+            }
+
+            HashMap<Node, Node> visited = new HashMap();
+
+            // 将题目给定的节点添加到队列
+            LinkedList<Node> queue = new LinkedList<>();
+            queue.add(node);
+            // 克隆第一个节点并存储到哈希表中
+            visited.put(node, new Node(node.val, new ArrayList()));
+
+            // 广度优先搜索
+            while (!queue.isEmpty()) {
+                // 取出队列的头节点
+                Node n = queue.remove();
+                // 遍历该节点的邻居
+                for (Node neighbor : n.neighbors) {
+                    if (!visited.containsKey(neighbor)) {
+                        // 如果没有被访问过，就克隆并存储在哈希表中
+                        visited.put(neighbor, new Node(neighbor.val, new ArrayList()));
+                        // 将邻居节点加入队列中
+                        queue.add(neighbor);
+                    }
+                    // 更新当前节点的邻居列表
+                    visited.get(n).neighbors.add(visited.get(neighbor));
+                }
+            }
+
+            return visited.get(node);
+        }
+    }
+
 
 }
