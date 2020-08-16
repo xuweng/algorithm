00c342448e0d71f9f3e8dd96533a61d56588cd80
xuweng <2028403305@qq.com>
2020-08-16 10:57:22 +0800

    5490. 吃掉 N 个橘子的最少天数
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/MinDays.java
@@ -0,0 +1,12 @@
+package com.leetcode.tag.contest;
+
+/**
+ * 5490. 吃掉 N 个橘子的最少天数
+ */
+public class MinDays {
+    class Solution {
+        public int minDays(int n) {
+            return 0;
+        }
+    }
+}
