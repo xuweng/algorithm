526b0294fc3134ae740f7771a0a37f9e6e10a96b
xuweng <2028403305@qq.com>
2020-08-16 10:52:37 +0800

    5489. 两球之间的磁力
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/MaxDistance.java
@@ -0,0 +1,15 @@
+package com.leetcode.tag.contest;
+
+/**
+ * 5489. 两球之间的磁力
+ */
+public class MaxDistance {
+    /**
+     * 根据示例找规律
+     */
+    class Solution {
+        public int maxDistance(int[] position, int m) {
+            return 0;
+        }
+    }
+}
