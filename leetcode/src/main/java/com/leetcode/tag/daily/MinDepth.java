3f65f8f101fe3d2ec7947767b752a1f0dd8e2411
xuweng <2028403305@qq.com>
2020-08-21 23:16:47 +0800

    111. 二叉树的最小深度
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/MinDepth.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/MinDepth.java
@@ -27,6 +27,35 @@ public class MinDepth {
         }
     }
 
+    /**
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/solution/er-cha-shu-de-zui-xiao-shen-du-by-leetcode-solutio/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int minDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            if (root.left == null && root.right == null) {
+                return 1;
+            }
+
+            int minDepth = Integer.MAX_VALUE;
+            if (root.left != null) {
+                minDepth = Math.min(minDepth(root.left), minDepth);
+            }
+            if (root.right != null) {
+                minDepth = Math.min(minDepth(root.right), minDepth);
+            }
+
+            return minDepth + 1;
+        }
+    }
+
+
     public class TreeNode {
         int val;
         TreeNode left;
