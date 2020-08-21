a1b69f99521e89f65006bc6f6c2e477298252c6b
xuweng <2028403305@qq.com>
2020-08-21 23:22:38 +0800

    方法二：广度优先搜索
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/MinDepth.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/MinDepth.java
@@ -1,5 +1,8 @@
 package com.leetcode.tag.daily;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 111. 二叉树的最小深度
  */
@@ -28,6 +31,8 @@ public class MinDepth {
     }
 
     /**
+     * 方法一：深度优先搜索
+     * <p>
      * 作者：LeetCode-Solution
      * 链接：https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/solution/er-cha-shu-de-zui-xiao-shen-du-by-leetcode-solutio/
      * 来源：力扣（LeetCode）
@@ -55,6 +60,46 @@ public class MinDepth {
         }
     }
 
+    /**
+     * 方法二：广度优先搜索
+     */
+    class Solution2 {
+        class QueueNode {
+            TreeNode node;
+            int depth;
+
+            public QueueNode(TreeNode node, int depth) {
+                this.node = node;
+                this.depth = depth;
+            }
+        }
+
+        public int minDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            Queue<QueueNode> queue = new LinkedList<>();
+            queue.offer(new QueueNode(root, 1));
+            while (!queue.isEmpty()) {
+                QueueNode nodeDepth = queue.poll();
+                TreeNode node = nodeDepth.node;
+                int depth = nodeDepth.depth;
+                //当我们找到一个叶子节点时，直接返回这个叶子节点的深度。广度优先搜索的性质保证了最先搜索到的叶子节点的深度一定最小。
+                if (node.left == null && node.right == null) {
+                    return depth;
+                }
+                if (node.left != null) {
+                    queue.offer(new QueueNode(node.left, depth + 1));
+                }
+                if (node.right != null) {
+                    queue.offer(new QueueNode(node.right, depth + 1));
+                }
+            }
+
+            return 0;
+        }
+    }
 
     public class TreeNode {
         int val;
