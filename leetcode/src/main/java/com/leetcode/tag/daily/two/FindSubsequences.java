86bcd64bbc6996e1129b78c7320b171f693edbc4
xuweng <2028403305@qq.com>
2020-08-25 11:51:23 +0800

    491. 递增子序列
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/FindSubsequences.java
@@ -0,0 +1,21 @@
+package com.leetcode.tag.daily.two;
+
+import java.util.List;
+
+/**
+ * 491. 递增子序列
+ * <p>
+ * 给定数组的长度不会超过15。
+ * 数组中的整数范围是 [-100,100]。
+ * 给定数组中可能包含重复数字，相等的数字应该被视为递增的一种情况。
+ */
+public class FindSubsequences {
+    /**
+     * 假设n-1解决,推导n?这样推到太麻烦。
+     */
+    class Solution {
+        public List<List<Integer>> findSubsequences(int[] nums) {
+            return null;
+        }
+    }
+}
