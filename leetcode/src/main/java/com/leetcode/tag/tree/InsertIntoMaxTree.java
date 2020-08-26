b5d7ba93eb114836307dce92867d19142ee0c29e
xuweng <2028403305@qq.com>
2020-08-26 16:22:45 +0800

    998. 最大二叉树 II
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/InsertIntoMaxTree.java
@@ -0,0 +1,35 @@
+package com.leetcode.tag.tree;
+
+/**
+ * 998. 最大二叉树 II
+ * <p>
+ * 不太搞懂题意
+ * <p>
+ * 搞懂题意。搞懂题意。搞懂题意。搞懂题意。搞懂题意
+ */
+public class InsertIntoMaxTree {
+    class Solution {
+        public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+            return null;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
