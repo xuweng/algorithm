890274071f2e54c502bf90dde89e42f64557bcc3
xuweng <2028403305@qq.com>
2020-08-26 15:43:56 +0800

    迭代
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/Preorder.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/Preorder.java
@@ -1,6 +1,8 @@
 package com.leetcode.tag.tree;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -31,6 +33,40 @@ public class Preorder {
 
         }
     }
+
+    /**
+     * 方法一：迭代
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/solution/ncha-shu-de-qian-xu-bian-li-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        /**
+         * 保证栈顶的节点就是我们当前遍历到的节点
+         *
+         * @param root
+         * @return
+         */
+        public List<Integer> preorder(Node root) {
+            LinkedList<Node> stack = new LinkedList<>();
+            LinkedList<Integer> output = new LinkedList<>();
+            if (root == null) {
+                return output;
+            }
+
+            stack.add(root);
+            while (!stack.isEmpty()) {
+                Node node = stack.pollLast();
+                output.add(node.val);
+                Collections.reverse(node.children);
+                stack.addAll(node.children);
+            }
+            return output;
+        }
+    }
+
 
     class Node {
         public int val;
