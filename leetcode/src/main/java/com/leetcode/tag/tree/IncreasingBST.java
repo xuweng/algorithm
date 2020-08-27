d72a3b8fa744e2376d244a4e8aed21c837a70eca
xuweng <2028403305@qq.com>
2020-08-27 12:13:21 +0800

    方法二：中序遍历 + 更改树的连接方式
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/IncreasingBST.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/IncreasingBST.java
@@ -35,6 +35,80 @@ public class IncreasingBST {
         }
     }
 
+    /**
+     * 复杂度分析
+     * <p>
+     * 时间复杂度：O(N)，其中 N 是树上的节点个数。
+     * <p>
+     * 空间复杂度：O(N)。
+     * <p>
+     * 方法一：中序遍历 + 构造新的树
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/increasing-order-search-tree/solution/di-zeng-shun-xu-cha-zhao-shu-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public TreeNode increasingBST(TreeNode root) {
+            List<Integer> vals = new ArrayList<>();
+            inorder(root, vals);
+
+            TreeNode ans = new TreeNode(0), cur = ans;
+            for (int v : vals) {
+                cur.right = new TreeNode(v);
+                cur = cur.right;
+            }
+            return ans.right;
+        }
+
+        public void inorder(TreeNode node, List<Integer> vals) {
+            if (node == null) {
+                return;
+            }
+            inorder(node.left, vals);
+            vals.add(node.val);
+            inorder(node.right, vals);
+        }
+    }
+
+    /**
+     * 方法二：中序遍历 + 更改树的连接方式
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/increasing-order-search-tree/solution/di-zeng-shun-xu-cha-zhao-shu-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        TreeNode cur;
+
+        public TreeNode increasingBST(TreeNode root) {
+            TreeNode ans = new TreeNode(0);
+            cur = ans;
+            inorder(root);
+            return ans.right;
+        }
+
+        /**
+         * 树上进行中序遍历，但会将树中的节点之间重新连接而不使用额外的空间。具体地，
+         * <p>
+         * 当我们遍历到一个节点时，把它的左孩子设为空，并将其本身作为上一个遍历到的节点的右孩子。
+         *
+         * @param node
+         */
+        public void inorder(TreeNode node) {
+            if (node == null) {
+                return;
+            }
+            inorder(node.left);
+            node.left = null;
+            cur.right = node;
+            cur = node;
+            inorder(node.right);
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
