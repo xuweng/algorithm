a0c339fc2919ed32df3ed7517e4b30605203c088
xuweng <2028403305@qq.com>
2020-08-27 12:54:34 +0800

    求 “二叉搜索树第 k 大的节点” 可转化为求 “此树的中序遍历倒序的第 k 个节点”。
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/KthLargest.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/KthLargest.java
@@ -34,6 +34,38 @@ public class KthLargest {
 
     }
 
+    /**
+     * 求 “二叉搜索树第 k 大的节点” 可转化为求 “此树的中序遍历倒序的第 k 个节点”。
+     * <p>
+     * 作者：jyd
+     * 链接：https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof/solution/mian-shi-ti-54-er-cha-sou-suo-shu-de-di-k-da-jie-d/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        int res, k;
+
+        public int kthLargest(TreeNode root, int k) {
+            this.k = k;
+            dfs(root);
+            return res;
+        }
+
+        void dfs(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+            dfs(root.right);
+            if (k == 0) {
+                return;
+            }
+            if (--k == 0) {
+                res = root.val;
+            }
+            dfs(root.left);
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
