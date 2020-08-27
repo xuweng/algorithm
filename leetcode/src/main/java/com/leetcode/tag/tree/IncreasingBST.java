15daead84049d91398575402fd1404501dba8d5e
xuweng <2028403305@qq.com>
2020-08-27 12:33:39 +0800

    移动cur，相当一个指针
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/IncreasingBST.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/IncreasingBST.java
@@ -81,6 +81,7 @@ public class IncreasingBST {
      * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
      */
     class Solution2 {
+        //移动cur，相当一个指针
         TreeNode cur;
 
         public TreeNode increasingBST(TreeNode root) {
