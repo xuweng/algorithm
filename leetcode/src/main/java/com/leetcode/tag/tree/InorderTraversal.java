d5498b0034436e0f2fa76732979bc9072478ef46
xuweng <2028403305@qq.com>
2020-08-27 11:47:52 +0800

    左子树先全部入栈
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/InorderTraversal.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/InorderTraversal.java
@@ -2,6 +2,7 @@ package com.leetcode.tag.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 94. 二叉树的中序遍历
@@ -25,6 +26,32 @@ public class InorderTraversal {
             inorderTraversal(root.right, result);
         }
     }
+
+    /**
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal/solution/er-cha-shu-de-zhong-xu-bian-li-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> res = new ArrayList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode curr = root;
+            while (curr != null || !stack.isEmpty()) {
+                //左子树先全部入栈
+                while (curr != null) {
+                    stack.push(curr);
+                    curr = curr.left;
+                }
+                curr = stack.pop();
+                res.add(curr.val);
+                curr = curr.right;
+            }
+            return res;
+        }
+    }
+
 
     public class TreeNode {
         int val;
