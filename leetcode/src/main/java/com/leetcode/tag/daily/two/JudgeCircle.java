fd12fbfc26bb10711cbbf8a3a1c27aec77bf1a09
xuweng <2028403305@qq.com>
2020-08-28 23:22:13 +0800

    模拟题目
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/two/JudgeCircle.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/JudgeCircle.java
@@ -46,9 +46,9 @@ public class JudgeCircle {
             for (int i = 0; i < length; i++) {
                 char move = moves.charAt(i);
                 if (move == 'U') {
-                    y--;
-                } else if (move == 'D') {
                     y++;
+                } else if (move == 'D') {
+                    y--;
                 } else if (move == 'L') {
                     x--;
                 } else if (move == 'R') {
