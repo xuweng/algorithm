b93d231f2db9043db80d3d69b75441dad970a667
xuweng <2028403305@qq.com>
2020-08-30 12:31:12 +0800

    错误的状态转移方程
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/MaxProduct.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/MaxProduct.java
@@ -4,6 +4,19 @@ package com.leetcode.tag.dp;
  * 152. 乘积最大子数组
  */
 public class MaxProduct {
+    /**
+     * 错误的状态转移方程：
+     * <p>
+     * fmax(i) = max{ f(i - 1) * a_i, a_i}
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/maximum-product-subarray/solution/cheng-ji-zui-da-zi-shu-zu-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @return
+     */
     public int maxProduct(int[] nums) {
         int[] maxF = new int[nums.length];
         int[] minF = new int[nums.length];
