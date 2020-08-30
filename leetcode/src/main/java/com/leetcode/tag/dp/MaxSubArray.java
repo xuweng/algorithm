39465874bee886a8de171d6261443cacbb70f810
xuweng <2028403305@qq.com>
2020-08-30 11:40:54 +0800

    53. 最大子序和
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dp/MaxSubArray.java
@@ -0,0 +1,26 @@
+package com.leetcode.tag.dp;
+
+/**
+ * 53. 最大子序和
+ */
+public class MaxSubArray {
+    /**
+     * 用 f(i) 代表以第 i 个数结尾的「连续子数组的最大和」
+     * <p>
+     * 动态规划转移方程：
+     * <p>
+     * f(i) = max{f(i - 1)+ a_i,a_i}
+     */
+    class Solution {
+        public int maxSubArray(int[] nums) {
+            int pre = 0, maxAns = nums[0];
+            for (int x : nums) {
+                pre = Math.max(pre + x, x);
+                maxAns = Math.max(maxAns, pre);
+            }
+            return maxAns;
+        }
+    }
+
+    ;
+}
