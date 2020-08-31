289d9d3e41816f45df57e7c163b67d29512f89c3
xuweng <2028403305@qq.com>
2020-08-31 13:29:13 +0800

    方法一：后序遍历 + 剪枝 （从底至顶）
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/IsBalanced.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/IsBalanced.java
@@ -33,6 +33,37 @@ public class IsBalanced {
         }
     }
 
+    /**
+     * 方法一：后序遍历 + 剪枝 （从底至顶）
+     * <p>
+     * 作者：jyd
+     * 链接：https://leetcode-cn.com/problems/ping-heng-er-cha-shu-lcof/solution/mian-shi-ti-55-ii-ping-heng-er-cha-shu-cong-di-zhi/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public boolean isBalanced(TreeNode root) {
+            return recur(root) != -1;
+        }
+
+        private int recur(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            int left = recur(root.left);
+            if (left == -1) {
+                return -1;
+            }
+            int right = recur(root.right);
+            if (right == -1) {
+                return -1;
+            }
+            //类似二分。返回-1.
+            return Math.abs(left - right) <= 1 ? Math.max(left, right) + 1 : -1;
+        }
+    }
+
+
     class TreeNode {
         int val;
         TreeNode left;
