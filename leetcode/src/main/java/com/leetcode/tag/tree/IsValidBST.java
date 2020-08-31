0552db0524251503fec7d60f9b536deea3bff4d0
xuweng <2028403305@qq.com>
2020-08-31 17:29:36 +0800

    这个递归厉害
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/IsValidBST.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/IsValidBST.java
@@ -22,6 +22,41 @@ public class IsValidBST {
         }
     }
 
+    /**
+     * 作者：xiao-lu-27
+     * 链接：https://leetcode-cn.com/problems/legal-binary-search-tree-lcci/solution/dai-qu-jian-de-di-gui-jian-dan-yi-dong-shuang-bai-/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public boolean isValidBST(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+            return isValidBSTHelp(root.left, Long.MIN_VALUE, root.val) && isValidBSTHelp(root.right, root.val, Long.MAX_VALUE);
+        }
+
+        /**
+         * 这个递归厉害。
+         *
+         * @param root
+         * @param min
+         * @param max
+         * @return
+         */
+        private boolean isValidBSTHelp(TreeNode root, long min, long max) {
+            if (root == null) {
+                return true;
+            }
+            if (root.val <= min || root.val >= max) {
+                return false;
+            }
+            boolean left = isValidBSTHelp(root.left, min, root.val);
+            boolean right = isValidBSTHelp(root.right, root.val, max);
+            return left && right;
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
