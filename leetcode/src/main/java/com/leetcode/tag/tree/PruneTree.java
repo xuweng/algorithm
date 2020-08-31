7bb356d1dbd1890288378c8a17bf3db616c68416
xuweng <2028403305@qq.com>
2020-08-31 16:41:32 +0800

    后序遍历
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/PruneTree.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/PruneTree.java
@@ -1,5 +1,8 @@
 package com.leetcode.tag.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 后序遍历。后序遍历。后序遍历。后序遍历
  * <p>
@@ -16,20 +19,29 @@ public class PruneTree {
      * 算法错误。包含0的都干掉了。
      */
     class Solution {
+        List<TreeNode> list = new ArrayList<>();
+
         public TreeNode pruneTree(TreeNode root) {
             if (root == null) {
                 return null;
+            }
+            if (root.val == 1) {
+                list.add(root);
             }
 
             TreeNode left = pruneTree(root.left);
             TreeNode right = pruneTree(root.right);
             //天然获取父节点root
-            if (left != null && left.val == 0) {
+            if (left != null && left.val == 0 && !list.contains(left)) {
                 root.left = null;
             }
-            if (right != null && right.val == 0) {
+            if (right != null && right.val == 0 && !list.contains(right)) {
                 root.right = null;
             }
+            if (list.contains(left) || list.contains(right)) {
+                list.add(root);
+            }
+
             return root;
         }
     }
