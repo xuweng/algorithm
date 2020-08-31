8893aef7508ce2a26725b060a4ed1e3b9512d38f
xuweng <2028403305@qq.com>
2020-08-31 13:17:10 +0800

    剑指 Offer 55 - II. 平衡二叉树
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/IsBalanced.java
@@ -0,0 +1,28 @@
+package com.leetcode.tag.tree;
+
+/**
+ * 剑指 Offer 55 - II. 平衡二叉树
+ * <p>
+ * 不用做晦涩题目。直接看题解。
+ */
+public class IsBalanced {
+    class Solution {
+        public boolean isBalanced(TreeNode root) {
+            return root == null || Math.abs(height(root.left) - height(root.right)) <= 1;
+        }
+
+        private int height(TreeNode root) {
+            return root == null ? 0 : Math.max(height(root.left), height(root.right)) + 1;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
