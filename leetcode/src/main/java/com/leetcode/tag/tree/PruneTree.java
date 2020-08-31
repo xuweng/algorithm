f5a6b2f204d6e9b621b39cbd183c54e01c37b09f
xuweng <2028403305@qq.com>
2020-08-31 16:50:59 +0800

    后序遍历的返回太重要了
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/PruneTree.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/PruneTree.java
@@ -59,6 +59,35 @@ public class PruneTree {
         }
     }
 
+    /**
+     * 后序遍历的返回太重要了
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/binary-tree-pruning/solution/er-cha-shu-jian-zhi-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public TreeNode pruneTree(TreeNode root) {
+            return containsOne(root) ? root : null;
+        }
+
+        public boolean containsOne(TreeNode node) {
+            if (node == null) {
+                return false;
+            }
+            boolean a1 = containsOne(node.left);
+            boolean a2 = containsOne(node.right);
+            if (!a1) {
+                node.left = null;
+            }
+            if (!a2) {
+                node.right = null;
+            }
+            return node.val == 1 || a1 || a2;
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
