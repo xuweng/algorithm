ec10eeaa8df8a83d3747a21018287a3d0b968223
xuweng <2028403305@qq.com>
2020-08-31 15:58:01 +0800

    使用一个 HashMap 来避免重复计算
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/IsBalanced.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/IsBalanced.java
@@ -1,5 +1,8 @@
 package com.leetcode.tag.tree;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 剑指 Offer 55 - II. 平衡二叉树
  * <p>
@@ -104,6 +107,34 @@ public class IsBalanced {
 
         private int depth(TreeNode root) {
             return root == null ? 0 : Math.max(depth(root.left), depth(root.right)) + 1;
+        }
+    }
+
+    class S {
+        // 存在大量的重复计算 depth
+        // 额外使用一个 HashMap 来避免重复计算
+        Map<TreeNode, Integer> map = new HashMap<>();
+
+        public boolean isBalanced(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+            // 对每棵子树递归检查是否平衡
+            return Math.abs(depth(root.left) - depth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+        }
+
+        // 获取树的最大深度
+        public int depth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            if (map.containsKey(root)) {
+                return map.get(root);
+            }
+            int curDepth = Math.max(depth(root.left), depth(root.right)) + 1;
+            map.put(root, curDepth);
+            return curDepth;
+
         }
     }
 
