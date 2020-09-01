757c66d4deb6d6fcf0e7aa03b39d3484a8dc67f8
xuweng <2028403305@qq.com>
2020-09-01 11:35:18 +0800

    方法一：递归
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/two/PredictTheWinner.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/PredictTheWinner.java
@@ -13,4 +13,28 @@ public class PredictTheWinner {
             return true;
         }
     }
+
+    /**
+     * 方法一：递归
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/predict-the-winner/solution/yu-ce-ying-jia-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public boolean PredictTheWinner(int[] nums) {
+            return total(nums, 0, nums.length - 1, 1) >= 0;
+        }
+
+        public int total(int[] nums, int start, int end, int turn) {
+            if (start == end) {
+                return nums[start] * turn;
+            }
+            int scoreStart = nums[start] * turn + total(nums, start + 1, end, -turn);
+            int scoreEnd = nums[end] * turn + total(nums, start, end - 1, -turn);
+            return Math.max(scoreStart * turn, scoreEnd * turn) * turn;
+        }
+    }
+
 }
