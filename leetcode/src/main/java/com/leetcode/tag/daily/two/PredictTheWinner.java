d2e04ea1b12f70fce2e7f47a86fc065a678da3f2
xuweng <2028403305@qq.com>
2020-09-01 11:42:47 +0800

    方法二：动态规划
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/two/PredictTheWinner.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/PredictTheWinner.java
@@ -37,4 +37,29 @@ public class PredictTheWinner {
         }
     }
 
+    /**
+     * 方法二：动态规划
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/predict-the-winner/solution/yu-ce-ying-jia-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public boolean PredictTheWinner(int[] nums) {
+            int length = nums.length;
+            int[][] dp = new int[length][length];
+            for (int i = 0; i < length; i++) {
+                dp[i][i] = nums[i];
+            }
+            for (int i = length - 2; i >= 0; i--) {
+                for (int j = i + 1; j < length; j++) {
+                    dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+                }
+            }
+            return dp[0][length - 1] >= 0;
+        }
+    }
+
+
 }
