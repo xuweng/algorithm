0ce92acc56b7bf2ed1ad75136d2ee8ce079f10e4
xuweng <2028403305@qq.com>
2020-09-01 11:13:09 +0800

    486. 预测赢家
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/PredictTheWinner.java
@@ -0,0 +1,16 @@
+package com.leetcode.tag.daily.two;
+
+/**
+ * 486. 预测赢家
+ * <p>
+ * 不做太晦涩的题。
+ * <p>
+ * 直接看题解直接看代码。
+ */
+public class PredictTheWinner {
+    class Solution {
+        public boolean PredictTheWinner(int[] nums) {
+            return true;
+        }
+    }
+}
