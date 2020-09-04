d9f80eaf7c697dcc0bf44d48c2f378f5583f35f7
xuweng <2028403305@qq.com>
2020-09-04 12:17:48 +0800

    1022. 从根到叶的二进制数之和
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/SumRootToLeaf.java
@@ -0,0 +1,26 @@
+package com.leetcode.tag.tree;
+
+/**
+ * 1022. 从根到叶的二进制数之和
+ * <p>
+ * 树就是各种遍历。递归树。决策树。
+ * <p>
+ * 树就是各种遍历。递归树。决策树。
+ */
+public class SumRootToLeaf {
+    class Solution {
+        public int sumRootToLeaf(TreeNode root) {
+            return 0;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
