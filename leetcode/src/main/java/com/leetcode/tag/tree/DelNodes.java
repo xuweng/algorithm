002bfbd1e45592fdd71b948e156fb5eb04b2dce8
xuweng <2028403305@qq.com>
2020-09-05 00:20:46 +0800

    带返回的后序遍历
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/DelNodes.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/DelNodes.java
@@ -1,9 +1,6 @@
 package com.leetcode.tag.tree;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -51,6 +48,7 @@ public class DelNodes {
             }
             del = toDelete.contains(root.val);
             //如果子节点被删除，父节点的left、right字段需要更新
+            //将parentExists标志传递给子节点
             if (helper(root.left, ans, !del)) {
                 root.left = null;
             }
@@ -61,6 +59,63 @@ public class DelNodes {
                 ans.add(root);
             }
             return del;
+        }
+    }
+
+    /**
+     * 作者：walker-3
+     * 链接：https://leetcode-cn.com/problems/delete-nodes-and-return-forest/solution/javashi-yong-hou-xu-bian-li-de-fang-shi-by-walker-/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+            // 为了快速判断节点是否要被删除，使用HashSet加快查找
+            Set<Integer> deleteSet = new HashSet<>();
+            for (int x : to_delete) {
+                deleteSet.add(x);
+            }
+
+            // 给根节点伪造一个父节点
+            TreeNode fakeRoot = new TreeNode(0);
+            fakeRoot.left = root;
+
+            List<TreeNode> ans = new ArrayList<>();
+            foo(ans, fakeRoot, root, deleteSet);
+            // 如果根节点还在，别忘记加入根节点
+            if (fakeRoot.left != null) {
+                ans.add(root);
+            }
+
+            return ans;
+        }
+
+        // 使用后续遍历方式删除节点，这样不会影响子树的删除
+        private void foo(List<TreeNode> ans, TreeNode parent, TreeNode root, Set<Integer> deleteSet) {
+            if (root == null) {
+                return;
+            }
+            if (root.left != null) {
+                foo(ans, root, root.left, deleteSet);
+            }
+            if (root.right != null) {
+                foo(ans, root, root.right, deleteSet);
+            }
+            // 如果根节点要删除
+            if (deleteSet.contains(root.val)) {
+                if (root.left != null) {
+                    ans.add(root.left);
+                }
+                if (root.right != null) {
+                    ans.add(root.right);
+                }
+                // 断掉父节点与本节点的连接
+                if (parent.left == root) {
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                }
+            }
         }
     }
 
