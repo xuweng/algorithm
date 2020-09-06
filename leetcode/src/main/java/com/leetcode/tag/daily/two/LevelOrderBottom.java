7bfb198deb916583f058aa7d7b099bd23832cad3
xuweng <2028403305@qq.com>
2020-09-06 23:21:01 +0800

    方法一：广度优先搜索
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/two/LevelOrderBottom.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/LevelOrderBottom.java
@@ -1,8 +1,6 @@
 package com.leetcode.tag.daily.two;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * 107. 二叉树的层次遍历 II
@@ -30,6 +28,44 @@ public class LevelOrderBottom {
             hou(root.right, level + 1);
 
             result.get(level).add(root.val);
+        }
+    }
+
+    /**
+     * 方法一：广度优先搜索
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii/solution/er-cha-shu-de-ceng-ci-bian-li-ii-by-leetcode-solut/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public List<List<Integer>> levelOrderBottom(TreeNode root) {
+            List<List<Integer>> levelOrder = new LinkedList<>();
+            if (root == null) {
+                return levelOrder;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                List<Integer> level = new ArrayList<>();
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    level.add(node.val);
+                    TreeNode left = node.left, right = node.right;
+                    //入队
+                    if (left != null) {
+                        queue.offer(left);
+                    }
+                    //入队
+                    if (right != null) {
+                        queue.offer(right);
+                    }
+                }
+                levelOrder.add(0, level);
+            }
+            return levelOrder;
         }
     }
 
