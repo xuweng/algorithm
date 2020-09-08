c07cddec25dfb279994c8cc156f0c8005747bf38
xuweng <2028403305@qq.com>
2020-09-08 22:17:33 +0800

    优化：分析搜索起点的上界进行剪枝
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/two/Combine.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/Combine.java
@@ -141,4 +141,37 @@ public class Combine {
         }
     }
 
+    /**
+     * 作者：liweiwei1419
+     * 链接：https://leetcode-cn.com/problems/combinations/solution/hui-su-suan-fa-jian-zhi-python-dai-ma-java-dai-ma-/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution4 {
+        public List<List<Integer>> combine(int n, int k) {
+            List<List<Integer>> res = new ArrayList<>();
+            if (k <= 0 || n < k) {
+                return res;
+            }
+            Deque<Integer> path = new ArrayDeque<>();
+            dfs(n, k, 1, path, res);
+            return res;
+        }
+
+        private void dfs(int n, int k, int index, Deque<Integer> path, List<List<Integer>> res) {
+            if (path.size() == k) {
+                res.add(new ArrayList<>(path));
+                return;
+            }
+
+            //剪枝过程就是：把 i <= n 改成 i <= n - (k - path.size()) + 1
+            // 只有这里 i <= n - (k - path.size()) + 1 与参考代码 1 不同
+            for (int i = index; i <= n - (k - path.size()) + 1; i++) {
+                path.addLast(i);
+                dfs(n, k, i + 1, path, res);
+                path.removeLast();
+            }
+        }
+    }
+
 }
