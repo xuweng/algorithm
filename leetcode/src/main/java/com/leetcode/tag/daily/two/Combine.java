06a6cfe14e5bcef2445e2b2ab61c40899ea5ab97
xuweng <2028403305@qq.com>
2020-09-08 16:32:28 +0800

    77. 组合
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/Combine.java
@@ -0,0 +1,35 @@
+package com.leetcode.tag.daily.two;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 77. 组合
+ */
+public class Combine {
+    class Solution {
+        public List<List<Integer>> combine(int n, int k) {
+            Deque<Integer> stack = new LinkedList<>();
+            List<List<Integer>> result = new ArrayList<>();
+
+            back(n, 1, k, stack, result);
+
+            return result;
+        }
+
+        private void back(int n, int start, int k, Deque<Integer> stack, List<List<Integer>> result) {
+            if (stack.size() == k) {
+                result.add(new ArrayList<>(stack));
+                return;
+            }
+            for (int i = start; i < n; i++) {
+                stack.push(i);
+                for (int j = start + 1; j <= n; j++) {
+                    back(n, j, k, stack, result);
+                }
+            }
+        }
+    }
+}
