24c6442ae7abadce398703fe0bc4a305e911a781
xuweng <2028403305@qq.com>
2020-09-08 22:22:39 +0800

    方法二：按照每一个数选与不选画出二叉树
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/two/Combine.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/Combine.java
@@ -174,4 +174,46 @@ public class Combine {
         }
     }
 
+    /**
+     * 方法二：按照每一个数选与不选画出二叉树
+     * <p>
+     * 作者：liweiwei1419
+     * 链接：https://leetcode-cn.com/problems/combinations/solution/hui-su-suan-fa-jian-zhi-python-dai-ma-java-dai-ma-/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution5 {
+        public List<List<Integer>> combine(int n, int k) {
+            List<List<Integer>> res = new ArrayList<>();
+            if (k <= 0 || n < k) {
+                return res;
+            }
+
+            // 为了防止底层动态数组扩容，初始化的时候传入最大长度
+            Deque<Integer> path = new ArrayDeque<>(k);
+            dfs(1, n, k, path, res);
+            return res;
+        }
+
+        private void dfs(int begin, int n, int k, Deque<Integer> path, List<List<Integer>> res) {
+            if (k == 0) {
+                res.add(new ArrayList<>(path));
+                return;
+            }
+
+            // 基础版本的递归终止条件：if (begin == n + 1) {
+            if (begin > n - k + 1) {
+                return;
+            }
+            // 不选当前考虑的数 begin，直接递归到下一层
+            dfs(begin + 1, n, k, path, res);
+
+            // 不选当前考虑的数 begin，递归到下一层的时候 k - 1，这里 k 表示还需要选多少个数
+            path.addLast(begin);
+            dfs(begin + 1, n, k - 1, path, res);
+            // 深度优先遍历有回头的过程，因此需要撤销选择
+            path.removeLast();
+        }
+    }
+
 }
