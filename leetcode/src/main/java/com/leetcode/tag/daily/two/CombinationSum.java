0c9b2893992410f56a996d264a407698f44c3a09
xuweng <2028403305@qq.com>
2020-09-09 22:38:59 +0800

    39. 组合总和
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/CombinationSum.java
@@ -0,0 +1,38 @@
+package com.leetcode.tag.daily.two;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 39. 组合总和
+ */
+public class CombinationSum {
+    class Solution {
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+            List<List<Integer>> result = new ArrayList<>();
+            Deque<Integer> stack = new LinkedList<>();
+
+            back(candidates, target, 0, result, stack);
+
+            return result;
+        }
+
+
+        private void back(int[] candidates, int target, int sum, List<List<Integer>> result, Deque<Integer> stack) {
+            if (sum == target) {
+                result.add(new ArrayList<>(stack));
+                return;
+            }
+            if (sum > target) {
+                return;
+            }
+            for (int candidate : candidates) {
+                stack.push(candidate);
+                back(candidates, target, sum + candidate, result, stack);
+                stack.pop();
+            }
+        }
+    }
+}
