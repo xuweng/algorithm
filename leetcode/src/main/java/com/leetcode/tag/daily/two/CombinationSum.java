7e2d312a9e02460887f5149ffa5f7822a03bf651
xuweng <2028403305@qq.com>
2020-09-09 23:20:17 +0800

    什么时候使用 used 数组，什么时候使用 begin 变量
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/two/CombinationSum.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/CombinationSum.java
@@ -10,6 +10,19 @@ import java.util.*;
  * 递归树
  * <p>
  * 候选集动态改变
+ * <p>
+ * 什么时候使用 used 数组，什么时候使用 begin 变量
+ * <p>
+ * 排列问题，讲究顺序（即 [2, 2, 3] 与 [2, 3, 2] 视为不同列表时），需要记录哪些数字已经使用过，此时用 used 数组；
+ * <p>
+ * 组合问题，不讲究顺序（即 [2, 2, 3] 与 [2, 3, 2] 视为相同列表时），需要按照某种顺序搜索，此时使用 begin 变量。
+ * <p>
+ * 注意：具体问题应该具体分析， 理解算法的设计思想 是至关重要的，请不要死记硬背。
+ * <p>
+ * 作者：liweiwei1419
+ * 链接：https://leetcode-cn.com/problems/combination-sum/solution/hui-su-suan-fa-jian-zhi-python-dai-ma-java-dai-m-2/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
  */
 public class CombinationSum {
     class Solution {
