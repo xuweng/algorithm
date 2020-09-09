2a073bbdb21f61ca0a41a513879a68ae66c8222c
xuweng <2028403305@qq.com>
2020-09-09 23:15:41 +0800

    剪枝
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/two/CombinationSum.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/CombinationSum.java
@@ -156,4 +156,47 @@ public class CombinationSum {
         }
     }
 
+    /**
+     * 作者：liweiwei1419
+     * 链接：https://leetcode-cn.com/problems/combination-sum/solution/hui-su-suan-fa-jian-zhi-python-dai-ma-java-dai-m-2/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution3 {
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+            int len = candidates.length;
+            List<List<Integer>> res = new ArrayList<>();
+            if (len == 0) {
+                return res;
+            }
+
+            // 排序是剪枝的前提
+            Arrays.sort(candidates);
+
+            Deque<Integer> path = new ArrayDeque<>();
+            dfs(candidates, 0, len, target, path, res);
+            return res;
+        }
+
+        private void dfs(int[] candidates, int begin, int len, int target, Deque<Integer> path, List<List<Integer>> res) {
+            // 由于进入更深层的时候，小于 0 的部分被剪枝，因此递归终止条件值只判断等于 0 的情况
+            if (target == 0) {
+                res.add(new ArrayList<>(path));
+                return;
+            }
+
+            for (int i = begin; i < len; i++) {
+                // 重点理解这里剪枝，前提是候选数组已经有序，
+                // 当前元素比target大，后面的元素也比target大。
+                if (target - candidates[i] < 0) {
+                    break;
+                }
+
+                path.addLast(candidates[i]);
+                dfs(candidates, i, len, target - candidates[i], path, res);
+                path.removeLast();
+            }
+        }
+    }
+
 }
