a5653162038977b5fd0ed1c3f6cd42dc944cf630
xuweng <2028403305@qq.com>
2020-09-10 11:56:22 +0800

    40. 组合总和 II
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/CombinationSum2.java
@@ -0,0 +1,40 @@
+package com.leetcode.tag.daily.two;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 40. 组合总和 II
+ */
+public class CombinationSum2 {
+    /**
+     * candidates 中的每个数字在每个组合中只能使用一次
+     */
+    class Solution {
+        public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+            List<List<Integer>> result = new ArrayList<>();
+            Deque<Integer> stack = new LinkedList<>();
+
+            back(candidates, target, 0, result, stack);
+
+            return result;
+        }
+
+        private void back(int[] candidates, int target, int begin, List<List<Integer>> result, Deque<Integer> stack) {
+            if (target < 0) {
+                return;
+            }
+            if (target == 0) {
+                result.add(new ArrayList<>(stack));
+                return;
+            }
+            for (int i = begin; i < candidates.length; i++) {
+                stack.push(candidates[i]);
+                back(candidates, target - candidates[i], i + 1, result, stack);
+                stack.pop();
+            }
+        }
+    }
+}
