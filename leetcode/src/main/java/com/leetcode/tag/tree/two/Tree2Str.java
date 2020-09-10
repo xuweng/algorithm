a4a46dda3dabcabd7253a67849d9c41c5749e9fc
xuweng <2028403305@qq.com>
2020-09-10 22:32:51 +0800

    606. 根据二叉树创建字符串
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/Tree2Str.java
@@ -0,0 +1,34 @@
+package com.leetcode.tag.tree.two;
+
+/**
+ * 606. 根据二叉树创建字符串
+ * <p>
+ * 慢慢想
+ */
+public class Tree2Str {
+    class Solution {
+        public String tree2str(TreeNode t) {
+            if (t == null) {
+                return "()";
+            }
+            if (t.left == null && t.right == null) {
+                return "(" + t.val + ")";
+            }
+
+            String left = "(" + tree2str(t.left) + ")";
+            String right = "(" + tree2str(t.right) + ")";
+
+            return t.val + left + right;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
