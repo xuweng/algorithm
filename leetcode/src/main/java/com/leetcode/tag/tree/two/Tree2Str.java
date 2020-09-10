06f7766f9cd25526d800348e1f02dfc09c4f26bf
xuweng <2028403305@qq.com>
2020-09-10 22:44:19 +0800

    就差这个条件 根据示例，这样去掉括号
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/two/Tree2Str.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/Tree2Str.java
@@ -25,6 +25,30 @@ public class Tree2Str {
         }
     }
 
+    /**
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/construct-string-from-binary-tree/solution/gen-ju-er-cha-shu-chuang-jian-zi-fu-chuan-by-leetc/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * }
+     */
+    class Solution1 {
+        public String tree2str(TreeNode t) {
+            if (t == null) {
+                return "";
+            }
+            if (t.left == null && t.right == null) {
+                return t.val + "";
+            }
+            //就差这个条件
+            //根据示例，这样去掉括号
+            if (t.right == null) {
+                return t.val + "(" + tree2str(t.left) + ")";
+            }
+            return t.val + "(" + tree2str(t.left) + ")(" + tree2str(t.right) + ")";
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
