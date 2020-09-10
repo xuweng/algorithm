0955f37751a9e75f86b36b16d32e69dc74ee2da5
xuweng <2028403305@qq.com>
2020-09-10 23:24:26 +0800

    没有思路直接看题解学习
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/MinCameraCover.java
@@ -0,0 +1,26 @@
+package com.leetcode.tag.tree.two;
+
+/**
+ * 968. 监控二叉树
+ * <p>
+ * 这种题一开始就看题解
+ * <p>
+ * 没有思路直接看题解学习。
+ */
+public class MinCameraCover {
+    class Solution {
+        public int minCameraCover(TreeNode root) {
+            return 0;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
