8ab1e16e81865ccd04da0d3ebc6d5ea8b5faeda3
xuweng <2028403305@qq.com>
2020-09-11 12:38:19 +0800

    216. 组合总和 III
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/two/CombinationSum3.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/CombinationSum3.java
@@ -1,9 +1,6 @@
 package com.leetcode.tag.daily.two;
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 216. 组合总和 III
@@ -35,6 +32,38 @@ public class CombinationSum3 {
                 stack.push(i);
                 back(k, n - i, i + 1, result, stack);
                 stack.pop();
+            }
+        }
+    }
+
+    static class S {
+        public static List<List<Integer>> combinationSum3(int k, int n) {
+            List<List<Integer>> res = new ArrayList<>();
+            Deque<Integer> path = new ArrayDeque<>();//存储根节点开始的路径
+            dfs3(1, k, path, n, res);
+            return res;
+        }
+
+        public static void dfs3(int begin, int k, Deque<Integer> path, int target, List<List<Integer>> res) {
+            // 1.结束条件
+            if (target == 0 && path.size() == k) {
+                res.add(new ArrayList<>(path));
+                return;
+            }
+
+            // 2.选择列表
+            for (int i = begin; i < 10; i++) {
+                // 大剪枝
+                // 不用递归下一层
+                if (target - i < 0) {
+                    return;
+                }
+                // 选择
+                path.addLast(i);
+                // 递归
+                dfs3(i + 1, k, path, target - i, res);
+                // 撤销选择
+                path.removeLast();
             }
         }
     }
