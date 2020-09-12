db236d52ca5595e5a3481581b87a722ac4e9f887
xuweng <2028403305@qq.com>
2020-09-12 17:16:38 +0800

    617. 合并二叉树
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/MergeTrees.java
@@ -0,0 +1,40 @@
+package com.leetcode.tag.tree.two;
+
+/**
+ * 617. 合并二叉树
+ * <p>
+ * 不纠结。不纠结。不纠结。不纠结
+ */
+public class MergeTrees {
+    class Solution {
+        public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+            if (t1 == null && t2 == null) {
+                return null;
+            }
+            if (t1 == null) {
+                return new TreeNode(t2.val);
+            }
+            if (t2 == null) {
+                return new TreeNode(t1.val);
+            }
+            TreeNode root = new TreeNode(t1.val + t2.val);
+            TreeNode left = mergeTrees(t1.left, t2.left);
+            TreeNode right = mergeTrees(t1.right, t2.right);
+
+            root.left = left;
+            root.right = right;
+
+            return root;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
