a91fdc105c87b95f5e9e70cfae4f58b63f25f6f0
xuweng <2028403305@qq.com>
2020-09-12 17:26:49 +0800

    官方
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/two/MergeTrees.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/MergeTrees.java
@@ -29,6 +29,30 @@ public class MergeTrees {
         }
     }
 
+    /**
+     * 方法一：递归
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/merge-two-binary-trees/solution/he-bing-er-cha-shu-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * }
+     */
+    class Solution1 {
+        public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+            if (t1 == null) {
+                return t2;
+            }
+            if (t2 == null) {
+                return t1;
+            }
+            t1.val += t2.val;
+            t1.left = mergeTrees(t1.left, t2.left);
+            t1.right = mergeTrees(t1.right, t2.right);
+            return t1;
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
