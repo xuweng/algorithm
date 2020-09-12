425a5f7abd01eb7e55ebed2a113e145b7dc94de2
xuweng <2028403305@qq.com>
2020-09-12 15:19:29 +0800

    方法一：深度优先搜索
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/two/AverageOfLevels.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/AverageOfLevels.java
@@ -38,6 +38,43 @@ public class AverageOfLevels {
         }
     }
 
+    /**
+     * 方法一：深度优先搜索
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/average-of-levels-in-binary-tree/solution/er-cha-shu-de-ceng-ping-jun-zhi-by-leetcode-soluti/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public List<Double> averageOfLevels(TreeNode root) {
+            List<Integer> counts = new ArrayList<Integer>();
+            List<Double> sums = new ArrayList<Double>();
+            dfs(root, 0, counts, sums);
+            List<Double> averages = new ArrayList<Double>();
+            int size = sums.size();
+            for (int i = 0; i < size; i++) {
+                averages.add(sums.get(i) / counts.get(i));
+            }
+            return averages;
+        }
+
+        public void dfs(TreeNode root, int level, List<Integer> counts, List<Double> sums) {
+            if (root == null) {
+                return;
+            }
+            if (level < sums.size()) {
+                sums.set(level, sums.get(level) + root.val);
+                counts.set(level, counts.get(level) + 1);
+            } else {
+                sums.add(1.0 * root.val);
+                counts.add(1);
+            }
+            dfs(root.left, level + 1, counts, sums);
+            dfs(root.right, level + 1, counts, sums);
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
