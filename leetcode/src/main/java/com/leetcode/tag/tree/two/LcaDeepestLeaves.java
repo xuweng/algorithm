4a15a0f040597f5a197cb46cd0fd976678889cb8
xuweng <2028403305@qq.com>
2020-09-13 22:25:29 +0800

    1123. 最深叶节点的最近公共祖先
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/LcaDeepestLeaves.java
@@ -0,0 +1,24 @@
+package com.leetcode.tag.tree.two;
+
+/**
+ * 1123. 最深叶节点的最近公共祖先
+ * <p>
+ * 看不懂题目。直接看答案。
+ */
+public class LcaDeepestLeaves {
+    class Solution {
+        public TreeNode lcaDeepestLeaves(TreeNode root) {
+            return null;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
