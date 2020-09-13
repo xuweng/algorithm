9f7a6e96b67312729caae34ab19fe1ce7e0c7848
xuweng <2028403305@qq.com>
2020-09-13 23:15:25 +0800

    方法二：层序遍历（BFS）
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/two/MaxDepth.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/MaxDepth.java
@@ -1,5 +1,8 @@
 package com.leetcode.tag.tree.two;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 剑指 Offer 55 - I. 二叉树的深度
  */
@@ -7,6 +10,41 @@ public class MaxDepth {
     class Solution {
         public int maxDepth(TreeNode root) {
             return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        }
+    }
+
+    /**
+     * 方法二：层序遍历（BFS）
+     * <p>
+     * 作者：jyd
+     * 链接：https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/solution/mian-shi-ti-55-i-er-cha-shu-de-shen-du-xian-xu-bia/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int maxDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            List<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+
+            List<TreeNode> tmp;
+            int res = 0;
+            while (!queue.isEmpty()) {
+                tmp = new LinkedList<>();
+                for (TreeNode node : queue) {
+                    if (node.left != null) {
+                        tmp.add(node.left);
+                    }
+                    if (node.right != null) {
+                        tmp.add(node.right);
+                    }
+                }
+                queue = tmp;
+                res++;
+            }
+            return res;
         }
     }
 
