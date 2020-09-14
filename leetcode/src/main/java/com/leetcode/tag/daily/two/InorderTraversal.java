94b0b2cba0469053c4e6a08e23176424a7a987f8
xuweng <2028403305@qq.com>
2020-09-14 22:17:50 +0800

    94. 二叉树的中序遍历
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/InorderTraversal.java
@@ -0,0 +1,45 @@
+package com.leetcode.tag.daily.two;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 94. 二叉树的中序遍历
+ */
+public class InorderTraversal {
+    /**
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal/solution/er-cha-shu-de-zhong-xu-bian-li-by-leetcode-solutio/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution {
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> res = new ArrayList<>();
+            Deque<TreeNode> stk = new LinkedList<>();
+            while (root != null || !stk.isEmpty()) {
+                while (root != null) {
+                    stk.push(root);
+                    root = root.left;
+                }
+                root = stk.pop();
+                res.add(root.val);
+                root = root.right;
+            }
+            return res;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
