b4379ad6fa87b6f5c4f950fdf76a4c6aedff88b3
xuweng <2028403305@qq.com>
2020-09-14 22:35:23 +0800

    方法一：深度优先搜索
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/two/RightSideView.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/RightSideView.java
@@ -1,6 +1,6 @@
 package com.leetcode.tag.tree.two;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * 199. 二叉树的右视图
@@ -13,6 +13,53 @@ public class RightSideView {
     class Solution {
         public List<Integer> rightSideView(TreeNode root) {
             return null;
+        }
+    }
+
+    /**
+     * 方法一：深度优先搜索
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/binary-tree-right-side-view/solution/er-cha-shu-de-you-shi-tu-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public List<Integer> rightSideView(TreeNode root) {
+            Map<Integer, Integer> rightmostValueAtDepth = new HashMap<>();
+            int max_depth = -1;
+
+            Stack<TreeNode> nodeStack = new Stack<>();
+            Stack<Integer> depthStack = new Stack<>();
+            nodeStack.push(root);
+            depthStack.push(0);
+
+            while (!nodeStack.isEmpty()) {
+                TreeNode node = nodeStack.pop();
+                int depth = depthStack.pop();
+
+                if (node != null) {
+                    // 维护二叉树的最大深度
+                    max_depth = Math.max(max_depth, depth);
+
+                    // 如果不存在对应深度的节点我们才插入
+                    if (!rightmostValueAtDepth.containsKey(depth)) {
+                        rightmostValueAtDepth.put(depth, node.val);
+                    }
+
+                    nodeStack.push(node.left);
+                    nodeStack.push(node.right);
+                    depthStack.push(depth + 1);
+                    depthStack.push(depth + 1);
+                }
+            }
+
+            List<Integer> rightView = new ArrayList<>();
+            for (int depth = 0; depth <= max_depth; depth++) {
+                rightView.add(rightmostValueAtDepth.get(depth));
+            }
+
+            return rightView;
         }
     }
 
