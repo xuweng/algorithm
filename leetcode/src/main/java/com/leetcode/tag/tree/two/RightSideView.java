85bbd8e46ba476bb34a99d2679ec677a5b83b491
xuweng <2028403305@qq.com>
2020-09-14 22:32:08 +0800

    199. 二叉树的右视图
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/RightSideView.java
@@ -0,0 +1,28 @@
+package com.leetcode.tag.tree.two;
+
+import java.util.List;
+
+/**
+ * 199. 二叉树的右视图
+ * <p>
+ * 十分钟看答案
+ * <p>
+ * 十分钟看答案
+ */
+public class RightSideView {
+    class Solution {
+        public List<Integer> rightSideView(TreeNode root) {
+            return null;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
