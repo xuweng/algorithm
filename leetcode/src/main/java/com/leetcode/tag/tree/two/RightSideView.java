e8e1c9a980e6b53ed833f44d1736f823c5f1a9e2
xuweng <2028403305@qq.com>
2020-09-14 23:18:38 +0800

    代码简洁
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/two/RightSideView.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/RightSideView.java
@@ -125,6 +125,37 @@ public class RightSideView {
         }
     }
 
+    /**
+     * 代码简洁
+     */
+    class Solution3 {
+        public List<Integer> rightSideView(TreeNode root) {
+            List<Integer> res = new ArrayList<>();
+            if (root == null) {
+                return res;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                    //将当前层的最后一个节点放入结果列表
+                    if (i == size - 1) {
+                        res.add(node.val);
+                    }
+                }
+            }
+            return res;
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
