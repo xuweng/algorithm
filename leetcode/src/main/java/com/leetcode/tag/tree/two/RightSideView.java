c54a763ae8c109701a97645e3a2a5c3f9b8fa58c
xuweng <2028403305@qq.com>
2020-09-14 23:20:34 +0800

    DFS
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/two/RightSideView.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/RightSideView.java
@@ -156,6 +156,35 @@ public class RightSideView {
         }
     }
 
+    /**
+     * 二、DFS
+     * <p>
+     * 我们按照 「根结点 -> 右子树 -> 左子树」 的顺序访问， 就可以保证每层都是最先访问最右边的节点的。
+     * <p>
+     * （与先序遍历 「根结点 -> 左子树 -> 右子树」 正好相反，先序遍历每层最先访问的是最左边的节点）
+     */
+    class Solution4 {
+        List<Integer> res = new ArrayList<>();
+
+        public List<Integer> rightSideView(TreeNode root) {
+            dfs(root, 0); // 从根节点开始访问，根节点深度是0
+            return res;
+        }
+
+        private void dfs(TreeNode root, int depth) {
+            if (root == null) {
+                return;
+            }
+            // 先访问 当前节点，再递归地访问 右子树 和 左子树。
+            if (depth == res.size()) {   // 如果当前节点所在深度还没有出现在res里，说明在该深度下当前节点是第一个被访问的节点，因此将当前节点加入res中。
+                res.add(root.val);
+            }
+            depth++;
+            dfs(root.right, depth);
+            dfs(root.left, depth);
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
