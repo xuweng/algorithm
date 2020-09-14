3ff6966f9303472aea765f26752def488aac7193
xuweng <2028403305@qq.com>
2020-09-14 22:22:09 +0800

    方法三：Morris 中序遍历
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/two/InorderTraversal.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/InorderTraversal.java
@@ -35,6 +35,49 @@ public class InorderTraversal {
         }
     }
 
+    /**
+     * 方法三：Morris 中序遍历
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal/solution/er-cha-shu-de-zhong-xu-bian-li-by-leetcode-solutio/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> res = new ArrayList<Integer>();
+            TreeNode predecessor = null;
+
+            while (root != null) {
+                if (root.left != null) {
+                    // predecessor 节点就是当前 root 节点向左走一步，然后一直向右走至无法走为止
+                    predecessor = root.left;
+                    while (predecessor.right != null && predecessor.right != root) {
+                        predecessor = predecessor.right;
+                    }
+
+                    // 让 predecessor 的右指针指向 root，继续遍历左子树
+                    if (predecessor.right == null) {
+                        predecessor.right = root;
+                        root = root.left;
+                    }
+                    // 说明左子树已经访问完了，我们需要断开链接
+                    else {
+                        res.add(root.val);
+                        predecessor.right = null;
+                        root = root.right;
+                    }
+                }
+                // 如果没有左孩子，则直接访问右孩子
+                else {
+                    res.add(root.val);
+                    root = root.right;
+                }
+            }
+            return res;
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
