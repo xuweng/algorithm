235277ce24d07bdfd7342d6c3cc32c307f3e5cb3
xuweng <2028403305@qq.com>
2020-09-15 11:22:46 +0800

    方法二：位运算优化
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/two/SolveSudoku.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/SolveSudoku.java
@@ -95,4 +95,60 @@ public class SolveSudoku {
         }
     }
 
+    /**
+     * 方法二：位运算优化
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/sudoku-solver/solution/jie-shu-du-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        private int[] line = new int[9];
+        private int[] column = new int[9];
+        private int[][] block = new int[3][3];
+        private boolean valid = false;
+        private List<int[]> spaces = new ArrayList<>();
+
+        public void solveSudoku(char[][] board) {
+            for (int i = 0; i < 9; ++i) {
+                for (int j = 0; j < 9; ++j) {
+                    if (board[i][j] == '.') {
+                        spaces.add(new int[]{i, j});
+                    } else {
+                        int digit = board[i][j] - '0' - 1;
+                        flip(i, j, digit);
+                    }
+                }
+            }
+
+            dfs(board, 0);
+        }
+
+        public void dfs(char[][] board, int pos) {
+            if (pos == spaces.size()) {
+                valid = true;
+                return;
+            }
+
+            int[] space = spaces.get(pos);
+            int i = space[0], j = space[1];
+            int mask = ~(line[i] | column[j] | block[i / 3][j / 3]) & 0x1ff;
+            for (; mask != 0 && !valid; mask &= (mask - 1)) {
+                int digitMask = mask & (-mask);
+                int digit = Integer.bitCount(digitMask - 1);
+                flip(i, j, digit);
+                board[i][j] = (char) (digit + '0' + 1);
+                dfs(board, pos + 1);
+                flip(i, j, digit);
+            }
+        }
+
+        public void flip(int i, int j, int digit) {
+            line[i] ^= (1 << digit);
+            column[j] ^= (1 << digit);
+            block[i / 3][j / 3] ^= (1 << digit);
+        }
+    }
+
 }
