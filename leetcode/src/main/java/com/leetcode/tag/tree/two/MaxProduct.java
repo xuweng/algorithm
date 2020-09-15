ea0e81eb6117f2bfb27e55c6cf7f023d76570e7e
xuweng <2028403305@qq.com>
2020-09-15 12:30:35 +0800

    1339. 分裂二叉树的最大乘积
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/MaxProduct.java
@@ -0,0 +1,24 @@
+package com.leetcode.tag.tree.two;
+
+/**
+ * 1339. 分裂二叉树的最大乘积
+ * <p>
+ * 直接看题解学习
+ */
+public class MaxProduct {
+    class Solution {
+        public int maxProduct(TreeNode root) {
+            return 0;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
