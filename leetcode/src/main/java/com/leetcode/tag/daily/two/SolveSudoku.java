900e60314118d201a457c5c87fb3d71172992ccf
xuweng <2028403305@qq.com>
2020-09-15 10:53:22 +0800

    一行搞定
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/two/SolveSudoku.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/SolveSudoku.java
@@ -1,5 +1,8 @@
 package com.leetcode.tag.daily.two;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 37. 解数独
  * <p>
@@ -18,4 +21,59 @@ public class SolveSudoku {
 
         }
     }
+
+    /**
+     * 方法一：递归
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/sudoku-solver/solution/jie-shu-du-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        //行
+        private boolean[][] line = new boolean[9][9];
+        //列
+        private boolean[][] column = new boolean[9][9];
+        //小宫格
+        private boolean[][][] block = new boolean[3][3][9];
+        private boolean valid = false;
+        private List<int[]> spaces = new ArrayList<>();
+
+        public void solveSudoku(char[][] board) {
+            for (int i = 0; i < 9; ++i) {
+                for (int j = 0; j < 9; ++j) {
+                    if (board[i][j] == '.') {
+                        spaces.add(new int[]{i, j});
+                    } else {
+                        int digit = board[i][j] - '0' - 1;
+                        line[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = true;
+                    }
+                }
+            }
+
+            dfs(board, 0);
+        }
+
+        public void dfs(char[][] board, int pos) {
+            if (pos == spaces.size()) {
+                valid = true;
+                return;
+            }
+
+            int[] space = spaces.get(pos);
+            int i = space[0], j = space[1];
+            for (int digit = 0; digit < 9 && !valid; ++digit) {
+                if (!line[i][digit] && !column[j][digit] && !block[i / 3][j / 3][digit]) {
+                    //一行搞定
+                    line[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = true;
+                    board[i][j] = (char) (digit + '0' + 1);
+                    dfs(board, pos + 1);
+                    //一行搞定
+                    line[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = false;
+                }
+            }
+        }
+    }
+
 }
