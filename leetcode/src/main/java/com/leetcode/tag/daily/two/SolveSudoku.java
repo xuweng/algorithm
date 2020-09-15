c00cd371d298e001a3d1c1a5dee0c270871c9ca0
xuweng <2028403305@qq.com>
2020-09-15 10:48:43 +0800

    37. 解数独
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/two/SolveSudoku.java
@@ -0,0 +1,21 @@
+package com.leetcode.tag.daily.two;
+
+/**
+ * 37. 解数独
+ * <p>
+ * 十分钟看答案
+ * <p>
+ * 十分钟看答案
+ * <p>
+ * 十分钟看答案
+ */
+public class SolveSudoku {
+    /**
+     * 检查行、列、3*3宫格
+     */
+    class Solution {
+        public void solveSudoku(char[][] board) {
+
+        }
+    }
+}
