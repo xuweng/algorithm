c9ce824d2875307b535e616cd6bc9a918f49c35b
xuweng <2028403305@qq.com>
2020-09-16 12:38:49 +0800

    虽然简单,就是想不出来
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/two/LongestZigZag.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/LongestZigZag.java
@@ -4,6 +4,10 @@ package com.leetcode.tag.tree.two;
  * 1372. 二叉树中的最长交错路径
  * <p>
  * 用一个栈就可以完成遍历,不用cur
+ * <p>
+ * 用一个栈。不用cur。
+ * <p>
+ * 虽然简单,就是想不出来
  */
 public class LongestZigZag {
     class Solution {
