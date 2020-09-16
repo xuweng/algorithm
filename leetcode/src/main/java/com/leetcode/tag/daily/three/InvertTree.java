989f8c1642ea65f69e158d6d7c9ad508e06a35f3
xuweng <2028403305@qq.com>
2020-09-16 12:15:54 +0800

    方法一：递归
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/InvertTree.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/InvertTree.java
@@ -38,6 +38,27 @@ public class InvertTree {
         }
     }
 
+    /**
+     * 方法一：递归
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/invert-binary-tree/solution/fan-zhuan-er-cha-shu-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) {
+                return null;
+            }
+            TreeNode left = invertTree(root.left);
+            TreeNode right = invertTree(root.right);
+            root.left = right;
+            root.right = left;
+            return root;
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
