caadaea03df70a554e87b20f83c71e76ae30c779
xuweng <2028403305@qq.com>
2020-09-17 16:21:56 +0800

    根据高度来合并 路径压缩
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/FindRedundantConnection.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/FindRedundantConnection.java
@@ -2,6 +2,11 @@ package com.leetcode.tag.daily.three;
 
 /**
  * 684. 冗余连接
+ * <p>
+ * 并查集
+ * <p>
+ * 根据高度来合并(UnionUnion byby RankRank)
+ * 路径压缩(PathPath CompressionCompression)
  */
 public class FindRedundantConnection {
     class Solution {
