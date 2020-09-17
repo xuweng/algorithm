cede85e10251ea4bb44f074ed26144bc72aad760
xuweng <2028403305@qq.com>
2020-09-17 16:29:08 +0800

    并查集
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/FindRedundantConnection.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/FindRedundantConnection.java
@@ -10,10 +10,67 @@ package com.leetcode.tag.daily.three;
  */
 public class FindRedundantConnection {
     class Solution {
+        int[] parents;
+
         public int[] findRedundantConnection(int[][] edges) {
-            return null;
+            if (edges == null || edges.length == 0) {
+                return new int[]{0, 0};
+            }
+            int n = edges.length + 1; //注意此处下标多放一个
+
+            init(n);
+
+            for (int[] edge : edges) {
+                int x = edge[0], y = edge[1];
+                if ((!union(x, y))) {
+                    //第二次出现了联通的边时，表示已经找到了
+                    return edge;
+                }
+            }
+            return new int[]{0, 0};
         }
+
+        /**
+         * 初始化parents
+         *
+         * @param n
+         */
+        public void init(int n) {
+            parents = new int[n];
+            for (int i = 0; i < n; i++) {
+                parents[i] = i;
+            }
+        }
+
+        /**
+         * 递归版路径压缩，找到x的根节点
+         *
+         * @param x
+         * @return
+         */
+        public int find(int x) {
+            if (x != parents[x]) {
+                parents[x] = find(parents[x]);
+            }
+            return parents[x];
+        }
+
+        /**
+         * 改写union方法，第一次当x与y没有联通时，将其设置联通关系，返回ture
+         * 第二次x和y的跟节点发现一致时，他们已经联通了，返回false
+         *
+         * @param x
+         * @param y
+         * @return
+         */
+        public boolean union(int x, int y) {
+            int rootX = find(x), rootY = find(y);
+            if (rootX == rootY) {
+                return false;
+            }
+            parents[rootX] = rootY;
+            return true;
+        }
+
     }
-
-
 }
