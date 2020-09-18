f14c975cc1ea466a51c66cf770ec6942871a81ee
xuweng <2028403305@qq.com>
2020-09-18 18:26:18 +0800

    方法一：搜索回溯
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/PermuteUnique.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/PermuteUnique.java
@@ -50,4 +50,43 @@ public class PermuteUnique {
             }
         }
     }
+
+    /**
+     * 方法一：搜索回溯
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/permutations-ii/solution/quan-pai-lie-ii-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        boolean[] vis;
+
+        public List<List<Integer>> permuteUnique(int[] nums) {
+            List<List<Integer>> ans = new ArrayList<List<Integer>>();
+            List<Integer> perm = new ArrayList<Integer>();
+            vis = new boolean[nums.length];
+            Arrays.sort(nums);
+            backtrack(nums, ans, 0, perm);
+            return ans;
+        }
+
+        public void backtrack(int[] nums, List<List<Integer>> ans, int idx, List<Integer> perm) {
+            if (idx == nums.length) {
+                ans.add(new ArrayList<Integer>(perm));
+                return;
+            }
+            for (int i = 0; i < nums.length; ++i) {
+                if (vis[i] || (i > 0 && nums[i] == nums[i - 1] && !vis[i - 1])) {
+                    continue;
+                }
+                perm.add(nums[i]);
+                vis[i] = true;
+                backtrack(nums, ans, idx + 1, perm);
+                vis[i] = false;
+                perm.remove(idx);
+            }
+        }
+    }
+
 }
