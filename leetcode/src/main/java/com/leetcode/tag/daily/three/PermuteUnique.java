a54a965042652a8f1089fcab08a01d6b1e16abba
xuweng <2028403305@qq.com>
2020-09-18 12:02:45 +0800

    47. 全排列 II
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/PermuteUnique.java
@@ -0,0 +1,39 @@
+package com.leetcode.tag.daily.three;
+
+import java.util.*;
+
+/**
+ * 47. 全排列 II
+ */
+public class PermuteUnique {
+    class Solution {
+        public List<List<Integer>> permuteUnique(int[] nums) {
+            Arrays.sort(nums);
+
+            boolean[] used = new boolean[nums.length];
+            Deque<Integer> stack = new LinkedList<>();
+            List<List<Integer>> result = new ArrayList<>();
+
+            back(nums, used, stack, result);
+
+            return result;
+        }
+
+        private void back(int[] nums, boolean[] used, Deque<Integer> stack, List<List<Integer>> result) {
+            if (stack.size() == nums.length) {
+                result.add(new ArrayList<>(stack));
+                return;
+            }
+            for (int i = 0; i < nums.length; i++) {
+                if (i > 0 && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                used[i] = true;
+                stack.push(nums[i]);
+                back(nums, used, stack, result);
+                used[i] = false;
+                stack.pop();
+            }
+        }
+    }
+}
