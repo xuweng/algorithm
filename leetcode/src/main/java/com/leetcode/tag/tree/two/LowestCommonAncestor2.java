c5bdce9b2db98f602bbf61412ac2a55736e0f44b
xuweng <2028403305@qq.com>
2020-09-18 19:09:26 +0800

    235. 二叉搜索树的最近公共祖先
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/LowestCommonAncestor2.java
@@ -0,0 +1,28 @@
+package com.leetcode.tag.tree.two;
+
+/**
+ * 235. 二叉搜索树的最近公共祖先
+ */
+public class LowestCommonAncestor2 {
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root.val > p.val && root.val > q.val) {
+                return lowestCommonAncestor(root.left, p, q);
+            }
+            if (root.val < p.val && root.val < q.val) {
+                return lowestCommonAncestor(root.right, p, q);
+            }
+            return root;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
