b2ff4642d068ceecf7ba75db855bbf27f6475107
xuweng <2028403305@qq.com>
2020-09-19 11:11:57 +0800

    方法二：广度优先搜索
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/SumOfLeftLeaves.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/SumOfLeftLeaves.java
@@ -1,5 +1,8 @@
 package com.leetcode.tag.daily.three;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 404. 左叶子之和
  */
@@ -26,6 +29,8 @@ public class SumOfLeftLeaves {
     }
 
     /**
+     * 方法一：深度优先搜索
+     * <p>
      * 作者：LeetCode-Solution
      * 链接：https://leetcode-cn.com/problems/sum-of-left-leaves/solution/zuo-xie-zi-zhi-he-by-leetcode-solution/
      * 来源：力扣（LeetCode）
@@ -43,6 +48,44 @@ public class SumOfLeftLeaves {
             }
             if (node.right != null && !isLeafNode(node.right)) {
                 ans += dfs(node.right);
+            }
+            return ans;
+        }
+
+        public boolean isLeafNode(TreeNode node) {
+            return node.left == null && node.right == null;
+        }
+    }
+
+    /**
+     * 方法二：广度优先搜索
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/sum-of-left-leaves/solution/zuo-xie-zi-zhi-he-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public int sumOfLeftLeaves(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            int ans = 0;
+            while (!queue.isEmpty()) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    if (isLeafNode(node.left)) {
+                        ans += node.left.val;
+                    } else {
+                        queue.offer(node.left);
+                    }
+                }
+                if (node.right != null && !isLeafNode(node.right)) {
+                    queue.offer(node.right);
+                }
             }
             return ans;
         }
