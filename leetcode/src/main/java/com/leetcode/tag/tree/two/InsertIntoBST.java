f2467806f7db8f835909b1d9a4ceda7de879aaa0
xuweng <2028403305@qq.com>
2020-09-19 12:07:01 +0800

    官方
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/two/InsertIntoBST.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/InsertIntoBST.java
@@ -21,6 +21,24 @@ public class InsertIntoBST {
         }
     }
 
+    class Solution1 {
+        public TreeNode insertIntoBST(TreeNode root, int val) {
+            if (root == null) {
+                return new TreeNode(val);
+            }
+
+            // insert into the right subtree
+            if (val > root.val) {
+                root.right = insertIntoBST(root.right, val);
+            }
+            // insert into the left subtree
+            else {
+                root.left = insertIntoBST(root.left, val);
+            }
+            return root;
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
