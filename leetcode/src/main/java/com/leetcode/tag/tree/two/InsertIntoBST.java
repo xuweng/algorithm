c559cfa9d2f79612345a3bfab7648d4163e1c251
xuweng <2028403305@qq.com>
2020-09-19 12:02:28 +0800

    701. 二叉搜索树中的插入操作
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/InsertIntoBST.java
@@ -0,0 +1,40 @@
+package com.leetcode.tag.tree.two;
+
+/**
+ * 701. 二叉搜索树中的插入操作
+ */
+public class InsertIntoBST {
+    class Solution {
+        public TreeNode insertIntoBST(TreeNode root, int val) {
+            if (root == null) {
+                return new TreeNode(val);
+            }
+            if (val <= root.val) {
+                root.left = insertIntoBST(root.left, val);
+            } else {
+                root.right = insertIntoBST(root.right, val);
+            }
+
+            return root;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
