f661a7f817d9efbd4f406e9ea0d39e7d4a329012
xuweng <2028403305@qq.com>
2020-09-19 11:21:49 +0800

    1145. 二叉树着色游戏
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/BtreeGameWinningMove.java
@@ -0,0 +1,22 @@
+package com.leetcode.tag.tree.two;
+
+/**
+ * 1145. 二叉树着色游戏
+ */
+public class BtreeGameWinningMove {
+    class Solution {
+        public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
+            return true;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
