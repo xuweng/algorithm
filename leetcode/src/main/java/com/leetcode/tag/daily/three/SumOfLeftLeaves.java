de7c703f52c54001e3aabca0eb4bcb1eefbcebe6
xuweng <2028403305@qq.com>
2020-09-19 11:09:19 +0800

    官方
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/SumOfLeftLeaves.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/SumOfLeftLeaves.java
@@ -25,6 +25,33 @@ public class SumOfLeftLeaves {
         }
     }
 
+    /**
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/sum-of-left-leaves/solution/zuo-xie-zi-zhi-he-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int sumOfLeftLeaves(TreeNode root) {
+            return root != null ? dfs(root) : 0;
+        }
+
+        public int dfs(TreeNode node) {
+            int ans = 0;
+            if (node.left != null) {
+                ans += isLeafNode(node.left) ? node.left.val : dfs(node.left);
+            }
+            if (node.right != null && !isLeafNode(node.right)) {
+                ans += dfs(node.right);
+            }
+            return ans;
+        }
+
+        public boolean isLeafNode(TreeNode node) {
+            return node.left == null && node.right == null;
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
