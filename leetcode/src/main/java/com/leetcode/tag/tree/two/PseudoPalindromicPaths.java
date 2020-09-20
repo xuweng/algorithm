d17dc229308c648760523eb2e12d5c246b37092c
xuweng <2028403305@qq.com>
2020-09-20 12:59:30 +0800

    方法1.2 DFS + 位运算（推荐）
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/two/PseudoPalindromicPaths.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/PseudoPalindromicPaths.java
@@ -93,6 +93,48 @@ public class PseudoPalindromicPaths {
 
     }
 
+    /**
+     * 方法1.2 DFS + 位运算（推荐）
+     * <p>
+     * 作者：lixianfeng
+     * 链接：https://leetcode-cn.com/problems/pseudo-palindromic-paths-in-a-binary-tree/solution/java-by-lixianfeng/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class S1 {
+        int res = 0;
+
+        public int pseudoPalindromicPaths(TreeNode root) {
+            preorder(root, 0);
+
+            return res;
+        }
+
+        public void preorder(TreeNode root, int num) {
+            if (root == null) {
+                return;
+            }
+            //根据异或的性质，相同的节点数值，经过异或为0
+            // num ^ (1 << root.val)表示当前val值是多少，就左移val位，并异或
+            int tmp = num ^ (1 << root.val);
+            // 叶子节点
+            if (root.left == null && root.right == null) {
+                // tmp==0表示当前路径奇数个元素的个数为0
+                // tmp&(tmp-1)会消除tmp中二进制位的最右边一个1，值等于0，表示只有一个1，奇数个元素的个数为1
+                if (tmp == 0 || (tmp & (tmp - 1)) == 0) {
+                    res++;
+                }
+            }
+            if (root.left != null) {
+                preorder(root.left, tmp);
+            }
+            if (root.right != null) {
+                preorder(root.right, tmp);
+            }
+        }
+
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
