e09d425cdc9f66db784566d7f1bc3f191ea6f15e
xuweng <2028403305@qq.com>
2020-09-20 12:43:51 +0800

    1457. 二叉树中的伪回文路径
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/PseudoPalindromicPaths.java
@@ -0,0 +1,61 @@
+package com.leetcode.tag.tree.two;
+
+/**
+ * 1457. 二叉树中的伪回文路径
+ */
+public class PseudoPalindromicPaths {
+    /**
+     * 作者：rational-irrationality
+     * 链接：https://leetcode-cn.com/problems/pseudo-palindromic-paths-in-a-binary-tree/solution/java-dfs-shuang-bai-by-rational-irrationality/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution {
+        int ans = 0;
+
+        public int pseudoPalindromicPaths(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            helper(root, 0);
+            return ans;
+        }
+
+        void helper(TreeNode node, int temp) {
+            temp ^= (1 << node.val);//node节点的val为几就左移几位
+            if (node.left == null && node.right == null) {//判断是否叶子节点
+                if (temp == 0 || (temp & (temp - 1)) == 0) {//判断是否为伪回文
+                    ans++;
+                }
+            }
+            //剪枝
+            //这种遍历也可以
+            //满二叉树.满二叉树.满二叉树
+            if (node.left != null) {
+                helper(node.left, temp);
+            }
+            if (node.right != null) {
+                helper(node.right, temp);
+            }
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
