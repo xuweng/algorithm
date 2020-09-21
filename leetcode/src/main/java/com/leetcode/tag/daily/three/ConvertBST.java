fe85face25e28d394065a6d55a2058c133650e08
xuweng <2028403305@qq.com>
2020-09-21 14:19:38 +0800

    538. 把二叉搜索树转换为累加树
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/ConvertBST.java
@@ -0,0 +1,48 @@
+package com.leetcode.tag.daily.three;
+
+/**
+ * 538. 把二叉搜索树转换为累加树
+ */
+public class ConvertBST {
+    class Solution {
+        int sum;
+
+        public TreeNode convertBST(TreeNode root) {
+            pre(root);
+
+            return root;
+        }
+
+        private void pre(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+            pre(root, root.val);
+            root.val += sum;
+            sum = 0;
+            pre(root.left);
+            pre(root.right);
+        }
+
+        private void pre(TreeNode root, int val) {
+            if (root == null) {
+                return;
+            }
+            if (root.val > val) {
+                sum += root.val;
+            }
+            pre(root.left, val);
+            pre(root.right, val);
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
