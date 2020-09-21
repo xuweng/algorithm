1f6de4dee3cea11b4b2671a1c8603d793da9573a
xuweng <2028403305@qq.com>
2020-09-21 17:33:39 +0800

    方法三：层序遍历 + 倒序
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/two/LevelOrder.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/LevelOrder.java
@@ -46,11 +46,11 @@ public class LevelOrder {
      */
     class Solution1 {
         public List<List<Integer>> levelOrder(TreeNode root) {
-            Queue<TreeNode> queue = new LinkedList<>();
             List<List<Integer>> res = new ArrayList<>();
             if (root == null) {
                 return res;
             }
+            Queue<TreeNode> queue = new LinkedList<>();
             queue.add(root);
 
             while (!queue.isEmpty()) {
@@ -58,10 +58,10 @@ public class LevelOrder {
                 for (int i = queue.size(); i > 0; i--) {
                     TreeNode node = queue.poll();
                     if (res.size() % 2 == 0) {
-                        //头插法.倒序
+                        //头插法.倒序.相当栈.
                         tmp.addLast(node.val); // 偶数层 -> 队列头部
                     } else {
-                        //尾插法.正序.
+                        //尾插法.正序.相当队列.
                         tmp.addFirst(node.val); // 奇数层 -> 队列尾部
                     }
                     if (node.left != null) {
@@ -70,6 +70,44 @@ public class LevelOrder {
                     if (node.right != null) {
                         queue.add(node.right);
                     }
+                }
+                res.add(tmp);
+            }
+            return res;
+        }
+    }
+
+    /**
+     * 方法三：层序遍历 + 倒序
+     * <p>
+     * 作者：jyd
+     * 链接：https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof/solution/mian-shi-ti-32-iii-cong-shang-dao-xia-da-yin-er--3/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> res = new ArrayList<>();
+            if (root == null) {
+                return res;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                List<Integer> tmp = new ArrayList<>();
+                for (int i = queue.size(); i > 0; i--) {
+                    TreeNode node = queue.poll();
+                    tmp.add(node.val);
+                    if (node.left != null) {
+                        queue.add(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.add(node.right);
+                    }
+                }
+                //偶数层倒序： 若 res 的长度为 奇数 ，说明当前是偶数层，则对 tmp 执行 倒序 操作。
+                if (res.size() % 2 == 1) {
+                    Collections.reverse(tmp);
                 }
                 res.add(tmp);
             }
