4332c7e3372e41213f88465b91312ab156ab0eaf
xuweng <2028403305@qq.com>
2020-09-21 17:11:43 +0800

    方法一：层序遍历 + 双端队列
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/two/LevelOrder.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/two/LevelOrder.java
@@ -1,8 +1,6 @@
 package com.leetcode.tag.tree.two;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * 剑指 Offer 32 - III. 从上到下打印二叉树 III
@@ -31,6 +29,49 @@ public class LevelOrder {
             result.get(level).add(root.val);
             level(root.left, level + 1);
             level(root.right, level + 1);
+        }
+    }
+
+    /**
+     * 方法一：层序遍历 + 双端队列
+     * <p>
+     * 奇数层正序
+     * <p>
+     * 偶数层倒序
+     * <p>
+     * 作者：jyd
+     * 链接：https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof/solution/mian-shi-ti-32-iii-cong-shang-dao-xia-da-yin-er--3/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            Queue<TreeNode> queue = new LinkedList<>();
+            List<List<Integer>> res = new ArrayList<>();
+            if (root == null) {
+                return res;
+            }
+            queue.add(root);
+
+            while (!queue.isEmpty()) {
+                LinkedList<Integer> tmp = new LinkedList<>();
+                for (int i = queue.size(); i > 0; i--) {
+                    TreeNode node = queue.poll();
+                    if (res.size() % 2 == 0) {
+                        tmp.addLast(node.val); // 偶数层 -> 队列头部
+                    } else {
+                        tmp.addFirst(node.val); // 奇数层 -> 队列尾部
+                    }
+                    if (node.left != null) {
+                        queue.add(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.add(node.right);
+                    }
+                }
+                res.add(tmp);
+            }
+            return res;
         }
     }
 
