a30cd5a9e4d30def2bf8b899d01d00b9233c676b
xuweng <2028403305@qq.com>
2020-09-22 21:29:20 +0800

    每个结点有3种状态
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/MinCameraCover.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/MinCameraCover.java
@@ -94,6 +94,82 @@ public class MinCameraCover {
         }
     }
 
+    /**
+     * 每个节点可能有几种状态：
+     * <p>
+     * 可以说有如下三种：
+     * <p>
+     * 该节点无覆盖
+     * 本节点有摄像头
+     * 本节点有覆盖
+     * <p>
+     * 我们分别有三个数字来表示：
+     * <p>
+     * 0：该节点无覆盖
+     * 1：本节点有摄像头
+     * 2：本节点有覆盖
+     * <p>
+     * 作者：carlsun-2
+     * 链接：https://leetcode-cn.com/problems/binary-tree-cameras/solution/968-jian-kong-er-cha-shu-di-gui-shang-de-zhuang-ta/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution3 {
+        private int result;
+
+        int traversal(TreeNode cur) {
+
+            // 空节点，该节点有覆盖
+            if (cur == null) {
+                return 2;
+            }
+
+            int left = traversal(cur.left);    // 左
+            int right = traversal(cur.right);  // 右
+
+            // 情况1
+            // 左右节点都有覆盖
+            if (left == 2 && right == 2) {
+                return 0;
+            }
+
+            // 情况2
+            // left == 0 && right == 0 左右节点无覆盖
+            // left == 1 && right == 0 左节点有摄像头，右节点无覆盖
+            // left == 0 && right == 1 左节点有无覆盖，右节点摄像头
+            // left == 0 && right == 2 左节点无覆盖，右节点覆盖
+            // left == 2 && right == 0 左节点覆盖，右节点无覆盖
+            if (left == 0 || right == 0) {
+                result++;
+                return 1;
+            }
+
+            // 情况3
+            // left == 1 && right == 2 左节点有摄像头，右节点有覆盖
+            // left == 2 && right == 1 左节点有覆盖，右节点有摄像头
+            // left == 1 && right == 1 左右节点都有摄像头
+            // 其他情况前段代码均已覆盖
+            if (left == 1 || right == 1) {
+                return 2;
+            }
+
+            // 以上代码我没有使用else，主要是为了把各个分支条件展现出来，这样代码有助于读者理解
+            // 这个 return -1 逻辑不会走到这里。
+            return -1;
+        }
+
+        public int minCameraCover(TreeNode root) {
+            result = 0;
+            // 情况4
+            if (traversal(root) == 0) { // root 无覆盖
+                result++;
+            }
+            return result;
+        }
+    }
+
+    ;
+
     class TreeNode {
         int val;
         TreeNode left;
