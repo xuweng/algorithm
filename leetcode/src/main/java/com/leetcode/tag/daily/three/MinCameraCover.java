f2eeb847c0ae7ddba31d6943649349e1bd5e9187
xuweng <2028403305@qq.com>
2020-09-22 13:42:55 +0800

    树形dp
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/MinCameraCover.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/MinCameraCover.java
@@ -54,6 +54,42 @@ public class MinCameraCover {
         }
     }
 
+    /**
+     * 作者：rorke76753
+     * 链接：https://leetcode-cn.com/problems/binary-tree-cameras/solution/dfsjavajian-dan-yi-dong-by-rorke76753/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        int result = 0;
+
+        public int minCameraCover(TreeNode root) {
+            if (dfs(root) == 1) {
+                result++;
+            }
+            return result;
+        }
+
+        //0:可被观测但无监控，上一层节点为1
+        //1：不可被观测到，上一层节点为2
+        //2：有摄像机，上一层节点为0
+        private int dfs(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            int leftStatus = dfs(root.left);
+            int rightStatus = dfs(root.right);
+            if (leftStatus == 1 || rightStatus == 1) {
+                result++;
+                return 2;
+            } else if (leftStatus == 2 || rightStatus == 2) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
