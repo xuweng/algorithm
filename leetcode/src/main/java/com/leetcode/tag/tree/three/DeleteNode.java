04e47296ce6e24cff5c413b970b3d49a2986ea62
xuweng <2028403305@qq.com>
2020-09-22 22:28:25 +0800

    450. 删除二叉搜索树中的节点
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/three/DeleteNode.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/three/DeleteNode.java
@@ -31,6 +31,69 @@ public class DeleteNode {
         }
     }
 
+    /**
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/delete-node-in-a-bst/solution/shan-chu-er-cha-sou-suo-shu-zhong-de-jie-dian-by-l/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        /*
+        One step right and then always left
+        */
+        public int successor(TreeNode root) {
+            root = root.right;
+            while (root.left != null) {
+                root = root.left;
+            }
+            return root.val;
+        }
+
+        /*
+        One step left and then always right
+        */
+        public int predecessor(TreeNode root) {
+            root = root.left;
+            while (root.right != null) {
+                root = root.right;
+            }
+            return root.val;
+        }
+
+        public TreeNode deleteNode(TreeNode root, int key) {
+            if (root == null) {
+                return null;
+            }
+
+            // delete from the right subtree
+            if (key > root.val) {
+                root.right = deleteNode(root.right, key);
+            }
+            // delete from the left subtree
+            else if (key < root.val) {
+                root.left = deleteNode(root.left, key);
+            }
+            // delete the current node
+            else {
+                // the node is a leaf
+                if (root.left == null && root.right == null) {
+                    root = null;
+                }
+                // the node is not a leaf and has a right child
+                else if (root.right != null) {
+                    root.val = successor(root);
+                    root.right = deleteNode(root.right, root.val);
+                }
+                // the node is not a leaf, has no right child, and has a left child
+                else {
+                    root.val = predecessor(root);
+                    root.left = deleteNode(root.left, root.val);
+                }
+            }
+            return root;
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
