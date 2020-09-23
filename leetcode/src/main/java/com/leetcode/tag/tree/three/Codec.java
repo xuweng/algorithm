03ae4cc8dc244a92bc977a7cbfc8e6b48eb16a14
xuweng <2028403305@qq.com>
2020-09-23 23:38:23 +0800

    剑指 Offer 37. 序列化二叉树
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/three/Codec.java
@@ -0,0 +1,88 @@
+package com.leetcode.tag.tree.three;
+
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Queue;
+
+/**
+ * 剑指 Offer 37. 序列化二叉树
+ * <p>
+ * 作者：jyd
+ * 链接：https://leetcode-cn.com/problems/xu-lie-hua-er-cha-shu-lcof/solution/mian-shi-ti-37-xu-lie-hua-er-cha-shu-ceng-xu-bian-/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+public class Codec {
+    /**
+     * 序列化 serialize ：
+     * <p>
+     * 借助队列，对二叉树做层序遍历，并将越过叶节点的  null 也打印出来
+     *
+     * @param root
+     * @return
+     */
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return "[]";
+        }
+        StringBuilder res = new StringBuilder("[");
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node == null) {
+                res.append("null,");
+            } else {
+                res.append(node.val).append(",");
+                //left为null也会加入队列
+                queue.add(node.left);
+                //right为null也会加入队列
+                queue.add(node.right);
+            }
+        }
+        res.deleteCharAt(res.length() - 1).append("]");
+
+        return res.toString();
+    }
+
+    /**
+     * 反序列化 deserialize ：
+     *
+     * @param data
+     * @return
+     */
+    public TreeNode deserialize(String data) {
+        if ("[]".equals(data)) {
+            return null;
+        }
+        String[] vals = data.substring(1, data.length() - 1).split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(vals[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int i = 1;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (!Objects.equals(vals[i], "null")) {
+                node.left = new TreeNode(Integer.parseInt(vals[i]));
+                queue.add(node.left);
+            }
+            i++;
+            if (!Objects.equals(vals[i], "null")) {
+                node.right = new TreeNode(Integer.parseInt(vals[i]));
+                queue.add(node.right);
+            }
+            i++;
+        }
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
