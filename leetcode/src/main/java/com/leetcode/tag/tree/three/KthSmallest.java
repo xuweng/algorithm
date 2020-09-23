1593445b1459523410ef56a6031e8dfe3dbfef35
xuweng <2028403305@qq.com>
2020-09-23 09:58:11 +0800

    230. 二叉搜索树中第K小的元素
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/three/KthSmallest.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/three/KthSmallest.java
@@ -1,5 +1,8 @@
 package com.leetcode.tag.tree.three;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * 230. 二叉搜索树中第K小的元素
  * <p>
@@ -30,6 +33,61 @@ public class KthSmallest {
             zhong(root.right, k);
         }
 
+    }
+
+    /**
+     * 方法一：递归
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/solution/er-cha-sou-suo-shu-zhong-di-kxiao-de-yuan-su-by-le/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public ArrayList<Integer> inorder(TreeNode root, ArrayList<Integer> arr) {
+            if (root == null) {
+                return arr;
+            }
+            inorder(root.left, arr);
+            arr.add(root.val);
+            inorder(root.right, arr);
+            return arr;
+        }
+
+        public int kthSmallest(TreeNode root, int k) {
+            ArrayList<Integer> nums = inorder(root, new ArrayList<>());
+            //第 k-1 个元素就是第 k 小的元素
+            return nums.get(k - 1);
+        }
+    }
+
+    /**
+     * 方法二：迭代
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/solution/er-cha-sou-suo-shu-zhong-di-kxiao-de-yuan-su-by-le/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public int kthSmallest(TreeNode root, int k) {
+            LinkedList<TreeNode> stack = new LinkedList<>();
+
+            while (true) {
+                //left
+                while (root != null) {
+                    stack.add(root);
+                    root = root.left;
+                }
+                //root
+                root = stack.removeLast();
+                if (--k == 0) {
+                    return root.val;
+                }
+                //right
+                root = root.right;
+            }
+        }
     }
 
     class TreeNode {
