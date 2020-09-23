079b2c6d2e6e02e0bf52e581885d24b5495d0667
xuweng <2028403305@qq.com>
2020-09-23 23:13:21 +0800

    方法：递归
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/three/TrimBST.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/three/TrimBST.java
@@ -8,6 +8,10 @@ package com.leetcode.tag.tree.three;
  * 递归没有那么清晰.
  * <p>
  * 十分钟没思路看题解.
+ * <p>
+ * 递归真的是看起来想起来难啊
+ * <p>
+ * 感觉递归就得靠宏观逻辑，抠细节，哼哼，你就输了
  */
 public class TrimBST {
     class Solution {
@@ -23,6 +27,32 @@ public class TrimBST {
                 root.right = trimBST(root.right, low, high);
                 root.left = null;
             }
+            return root;
+        }
+    }
+
+    /**
+     * 方法：递归
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/trim-a-binary-search-tree/solution/xiu-jian-er-cha-sou-suo-shu-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public TreeNode trimBST(TreeNode root, int L, int R) {
+            if (root == null) {
+                return root;
+            }
+            if (root.val > R) {
+                return trimBST(root.left, L, R);
+            }
+            if (root.val < L) {
+                return trimBST(root.right, L, R);
+            }
+
+            root.left = trimBST(root.left, L, R);
+            root.right = trimBST(root.right, L, R);
             return root;
         }
     }
