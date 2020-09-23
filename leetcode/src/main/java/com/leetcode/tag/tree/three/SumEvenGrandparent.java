1aac3450d0930766805ddf6eb1eb09382479acd2
xuweng <2028403305@qq.com>
2020-09-23 10:37:33 +0800

    先序遍历
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/three/SumEvenGrandparent.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/three/SumEvenGrandparent.java
@@ -42,6 +42,38 @@ public class SumEvenGrandparent {
 
     }
 
+    /**
+     * 先序遍历
+     */
+    class Solution1 {
+        public int sumEvenGrandparent(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            int res = 0;
+            if (root.val % 2 == 0) {
+                if (root.left != null) {
+                    if (root.left.left != null) {
+                        res += root.left.left.val;
+                    }
+                    if (root.left.right != null) {
+                        res += root.left.right.val;
+                    }
+
+                }
+                if (root.right != null) {
+                    if (root.right.left != null) {
+                        res += root.right.left.val;
+                    }
+                    if (root.right.right != null) {
+                        res += root.right.right.val;
+                    }
+                }
+            }
+            return res + sumEvenGrandparent(root.left) + sumEvenGrandparent(root.right);
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
