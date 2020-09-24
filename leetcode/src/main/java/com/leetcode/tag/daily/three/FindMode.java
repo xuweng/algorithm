7ea8e7e912b7ca7e64942c700497289346713e00
xuweng <2028403305@qq.com>
2020-09-24 10:57:24 +0800

    501. 二叉搜索树中的众数
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/FindMode.java
@@ -0,0 +1,46 @@
+package com.leetcode.tag.daily.three;
+
+import java.util.*;
+
+/**
+ * 501. 二叉搜索树中的众数
+ */
+public class FindMode {
+    class Solution {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        public int[] findMode(TreeNode root) {
+            zhong(root);
+
+            Integer integer = map.values().stream().max(Integer::compareTo).orElse(0);
+
+            List<Integer> result = new ArrayList<>();
+            map.forEach((k, v) -> {
+                if (Objects.equals(v, integer)) {
+                    result.add(k);
+                }
+            });
+
+            return result.stream().mapToInt(item -> item).toArray();
+        }
+
+        private void zhong(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+            zhong(root.left);
+            map.put(root.val, map.getOrDefault(root.val, 0) + 1);
+            zhong(root.right);
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
