e8a0a4bbae4918bfa20c01d77788c5dca030cae9
xuweng <2028403305@qq.com>
2020-09-25 11:08:36 +0800

    保存root的下标
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/BuildTree.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/BuildTree.java
@@ -34,6 +34,58 @@ public class BuildTree {
         }
     }
 
+    /**
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/solution/cong-zhong-xu-yu-hou-xu-bian-li-xu-lie-gou-zao-14/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        //保存root的下标
+        int post_idx;
+        int[] postorder;
+        int[] inorder;
+        Map<Integer, Integer> idx_map = new HashMap<Integer, Integer>();
+
+        public TreeNode helper(int in_left, int in_right) {
+            // 如果这里没有节点构造二叉树了，就结束
+            if (in_left > in_right) {
+                return null;
+            }
+
+            // 选择 post_idx 位置的元素作为当前子树根节点
+            int root_val = postorder[post_idx];
+            TreeNode root = new TreeNode(root_val);
+
+            // 根据 root 所在位置分成左右两棵子树
+            int index = idx_map.get(root_val);
+
+            // 下标减一
+            post_idx--;
+            // 注意这里有需要先创建右子树，再创建左子树的依赖关系
+            // 构造右子树
+            root.right = helper(index + 1, in_right);
+            // 构造左子树
+            root.left = helper(in_left, index - 1);
+            return root;
+        }
+
+        public TreeNode buildTree(int[] inorder, int[] postorder) {
+            this.postorder = postorder;
+            this.inorder = inorder;
+            // 从后序遍历的最后一个元素开始
+            post_idx = postorder.length - 1;
+
+            // 建立（元素，下标）键值对的哈希表
+            int idx = 0;
+            for (Integer val : inorder) {
+                idx_map.put(val, idx++);
+            }
+
+            return helper(0, inorder.length - 1);
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
