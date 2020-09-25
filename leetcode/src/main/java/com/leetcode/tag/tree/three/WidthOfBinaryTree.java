7e439297a0918e33d09608f8e9b8b703db756bfc
xuweng <2028403305@qq.com>
2020-09-25 18:04:23 +0800

    662. 二叉树最大宽度
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/three/WidthOfBinaryTree.java
@@ -0,0 +1,55 @@
+package com.leetcode.tag.tree.three;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 662. 二叉树最大宽度
+ */
+public class WidthOfBinaryTree {
+    class Solution {
+        List<List<Integer>> lists = new ArrayList<>();
+
+        public int widthOfBinaryTree(TreeNode root) {
+            pre(root, 0);
+            int result = 0;
+            for (List<Integer> list : lists) {
+                result = Math.max(result, list.size());
+            }
+            return result;
+
+        }
+
+        private void pre(TreeNode root, int level) {
+            if (root == null) {
+                lists.get(level).add(null);
+                return;
+            }
+            if (lists.size() == level) {
+                lists.add(new ArrayList<>());
+            }
+            lists.get(level).add(root.val);
+            pre(root.left, level + 1);
+            pre(root.right, level + 1);
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
