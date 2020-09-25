532079b74b81f04edf06f47e1cd7d1782c65c73f
xuweng <2028403305@qq.com>
2020-09-25 22:51:54 +0800

    bfs
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/three/Connect.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/three/Connect.java
@@ -1,7 +1,9 @@
 package com.leetcode.tag.tree.three;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * 116. 填充每个节点的下一个右侧节点指针
@@ -36,6 +38,44 @@ public class Connect {
             lists.get(level).add(root);
             level(root.left, level + 1);
             level(root.right, level + 1);
+        }
+    }
+
+    /**
+     * bfs
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/solution/tian-chong-mei-ge-jie-dian-de-xia-yi-ge-you-ce-j-3/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public Node connect(Node root) {
+            if (root == null) {
+                return root;
+            }
+
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    Node node = queue.poll();
+                    if (i < size - 1) {
+                        //队头赋值.厉害.
+                        node.next = queue.peek();
+                    }
+
+                    //保存非null.先left后right.
+                    if (node.left != null) {
+                        queue.add(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.add(node.right);
+                    }
+                }
+            }
+            return root;
         }
     }
 
