a658b823bd004e4a5c516294ac1fbfabd251a2d6
xuweng <2028403305@qq.com>
2020-09-25 11:13:51 +0800

    下标减一.肯定先构造right.
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/BuildTree.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/BuildTree.java
@@ -86,6 +86,39 @@ public class BuildTree {
         }
     }
 
+    /**
+     *
+     */
+    class Solution2 {
+        //中序和倒序序列的索引
+        int in, post;
+
+        public TreeNode buildTree(int[] inorder, int[] postorder) {
+            in = inorder.length - 1;
+            post = postorder.length - 1;
+
+            return recursive(inorder, postorder, Integer.MAX_VALUE);
+        }
+
+        public TreeNode recursive(int[] inorder, int[] postorder, int stop) {
+            if (post < 0) {
+                return null;
+            }
+            if (inorder[in] == stop) {
+                in--;
+                return null;
+            }
+            int curVal = postorder[post--];
+            TreeNode cur = new TreeNode(curVal);
+            //注意顺序，倒着的后序遍历是先走右节点的先序遍历
+            cur.right = recursive(inorder, postorder, curVal);
+            //当先序遍历（倒）开始返回时，中序遍历（倒）开始记录节点，也就是该节点没有右节点
+            cur.left = recursive(inorder, postorder, stop);
+            //当中序遍历(倒)下一个节点为该节点父节点时，该节点没有左节点
+            return cur;
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
