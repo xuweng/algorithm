dcf62d76d776631a8cefe20319d2fd0eb1eadd89
xuweng <2028403305@qq.com>
2020-09-25 11:35:20 +0800

    958. 二叉树的完全性检验
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/three/IsCompleteTree.java
@@ -0,0 +1,56 @@
+package com.leetcode.tag.tree.three;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 958. 二叉树的完全性检验
+ */
+public class IsCompleteTree {
+    /**
+     * 树中所有节点的编号按照广度优先搜索顺序正好是升序
+     * <p>
+     * 们检测编号序列是否为无间隔的.检查最后一个编号是否正确，因为最后一个编号的值最大
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/check-completeness-of-a-binary-tree/solution/er-cha-shu-de-wan-quan-xing-jian-yan-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution {
+        public boolean isCompleteTree(TreeNode root) {
+            List<ANode> nodes = new ArrayList<>();
+            nodes.add(new ANode(root, 1));
+            int i = 0;
+            while (i < nodes.size()) {
+                ANode anode = nodes.get(i++);
+                if (anode.node != null) {
+                    nodes.add(new ANode(anode.node.left, anode.code * 2));
+                    nodes.add(new ANode(anode.node.right, anode.code * 2 + 1));
+                }
+            }
+
+            return nodes.get(i - 1).code == nodes.size();
+        }
+    }
+
+    class ANode {  // Annotated Node
+        TreeNode node;
+        int code;
+
+        ANode(TreeNode node, int code) {
+            this.node = node;
+            this.code = code;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
