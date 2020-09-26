54c500ae55b686fe7396d19fca410dfd5c2ba6a8
xuweng <2028403305@qq.com>
2020-09-26 16:35:24 +0800

    834. 树中距离之和
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/three/SumOfDistancesInTree.java
@@ -0,0 +1,59 @@
+package com.leetcode.tag.tree.three;
+
+import java.util.*;
+
+/**
+ * 834. 树中距离之和
+ */
+public class SumOfDistancesInTree {
+    /**
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/sum-of-distances-in-tree/solution/shu-zhong-ju-chi-zhi-he-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution {
+        int[] ans, count;
+        //下标表示结点.set表示所有边.
+        List<Set<Integer>> graph;
+        int N;
+
+        public int[] sumOfDistancesInTree(int N, int[][] edges) {
+            this.N = N;
+            graph = new ArrayList<>();
+            ans = new int[N];
+            count = new int[N];
+            Arrays.fill(count, 1);
+
+            for (int i = 0; i < N; ++i) {
+                graph.add(new HashSet<>());
+            }
+            for (int[] edge : edges) {
+                graph.get(edge[0]).add(edge[1]);
+                graph.get(edge[1]).add(edge[0]);
+            }
+            dfs(0, -1);
+            dfs2(0, -1);
+            return ans;
+        }
+
+        public void dfs(int node, int parent) {
+            for (int child : graph.get(node)) {
+                if (child != parent) {
+                    dfs(child, node);
+                    count[node] += count[child];
+                    ans[node] += ans[child] + count[child];
+                }
+            }
+        }
+
+        public void dfs2(int node, int parent) {
+            for (int child : graph.get(node)) {
+                if (child != parent) {
+                    ans[child] = ans[node] - count[child] + N - count[child];
+                    dfs2(child, node);
+                }
+            }
+        }
+    }
+}
