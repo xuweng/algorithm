64f2ea05ae05066ee0b737ed8ee6d49073ce18ea
xuweng <2028403305@qq.com>
2020-09-26 17:09:24 +0800

    面试题 04.10. 检查子树
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/three/CheckSubTree.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/three/CheckSubTree.java
@@ -20,6 +20,33 @@ public class CheckSubTree {
         }
     }
 
+    /**
+     *
+     */
+    class Solution1 {
+        public boolean checkSubTree(TreeNode t1, TreeNode t2) {
+            return (t1 != null && t2 != null) &&
+                    (recur(t1, t2) || checkSubTree(t1.right, t2) || checkSubTree(t1.left, t2));
+        }
+
+        /**
+         * t1和t2相等
+         *
+         * @param t1
+         * @param t2
+         * @return
+         */
+        private boolean recur(TreeNode t1, TreeNode t2) {
+            if (t2 == null) {
+                return true;
+            }
+            if (t1 == null || t1.val != t2.val) {
+                return false;
+            }
+            return recur(t1.left, t2.left) && recur(t2.right, t2.right);
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
