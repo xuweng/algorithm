354c06665c75698483889eadcd852f56ff7fb7cc
xuweng <2028403305@qq.com>
2020-09-26 11:41:47 +0800

    113. 路径总和 II
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/PathSum.java
@@ -0,0 +1,49 @@
+package com.leetcode.tag.daily.three;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 113. 路径总和 II
+ */
+public class PathSum {
+    class Solution {
+        public List<List<Integer>> pathSum(TreeNode root, int sum) {
+            List<List<Integer>> result = new ArrayList<>();
+            Deque<Integer> stack = new LinkedList<>();
+
+            pre(root, sum, result, stack);
+
+            return result;
+        }
+
+        private void pre(TreeNode root, int sum, List<List<Integer>> result, Deque<Integer> stack) {
+            if (root == null || root.val > sum) {
+                return;
+            }
+            if (sum == 0) {
+                result.add(new ArrayList<>(stack));
+                return;
+            }
+            stack.push(root.val);
+            pre(root.left, sum - root.val, result, stack);
+            stack.pop();
+
+            stack.push(root.val);
+            pre(root.right, sum - root.val, result, stack);
+            stack.pop();
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
