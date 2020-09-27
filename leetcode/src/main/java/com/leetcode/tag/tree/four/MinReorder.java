c0e4fa1671f7726503a1e5714321e986f93cfbd2
xuweng <2028403305@qq.com>
2020-09-27 21:41:41 +0800

    1466. 重新规划路线
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/four/MinReorder.java
@@ -0,0 +1,46 @@
+package com.leetcode.tag.tree.four;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 1466. 重新规划路线
+ * <p>
+ * 十分钟看答案
+ */
+public class MinReorder {
+    class Solution {
+        public int minReorder(int n, int[][] connections) {
+            List<List<Integer>> tree = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                tree.add(new ArrayList<>());
+            }
+            for (int i = 0; i < connections.length; i++) {
+                tree.get(connections[i][0]).add(i);
+                tree.get(connections[i][1]).add(i);
+            }
+            boolean[] visited = new boolean[connections.length];
+            Queue<Integer> queue = new LinkedList<>();
+            queue.offer(0);
+            int ans = 0;
+            while (!queue.isEmpty()) {
+                int node = queue.poll();
+                List<Integer> connection = tree.get(node);
+                for (Integer idx : connection) {
+                    if (visited[idx]) {
+                        continue;
+                    }
+                    visited[idx] = true;
+                    int a = connections[idx][0];
+                    int b = connections[idx][1];
+                    ans += a == node ? 1 : 0;
+                    queue.offer(a == node ? b : a);
+                }
+            }
+
+            return ans;
+        }
+    }
+}
