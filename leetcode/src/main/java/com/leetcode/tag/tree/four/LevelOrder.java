7139129f92a1795fc23eb4f82a48419f1424a8bc
xuweng <2028403305@qq.com>
2020-09-27 10:28:22 +0800

    bsf
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/four/LevelOrder.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/four/LevelOrder.java
@@ -1,7 +1,9 @@
 package com.leetcode.tag.tree.four;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * 剑指 Offer 32 - I. 从上到下打印二叉树
@@ -30,6 +32,40 @@ public class LevelOrder {
             result.get(level).add(root.val);
             levelOrder(root.left, level + 1);
             levelOrder(root.right, level + 1);
+        }
+    }
+
+    class Solution1 {
+        List<List<Integer>> result = new ArrayList<>();
+
+        public int[] levelOrder(TreeNode root) {
+            bfs(root);
+
+            List<Integer> list = new ArrayList<>();
+            for (List<Integer> l : result) {
+                list.addAll(l);
+            }
+            return list.stream().mapToInt(Integer::intValue).toArray();
+        }
+
+        private void bfs(TreeNode root) {
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+
+            while (!queue.isEmpty()) {
+                List<Integer> temp = new ArrayList<>();
+                for (int i = queue.size(); i >= 0; i--) {
+                    TreeNode node = queue.poll();
+                    temp.add(node.val);
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+                result.add(temp);
+            }
         }
     }
 
