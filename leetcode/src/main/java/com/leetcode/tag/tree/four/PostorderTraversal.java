67f72e3166e525149024c10fa2f9c5332b50781c
xuweng <2028403305@qq.com>
2020-09-27 22:54:41 +0800

    pre.in.post bfs+逆序
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/four/PostorderTraversal.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/four/PostorderTraversal.java
@@ -1,12 +1,15 @@
 package com.leetcode.tag.tree.four;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * 145. 二叉树的后序遍历.
  * <p>
  * 变成一颗tree后用bsf.
+ * <p>
+ * pre.in.post
  */
 public class PostorderTraversal {
     class Solution {
@@ -25,6 +28,36 @@ public class PostorderTraversal {
             post(root.left);
             post(root.right);
             result.add(root.val);
+        }
+    }
+
+    /**
+     * bfs+逆序
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/binary-tree-postorder-traversal/solution/er-cha-shu-de-hou-xu-bian-li-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public List<Integer> postorderTraversal(TreeNode root) {
+            LinkedList<Integer> output = new LinkedList<>();
+            if (root == null) {
+                return output;
+            }
+            LinkedList<TreeNode> stack = new LinkedList<>();
+            stack.add(root);
+            while (!stack.isEmpty()) {
+                TreeNode node = stack.pollLast();
+                output.addFirst(node.val);
+                if (node.left != null) {
+                    stack.add(node.left);
+                }
+                if (node.right != null) {
+                    stack.add(node.right);
+                }
+            }
+            return output;
         }
     }
 
