8eda4fedb7dab4c6df9ae8eb83ea81884b79c343
xuweng <2028403305@qq.com>
2020-09-28 23:37:20 +0800

    623. 在二叉树中增加一行
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/four/AddOneRow.java
@@ -0,0 +1,22 @@
+package com.leetcode.tag.tree.four;
+
+/**
+ * 623. 在二叉树中增加一行
+ */
+public class AddOneRow {
+    class Solution {
+        public TreeNode addOneRow(TreeNode root, int v, int d) {
+            return null;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
