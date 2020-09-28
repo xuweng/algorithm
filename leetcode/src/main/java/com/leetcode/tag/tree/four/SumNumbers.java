d31ef318c7c6b60af27af2c548971c27da44a6c5
xuweng <2028403305@qq.com>
2020-09-28 23:31:15 +0800

    这个计算厉害
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/four/SumNumbers.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/four/SumNumbers.java
@@ -27,6 +27,30 @@ public class SumNumbers {
         }
     }
 
+    /**
+     * 作者：liuzhaoce
+     * 链接：https://leetcode-cn.com/problems/sum-root-to-leaf-numbers/solution/0-ms-jiao-ke-shu-ji-jie-da-by-liuzhaoce/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int sumNumbers(TreeNode root) {
+            return helper(root, 0);
+        }
+
+        public int helper(TreeNode root, int i) {
+            if (root == null) {
+                return 0;
+            }
+            //这个计算厉害
+            int temp = i * 10 + root.val;
+            if (root.left == null && root.right == null) {
+                return temp;
+            }
+            return helper(root.left, temp) + helper(root.right, temp);
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
