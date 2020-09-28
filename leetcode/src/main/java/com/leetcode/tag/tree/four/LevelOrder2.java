0463cd60ff5e20a3bf64e372a9219057ffbb43e7
xuweng <2028403305@qq.com>
2020-09-28 11:55:24 +0800

    bfs
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/four/LevelOrder2.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/four/LevelOrder2.java
@@ -1,7 +1,9 @@
 package com.leetcode.tag.tree.four;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * 102. 二叉树的层序遍历
@@ -26,6 +28,31 @@ public class LevelOrder2 {
             result.get(level).add(root.val);
             pre(root.left, level + 1);
             pre(root.right, level + 1);
+        }
+    }
+
+    class Solution1 {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> result = new ArrayList<>();
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+
+            while (!queue.isEmpty()) {
+                List<Integer> temp = new ArrayList<>();
+                for (int i = queue.size(); i > 0; i--) {
+                    TreeNode node = queue.poll();
+                    temp.add(node.val);
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+                result.add(temp);
+            }
+
+            return result;
         }
     }
 
