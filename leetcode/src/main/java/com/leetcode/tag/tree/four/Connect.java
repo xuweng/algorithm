6c24effd937c4a6c75d2d99f3131167e827a93cb
xuweng <2028403305@qq.com>
2020-09-28 09:30:49 +0800

    117. 填充每个节点的下一个右侧节点指针 II
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/four/Connect.java
@@ -0,0 +1,50 @@
+package com.leetcode.tag.tree.four;
+
+/**
+ * 117. 填充每个节点的下一个右侧节点指针 II
+ */
+public class Connect {
+    class Solution {
+        public Node connect(Node root) {
+            if (root == null) {
+                return root;
+            }
+            if (root.left != null) {
+                root.left.next = root.right;
+            }
+            if (root.right != null && root.next != null) {
+                if (root.next.left != null) {
+                    root.right.next = root.next.left;
+                } else {
+                    root.right.next = root.next.right;
+                }
+            }
+            connect(root.left);
+            connect(root.right);
+
+            return root;
+
+        }
+    }
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+}
