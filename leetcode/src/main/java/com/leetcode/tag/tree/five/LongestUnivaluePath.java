e64ac1e9cd404c07e185715f5d5916f55f978f09
xuweng <2028403305@qq.com>
2020-09-29 16:49:54 +0800

    后序遍历
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/five/LongestUnivaluePath.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/five/LongestUnivaluePath.java
@@ -2,6 +2,8 @@ package com.leetcode.tag.tree.five;
 
 /**
  * 687. 最长同值路径
+ * <p>
+ * 十分钟看答案.十分钟看答案.
  */
 public class LongestUnivaluePath {
     /**
@@ -31,6 +33,43 @@ public class LongestUnivaluePath {
                 count = 0;
             }
             result = Math.max(result, count);
+        }
+    }
+
+    /**
+     * 后序遍历.类似之前的.
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/longest-univalue-path/solution/zui-chang-tong-zhi-lu-jing-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        int ans;
+
+        public int longestUnivaluePath(TreeNode root) {
+            ans = 0;
+            arrowLength(root);
+            return ans;
+        }
+
+        public int arrowLength(TreeNode node) {
+            if (node == null) {
+                return 0;
+            }
+            int left = arrowLength(node.left);
+            int right = arrowLength(node.right);
+            int arrowLeft = 0, arrowRight = 0;
+            if (node.left != null && node.left.val == node.val) {
+                arrowLeft += left + 1;
+            }
+            if (node.right != null && node.right.val == node.val) {
+                arrowRight += right + 1;
+            }
+            //答案是这个
+            ans = Math.max(ans, arrowLeft + arrowRight);
+            //返回子树最大
+            return Math.max(arrowLeft, arrowRight);
         }
     }
 
