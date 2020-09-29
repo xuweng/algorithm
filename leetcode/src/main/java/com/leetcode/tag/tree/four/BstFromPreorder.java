ee27ff4382c82d6602568ce23c20f7bca4d53c60
xuweng <2028403305@qq.com>
2020-09-29 12:14:45 +0800

    查找right的下标.如果没有right.right为null.
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/four/BstFromPreorder.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/four/BstFromPreorder.java
@@ -23,7 +23,8 @@ public class BstFromPreorder {
             if (low == high) {
                 return new TreeNode(preorder[low]);
             }
-            int index = 0;
+            //查找right的下标.如果没有right.right为null.
+            int index = high + 1;
             for (int i = low + 1; i <= high; i++) {
                 if (preorder[i] > preorder[low]) {
                     index = i;
