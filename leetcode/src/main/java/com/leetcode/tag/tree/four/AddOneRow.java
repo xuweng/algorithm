954cd9f3cb452668ad2b8a7c9e932ff8dee743e2
xuweng <2028403305@qq.com>
2020-09-29 10:51:51 +0800

    bfs
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/four/AddOneRow.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/four/AddOneRow.java
@@ -1,5 +1,8 @@
 package com.leetcode.tag.tree.four;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 623. 在二叉树中增加一行
  */
@@ -32,10 +35,56 @@ public class AddOneRow {
                 root.right = new TreeNode(v);
                 root.left.left = left;
                 root.right.right = right;
+
+                //加return需要的时间比不加需要的时间更多.
+                return;
             }
 
             pre(root.left, v, d, depth + 1);
             pre(root.right, v, d, depth + 1);
+        }
+    }
+
+    /**
+     * bfs
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/add-one-row-to-tree/solution/zai-er-cha-shu-zhong-zeng-jia-yi-xing-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public TreeNode addOneRow(TreeNode t, int v, int d) {
+            if (d == 1) {
+                TreeNode n = new TreeNode(v);
+                n.left = t;
+                return n;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(t);
+            int depth = 1;
+            while (!queue.isEmpty() && depth < d - 1) {
+                for (int i = queue.size(); i > 0; i--) {
+                    TreeNode node = queue.poll();
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+                depth++;
+            }
+            while (!queue.isEmpty()) {
+                TreeNode node = queue.remove();
+                TreeNode temp = node.left;
+                node.left = new TreeNode(v);
+                node.left.left = temp;
+                temp = node.right;
+                node.right = new TreeNode(v);
+                node.right.right = temp;
+            }
+            return t;
         }
     }
 
