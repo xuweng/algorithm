00aa8f55e994297b2dae8a185d4dd78009531b36
xuweng <2028403305@qq.com>
2020-09-29 18:31:38 +0800

    bst.天然剪枝.
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/five/InorderSuccessor.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/five/InorderSuccessor.java
@@ -42,6 +42,19 @@ public class InorderSuccessor {
 
     }
 
+    class S {
+        public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+            if (root == null) {
+                return null;
+            }
+            if (p.val >= root.val) {
+                return inorderSuccessor(root.right, p);
+            }
+            TreeNode left = inorderSuccessor(root.left, p);
+            return left == null ? root : left;
+        }
+    }
+
     /**
      *
      */
