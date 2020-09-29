485e6bea14086ce8d26e250ca06b1e01c73510e6
xuweng <2028403305@qq.com>
2020-09-29 16:53:22 +0800

    厉害
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/five/LongestUnivaluePath.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/five/LongestUnivaluePath.java
@@ -60,11 +60,12 @@ public class LongestUnivaluePath {
             int left = arrowLength(node.left);
             int right = arrowLength(node.right);
             int arrowLeft = 0, arrowRight = 0;
+            //这个厉害
             if (node.left != null && node.left.val == node.val) {
-                arrowLeft += left + 1;
+                arrowLeft = left + 1;
             }
             if (node.right != null && node.right.val == node.val) {
-                arrowRight += right + 1;
+                arrowRight = right + 1;
             }
             //答案是这个
             ans = Math.max(ans, arrowLeft + arrowRight);
