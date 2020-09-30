f10d6b0bd400e0bc8cf699ee2f677c3898b959fe
xuweng <2028403305@qq.com>
2020-09-30 16:13:44 +0800

    106. 从中序与后序遍历序列构造二叉树
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/five/BuildTree.java
@@ -0,0 +1,43 @@
+package com.leetcode.tag.tree.five;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 106. 从中序与后序遍历序列构造二叉树
+ */
+public class BuildTree {
+    class Solution {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        public TreeNode buildTree(int[] inorder, int[] postorder) {
+            if (inorder == null || inorder.length == 0 || postorder == null || postorder.length == 0) {
+                return null;
+            }
+            return buildTree(inorder, 0, inorder.length, postorder, 0, postorder.length);
+        }
+
+        public TreeNode buildTree(int[] inorder, int inorderStart, int inorderEnd, int[] postorder, int postorderStart, int postorderEnd) {
+            if (inorderStart > inorderEnd || postorderStart > postorderEnd) {
+                return null;
+            }
+
+            TreeNode root = new TreeNode(postorder[postorderEnd]);
+            int rootIndex = map.get(postorder[postorderEnd]);
+            root.left = buildTree(inorder, inorderStart, rootIndex - 1, postorder, postorderStart, postorderStart + rootIndex - inorderStart - 1);
+            root.right = buildTree(inorder, rootIndex + 1, inorderEnd, postorder, postorderStart + rootIndex - inorderStart, postorderEnd - 1);
+
+            return root;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
