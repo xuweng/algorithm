9a09fd398bb1d8d0db8991a63c8fa43bd7141720
xuweng <2028403305@qq.com>
2020-09-30 12:24:02 +0800

    DFS 细分为先序遍历 preorder，中序遍历 inorder 和后序遍历 postorder。 DFS 遍历通常有三种实现方式：递归 recursion，迭代 iteration，Morris，其中最简单的是递归。
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/five/MaxLevelSum.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/five/MaxLevelSum.java
@@ -8,6 +8,15 @@ import java.util.concurrent.atomic.AtomicInteger;
  * 1161. 最大层内元素和
  * <p>
  * 十分钟看答案.十分钟看答案.
+ * <p>
+ * DFS 细分为先序遍历 preorder，中序遍历 inorder 和后序遍历 postorder。
+ * <p>
+ * DFS 遍历通常有三种实现方式：递归 recursion，迭代 iteration，Morris，其中最简单的是递归。
+ * <p>
+ * 作者：LeetCode
+ * 链接：https://leetcode-cn.com/problems/maximum-level-sum-of-a-binary-tree/solution/zui-da-ceng-nei-yuan-su-he-by-leetcode/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
  */
 public class MaxLevelSum {
     class Solution {
@@ -39,6 +48,36 @@ public class MaxLevelSum {
             }
             pre(root.left, depth + 1);
             pre(root.right, depth + 1);
+        }
+    }
+
+    /**
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/maximum-level-sum-of-a-binary-tree/solution/zui-da-ceng-nei-yuan-su-he-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        int n = 10000;
+        int[] levelSum = new int[n];
+
+        public void inorder(TreeNode node, int level) {
+            if (node == null) {
+                return;
+            }
+            inorder(node.left, level + 1);
+            levelSum[level] += node.val;
+            inorder(node.right, level + 1);
+        }
+
+        public int maxLevelSum(TreeNode root) {
+            inorder(root, 1);
+
+            int maxIdx = 0;
+            for (int i = 0; i < n; ++i) {
+                maxIdx = levelSum[i] > levelSum[maxIdx] ? i : maxIdx;
+            }
+            return maxIdx;
         }
     }
 
