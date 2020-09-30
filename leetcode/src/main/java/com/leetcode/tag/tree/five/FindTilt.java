6ee3c3dbc1874e7a947ca89515e59b44d587b5fe
xuweng <2028403305@qq.com>
2020-09-30 23:41:59 +0800

    563. 二叉树的坡度
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/five/FindTilt.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/five/FindTilt.java
@@ -23,6 +23,31 @@ public class FindTilt {
         }
     }
 
+    /**
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/binary-tree-tilt/solution/er-cha-shu-de-po-du-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        int tilt = 0;
+
+        public int findTilt(TreeNode root) {
+            traverse(root);
+            return tilt;
+        }
+
+        public int traverse(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            int left = traverse(root.left);
+            int right = traverse(root.right);
+            tilt += Math.abs(left - right);
+            return left + right + root.val;
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
