e53a8873011638b9e8e62d0126a554215ad31157
xuweng <2028403305@qq.com>
2020-09-30 17:17:53 +0800

    叶子结点
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/five/PathSum.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/five/PathSum.java
@@ -1,9 +1,6 @@
 package com.leetcode.tag.tree.five;
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 剑指 Offer 34. 二叉树中和为某一值的路径
@@ -26,7 +23,10 @@ public class PathSum {
             stack.push(root.val);
             //叶子结点
             if (root.left == null && root.right == null && sum == root.val) {
-                result.add(new ArrayList<>(stack));
+                List<Integer> list = new ArrayList<>(stack);
+                Collections.reverse(list);
+
+                result.add(list);
             }
             back(root.left, sum - root.val, result, stack);
             back(root.right, sum - root.val, result, stack);
