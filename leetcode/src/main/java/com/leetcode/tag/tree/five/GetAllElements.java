3cbbd653c5ccca520a0c98c2810f498fa9ea05dc
xuweng <2028403305@qq.com>
2020-09-30 15:56:53 +0800

    方法三 遍历+优先队列
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/five/GetAllElements.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/five/GetAllElements.java
@@ -3,6 +3,7 @@ package com.leetcode.tag.tree.five;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * 1305. 两棵二叉搜索树中的所有元素
@@ -78,6 +79,44 @@ public class GetAllElements {
             dfs(root2);
 
             Collections.sort(ansList);
+            return ansList;
+        }
+
+    }
+
+    /**
+     * 方法三 遍历+优先队列
+     * <p>
+     * 在树遍历的时候用一个优先队列（默认小根堆）来添加元素；
+     * 然后，将优先队列的元素逐个取出到listlist中即可。
+     * <p>
+     * 作者：yuruiyin
+     * 链接：https://leetcode-cn.com/problems/all-elements-in-two-binary-search-trees/solution/java-san-chong-jie-fa-by-npe_tle/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class S1 {
+        private PriorityQueue<Integer> priorityQueue;
+
+        private void dfs(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+
+            priorityQueue.offer(root.val);
+            dfs(root.left);
+            dfs(root.right);
+        }
+
+        public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+            priorityQueue = new PriorityQueue<>();
+            dfs(root1);
+            dfs(root2);
+
+            List<Integer> ansList = new ArrayList<>();
+            while (!priorityQueue.isEmpty()) {
+                ansList.add(priorityQueue.poll());
+            }
             return ansList;
         }
 
