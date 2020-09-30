9861a6a0c3231017e932fde6cec576842923433b
xuweng <2028403305@qq.com>
2020-10-01 00:24:31 +0800

    简洁代码
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/five/MaxPathSum.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/five/MaxPathSum.java
@@ -29,6 +29,41 @@ public class MaxPathSum {
         }
     }
 
+    /**
+     * dfs
+     * <p>
+     * 简洁代码
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/binary-tree-maximum-path-sum/solution/er-cha-shu-zhong-de-zui-da-lu-jing-he-by-leetcode-/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        int maxSum = Integer.MIN_VALUE;
+
+        public int maxPathSum(TreeNode root) {
+            maxGain(root);
+            return maxSum;
+        }
+
+        public int maxGain(TreeNode node) {
+            if (node == null) {
+                return 0;
+            }
+
+            // 递归计算左右子节点的最大贡献值
+            // 只有在最大贡献值大于 0 时，才会选取对应子节点
+            int leftGain = Math.max(maxGain(node.left), 0);
+            int rightGain = Math.max(maxGain(node.right), 0);
+
+            maxSum = Math.max(maxSum, node.val + leftGain + rightGain);
+
+            // 返回节点的最大贡献值
+            return node.val + Math.max(leftGain, rightGain);
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
