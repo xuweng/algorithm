0ebccf407af9b16f33765795596a486f303cd09a
xuweng <2028403305@qq.com>
2020-10-01 22:53:15 +0800

    方法二：DFS 序列上做串匹配
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/five/IsSubtree.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/five/IsSubtree.java
@@ -1,5 +1,9 @@
 package com.leetcode.tag.tree.five;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 572. 另一个树的子树
  * <p>
@@ -55,7 +59,7 @@ public class IsSubtree {
         /**
          * 简洁代码.
          * <p>
-         * 简单的先序遍历
+         * 简单的先序遍历.厉害.
          *
          * @param s
          * @param t
@@ -73,6 +77,88 @@ public class IsSubtree {
                 return false;
             }
             return check(s.left, t.left) && check(s.right, t.right);
+        }
+    }
+
+    /**
+     * 方法二：DFS 序列上做串匹配
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/subtree-of-another-tree/solution/ling-yi-ge-shu-de-zi-shu-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        List<Integer> sOrder = new ArrayList<>();
+        List<Integer> tOrder = new ArrayList<>();
+        int maxElement, lNull, rNull;
+
+        public boolean isSubtree(TreeNode s, TreeNode t) {
+            maxElement = Integer.MIN_VALUE;
+            getMaxElement(s);
+            getMaxElement(t);
+            lNull = maxElement + 1;
+            rNull = maxElement + 2;
+
+            getDfsOrder(s, sOrder);
+            getDfsOrder(t, tOrder);
+
+            return kmp();
+        }
+
+        public void getMaxElement(TreeNode t) {
+            if (t == null) {
+                return;
+            }
+            maxElement = Math.max(maxElement, t.val);
+            getMaxElement(t.left);
+            getMaxElement(t.right);
+        }
+
+        public void getDfsOrder(TreeNode t, List<Integer> tar) {
+            if (t == null) {
+                return;
+            }
+            tar.add(t.val);
+            if (t.left != null) {
+                getDfsOrder(t.left, tar);
+            } else {
+                //两个null不一样
+                tar.add(lNull);
+            }
+            if (t.right != null) {
+                getDfsOrder(t.right, tar);
+            } else {
+                //两个null不一样
+                tar.add(rNull);
+            }
+        }
+
+        public boolean kmp() {
+            int sLen = sOrder.size(), tLen = tOrder.size();
+            int[] fail = new int[tOrder.size()];
+            Arrays.fill(fail, -1);
+            for (int i = 1, j = -1; i < tLen; ++i) {
+                while (j != -1 && !(tOrder.get(i).equals(tOrder.get(j + 1)))) {
+                    j = fail[j];
+                }
+                if (tOrder.get(i).equals(tOrder.get(j + 1))) {
+                    ++j;
+                }
+                fail[i] = j;
+            }
+            for (int i = 0, j = -1; i < sLen; ++i) {
+                while (j != -1 && !(sOrder.get(i).equals(tOrder.get(j + 1)))) {
+                    j = fail[j];
+                }
+                if (sOrder.get(i).equals(tOrder.get(j + 1))) {
+                    ++j;
+                }
+                if (j == tLen - 1) {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 
