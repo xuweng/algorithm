b7acc68827f45a35d9b0a058f28b5250f6ce8fa1
xuweng <2028403305@qq.com>
2020-10-01 13:06:08 +0800

    99. 恢复二叉搜索树
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/five/RecoverTree.java
@@ -0,0 +1,54 @@
+package com.leetcode.tag.tree.five;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 99. 恢复二叉搜索树
+ */
+public class RecoverTree {
+    class Solution {
+        List<TreeNode> list = new ArrayList<>();
+
+        public void recoverTree(TreeNode root) {
+            inorder(root);
+
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (list.get(i).val > list.get(i + 1).val) {
+                    int temp = list.get(i).val;
+                    list.get(i).val = list.get(i + 1).val;
+                    list.get(i + 1).val = temp;
+                    break;
+                }
+            }
+        }
+
+        private void inorder(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+            inorder(root.left);
+            list.add(root);
+            inorder(root.right);
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
