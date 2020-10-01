033a9f212f920caafe87d2e43b0b717f8b4acb18
xuweng <2028403305@qq.com>
2020-10-01 17:08:40 +0800

    这道题有点坑.难怪通过率这么低.
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/five/IsSubtree.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/five/IsSubtree.java
@@ -7,6 +7,8 @@ package com.leetcode.tag.tree.five;
  */
 public class IsSubtree {
     class Solution {
+        boolean result;
+
         public boolean isSubtree(TreeNode s, TreeNode t) {
             if (s == null) {
                 return t == null;
@@ -14,7 +16,30 @@ public class IsSubtree {
             if (t == null) {
                 return false;
             }
-            return (s.val == t.val && isSubtree(s.left, t.left) && isSubtree(s.right, t.right)) || (isSubtree(s.left, t)) || isSubtree(s.right, t);
+            preorder(s, t);
+
+            return result;
+        }
+
+        private void preorder(TreeNode s, TreeNode t) {
+            if (s == null || result) {
+                return;
+            }
+            if (s.val == t.val) {
+                result = myIsSubtree(s, t);
+            }
+            preorder(s.left, t);
+            preorder(s.right, t);
+        }
+
+        public boolean myIsSubtree(TreeNode s, TreeNode t) {
+            if (s == null) {
+                return t == null;
+            }
+            if (t == null || s.val != t.val) {
+                return false;
+            }
+            return myIsSubtree(s.left, t.left) && myIsSubtree(s.right, t.right);
         }
     }
 
