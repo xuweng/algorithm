6bac27265537f0b79115f8917d9ebcfcd8cca2b4
xuweng <2028403305@qq.com>
2020-10-01 23:14:15 +0800

    bfs
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/five/LevelOrder.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/five/LevelOrder.java
@@ -1,7 +1,9 @@
 package com.leetcode.tag.tree.five;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * 429. N叉树的层序遍历
@@ -27,6 +29,39 @@ public class LevelOrder {
             for (Node child : root.children) {
                 pre(child, level + 1);
             }
+        }
+    }
+
+    /**
+     * bfs
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/n-ary-tree-level-order-traversal/solution/ncha-shu-de-ceng-xu-bian-li-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public List<List<Integer>> levelOrder(Node root) {
+            List<List<Integer>> result = new ArrayList<>();
+            if (root == null) {
+                return result;
+            }
+            Queue<Node> queue = new LinkedList<>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                List<Integer> level = new ArrayList<>();
+                int size = queue.size();
+                for (int i = 0; i < size; i++) {
+                    Node node = queue.poll();
+                    level.add(node.val);
+                    if (node.children != null && !node.children.isEmpty()) {
+                        //addAll 厉害
+                        queue.addAll(node.children);
+                    }
+                }
+                result.add(level);
+            }
+            return result;
         }
     }
 
