412aed39d23e318af06c136f4d61a74dadd5a3de
xuweng <2028403305@qq.com>
2020-10-02 16:13:35 +0800

    不用翻转.改变遍历顺序也可以.
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/six/FlipMatchVoyage.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/six/FlipMatchVoyage.java
@@ -46,6 +46,56 @@ public class FlipMatchVoyage {
         }
     }
 
+    /**
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/flip-binary-tree-to-match-preorder-traversal/solution/fan-zhuan-er-cha-shu-yi-pi-pei-xian-xu-bian-li-by-/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        List<Integer> flipped;
+        int index;
+        int[] voyage;
+
+        public List<Integer> flipMatchVoyage(TreeNode root, int[] voyage) {
+            flipped = new ArrayList<>();
+            index = 0;
+            this.voyage = voyage;
+
+            dfs(root);
+            if (!flipped.isEmpty() && flipped.get(0) == -1) {
+                flipped.clear();
+                flipped.add(-1);
+            }
+
+            return flipped;
+        }
+
+        public void dfs(TreeNode node) {
+            if (node == null) {
+                return;
+            }
+            if (node.val != voyage[index++]) {
+                flipped.clear();
+                flipped.add(-1);
+                return;
+            }
+
+            if (index < voyage.length && node.left != null &&
+                    node.left.val != voyage[index]) {
+                flipped.add(node.val);
+                //不用翻转.改变遍历顺序也可以.
+                //right-left
+                dfs(node.right);
+                dfs(node.left);
+            } else {
+                //left-right
+                dfs(node.left);
+                dfs(node.right);
+            }
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
