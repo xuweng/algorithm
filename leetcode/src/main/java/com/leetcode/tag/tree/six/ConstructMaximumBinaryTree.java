998e9061a3be422316d875a0114fd5e54f82bfc2
xuweng <2028403305@qq.com>
2020-10-02 11:59:54 +0800

    654. 最大二叉树
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/six/ConstructMaximumBinaryTree.java
@@ -0,0 +1,44 @@
+package com.leetcode.tag.tree.six;
+
+/**
+ * 654. 最大二叉树
+ */
+public class ConstructMaximumBinaryTree {
+    class Solution {
+        public TreeNode constructMaximumBinaryTree(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return null;
+            }
+            return constructMaximumBinaryTree(nums, 0, nums.length - 1);
+        }
+
+        public TreeNode constructMaximumBinaryTree(int[] nums, int low, int high) {
+            if (low > high) {
+                return null;
+            }
+            int rootIndex = getMax(nums, low, high);
+            TreeNode root = new TreeNode(nums[rootIndex]);
+            root.left = constructMaximumBinaryTree(nums, low, rootIndex - 1);
+            root.right = constructMaximumBinaryTree(nums, rootIndex + 1, high);
+            return root;
+        }
+
+        private int getMax(int[] nums, int low, int high) {
+            int max = Integer.MIN_VALUE;
+            for (int i = low; i <= high; i++) {
+                max = Math.max(max, nums[i]);
+            }
+            return max;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
