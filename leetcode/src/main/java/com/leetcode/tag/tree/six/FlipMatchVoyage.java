00a8fbfa7784ab3a3682b407aad9bd234bfe1a23
xuweng <2028403305@qq.com>
2020-10-02 15:47:09 +0800

    971. 翻转二叉树以匹配先序遍历
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/six/FlipMatchVoyage.java
@@ -0,0 +1,55 @@
+package com.leetcode.tag.tree.six;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 971. 翻转二叉树以匹配先序遍历
+ */
+public class FlipMatchVoyage {
+    class Solution {
+        int rootIndex;
+        boolean flag;
+        List<Integer> result = new ArrayList<>();
+
+        public List<Integer> flipMatchVoyage(TreeNode root, int[] voyage) {
+            pre(root, null, voyage);
+
+            return flag ? new ArrayList<>() : result;
+        }
+
+        public void pre(TreeNode root, TreeNode parent, int[] voyage) {
+            if (root == null || flag) {
+                return;
+            }
+            if (root.val != voyage[rootIndex++]) {
+                flag = true;
+                return;
+            }
+            if (root.left != null) {
+                if (root.left.val != voyage[rootIndex]) {
+                    int temp = root.left.val;
+                    root.left.val = root.right.val;
+                    root.right.val = temp;
+
+                    result.add(parent.val);
+                }
+
+                pre(root.left, root, voyage);
+            }
+            if (root.right != null) {
+                pre(root.right, root, voyage);
+            }
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
