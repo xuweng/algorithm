daacff8ac7848123186d8ca3c0a49d7a56d367c1
xuweng <2028403305@qq.com>
2020-10-02 22:17:03 +0800

    数字字符串.保佑下标没有计算错误.
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/test/java/com/leetcode/tag/tree/six/RecoverFromPreorderTest.java
+++ leetcode/src/test/java/com/leetcode/tag/tree/six/RecoverFromPreorderTest.java
@@ -16,6 +16,28 @@ public class RecoverFromPreorderTest {
         System.out.println();
     }
 
+    @Test
+    public void recoverFromPreorderTest1() {
+        String S = "1-401--349---90--88";
+
+        RecoverFromPreorder.TreeNode treeNode = solution.recoverFromPreorder(S);
+
+        pre(treeNode);
+
+        System.out.println();
+    }
+
+    @Test
+    public void recoverFromPreorderTest2() {
+        String S = "1-2--3---4-5--6---7";
+
+        RecoverFromPreorder.TreeNode treeNode = solution.recoverFromPreorder(S);
+
+        pre(treeNode);
+
+        System.out.println();
+    }
+
     private void pre(RecoverFromPreorder.TreeNode root) {
         if (root == null) {
             return;
