f97cd006ef0f5bc8ea9278b35de060834bccc6de
xuweng <2028403305@qq.com>
2020-10-02 16:26:35 +0800

    bfs
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/tree/six/Connect.java
+++ leetcode/src/main/java/com/leetcode/tag/tree/six/Connect.java
@@ -1,7 +1,9 @@
 package com.leetcode.tag.tree.six;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 
 /**
  * 117. 填充每个节点的下一个右侧节点指针 II
@@ -26,6 +28,38 @@ public class Connect {
             map.put(level, root);
             pre(root.left, level + 1);
             pre(root.right, level + 1);
+        }
+    }
+
+    /**
+     * bfs
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii/solution/tian-chong-mei-ge-jie-dian-de-xia-yi-ge-you-ce-15/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public Node connect(Node root) {
+            if (root == null) {
+                return null;
+            }
+            Queue<Node> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                int n = queue.size();
+                for (int i = 1; i < n; ++i) {
+                    Node f = queue.poll();
+                    f.next = queue.peek();
+                    if (f.left != null) {
+                        queue.offer(f.left);
+                    }
+                    if (f.right != null) {
+                        queue.offer(f.right);
+                    }
+                }
+            }
+            return root;
         }
     }
 
