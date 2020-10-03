ca3818f638502026645d65cd002577c899ec4d3c
xuweng <2028403305@qq.com>
2020-10-03 22:16:59 +0800

    uf
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/one/AccountsMerge.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/AccountsMerge.java
@@ -9,6 +9,8 @@ public class AccountsMerge {
     /**
      * 方法一：深度优先搜索
      * <p>
+     * 问题归结为寻找图的连接组件
+     * <p>
      * 作者：LeetCode
      * 链接：https://leetcode-cn.com/problems/accounts-merge/solution/zhang-hu-he-bing-by-leetcode/
      * 来源：力扣（LeetCode）
@@ -28,6 +30,7 @@ public class AccountsMerge {
                         continue;
                     }
                     //这个函数厉害
+                    //无向图
                     graph.computeIfAbsent(email, x -> new ArrayList<>()).add(account.get(1));
                     graph.computeIfAbsent(account.get(1), x -> new ArrayList<>()).add(email);
                     emailToName.put(email, name);
@@ -60,4 +63,69 @@ public class AccountsMerge {
             return ans;
         }
     }
+
+    /**
+     * uf
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/accounts-merge/solution/zhang-hu-he-bing-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public List<List<String>> accountsMerge(List<List<String>> accounts) {
+            DSU dsu = new DSU();
+            Map<String, String> emailToName = new HashMap<>();
+            Map<String, Integer> emailtoid = new HashMap<>();
+            int id = 0;
+            for (List<String> account : accounts) {
+                String name = "";
+                for (String email : account) {
+                    if (Objects.equals(name, "")) {
+                        name = email;
+                        continue;
+                    }
+                    emailToName.put(email, name);
+                    if (!emailtoid.containsKey(email)) {
+                        emailtoid.put(email, id++);
+                    }
+                    dsu.union(emailtoid.get(account.get(1)), emailtoid.get(email));
+                }
+            }
+
+            Map<Integer, List<String>> ans = new HashMap<>();
+            for (String email : emailToName.keySet()) {
+                int index = dsu.find(emailtoid.get(email));
+                ans.computeIfAbsent(index, x -> new ArrayList<>()).add(email);
+            }
+            for (List<String> component : ans.values()) {
+                Collections.sort(component);
+                component.add(0, emailToName.get(component.get(0)));
+            }
+            return new ArrayList<>(ans.values());
+        }
+    }
+
+    class DSU {
+        int[] parent;
+
+        public DSU() {
+            parent = new int[10001];
+            for (int i = 0; i <= 10000; ++i) {
+                parent[i] = i;
+            }
+        }
+
+        public int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        public void union(int x, int y) {
+            parent[find(x)] = find(y);
+        }
+    }
+
 }
