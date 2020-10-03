bc088aff17ed9ae4b0f7686b2a05cf092f67b285
xuweng <2028403305@qq.com>
2020-10-03 10:54:29 +0800

    与运算
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/TwoSum.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/TwoSum.java
@@ -26,4 +26,68 @@ public class TwoSum {
             return null;
         }
     }
+
+    /**
+     * 方法一：暴力枚举
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/two-sum/solution/liang-shu-zhi-he-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int[] twoSum(int[] nums, int target) {
+            int n = nums.length;
+            for (int i = 0; i < n; ++i) {
+                for (int j = i + 1; j < n; ++j) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i, j};
+                    }
+                }
+            }
+            return new int[0];
+        }
+    }
+
+    /**
+     * 方法二：哈希表
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/two-sum/solution/liang-shu-zhi-he-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer, Integer> hashtable = new HashMap<>();
+            for (int i = 0; i < nums.length; ++i) {
+                if (hashtable.containsKey(target - nums[i])) {
+                    return new int[]{hashtable.get(target - nums[i]), i};
+                }
+                hashtable.put(nums[i], i);
+            }
+            return new int[0];
+        }
+    }
+
+    /**
+     * 与运算
+     */
+    class Solution3 {
+        public int[] twoSum(int[] nums, int target) {
+            int volume = 2 << 10; //2的10次方
+            int bitNum = volume - 1; //11111111111
+            int[] res = new int[volume];
+            for (int i = 0; i < nums.length; i++) {
+                int c = (target - nums[i]) & bitNum;
+                if (res[c] != 0) {
+                    return new int[]{res[c] - 1, i};
+                }
+                res[nums[i] & bitNum] = i + 1;
+            }
+            throw new IllegalArgumentException("No two sum solution");
+        }
+    }
+
+
 }
