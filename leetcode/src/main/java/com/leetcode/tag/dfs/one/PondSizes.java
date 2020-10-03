244ede8ef33848ee95beff3cc65545aa2f6f4c94
xuweng <2028403305@qq.com>
2020-10-03 18:26:27 +0800

    简洁代码
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/one/PondSizes.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/PondSizes.java
@@ -108,4 +108,40 @@ public class PondSizes {
         }
     }
 
+    class Solution2 {
+        public int[] pondSizes(int[][] land) {
+            int count;
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < land.length; i++) {
+                for (int j = 0; j < land[0].length; j++) {
+                    if (land[i][j] == 0) {
+                        count = dfs(land, i, j);
+                        list.add(count);
+                    }
+                }
+            }
+            int[] res = list.stream().mapToInt(integer -> integer).toArray();
+            Arrays.sort(res);
+            return res;
+        }
+
+        private int dfs(int[][] land, int i, int j) {
+            if (i < 0 || i >= land.length || j < 0 || j >= land[0].length || land[i][j] != 0) {
+                return 0;
+            }
+            land[i][j] = -1;
+            int count = 1;
+            count += dfs(land, i, j - 1);
+            count += dfs(land, i, j + 1);
+            count += dfs(land, i + 1, j);
+            count += dfs(land, i - 1, j);
+            count += dfs(land, i + 1, j + 1);
+            count += dfs(land, i - 1, j - 1);
+            count += dfs(land, i + 1, j - 1);
+            count += dfs(land, i - 1, j + 1);
+            return count;
+        }
+
+    }
+
 }
