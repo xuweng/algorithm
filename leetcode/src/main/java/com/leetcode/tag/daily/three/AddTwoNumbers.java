0ff5e49dad7aed5e20725d5ee1787aa19563ca8f
xuweng <2028403305@qq.com>
2020-10-04 11:39:00 +0800

    2. 两数相加
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/AddTwoNumbers.java
@@ -0,0 +1,44 @@
+package com.leetcode.tag.daily.three;
+
+/**
+ * 2. 两数相加
+ */
+public class AddTwoNumbers {
+    class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            int sum = Integer.parseInt(getInt(l1) + getInt(l2));
+            String str = String.valueOf(sum);
+
+            ListNode listNode = new ListNode(str.charAt(str.length() - 1));
+            ListNode tail = listNode;
+            for (int i = str.length() - 2; i >= 0; i--) {
+                ListNode l = new ListNode(str.charAt(i));
+                tail.next = l;
+                tail = l;
+            }
+
+            return listNode;
+        }
+
+        private String getInt(ListNode listNode) {
+            return listNode == null ? "" : getInt(listNode) + listNode.val;
+        }
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
