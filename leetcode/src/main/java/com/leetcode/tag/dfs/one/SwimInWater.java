8ceba69299885ff0acd344de1278bb4c97c4a2ea
xuweng <2028403305@qq.com>
2020-10-04 22:39:11 +0800

    方法一： 堆
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/one/SwimInWater.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/SwimInWater.java
@@ -16,6 +16,8 @@ public class SwimInWater {
      * <p>
      * 用优先队列保存下一步可以游向的平台，每次都选择高度最小的平台。以这种方式到达终点时，路径中遇到的最高平台就是答案。
      * <p>
+     * 所有的能走的点中我们永远只走水位最低的点，每到一个点，我们再记录所有的周围能走的点，并再次选择所有能走的点中水位最低的点
+     * <p>
      * 作者：LeetCode
      * 链接：https://leetcode-cn.com/problems/swim-in-rising-water/solution/shui-wei-shang-sheng-de-yong-chi-zhong-you-yong-by/
      * 来源：力扣（LeetCode）
@@ -24,17 +26,18 @@ public class SwimInWater {
     class Solution {
         public int swimInWater(int[][] grid) {
             int N = grid.length;
-            Set<Integer> seen = new HashSet<>();
-            PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt(k -> grid[k / N][k % N]));
-            pq.offer(0);
+            Set<Integer> visited = new HashSet<>();
+            PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(k -> grid[k / N][k % N]));
+            priorityQueue.offer(0);
             int ans = 0;
 
-            //上下左右
-            int[] dr = new int[]{1, -1, 0, 0};
-            int[] dc = new int[]{0, 0, 1, -1};
+            //上下左右4个方向
+            //其实是右下2个方向
+            int[] row = new int[]{1, -1, 0, 0};
+            int[] col = new int[]{0, 0, 1, -1};
 
-            while (!pq.isEmpty()) {
-                int k = pq.poll();
+            while (!priorityQueue.isEmpty()) {
+                int k = priorityQueue.poll();
                 //计算行
                 int r = k / N;
                 //计算列
@@ -46,11 +49,12 @@ public class SwimInWater {
 
                 //遍历上下左右
                 for (int i = 0; i < 4; ++i) {
-                    int cr = r + dr[i], cc = c + dc[i];
+                    int cr = r + row[i];
+                    int cc = c + col[i];
                     int ck = cr * N + cc;
-                    if (0 <= cr && cr < N && 0 <= cc && cc < N && !seen.contains(ck)) {
-                        pq.offer(ck);
-                        seen.add(ck);
+                    if (0 <= cr && cr < N && 0 <= cc && cc < N && !visited.contains(ck)) {
+                        priorityQueue.offer(ck);
+                        visited.add(ck);
                     }
                 }
             }
