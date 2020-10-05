9ce9ddc4cfce6cf71d6a062ed154b72a9595f31d
xuweng <2028403305@qq.com>
2020-10-05 16:33:59 +0800

    方法一：排序 + 双指针
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/FourSum.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/FourSum.java
@@ -58,6 +58,8 @@ public class FourSum {
 
     /**
      * 暴力
+     * <p>
+     * 最朴素的方法是使用四重循环枚举所有的四元组，然后使用哈希表进行去重操作，得到不包含重复四元组的最终答案
      */
     class Solution1 {
         public List<List<Integer>> fourSum(int[] nums, int target) {
@@ -86,4 +88,66 @@ public class FourSum {
             return result;
         }
     }
+
+    /**
+     * 方法一：排序 + 双指针
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/4sum/solution/si-shu-zhi-he-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public List<List<Integer>> fourSum(int[] nums, int target) {
+            List<List<Integer>> quadruplets = new ArrayList<>();
+            if (nums == null || nums.length < 4) {
+                return quadruplets;
+            }
+            Arrays.sort(nums);
+            int length = nums.length;
+            for (int i = 0; i < length - 3; i++) {
+                if (i > 0 && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                    break;
+                }
+                if (nums[i] + nums[length - 3] + nums[length - 2] + nums[length - 1] < target) {
+                    continue;
+                }
+                for (int j = i + 1; j < length - 2; j++) {
+                    if (j > i + 1 && nums[j] == nums[j - 1]) {
+                        continue;
+                    }
+                    if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                        break;
+                    }
+                    if (nums[i] + nums[j] + nums[length - 2] + nums[length - 1] < target) {
+                        continue;
+                    }
+                    int left = j + 1, right = length - 1;
+                    while (left < right) {
+                        int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                        if (sum == target) {
+                            quadruplets.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                            while (left < right && nums[left] == nums[left + 1]) {
+                                left++;
+                            }
+                            left++;
+                            while (left < right && nums[right] == nums[right - 1]) {
+                                right--;
+                            }
+                            right--;
+                        } else if (sum < target) {
+                            left++;
+                        } else {
+                            right--;
+                        }
+                    }
+                }
+            }
+            return quadruplets;
+        }
+    }
+
 }
