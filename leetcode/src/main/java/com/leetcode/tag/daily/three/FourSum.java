f63e9be61e7e31ddb0e0cd4d8e1278b0803c5754
xuweng <2028403305@qq.com>
2020-10-05 15:41:23 +0800

    排序去重
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/FourSum.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/FourSum.java
@@ -10,9 +10,10 @@ public class FourSum {
         public List<List<Integer>> fourSum(int[] nums, int target) {
             Deque<Integer> stack = new LinkedList<>();
             List<List<Integer>> result = new ArrayList<>();
+            boolean[] used = new boolean[nums.length];
 
             Arrays.sort(nums);
-            back(nums, target, 0, stack, result);
+            back(nums, target, used, 0, stack, result);
 
             return result;
         }
@@ -30,7 +31,7 @@ public class FourSum {
          * @param stack
          * @param result
          */
-        private void back(int[] nums, int target, int start, Deque<Integer> stack, List<List<Integer>> result) {
+        private void back(int[] nums, int target, boolean[] used, int start, Deque<Integer> stack, List<List<Integer>> result) {
             if (stack.size() == 4 && target == 0) {
                 result.add(new ArrayList<>(stack));
                 return;
@@ -39,12 +40,14 @@ public class FourSum {
                 return;
             }
             for (int i = start; i < nums.length; i++) {
-                if (i > 1 && nums[i] == nums[i - 1] && stack.isEmpty()) {
+                if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
                     continue;
                 }
+                used[i] = true;
                 stack.push(nums[i]);
-                back(nums, target - nums[i], i + 1, stack, result);
+                back(nums, target - nums[i], used, i + 1, stack, result);
                 stack.pop();
+                used[i] = false;
             }
         }
     }
