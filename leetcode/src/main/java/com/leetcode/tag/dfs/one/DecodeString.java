a491ce69b582e4ae0bf0582947b922063231efa4
xuweng <2028403305@qq.com>
2020-10-06 00:02:58 +0800

    394. 字符串解码
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/DecodeString.java
@@ -0,0 +1,72 @@
+package com.leetcode.tag.dfs.one;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+/**
+ * 394. 字符串解码
+ */
+public class DecodeString {
+    /**
+     * 方法一：栈操作
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/decode-string/solution/zi-fu-chuan-jie-ma-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution {
+        int ptr;
+
+        public String decodeString(String s) {
+            LinkedList<String> stk = new LinkedList<>();
+            ptr = 0;
+
+            while (ptr < s.length()) {
+                char cur = s.charAt(ptr);
+                if (Character.isDigit(cur)) {
+                    // 获取一个数字并进栈
+                    String digits = getDigits(s);
+                    stk.addLast(digits);
+                } else if (Character.isLetter(cur) || cur == '[') {
+                    // 获取一个字母并进栈
+                    stk.addLast(String.valueOf(s.charAt(ptr++)));
+                } else {
+                    ++ptr;
+                    LinkedList<String> sub = new LinkedList<>();
+                    while (!"[".equals(stk.peekLast())) {
+                        sub.addLast(stk.removeLast());
+                    }
+                    Collections.reverse(sub);
+                    // 左括号出栈
+                    stk.removeLast();
+                    // 此时栈顶为当前 sub 对应的字符串应该出现的次数
+                    int repTime = Integer.parseInt(stk.removeLast());
+                    StringBuilder t = new StringBuilder();
+                    String o = getString(sub);
+                    // 构造字符串
+                    while (repTime-- > 0) {
+                        t.append(o);
+                    }
+                    // 将构造好的字符串入栈
+                    stk.addLast(t.toString());
+                }
+            }
+
+            return getString(stk);
+        }
+
+        public String getDigits(String s) {
+            StringBuilder ret = new StringBuilder();
+            while (Character.isDigit(s.charAt(ptr))) {
+                ret.append(s.charAt(ptr++));
+            }
+            return ret.toString();
+        }
+
+        public String getString(LinkedList<String> v) {
+            return v.stream().collect(Collectors.joining());
+        }
+    }
+}
