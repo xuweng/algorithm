bd79cc290450104f7b4b3771f3e8ee705620f4bb
xuweng <2028403305@qq.com>
2020-10-06 17:27:20 +0800

    数字栈 字符串栈
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/one/DecodeString.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/DecodeString.java
@@ -2,6 +2,7 @@ package com.leetcode.tag.dfs.one;
 
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.Stack;
 
 /**
  * 394. 字符串解码
@@ -232,6 +233,38 @@ public class DecodeString {
                 ret = ret * 10 + s.charAt(i) - '0';
             }
             return ret;
+        }
+    }
+
+    /**
+     * 数字存放在数字栈，字符串存放在字符串栈，遇到右括号时候弹出一个数字栈，字母栈弹到左括号为止.
+     */
+    class S {
+        public String decodeString(String s) {
+            StringBuffer ans = new StringBuffer();
+            Stack<Integer> multiStack = new Stack<>();
+            Stack<StringBuffer> ansStack = new Stack<>();
+            int multi = 0;
+            for (char c : s.toCharArray()) {
+                if (Character.isDigit(c)) {
+                    multi = multi * 10 + c - '0';
+                } else if (c == '[') {
+                    ansStack.add(ans);
+                    multiStack.add(multi);
+                    ans = new StringBuffer();
+                    multi = 0;
+                } else if (Character.isAlphabetic(c)) {
+                    ans.append(c);
+                } else {
+                    StringBuffer ansTmp = ansStack.pop();
+                    int tmp = multiStack.pop();
+                    for (int i = 0; i < tmp; i++) {
+                        ansTmp.append(ans);
+                    }
+                    ans = ansTmp;
+                }
+            }
+            return ans.toString();
         }
     }
 }
