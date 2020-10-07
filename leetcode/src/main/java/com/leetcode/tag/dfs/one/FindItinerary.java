0f03720ad50eef50212a726eda41e9b934e79f11
xuweng <2028403305@qq.com>
2020-10-07 16:08:30 +0800

    方法一：Hierholzer 算法
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/one/FindItinerary.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/FindItinerary.java
@@ -51,4 +51,43 @@ public class FindItinerary {
             }
         }
     }
+
+    /**
+     * 方法一：Hierholzer 算法
+     * <p>
+     * 我们化简本题题意：给定一个 n 个点 m 条边的图，要求从指定的顶点出发，
+     * <p>
+     * 经过所有的边恰好一次（可以理解为给定起点的「一笔画」问题），使得路径的字典序最小。
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/reconstruct-itinerary/solution/zhong-xin-an-pai-xing-cheng-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        Map<String, PriorityQueue<String>> map = new HashMap<String, PriorityQueue<String>>();
+        List<String> itinerary = new LinkedList<String>();
+
+        public List<String> findItinerary(List<List<String>> tickets) {
+            for (List<String> ticket : tickets) {
+                String src = ticket.get(0), dst = ticket.get(1);
+                if (!map.containsKey(src)) {
+                    map.put(src, new PriorityQueue<String>());
+                }
+                map.get(src).offer(dst);
+            }
+            dfs("JFK");
+            Collections.reverse(itinerary);
+            return itinerary;
+        }
+
+        public void dfs(String curr) {
+            while (map.containsKey(curr) && map.get(curr).size() > 0) {
+                String tmp = map.get(curr).poll();
+                dfs(tmp);
+            }
+            itinerary.add(curr);
+        }
+    }
+
 }
