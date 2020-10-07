1217f61a348ad9c955a69fe3be883cea79616159
xuweng <2028403305@qq.com>
2020-10-07 16:15:10 +0800

    用优先队列厉害
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/one/FindItinerary.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/FindItinerary.java
@@ -65,6 +65,7 @@ public class FindItinerary {
      * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
      */
     class Solution1 {
+        //用优先队列厉害
         Map<String, PriorityQueue<String>> map = new HashMap<>();
         List<String> itinerary = new LinkedList<>();
 
