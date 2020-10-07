57bc009148ca91c245ddcac942f47cd0e9e5fec9
xuweng <2028403305@qq.com>
2020-10-07 22:29:05 +0800

    542. 01 矩阵
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/UpdateMatrix.java
@@ -0,0 +1,49 @@
+package com.leetcode.tag.dfs.one;
+
+/**
+ * 542. 01 矩阵
+ */
+public class UpdateMatrix {
+    class Solution {
+        int[] rows = {-1, 1, 0, 0};
+        int[] cols = {0, 0, -1, 1};
+        boolean[][] used;
+
+        public int[][] updateMatrix(int[][] matrix) {
+            if (matrix == null || matrix.length == 1) {
+                return new int[0][0];
+            }
+            used = new boolean[matrix.length][matrix[0].length];
+            for (int i = 0; i < matrix.length; i++) {
+                for (int i1 = 0; i1 < matrix[0].length; i1++) {
+                    if (matrix[i][i1] == 1) {
+                        matrix[i][i] = dfs(matrix, i, i, 0);
+                    }
+                }
+            }
+            return matrix;
+        }
+
+        private int dfs(int[][] matrix, int row, int col, int count) {
+            if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length) {
+                return Integer.MAX_VALUE;
+            }
+            if (matrix[row][col] == 0) {
+                return count;
+            }
+            used[row][col] = true;
+            int result = 0;
+            for (int i = 0; i < rows.length; i++) {
+                int r = row + rows[i];
+                int c = col + cols[i];
+                if (used[r][c]) {
+                    continue;
+                }
+                result = Math.min(result, dfs(matrix, r, c, count + 1));
+            }
+            used[row][col] = false;
+            return result;
+        }
+    }
+
+}
