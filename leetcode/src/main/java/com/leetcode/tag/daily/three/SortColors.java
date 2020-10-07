a0ded78b2220f241be1d580925eee990e74b6e6b
xuweng <2028403305@qq.com>
2020-10-07 14:09:49 +0800

    方法一：单指针
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/SortColors.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/SortColors.java
@@ -33,4 +33,42 @@ public class SortColors {
             }
         }
     }
+
+    /**
+     * 方法一：单指针
+     * <p>
+     * 思路与算法
+     * <p>
+     * 我们可以考虑对数组进行两次遍历。在第一次遍历中，我们将数组中所有的 0 交换到数组的头部。
+     * <p>
+     * 在第二次遍历中，我们将数组中所有的 1 交换到头部的 0 之后。此时，所有的 2 都出现在数组的尾部，这样我们就完成了排序。
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/sort-colors/solution/yan-se-fen-lei-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public void sortColors(int[] nums) {
+            int n = nums.length;
+            int ptr = 0;
+            for (int i = 0; i < n; ++i) {
+                if (nums[i] == 0) {
+                    int temp = nums[i];
+                    nums[i] = nums[ptr];
+                    nums[ptr] = temp;
+                    ++ptr;
+                }
+            }
+            for (int i = ptr; i < n; ++i) {
+                if (nums[i] == 1) {
+                    int temp = nums[i];
+                    nums[i] = nums[ptr];
+                    nums[ptr] = temp;
+                    ++ptr;
+                }
+            }
+        }
+    }
+
 }
