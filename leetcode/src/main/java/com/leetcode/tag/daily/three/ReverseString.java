9ea4cc2b890671d508e9e35f56357ad5dc50f046
xuweng <2028403305@qq.com>
2020-10-11 22:26:29 +0800

    方法一：双指针
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/ReverseString.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/ReverseString.java
@@ -23,4 +23,27 @@ public class ReverseString {
             s[high] = temp;
         }
     }
+
+    /**
+     * 方法一：双指针
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/reverse-string/solution/fan-zhuan-zi-fu-chuan-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public void reverseString(char[] s) {
+            int n = s.length;
+            int left = 0, right = n - 1;
+            while (left < right) {
+                char tmp = s[left];
+                s[left] = s[right];
+                s[right] = tmp;
+                ++left;
+                --right;
+            }
+        }
+    }
+
 }
