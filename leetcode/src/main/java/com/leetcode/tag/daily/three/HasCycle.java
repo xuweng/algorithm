fdc5466eb86137c40ab9c5c8a75e5d2a02f69fb5
xuweng <2028403305@qq.com>
2020-10-11 22:54:38 +0800

    方法二：快慢指针
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/HasCycle.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/HasCycle.java
@@ -54,6 +54,34 @@ public class HasCycle {
         }
     }
 
+    /**
+     * 方法二：快慢指针
+     * <p>
+     * 本方法需要读者对「Floyd 判圈算法」（又称龟兔赛跑算法）有所了解。
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/linked-list-cycle/solution/huan-xing-lian-biao-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public boolean hasCycle(ListNode head) {
+            if (head == null || head.next == null) {
+                return false;
+            }
+            ListNode slow = head;
+            ListNode fast = head.next;
+            while (slow != fast) {
+                if (fast == null || fast.next == null) {
+                    return false;
+                }
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return true;
+        }
+    }
+
     class ListNode {
         int val;
         ListNode next;
