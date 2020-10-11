993ef57b4b878a9836ddea17165edb780da21117
xuweng <2028403305@qq.com>
2020-10-11 21:52:47 +0800

    方法一：动态规划
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/CanPartition.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/CanPartition.java
@@ -2,6 +2,8 @@ package com.leetcode.tag.daily.three;
 
 /**
  * 416. 分割等和子集
+ * <p>
+ * 本题是经典的NP 完全问题
  */
 public class CanPartition {
     class Solution {
@@ -17,4 +19,50 @@ public class CanPartition {
             return true;
         }
     }
+
+    /**
+     * 方法一：动态规划
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/partition-equal-subset-sum/solution/fen-ge-deng-he-zi-ji-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public boolean canPartition(int[] nums) {
+            int n = nums.length;
+            if (n < 2) {
+                return false;
+            }
+            int sum = 0, maxNum = 0;
+            for (int num : nums) {
+                sum += num;
+                maxNum = Math.max(maxNum, num);
+            }
+            if (sum % 2 != 0) {
+                return false;
+            }
+            int target = sum / 2;
+            if (maxNum > target) {
+                return false;
+            }
+            boolean[][] dp = new boolean[n][target + 1];
+            for (int i = 0; i < n; i++) {
+                dp[i][0] = true;
+            }
+            dp[0][nums[0]] = true;
+            for (int i = 1; i < n; i++) {
+                int num = nums[i];
+                for (int j = 1; j <= target; j++) {
+                    if (j >= num) {
+                        dp[i][j] = dp[i - 1][j] | dp[i - 1][j - num];
+                    } else {
+                        dp[i][j] = dp[i - 1][j];
+                    }
+                }
+            }
+            return dp[n - 1][target];
+        }
+    }
+
 }
