474fa13aae7c09022e42a65dd8117acc5634a2f1
xuweng <2028403305@qq.com>
2020-10-12 12:06:02 +0800

    结合 数组的最大子数组和 的思路去解题
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/one/MaxPathSum.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/MaxPathSum.java
@@ -3,6 +3,10 @@ package com.leetcode.tag.dfs.one;
 /**
  * 124. 二叉树中的最大路径和
  * <p>
+ * 结合 数组的最大子数组和 的思路去解题
+ * 动态规划： dp[i] 代表以 nums[i] 为结尾的子数组的最大和。
+ * 转移方程： dp[i] = max(dp[i-1], 0) + nums[i]。
+ * <p>
  * 分析.
  * <p>
  * dp.dp.dp.dp.
