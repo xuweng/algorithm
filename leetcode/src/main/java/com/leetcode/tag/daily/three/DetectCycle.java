14c5c07029e11d38eb26f197b264ac27b32e733d
xuweng <2028403305@qq.com>
2020-10-12 11:13:01 +0800

    方法二：快慢指针
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/DetectCycle.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/DetectCycle.java
@@ -9,6 +9,8 @@ import java.util.Set;
  * 双指针.
  * <p>
  * dp.dp.
+ * <p>
+ * bst.中序遍历.pre
  */
 public class DetectCycle {
     class Solution {
@@ -41,6 +43,42 @@ public class DetectCycle {
                 fast = fast.next.next;
             }
             return slow;
+        }
+    }
+
+    /**
+     * 方法二：快慢指针
+     * <p>
+     * 如果链表中存在环，则 fast 指针最终将再次与 slow 指针在环中相遇。
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/linked-list-cycle-ii/solution/huan-xing-lian-biao-ii-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public ListNode detectCycle(ListNode head) {
+            if (head == null) {
+                return null;
+            }
+            ListNode slow = head, fast = head;
+            while (fast != null) {
+                slow = slow.next;
+                if (fast.next != null) {
+                    fast = fast.next.next;
+                } else {
+                    return null;
+                }
+                if (fast == slow) {
+                    ListNode ptr = head;
+                    while (ptr != slow) {
+                        ptr = ptr.next;
+                        slow = slow.next;
+                    }
+                    return ptr;
+                }
+            }
+            return null;
         }
     }
 
