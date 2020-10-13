20186b4b0de855069ebb5f789d04ac5c875fe76f
xuweng <2028403305@qq.com>
2020-10-13 11:02:46 +0800

    1080. 根到叶路径上的不足节点
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/one/SufficientSubset.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/SufficientSubset.java
@@ -54,6 +54,50 @@ public class SufficientSubset {
         }
     }
 
+    class Solution1 {
+        /**
+         * @param node
+         * @param s
+         * @param limit
+         * @return 返回 node 结点是否被保留（注意：这个返回值的意义，直接影响整个逻辑。）
+         */
+        private Boolean dfs(TreeNode node, int s, int limit) {
+            if (node.left == null && node.right == null) {
+                return s + node.val >= limit;
+            }
+
+            // 注意：如果 dfs 的返回值的意义是这个结点是否被保留，它们的默认值应该设置为 false
+            boolean leftSaved = false;
+            boolean rightSaved = false;
+
+            // 如果有左子树，就先递归处理左子树
+            if (node.left != null) {
+                leftSaved = dfs(node.left, s + node.val, limit);
+            }
+
+            // 如果有右子树，就先递归处理右子树
+            if (node.right != null) {
+                rightSaved = dfs(node.right, s + node.val, limit);
+            }
+
+            // 左右子树是否保留的结论得到了，由自己来执行是否删除它们
+            if (!leftSaved) {
+                node.left = null;
+            }
+
+            if (!rightSaved) {
+                node.right = null;
+            }
+
+            // 左右子树有一颗被保留，自己就应该被保留
+            return leftSaved || rightSaved;
+        }
+
+        public TreeNode sufficientSubset(TreeNode root, int limit) {
+            return !dfs(root, 0, limit) ? null : root;
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
