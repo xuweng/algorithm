7df687f23c7f3e483cef0113ce95932e6bd6dfd0
xuweng <2028403305@qq.com>
2020-10-13 21:44:31 +0800

    扁平化的操作也就是对二叉树进行先序遍历（深度优先搜索）
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/one/Flatten.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/Flatten.java
@@ -49,6 +49,75 @@ public class Flatten {
         }
     }
 
+    /**
+     * 扁平化的操作也就是对二叉树进行先序遍历（深度优先搜索）
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/flatten-a-multilevel-doubly-linked-list/solution/bian-ping-hua-duo-ji-shuang-xiang-lian-biao-by-lee/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public Node flatten(Node head) {
+            if (head == null) {
+                return null;
+            }
+            // 哑结点
+            // pseudo head to ensure the `prev` pointer is never none
+            Node pseudoHead = new Node(0, null, head, null);
+
+            flattenDfs(pseudoHead, head);
+
+            // detach the pseudo head from the real head
+            pseudoHead.next.prev = null;
+            return pseudoHead.next;
+        }
+
+        /**
+         * return the tail of the flatten list
+         * <p>
+         * 定义递归函数 flatten_dfs(prev, curr)，它接收两个指针作为函数参数并返回扁平化列表中的尾部指针
+         *
+         * @param prev prev 指针指向 curr 指向元素的前一个元素
+         * @param curr 指针指向我们要扁平化的子列表
+         * @return
+         */
+        public Node flattenDfs(Node prev, Node curr) {
+            if (curr == null) {
+                return prev;
+            }
+            curr.prev = prev;
+            prev.next = curr;
+
+            // the curr.next would be tempered in the recursive function
+            Node tempNext = curr.next;
+
+            // 左子树
+            Node tail = flattenDfs(curr, curr.child);
+            curr.child = null;
+
+            // 右子树
+            return flattenDfs(tail, tempNext);
+        }
+
+        class Node {
+            public int val;
+            public Node prev;
+            public Node next;
+            public Node child;
+
+            public Node() {
+            }
+
+            public Node(int val, Node prev, Node next, Node child) {
+                this.val = val;
+                this.prev = prev;
+                this.next = next;
+                this.child = child;
+            }
+        }
+    }
+
     class Node {
         public int val;
         public Node prev;
