7c3e95dcb98f162a82bb8c9426a3567032663858
xuweng <2028403305@qq.com>
2020-10-13 22:49:08 +0800

    方法一：枚举
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/one/FindTargetSumWays.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/FindTargetSumWays.java
@@ -25,4 +25,33 @@ public class FindTargetSumWays {
             dfs(nums, s, sum - nums[start], start + 1);
         }
     }
+
+    /**
+     * 方法一：枚举
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/target-sum/solution/mu-biao-he-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        int count = 0;
+
+        public int findTargetSumWays(int[] nums, int S) {
+            calculate(nums, 0, 0, S);
+            return count;
+        }
+
+        public void calculate(int[] nums, int i, int sum, int S) {
+            if (i == nums.length) {
+                if (sum == S) {
+                    count++;
+                }
+            } else {
+                calculate(nums, i + 1, sum + nums[i], S);
+                calculate(nums, i + 1, sum - nums[i], S);
+            }
+        }
+    }
+
 }
