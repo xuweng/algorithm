254d4fb0a205641eb556f3463c153b9ad90ce459
xuweng <2028403305@qq.com>
2020-10-13 16:29:51 +0800

    430. 扁平化多级双向链表
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/Flatten.java
@@ -0,0 +1,45 @@
+package com.leetcode.tag.dfs.one;
+
+/**
+ * 430. 扁平化多级双向链表
+ */
+public class Flatten {
+    class Solution {
+        public Node flatten(Node head) {
+            dfs(head);
+
+            return head;
+        }
+
+        private Node dfs(Node head) {
+            Node child = head;
+            while (child != null) {
+                if (child.child != null) {
+                    break;
+                }
+                child = child.next;
+            }
+            if (child == null) {
+                return head;
+            }
+            Node node = dfs(child.child);
+            node.next = child.next;
+            child.next.prev = node;
+            child.next = child.child;
+            child.child.prev = child;
+
+            Node result = child.child;
+            while (result.next != null) {
+                result = result.next;
+            }
+            return result;
+        }
+    }
+
+    class Node {
+        public int val;
+        public Node prev;
+        public Node next;
+        public Node child;
+    }
+}
