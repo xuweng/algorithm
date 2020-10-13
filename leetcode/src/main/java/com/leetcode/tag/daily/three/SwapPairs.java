7309c963c2787efe3d8280687d8ac7160a0dc037
xuweng <2028403305@qq.com>
2020-10-13 09:55:29 +0800

    方法一：递归
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/three/SwapPairs.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/three/SwapPairs.java
@@ -18,6 +18,52 @@ public class SwapPairs {
         }
     }
 
+    /**
+     * 方法一：递归
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs/solution/liang-liang-jiao-huan-lian-biao-zhong-de-jie-di-91/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public ListNode swapPairs(ListNode head) {
+            //递归的终止条件是链表中没有节点，或者链表中只有一个节点，此时无法进行两两交换。
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode newHead = head.next;
+            head.next = swapPairs(newHead.next);
+            newHead.next = head;
+            return newHead;
+        }
+    }
+
+    /**
+     * 方法二：迭代
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs/solution/liang-liang-jiao-huan-lian-biao-zhong-de-jie-di-91/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public ListNode swapPairs(ListNode head) {
+            ListNode dummyHead = new ListNode(0);
+            dummyHead.next = head;
+            ListNode temp = dummyHead;
+            while (temp.next != null && temp.next.next != null) {
+                ListNode node1 = temp.next;
+                ListNode node2 = temp.next.next;
+                temp.next = node2;
+                node1.next = node2.next;
+                node2.next = node1;
+                temp = node1;
+            }
+            return dummyHead.next;
+        }
+    }
+
     class ListNode {
         int val;
         ListNode next;
