1b48ba2fe96ff5ba2b1ca1c7f9f0aa54ff965100
xuweng <2028403305@qq.com>
2020-10-13 10:56:42 +0800

    1080. 根到叶路径上的不足节点
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/one/SufficientSubset.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/SufficientSubset.java
@@ -15,46 +15,42 @@ public class SufficientSubset {
     class Solution {
         /**
          * @param node
-         * @param s
+         * @param sum
          * @param limit
          * @return 返回 node 结点是否被删除（注意：这个返回值的意义，直接影响整个逻辑。）
          */
-        private boolean dfs(TreeNode node, int s, int limit) {
+        private boolean dfs(TreeNode node, int sum, int limit) {
             if (node.left == null && node.right == null) {
-                return s + node.val < limit;
+                return sum + node.val < limit;
             }
 
             // 注意：如果 dfs 的返回值的意义是这个结点是否被删除，它们的默认值应该设置为 true
-            boolean lTreeDeleted = true;
-            boolean rTreeDeleted = true;
+            boolean leftDeleted = true;
+            boolean rightDeleted = true;
 
             // 如果有左子树，就先递归处理左子树
             if (node.left != null) {
-                lTreeDeleted = dfs(node.left, s + node.val, limit);
+                leftDeleted = dfs(node.left, sum + node.val, limit);
             }
             // 如果有右子树，就先递归处理右子树
             if (node.right != null) {
-                rTreeDeleted = dfs(node.right, s + node.val, limit);
+                rightDeleted = dfs(node.right, sum + node.val, limit);
             }
 
             // 左右子树是否保留的结论得到了，由自己来执行是否删除它们
-            if (lTreeDeleted) {
+            if (leftDeleted) {
                 node.left = null;
             }
-            if (rTreeDeleted) {
+            if (rightDeleted) {
                 node.right = null;
             }
 
             // 只有左右子树都被删除了，自己才没有必要保留
-            return lTreeDeleted && rTreeDeleted;
+            return leftDeleted && rightDeleted;
         }
 
         public TreeNode sufficientSubset(TreeNode root, int limit) {
-            boolean rootDeleted = dfs(root, 0, limit);
-            if (rootDeleted) {
-                return null;
-            }
-            return root;
+            return dfs(root, 0, limit) ? null : root;
         }
     }
 
