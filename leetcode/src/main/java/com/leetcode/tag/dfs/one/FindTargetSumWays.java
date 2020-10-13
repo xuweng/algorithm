6533a4eca8e320ebe6c9f52c3314ebaf2406da2f
xuweng <2028403305@qq.com>
2020-10-13 22:29:02 +0800

    494. 目标和
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/FindTargetSumWays.java
@@ -0,0 +1,28 @@
+package com.leetcode.tag.dfs.one;
+
+/**
+ * 494. 目标和
+ */
+public class FindTargetSumWays {
+    class Solution {
+        int result;
+
+        public int findTargetSumWays(int[] nums, int S) {
+            dfs(nums, S, 0, 0);
+
+            return result;
+        }
+
+        private void dfs(int[] nums, int s, int sum, int start) {
+            if (start >= nums.length) {
+                return;
+            }
+            if (s == sum) {
+                result++;
+                return;
+            }
+            dfs(nums, s, nums[start], start + 1);
+            dfs(nums, s, -nums[start], start + 1);
+        }
+    }
+}
