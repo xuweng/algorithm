874ad7126e6b134d6518526e3cc2a91999c59095
xuweng <2028403305@qq.com>
2020-10-15 00:33:43 +0800

    画表格理解dp状态转换 计算顺序
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/one/FindTargetSumWays.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/FindTargetSumWays.java
@@ -107,6 +107,37 @@ public class FindTargetSumWays {
         }
     }
 
+    class Solution3 {
+        /**
+         * 用 dp[i][j] 表示用数组中的前 i 个元素，组成和为 j 的方案数
+         * <p>
+         * i-1---->i?
+         * <p>
+         * i-1如何到i?
+         * <p>
+         * dp[i][j] = dp[i - 1][j - nums[i]] + dp[i - 1][j + nums[i]]
+         *
+         * @param nums
+         * @param S
+         * @return
+         */
+        public int findTargetSumWays(int[] nums, int S) {
+            int[][] dp = new int[nums.length][2001];
+            //于数组中所有数的和不超过 1000，那么 j 的最小值可以达到 -1000
+            //不允许数组的下标为负数的，因此我们需要给 dp[i][j] 的第二维预先增加 1000
+            dp[0][nums[0] + 1000] = 1;
+            dp[0][-nums[0] + 1000] = 1;
+            //枚举前i个数
+            for (int i = 1; i < nums.length; i++) {
+                //枚举和
+                for (int sum = -1000; sum <= 1000; sum++) {
+                    dp[i][sum + 1000] = dp[i - 1][sum - nums[i] + 1000] + dp[i - 1][sum + nums[i] + 1000];
+                }
+            }
+            return S > 1000 ? 0 : dp[nums.length - 1][S + 1000];
+        }
+    }
+
     class S {
         public int findTargetSumWays(int[] nums, int s) {
             int sum = Arrays.stream(nums).sum();
