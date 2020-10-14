4e42e3a4c3ae1c954e5bb5b66c9e3ea9319e7f09
xuweng <2028403305@qq.com>
2020-10-14 23:45:00 +0800

    用示例来理解代码
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/one/FindTargetSumWays.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/FindTargetSumWays.java
@@ -59,4 +59,42 @@ public class FindTargetSumWays {
         }
     }
 
+    /**
+     * 方法二：动态规划
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/target-sum/solution/mu-biao-he-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        /**
+         * 用 dp[i][j] 表示用数组中的前 i 个元素，组成和为 j 的方案数
+         * <p>
+         * i-1---->i?
+         * <p>
+         * i-1如何到i?
+         * <p>
+         * dp[i][j] = dp[i - 1][j - nums[i]] + dp[i - 1][j + nums[i]]
+         *
+         * @param nums
+         * @param S
+         * @return
+         */
+        public int findTargetSumWays(int[] nums, int S) {
+            int[][] dp = new int[nums.length][2001];
+            dp[0][nums[0] + 1000] = 1;
+            dp[0][-nums[0] + 1000] += 1;
+            for (int i = 1; i < nums.length; i++) {
+                for (int sum = -1000; sum <= 1000; sum++) {
+                    if (dp[i - 1][sum + 1000] > 0) {
+                        dp[i][sum + nums[i] + 1000] += dp[i - 1][sum + 1000];
+                        dp[i][sum - nums[i] + 1000] += dp[i - 1][sum + 1000];
+                    }
+                }
+            }
+            return S > 1000 ? 0 : dp[nums.length - 1][S + 1000];
+        }
+    }
+
 }
