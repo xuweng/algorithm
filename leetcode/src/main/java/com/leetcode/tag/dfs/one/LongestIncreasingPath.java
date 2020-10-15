1a3f9422990f82a8cd061d7cba5abdd19014d277
xuweng <2028403305@qq.com>
2020-10-15 12:43:42 +0800

    方法一：记忆化深度优先搜索
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/one/LongestIncreasingPath.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/LongestIncreasingPath.java
@@ -41,4 +41,48 @@ public class LongestIncreasingPath {
             visited[row][col] = false;
         }
     }
+
+    /**
+     * 方法一：记忆化深度优先搜索
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/longest-increasing-path-in-a-matrix/solution/ju-zhen-zhong-de-zui-chang-di-zeng-lu-jing-by-le-2/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        public int rows, columns;
+
+        public int longestIncreasingPath(int[][] matrix) {
+            if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+                return 0;
+            }
+            rows = matrix.length;
+            columns = matrix[0].length;
+            int[][] memo = new int[rows][columns];
+            int ans = 0;
+            for (int i = 0; i < rows; ++i) {
+                for (int j = 0; j < columns; ++j) {
+                    ans = Math.max(ans, dfs(matrix, i, j, memo));
+                }
+            }
+            return ans;
+        }
+
+        public int dfs(int[][] matrix, int row, int column, int[][] memo) {
+            if (memo[row][column] != 0) {
+                return memo[row][column];
+            }
+            ++memo[row][column];
+            for (int[] dir : dirs) {
+                int newRow = row + dir[0], newColumn = column + dir[1];
+                if (newRow >= 0 && newRow < rows && newColumn >= 0 && newColumn < columns && matrix[newRow][newColumn] > matrix[row][column]) {
+                    memo[row][column] = Math.max(memo[row][column], dfs(matrix, newRow, newColumn, memo) + 1);
+                }
+            }
+            return memo[row][column];
+        }
+    }
+
 }
