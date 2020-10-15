4e997028cc54686e9c787c979793d1b749b14de5
xuweng <2028403305@qq.com>
2020-10-15 17:42:59 +0800

    方法一：记忆化深度优先搜索
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/one/LongestIncreasingPath.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/one/LongestIncreasingPath.java
@@ -53,32 +53,43 @@ public class LongestIncreasingPath {
     class Solution1 {
         public int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         public int rows, columns;
+        int[][] memo;
 
         public int longestIncreasingPath(int[][] matrix) {
+            //校验
             if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
                 return 0;
             }
             rows = matrix.length;
             columns = matrix[0].length;
-            int[][] memo = new int[rows][columns];
+            memo = new int[rows][columns];
             int ans = 0;
             for (int i = 0; i < rows; ++i) {
                 for (int j = 0; j < columns; ++j) {
-                    ans = Math.max(ans, dfs(matrix, i, j, memo));
+                    ans = Math.max(ans, dfs(matrix, i, j));
                 }
             }
             return ans;
         }
 
-        public int dfs(int[][] matrix, int row, int column, int[][] memo) {
+        /**
+         * 定义函数为求坐标的最长递增路径
+         *
+         * @param matrix
+         * @param row
+         * @param column
+         * @return
+         */
+        public int dfs(int[][] matrix, int row, int column) {
             if (memo[row][column] != 0) {
                 return memo[row][column];
             }
+            //初始化为1
             ++memo[row][column];
             for (int[] dir : dirs) {
                 int newRow = row + dir[0], newColumn = column + dir[1];
                 if (newRow >= 0 && newRow < rows && newColumn >= 0 && newColumn < columns && matrix[newRow][newColumn] > matrix[row][column]) {
-                    memo[row][column] = Math.max(memo[row][column], dfs(matrix, newRow, newColumn, memo) + 1);
+                    memo[row][column] = Math.max(memo[row][column], dfs(matrix, newRow, newColumn) + 1);
                 }
             }
             return memo[row][column];
