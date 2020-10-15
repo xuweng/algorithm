394b27476941b11cdea9b008d677edbcd3e75e05
xuweng <2028403305@qq.com>
2020-10-15 22:20:01 +0800

    695. 岛屿的最大面积
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/two/MaxAreaOfIsland.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/two/MaxAreaOfIsland.java
@@ -16,7 +16,7 @@ public class MaxAreaOfIsland {
             visited = new boolean[grid.length][grid[0].length];
             int max = 0;
             for (int i = 0; i < grid.length; i++) {
-                for (int j = 0; j < grid.length; j++) {
+                for (int j = 0; j < grid[0].length; j++) {
                     if (visited[i][j] || grid[i][j] == 0) {
                         continue;
                     }
