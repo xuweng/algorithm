5be7c08b9eab11f9d9f874a0b6248ca7b13f7cca
xuweng <2028403305@qq.com>
2020-10-15 22:25:32 +0800

    难点:矩阵缓存
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/two/MaxAreaOfIsland.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/two/MaxAreaOfIsland.java
@@ -42,4 +42,41 @@ public class MaxAreaOfIsland {
             return result;
         }
     }
+
+    /**
+     * 方法一：深度优先搜索
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/max-area-of-island/solution/dao-yu-de-zui-da-mian-ji-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int maxAreaOfIsland(int[][] grid) {
+            int ans = 0;
+            for (int i = 0; i != grid.length; ++i) {
+                for (int j = 0; j != grid[0].length; ++j) {
+                    ans = Math.max(ans, dfs(grid, i, j));
+                }
+            }
+            return ans;
+        }
+
+        public int dfs(int[][] grid, int curI, int curJ) {
+            if (curI < 0 || curJ < 0 || curI == grid.length || curJ == grid[0].length || grid[curI][curJ] != 1) {
+                return 0;
+            }
+            //修改矩阵.作用类似visited.
+            grid[curI][curJ] = 0;
+            int[] di = {0, 0, 1, -1};
+            int[] dj = {1, -1, 0, 0};
+            int ans = 1;
+            for (int index = 0; index != 4; ++index) {
+                int nextI = curI + di[index], nextJ = curJ + dj[index];
+                ans += dfs(grid, nextI, nextJ);
+            }
+            return ans;
+        }
+    }
+
 }
