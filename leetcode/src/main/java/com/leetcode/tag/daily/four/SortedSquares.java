313b803158561f24f574db7f83e65d7de5cdc845
xuweng <2028403305@qq.com>
2020-10-16 11:17:09 +0800

    977. 有序数组的平方
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/SortedSquares.java
@@ -0,0 +1,24 @@
+package com.leetcode.tag.daily.four;
+
+/**
+ * 977. 有序数组的平方
+ */
+public class SortedSquares {
+    class Solution {
+        public int[] sortedSquares(int[] A) {
+            if (A == null || A.length == 0) {
+                return new int[0];
+            }
+            for (int i = A.length - 1; i >= 0; i--) {
+                if (Math.abs(A[i]) < Math.abs(A[0])) {
+                    int temp = A[0];
+                    A[0] = A[i];
+                    A[i] = temp;
+                }
+                A[i] = A[i] * A[i];
+            }
+
+            return A;
+        }
+    }
+}
