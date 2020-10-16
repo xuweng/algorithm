562b88a8e966c1f9381dccf3fcdc25625f3b6a6e
xuweng <2028403305@qq.com>
2020-10-16 11:27:16 +0800

    方法二：双指针
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/four/SortedSquares.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/SortedSquares.java
@@ -24,4 +24,67 @@ public class SortedSquares {
             return A;
         }
     }
+
+    /**
+     * 方法一：直接排序
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/squares-of-a-sorted-array/solution/you-xu-shu-zu-de-ping-fang-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int[] sortedSquares(int[] A) {
+            int[] ans = new int[A.length];
+            for (int i = 0; i < A.length; ++i) {
+                ans[i] = A[i] * A[i];
+            }
+            Arrays.sort(ans);
+            return ans;
+        }
+    }
+
+    /**
+     * 方法二：双指针
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/squares-of-a-sorted-array/solution/you-xu-shu-zu-de-ping-fang-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public int[] sortedSquares(int[] A) {
+            int n = A.length;
+            int negative = -1;
+            for (int i = 0; i < n; ++i) {
+                if (A[i] < 0) {
+                    negative = i;
+                } else {
+                    break;
+                }
+            }
+
+            int[] ans = new int[n];
+            int index = 0, i = negative, j = negative + 1;
+            while (i >= 0 || j < n) {
+                if (i < 0) {
+                    ans[index] = A[j] * A[j];
+                    ++j;
+                } else if (j == n) {
+                    ans[index] = A[i] * A[i];
+                    --i;
+                } else if (A[i] * A[i] < A[j] * A[j]) {
+                    ans[index] = A[i] * A[i];
+                    --i;
+                } else {
+                    ans[index] = A[j] * A[j];
+                    ++j;
+                }
+                ++index;
+            }
+
+            return ans;
+        }
+    }
+
 }
