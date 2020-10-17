6fb41eb258323890c951a395a3782b44a55ea423
xuweng <2028403305@qq.com>
2020-10-17 11:02:31 +0800

    方法二：基于位运算的回溯
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/four/TotalNQueens.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/TotalNQueens.java
@@ -94,4 +94,33 @@ public class TotalNQueens {
         }
     }
 
+    /**
+     * 方法二：基于位运算的回溯
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/n-queens-ii/solution/nhuang-hou-ii-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public int totalNQueens(int n) {
+            return solve(n, 0, 0, 0, 0);
+        }
+
+        public int solve(int n, int row, int columns, int diagonals1, int diagonals2) {
+            if (row == n) {
+                return 1;
+            }
+            int count = 0;
+            int availablePositions = ((1 << n) - 1) & (~(columns | diagonals1 | diagonals2));
+            while (availablePositions != 0) {
+                int position = availablePositions & (-availablePositions);
+                availablePositions = availablePositions & (availablePositions - 1);
+                count += solve(n, row + 1, columns | position, (diagonals1 | position) << 1, (diagonals2 | position) >> 1);
+            }
+
+            return count;
+        }
+    }
+
 }
