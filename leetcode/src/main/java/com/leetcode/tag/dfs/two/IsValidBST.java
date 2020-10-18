77c8b70f24a5586f80249ef1c09aaa2c0ffd884d
xuweng <2028403305@qq.com>
2020-10-18 16:27:18 +0800

    98. 验证二叉搜索树
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dfs/two/IsValidBST.java
@@ -0,0 +1,36 @@
+package com.leetcode.tag.dfs.two;
+
+/**
+ * 98. 验证二叉搜索树
+ * <p>
+ * 用示例来理解代码
+ * <p>
+ * 用示例来执行代码
+ */
+public class IsValidBST {
+    class Solution {
+        public boolean isValidBST(TreeNode root) {
+            return dfs(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        }
+
+        public boolean dfs(TreeNode root, int min, int max) {
+            if (root == null) {
+                return true;
+            }
+            if (root.val < min || root.val > max) {
+                return false;
+            }
+            return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
