c464f836002eff36eae3b99c3123bd6086a055bc
xuweng <2028403305@qq.com>
2020-10-18 16:28:42 +0800

    98. 验证二叉搜索树
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/two/IsValidBST.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/two/IsValidBST.java
@@ -17,7 +17,7 @@ public class IsValidBST {
             if (root == null) {
                 return true;
             }
-            if (root.val < min || root.val > max) {
+            if (root.val <= min || root.val >= max) {
                 return false;
             }
             return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
