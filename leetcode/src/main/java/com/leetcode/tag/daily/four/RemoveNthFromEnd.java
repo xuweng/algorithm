c58d9bead49bffd4b6d39863f2bb0a83df59e465
xuweng <2028403305@qq.com>
2020-10-18 12:55:06 +0800

    哑节点
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/four/RemoveNthFromEnd.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/RemoveNthFromEnd.java
@@ -28,6 +28,37 @@ public class RemoveNthFromEnd {
         }
     }
 
+    /**
+     * 方法一：计算链表长度
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/solution/shan-chu-lian-biao-de-dao-shu-di-nge-jie-dian-b-61/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            //添加一个哑节点（dummy node），它的 next 指针指向链表的头节点。这样一来，我们就不需要对头节点进行特殊的判断了。
+            ListNode dummy = new ListNode(0, head);
+            int length = getLength(head);
+            ListNode cur = dummy;
+            for (int i = 1; i < length - n + 1; ++i) {
+                cur = cur.next;
+            }
+            cur.next = cur.next.next;
+            return dummy.next;
+        }
+
+        public int getLength(ListNode head) {
+            int length = 0;
+            while (head != null) {
+                ++length;
+                head = head.next;
+            }
+            return length;
+        }
+    }
+
     class ListNode {
         int val;
         ListNode next;
