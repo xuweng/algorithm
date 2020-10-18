84b365df3fede905271a4a8ae819c65dec93ca80
xuweng <2028403305@qq.com>
2020-10-18 13:34:52 +0800

    tarjan算法
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/two/CriticalConnections.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/two/CriticalConnections.java
@@ -73,4 +73,70 @@ public class CriticalConnections {
             return id[node];
         }
     }
+
+    /**
+     * tarjan算法
+     * <p>
+     * 作者：iame
+     * 链接：https://leetcode-cn.com/problems/critical-connections-in-a-network/solution/1192-cha-zhao-ji-qun-nei-de-guan-jian-lian-jie-jav/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        //无向图.下标是结点.
+        private List<Integer>[] edges;
+        private int[] dfn;
+        private int[] lOW;
+        private boolean[] visited;
+        private List<List<Integer>> ans;
+        private int t;
+
+        public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+            this.edges = new ArrayList[n];
+            this.dfn = new int[n];
+            this.lOW = new int[n];
+            this.visited = new boolean[n];
+            this.ans = new ArrayList<>();
+            this.t = 0;
+
+            //构建无向图
+            for (int i = 0; i < n; i++) {
+                edges[i] = new ArrayList<>();
+            }
+            for (List<Integer> conn : connections) {
+                int n1 = conn.get(0), n2 = conn.get(1);
+                edges[n1].add(n2);
+                edges[n2].add(n1);
+            }
+
+            tarjan(0, -1);
+            return ans;
+        }
+
+        public void tarjan(int cur, int pre) {
+            t++;
+            dfn[cur] = t;
+            lOW[cur] = t;
+            visited[cur] = true;
+            for (int node : edges[cur]) {
+                if (node == pre) {
+                    continue;
+                }
+                if (visited[node]) {
+                    lOW[cur] = Math.min(lOW[cur], dfn[node]);
+                } else {
+                    tarjan(node, cur);
+                    lOW[cur] = Math.min(lOW[cur], lOW[node]);
+                    if (lOW[node] > dfn[cur]) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(cur);
+                        list.add(node);
+                        ans.add(list);
+                    }
+                }
+            }
+
+        }
+    }
+
 }
