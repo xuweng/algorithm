6f01d60b29c1972e99ba8729ee73918382e723ad
xuweng <2028403305@qq.com>
2020-10-19 11:39:14 +0800

    方法二：动态规划 + 状态压缩
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/two/Makesquare.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/two/Makesquare.java
@@ -1,7 +1,10 @@
 package com.leetcode.tag.dfs.two;
 
+import javafx.util.Pair;
+
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -153,6 +156,101 @@ public class Makesquare {
                 sums[i] -= nums[index];
             }
             return false;
+        }
+    }
+
+    /**
+     * 方法二：动态规划 + 状态压缩
+     */
+    class Solution2 {
+        // The memoization cache to be used during recursion.
+        public HashMap<Pair<Integer, Integer>, Boolean> memo;
+
+        // Array containing our matchsticks.
+        public int[] nums;
+
+        // Possible side of our square depending on the total sum of all the matchsticks. 
+        public int possibleSquareSide;
+
+        // Default constructor to initialise our memo cache.
+        public Solution2() {
+            this.memo = new HashMap<>();
+        }
+
+        // Main DP function.
+        public boolean recurse(Integer mask, Integer sidesDone) {
+            int total = 0;
+            int l = this.nums.length;
+
+            // The memo key for this recursion
+            Pair<Integer, Integer> memoKey = new Pair(mask, sidesDone);
+
+            // Find out the sum of matchsticks used till now.
+            for (int i = l - 1; i >= 0; i--) {
+                if ((mask & (1 << i)) == 0) {
+                    total += this.nums[l - 1 - i];
+                }
+            }
+
+            // If the sum if divisible by our square's side, then we increment our number of complete sides formed variable.
+            if (total > 0 && total % this.possibleSquareSide == 0) {
+                sidesDone++;
+            }
+
+            // Base case.
+            if (sidesDone == 3) {
+                return true;
+            }
+
+
+            // Return precomputed results
+            if (this.memo.containsKey(memoKey)) {
+                return this.memo.get(memoKey);
+            }
+
+            boolean ans = false;
+            int c = total / this.possibleSquareSide;
+
+            // Remaining vlength in the current partially formed side.
+            int rem = this.possibleSquareSide * (c + 1) - total;
+
+            // Try out all remaining options (that are valid)
+            for (int i = l - 1; i >= 0; i--) {
+                if (this.nums[l - 1 - i] <= rem && (mask & (1 << i)) > 0) {
+                    if (this.recurse(mask ^ (1 << i), sidesDone)) {
+                        ans = true;
+                        break;
+                    }
+                }
+            }
+
+            // Cache the computed results.
+            this.memo.put(memoKey, ans);
+            return ans;
+        }
+
+        public boolean makesquare(int[] nums) {
+
+            // Empty matchsticks.
+            if (nums == null || nums.length == 0) {
+                return false;
+            }
+
+            // Find the perimeter of the square (if at all possible)
+            int L = nums.length;
+            int perimeter = 0;
+            for (int num : nums) {
+                perimeter += num;
+            }
+
+            int possibleSquareSide = perimeter / 4;
+            if (possibleSquareSide * 4 != perimeter) {
+                return false;
+            }
+
+            this.nums = nums;
+            this.possibleSquareSide = possibleSquareSide;
+            return this.recurse((1 << L) - 1, 0);
         }
     }
 
