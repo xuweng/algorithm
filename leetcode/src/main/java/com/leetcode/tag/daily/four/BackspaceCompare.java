b8b9f7ba1c4b3bbb0ec9db053f8a6ddbf0b8e6a6
xuweng <2028403305@qq.com>
2020-10-19 10:44:39 +0800

    方法一：重构字符串
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/four/BackspaceCompare.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/BackspaceCompare.java
@@ -30,4 +30,35 @@ public class BackspaceCompare {
             }
         }
     }
+
+    /**
+     * 方法一：重构字符串
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/backspace-string-compare/solution/bi-jiao-han-tui-ge-de-zi-fu-chuan-by-leetcode-solu/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public boolean backspaceCompare(String S, String T) {
+            return build(S).equals(build(T));
+        }
+
+        public String build(String str) {
+            StringBuilder ret = new StringBuilder();
+            int length = str.length();
+            for (int i = 0; i < length; ++i) {
+                char ch = str.charAt(i);
+                if (ch != '#') {
+                    ret.append(ch);
+                } else {
+                    if (ret.length() > 0) {
+                        ret.deleteCharAt(ret.length() - 1);
+                    }
+                }
+            }
+            return ret.toString();
+        }
+    }
+
 }
