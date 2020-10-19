2b30905e2a79316b8f099d277b4a725354f3398c
xuweng <2028403305@qq.com>
2020-10-19 10:26:30 +0800

    844. 比较含退格的字符串
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/BackspaceCompare.java
@@ -0,0 +1,34 @@
+package com.leetcode.tag.daily.four;
+
+/**
+ * 844. 比较含退格的字符串
+ */
+public class BackspaceCompare {
+    class Solution {
+        public boolean backspaceCompare(String S, String T) {
+            StringBuilder stringBuilder = new StringBuilder(S);
+            StringBuilder s = new StringBuilder(T);
+
+            dfs(stringBuilder, 0);
+            dfs(s, 0);
+
+            return stringBuilder.toString().equals(s.toString());
+        }
+
+        private void dfs(StringBuilder s, int index) {
+            if (index >= s.length() || index == -1) {
+                return;
+            }
+            if (s.charAt(index) == '#') {
+                if (index >= 1) {
+                    s.delete(index - 1, index);
+                } else {
+                    s.deleteCharAt(index);
+                }
+                dfs(s, s.indexOf("#"));
+            } else {
+                dfs(s, index + 1);
+            }
+        }
+    }
+}
