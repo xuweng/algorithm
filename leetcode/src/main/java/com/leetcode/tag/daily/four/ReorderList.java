53444b6e82dcf62b9cbd85024e422b81aa0e8329
xuweng <2028403305@qq.com>
2020-10-20 14:11:32 +0800

    方法一：线性表
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/four/ReorderList.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/ReorderList.java
@@ -38,6 +38,39 @@ public class ReorderList {
         }
     }
 
+    /**
+     * 方法一：线性表
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/reorder-list/solution/zhong-pai-lian-biao-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public void reorderList(ListNode head) {
+            if (head == null) {
+                return;
+            }
+            List<ListNode> list = new ArrayList<>();
+            ListNode node = head;
+            while (node != null) {
+                list.add(node);
+                node = node.next;
+            }
+            int i = 0, j = list.size() - 1;
+            while (i < j) {
+                list.get(i).next = list.get(j);
+                i++;
+                if (i == j) {
+                    break;
+                }
+                list.get(j).next = list.get(i);
+                j--;
+            }
+            list.get(i).next = null;
+        }
+    }
+
     class ListNode {
         int val;
         ListNode next;
