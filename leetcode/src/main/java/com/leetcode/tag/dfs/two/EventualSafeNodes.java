4ea6428332e9f8b952ec1bed9571cbc7c3b2eb3f
xuweng <2028403305@qq.com>
2020-10-20 15:49:58 +0800

    802. 找到最终的安全状态
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dfs/two/EventualSafeNodes.java
@@ -0,0 +1,48 @@
+package com.leetcode.tag.dfs.two;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 802. 找到最终的安全状态
+ * <p>
+ * 十分钟。十分钟。十分钟
+ * <p>
+ * 拓扑排序。拓扑排序。拓扑排序。拓扑排序.
+ */
+public class EventualSafeNodes {
+    class Solution {
+        List<Integer> result = new ArrayList<>();
+        boolean[] visited;
+
+        public List<Integer> eventualSafeNodes(int[][] graph) {
+            visited = new boolean[graph.length];
+            for (int i = 0; i < graph.length; i++) {
+                if (visited[i]) {
+                    continue;
+                }
+                dfs(graph, i);
+            }
+
+            return result;
+        }
+
+        private int dfs(int[][] graph, int i) {
+            if (visited[i]) {
+                return Integer.MAX_VALUE - 1;
+            }
+            visited[i] = true;
+            if (graph[i].length == 0) {
+                return 0;
+            }
+            int r = 0;
+            for (int j : graph[i]) {
+                r = Math.min(r, dfs(graph, j)) + 1;
+            }
+            if (r <= i) {
+                result.add(i);
+            }
+            return r;
+        }
+    }
+}
