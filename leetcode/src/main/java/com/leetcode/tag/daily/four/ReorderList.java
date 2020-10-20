ecec81e3dc3250a046cca4b0e1a5f3b81ab4c7d7
xuweng <2028403305@qq.com>
2020-10-20 13:48:15 +0800

    143. 重排链表
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/ReorderList.java
@@ -0,0 +1,52 @@
+package com.leetcode.tag.daily.four;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 143. 重排链表
+ */
+public class ReorderList {
+    class Solution {
+        public void reorderList(ListNode head) {
+            if (head == null) {
+                return;
+            }
+            ListNode p = head;
+            List<ListNode> list = new ArrayList<>();
+            while (p != null) {
+                list.add(p);
+                p = p.next;
+            }
+
+            dfs(list, 0, list.size() - 1);
+        }
+
+        private ListNode dfs(List<ListNode> list, int start, int end) {
+            if (start >= end) {
+                return list.get(start);
+            }
+            list.get(start).next = list.get(end);
+            list.get(end).next = dfs(list, start + 1, end - 1);
+
+            return list.get(start);
+        }
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
