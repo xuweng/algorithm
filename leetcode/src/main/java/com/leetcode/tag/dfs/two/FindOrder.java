199089f4169d540f82e6b71478bf62ba8b59d66a
xuweng <2028403305@qq.com>
2020-10-20 15:00:31 +0800

    方法二：广度优先搜索
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/two/FindOrder.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/two/FindOrder.java
@@ -1,9 +1,6 @@
 package com.leetcode.tag.dfs.two;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 210. 课程表 II
@@ -159,6 +156,69 @@ public class FindOrder {
             }
             visited[i] = 2;
             result[index--] = i;
+        }
+    }
+
+    /**
+     * 方法二：广度优先搜索
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/course-schedule-ii/solution/ke-cheng-biao-ii-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution3 {
+        // 存储有向图
+        // 下标是结点
+        List<List<Integer>> edges;
+        // 存储每个节点的入度
+        // 下标是结点
+        int[] indeg;
+        // 存储答案
+        int[] result;
+        // 答案下标
+        int index;
+
+        public int[] findOrder(int numCourses, int[][] prerequisites) {
+            edges = new ArrayList<>();
+            for (int i = 0; i < numCourses; ++i) {
+                edges.add(new ArrayList<>());
+            }
+            indeg = new int[numCourses];
+            result = new int[numCourses];
+            index = 0;
+            for (int[] info : prerequisites) {
+                edges.get(info[1]).add(info[0]);
+                //统计入度
+                ++indeg[info[0]];
+            }
+
+            Queue<Integer> queue = new LinkedList<>();
+            // 将所有入度为 0 的节点放入队列中
+            for (int i = 0; i < numCourses; ++i) {
+                if (indeg[i] == 0) {
+                    queue.offer(i);
+                }
+            }
+
+            while (!queue.isEmpty()) {
+                // 从队首取出一个节点
+                int u = queue.poll();
+                // 放入答案中
+                result[index++] = u;
+                for (int v : edges.get(u)) {
+                    --indeg[v];
+                    // 如果相邻节点 v 的入度为 0，就可以选 v 对应的课程了
+                    if (indeg[v] == 0) {
+                        queue.offer(v);
+                    }
+                }
+            }
+
+            if (index != numCourses) {
+                return new int[0];
+            }
+            return result;
         }
     }
 
