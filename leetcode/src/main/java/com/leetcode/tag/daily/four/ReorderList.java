5ac4af5b9f9141ed5e5b9d697be93e7b8d3258fa
xuweng <2028403305@qq.com>
2020-10-20 14:12:58 +0800

    方法二：寻找链表中点 + 链表逆序 + 合并链表
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/four/ReorderList.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/ReorderList.java
@@ -71,6 +71,64 @@ public class ReorderList {
         }
     }
 
+    /**
+     * 方法二：寻找链表中点 + 链表逆序 + 合并链表
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/reorder-list/solution/zhong-pai-lian-biao-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public void reorderList(ListNode head) {
+            if (head == null) {
+                return;
+            }
+            ListNode mid = middleNode(head);
+            ListNode l2 = mid.next;
+            mid.next = null;
+            l2 = reverseList(l2);
+            mergeList(head, l2);
+        }
+
+        public ListNode middleNode(ListNode head) {
+            ListNode slow = head;
+            ListNode fast = head;
+            while (fast.next != null && fast.next.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+
+        public ListNode reverseList(ListNode head) {
+            ListNode prev = null;
+            ListNode curr = head;
+            while (curr != null) {
+                ListNode nextTemp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = nextTemp;
+            }
+            return prev;
+        }
+
+        public void mergeList(ListNode l1, ListNode l2) {
+            ListNode l1Tmp;
+            ListNode l2Tmp;
+            while (l1 != null && l2 != null) {
+                l1Tmp = l1.next;
+                l2Tmp = l2.next;
+
+                l1.next = l2;
+                l1 = l1Tmp;
+
+                l2.next = l1;
+                l2 = l2Tmp;
+            }
+        }
+    }
+
     class ListNode {
         int val;
         ListNode next;
