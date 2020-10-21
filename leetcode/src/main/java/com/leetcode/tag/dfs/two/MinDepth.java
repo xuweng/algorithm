2bc644d89f7f28084cc5cbf2f8a3ec27246bea15
xuweng <2028403305@qq.com>
2020-10-21 23:20:35 +0800

    方法二：广度优先搜索
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/two/MinDepth.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/two/MinDepth.java
@@ -1,5 +1,8 @@
 package com.leetcode.tag.dfs.two;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 111. 二叉树的最小深度
  */
@@ -16,6 +19,51 @@ public class MinDepth {
                 return Math.max(left, right) + 1;
             }
             return Math.min(left, right) + 1;
+        }
+    }
+
+    /**
+     * 方法二：广度优先搜索
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/solution/er-cha-shu-de-zui-xiao-shen-du-by-leetcode-solutio/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        class QueueNode {
+            TreeNode node;
+            int depth;
+
+            public QueueNode(TreeNode node, int depth) {
+                this.node = node;
+                this.depth = depth;
+            }
+        }
+
+        public int minDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            Queue<QueueNode> queue = new LinkedList<>();
+            queue.offer(new QueueNode(root, 1));
+            while (!queue.isEmpty()) {
+                QueueNode nodeDepth = queue.poll();
+                TreeNode node = nodeDepth.node;
+                int depth = nodeDepth.depth;
+                if (node.left == null && node.right == null) {
+                    return depth;
+                }
+                if (node.left != null) {
+                    queue.offer(new QueueNode(node.left, depth + 1));
+                }
+                if (node.right != null) {
+                    queue.offer(new QueueNode(node.right, depth + 1));
+                }
+            }
+
+            return 0;
         }
     }
 
