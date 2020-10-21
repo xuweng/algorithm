8c22ed8eeb2adaa8f6b0372dce3c4526a7815264
xuweng <2028403305@qq.com>
2020-10-21 22:58:26 +0800

    797. 所有可能的路径
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/two/AllPathsSourceTarget.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/two/AllPathsSourceTarget.java
@@ -36,4 +36,27 @@ public class AllPathsSourceTarget {
             stack.removeLast();
         }
     }
+
+    class Solution1 {
+        public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+            List<List<Integer>> res = new ArrayList<>();
+            if (graph.length == 0) {
+                return res;
+            }
+            bfs(graph, 0, new ArrayList<>(), res);
+            return res;
+        }
+
+        // curr:当前节点编号
+        public void bfs(int[][] graph, int curr, List<Integer> list, List<List<Integer>> res) {
+            list.add(curr);
+            if (curr == graph.length - 1) {
+                res.add(new ArrayList<>(list));
+            }
+            for (int i : graph[curr]) {
+                bfs(graph, i, list, res);
+            }
+            list.remove(list.size() - 1);
+        }
+    }
 }
