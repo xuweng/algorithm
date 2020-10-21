88c2b46482d81661bfdd3666e7ad133aaf8fe9d7
xuweng <2028403305@qq.com>
2020-10-21 16:11:58 +0800

    1519. 子树中标签相同的节点数
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dfs/two/CountSubTrees.java
@@ -0,0 +1,52 @@
+package com.leetcode.tag.dfs.two;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 1519. 子树中标签相同的节点数
+ * <p>
+ * 十分钟.十分钟.十分钟
+ * <p>
+ * 越来越熟练.越来越熟练.越来越熟练.
+ */
+public class CountSubTrees {
+    class Solution {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int[] result;
+
+        public int[] countSubTrees(int n, int[][] edges, String labels) {
+            if (edges == null) {
+                return new int[0];
+            }
+            result = new int[n];
+            for (int[] edge : edges) {
+                map.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
+            }
+            dfs(0, labels);
+
+            return result;
+        }
+
+        private int dfs(int i, String labels) {
+            if (!map.containsKey(i)) {
+                result[i] = 1;
+                return 1;
+            }
+            int count = 1;
+            for (Integer j : map.get(i)) {
+                if (labels.charAt(i) == labels.charAt(j)) {
+                    count += dfs(j, labels);
+                } else {
+                    dfs(j, labels);
+                }
+            }
+            result[i] = count;
+
+            return count;
+        }
+
+    }
+}
