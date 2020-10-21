61e8e11d363d2eb1d6d19941841ffb0455d69a2c
xuweng <2028403305@qq.com>
2020-10-21 23:46:25 +0800

    1254. 统计封闭岛屿的数目
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dfs/two/ClosedIsland.java
@@ -0,0 +1,46 @@
+package com.leetcode.tag.dfs.two;
+
+/**
+ * 1254. 统计封闭岛屿的数目
+ */
+public class ClosedIsland {
+    class Solution {
+        int[] rows = {-1, 1, 0, 0};
+        int[] cols = {0, 0, -1, 1};
+        boolean[][] visited;
+
+        public int closedIsland(int[][] grid) {
+            if (grid == null || grid.length == 0) {
+                return 0;
+            }
+            visited = new boolean[grid.length][grid[0].length];
+            int result = 0;
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    if (grid[i][j] == 1 || visited[i][j]) {
+                        continue;
+                    }
+                    if (dfs(grid, i, j)) {
+                        result++;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private boolean dfs(int[][] grid, int row, int col) {
+            if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
+                return false;
+            }
+            if (visited[row][col] || grid[row][col] == 1) {
+                return true;
+            }
+            visited[row][col] = true;
+            boolean result = true;
+            for (int i = 0; i < rows.length; i++) {
+                result = result && dfs(grid, row + rows[i], col + cols[i]);
+            }
+            return result;
+        }
+    }
+}
