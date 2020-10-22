7c3f5e2f258366c1584d660b529de7d4d19553ec
xuweng <2028403305@qq.com>
2020-10-22 23:36:27 +0800

    dfs
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/two/ClosedIsland.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/two/ClosedIsland.java
@@ -100,6 +100,42 @@ public class ClosedIsland {
         }
     }
 
+    class Solution3 {
+        int[] rows = {-1, 1, 0, 0};
+        int[] cols = {0, 0, -1, 1};
+
+        public int closedIsland(int[][] grid) {
+            if (grid == null || grid.length == 0) {
+                return 0;
+            }
+            int result = 0;
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    if (grid[i][j] == 1) {
+                        continue;
+                    }
+                    result += dfs(grid, i, j);
+                }
+            }
+            return result;
+        }
+
+        private int dfs(int[][] grid, int row, int col) {
+            if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
+                return 0;
+            }
+            if (grid[row][col] == 1) {
+                return 1;
+            }
+            grid[row][col] = 1;
+            int result = 1;
+            for (int i = 0; i < rows.length; i++) {
+                result = Math.min(result, dfs(grid, row + rows[i], col + cols[i]));
+            }
+            return result;
+        }
+    }
+
     /**
      * dfs
      * <p>
