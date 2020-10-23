87eb42a46aa834e3aeeb367a7533c0636ada736a
xuweng <2028403305@qq.com>
2020-10-23 23:30:38 +0800

    方法二：递归
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/four/IsPalindrome.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/IsPalindrome.java
@@ -84,15 +84,16 @@ public class IsPalindrome {
         private ListNode frontPointer;
 
         private boolean recursivelyCheck(ListNode currentNode) {
-            if (currentNode != null) {
-                if (!recursivelyCheck(currentNode.next)) {
-                    return false;
-                }
-                if (currentNode.val != frontPointer.val) {
-                    return false;
-                }
-                frontPointer = frontPointer.next;
+            if (currentNode == null) {
+                return true;
             }
+            if (!recursivelyCheck(currentNode.next)) {
+                return false;
+            }
+            if (currentNode.val != frontPointer.val) {
+                return false;
+            }
+            frontPointer = frontPointer.next;
             return true;
         }
 
