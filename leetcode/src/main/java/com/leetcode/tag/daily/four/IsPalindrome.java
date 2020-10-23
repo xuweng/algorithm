01dc837d778059d5799830af272da5b40e6eba5e
xuweng <2028403305@qq.com>
2020-10-23 23:36:33 +0800

    方法三：快慢指针
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/four/IsPalindrome.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/IsPalindrome.java
@@ -104,6 +104,85 @@ public class IsPalindrome {
         }
     }
 
+    /**
+     * 方法三：快慢指针
+     * <p>
+     * 整个流程可以分为以下五个步骤：
+     * <p>
+     * 找到前半部分链表的尾节点。
+     * 反转后半部分链表。
+     * 判断是否回文。
+     * 恢复链表。
+     * 返回结果。
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/palindrome-linked-list/solution/hui-wen-lian-biao-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution3 {
+        public boolean isPalindrome(ListNode head) {
+            if (head == null) {
+                return true;
+            }
+
+            // 找到前半部分链表的尾节点并反转后半部分链表
+            ListNode firstHalfEnd = endOfFirstHalf(head);
+            ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+
+            // 判断是否回文
+            ListNode p1 = head;
+            ListNode p2 = secondHalfStart;
+            boolean result = true;
+            while (result && p2 != null) {
+                if (p1.val != p2.val) {
+                    result = false;
+                }
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+
+            // 还原链表并返回结果
+            firstHalfEnd.next = reverseList(secondHalfStart);
+            return result;
+        }
+
+        private ListNode reverseList(ListNode head) {
+            ListNode prev = null;
+            ListNode curr = head;
+            while (curr != null) {
+                ListNode nextTemp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = nextTemp;
+            }
+            return prev;
+        }
+
+        /**
+         * 可以使用快慢指针在一次遍历中找到：慢指针一次走一步，快指针一次走两步，快慢指针同时出发。
+         * <p>
+         * 当快指针移动到链表的末尾时，慢指针恰好到链表的中间。通过慢指针将链表分为两部分。
+         * <p>
+         * 作者：LeetCode-Solution
+         * 链接：https://leetcode-cn.com/problems/palindrome-linked-list/solution/hui-wen-lian-biao-by-leetcode-solution/
+         * 来源：力扣（LeetCode）
+         * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+         *
+         * @param head
+         * @return
+         */
+        private ListNode endOfFirstHalf(ListNode head) {
+            ListNode fast = head;
+            ListNode slow = head;
+            while (fast.next != null && fast.next.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
+    }
+
     class ListNode {
         int val;
         ListNode next;
