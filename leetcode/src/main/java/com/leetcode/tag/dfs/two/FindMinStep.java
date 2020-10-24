f64f314471d22743e9cc2bcda032b5eee9c81d10
xuweng <2028403305@qq.com>
2020-10-24 16:20:44 +0800

    488. 祖玛游戏
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dfs/two/FindMinStep.java
@@ -0,0 +1,12 @@
+package com.leetcode.tag.dfs.two;
+
+/**
+ * 488. 祖玛游戏
+ */
+public class FindMinStep {
+    class Solution {
+        public int findMinStep(String board, String hand) {
+            return 0;
+        }
+    }
+}
