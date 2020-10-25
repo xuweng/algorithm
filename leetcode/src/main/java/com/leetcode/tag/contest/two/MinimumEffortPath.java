1b6312d94996ea7bcdff6c52bf3a281185b09a85
xuweng <2028403305@qq.com>
2020-10-25 12:33:29 +0800

    5548. 最小体力消耗路径
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/contest/two/MinimumEffortPath.java
+++ leetcode/src/main/java/com/leetcode/tag/contest/two/MinimumEffortPath.java
@@ -8,35 +8,31 @@ public class MinimumEffortPath {
         int[] rows = {-1, 1, 0, 0};
         int[] cols = {0, 0, -1, 1};
         boolean[][] visited;
-        int[][] memo;
         int result = Integer.MAX_VALUE;
 
         public int minimumEffortPath(int[][] heights) {
             visited = new boolean[heights.length][heights[0].length];
-            memo = new int[heights.length][heights[0].length];
 
-            dfs(heights, 0, 0);
+            dfs(heights, 0, 0, heights.length - 1, heights[0].length - 1);
 
             return result;
         }
 
-        public void dfs(int[][] heights, int row, int col) {
-            if (memo[row][col] != 0) {
+        public void dfs(int[][] heights, int row, int col, int endR, int endC) {
+            visited[row][col] = true;
+            if (row == endR && col == endC) {
                 return;
             }
-
-            visited[row][col] = true;
             for (int i = 0; i < rows.length; i++) {
                 int r = row + rows[i];
                 int c = col + cols[i];
                 if (r < 0 || r >= heights.length || c < 0 || c >= heights[0].length || visited[r][c]) {
                     continue;
                 }
-                dfs(heights, r, c);
+                dfs(heights, r, c, endR, endC);
                 result = Math.min(result, Math.abs(heights[r][c] - heights[row][col]));
             }
 
-            memo[row][col] = result;
         }
     }
 }
