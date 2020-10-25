63494855103aac768ee80a1c74cf5a204cc2a306
xuweng <2028403305@qq.com>
2020-10-25 16:18:38 +0800

    连通块编号
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/two/LargestIsland.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/two/LargestIsland.java
@@ -1,8 +1,6 @@
 package com.leetcode.tag.dfs.two;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * 827. 最大人工岛
@@ -80,4 +78,103 @@ public class LargestIsland {
             return seen.size();
         }
     }
+
+    /**
+     * 方法 2：连通块大小【通过】
+     * <p>
+     * 记录连通块编号
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/making-a-large-island/solution/zui-da-ren-gong-dao-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        int[] dr = new int[]{-1, 0, 1, 0};
+        int[] dc = new int[]{0, -1, 0, 1};
+        int[][] grid;
+        int N;
+
+        public int largestIsland(int[][] grid) {
+            this.grid = grid;
+            N = grid.length;
+
+            int index = 2;
+            int[] area = new int[N * N + 2];
+            for (int r = 0; r < N; ++r) {
+                for (int c = 0; c < N; ++c) {
+                    if (grid[r][c] == 1) {
+                        area[index] = dfs(r, c, index++);
+                    }
+                }
+            }
+
+            int ans = 0;
+            for (int x : area) {
+                ans = Math.max(ans, x);
+            }
+            for (int r = 0; r < N; ++r) {
+                for (int c = 0; c < N; ++c) {
+                    if (grid[r][c] == 0) {
+                        Set<Integer> seen = new HashSet<>();
+                        for (Integer move : neighbors(r, c)) {
+                            if (grid[move / N][move % N] > 1) {
+                                seen.add(grid[move / N][move % N]);
+                            }
+                        }
+
+                        int bns = 1;
+                        for (int i : seen) {
+                            bns += area[i];
+                        }
+                        ans = Math.max(ans, bns);
+                    }
+                }
+            }
+
+            return ans;
+        }
+
+        /**
+         * @param r
+         * @param c
+         * @param index 连通块编号
+         * @return
+         */
+        public int dfs(int r, int c, int index) {
+            int ans = 1;
+            grid[r][c] = index;
+            for (Integer move : neighbors(r, c)) {
+                if (grid[move / N][move % N] == 1) {
+                    // 邻接顶点编号为index
+                    grid[move / N][move % N] = index;
+                    ans += dfs(move / N, move % N, index);
+                }
+            }
+
+            return ans;
+        }
+
+        /**
+         * 获取邻接顶点
+         *
+         * @param r
+         * @param c
+         * @return
+         */
+        public List<Integer> neighbors(int r, int c) {
+            List<Integer> ans = new ArrayList<>();
+            for (int k = 0; k < 4; ++k) {
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+                if (0 > nr || nr >= N || 0 > nc || nc >= N) {
+                    continue;
+                }
+                ans.add(nr * N + nc);
+            }
+
+            return ans;
+        }
+    }
+
 }
