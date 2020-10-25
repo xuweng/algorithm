a9732a5cba18b8ab722bdb91758a96c550af9d7d
xuweng <2028403305@qq.com>
2020-10-25 10:31:55 +0800

    845. 数组中的最长山脉
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/four/LongestMountain.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/LongestMountain.java
@@ -44,4 +44,68 @@ public class LongestMountain {
         }
     }
 
+    /**
+     * 方法二：枚举山脚
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/longest-mountain-in-array/solution/shu-zu-zhong-de-zui-chang-shan-mai-by-leetcode-sol/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int longestMountain(int[] A) {
+            int n = A.length;
+            int ans = 0;
+            // 一个指针枚举左侧山脚
+            int left = 0;
+            while (left + 2 < n) {
+                // 另一个指针不断向右移动到右侧山脚
+                int right = left + 1;
+                if (A[left] < A[left + 1]) {
+                    while (right + 1 < n && A[right] < A[right + 1]) {
+                        ++right;
+                    }
+                    if (right < n - 1 && A[right] > A[right + 1]) {
+                        while (right + 1 < n && A[right] > A[right + 1]) {
+                            ++right;
+                        }
+                        ans = Math.max(ans, right - left + 1);
+                    } else {
+                        ++right;
+                    }
+                }
+                // 右侧山脚有可能是下一个左侧山脚，因此我们需要将 right 的值赋予 left，以便与进行下一次枚举
+                left = right;
+            }
+            return ans;
+        }
+    }
+
+    /**
+     * 枚举山顶
+     */
+    class Solution2 {
+        public int longestMountain(int[] A) {
+            int ans = 2;
+            int l = A.length;
+            for (int i = 1; i < l - 1; i++) {
+                //先找到山顶
+                if (A[i] > A[i - 1] && A[i] > A[i + 1]) {
+                    int j = i - 1;
+                    int k = i + 1;
+                    // 枚举左山脚
+                    while (j >= 0 && A[j] < A[j + 1]) {
+                        j--;
+                    }
+                    // 枚举右山脚
+                    while (k < l && A[k] < A[k - 1]) {
+                        k++;
+                    }
+                    ans = Math.max(ans, k - j - 1);
+                }
+            }
+            return ans == 2 ? 0 : ans;
+        }
+    }
+
 }
