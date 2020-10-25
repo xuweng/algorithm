408276bb0449971faac4edd3ca06acfe71a28779
xuweng <2028403305@qq.com>
2020-10-25 16:46:43 +0800

    二分查找+DFS
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/contest/two/MinimumEffortPath.java
+++ leetcode/src/main/java/com/leetcode/tag/contest/two/MinimumEffortPath.java
@@ -16,13 +16,13 @@ public class MinimumEffortPath {
         public int minimumEffortPath(int[][] heights) {
             visited = new boolean[heights.length][heights[0].length];
 
-            dfs(heights, 0, 0, heights.length - 1, heights[0].length - 1, 0);
+            dfs(heights, 0, 0, 0);
 
             return result;
         }
 
-        public void dfs(int[][] heights, int row, int col, int endR, int endC, int cha) {
-            if (row == endR && col == endC) {
+        public void dfs(int[][] heights, int row, int col, int cha) {
+            if (row == heights.length - 1 && col == heights[0].length - 1) {
                 result = Math.min(result, cha);
                 return;
             }
@@ -33,7 +33,7 @@ public class MinimumEffortPath {
                 if (r < 0 || r >= heights.length || c < 0 || c >= heights[0].length || visited[r][c]) {
                     continue;
                 }
-                dfs(heights, r, c, endR, endC, Math.max(cha, Math.abs(heights[r][c] - heights[row][col])));
+                dfs(heights, r, c, Math.max(cha, Math.abs(heights[r][c] - heights[row][col])));
             }
             visited[row][col] = false;
 
@@ -82,5 +82,65 @@ public class MinimumEffortPath {
             meno[row][col] = re;
             return re;
         }
+    }
+
+    /**
+     * 二分查找+DFS
+     * <p>
+     * 最大值最小化问题
+     * <p>
+     * 作者：soap88
+     * 链接：https://leetcode-cn.com/problems/path-with-minimum-effort/solution/er-fen-cha-zhao-dfs-by-soap88/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        public int minimumEffortPath(int[][] heights) {
+            int left = 0;
+            int right = 1000000;
+            //二分查找搜索最小值
+            while (left < right) {
+                int mid = (left + right) >>> 1;
+                if (!dfs(0, 0, mid, heights, new boolean[heights.length][heights[0].length])) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            return left;
+        }
+
+        /**
+         * 检查是否存在一条从(x,y)到终点的路径，该路径中相邻顶点绝对值差不大于max
+         *
+         * @param x
+         * @param y
+         * @param max
+         * @param h
+         * @param visited
+         * @return
+         */
+        boolean dfs(int x, int y, int max, int[][] h, boolean[][] visited) {
+            if (x == h.length - 1 && y == h[0].length - 1) {
+                // 到达终点
+                return true;
+            }
+            visited[x][y] = true;
+            for (int[] dir : dirs) {
+                int nx = x + dir[0];
+                int ny = y + dir[1];
+                if (nx < 0 || nx >= h.length || ny < 0 || ny >= h[0].length || visited[nx][ny]
+                        || Math.abs(h[nx][ny] - h[x][y]) > max) {
+                    continue;
+                }
+                if (dfs(nx, ny, max, h, visited)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
     }
 }
