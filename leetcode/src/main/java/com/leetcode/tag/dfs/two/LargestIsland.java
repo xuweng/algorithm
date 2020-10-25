1b55ac13669fd54839025765a4b7e46645c16d32
xuweng <2028403305@qq.com>
2020-10-25 16:27:07 +0800

    连通块编号
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/two/LargestIsland.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/two/LargestIsland.java
@@ -177,4 +177,78 @@ public class LargestIsland {
         }
     }
 
+    /**
+     * 作者：caipengbo
+     * 链接：https://leetcode-cn.com/problems/making-a-large-island/solution/jian-dan-yi-yu-li-jie-de-java-dfsfang-fa-by-caipen/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution3 {
+        private int curIslandArea;
+
+        public int largestIsland(int[][] grid) {
+            // 1 <= grid.length = grid[0].length <= 50
+            int m = grid.length, n = grid[0].length;
+            int[] area = new int[250];
+            int index = 2, maxIslandArea = 0;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    curIslandArea = 0;
+                    dfs(grid, i, j, index);
+                    if (curIslandArea != 0) {
+                        // 岛屿的最大面积
+                        maxIslandArea = Math.max(maxIslandArea, curIslandArea);
+                        // 记录每一块岛屿的面积
+                        area[index++] = curIslandArea;
+                    }
+                }
+            }
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == 0) {
+                        maxIslandArea = Math.max(maxIslandArea, changeSea(grid, m, n, area, i, j));
+                    }
+                }
+            }
+            return maxIslandArea;
+        }
+
+        // DFS 求每一个小岛，并且获得每一个小岛的面积
+        private void dfs(int[][] grid, int i, int j, int index) {
+            int m = grid.length, n = grid[0].length;
+            if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != 1) {
+                return;
+            }
+            grid[i][j] = index;
+            curIslandArea++;
+            // 在此处判断，不用进入下一层递归
+            dfs(grid, i - 1, j, index);
+            dfs(grid, i + 1, j, index);
+            dfs(grid, i, j - 1, index);
+            dfs(grid, i, j + 1, index);
+        }
+
+        // 尝试改变每一个海洋(为0的地方)
+        private int changeSea(int[][] grid, int m, int n, int[] area, int i, int j) {
+            Set<Integer> set = new HashSet<>();
+            int areaSum = 1;
+            if (i - 1 >= 0) {
+                set.add(grid[i - 1][j]);
+            }
+            if (i + 1 < m) {
+                set.add(grid[i + 1][j]);
+            }
+            if (j - 1 >= 0) {
+                set.add(grid[i][j - 1]);
+            }
+            if (j + 1 < n) {
+                set.add(grid[i][j + 1]);
+            }
+            for (Integer index : set) {
+                areaSum += area[index];
+            }
+            return areaSum;
+        }
+    }
+
 }
