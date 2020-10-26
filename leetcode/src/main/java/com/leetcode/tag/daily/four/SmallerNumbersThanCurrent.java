b437469892486c6a17d65facd35b63036550524e
xuweng <2028403305@qq.com>
2020-10-26 12:15:25 +0800

    1365. 有多少小于当前数字的数字
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/SmallerNumbersThanCurrent.java
@@ -0,0 +1,34 @@
+package com.leetcode.tag.daily.four;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 1365. 有多少小于当前数字的数字
+ * <p>
+ * 十分钟.十分钟.十分钟
+ */
+public class SmallerNumbersThanCurrent {
+    class Solution {
+        public int[] smallerNumbersThanCurrent(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return new int[0];
+            }
+            int[] result = new int[200];
+            List<Integer> list = Arrays.stream(nums).boxed().sorted().collect(Collectors.toList());
+            result[list.get(0)] = 0;
+            for (int i = 1; i < list.size(); i++) {
+                if (list.get(i) > list.get(i - 1)) {
+                    result[list.get(i)] = i;
+                } else {
+                    result[list.get(i)] = result[list.get(i) - 1];
+                }
+            }
+            for (int i = 0; i < nums.length; i++) {
+                nums[i] = result[nums[i]];
+            }
+            return nums;
+        }
+    }
+}
