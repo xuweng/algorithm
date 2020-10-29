be26bdfcb63b011122be765ed3b9bed11e4ff4ef
xuweng <2028403305@qq.com>
2020-10-29 11:02:04 +0800

    方法一：深度优先搜索
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/four/SumNumbers.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/SumNumbers.java
@@ -25,6 +25,32 @@ public class SumNumbers {
         }
     }
 
+    /**
+     * 方法一：深度优先搜索
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/sum-root-to-leaf-numbers/solution/qiu-gen-dao-xie-zi-jie-dian-shu-zi-zhi-he-by-leetc/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int sumNumbers(TreeNode root) {
+            return dfs(root, 0);
+        }
+
+        public int dfs(TreeNode root, int prevSum) {
+            if (root == null) {
+                return 0;
+            }
+            int sum = prevSum * 10 + root.val;
+            if (root.left == null && root.right == null) {
+                return sum;
+            }
+            return dfs(root.left, sum) + dfs(root.right, sum);
+
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
