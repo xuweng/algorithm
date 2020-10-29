0f560840894ac2482b213b799a7e153c88aecc8b
xuweng <2028403305@qq.com>
2020-10-29 11:29:50 +0800

    方法二：迪杰斯特拉最短路径算法（Dijkstra's）[Accepted]
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/two/NetworkDelayTime.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/two/NetworkDelayTime.java
@@ -116,4 +116,65 @@ public class NetworkDelayTime {
             }
         }
     }
+
+    /**
+     * 方法二：迪杰斯特拉最短路径算法（Dijkstra's）[Accepted]
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/network-delay-time/solution/wang-luo-yan-chi-shi-jian-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        Map<Integer, Integer> dist;
+
+        public int networkDelayTime(int[][] times, int N, int K) {
+            Map<Integer, List<int[]>> graph = new HashMap<>();
+            for (int[] edge : times) {
+                if (!graph.containsKey(edge[0])) {
+                    graph.put(edge[0], new ArrayList<>());
+                }
+                graph.get(edge[0]).add(new int[]{edge[1], edge[2]});
+            }
+            dist = new HashMap<>();
+            for (int node = 1; node <= N; ++node) {
+                dist.put(node, Integer.MAX_VALUE);
+            }
+
+            dist.put(K, 0);
+            boolean[] seen = new boolean[N + 1];
+
+            while (true) {
+                int candNode = -1;
+                int candDist = Integer.MAX_VALUE;
+                for (int i = 1; i <= N; ++i) {
+                    if (!seen[i] && dist.get(i) < candDist) {
+                        candDist = dist.get(i);
+                        candNode = i;
+                    }
+                }
+
+                if (candNode < 0) {
+                    break;
+                }
+                seen[candNode] = true;
+                if (graph.containsKey(candNode)) {
+                    for (int[] info : graph.get(candNode)) {
+                        dist.put(info[0],
+                                Math.min(dist.get(info[0]), dist.get(candNode) + info[1]));
+                    }
+                }
+            }
+
+            int ans = 0;
+            for (int cand : dist.values()) {
+                if (cand == Integer.MAX_VALUE) {
+                    return -1;
+                }
+                ans = Math.max(ans, cand);
+            }
+            return ans;
+        }
+    }
+
 }
