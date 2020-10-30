ccf190889dc2643eca50eb448bfb8a5057a1fbb6
xuweng <2028403305@qq.com>
2020-10-30 11:49:09 +0800

    463. 岛屿的周长
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/IslandPerimeter.java
@@ -0,0 +1,43 @@
+package com.leetcode.tag.daily.four;
+
+/**
+ * 463. 岛屿的周长
+ */
+public class IslandPerimeter {
+    /**
+     * 方法一：迭代
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/island-perimeter/solution/dao-yu-de-zhou-chang-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution {
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+
+        public int islandPerimeter(int[][] grid) {
+            int n = grid.length, m = grid[0].length;
+            int ans = 0;
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < m; ++j) {
+                    if (grid[i][j] == 0) {
+                        continue;
+                    }
+                    int cnt = 0;
+                    for (int k = 0; k < dx.length; ++k) {
+                        int tx = i + dx[k];
+                        int ty = j + dy[k];
+                        if (tx < 0 || tx >= n || ty < 0 || ty >= m || grid[tx][ty] == 0) {
+                            // 看其四个方向是否为边界或者水域
+                            cnt += 1;
+                        }
+                    }
+                    ans += cnt;
+                }
+            }
+            return ans;
+        }
+    }
+
+}
