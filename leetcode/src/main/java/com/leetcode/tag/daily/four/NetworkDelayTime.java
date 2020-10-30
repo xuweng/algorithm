e151e7bbf423a072eb3f62b6c736cfff6a58fd51
xuweng <2028403305@qq.com>
2020-10-30 12:18:54 +0800

    743. 网络延迟时间
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/NetworkDelayTime.java
@@ -0,0 +1,52 @@
+package com.leetcode.tag.daily.four;
+
+import java.util.*;
+
+/**
+ * 743. 网络延迟时间
+ * <p>
+ * 递归终止条件
+ * <p>
+ * 递归终止条件
+ */
+public class NetworkDelayTime {
+    class Solution {
+        Map<Integer, List<int[]>> map = new HashMap<>();
+        int[] dist;
+
+        public int networkDelayTime(int[][] times, int N, int K) {
+            if (times == null || times.length == 0) {
+                return 0;
+            }
+            for (int[] time : times) {
+                map.computeIfAbsent(time[0], k -> new ArrayList<>()).add(new int[]{time[1], time[2]});
+            }
+            dist = new int[N + 1];
+            Arrays.fill(dist, Integer.MAX_VALUE);
+
+            dfs(K, 0);
+
+            int result = 0;
+            for (int i : dist) {
+                if (i == Integer.MAX_VALUE) {
+                    return -1;
+                }
+                result = Math.max(result, i);
+            }
+            return result;
+        }
+
+        private void dfs(int i, int dis) {
+            if (dis >= dist[i]) {
+                return;
+            }
+            dist[i] = dis;
+            if (!map.containsKey(i)) {
+                return;
+            }
+            for (int[] ints : map.get(i)) {
+                dfs(ints[0], dis + ints[1]);
+            }
+        }
+    }
+}
