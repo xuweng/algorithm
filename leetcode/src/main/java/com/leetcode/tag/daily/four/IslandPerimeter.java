592be663344cc95dfa5b482e9af71f5efb9049f9
xuweng <2028403305@qq.com>
2020-10-30 11:52:48 +0800

    方法二：深度优先搜索
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/four/IslandPerimeter.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/IslandPerimeter.java
@@ -40,4 +40,48 @@ public class IslandPerimeter {
         }
     }
 
+    /**
+     * 方法二：深度优先搜索
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/island-perimeter/solution/dao-yu-de-zhou-chang-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+
+        public int islandPerimeter(int[][] grid) {
+            int n = grid.length, m = grid[0].length;
+            int ans = 0;
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < m; ++j) {
+                    if (grid[i][j] == 0) {
+                        continue;
+                    }
+                    ans += dfs(i, j, grid, n, m);
+                }
+            }
+            return ans;
+        }
+
+        public int dfs(int x, int y, int[][] grid, int n, int m) {
+            if (x < 0 || x >= n || y < 0 || y >= m || grid[x][y] == 0) {
+                return 1;
+            }
+            if (grid[x][y] == 2) {
+                return 0;
+            }
+            grid[x][y] = 2;
+            int res = 0;
+            for (int i = 0; i < 4; ++i) {
+                int tx = x + dx[i];
+                int ty = y + dy[i];
+                res += dfs(tx, ty, grid, n, m);
+            }
+            return res;
+        }
+    }
+
 }
