8b86a053768ebd9c0550546245b9be7d80f60fd8
xuweng <2028403305@qq.com>
2020-10-31 12:02:51 +0800

    方法二: 广度优先搜索
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/three/CanFinish.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/three/CanFinish.java
@@ -1,14 +1,13 @@
 package com.leetcode.tag.dfs.three;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 207. 课程表
  * <p>
  * 图:环.父结点.相同结点.
+ * <p>
+ * 拓扑排序.有向无环图.
  */
 public class CanFinish {
     class Solution {
@@ -54,4 +53,54 @@ public class CanFinish {
             return false;
         }
     }
+
+    /**
+     * 方法二: 广度优先搜索
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/course-schedule/solution/ke-cheng-biao-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        List<List<Integer>> edges;
+        int[] indeg;
+
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            edges = new ArrayList<>();
+            for (int i = 0; i < numCourses; ++i) {
+                edges.add(new ArrayList<>());
+            }
+            indeg = new int[numCourses];
+            for (int[] info : prerequisites) {
+                edges.get(info[1]).add(info[0]);
+                ++indeg[info[0]];
+            }
+
+            // 队列保存入度为0的结点
+            Queue<Integer> queue = new LinkedList<>();
+            for (int i = 0; i < numCourses; ++i) {
+                if (indeg[i] == 0) {
+                    queue.offer(i);
+                }
+            }
+
+            int visited = 0;
+            while (!queue.isEmpty()) {
+                ++visited;
+                int u = queue.poll();
+                for (int v : edges.get(u)) {
+                    // 邻接顶点入度减1
+                    --indeg[v];
+                    if (indeg[v] == 0) {
+                        // 入度为0入队
+                        queue.offer(v);
+                    }
+                }
+            }
+
+            return visited == numCourses;
+        }
+    }
+
 }
