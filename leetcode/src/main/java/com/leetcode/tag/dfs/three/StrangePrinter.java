a5a38eba254cf3ca8755beccf530d1a752c74f71
xuweng <2028403305@qq.com>
2020-11-02 11:39:28 +0800

    区间DP
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/three/StrangePrinter.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/three/StrangePrinter.java
@@ -52,4 +52,37 @@ public class StrangePrinter {
         }
     }
 
+    /**
+     * 区间DP
+     * <p>
+     * 作者：GitKid
+     * 链接：https://leetcode-cn.com/problems/strange-printer/solution/java-yan-shi-dpfen-xi-fa-qu-jian-dp-qing-xi-yi-don/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int strangePrinter(String s) {
+            int n = s.length();
+            if (n == 0) {
+                return 0;
+            }
+            int[][] dp = new int[n + 1][n + 1];
+            for (int i = 0; i < n; i++) {
+                dp[i][i] = 1;
+            }
+            for (int len = 2; len <= n; len++) {
+                for (int i = 0; i + len - 1 < n; i++) {
+                    int j = i + len - 1;
+                    dp[i][j] = dp[i + 1][j] + 1;
+                    for (int k = i + 1; k <= j; k++) {
+                        if (s.charAt(i) == s.charAt(k)) {
+                            dp[i][j] = Math.min(dp[i][j], dp[i][k - 1] + dp[k + 1][j]);
+                        }
+                    }
+                }
+            }
+            return dp[0][n - 1];
+        }
+    }
+
 }
