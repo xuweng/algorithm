a571ef332888c2009bae5fa568de87569fb366c6
xuweng <2028403305@qq.com>
2020-11-03 11:02:57 +0800

    面试题 08.10. 颜色填充
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/three/FloodFill.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/three/FloodFill.java
@@ -7,17 +7,21 @@ public class FloodFill {
     class Solution {
         int[] rows = {-1, 1, 0, 0};
         int[] cols = {0, 0, -1, 1};
+        boolean[][] visited;
 
         public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+            visited = new boolean[image.length][image[0].length];
+
             dfs(image, sr, sc, newColor, image[sr][sc]);
 
             return image;
         }
 
         private void dfs(int[][] image, int row, int col, int newColor, int old) {
-            if (row < 0 || row >= image.length || col < 0 || col >= image[0].length || image[row][col] != old) {
+            if (row < 0 || row >= image.length || col < 0 || col >= image[0].length || image[row][col] != old || visited[row][col]) {
                 return;
             }
+            visited[row][col] = true;
             image[row][col] = newColor;
             for (int i = 0; i < rows.length; i++) {
                 dfs(image, row + rows[i], col + cols[i], newColor, old);
