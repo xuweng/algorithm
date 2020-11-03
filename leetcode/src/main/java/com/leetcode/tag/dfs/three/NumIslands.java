4bbb30042573c4aff9dfab84707a1f1c0b0d9290
xuweng <2028403305@qq.com>
2020-11-03 11:34:51 +0800

    方法二：广度优先搜索
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/three/NumIslands.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/three/NumIslands.java
@@ -1,5 +1,8 @@
 package com.leetcode.tag.dfs.three;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 200. 岛屿数量
  */
@@ -36,4 +39,60 @@ public class NumIslands {
             }
         }
     }
+
+    /**
+     * 方法二：广度优先搜索
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/number-of-islands/solution/dao-yu-shu-liang-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int numIslands(char[][] grid) {
+            if (grid == null || grid.length == 0) {
+                return 0;
+            }
+
+            int nr = grid.length;
+            int nc = grid[0].length;
+            int numIslands = 0;
+
+            for (int r = 0; r < nr; ++r) {
+                for (int c = 0; c < nc; ++c) {
+                    if (grid[r][c] != '1') {
+                        continue;
+                    }
+                    ++numIslands;
+                    grid[r][c] = '0';
+                    Queue<Integer> neighbors = new LinkedList<>();
+                    neighbors.add(r * nc + c);
+                    while (!neighbors.isEmpty()) {
+                        int id = neighbors.remove();
+                        int row = id / nc;
+                        int col = id % nc;
+                        if (row - 1 >= 0 && grid[row - 1][col] == '1') {
+                            neighbors.add((row - 1) * nc + col);
+                            grid[row - 1][col] = '0';
+                        }
+                        if (row + 1 < nr && grid[row + 1][col] == '1') {
+                            neighbors.add((row + 1) * nc + col);
+                            grid[row + 1][col] = '0';
+                        }
+                        if (col - 1 >= 0 && grid[row][col - 1] == '1') {
+                            neighbors.add(row * nc + col - 1);
+                            grid[row][col - 1] = '0';
+                        }
+                        if (col + 1 < nc && grid[row][col + 1] == '1') {
+                            neighbors.add(row * nc + col + 1);
+                            grid[row][col + 1] = '0';
+                        }
+                    }
+                }
+            }
+
+            return numIslands;
+        }
+    }
+
 }
