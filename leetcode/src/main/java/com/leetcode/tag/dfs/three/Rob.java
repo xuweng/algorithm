ae84b46bc9aeded348a2bb700c2d59d9ef1f2e8a
xuweng <2028403305@qq.com>
2020-11-04 11:51:32 +0800

    每个点有两种状态（选中和不选中）
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/three/Rob.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/three/Rob.java
@@ -36,6 +36,32 @@ public class Rob {
         }
     }
 
+    /**
+     * 每个点有两种状态（选中和不选中）
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/house-robber-iii/solution/da-jia-jie-she-iii-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int rob(TreeNode root) {
+            int[] rootStatus = dfs(root);
+            return Math.max(rootStatus[0], rootStatus[1]);
+        }
+
+        public int[] dfs(TreeNode node) {
+            if (node == null) {
+                return new int[]{0, 0};
+            }
+            int[] l = dfs(node.left);
+            int[] r = dfs(node.right);
+            int selected = node.val + l[1] + r[1];
+            int notSelected = Math.max(l[0], l[1]) + Math.max(r[0], r[1]);
+            return new int[]{selected, notSelected};
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
