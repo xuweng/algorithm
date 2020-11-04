a1c4c74eb07c1fa13cac697ecb6814a90e8accee
xuweng <2028403305@qq.com>
2020-11-04 14:43:07 +0800

    剑指 Offer 53 - II. 0～n-1中缺失的数字
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/binarysearch/one/MissingNumber.java
@@ -0,0 +1,29 @@
+package com.leetcode.tag.binarysearch.one;
+
+/**
+ * 剑指 Offer 53 - II. 0～n-1中缺失的数字
+ */
+public class MissingNumber {
+    class Solution {
+        public int missingNumber(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return -1;
+            }
+            return bs(nums, 0, nums.length - 1);
+        }
+
+        private int bs(int[] nums, int low, int high) {
+            if (low > high) {
+                return -1;
+            }
+            int mid = low + (high - low) / 2;
+            if (nums[mid] != mid) {
+                return mid;
+            } else if (nums[mid] - nums[low] == mid - low) {
+                return bs(nums, mid, high);
+            } else {
+                return bs(nums, low, mid);
+            }
+        }
+    }
+}
