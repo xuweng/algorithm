8052646c35b18c806b6b94d84cd2911b20ce882f
xuweng <2028403305@qq.com>
2020-11-04 10:46:21 +0800

    方法二： 并查集
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dfs/three/RemoveStones.java
+++ leetcode/src/main/java/com/leetcode/tag/dfs/three/RemoveStones.java
@@ -1,5 +1,7 @@
 package com.leetcode.tag.dfs.three;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Stack;
 
 /**
@@ -56,6 +58,48 @@ public class RemoveStones {
             }
 
             return ans;
+        }
+    }
+
+    /**
+     * 方法二： 并查集
+     */
+    class Solution1 {
+        public int removeStones(int[][] stones) {
+            DSU dsu = new DSU(20000);
+
+            for (int[] stone : stones) {
+                dsu.union(stone[0], stone[1] + 10000);
+            }
+
+            Set<Integer> seen = new HashSet<>();
+            for (int[] stone : stones) {
+                seen.add(dsu.find(stone[0]));
+            }
+
+            return stones.length - seen.size();
+        }
+    }
+
+    class DSU {
+        int[] parent;
+
+        public DSU(int n) {
+            parent = new int[n];
+            for (int i = 0; i < n; ++i) {
+                parent[i] = i;
+            }
+        }
+
+        public int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        public void union(int x, int y) {
+            parent[find(x)] = find(y);
         }
     }
 
