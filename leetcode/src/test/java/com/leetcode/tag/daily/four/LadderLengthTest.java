de75c26dbfe59fca893ea4b1b6ce1203011f64f0
xuweng <2028403305@qq.com>
2020-11-05 23:09:59 +0800

    127. 单词接龙
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/test/java/com/leetcode/tag/daily/four/LadderLengthTest.java
+++ leetcode/src/test/java/com/leetcode/tag/daily/four/LadderLengthTest.java
@@ -16,4 +16,14 @@ public class LadderLengthTest {
 
         solution.ladderLength(beginWord, endWord, wordList);
     }
+
+
+    @Test
+    public void ladderLength1() {
+        String beginWord = "qa";
+        String endWord = "sq";
+        List<String> wordList = Arrays.asList("si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye");
+
+        solution.ladderLength(beginWord, endWord, wordList);
+    }
 }
