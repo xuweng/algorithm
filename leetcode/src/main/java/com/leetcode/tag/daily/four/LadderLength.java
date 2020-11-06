1abac19444d58fd55b67c116fa5e6fb7582320ca
xuweng <2028403305@qq.com>
2020-11-06 10:20:16 +0800

    重复计算
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/four/LadderLength.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/LadderLength.java
@@ -1,8 +1,6 @@
 package com.leetcode.tag.daily.four;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -37,6 +35,61 @@ public class LadderLength {
                 return;
             }
             memo.add(beginWord);
+
+            set.add(beginWord);
+            List<String> strings = getList(beginWord, wordList);
+            for (String string : strings) {
+                dfs(string, endWord, wordList, count + 1);
+            }
+            set.remove(beginWord);
+        }
+
+        private List<String> getList(String beginWord, List<String> wordList) {
+            return wordList.stream().filter(s -> check(beginWord, s) && !set.contains(s)).collect(Collectors.toList());
+        }
+
+        private boolean check(String s, String s1) {
+            if (s.length() != s1.length()) {
+                return false;
+            }
+            int count = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) != s1.charAt(i)) {
+                    count++;
+                }
+            }
+            return count == 1;
+        }
+    }
+
+    class Solution1 {
+        int min = Integer.MAX_VALUE;
+        Set<String> set = new HashSet<>();
+        Map<String, Integer> memo = new HashMap<>();
+
+        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+            dfs(beginWord, endWord, wordList, 1);
+
+            return min == Integer.MAX_VALUE ? 0 : min;
+        }
+
+        /**
+         * 重复计算
+         *
+         * @param beginWord
+         * @param endWord
+         * @param wordList
+         * @param count
+         */
+        private void dfs(String beginWord, String endWord, List<String> wordList, int count) {
+            if (beginWord.equals(endWord)) {
+                min = Math.min(min, count);
+                return;
+            }
+            if (memo.containsKey(beginWord) && memo.get(beginWord) <= count) {
+                return;
+            }
+            memo.put(beginWord, count);
 
             set.add(beginWord);
             List<String> strings = getList(beginWord, wordList);
