b32ae81f57010cd8d175e11d94a6db804f4539c4
xuweng <2028403305@qq.com>
2020-11-06 10:50:04 +0800

    重复计算
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/four/LadderLength.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/LadderLength.java
@@ -117,4 +117,62 @@ public class LadderLength {
             return count == 1;
         }
     }
+
+    class Solution2 {
+        int min = Integer.MAX_VALUE;
+        Set<String> set = new HashSet<>();
+        Map<String, Integer> memo = new HashMap<>();
+
+        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+            dfs(beginWord, endWord, wordList, 1);
+
+            return min == Integer.MAX_VALUE ? 0 : min;
+        }
+
+        /**
+         * 重复计算
+         *
+         * @param beginWord
+         * @param endWord
+         * @param wordList
+         * @param count
+         */
+        private int dfs(String beginWord, String endWord, List<String> wordList, int count) {
+            if (beginWord.equals(endWord)) {
+                min = Math.min(min, count);
+                return 0;
+            }
+            if (memo.containsKey(beginWord)) {
+                min = Math.min(min, count + memo.get(beginWord));
+                return memo.get(beginWord);
+            }
+
+            set.add(beginWord);
+            List<String> strings = getList(beginWord, wordList);
+            int result = Integer.MAX_VALUE;
+            for (String string : strings) {
+                result = Math.min(result, dfs(string, endWord, wordList, count + 1) + 1);
+            }
+            set.remove(beginWord);
+            memo.put(beginWord, result);
+            return result;
+        }
+
+        private List<String> getList(String beginWord, List<String> wordList) {
+            return wordList.stream().filter(s -> check(beginWord, s) && !set.contains(s)).collect(Collectors.toList());
+        }
+
+        private boolean check(String s, String s1) {
+            if (s.length() != s1.length()) {
+                return false;
+            }
+            int count = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) != s1.charAt(i)) {
+                    count++;
+                }
+            }
+            return count == 1;
+        }
+    }
 }
