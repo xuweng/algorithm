bc07ed6f97973fb19f8a3ed83691a6bf2a6711e5
xuweng <2028403305@qq.com>
2020-11-07 14:57:17 +0800

    69. x 的平方根
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/binarysearch/one/MySqrt.java
@@ -0,0 +1,26 @@
+package com.leetcode.tag.binarysearch.one;
+
+/**
+ * 69. x 的平方根
+ */
+public class MySqrt {
+    class Solution {
+        public int mySqrt(int x) {
+            return bs(x, 0, x);
+        }
+
+        private int bs(int x, int left, int right) {
+            if (left > right) {
+                return left;
+            }
+            int mid = left + (right - left) / 2;
+            if (mid * mid == x) {
+                return mid;
+            } else if (mid * mid > x) {
+                return bs(x, left, mid - 1);
+            } else {
+                return bs(x, mid + 1, right);
+            }
+        }
+    }
+}
