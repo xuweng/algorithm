b10b32e1604d166f09dadfd89f1ab94d2b140ffe
xuweng <2028403305@qq.com>
2020-11-07 11:33:06 +0800

    面试题 10.05. 稀疏数组搜索
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/binarysearch/one/FindString.java
+++ leetcode/src/main/java/com/leetcode/tag/binarysearch/one/FindString.java
@@ -1,5 +1,7 @@
 package com.leetcode.tag.binarysearch.one;
 
+import java.util.stream.IntStream;
+
 /**
  * 面试题 10.05. 稀疏数组搜索
  */
@@ -9,12 +11,7 @@ public class FindString {
             if (words == null || words.length == 0 || s == null) {
                 return -1;
             }
-            for (int i = 0; i < words.length; i++) {
-                if (words[i].equals(s)) {
-                    return i;
-                }
-            }
-            return -1;
+            return IntStream.range(0, words.length).filter(i -> words[i].equals(s)).findFirst().orElse(-1);
         }
     }
 }
