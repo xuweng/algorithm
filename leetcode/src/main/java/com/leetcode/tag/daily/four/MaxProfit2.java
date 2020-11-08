23495d40e3e61ffe547becfffd8734d80ad4c441
xuweng <2028403305@qq.com>
2020-11-08 17:04:03 +0800

    dp
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/four/MaxProfit2.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/MaxProfit2.java
@@ -170,4 +170,34 @@ public class MaxProfit2 {
         }
     }
 
+    class Solution3 {
+        public int maxProfit(int[] prices) {
+            if (prices == null || prices.length == 0) {
+                return 0;
+            }
+            int[][][] dp = new int[prices.length][3][2];
+            dp[0][0][0] = 0;
+            dp[0][0][1] = -prices[0];
+
+            dp[0][1][0] = 0;
+            dp[0][1][1] = -prices[0];
+
+            dp[0][2][0] = 0;
+            dp[0][2][1] = -prices[0];
+
+            // 必须在再次购买前出售掉之前的股票(前i-1天有股票,先卖股票,第i天才买股票)
+            // 卖股票才算一次交易
+            for (int i = 1; i < prices.length; i++) {
+                dp[i][0][0] = dp[i - 1][0][0];
+
+                dp[i][1][0] = Math.max(dp[i - 1][1][0], dp[i - 1][0][1] + prices[i]);
+                dp[i][1][1] = Math.max(dp[i - 1][1][1], dp[i - 1][0][0] - prices[i]);
+
+                dp[i][2][0] = Math.max(dp[i - 1][2][0], dp[i - 1][1][1] + prices[i]);
+            }
+
+            return Math.max(dp[dp.length - 1][0][0], Math.max(dp[dp.length - 1][1][0], dp[dp.length - 1][2][0]));
+        }
+    }
+
 }
