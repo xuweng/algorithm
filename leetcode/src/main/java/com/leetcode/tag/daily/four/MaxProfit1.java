6c63baf158c3736d46bc1efb268a21d7684a7cac
xuweng <2028403305@qq.com>
2020-11-08 15:54:21 +0800

    121. 买卖股票的最佳时机
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/four/MaxProfit1.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/MaxProfit1.java
@@ -18,4 +18,31 @@ public class MaxProfit1 {
             return result;
         }
     }
+
+    /**
+     * 方法二：一次遍历
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/solution/121-mai-mai-gu-piao-de-zui-jia-shi-ji-by-leetcode-/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int maxProfit(int prices[]) {
+            // 最小值
+            int minPrice = Integer.MAX_VALUE;
+            int maxProfit = 0;
+            for (int price : prices) {
+                if (price < minPrice) {
+                    // 更新最小股票
+                    minPrice = price;
+                } else {
+                    // 当前股票大于最小股票
+                    maxProfit = Math.max(maxProfit, price - minPrice);
+                }
+            }
+            return maxProfit;
+        }
+    }
+
 }
