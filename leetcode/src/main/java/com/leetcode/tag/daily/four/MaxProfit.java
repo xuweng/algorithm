ba34890f52c38be078b3bbf35700630a32773625
xuweng <2028403305@qq.com>
2020-11-08 13:27:40 +0800

    方法二：贪心
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/four/MaxProfit.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/four/MaxProfit.java
@@ -108,4 +108,23 @@ public class MaxProfit {
         }
     }
 
+    /**
+     * 方法二：贪心
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/solution/mai-mai-gu-piao-de-zui-jia-shi-ji-ii-by-leetcode-s/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution4 {
+        public int maxProfit(int[] prices) {
+            int ans = 0;
+            int n = prices.length;
+            for (int i = 1; i < n; ++i) {
+                ans += Math.max(0, prices[i] - prices[i - 1]);
+            }
+            return ans;
+        }
+    }
+
 }
