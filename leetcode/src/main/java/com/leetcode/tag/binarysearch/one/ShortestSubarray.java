341bc47b1ad2c96427bc8806cd906547773226ef
xuweng <2028403305@qq.com>
2020-11-09 10:11:36 +0800

    862. 和至少为 K 的最短子数组
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/binarysearch/one/ShortestSubarray.java
@@ -0,0 +1,30 @@
+package com.leetcode.tag.binarysearch.one;
+
+/**
+ * 862. 和至少为 K 的最短子数组
+ * <p>
+ * 买股票:交易次数不会改变
+ * <p>
+ * 卖股票:交易次数+1
+ */
+public class ShortestSubarray {
+    class Solution {
+        public int shortestSubarray(int[] A, int K) {
+            if (A == null || A.length == 0) {
+                return -1;
+            }
+            int result = -1;
+            for (int i = 0; i < A.length; i++) {
+                int sum = A[i];
+                for (int j = i + 1; j < A.length; j++) {
+                    sum += A[j];
+                    if (sum >= K) {
+                        result = Math.min(result, j - i + 1);
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
