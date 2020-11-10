caa92b88921c9abb41f275cda19b2abc94832ae2
xuweng <2028403305@qq.com>
2020-11-10 22:23:25 +0800

    1292. 元素和小于等于阈值的正方形的最大边长
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/binarysearch/one/MaxSideLength.java
@@ -0,0 +1,46 @@
+package com.leetcode.tag.binarysearch.one;
+
+/**
+ * 1292. 元素和小于等于阈值的正方形的最大边长
+ * <p>
+ * 经典题目.经典题目.经典题目.经典题目.
+ */
+public class MaxSideLength {
+    /**
+     * 解法一 前缀和
+     * <p>
+     * 作者：hlxing
+     * 链接：https://leetcode-cn.com/problems/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/solution/qing-xi-tu-jie-mo-neng-de-qian-zhui-he-by-hlxing/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution {
+        public int maxSideLength(int[][] mat, int threshold) {
+            int m = mat.length, n = mat[0].length;
+            //二维前缀和
+            //引入二维前缀和的计算方法，通过预处理可以在 O(1) 时间内计算出一块区域内元素的总和
+            int[][] dp = new int[m + 1][n + 1];
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    dp[i][j] = mat[i - 1][j - 1] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
+                }
+            }
+            int ans = 0;
+            for (int k = 1; k <= Math.min(m, n); k++) {
+                for (int i = 1; i <= m; i++) {
+                    for (int j = 1; j <= n; j++) {
+                        if (i - k < 0 || j - k < 0) {
+                            continue;
+                        }
+                        int tmp = dp[i][j] - dp[i - k][j] - dp[i][j - k] + dp[i - k][j - k];
+                        if (tmp <= threshold) {
+                            ans = Math.max(ans, k);
+                        }
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+
+}
