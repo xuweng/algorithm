45694fd6926154a2c57c28592f1daa940c4c960d
xuweng <2028403305@qq.com>
2020-11-10 09:07:57 +0800

    最长上升子序列
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/binarysearch/one/LengthOfLIS2.java
@@ -0,0 +1,37 @@
+package com.leetcode.tag.binarysearch.one;
+
+/**
+ * 最长上升子序列
+ */
+public class LengthOfLIS2 {
+    /**
+     * 方法一：动态规划
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/longest-increasing-subsequence/solution/zui-chang-shang-sheng-zi-xu-lie-by-leetcode-soluti/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution {
+        public int lengthOfLIS(int[] nums) {
+            if (nums.length == 0) {
+                return 0;
+            }
+            //定义 dp[i] 为考虑前 i 个元素，以第 i 个数字结尾的最长上升子序列的长度，注意 nums[i] 必须被选取。
+            int[] dp = new int[nums.length];
+            dp[0] = 1;
+            int maxans = 1;
+            for (int i = 1; i < nums.length; i++) {
+                dp[i] = 1;
+                for (int j = 0; j < i; j++) {
+                    if (nums[i] > nums[j]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                }
+                maxans = Math.max(maxans, dp[i]);
+            }
+            return maxans;
+        }
+    }
+
+}
