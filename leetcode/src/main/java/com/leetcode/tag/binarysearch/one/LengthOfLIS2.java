af33b58b67150ba3a7a70dd65cdbc6214a07eca1
xuweng <2028403305@qq.com>
2020-11-10 09:12:46 +0800

    最长上升子序列
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/binarysearch/one/LengthOfLIS2.java
+++ leetcode/src/main/java/com/leetcode/tag/binarysearch/one/LengthOfLIS2.java
@@ -34,4 +34,41 @@ public class LengthOfLIS2 {
         }
     }
 
+    /**
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/longest-increasing-subsequence/solution/zui-chang-shang-sheng-zi-xu-lie-by-leetcode-soluti/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int lengthOfLIS(int[] nums) {
+            int len = 1, n = nums.length;
+            if (n == 0) {
+                return 0;
+            }
+            int[] d = new int[n + 1];
+            d[len] = nums[0];
+            for (int i = 1; i < n; ++i) {
+                if (nums[i] > d[len]) {
+                    d[++len] = nums[i];
+                } else {
+                    // 二分查找第一个比i大的数据
+                    int l = 1, r = len, pos = 0; // 如果找不到说明所有的数都比 nums[i] 大，此时要更新 d[1]，所以这里将 pos 设为 0
+                    while (l <= r) {
+                        int mid = (l + r) >> 1;
+                        if (d[mid] < nums[i]) {
+                            pos = mid;
+                            l = mid + 1;
+                        } else {
+                            r = mid - 1;
+                        }
+                    }
+                    // i赋值
+                    d[pos + 1] = nums[i];
+                }
+            }
+            return len;
+        }
+    }
+
 }
