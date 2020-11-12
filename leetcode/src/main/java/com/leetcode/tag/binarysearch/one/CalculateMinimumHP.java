761aa52adc9152ae4092831d586cf12c9dce30b1
xuweng <2028403305@qq.com>
2020-11-12 21:19:36 +0800

    解法2：DFS + 记忆化
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/binarysearch/one/CalculateMinimumHP.java
+++ leetcode/src/main/java/com/leetcode/tag/binarysearch/one/CalculateMinimumHP.java
@@ -211,4 +211,38 @@ public class CalculateMinimumHP {
         }
     }
 
+    /**
+     * 解法2：DFS + 记忆化
+     */
+    class Solution6 {
+        int[][] memo; // 定义记忆化数组
+
+        public int calculateMinimumHP(int[][] dungeon) {
+            memo = new int[dungeon.length][dungeon[0].length];
+            return dfs(dungeon, dungeon.length, dungeon[0].length, 0, 0);
+        }
+
+        private int dfs(int[][] dungeon, int m, int n, int i, int j) {
+            // 到达终点，递归终止。
+            if (i == m - 1 && j == n - 1) {
+                return Math.max(1 - dungeon[i][j], 1);
+            }
+            // 如果memo数组中有值，直接取出并返回，不进行后续的搜索。
+            if (memo[i][j] > 0) {
+                return memo[i][j];
+            }
+            // 同解法一，向右搜+向下搜
+            int minRes;
+            if (i == m - 1) {
+                minRes = Math.max(dfs(dungeon, m, n, i, j + 1) - dungeon[i][j], 1);
+            } else if (j == n - 1) {
+                minRes = Math.max(dfs(dungeon, m, n, i + 1, j) - dungeon[i][j], 1);
+            } else {
+                minRes = Math.max(Math.min(dfs(dungeon, m, n, i + 1, j), dfs(dungeon, m, n, i, j + 1)) - dungeon[i][j], 1);
+            }
+            // 将结果存入memo数组
+            return memo[i][j] = minRes;
+        }
+    }
+
 }
