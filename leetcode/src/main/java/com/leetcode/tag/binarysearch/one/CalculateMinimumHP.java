514bfb6fe941f1bd6f39f236fd551a6e323e8a03
xuweng <2028403305@qq.com>
2020-11-12 20:50:18 +0800

    174. 地下城游戏
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/binarysearch/one/CalculateMinimumHP.java
+++ leetcode/src/main/java/com/leetcode/tag/binarysearch/one/CalculateMinimumHP.java
@@ -63,27 +63,25 @@ public class CalculateMinimumHP {
             if (dungeon == null || dungeon.length == 0 || dungeon[0].length == 0) {
                 return 0;
             }
-            int rowLen = dungeon.length;
-            int colLen = dungeon[0].length;
             // 最低的耗血量为 + 1；就是骑士的救公主的最低血量。
-            return dfs(0, 0, rowLen, colLen, dungeon) + 1;
+            return dfs(0, 0, dungeon) + 1;
         }
 
-        public int dfs(int rowIndex, int colIndex, int rowSize, int colSize, int[][] dungeon) {
-            if (rowIndex >= rowSize || colIndex >= colSize) {
+        public int dfs(int rowIndex, int colIndex, int[][] dungeon) {
+            if (rowIndex >= dungeon.length || colIndex >= dungeon[0].length) {
                 return Integer.MAX_VALUE;
             }
             // 退出条件
-            if (rowIndex == rowSize - 1 && colIndex == colSize - 1) {
+            if (rowIndex == dungeon.length - 1 && colIndex == dungeon[0].length - 1) {
                 // 最后一格
                 // 如果最后一个大于等于0，就返还0。
                 //如果最后一个小于零，就返回负的值。
                 return dungeon[rowIndex][colIndex] >= 0 ? 0 : -dungeon[rowIndex][colIndex];
             }
             //  右边格子的最优解，也就是最低的耗血量
-            int rightMin = dfs(rowIndex, colIndex + 1, rowSize, colSize, dungeon);
+            int rightMin = dfs(rowIndex, colIndex + 1, dungeon);
             //  下边格子的最优解
-            int downMin = dfs(rowIndex + 1, colIndex, rowSize, colSize, dungeon);
+            int downMin = dfs(rowIndex + 1, colIndex, dungeon);
             // f(i,j) = min(f(i+1, j), f(i, j+1)) - dungeon[i][j]
             int needMin = Math.min(rightMin, downMin) - dungeon[rowIndex][colIndex];
             return Math.max(needMin, 0);
