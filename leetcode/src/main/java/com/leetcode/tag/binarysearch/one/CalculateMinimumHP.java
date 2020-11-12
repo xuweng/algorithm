36fe709f38f61f5f9092aff5de3f3e3f1d892286
xuweng <2028403305@qq.com>
2020-11-12 21:07:11 +0800

    从下到上.从右到左计算.
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/binarysearch/one/CalculateMinimumHP.java
+++ leetcode/src/main/java/com/leetcode/tag/binarysearch/one/CalculateMinimumHP.java
@@ -129,10 +129,55 @@ public class CalculateMinimumHP {
             //  下边格子的最优解
             int downMin = dfs(rowIndex + 1, colIndex, dungeon);
             // f(i,j) = min(f(i+1, j), f(i, j+1)) - dungeon[i][j]
+            // 当前格子为正数或者为负数.
             int needMin = Math.min(rightMin, downMin) - dungeon[rowIndex][colIndex];
 
             memory[rowIndex][colIndex] = Math.max(needMin, 0);
             return memory[rowIndex][colIndex];
+        }
+
+    }
+
+    /**
+     * 作者：fakerleet
+     * 链接：https://leetcode-cn.com/problems/dungeon-game/solution/cong-hui-su-dao-ji-yi-hua-sou-suo-dao-dong-tai-gui/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution4 {
+        public int calculateMinimumHPBest(int[][] dungeon) {
+            if (dungeon == null || dungeon.length == 0 || dungeon[0].length == 0) {
+                return 0;
+            }
+            int rowSize = dungeon.length;
+            int colSize = dungeon[0].length;
+            int[][] dp = new int[rowSize][colSize];
+            // 设置最后一个值。
+            dp[rowSize - 1][colSize - 1] = Math.max(0, -dungeon[rowSize - 1][colSize - 1]);
+
+            // 设置最后一列的值
+            for (int i = rowSize - 2; i >= 0; --i) {
+                int needMin = dp[i + 1][colSize - 1] - dungeon[i][colSize - 1];
+                dp[i][colSize - 1] = Math.max(0, needMin);
+            }
+
+            // 设置最后一行的值
+            for (int i = colSize - 2; i >= 0; --i) {
+                int needMin = dp[rowSize - 1][i + 1] - dungeon[rowSize - 1][i];
+                dp[rowSize - 1][i] = Math.max(0, needMin);
+            }
+
+            // 从下到上.从右到左计算.
+            // 倒数第二行开始
+            for (int i = rowSize - 2; i >= 0; --i) {
+                // 倒数第二列开始
+                for (int j = colSize - 2; j >= 0; --j) {
+                    // 从右边和下边选择一个最小值，然后减去当前的 dungeon 值
+                    int needMin = Math.min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j];
+                    dp[i][j] = Math.max(0, needMin);
+                }
+            }
+            return dp[0][0] + 1;
         }
 
     }
