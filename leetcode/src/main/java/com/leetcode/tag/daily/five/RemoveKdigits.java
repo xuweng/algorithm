3fbd100490e79a2ba2538931005f313f7f16fe27
xuweng <2028403305@qq.com>
2020-11-15 09:07:23 +0800

    402. 移掉K位数字
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/five/RemoveKdigits.java
@@ -0,0 +1,53 @@
+package com.leetcode.tag.daily.five;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 402. 移掉K位数字
+ */
+public class RemoveKdigits {
+    /**
+     * 方法一：贪心 + 单调栈
+     * <p>
+     * 要使得剩下的数字最小，需要保证靠前的数字尽可能小
+     * <p>
+     * 删除一个数字」的贪心策略：
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/remove-k-digits/solution/yi-diao-kwei-shu-zi-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution {
+        public String removeKdigits(String num, int k) {
+            Deque<Character> deque = new LinkedList<>();
+            int length = num.length();
+            for (int i = 0; i < length; ++i) {
+                char digit = num.charAt(i);
+                while (!deque.isEmpty() && k > 0 && deque.peekLast() > digit) {
+                    deque.pollLast();
+                    k--;
+                }
+                deque.offerLast(digit);
+            }
+
+            for (int i = 0; i < k; ++i) {
+                deque.pollLast();
+            }
+
+            StringBuilder ret = new StringBuilder();
+            boolean leadingZero = true;
+            while (!deque.isEmpty()) {
+                char digit = deque.pollFirst();
+                if (leadingZero && digit == '0') {
+                    continue;
+                }
+                leadingZero = false;
+                ret.append(digit);
+            }
+            return ret.length() == 0 ? "0" : ret.toString();
+        }
+    }
+
+}
