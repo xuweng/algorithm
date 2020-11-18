fb51ab06bd1424c777326b40da616feb2878a63e
xuweng <2028403305@qq.com>
2020-11-18 21:44:06 +0800

    300. 最长上升子序列
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/binarysearch/two/LengthOfLIS.java
@@ -0,0 +1,37 @@
+package com.leetcode.tag.binarysearch.two;
+
+/**
+ * 300. 最长上升子序列
+ * <p>
+ * 连续。不连续。
+ * <p>
+ * 数组的环计算。
+ */
+public class LengthOfLIS {
+    class Solution {
+        /**
+         * 不连续
+         *
+         * @param nums
+         * @return
+         */
+        public int lengthOfLIS(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return 0;
+            }
+            int[] dp = new int[nums.length];
+            dp[0] = 1;
+            int result = 1;
+            for (int i = 1; i < nums.length; i++) {
+                dp[i] = 1;
+                for (int j = 0; j < i; j++) {
+                    if (dp[i] > dp[j]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                }
+                result = Math.max(result, dp[i]);
+            }
+            return result;
+        }
+    }
+}
