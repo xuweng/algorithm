1ea992c02c891cf5fa931040c924550f6cfea56e
xuweng <2028403305@qq.com>
2020-11-19 21:28:32 +0800

    参考了快速排序的思想
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/five/MoveZeroes.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/five/MoveZeroes.java
@@ -36,4 +36,32 @@ public class MoveZeroes {
         }
     }
 
+    /**
+     * 参考了快速排序的思想
+     * <p>
+     * 可以用0当做这个point，把不等于0(注意题目没说不能有负数)的放到中间点的左边，等于0的放到其右边。
+     * <p>
+     * 作者：wang_ni_ma
+     * 链接：https://leetcode-cn.com/problems/move-zeroes/solution/dong-hua-yan-shi-283yi-dong-ling-by-wang_ni_ma/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public void moveZeroes(int[] nums) {
+            if (nums == null) {
+                return;
+            }
+            //两个指针i和j
+            int j = 0;
+            for (int i = 0; i < nums.length; i++) {
+                //当前元素!=0，就把其交换到左边，等于0的交换到右边
+                if (nums[i] != 0) {
+                    int tmp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j++] = tmp;
+                }
+            }
+        }
+    }
+
 }
