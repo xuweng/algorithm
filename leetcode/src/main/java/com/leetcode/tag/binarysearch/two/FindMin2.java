8dda3500a38d506af6c6110fe599025db97a80b0
xuweng <2028403305@qq.com>
2020-11-21 22:25:18 +0800

    方法一：二分查找
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/binarysearch/two/FindMin2.java
+++ leetcode/src/main/java/com/leetcode/tag/binarysearch/two/FindMin2.java
@@ -31,4 +31,44 @@ public class FindMin2 {
             return nums[mid] > nums[high] ? bs(nums, mid + 1, high) : bs(nums, low, mid);
         }
     }
+
+    /**
+     * 看图
+     * <p>
+     * 看图。看图。看图。看图。看图。看图。看图。看图。看图。看图。看图。看图。看图。看图。看图。看图。看图。看图。
+     * <p>
+     * 合并函数
+     * <p>
+     * 合并函数
+     * <p>
+     * 合并函数.合并函数.合并函数.
+     * <p>
+     * 快慢指针.
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof/solution/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-by-leetcode-s/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int minArray(int[] numbers) {
+            int low = 0;
+            int high = numbers.length - 1;
+            while (low < high) {
+                int pivot = low + (high - low) / 2;
+                if (numbers[pivot] < numbers[high]) {
+                    high = pivot;
+                } else if (numbers[pivot] > numbers[high]) {
+                    low = pivot + 1;
+                } else {
+                    // 重复元素
+                    // numbers[pivot]==numbers[high]
+                    // 排除numbers[high]
+                    high -= 1;
+                }
+            }
+            return numbers[low];
+        }
+    }
+
 }
