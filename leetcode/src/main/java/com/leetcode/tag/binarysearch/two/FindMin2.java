2a6b5b7c807e29932bcc5b186d57a09186f7a451
xuweng <2028403305@qq.com>
2020-11-21 22:32:47 +0800

    重复元素 线性查找
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/binarysearch/two/FindMin2.java
+++ leetcode/src/main/java/com/leetcode/tag/binarysearch/two/FindMin2.java
@@ -73,4 +73,29 @@ public class FindMin2 {
         }
     }
 
+    class Solution2 {
+        public int minArray(int[] numbers) {
+            int i = 0, j = numbers.length - 1;
+            while (i < j) {
+                int m = (i + j) / 2;
+                if (numbers[m] > numbers[j]) {
+                    i = m + 1;
+                } else if (numbers[m] < numbers[j]) {
+                    j = m;
+                } else {
+                    // 重复元素
+                    // 使用线性查找
+                    int x = i;
+                    for (int k = i + 1; k < j; k++) {
+                        if (numbers[k] < numbers[x]) {
+                            x = k;
+                        }
+                    }
+                    return numbers[x];
+                }
+            }
+            return numbers[i];
+        }
+    }
+
 }
