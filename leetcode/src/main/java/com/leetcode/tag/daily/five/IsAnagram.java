f90d0e65974329e397fcc1ddbb1eca89a7dfbd0d
xuweng <2028403305@qq.com>
2020-11-22 09:37:31 +0800

    242. 有效的字母异位词
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/five/IsAnagram.java
@@ -0,0 +1,37 @@
+package com.leetcode.tag.daily.five;
+
+import java.util.Arrays;
+
+/**
+ * 242. 有效的字母异位词
+ */
+public class IsAnagram {
+    /**
+     * 方法一：排序
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/valid-anagram/solution/you-xiao-de-zi-mu-yi-wei-ci-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution {
+        /**
+         * t 是 s 的异位词等价于「两个字符串排序后相等」
+         *
+         * @param s
+         * @param t
+         * @return
+         */
+        public boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) {
+                return false;
+            }
+            char[] str1 = s.toCharArray();
+            char[] str2 = t.toCharArray();
+            Arrays.sort(str1);
+            Arrays.sort(str2);
+            return Arrays.equals(str1, str2);
+        }
+    }
+
+}
