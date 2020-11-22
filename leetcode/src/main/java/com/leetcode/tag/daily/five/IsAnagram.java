9a93ffccb7e75c17fa59f04bc24fc1a045144842
xuweng <2028403305@qq.com>
2020-11-22 09:39:42 +0800

    方法二：哈希表
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/five/IsAnagram.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/five/IsAnagram.java
@@ -34,4 +34,31 @@ public class IsAnagram {
         }
     }
 
+    /**
+     * c
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/valid-anagram/solution/you-xiao-de-zi-mu-yi-wei-ci-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) {
+                return false;
+            }
+            int[] table = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                table[s.charAt(i) - 'a']++;
+            }
+            for (int i = 0; i < t.length(); i++) {
+                table[t.charAt(i) - 'a']--;
+                if (table[t.charAt(i) - 'a'] < 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
 }
