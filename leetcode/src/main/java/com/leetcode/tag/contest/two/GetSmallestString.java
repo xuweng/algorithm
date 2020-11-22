d8639961a4d2894f6fc7bfc00e2df6a102825467
xuweng <2028403305@qq.com>
2020-11-22 10:49:57 +0800

    5606. 具有给定数值的最小字符串
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/two/GetSmallestString.java
@@ -0,0 +1,12 @@
+package com.leetcode.tag.contest.two;
+
+/**
+ * 5606. 具有给定数值的最小字符串
+ */
+public class GetSmallestString {
+    class Solution {
+        public String getSmallestString(int n, int k) {
+            return null;
+        }
+    }
+}
