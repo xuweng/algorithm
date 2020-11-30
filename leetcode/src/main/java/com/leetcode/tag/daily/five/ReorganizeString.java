01435ac3cdfab9f3fe7b18cda576c03f831c9c71
xuweng <2028403305@qq.com>
2020-11-30 20:51:38 +0800

    方法二：基于计数的贪心算法
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/five/ReorganizeString.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/five/ReorganizeString.java
@@ -65,4 +65,48 @@ public class ReorganizeString {
         }
     }
 
+    /**
+     * 方法二：基于计数的贪心算法
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/reorganize-string/solution/zhong-gou-zi-fu-chuan-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public String reorganizeString(String S) {
+            if (S.length() < 2) {
+                return S;
+            }
+            int[] counts = new int[26];
+            int maxCount = 0;
+            int length = S.length();
+            for (int i = 0; i < length; i++) {
+                char c = S.charAt(i);
+                counts[c - 'a']++;
+                maxCount = Math.max(maxCount, counts[c - 'a']);
+            }
+            if (maxCount > (length + 1) / 2) {
+                return "";
+            }
+            char[] reorganizeArray = new char[length];
+            int evenIndex = 0, oddIndex = 1;
+            int halfLength = length / 2;
+            for (int i = 0; i < 26; i++) {
+                char c = (char) ('a' + i);
+                while (counts[i] > 0 && counts[i] <= halfLength && oddIndex < length) {
+                    reorganizeArray[oddIndex] = c;
+                    counts[i]--;
+                    oddIndex += 2;
+                }
+                while (counts[i] > 0) {
+                    reorganizeArray[evenIndex] = c;
+                    counts[i]--;
+                    evenIndex += 2;
+                }
+            }
+            return new String(reorganizeArray);
+        }
+    }
+
 }
