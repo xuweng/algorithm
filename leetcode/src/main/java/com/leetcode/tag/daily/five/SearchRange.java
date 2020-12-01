d7d89fafd40ddc8c1e0975e87b6d96fafd578370
xuweng <2028403305@qq.com>
2020-12-01 20:31:54 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/five/SearchRange.java
@@ -0,0 +1,38 @@
+package com.leetcode.tag.daily.five;
+
+/**
+ * 34. 在排序数组中查找元素的第一个和最后一个位置
+ */
+public class SearchRange {
+    /**
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/solution/zai-pai-xu-shu-zu-zhong-cha-zhao-yuan-su-de-di-3-4/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution {
+        public int[] searchRange(int[] nums, int target) {
+            int leftIdx = binarySearch(nums, target, true);
+            int rightIdx = binarySearch(nums, target, false) - 1;
+            if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+                return new int[]{leftIdx, rightIdx};
+            }
+            return new int[]{-1, -1};
+        }
+
+        public int binarySearch(int[] nums, int target, boolean lower) {
+            int left = 0, right = nums.length - 1, ans = nums.length;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                    right = mid - 1;
+                    ans = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return ans;
+        }
+    }
+
+}
