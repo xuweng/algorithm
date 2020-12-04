aa677933e0082d79cfd2ba433094cb82eec491e2
xuweng <2028403305@qq.com>
2020-12-04 20:19:33 +0800

    方法二：贪心
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/five/IsPossible.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/five/IsPossible.java
@@ -45,4 +45,46 @@ public class IsPossible {
         }
     }
 
+    /**
+     * 方法二：贪心
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/split-array-into-consecutive-subsequences/solution/fen-ge-shu-zu-wei-lian-xu-zi-xu-lie-by-l-lbs5/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public boolean isPossible(int[] nums) {
+            Map<Integer, Integer> countMap = new HashMap<>();
+            Map<Integer, Integer> endMap = new HashMap<>();
+            for (int x : nums) {
+                int count = countMap.getOrDefault(x, 0) + 1;
+                countMap.put(x, count);
+            }
+            for (int x : nums) {
+                int count = countMap.getOrDefault(x, 0);
+                if (count > 0) {
+                    int prevEndCount = endMap.getOrDefault(x - 1, 0);
+                    if (prevEndCount > 0) {
+                        countMap.put(x, count - 1);
+                        endMap.put(x - 1, prevEndCount - 1);
+                        endMap.put(x, endMap.getOrDefault(x, 0) + 1);
+                    } else {
+                        int count1 = countMap.getOrDefault(x + 1, 0);
+                        int count2 = countMap.getOrDefault(x + 2, 0);
+                        if (count1 > 0 && count2 > 0) {
+                            countMap.put(x, count - 1);
+                            countMap.put(x + 1, count1 - 1);
+                            countMap.put(x + 2, count2 - 1);
+                            endMap.put(x + 2, endMap.getOrDefault(x + 2, 0) + 1);
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
 }
