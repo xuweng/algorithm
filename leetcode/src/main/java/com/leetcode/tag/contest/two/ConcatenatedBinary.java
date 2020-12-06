ec7a49b170ca302984c2a34d77f200e6fd0c68aa
xuweng <2028403305@qq.com>
2020-12-06 11:14:01 +0800

    5619. 最小不兼容性
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/contest/two/ConcatenatedBinary.java
+++ leetcode/src/main/java/com/leetcode/tag/contest/two/ConcatenatedBinary.java
@@ -4,12 +4,16 @@ package com.leetcode.tag.contest.two;
  * 5620. 连接连续二进制数字
  */
 public class ConcatenatedBinary {
+    /**
+     * 大数溢出
+     */
     class Solution {
         public int concatenatedBinary(int n) {
             StringBuilder result = new StringBuilder();
             for (int i = 1; i <= n; i++) {
                 result.append(Integer.toBinaryString(i));
             }
+            // 大数溢出
             return (int) (Long.valueOf(result.toString(), 2) % (Math.pow(10, 9) + 7));
         }
     }
---
+++ leetcode/src/main/java/com/leetcode/tag/contest/two/MinimumIncompatibility.java
@@ -0,0 +1,12 @@
+package com.leetcode.tag.contest.two;
+
+/**
+ * 5619. 最小不兼容性
+ */
+public class MinimumIncompatibility {
+    class Solution {
+        public int minimumIncompatibility(int[] nums, int k) {
+            return 0;
+        }
+    }
+}
