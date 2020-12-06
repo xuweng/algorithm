4b008b4ce2d35aa72889e8626cdbbf8dfb1e0bec
xuweng <2028403305@qq.com>
2020-12-06 11:19:11 +0800

    聪明的策略 蛮力法
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/contest/two/MinimumIncompatibility.java
+++ leetcode/src/main/java/com/leetcode/tag/contest/two/MinimumIncompatibility.java
@@ -4,6 +4,8 @@ package com.leetcode.tag.contest.two;
  * 5619. 最小不兼容性
  * <p>
  * 聪明的策略 聪明的策略 聪明的策略 聪明的策略 聪明的策略
+ * <p>
+ * 蛮力法 蛮力法 蛮力法 蛮力法 蛮力法 蛮力法 蛮力法 蛮力法
  */
 public class MinimumIncompatibility {
     class Solution {
