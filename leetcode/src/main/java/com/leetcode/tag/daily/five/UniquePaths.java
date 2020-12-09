4fd432422dfe724d9836987d601a5b5718efd445
xuweng <2028403305@qq.com>
2020-12-09 20:45:32 +0800

    62. 不同路径
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/five/UniquePaths.java
@@ -0,0 +1,34 @@
+package com.leetcode.tag.daily.five;
+
+/**
+ * 62. 不同路径
+ */
+public class UniquePaths {
+    class Solution {
+        int result;
+
+        int m;
+        int n;
+
+        public int uniquePaths(int m, int n) {
+            this.m = m;
+            this.n = n;
+
+            return result;
+        }
+
+        public void back(int row, int col) {
+            if (row == m && col == n) {
+                result++;
+                return;
+            }
+            if (row < m) {
+                back(row + 1, col);
+            }
+            if (col < n) {
+                back(row, col + 1);
+            }
+        }
+    }
+
+}
