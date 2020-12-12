76efc75988fc9e23e01bbdd4872d116af9b0c2b4
xuweng <2028403305@qq.com>
2020-12-12 21:40:27 +0800

    方法三：贪心
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/WiggleMaxLength.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/WiggleMaxLength.java
@@ -66,4 +66,31 @@ public class WiggleMaxLength {
         }
     }
 
+    /**
+     * 方法三：贪心
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/wiggle-subsequence/solution/bai-dong-xu-lie-by-leetcode-solution-yh2m/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public int wiggleMaxLength(int[] nums) {
+            int n = nums.length;
+            if (n < 2) {
+                return n;
+            }
+            int prevdiff = nums[1] - nums[0];
+            int ret = prevdiff != 0 ? 2 : 1;
+            for (int i = 2; i < n; i++) {
+                int diff = nums[i] - nums[i - 1];
+                if ((diff > 0 && prevdiff <= 0) || (diff < 0 && prevdiff >= 0)) {
+                    ret++;
+                    prevdiff = diff;
+                }
+            }
+            return ret;
+        }
+    }
+
 }
