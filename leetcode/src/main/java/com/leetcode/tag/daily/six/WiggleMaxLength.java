17b37267cc3a1e61ecd9d4add2a21ca503c15c40
xuweng <2028403305@qq.com>
2020-12-12 21:47:48 +0800

    一定要看图
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/WiggleMaxLength.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/WiggleMaxLength.java
@@ -93,4 +93,29 @@ public class WiggleMaxLength {
         }
     }
 
+    /**
+     * 一定要看图 一定要看图 一定要看图 一定要看图 一定要看图
+     * <p>
+     * 作者：lgh18
+     * 链接：https://leetcode-cn.com/problems/wiggle-subsequence/solution/tan-xin-si-lu-qing-xi-er-zheng-que-de-ti-jie-by-lg/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution3 {
+        public int wiggleMaxLength(int[] nums) {
+            int down = 1, up = 1;
+            // 从索引1开始
+            for (int i = 1; i < nums.length; i++) {
+                // 只需要和i-1比较?
+                if (nums[i] > nums[i - 1]) {
+                    up = down + 1;
+                } else if (nums[i] < nums[i - 1]) {
+                    down = up + 1;
+                }
+            }
+            return nums.length == 0 ? 0 : Math.max(down, up);
+        }
+
+    }
+
 }
