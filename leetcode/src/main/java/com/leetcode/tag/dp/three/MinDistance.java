fff4c7ca72fd675d7ca039d8f3c10056b2439006
xuweng <2028403305@qq.com>
2020-12-14 12:27:36 +0800

    具体方案 从终点开始回溯 有多条路径
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/three/MinDistance.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/three/MinDistance.java
@@ -45,4 +45,66 @@ public class MinDistance {
             return dp[word1.length()][word2.length()];
         }
     }
+
+    /**
+     * 看图 看图 看图 看图 看图 看图 看图 看图 看图 看图 看图
+     * <p>
+     * 具体方案 从终点开始回溯 有多条路径
+     */
+    class Solution1 {
+        // int[][] dp;
+        // 这里只求出了最小的编辑距离，那具体的操作是什么？
+        // 给 dp 数组增加额外的信息即可：
+        Node[][] dp;
+
+        class Node {
+            int val;
+            int choice;
+            // 0 代表啥都不做
+            // 1 代表插入
+            // 2 代表删除
+            // 3 代表替换
+        }
+
+        /**
+         * 每个 dp[i][j] 只和它附近的三个状态有关
+         * <p>
+         * 这里只求出了最小的编辑距离，那具体的操作是什么？
+         *
+         * @param s1
+         * @param s2
+         * @return
+         */
+        int minDistance(String s1, String s2) {
+            int m = s1.length(), n = s2.length();
+            int[][] dp = new int[m + 1][n + 1];
+            // base case
+            for (int i = 1; i <= m; i++) {
+                dp[i][0] = i;
+            }
+            for (int j = 1; j <= n; j++) {
+                dp[0][j] = j;
+            }
+            // 自底向上求解
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    } else {
+                        dp[i][j] = min(
+                                dp[i - 1][j] + 1,
+                                dp[i][j - 1] + 1,
+                                dp[i - 1][j - 1] + 1
+                        );
+                    }
+                }
+            }
+            // 储存着整个 s1 和 s2 的最小编辑距离
+            return dp[m][n];
+        }
+
+        int min(int a, int b, int c) {
+            return Math.min(a, Math.min(b, c));
+        }
+    }
 }
