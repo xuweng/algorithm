d237c0341d58a5bb677feee8d86402815c569a3b
xuweng <2028403305@qq.com>
2020-12-14 11:11:01 +0800

    方法一：排序
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/GroupAnagrams.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/GroupAnagrams.java
@@ -1,9 +1,6 @@
 package com.leetcode.tag.daily.six;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.IntStream;
 
 /**
@@ -26,4 +23,34 @@ public class GroupAnagrams {
             return IntStream.range(0, s.length()).map(s::charAt).sum();
         }
     }
+
+    /**
+     * 方法一：排序
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/group-anagrams/solution/zi-mu-yi-wei-ci-fen-zu-by-leetcode-solut-gyoc/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        /**
+         * 由于互为字母异位词的两个字符串包含的字母相同，因此对两个字符串分别进行排序之后得到的字符串一定是相同的
+         *
+         * @param strs
+         * @return
+         */
+        public List<List<String>> groupAnagrams(String[] strs) {
+            Map<String, List<String>> map = new HashMap<>();
+            for (String str : strs) {
+                char[] array = str.toCharArray();
+                Arrays.sort(array);
+                String key = new String(array);
+                List<String> list = map.getOrDefault(key, new ArrayList<>());
+                list.add(str);
+                map.put(key, list);
+            }
+            return new ArrayList<>(map.values());
+        }
+    }
+
 }
