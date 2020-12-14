b30602335ccfeb0cef0551fae18bb3fdee059747
xuweng <2028403305@qq.com>
2020-12-14 10:54:43 +0800

    49. 字母异位词分组
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/GroupAnagrams.java
@@ -0,0 +1,32 @@
+package com.leetcode.tag.daily.six;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 49. 字母异位词分组
+ */
+public class GroupAnagrams {
+    class Solution {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            if (strs == null || strs.length == 0) {
+                return new ArrayList<>();
+            }
+            Map<Integer, List<String>> map = new HashMap<>();
+            for (String str : strs) {
+                map.computeIfAbsent(get(str), v -> new ArrayList<>()).add(str);
+            }
+            return (List<List<String>>) map.values();
+        }
+
+        private int get(String s) {
+            int sum = 0;
+            for (int i = 0; i < s.length(); i++) {
+                sum += s.charAt(i) - 'a';
+            }
+            return sum;
+        }
+    }
+}
