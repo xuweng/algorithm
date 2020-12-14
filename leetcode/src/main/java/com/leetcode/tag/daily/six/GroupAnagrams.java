0fca63b5bfce2b25322f6a5042c3f33fabcae2c6
xuweng <2028403305@qq.com>
2020-12-14 11:18:45 +0800

    方法二：计数
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/GroupAnagrams.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/GroupAnagrams.java
@@ -50,4 +50,42 @@ public class GroupAnagrams {
         }
     }
 
+    /**
+     * 方法二：计数
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/group-anagrams/solution/zi-mu-yi-wei-ci-fen-zu-by-leetcode-solut-gyoc/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        /**
+         * 由于互为字母异位词的两个字符串包含的字母相同，因此两个字符串中的相同字母出现的次数一定是相同的，
+         * <p>
+         * 故可以将每个字母出现的次数使用字符串表示，作为哈希表的键。
+         *
+         * @param strs
+         * @return
+         */
+        public List<List<String>> groupAnagrams(String[] strs) {
+            Map<String, List<String>> map = new HashMap<>();
+            for (String str : strs) {
+                int[] counts = new int[26];
+                int length = str.length();
+                for (int i = 0; i < length; i++) {
+                    counts[str.charAt(i) - 'a']++;
+                }
+                // 将每个出现次数大于 0 的字母和出现次数按顺序拼接成字符串，作为哈希表的键
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < 26; i++) {
+                    if (counts[i] != 0) {
+                        sb.append((char) ('a' + i)).append(counts[i]);
+                    }
+                }
+                map.computeIfAbsent(sb.toString(), v -> new ArrayList<>()).add(str);
+            }
+            return new ArrayList<>(map.values());
+        }
+    }
+
 }
