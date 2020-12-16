3fc039559157736459492b3160f52e9ab0b05ef7
xuweng <2028403305@qq.com>
2020-12-16 22:50:45 +0800

    方法一：哈希表
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/WordPattern.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/WordPattern.java
@@ -17,11 +17,16 @@ public class WordPattern {
      */
     class Solution {
         public boolean wordPattern(String pattern, String str) {
+            //这种关系被称为「双射」。
+            //任意一个字符串也只被唯一的一个字符对应
             Map<String, Character> str2ch = new HashMap<>();
+            //任意一个字符都对应着唯一的字符串
             Map<Character, String> ch2str = new HashMap<>();
             int m = str.length();
             int i = 0;
+            //枚举pattern 中的每一个字符
             for (int p = 0; p < pattern.length(); ++p) {
+                //利用双指针来均摊线性地找到该字符在 str 中对应的字符串
                 char ch = pattern.charAt(p);
                 if (i >= m) {
                     return false;
@@ -30,6 +35,7 @@ public class WordPattern {
                 while (j < m && str.charAt(j) != ' ') {
                     j++;
                 }
+                // 按照空格截取单词
                 String tmp = str.substring(i, j);
                 if (str2ch.containsKey(tmp) && str2ch.get(tmp) != ch) {
                     return false;
