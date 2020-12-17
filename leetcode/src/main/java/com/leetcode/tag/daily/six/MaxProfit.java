63a5e2519ae727dfe9c9da48a26e8376cc3911e4
xuweng <2028403305@qq.com>
2020-12-17 21:08:18 +0800

    714. 买卖股票的最佳时机含手续费
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/MaxProfit.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/MaxProfit.java
@@ -13,7 +13,7 @@ public class MaxProfit {
             int[][] dp = new int[prices.length][2];
             dp[0][1] = -prices[0];
             for (int i = 1; i < prices.length; i++) {
-                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
                 dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
             }
             return dp[dp.length - 1][0];
