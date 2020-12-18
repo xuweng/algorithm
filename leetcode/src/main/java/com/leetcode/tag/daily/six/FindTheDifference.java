83110b5000bfc9fcb3a270bab0bfe8f323c3ea9b
xuweng <2028403305@qq.com>
2020-12-18 20:25:53 +0800

    389. 找不同
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/FindTheDifference.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/FindTheDifference.java
@@ -1,5 +1,7 @@
 package com.leetcode.tag.daily.six;
 
+import java.util.Arrays;
+
 /**
  * 389. 找不同
  */
@@ -9,6 +11,28 @@ public class FindTheDifference {
             for (int i = 0; i < t.length(); i++) {
                 if (!s.contains(String.valueOf(t.charAt(i)))) {
                     return t.charAt(i);
+                }
+            }
+            return t.charAt(0);
+        }
+    }
+
+    class Solution1 {
+        public char findTheDifference(String s, String t) {
+            if (s.isEmpty()) {
+                return t.charAt(0);
+            }
+            char[] chars = s.toCharArray();
+            char[] chars1 = t.toCharArray();
+            Arrays.sort(chars);
+            Arrays.sort(chars1);
+
+            for (int i = 0; i < chars1.length; i++) {
+                if (i == s.length()) {
+                    return chars1[i];
+                }
+                if (chars[i] != chars1[i]) {
+                    return chars1[i];
                 }
             }
             return t.charAt(0);
