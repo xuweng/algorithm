17b43ec0680816519e42195942d573a5b0b7f614
xuweng <2028403305@qq.com>
2020-12-19 20:58:26 +0800

    使用辅助数组
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/Rotate.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/Rotate.java
@@ -13,18 +13,24 @@ public class Rotate {
      * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
      */
     class Solution {
+        /**
+         * 对于矩阵中第 i 行的第 j 个元素，在旋转后，它出现在倒数第 i 列的第 j 个位置。
+         * <p>
+         * 对于矩阵中的元素 matrix[row][col]，在旋转后，它的新位置为 matrix[col][n−row−1]
+         *
+         * @param matrix
+         */
         public void rotate(int[][] matrix) {
             int n = matrix.length;
-            int[][] matrix_new = new int[n][n];
+            //使用辅助数组
+            int[][] matrixNew = new int[n][n];
             for (int i = 0; i < n; ++i) {
                 for (int j = 0; j < n; ++j) {
-                    matrix_new[j][n - i - 1] = matrix[i][j];
+                    matrixNew[j][n - i - 1] = matrix[i][j];
                 }
             }
             for (int i = 0; i < n; ++i) {
-                for (int j = 0; j < n; ++j) {
-                    matrix[i][j] = matrix_new[i][j];
-                }
+                System.arraycopy(matrixNew[i], 0, matrix[i], 0, n);
             }
         }
     }
