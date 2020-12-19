824c50fc1e954c5004e813b904ebfc012f970a71
xuweng <2028403305@qq.com>
2020-12-19 22:06:08 +0800

    322. 零钱兑换
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/three/CoinChange2.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/three/CoinChange2.java
@@ -18,7 +18,7 @@ public class CoinChange2 {
                     }
                 }
             }
-            return dp[amount];
+            return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
         }
     }
 }
