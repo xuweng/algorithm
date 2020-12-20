086e17858a3adbddc2123fe8f5e851143dd43e12
xuweng <2028403305@qq.com>
2020-12-20 20:39:55 +0800

    方法一：贪心 + 栈
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/RemoveDuplicateLetters.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/RemoveDuplicateLetters.java
@@ -1,5 +1,8 @@
 package com.leetcode.tag.daily.six;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * 316. 去除重复字母
  */
@@ -62,6 +65,38 @@ public class RemoveDuplicateLetters {
                     sb.append(ch);
                 }
                 num[ch - 'a'] -= 1;
+            }
+            return sb.toString();
+        }
+    }
+
+    /**
+     * 方法一：贪心 + 栈
+     */
+    class Solution2 {
+        public String removeDuplicateLetters(String s) {
+            char[] chars = s.toCharArray();
+            int[] lastInx = new int[26];
+            for (int i = 0; i < chars.length; i++) {
+                lastInx[chars[i] - 'a'] = i; //记录每个元素最后一次出现的位置
+            }
+            Deque<Character> stack = new ArrayDeque<>();
+            boolean[] visited = new boolean[26]; //某一个字符是否在栈中出现
+            for (int i = 0; i < chars.length; i++) {
+                if (visited[chars[i] - 'a']) { //如果出现舍弃当前字符
+                    continue;
+                }
+                //当前字符在栈顶元素之前，且栈顶元素在后面还有
+                while (!stack.isEmpty() && stack.peekLast() > chars[i] && lastInx[stack.peekLast() - 'a'] > i) {
+                    Character c = stack.removeLast();  //移除栈顶元素
+                    visited[c - 'a'] = false; //表示该字符没有在栈中出现
+                }
+                stack.addLast(chars[i]);
+                visited[chars[i] - 'a'] = true;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (Character c : stack) {
+                sb.append(c);
             }
             return sb.toString();
         }
