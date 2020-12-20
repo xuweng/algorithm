c7dd7216e26a00b1da4b8cf7bdeb64b7daadcb84
xuweng <2028403305@qq.com>
2020-12-20 20:42:37 +0800

    方法一：贪心 + 栈
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/RemoveDuplicateLetters.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/RemoveDuplicateLetters.java
@@ -2,6 +2,7 @@ package com.leetcode.tag.daily.six;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.stream.Collectors;
 
 /**
  * 316. 去除重复字母
@@ -78,27 +79,30 @@ public class RemoveDuplicateLetters {
             char[] chars = s.toCharArray();
             int[] lastInx = new int[26];
             for (int i = 0; i < chars.length; i++) {
-                lastInx[chars[i] - 'a'] = i; //记录每个元素最后一次出现的位置
+                // 不断覆盖下标
+                // 记录每个元素最后一次出现的位置
+                lastInx[chars[i] - 'a'] = i;
             }
             Deque<Character> stack = new ArrayDeque<>();
-            boolean[] visited = new boolean[26]; //某一个字符是否在栈中出现
+            //某一个字符是否在栈中出现
+            boolean[] visited = new boolean[26];
             for (int i = 0; i < chars.length; i++) {
-                if (visited[chars[i] - 'a']) { //如果出现舍弃当前字符
+                //如果出现舍弃当前字符
+                if (visited[chars[i] - 'a']) {
                     continue;
                 }
                 //当前字符在栈顶元素之前，且栈顶元素在后面还有
                 while (!stack.isEmpty() && stack.peekLast() > chars[i] && lastInx[stack.peekLast() - 'a'] > i) {
-                    Character c = stack.removeLast();  //移除栈顶元素
-                    visited[c - 'a'] = false; //表示该字符没有在栈中出现
+                    //移除栈顶元素
+                    Character c = stack.removeLast();
+                    //表示该字符没有在栈中出现
+                    visited[c - 'a'] = false;
                 }
+                // 入栈
                 stack.addLast(chars[i]);
                 visited[chars[i] - 'a'] = true;
             }
-            StringBuilder sb = new StringBuilder();
-            for (Character c : stack) {
-                sb.append(c);
-            }
-            return sb.toString();
+            return stack.stream().map(String::valueOf).collect(Collectors.joining());
         }
     }
 
