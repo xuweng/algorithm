752487c1028c10964a6b250cdf57ef9d0be54d80
xuweng <2028403305@qq.com>
2020-12-20 21:16:53 +0800

    322. 零钱兑换
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dp/three/CoinChange3.java
@@ -0,0 +1,26 @@
+package com.leetcode.tag.dp.three;
+
+/**
+ * 322. 零钱兑换
+ * <p>
+ * 一题多遍 一题多遍 一题多遍
+ */
+public class CoinChange3 {
+    class Solution {
+        public int coinChange(int[] coins, int amount) {
+            if (coins == null || coins.length == 0) {
+                return 0;
+            }
+            int[] dp = new int[amount + 1];
+            for (int i = 1; i <= amount; i++) {
+                dp[i] = Integer.MAX_VALUE;
+                for (int coin : coins) {
+                    if (i >= coin && dp[i - coin] != Integer.MAX_VALUE) {
+                        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                    }
+                }
+            }
+            return dp[amount];
+        }
+    }
+}
