4907e83fe169b5f498e2d282fa77fe49e851b2f3
xuweng <2028403305@qq.com>
2020-12-20 20:27:10 +0800

    方法一：贪心 + 栈
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/RemoveDuplicateLetters.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/RemoveDuplicateLetters.java
@@ -22,4 +22,42 @@ public class RemoveDuplicateLetters {
             return result.toString();
         }
     }
+
+    /**
+     * 方法一：贪心 + 栈
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/remove-duplicate-letters/solution/qu-chu-zhong-fu-zi-mu-by-leetcode-soluti-vuso/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public String removeDuplicateLetters(String s) {
+            boolean[] vis = new boolean[26];
+            int[] num = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                num[s.charAt(i) - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.length(); i++) {
+                char ch = s.charAt(i);
+                if (!vis[ch - 'a']) {
+                    while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch) {
+                        if (num[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                            vis[sb.charAt(sb.length() - 1) - 'a'] = false;
+                            sb.deleteCharAt(sb.length() - 1);
+                        } else {
+                            break;
+                        }
+                    }
+                    vis[ch - 'a'] = true;
+                    sb.append(ch);
+                }
+                num[ch - 'a'] -= 1;
+            }
+            return sb.toString();
+        }
+    }
+
 }
