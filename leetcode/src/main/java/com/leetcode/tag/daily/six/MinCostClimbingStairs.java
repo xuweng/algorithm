7acdf41fbd2ed141cfa6e219d21a2964c4cd3fd0
xuweng <2028403305@qq.com>
2020-12-21 12:01:34 +0800

    滚动数组
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/MinCostClimbingStairs.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/MinCostClimbingStairs.java
@@ -94,6 +94,7 @@ public class MinCostClimbingStairs {
             int[] dp = new int[n + 1];
             //可以选择下标 0 或 1 作为初始阶梯
             dp[0] = dp[1] = 0;
+            // 这里是i <= n
             for (int i = 2; i <= n; i++) {
                 dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
             }
