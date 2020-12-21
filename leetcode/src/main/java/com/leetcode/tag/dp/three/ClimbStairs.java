f2fdcb878018f1c5990b25321f0eceee8d1e7cab
xuweng <2028403305@qq.com>
2020-12-21 14:15:12 +0800

    70. 爬楼梯
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dp/three/ClimbStairs.java
@@ -0,0 +1,29 @@
+package com.leetcode.tag.dp.three;
+
+/**
+ * 70. 爬楼梯
+ */
+public class ClimbStairs {
+    class Solution {
+        int[] meno;
+
+        public int climbStairs(int n) {
+            meno = new int[n + 1];
+            return back(n);
+        }
+
+        private int back(int n) {
+            if (n <= 0) {
+                return 0;
+            }
+            if (n == 1) {
+                return 1;
+            }
+            if (meno[n] != 0) {
+                return meno[n];
+            }
+            meno[n] = back(n - 1) + back(n - 2);
+            return meno[n];
+        }
+    }
+}
