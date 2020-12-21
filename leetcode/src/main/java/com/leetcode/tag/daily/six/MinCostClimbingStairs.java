dd2d674e8375901aa329340522a6f01bc8d96cba
xuweng <2028403305@qq.com>
2020-12-21 11:33:03 +0800

    746. 使用最小花费爬楼梯
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/MinCostClimbingStairs.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/MinCostClimbingStairs.java
@@ -5,22 +5,41 @@ package com.leetcode.tag.daily.six;
  */
 public class MinCostClimbingStairs {
     static class Solution {
-        int[] meno;
+        int[] meno1;
+        int[] meno2;
 
         public int minCostClimbingStairs(int[] cost) {
             if (cost == null || cost.length == 0) {
                 return 0;
             }
-            meno = new int[cost.length];
+            meno1 = new int[cost.length];
+            meno2 = new int[cost.length];
 
-            return Math.min(back(cost, 0), back(cost, 1));
+            return Math.min(back1(cost, 0), back2(cost, 1));
         }
 
-        private int back(int[] cost, int start) {
+        private int back1(int[] cost, int start) {
             if (start >= cost.length) {
                 return 0;
             }
-            return cost[start] + Math.min(back(cost, start + 1), back(cost, start + 2));
+            if (meno1[start] != 0) {
+                return meno1[start];
+            }
+            int result = cost[start] + Math.min(back1(cost, start + 1), back1(cost, start + 2));
+            meno1[start] = result;
+            return meno1[start];
+        }
+
+        private int back2(int[] cost, int start) {
+            if (start >= cost.length) {
+                return 0;
+            }
+            if (meno2[start] != 0) {
+                return meno2[start];
+            }
+            int result = cost[start] + Math.min(back1(cost, start + 1), back1(cost, start + 2));
+            meno2[start] = result;
+            return meno2[start];
         }
     }
 }
