d3b1435986d7f9304c8ef1df59a0f8f0745f6d12
xuweng <2028403305@qq.com>
2020-12-21 11:59:36 +0800

    滚动数组
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/MinCostClimbingStairs.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/MinCostClimbingStairs.java
@@ -124,4 +124,22 @@ public class MinCostClimbingStairs {
         }
     }
 
+    /**
+     * 作者：armeria-program
+     * 链接：https://leetcode-cn.com/problems/min-cost-climbing-stairs/solution/yi-bu-yi-bu-tui-dao-dong-tai-gui-hua-de-duo-chong-/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution5 {
+        public int minCostClimbingStairs(int[] cost) {
+            int[] dp = new int[cost.length];
+            dp[0] = cost[0];
+            dp[1] = cost[1];
+            for (int i = 2; i < cost.length; i++) {
+                dp[i] = Math.min(dp[i - 2], dp[i - 1]) + cost[i];
+            }
+            return Math.min(dp[cost.length - 2], dp[cost.length - 1]);
+        }
+    }
+
 }
