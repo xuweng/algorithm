7af3acd4d752c86ea426efc346d8b1f6b8d62131
xuweng <2028403305@qq.com>
2020-12-21 14:35:17 +0800

    滚动数组
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/three/ClimbStairs.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/three/ClimbStairs.java
@@ -49,4 +49,25 @@ public class ClimbStairs {
             return dp[n];
         }
     }
+
+    /**
+     * 滚动数组
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/climbing-stairs/solution/pa-lou-ti-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public int climbStairs(int n) {
+            int p, q = 0, r = 1;
+            for (int i = 1; i <= n; ++i) {
+                p = q;
+                q = r;
+                r = p + q;
+            }
+            return r;
+        }
+    }
+
 }
