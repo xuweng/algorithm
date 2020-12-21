a4ea1dcfb1c24d8ef4e327e2eb5a6c2691099613
xuweng <2028403305@qq.com>
2020-12-21 12:08:53 +0800

    滚动数组
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/MinCostClimbingStairs.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/MinCostClimbingStairs.java
@@ -145,4 +145,32 @@ public class MinCostClimbingStairs {
         }
     }
 
+    class Solution6 {
+        int[] meno;
+
+        public int minCostClimbingStairs(int[] cost) {
+            if (cost == null || cost.length == 0) {
+                return 0;
+            }
+
+            meno = new int[cost.length];
+
+            back(cost, cost.length - 1);
+
+            return Math.min(meno[meno.length - 1], meno[meno.length - 2]);
+        }
+
+        private int back(int[] cost, int start) {
+            if (start == 0 || start == 1) {
+                return cost[start];
+            }
+            if (meno[start] != 0) {
+                return meno[start];
+            }
+            int b = Math.min(back(cost, start - 1), back(cost, start - 2)) + cost[start];
+            meno[start] = b;
+            return b;
+        }
+    }
+
 }
