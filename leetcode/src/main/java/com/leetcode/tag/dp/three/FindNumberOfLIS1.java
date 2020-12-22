68cb87105767e33c1bb851c8214ce7b69b4d46f8
xuweng <2028403305@qq.com>
2020-12-22 21:11:41 +0800

    673. 最长递增子序列的个数
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/three/FindNumberOfLIS1.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/three/FindNumberOfLIS1.java
@@ -11,16 +11,17 @@ public class FindNumberOfLIS1 {
             }
             int[] dp = new int[nums.length];
             int[] c = new int[nums.length];
+            dp[0] = 1;
+            c[0] = 1;
             for (int i = 0; i < nums.length; i++) {
                 for (int j = 0; j < i; j++) {
-                    if (dp[j] > dp[i]) {
-                        continue;
-                    }
-                    if (dp[i] < dp[j] + 1) {
-                        dp[i] = dp[j] + 1;
-                        c[i] = c[j];
-                    } else {
-                        c[i] += c[j];
+                    if (dp[j] < dp[i]) {
+                        if (dp[i] < dp[j] + 1) {
+                            dp[i] = dp[j] + 1;
+                            c[i] = c[j];
+                        } else {
+                            c[i] += c[j];
+                        }
                     }
                 }
             }
