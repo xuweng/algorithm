dd23e68d68ae65e8814124d75674793371254692
xuweng <2028403305@qq.com>
2020-12-22 20:58:40 +0800

    673. 最长递增子序列的个数
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dp/three/FindNumberOfLIS1.java
@@ -0,0 +1,42 @@
+package com.leetcode.tag.dp.three;
+
+/**
+ * 673. 最长递增子序列的个数
+ */
+public class FindNumberOfLIS1 {
+    class Solution {
+        public int findNumberOfLIS(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return 0;
+            }
+            int[] dp = new int[nums.length];
+            int[] c = new int[nums.length];
+            dp[0] = 1;
+            c[0] = 1;
+            for (int i = 1; i < nums.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (dp[j] > dp[i]) {
+                        continue;
+                    }
+                    if (dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                        c[i] = c[j];
+                    } else {
+                        c[i] += c[j];
+                    }
+                }
+            }
+            int max = 0;
+            for (int num : nums) {
+                max = Math.max(max, num);
+            }
+            int result = 0;
+            for (int i : c) {
+                if (i == max) {
+                    result++;
+                }
+            }
+            return result;
+        }
+    }
+}
