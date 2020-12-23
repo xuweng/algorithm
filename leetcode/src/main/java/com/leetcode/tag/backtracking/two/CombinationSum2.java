418cce71340a15b502e0cfa13a0eeb2e404af20a
xuweng <2028403305@qq.com>
2020-12-23 22:00:22 +0800

    40. 组合总和 II
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/two/CombinationSum2.java
@@ -0,0 +1,29 @@
+package com.leetcode.tag.backtracking.two;
+
+import java.util.*;
+
+/**
+ * 40. 组合总和 II
+ */
+public class CombinationSum2 {
+    class Solution {
+        List<List<Integer>> result = new ArrayList<>();
+        Deque<Integer> deque = new LinkedList<>();
+
+        public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+            Arrays.sort(candidates);
+
+            back(candidates, target, 0);
+
+            return result;
+        }
+
+        private void back(int[] candidates, int target, int start) {
+            for (int i = start; i < candidates.length && candidates[i] <= target; i++) {
+                deque.push(candidates[i]);
+                back(candidates, target - candidates[i], i + 1);
+                deque.pop();
+            }
+        }
+    }
+}
