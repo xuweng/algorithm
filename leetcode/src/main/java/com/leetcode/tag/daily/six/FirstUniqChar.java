6c0ede695eefaecae4f1e8c2cee6128e9b96314b
xuweng <2028403305@qq.com>
2020-12-23 20:21:30 +0800

    387. 字符串中的第一个唯一字符
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/FirstUniqChar.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/FirstUniqChar.java
@@ -4,16 +4,17 @@ package com.leetcode.tag.daily.six;
  * 387. 字符串中的第一个唯一字符
  */
 public class FirstUniqChar {
-    class Solution {
+    static class Solution {
         public int firstUniqChar(String s) {
             int[] ints = new int[26];
             for (int i = 0; i < s.length(); i++) {
                 ints[s.charAt(i) - 'a']++;
             }
             char c = 0;
-            for (int anInt : ints) {
+            for (int i = 0; i < ints.length; i++) {
+                int anInt = ints[i];
                 if (anInt == 1) {
-                    c = (char) (anInt + 'a');
+                    c = (char) (i + 'a');
                     break;
                 }
             }
---
+++ leetcode/src/test/java/com/leetcode/tag/daily/six/FirstUniqCharTest.java
@@ -0,0 +1,14 @@
+package com.leetcode.tag.daily.six;
+
+import org.junit.jupiter.api.Test;
+
+class FirstUniqCharTest {
+    FirstUniqChar.Solution solution = new FirstUniqChar.Solution();
+
+    @Test
+    void firstUniqChar() {
+        String s = "loveleetcode";
+
+        solution.firstUniqChar(s);
+    }
+}
