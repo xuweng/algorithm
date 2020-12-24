a78aaa4be9f0c3276376ab5d77c144b7e3bfad5e
xuweng <2028403305@qq.com>
2020-12-24 20:31:43 +0800

    135. 分发糖果
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/Candy.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/Candy.java
@@ -49,4 +49,36 @@ public class Candy {
         }
     }
 
+    /**
+     * 方法二：常数空间遍历
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/candy/solution/fen-fa-tang-guo-by-leetcode-solution-f01p/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int candy(int[] ratings) {
+            int n = ratings.length;
+            int ret = 1;
+            int inc = 1, dec = 0, pre = 1;
+            for (int i = 1; i < n; i++) {
+                if (ratings[i] >= ratings[i - 1]) {
+                    dec = 0;
+                    pre = ratings[i] == ratings[i - 1] ? 1 : pre + 1;
+                    ret += pre;
+                    inc = pre;
+                } else {
+                    dec++;
+                    if (dec == inc) {
+                        dec++;
+                    }
+                    ret += dec;
+                    pre = 1;
+                }
+            }
+            return ret;
+        }
+    }
+
 }
