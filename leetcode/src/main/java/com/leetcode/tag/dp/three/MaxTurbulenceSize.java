d968d73ad4807f32ea5da5d323a7fbb0e5f79e71
xuweng <2028403305@qq.com>
2020-12-24 21:26:09 +0800

    方法：滑动窗口
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/three/MaxTurbulenceSize.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/three/MaxTurbulenceSize.java
@@ -32,4 +32,33 @@ public class MaxTurbulenceSize {
             return max;
         }
     }
+
+    /**
+     * 方法：滑动窗口
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/longest-turbulent-subarray/solution/zui-chang-tuan-liu-zi-shu-zu-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int maxTurbulenceSize(int[] A) {
+            int N = A.length;
+            int ans = 1;
+            int anchor = 0;
+
+            for (int i = 1; i < N; ++i) {
+                int c = Integer.compare(A[i - 1], A[i]);
+                if (i == N - 1 || c * Integer.compare(A[i], A[i + 1]) != -1) {
+                    if (c != 0) {
+                        ans = Math.max(ans, i - anchor + 1);
+                    }
+                    anchor = i;
+                }
+            }
+
+            return ans;
+        }
+    }
+
 }
