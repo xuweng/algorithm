d9cb924da161a76d7ae7883cf166c8d2ebf8e757
xuweng <2028403305@qq.com>
2020-12-25 22:42:10 +0800

    10. 正则表达式匹配
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dp/three/IsMatch.java
@@ -0,0 +1,12 @@
+package com.leetcode.tag.dp.three;
+
+/**
+ * 10. 正则表达式匹配
+ */
+public class IsMatch {
+    class Solution {
+        public boolean isMatch(String s, String p) {
+            return true;
+        }
+    }
+}
