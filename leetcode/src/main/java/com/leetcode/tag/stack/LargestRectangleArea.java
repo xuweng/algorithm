adc59c7b1e84981c1070a52096e9998bebc62fbf
xuweng <2028403305@qq.com>
2020-12-26 20:35:23 +0800

    方法一：单调栈
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/stack/LargestRectangleArea.java
+++ leetcode/src/main/java/com/leetcode/tag/stack/LargestRectangleArea.java
@@ -1,5 +1,7 @@
 package com.leetcode.tag.stack;
 
+import java.util.Stack;
+
 /**
  * 84. 柱状图中最大的矩形
  */
@@ -28,4 +30,45 @@ public class LargestRectangleArea {
             return max;
         }
     }
+
+    /**
+     * 方法一：单调栈
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/largest-rectangle-in-histogram/solution/zhu-zhuang-tu-zhong-zui-da-de-ju-xing-by-leetcode-/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int largestRectangleArea(int[] heights) {
+            int n = heights.length;
+            int[] left = new int[n];
+            int[] right = new int[n];
+
+            Stack<Integer> monoStack = new Stack<>();
+            for (int i = 0; i < n; ++i) {
+                while (!monoStack.isEmpty() && heights[monoStack.peek()] >= heights[i]) {
+                    monoStack.pop();
+                }
+                left[i] = (monoStack.isEmpty() ? -1 : monoStack.peek());
+                monoStack.push(i);
+            }
+
+            monoStack.clear();
+            for (int i = n - 1; i >= 0; --i) {
+                while (!monoStack.isEmpty() && heights[monoStack.peek()] >= heights[i]) {
+                    monoStack.pop();
+                }
+                right[i] = (monoStack.isEmpty() ? n : monoStack.peek());
+                monoStack.push(i);
+            }
+
+            int ans = 0;
+            for (int i = 0; i < n; ++i) {
+                ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+            }
+            return ans;
+        }
+    }
+
 }
