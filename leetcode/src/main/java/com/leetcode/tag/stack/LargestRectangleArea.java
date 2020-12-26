3e84e9aeb34d02e7e6c666dc55a2218e72917d54
xuweng <2028403305@qq.com>
2020-12-26 20:32:25 +0800

    84. 柱状图中最大的矩形
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/stack/LargestRectangleArea.java
@@ -0,0 +1,31 @@
+package com.leetcode.tag.stack;
+
+/**
+ * 84. 柱状图中最大的矩形
+ */
+public class LargestRectangleArea {
+    class Solution {
+        public int largestRectangleArea(int[] heights) {
+            if (heights == null || heights.length == 0) {
+                return 0;
+            }
+            int max = 0;
+            for (int i = 0; i < heights.length; i++) {
+                int leftIndex = i - 1;
+                int left = 0;
+                while (leftIndex >= 0 && heights[leftIndex] >= heights[i]) {
+                    left++;
+                    leftIndex--;
+                }
+                int rightIndex = i - 1;
+                int right = 0;
+                while (rightIndex >= 0 && heights[rightIndex] >= heights[i]) {
+                    right++;
+                    rightIndex++;
+                }
+                max = Math.max(max, (left + right + 1) * heights[i]);
+            }
+            return max;
+        }
+    }
+}
