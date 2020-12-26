ad1a56330a3b057e6088e161ee3f2daff7b3d0ea
xuweng <2028403305@qq.com>
2020-12-26 21:56:57 +0800

    10. 正则表达式匹配
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/three/IsMatch.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/three/IsMatch.java
@@ -16,8 +16,9 @@ public class IsMatch {
             boolean firstMatch = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
 
             // p第1个字符是否是*
-            boolean pNext = p.length() >= 2 && p.charAt(1) == '*';
+            boolean pNext = p.length() > 1 && p.charAt(1) == '*';
             if (pNext) {
+                //'*' 匹配零个或多个前面的那一个元素
                 return isMatch(s, p.substring(2)) || (firstMatch && isMatch(s.substring(1), p));
             } else {
                 return firstMatch && isMatch(s.substring(1), p.substring(1));
