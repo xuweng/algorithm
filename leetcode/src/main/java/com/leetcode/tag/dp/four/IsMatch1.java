068ef42f624c7c2071664225c881f61e6c427edf
xuweng <2028403305@qq.com>
2020-12-27 22:12:02 +0800

    44. 通配符匹配
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/four/IsMatch1.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/four/IsMatch1.java
@@ -6,6 +6,9 @@ package com.leetcode.tag.dp.four;
 public class IsMatch1 {
     class Solution {
         public boolean isMatch(String s, String p) {
+            if (p.isEmpty()) {
+                return s.isEmpty();
+            }
             boolean first = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '?');
             if (first) {
                 return isMatch(s.substring(1), p.substring(1));
