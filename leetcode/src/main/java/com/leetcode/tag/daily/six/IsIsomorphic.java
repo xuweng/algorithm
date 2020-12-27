c1ef2a22b0082c4c5d9b8b936aa09716851640c2
xuweng <2028403305@qq.com>
2020-12-27 20:45:48 +0800

    简洁代码
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/IsIsomorphic.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/IsIsomorphic.java
@@ -26,4 +26,21 @@ public class IsIsomorphic {
             return true;
         }
     }
+
+    class Solution1 {
+        public boolean isIsomorphic(String s, String t) {
+            char[] chars = s.toCharArray();
+            char[] chart = t.toCharArray();
+            int[] preIndexOfs = new int[256];
+            int[] preIndexOft = new int[256];
+            for (int i = 0; i < chars.length; i++) {
+                if (preIndexOfs[chars[i]] != preIndexOft[chart[i]]) {
+                    return false;
+                }
+                preIndexOfs[chars[i]] = i + 1;
+                preIndexOft[chart[i]] = i + 1;
+            }
+            return true;
+        }
+    }
 }
