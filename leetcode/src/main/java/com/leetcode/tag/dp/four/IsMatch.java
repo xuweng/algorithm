066309f4d4a587d2c72e973166a15777778ab6ad
xuweng <2028403305@qq.com>
2020-12-27 21:44:21 +0800

    方法一：动态规划
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/four/IsMatch.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/four/IsMatch.java
@@ -31,4 +31,51 @@ public class IsMatch {
             }
         }
     }
+
+    /**
+     * 方法一：动态规划
+     * <p>
+     * 十分钟 十分钟 十分钟 十分钟 十分钟 十分钟 十分钟 十分钟 十分钟
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/regular-expression-matching/solution/zheng-ze-biao-da-shi-pi-pei-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public boolean isMatch(String s, String p) {
+            int m = s.length();
+            int n = p.length();
+
+            //用 f[i][j] 表示 s 的前 i 个字符与 p 中的前 j 个字符是否能够匹配
+            boolean[][] f = new boolean[m + 1][n + 1];
+            f[0][0] = true;
+            for (int i = 0; i <= m; ++i) {
+                for (int j = 1; j <= n; ++j) {
+                    if (p.charAt(j - 1) == '*') {
+                        f[i][j] = f[i][j - 2];
+                        if (matches(s, p, i, j - 1)) {
+                            f[i][j] = f[i][j] || f[i - 1][j];
+                        }
+                    } else {
+                        if (matches(s, p, i, j)) {
+                            f[i][j] = f[i - 1][j - 1];
+                        }
+                    }
+                }
+            }
+            return f[m][n];
+        }
+
+        public boolean matches(String s, String p, int i, int j) {
+            if (i == 0) {
+                return false;
+            }
+            if (p.charAt(j - 1) == '.') {
+                return true;
+            }
+            return s.charAt(i - 1) == p.charAt(j - 1);
+        }
+    }
+
 }
