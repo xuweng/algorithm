b31b855381748bbfc775e0766ea3e064a995c942
xuweng <2028403305@qq.com>
2020-12-27 21:15:50 +0800

    10. 正则表达式匹配
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dp/four/IsMatch.java
@@ -0,0 +1,24 @@
+package com.leetcode.tag.dp.four;
+
+import java.util.Objects;
+
+/**
+ * 10. 正则表达式匹配
+ */
+public class IsMatch {
+    class Solution {
+        public boolean isMatch(String s, String p) {
+            if (p == null || p.isEmpty()) {
+                return Objects.equals(s, p);
+            }
+            boolean first = s.charAt(0) == p.charAt(0) || p.charAt(0) == '.';
+
+            boolean next = p.length() > 1 && p.charAt(1) == '*';
+            if (next) {
+                return isMatch(s, p.substring(2)) || (first && isMatch(s.substring(1), p));
+            } else {
+                return first && isMatch(s.substring(1), p.substring(1));
+            }
+        }
+    }
+}
