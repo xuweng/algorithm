c914a8c484434f7f8880fbaa66763fb400199d15
xuweng <2028403305@qq.com>
2020-12-27 22:39:15 +0800

    只需要p非空判断 s另外非空判断
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/four/IsMatch1.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/four/IsMatch1.java
@@ -23,7 +23,7 @@ public class IsMatch1 {
                 if (!s.isEmpty() && s.charAt(0) != p.charAt(1)) {
                     return isMatch(s.substring(1), p);
                 } else {
-                    return isMatch(s, p.substring(1));
+                    return isMatch(s, p.substring(1)) || (!s.isEmpty() && isMatch(s.substring(1), p));
                 }
             }
 
