900cc94159c03dc4b2c71bfa35ebe770c40f805e
xuweng <2028403305@qq.com>
2020-12-27 22:12:31 +0800

    44. 通配符匹配
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/four/IsMatch1.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/four/IsMatch1.java
@@ -12,7 +12,7 @@ public class IsMatch1 {
             boolean first = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '?');
             if (first) {
                 return isMatch(s.substring(1), p.substring(1));
-            } else {
+            }
                 if (p.charAt(0) == '*') {
                     if (p.length() > 1 && s.charAt(0) != p.charAt(1)) {
                         return isMatch(s.substring(1), p);
@@ -20,7 +20,7 @@ public class IsMatch1 {
                         return isMatch(s, p.substring(1));
                     }
                 }
-            }
+
             return false;
         }
     }
