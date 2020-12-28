15fabe028fc93e4b9c311390cbc60e8b3529460c
xuweng <2028403305@qq.com>
2020-12-28 20:56:36 +0800

    188. 买卖股票的最佳时机 IV
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/four/MaxProfit.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/four/MaxProfit.java
@@ -34,4 +34,60 @@ public class MaxProfit {
             return max;
         }
     }
+
+    /**
+     * 作者：lippon
+     * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iv/solution/java-dong-tai-gui-hua-qing-xi-jian-ming-ot6n0/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int maxProfit(int k, int[] prices) {
+            int n = prices.length;
+            if (n == 0) {
+                return 0;
+            }
+
+            // 如果k超过了最大可买卖次数，那就将k置为最大买卖次数
+            // 最大买卖次数就是天数的一半，如果当前卖出又买入，是没有意义的
+            k = Math.min(k, n / 2);
+
+            // 状态表示数组，三个维度分别代表：第几天，手上是否有股票0没有1有，还剩下多少次买卖机会
+            // 数组值表示当前还有多少钱
+            int[][][] f = new int[n][2][k + 1];
+
+            // 设置初始值，第一天手上有股票的状态，就是买入第一天价格的值
+            for (int i = 0; i <= k; i++) {
+                f[0][1][i] = -prices[0];
+            }
+            // 状态转移，遍历天数
+            for (int i = 1; i < n; i++) {
+                // 枚举当天所有可能买卖次数的状态
+                for (int j = 1; j <= k; j++) {
+                    // 当前不持有股票的状态转移
+                    if (j < k) {
+                        // 当前不持有股票，要么前一天也没有股票，要么前一天有股票但是今天卖出了
+                        // 如果今天卖出的话，买卖次数就会比前一天少一次，当天是j，那么前一天就是j+1
+                        f[i][0][j] = Math.max(f[i - 1][0][j], f[i - 1][1][j + 1] + prices[i]);
+                    } else {
+                        // 剩余买卖次数j等于初始值剩余交易次数k的时候，那么说明从来没有产生过交易，所以需要从前一天的不持有状态转移过来
+                        f[i][0][j] = f[i - 1][0][j];
+                    }
+
+                    // 当前持有股票的状态转移，要么前一天也有股票，要么前一天不持有股票但是当天买入了股票
+                    f[i][1][j] = Math.max(f[i - 1][1][j], f[i - 1][0][j] - prices[i]);
+                }
+            }
+
+            int res = 0;
+            // 获取最大值，最大值肯定出现在当前手上不持有股票的状态
+            for (int i = 0; i <= k; i++) {
+                res = Math.max(res, f[n - 1][0][i]);
+            }
+
+            return res;
+
+        }
+    }
+
 }
