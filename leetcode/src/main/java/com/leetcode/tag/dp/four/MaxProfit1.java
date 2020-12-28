1584edd9740a000ee0f03fb91fec349dfc2d895c
xuweng <2028403305@qq.com>
2020-12-28 21:20:42 +0800

    121. 买卖股票的最佳时机
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/four/MaxProfit1.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/four/MaxProfit1.java
@@ -14,7 +14,7 @@ public class MaxProfit1 {
 
             int max = 0;
             for (int i = 1; i < prices.length; i++) {
-                dp[i][0][0] = Math.max(dp[i - 1][0][0], dp[i - 1][1][0] - prices[i]);
+                dp[i][0][0] = Math.max(dp[i - 1][0][0], dp[i - 1][1][0] + prices[i]);
                 for (int j = 1; j <= 1; j++) {
                     dp[i][0][j] = Math.max(dp[i - 1][0][j], dp[i - 1][1][j - 1] + prices[i]);
                     dp[i][1][j] = Math.max(dp[i - 1][1][j], dp[i - 1][0][j] - prices[i]);
