2d169c0c7a71020e8a92af2b4857222c8a029af6
xuweng <2028403305@qq.com>
2020-12-28 22:25:38 +0800

    交易次数从1开始计算.上面对交易次数为0初始化
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/four/MaxProfit2.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/four/MaxProfit2.java
@@ -16,10 +16,15 @@ public class MaxProfit2 {
 
             int max = 0;
             for (int i = 1; i < n; i++) {
+                // 没有股票
                 dp[i][0][0] = dp[i - 1][0][0];
+                // 有股票
                 dp[i][1][0] = Math.max(dp[i - 1][1][0], dp[i - 1][0][0] - prices[i]);
+                // 交易次数从1开始计算.上面对交易次数为0初始化
                 for (int j = 1; j <= k; j++) {
+                    // 没有股票
                     dp[i][0][j] = Math.max(dp[i - 1][0][j], dp[i - 1][1][j - 1] + prices[i]);
+                    // 有股票
                     dp[i][1][j] = Math.max(dp[i - 1][1][j], dp[i - 1][0][j] - prices[i]);
                     max = Math.max(max, dp[i][0][j]);
                 }
