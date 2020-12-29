2fd8763808d0d4c595669ac030e2ab16e74242ce
xuweng <2028403305@qq.com>
2020-12-29 21:20:33 +0800

    321. 拼接最大数
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dp/four/MaxNumber.java
@@ -0,0 +1,26 @@
+package com.leetcode.tag.dp.four;
+
+/**
+ * 321. 拼接最大数
+ */
+public class MaxNumber {
+    class Solution {
+        public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+            int[][][] dp = new int[nums1.length + 1][nums2.length + 1][k + 1];
+
+
+            for (int i = 1; i <= nums1.length; i++) {
+                for (int j = 1; j <= nums2.length; j++) {
+                    for (int l = 1; l <= k; l++) {
+                        if (nums1[i] == nums2[j]) {
+
+                        }
+                    }
+                }
+            }
+
+            return nums1;
+        }
+
+    }
+}
