1d2bc9dbbcde1c70378408624626bb8d3c191830
xuweng <2028403305@qq.com>
2020-12-31 22:59:31 +0800

    152. 乘积最大子数组
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/four/MaxProduct1.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/four/MaxProduct1.java
@@ -16,8 +16,8 @@ public class MaxProduct1 {
             int max = nums[0];
             for (int i = 1; i < nums.length; i++) {
                 if (nums[i] < 0) {
-                    dp[i][0] = Math.min(nums[i], dp[i][1] * nums[i]);
-                    dp[i][1] = dp[i - 1][0] * nums[i];
+                    dp[i][0] = Math.min(nums[i], dp[i - 1][1] * nums[i]);
+                    dp[i][1] = Math.max(1, dp[i - 1][0] * nums[i]);
                 } else if (nums[i] > 0) {
                     dp[i][0] = dp[i - 1][0] * nums[i];
                     dp[i][1] = dp[i - 1][1] * nums[i];
