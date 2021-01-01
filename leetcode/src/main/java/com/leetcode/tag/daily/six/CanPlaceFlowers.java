e96977c6cee317c14b0dbdc19a74a0be77a94e45
xuweng <2028403305@qq.com>
2021-01-01 09:43:38 +0800

    605. 种花问题
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/CanPlaceFlowers.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/CanPlaceFlowers.java
@@ -17,9 +17,11 @@ public class CanPlaceFlowers {
             for (int i = 0; i < flowerbed.length; i++) {
                 if (i == 0) {
                     if (flowerbed[i] == 0) {
-                        if (i + 1 < flowerbed.length && flowerbed[i + 1] == 0) {
-                            result++;
-                            flowerbed[i] = 1;
+                        if (i + 1 < flowerbed.length) {
+                            if (flowerbed[i + 1] == 0) {
+                                result++;
+                                flowerbed[i] = 1;
+                            }
                         } else {
                             result++;
                             flowerbed[i] = 1;
