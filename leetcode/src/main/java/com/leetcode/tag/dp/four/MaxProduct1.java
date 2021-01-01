a6955591041555543348cf24f618dc32526bb5b8
xuweng <2028403305@qq.com>
2021-01-01 15:19:21 +0800

    初始化为1
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/four/MaxProduct1.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/four/MaxProduct1.java
@@ -10,13 +10,11 @@ public class MaxProduct1 {
                 return 0;
             }
             int[][] dp = new int[nums.length][2];
-            dp[0][0] = nums[0] >= 0 ? 1 : nums[0];
-            dp[0][1] = nums[0] >= 0 ? nums[0] : 1;
+            dp[0][0] = Math.min(nums[0], 0);
+            dp[0][1] = Math.max(nums[0], 0);
 
             int max = nums[0];
             for (int i = 1; i < nums.length; i++) {
-                dp[i][0] = Integer.MIN_VALUE;
-                dp[i][1] = Integer.MIN_VALUE;
                 if (nums[i] < 0) {
                     dp[i][0] = Math.min(nums[i], dp[i - 1][1] * nums[i]);
                     dp[i][1] = dp[i - 1][0] * nums[i];
