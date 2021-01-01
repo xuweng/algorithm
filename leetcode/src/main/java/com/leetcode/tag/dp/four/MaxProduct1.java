14a703f27aa43e23468d3c6786a0f903684c9306
xuweng <2028403305@qq.com>
2021-01-01 15:35:45 +0800

    分类讨论 思路清晰
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/four/MaxProduct1.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/four/MaxProduct1.java
@@ -4,6 +4,9 @@ package com.leetcode.tag.dp.four;
  * 152. 乘积最大子数组
  */
 public class MaxProduct1 {
+    /**
+     * 分类讨论 思路清晰
+     */
     class Solution {
         public int maxProduct(int[] nums) {
             if (nums == null || nums.length == 0) {
@@ -31,4 +34,30 @@ public class MaxProduct1 {
             return max;
         }
     }
+
+    /**
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/maximum-product-subarray/solution/cheng-ji-zui-da-zi-shu-zu-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int maxProduct(int[] nums) {
+            int length = nums.length;
+            int[] maxF = new int[length];
+            int[] minF = new int[length];
+            System.arraycopy(nums, 0, maxF, 0, length);
+            System.arraycopy(nums, 0, minF, 0, length);
+            for (int i = 1; i < length; ++i) {
+                maxF[i] = Math.max(maxF[i - 1] * nums[i], Math.max(nums[i], minF[i - 1] * nums[i]));
+                minF[i] = Math.min(minF[i - 1] * nums[i], Math.min(nums[i], maxF[i - 1] * nums[i]));
+            }
+            int ans = maxF[0];
+            for (int i = 1; i < length; ++i) {
+                ans = Math.max(ans, maxF[i]);
+            }
+            return ans;
+        }
+    }
+
 }
