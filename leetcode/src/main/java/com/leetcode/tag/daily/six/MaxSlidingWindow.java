0f007a6d5f3209eaced76bbdbc7cf7107319644a
xuweng <2028403305@qq.com>
2021-01-02 10:55:55 +0800

    方法一：优先队列
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/MaxSlidingWindow.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/MaxSlidingWindow.java
@@ -2,6 +2,7 @@ package com.leetcode.tag.daily.six;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * 239. 滑动窗口最大值
@@ -29,4 +30,33 @@ public class MaxSlidingWindow {
             return max;
         }
     }
+
+    /**
+     * 方法一：优先队列
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/sliding-window-maximum/solution/hua-dong-chuang-kou-zui-da-zhi-by-leetco-ki6m/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            int n = nums.length;
+            PriorityQueue<int[]> pq = new PriorityQueue<>((pair1, pair2) -> pair1[0] != pair2[0] ? pair2[0] - pair1[0] : pair2[1] - pair1[1]);
+            for (int i = 0; i < k; ++i) {
+                pq.offer(new int[]{nums[i], i});
+            }
+            int[] ans = new int[n - k + 1];
+            ans[0] = pq.peek()[0];
+            for (int i = k; i < n; ++i) {
+                pq.offer(new int[]{nums[i], i});
+                while (pq.peek()[1] <= i - k) {
+                    pq.poll();
+                }
+                ans[i - k + 1] = pq.peek()[0];
+            }
+            return ans;
+        }
+    }
+
 }
