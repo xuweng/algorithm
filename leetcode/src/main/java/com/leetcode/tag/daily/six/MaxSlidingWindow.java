1e86dcdba797eca0c83dabd07665befb9ba53c48
xuweng <2028403305@qq.com>
2021-01-02 11:37:35 +0800

    简洁代码 代码模板
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/six/MaxSlidingWindow.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/MaxSlidingWindow.java
@@ -107,5 +107,48 @@ public class MaxSlidingWindow {
         }
     }
 
+    /**
+     * 简洁代码
+     * <p>
+     * 简洁代码
+     * <p>
+     * 代码模板
+     * <p>
+     * 解释一下为什么队列中要存放数组下标的值而不是直接存储数值，因为要判断队首的值是否在窗口范围内，由数组下标取值很方便，而由值取数组下标不是很方便
+     * <p>
+     * 作者：hanyuhuang
+     * 链接：https://leetcode-cn.com/problems/sliding-window-maximum/solution/shuang-xiang-dui-lie-jie-jue-hua-dong-chuang-kou-2/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution3 {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            if (nums == null || nums.length < 2) {
+                return nums;
+            }
+            // 双向队列 保存当前窗口最大值的数组位置 保证队列中数组位置的数值按从大到小排序
+            LinkedList<Integer> queue = new LinkedList<>();
+            // 结果数组
+            int[] result = new int[nums.length - k + 1];
+            // 遍历nums数组
+            for (int i = 0; i < nums.length; i++) {
+                // 保证从大到小 如果前面数小则需要依次弹出，直至满足要求
+                while (!queue.isEmpty() && nums[queue.peekLast()] <= nums[i]) {
+                    queue.pollLast();
+                }
+                // 添加当前值对应的数组下标
+                queue.addLast(i);
+                // 判断当前队列中队首的值是否有效
+                if (queue.peek() <= i - k) {
+                    queue.poll();
+                }
+                // 当窗口长度为k时 保存当前窗口中最大值
+                if (i + 1 >= k) {
+                    result[i + 1 - k] = nums[queue.peek()];
+                }
+            }
+            return result;
+        }
+    }
 
 }
