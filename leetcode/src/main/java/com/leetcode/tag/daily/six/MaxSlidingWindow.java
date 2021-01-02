e15e4df28455bdfa8258a5e106a6dd4adca688db
xuweng <2028403305@qq.com>
2021-01-02 10:48:56 +0800

    239. 滑动窗口最大值
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/MaxSlidingWindow.java
@@ -0,0 +1,33 @@
+package com.leetcode.tag.daily.six;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 239. 滑动窗口最大值
+ */
+public class MaxSlidingWindow {
+    class Solution {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            if (nums == null || nums.length == 0) {
+                return new int[0];
+            }
+            List<Integer> result = new ArrayList<>();
+            for (int i = 0; i <= nums.length - k; i++) {
+                int max = max(nums, i, i + k);
+                result.add(max);
+            }
+
+            return result.stream().mapToInt(integer -> integer).toArray();
+        }
+
+        private int max(int[] nums, int start, int end) {
+            int max = Integer.MIN_VALUE;
+            for (int i = start; i < end; i++) {
+                max = Math.max(max, nums[i]);
+            }
+
+            return max;
+        }
+    }
+}
