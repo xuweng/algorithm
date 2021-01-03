3eaeda641a421947600c72483f61b1108fd3b048
xuweng <2028403305@qq.com>
2021-01-03 21:30:12 +0800

    322. 零钱兑换 老是搞错数组和dp数组
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/five/CoinChange1.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/five/CoinChange1.java
@@ -24,7 +24,7 @@ public class CoinChange1 {
                 }
             }
 
-            return dp[amount];
+            return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
         }
     }
 }
