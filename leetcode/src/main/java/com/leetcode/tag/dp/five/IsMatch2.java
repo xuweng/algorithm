3316f2d2bae23cbd15daa83867d017c5d064ed46
xuweng <2028403305@qq.com>
2021-01-04 21:21:33 +0800

    10. 正则表达式匹配
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dp/five/IsMatch2.java
@@ -0,0 +1,21 @@
+package com.leetcode.tag.dp.five;
+
+/**
+ * 10. 正则表达式匹配
+ */
+public class IsMatch2 {
+    class Solution {
+        public boolean isMatch(String s, String p) {
+            if (p.isEmpty()) {
+                return s.isEmpty();
+            }
+            boolean first = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
+            boolean next = s.length() > 1 && p.charAt(1) == '*';
+            if (next) {
+                return isMatch(s, p.substring(2)) || (first || isMatch(s.substring(1), p));
+            } else {
+                return first && isMatch(s.substring(1), p.substring(1));
+            }
+        }
+    }
+}
