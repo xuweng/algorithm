4ac5f972b1fed1948b211a70b20aa9e76204dece
xuweng <2028403305@qq.com>
2021-01-04 21:22:33 +0800

    10. 正则表达式匹配
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/five/IsMatch2.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/five/IsMatch2.java
@@ -10,7 +10,7 @@ public class IsMatch2 {
                 return s.isEmpty();
             }
             boolean first = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
-            boolean next = s.length() > 1 && p.charAt(1) == '*';
+            boolean next = p.length() > 1 && p.charAt(1) == '*';
             if (next) {
                 return isMatch(s, p.substring(2)) || (first || isMatch(s.substring(1), p));
             } else {
