bc59397ee103cf321f28cb58896fd9f39d942dc6
xuweng <2028403305@qq.com>
2021-01-05 20:23:29 +0800

    830. 较大分组的位置
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/six/LargeGroupPositions.java
@@ -0,0 +1,34 @@
+package com.leetcode.tag.daily.six;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 830. 较大分组的位置
+ */
+public class LargeGroupPositions {
+    class Solution {
+        public List<List<Integer>> largeGroupPositions(String s) {
+            if (s == null || s.isEmpty()) {
+                return null;
+            }
+
+            List<List<Integer>> result = new ArrayList<>();
+            int houIndex = s.length() - 1;
+            for (int i = s.length() - 2; i >= 0; i--) {
+                if (s.charAt(i) != s.charAt(houIndex)) {
+                    int count = houIndex - i;
+                    if (count >= 3) {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(i + 1, houIndex);
+
+                        result.add(list);
+                    }
+                    houIndex = i;
+                }
+            }
+
+            return result;
+        }
+    }
+}
