6bea1de29928745e18423162dc95b15f7e21372a
xuweng <2028403305@qq.com>
2021-01-06 21:38:55 +0800

    72. 编辑距离
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dp/six/MinDistance.java
@@ -0,0 +1,34 @@
+package com.leetcode.tag.dp.six;
+
+/**
+ * 72. 编辑距离
+ */
+public class MinDistance {
+    class Solution {
+        public int minDistance(String word1, String word2) {
+            int row = word1.length();
+            int col = word2.length();
+            int[][] dp = new int[row + 1][col + 1];
+
+            // 第1行
+            for (int i = 1; i <= col; i++) {
+                dp[0][i] = i;
+            }
+            // 第1列
+            for (int i = 1; i <= row; i++) {
+                dp[1][0] = i;
+            }
+            for (int i = 1; i <= row; i++) {
+                for (int j = 1; j <= col; j++) {
+                    if (word1.charAt(i) == word2.charAt(j)) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    } else {
+                        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    }
+                }
+            }
+
+            return dp[row][col];
+        }
+    }
+}
