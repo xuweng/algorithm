258ccfc0d9514f83f77c274abfa9b1f5b75e27a0
xuweng <2028403305@qq.com>
2021-01-06 21:41:45 +0800

    初始化
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/six/MinDistance.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/six/MinDistance.java
@@ -23,7 +23,7 @@ public class MinDistance {
             }
             for (int i = 1; i <= row; i++) {
                 for (int j = 1; j <= col; j++) {
-                    if (word1.charAt(i) == word2.charAt(j)) {
+                    if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
                         dp[i][j] = dp[i - 1][j - 1];
                     } else {
                         dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
