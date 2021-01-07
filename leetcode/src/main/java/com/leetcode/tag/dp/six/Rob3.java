0b02b9a398b2f23f320a452eb90cee23a164fec5
xuweng <2028403305@qq.com>
2021-01-07 21:33:36 +0800

    213. 打家劫舍 II
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/six/Rob3.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/six/Rob3.java
@@ -24,7 +24,7 @@ public class Rob3 {
         private int rob(int[] nums, int start, int end) {
             int[] dp = new int[nums.length];
             dp[start] = nums[start];
-            dp[start + 1] = Math.max(nums[0], nums[1]);
+            dp[start + 1] = Math.max(nums[start], nums[start + 1]);
 
             for (int i = start + 2; i <= end; i++) {
                 dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
