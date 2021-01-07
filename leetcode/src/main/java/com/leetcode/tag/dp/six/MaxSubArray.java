0837562183013d701341ed43e3809947ae497cc2
xuweng <2028403305@qq.com>
2021-01-07 20:23:42 +0800

    53. 最大子序和
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/six/MaxSubArray.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/six/MaxSubArray.java
@@ -14,7 +14,7 @@ public class MaxSubArray {
 
             int max = nums[0];
             for (int i = 1; i < nums.length; i++) {
-                dp[i] = Math.max(dp[i], dp[i - 1] + nums[i]);
+                dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
 
                 max = Math.max(max, dp[i]);
             }
