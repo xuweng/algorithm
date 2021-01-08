ff6da601210cb7befef76a4bada57943a3d8b53b
xuweng <2028403305@qq.com>
2021-01-08 21:52:57 +0800

    416. 分割等和子集
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/six/CanPartition.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/six/CanPartition.java
@@ -19,7 +19,7 @@ public class CanPartition {
                     if (j < nums[i - 1]) {
                         dp[i][j] = dp[i - 1][j];
                     } else {
-                        dp[i][j] = dp[i - 1][j] || dp[i][j - nums[i - 1]];
+                        dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
                     }
                 }
             }
