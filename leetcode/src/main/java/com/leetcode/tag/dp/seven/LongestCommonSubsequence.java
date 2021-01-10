83391b1a0bb38561035d86cf87f130308eb17b4a
xuweng <2028403305@qq.com>
2021-01-10 21:16:08 +0800

    1143. 最长公共子序列
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/seven/LongestCommonSubsequence.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/seven/LongestCommonSubsequence.java
@@ -6,7 +6,7 @@ package com.leetcode.tag.dp.seven;
 public class LongestCommonSubsequence {
     class Solution {
         public int longestCommonSubsequence(String text1, String text2) {
-            int[][] dp = new int[text1.length() + 1][text2.length()];
+            int[][] dp = new int[text1.length() + 1][text2.length() + 1];
             for (int i = 1; i <= text1.length(); i++) {
                 for (int j = 1; j <= text2.length(); j++) {
                     if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
