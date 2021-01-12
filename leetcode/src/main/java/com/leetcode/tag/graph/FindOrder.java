c2cb16824eb3ed912a885693bb61ee2ec1e788b2
xuweng <2028403305@qq.com>
2021-01-12 21:27:35 +0800

    210. 课程表 II
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/graph/FindOrder.java
@@ -0,0 +1,56 @@
+package com.leetcode.tag.graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 210. 课程表 II
+ * <p>
+ * bfs 有环 不会入队
+ */
+public class FindOrder {
+    class Solution {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int[] visited;
+        int[] result;
+        int index;
+
+        public int[] findOrder(int numCourses, int[][] prerequisites) {
+            for (int[] prerequisite : prerequisites) {
+                map.computeIfAbsent(prerequisite[1], v -> new ArrayList<>()).add(prerequisite[0]);
+            }
+            visited = new int[numCourses];
+            result = new int[numCourses];
+            for (int i = 0; i < numCourses; i++) {
+                if (visited[i] == 0 && dfs(i)) {
+                    return new int[0];
+                }
+            }
+
+            return result;
+        }
+
+        private boolean dfs(int i) {
+            if (visited[i] == 1) {
+                return true;
+            }
+            if (visited[i] == 2) {
+                return false;
+            }
+            visited[i] = 1;
+            result[index++] = i;
+            if (map.containsKey(i)) {
+                List<Integer> list = map.get(i);
+                for (Integer integer : list) {
+                    if (dfs(i)) {
+                        return true;
+                    }
+                }
+            }
+            visited[i] = 2;
+            return false;
+        }
+    }
+}
