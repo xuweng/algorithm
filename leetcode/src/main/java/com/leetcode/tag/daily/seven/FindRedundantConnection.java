b3329319e4e6c814e4902314cbb3e5cbb44b6bb9
xuweng <2028403305@qq.com>
2021-01-13 20:43:23 +0800

    684. 冗余连接
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/seven/FindRedundantConnection.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/seven/FindRedundantConnection.java
@@ -1,5 +1,10 @@
 package com.leetcode.tag.daily.seven;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 684. 冗余连接
  */
@@ -47,6 +52,52 @@ public class FindRedundantConnection {
             }
             parent[iRoot] = jRoot;
             return true;
+        }
+    }
+
+    class Solution1 {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int[] visited;
+        int[] result;
+
+        public int[] findRedundantConnection(int[][] edges) {
+            if (edges == null || edges.length == 0) {
+                return new int[0];
+            }
+            visited = new int[edges.length + 1];
+            result = new int[2];
+            for (int[] edge : edges) {
+                map.computeIfAbsent(edge[0], v -> new ArrayList<>()).add(edge[1]);
+                map.computeIfAbsent(edge[1], v -> new ArrayList<>()).add(edge[0]);
+            }
+            for (int i = 1; i <= edges.length; i++) {
+                if (visited[i] == 0 && dfs(i)) {
+                    result[0] = i;
+                }
+            }
+
+            return result;
+        }
+
+        private boolean dfs(int i) {
+            if (visited[i] == 1) {
+                result[1] = i;
+                return true;
+            }
+            if (visited[i] == 2) {
+                return false;
+            }
+            visited[i] = 1;
+            if (map.containsKey(i)) {
+                List<Integer> list = map.get(i);
+                for (Integer integer : list) {
+                    if (dfs(integer)) {
+                        return true;
+                    }
+                }
+            }
+            visited[i] = 2;
+            return false;
         }
     }
 }
