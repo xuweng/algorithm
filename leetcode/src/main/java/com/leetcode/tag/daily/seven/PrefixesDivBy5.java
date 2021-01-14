8ceaa4dcf4d4416f26daca5f765a05ac145e9b4c
xuweng <2028403305@qq.com>
2021-01-14 20:25:21 +0800

    方法一：模拟
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/seven/PrefixesDivBy5.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/seven/PrefixesDivBy5.java
@@ -25,4 +25,25 @@ public class PrefixesDivBy5 {
             return result;
         }
     }
+
+    /**
+     * 方法一：模拟
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/binary-prefix-divisible-by-5/solution/ke-bei-5-zheng-chu-de-er-jin-zhi-qian-zh-asih/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public List<Boolean> prefixesDivBy5(int[] A) {
+            List<Boolean> list = new ArrayList<>();
+            int prefix = 0;
+            for (int j : A) {
+                prefix = ((prefix << 1) + j) % 5;
+                list.add(prefix == 0);
+            }
+            return list;
+        }
+    }
+
 }
