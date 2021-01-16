7a91c05086bd2603eebe4998b26272a5d4bd0d2f
xuweng <2028403305@qq.com>
2021-01-16 22:52:51 +0800

    96. 不同的二叉搜索树
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/nine/NumTrees.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/nine/NumTrees.java
@@ -11,7 +11,7 @@ public class NumTrees {
 
             for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= i; j++) {
-                    dp[i] += dp[j - 1] + dp[i - j];
+                    dp[i] += dp[j - 1] * dp[i - j];
                 }
             }
 
