a618831ba70f39064b141db8def7c9c14098fa9d
xuweng <2028403305@qq.com>
2021-01-17 18:32:03 +0800

    85. 最大矩形
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/two/MaximalRectangle.java
@@ -0,0 +1,51 @@
+package com.leetcode.tag.contest.two;
+
+/**
+ * 85. 最大矩形
+ * <p>
+ * 知道思路后，直接写代码
+ * <p>
+ * 一定要自己写一遍 一定要自己写 一定要自己写 一定要自己写
+ */
+public class MaximalRectangle {
+    class Solution {
+        public int maximalRectangle(char[][] matrix) {
+            if (matrix == null || matrix.length == 0) {
+                return 0;
+            }
+            int[][] ints = new int[matrix.length][matrix[0].length];
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    ints[i][j] = Integer.parseInt(String.valueOf(matrix[i][j]));
+                }
+            }
+
+            int max = ints[0][0];
+            for (int i = 0; i < ints.length; i++) {
+                for (int j = 1; j < ints[0].length; j++) {
+                    if (ints[i][j] == 1) {
+                        if (i == 0) {
+                            max = 1;
+                        }
+                        ints[i][j] = ints[i][j - 1] + 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < ints.length; i++) {
+                for (int j = 0; j < ints[0].length; j++) {
+                    int height = 1;
+                    int min = ints[i][j];
+                    for (int k = i; k >= 0; k--) {
+                        min = Math.min(min, ints[k][j]);
+
+                        max = Math.max(max, min * height);
+                        height++;
+                    }
+                }
+            }
+
+            return max;
+        }
+    }
+}
