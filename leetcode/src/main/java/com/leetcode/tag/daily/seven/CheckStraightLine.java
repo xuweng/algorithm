c574b90b6421c94fc2b1643b8e5d9588f88f2e13
xuweng <2028403305@qq.com>
2021-01-17 10:25:52 +0800

    方法一：数学
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/seven/CheckStraightLine.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/seven/CheckStraightLine.java
@@ -20,4 +20,32 @@ public class CheckStraightLine {
             return true;
         }
     }
+
+    /**
+     * 方法一：数学
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/check-if-it-is-a-straight-line/solution/zhui-dian-cheng-xian-by-leetcode-solutio-lpt6/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public boolean checkStraightLine(int[][] coordinates) {
+            int deltaX = coordinates[0][0], deltaY = coordinates[0][1];
+            int n = coordinates.length;
+            for (int i = 0; i < n; i++) {
+                coordinates[i][0] -= deltaX;
+                coordinates[i][1] -= deltaY;
+            }
+            int A = coordinates[1][1], B = -coordinates[1][0];
+            for (int i = 2; i < n; i++) {
+                int x = coordinates[i][0], y = coordinates[i][1];
+                if (A * x + B * y != 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
 }
