f7bd0f72358ab12ece0dd12d7d3f787aa6a23ea2
xuweng <2028403305@qq.com>
2021-01-17 11:42:36 +0800

    5655. 重新排列后的最大子矩阵
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/two/LargestSubmatrix.java
@@ -0,0 +1,12 @@
+package com.leetcode.tag.contest.two;
+
+/**
+ * 5655. 重新排列后的最大子矩阵
+ */
+public class LargestSubmatrix {
+    class Solution {
+        public int largestSubmatrix(int[][] matrix) {
+            return 0;
+        }
+    }
+}
