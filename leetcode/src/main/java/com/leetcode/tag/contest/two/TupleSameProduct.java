bcceb61b2f4eb4b0a88ebc0f639cf2586620327e
xuweng <2028403305@qq.com>
2021-01-17 11:09:11 +0800

    5243. 同积元组
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/contest/two/TupleSameProduct.java
+++ leetcode/src/main/java/com/leetcode/tag/contest/two/TupleSameProduct.java
@@ -32,4 +32,27 @@ public class TupleSameProduct {
             return result * 8;
         }
     }
+
+    class Solution1 {
+        public int tupleSameProduct(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return 0;
+            }
+            Arrays.sort(nums);
+            int result = 0;
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    for (int k = j + 1; k < nums.length; k++) {
+                        for (int l = k + 1; l < nums.length; l++) {
+                            if (nums[i] * nums[l] == nums[j] * nums[k] || nums[i] * nums[k] == nums[j] * nums[l]) {
+                                result++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return result * 8;
+        }
+    }
 }
