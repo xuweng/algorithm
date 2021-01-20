1d241bf947289065f9bed80112f5ef4672eba673
xuweng <2028403305@qq.com>
2021-01-20 21:17:33 +0800

    516. 最长回文子序列
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/ten/LongestPalindromeSubseq1.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/ten/LongestPalindromeSubseq1.java
@@ -13,7 +13,7 @@ public class LongestPalindromeSubseq1 {
             for (int i = 0; i < s.length(); i++) {
                 dp[i][i] = 1;
             }
-            for (int i = s.length() - 2; i > 0; i--) {
+            for (int i = s.length() - 2; i >= 0; i--) {
                 for (int j = i + 1; j < s.length(); j++) {
                     if (s.charAt(i) == s.charAt(j)) {
                         dp[i][j] = dp[i + 1][j - 1] + 2;
