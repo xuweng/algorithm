9b9fd45b3abec8e2e746b51e27c1041b94104893
xuweng <2028403305@qq.com>
2021-01-22 21:34:33 +0800

    72. 编辑距离
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/eleven/MinDistance.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/eleven/MinDistance.java
@@ -18,7 +18,7 @@ public class MinDistance {
                     if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
                         dp[i][j] = dp[i - 1][j - 1];
                     } else {
-                        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.max(dp[i][j - 1], dp[i - 1][j])) + 1;
+                        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i][j - 1], dp[i - 1][j])) + 1;
                     }
                 }
             }
