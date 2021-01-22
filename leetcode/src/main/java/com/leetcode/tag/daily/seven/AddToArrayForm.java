1f233e0398f01db5aad11031657dab986083c615
xuweng <2028403305@qq.com>
2021-01-22 20:18:18 +0800

    大数溢出
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/seven/AddToArrayForm.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/seven/AddToArrayForm.java
@@ -37,4 +37,27 @@ public class AddToArrayForm {
         }
     }
 
+    /**
+     * 大数溢出
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/add-to-array-form-of-integer/solution/shu-zu-xing-shi-de-zheng-shu-jia-fa-by-l-jljp/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public List<Integer> addToArrayForm(int[] A, int K) {
+            List<Integer> res = new ArrayList<>();
+            int n = A.length;
+            for (int i = n - 1; i >= 0 || K > 0; --i, K /= 10) {
+                if (i >= 0) {
+                    K += A[i];
+                }
+                res.add(K % 10);
+            }
+            Collections.reverse(res);
+            return res;
+        }
+    }
+
 }
