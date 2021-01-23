3a86d00e7b3af503d822662cce38e78375ae5533
xuweng <2028403305@qq.com>
2021-01-23 21:07:40 +0800

    方法二：并查集
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/seven/MakeConnected.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/seven/MakeConnected.java
@@ -1,6 +1,7 @@
 package com.leetcode.tag.daily.seven;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -55,6 +56,76 @@ public class MakeConnected {
                     dfs(v);
                 }
             }
+        }
+    }
+
+    /**
+     * 方法二：并查集
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/number-of-operations-to-make-network-connected/solution/lian-tong-wang-luo-de-cao-zuo-ci-shu-by-leetcode-s/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int makeConnected(int n, int[][] connections) {
+            if (connections.length < n - 1) {
+                return -1;
+            }
+
+            UnionFind uf = new UnionFind(n);
+            for (int[] conn : connections) {
+                uf.unite(conn[0], conn[1]);
+            }
+
+            return uf.setCount - 1;
+        }
+    }
+
+    // 并查集模板
+    class UnionFind {
+        int[] parent;
+        int[] size;
+        int n;
+        // 当前连通分量数目
+        int setCount;
+
+        public UnionFind(int n) {
+            this.n = n;
+            this.setCount = n;
+            this.parent = new int[n];
+            this.size = new int[n];
+            Arrays.fill(size, 1);
+            for (int i = 0; i < n; ++i) {
+                parent[i] = i;
+            }
+        }
+
+        public int findset(int x) {
+            return parent[x] == x ? x : (parent[x] = findset(parent[x]));
+        }
+
+        public boolean unite(int x, int y) {
+            x = findset(x);
+            y = findset(y);
+            if (x == y) {
+                return false;
+            }
+            if (size[x] < size[y]) {
+                int temp = x;
+                x = y;
+                y = temp;
+            }
+            parent[y] = x;
+            size[x] += size[y];
+            --setCount;
+            return true;
+        }
+
+        public boolean connected(int x, int y) {
+            x = findset(x);
+            y = findset(y);
+            return x == y;
         }
     }
 
