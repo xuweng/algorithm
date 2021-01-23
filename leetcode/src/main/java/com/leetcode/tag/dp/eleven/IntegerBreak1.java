6e5494e2a39a88ee126de15f35ed815dbe0dfb25
xuweng <2028403305@qq.com>
2021-01-23 21:50:13 +0800

    343. 整数拆分
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/eleven/IntegerBreak1.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/eleven/IntegerBreak1.java
@@ -8,7 +8,7 @@ public class IntegerBreak1 {
         public int integerBreak(int n) {
             int[] dp = new int[n + 1];
             dp[2] = 1;
-            for (int i = 1; i <= n; i++) {
+            for (int i = 3; i <= n; i++) {
                 for (int j = 1; j < i; j++) {
                     dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
                 }
