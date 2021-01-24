21ef5ac5037f52912e7304adea86be4f6967dfa2
xuweng <2028403305@qq.com>
2021-01-24 11:42:30 +0800

    5663. 找出第 K 大的异或坐标值
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/contest/two/KthLargestValue.java
+++ leetcode/src/main/java/com/leetcode/tag/contest/two/KthLargestValue.java
@@ -12,20 +12,32 @@ public class KthLargestValue {
         public int kthLargestValue(int[][] matrix, int k) {
             List<Integer> list = new ArrayList<>();
             list.add(matrix[0][0]);
-            int pre = matrix[0][0];
             for (int i = 0; i < matrix.length; i++) {
                 for (int j = 0; j < matrix[0].length; j++) {
                     if (i == 0 && j == 0) {
                         continue;
                     }
-                    int v = pre ^ matrix[i][j];
-                    list.add(v);
-                    pre = v;
+                    list.add(zheng(matrix, i, j));
                 }
             }
             list.sort(Collections.reverseOrder());
 
             return list.get(k - 1);
+        }
+
+        private int zheng(int[][] matrix, int start, int end) {
+            int pre = matrix[0][0];
+            for (int i = 0; i <= start; i++) {
+                for (int j = 0; j <= end; j++) {
+                    if (i == 0 && j == 0) {
+                        continue;
+                    }
+                    int v = pre ^ matrix[i][j];
+                    pre = v;
+                }
+            }
+
+            return pre;
         }
     }
 }
