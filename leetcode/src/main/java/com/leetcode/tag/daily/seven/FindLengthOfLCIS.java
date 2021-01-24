93c0d91c4d1eaf0f411575cfaf246dd47c4f3c8a
xuweng <2028403305@qq.com>
2021-01-24 10:30:18 +0800

    674. 最长连续递增序列
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/seven/FindLengthOfLCIS.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/seven/FindLengthOfLCIS.java
@@ -10,15 +10,10 @@ public class FindLengthOfLCIS {
                 return 0;
             }
             int[] dp = new int[nums.length];
-            int max = 0;
-            for (int i = 0; i < nums.length; i++) {
-                dp[i] = 1;
-                for (int j = 0; j < i; j++) {
-                    if (nums[j] >= nums[i]) {
-                        continue;
-                    }
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
+            dp[0] = 1;
+            int max = 1;
+            for (int i = 1; i < nums.length; i++) {
+                dp[i] = nums[i - 1] >= nums[i] ? 1 : dp[i - 1] + 1;
 
                 max = Math.max(max, dp[i]);
             }
