e4dc3acc26a6f44853afb10dd59fefa19e8e0c1c
xuweng <2028403305@qq.com>
2021-01-24 11:54:44 +0800

    5663. 找出第 K 大的异或坐标值
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/contest/two/KthLargestValue.java
+++ leetcode/src/main/java/com/leetcode/tag/contest/two/KthLargestValue.java
@@ -1,6 +1,7 @@
 package com.leetcode.tag.contest.two;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,15 +10,24 @@ import java.util.List;
  */
 public class KthLargestValue {
     class Solution {
+        int[][] meno;
+
         public int kthLargestValue(int[][] matrix, int k) {
+            meno = new int[matrix.length][matrix[0].length];
+            for (int[] ints : meno) {
+                Arrays.fill(ints, -1);
+            }
+
             List<Integer> list = new ArrayList<>();
             list.add(matrix[0][0]);
+            meno[0][0] = matrix[0][0];
             for (int i = 0; i < matrix.length; i++) {
                 for (int j = 0; j < matrix[0].length; j++) {
                     if (i == 0 && j == 0) {
                         continue;
                     }
-                    list.add(zheng(matrix, i, j));
+                    int zheng = zheng(matrix, i, j);
+                    list.add(zheng);
                 }
             }
             list.sort(Collections.reverseOrder());
@@ -25,18 +35,23 @@ public class KthLargestValue {
             return list.get(k - 1);
         }
 
-        private int zheng(int[][] matrix, int start, int end) {
-            int pre = matrix[0][0];
-            for (int i = 0; i <= start; i++) {
-                for (int j = 0; j <= end; j++) {
-                    if (i == 0 && j == 0) {
-                        continue;
-                    }
-                    pre = pre ^ matrix[i][j];
+        private int zheng(int[][] matrix, int row, int col) {
+            int v = 0;
+            if (row == 0) {
+                for (int i = 1; i <= col; i++) {
+                    v = meno[row][i - 1] ^ matrix[row][i];
+                }
+            } else if (col == 0) {
+                for (int i = 1; i <= row; i++) {
+                    v = meno[i - 1][col] ^ matrix[i][col];
+                }
+            } else {
+                for (int i = 1; i <= col; i++) {
+                    v = meno[row - 1][col] ^ matrix[row][col];
                 }
             }
 
-            return pre;
+            return v;
         }
     }
 }
