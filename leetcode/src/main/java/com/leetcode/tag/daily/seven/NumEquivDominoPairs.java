fe20dd8095a2bc1696c5c35e78ff6a285c697c19
xuweng <2028403305@qq.com>
2021-01-26 20:51:06 +0800

    1128. 等价多米诺骨牌对的数量
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/seven/NumEquivDominoPairs.java
@@ -0,0 +1,29 @@
+package com.leetcode.tag.daily.seven;
+
+/**
+ * 1128. 等价多米诺骨牌对的数量
+ */
+public class NumEquivDominoPairs {
+    /**
+     * 方法一：二元组表示 + 计数
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/number-of-equivalent-domino-pairs/solution/deng-jie-duo-mi-nuo-gu-pai-dui-de-shu-li-yjlz/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution {
+        public int numEquivDominoPairs(int[][] dominoes) {
+            int[] num = new int[100];
+            int ret = 0;
+            for (int[] domino : dominoes) {
+                //将每一个二元对拼接成一个两位的正整数
+                int val = domino[0] < domino[1] ? domino[0] * 10 + domino[1] : domino[1] * 10 + domino[0];
+                ret += num[val];
+                num[val]++;
+            }
+            return ret;
+        }
+    }
+
+}
