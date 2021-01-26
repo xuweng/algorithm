f3e5b0d1f6a395d1d4fcc76c5254a8032a0ab589
xuweng <2028403305@qq.com>
2021-01-26 21:16:04 +0800

    状态压缩 二进制
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/seven/FindTheLongestSubstring.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/seven/FindTheLongestSubstring.java
@@ -19,6 +19,19 @@ public class FindTheLongestSubstring {
     class Solution {
         /**
          * pre[i][k] 表示在字符串前 i 个字符中，第 k 个元音字母一共出现的次数
+         * <p>
+         * dp[pattern] 的作用是用来记录当前索引值下对应的元音奇偶次数组合特征
+         * <p>
+         * 定义特征，aeiou 分别对应二进制 00001，00010，00100，01000，10000
+         * 其中 0 表示对应元音出现了偶数次数，1 表示奇数
+         * <p>
+         * pattern 为 10，也就是对应二进制 01010，dp[pattern] = 8 的意思为，
+         * 当索引值为 8 的时候，e 和 o 都出现了奇数次，其它元音为偶数次。
+         * <p>
+         * 作者：will_never_die
+         * 链接：https://leetcode-cn.com/problems/find-the-longest-substring-containing-vowels-in-even-counts/solution/xiang-xi-jie-shi-by-will_never_die/
+         * 来源：力扣（LeetCode）
+         * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
          *
          * @param s
          * @return
