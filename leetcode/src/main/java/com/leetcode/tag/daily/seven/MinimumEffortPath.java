277c96141b7098a52dd8f6682f5dc783c600d5b5
xuweng <2028403305@qq.com>
2021-01-29 20:41:22 +0800

    Dijkstra 算法
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/seven/MinimumEffortPath.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/seven/MinimumEffortPath.java
@@ -183,4 +183,46 @@ public class MinimumEffortPath {
         }
     }
 
+    /**
+     * Dijkstra 算法
+     */
+    class Solution3 {
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        public int minimumEffortPath(int[][] heights) {
+            int m = heights.length;
+            int n = heights[0].length;
+            PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(edge -> edge[2]));
+            pq.offer(new int[]{0, 0, 0});
+
+            int[] dist = new int[m * n];
+            Arrays.fill(dist, Integer.MAX_VALUE);
+            dist[0] = 0;
+            boolean[] seen = new boolean[m * n];
+
+            while (!pq.isEmpty()) {
+                int[] edge = pq.poll();
+                int x = edge[0], y = edge[1], d = edge[2];
+                int id = x * n + y;
+                if (seen[id]) {
+                    continue;
+                }
+                if (x == m - 1 && y == n - 1) {
+                    break;
+                }
+                seen[id] = true;
+                for (int i = 0; i < 4; ++i) {
+                    int nx = x + dirs[i][0];
+                    int ny = y + dirs[i][1];
+                    if (nx >= 0 && nx < m && ny >= 0 && ny < n && Math.max(d, Math.abs(heights[x][y] - heights[nx][ny])) < dist[nx * n + ny]) {
+                        dist[nx * n + ny] = Math.max(d, Math.abs(heights[x][y] - heights[nx][ny]));
+                        pq.offer(new int[]{nx, ny, dist[nx * n + ny]});
+                    }
+                }
+            }
+
+            return dist[m * n - 1];
+        }
+    }
+
 }
