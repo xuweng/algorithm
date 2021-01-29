fe3cc2bd78b7f336612551d77891cd10783cb2b0
xuweng <2028403305@qq.com>
2021-01-29 21:40:30 +0800

    474. 一和零
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/one/FindMaxForm.java
+++ leetcode/src/main/java/com/leetcode/tag/must/one/FindMaxForm.java
@@ -35,4 +35,32 @@ public class FindMaxForm {
             return result;
         }
     }
+
+    class Solution1 {
+        public int findMaxForm(String[] strs, int m, int n) {
+            if (strs == null || strs.length == 0) {
+                return 0;
+            }
+            int[][] dp = new int[m + 1][n + 1];
+            for (String str : strs) {
+                int[] count = count(str);
+                for (int i = m; i >= count[0]; i--) {
+                    for (int j = n; j >= count[1]; j--) {
+                        dp[i][j] = Math.max(dp[i][j], dp[i - count[0]][j - count[1]] + 1);
+                    }
+                }
+            }
+
+            return dp[m][n];
+        }
+
+        private int[] count(String s) {
+            int[] result = new int[2];
+            for (int i = 0; i < s.length(); i++) {
+                result[s.charAt(i) - '0']++;
+            }
+
+            return result;
+        }
+    }
 }
