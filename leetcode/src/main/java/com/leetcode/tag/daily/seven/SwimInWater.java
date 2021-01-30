29f3f991ca709b7ab42baea99d0ea755e7bde047
xuweng <2028403305@qq.com>
2021-01-30 21:04:33 +0800

    方法三：Dijkstra 算法
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/seven/SwimInWater.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/seven/SwimInWater.java
@@ -1,5 +1,10 @@
 package com.leetcode.tag.daily.seven;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  * 778. 水位上升的泳池中游泳
  */
@@ -152,6 +157,61 @@ public class SwimInWater {
                 }
                 parent[root(p)] = root(q);
             }
+        }
+    }
+
+    /**
+     * 方法三：Dijkstra 算法
+     */
+    class Solution2 {
+        // Dijkstra 算法（应用前提：没有负权边，找单源最短路径）
+        public int swimInWater(int[][] grid) {
+            int n = grid.length;
+
+            Queue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(o -> grid[o[0]][o[1]]));
+            minHeap.offer(new int[]{0, 0});
+
+            boolean[][] visited = new boolean[n][n];
+            // distTo[i][j] 表示：到顶点 [i, j] 须要等待的最少的时间
+            int[][] distTo = new int[n][n];
+            for (int[] row : distTo) {
+                Arrays.fill(row, n * n);
+            }
+            distTo[0][0] = grid[0][0];
+
+            int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+            while (!minHeap.isEmpty()) {
+                // 找最短的边
+                int[] front = minHeap.poll();
+                int currentX = front[0];
+                int currentY = front[1];
+                if (visited[currentX][currentY]) {
+                    continue;
+                }
+
+                // 确定最短路径顶点
+                visited[currentX][currentY] = true;
+                if (currentX == n - 1 && currentY == n - 1) {
+                    return distTo[n - 1][n - 1];
+                }
+
+                // 更新
+                for (int[] direction : directions) {
+                    int newX = currentX + direction[0];
+                    int newY = currentY + direction[1];
+                    if (inArea(newX, newY, n) && !visited[newX][newY] &&
+                            Math.max(distTo[currentX][currentY], grid[newX][newY]) < distTo[newX][newY]) {
+                        // 更新距离表
+                        distTo[newX][newY] = Math.max(distTo[currentX][currentY], grid[newX][newY]);
+                        minHeap.offer(new int[]{newX, newY});
+                    }
+                }
+            }
+            return -1;
+        }
+
+        private boolean inArea(int x, int y, int n) {
+            return x >= 0 && x < n && y >= 0 && y < n;
         }
     }
 
