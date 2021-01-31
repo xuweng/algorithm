31ea34b500b71f09aa99b27c05c6c5c0a7a0ca84
xuweng <2028403305@qq.com>
2021-01-31 22:14:27 +0800

    131. 分割回文串
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/backtracking/two/Partition.java
@@ -0,0 +1,50 @@
+package com.leetcode.tag.backtracking.two;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 131. 分割回文串
+ */
+public class Partition {
+    class Solution {
+        Deque<String> deque = new LinkedList<>();
+        List<List<String>> result = new ArrayList<>();
+
+        public List<List<String>> partition(String s) {
+            back(s, 0);
+
+            return result;
+        }
+
+        private void back(String s, int index) {
+            if (index >= s.length()) {
+                result.add(new ArrayList<>(deque));
+                return;
+            }
+            for (int i = index; i < s.length(); i++) {
+                if (!isHui(s, index, i)) {
+                    continue;
+                }
+                String substring = s.substring(index, i + 1);
+                deque.offerLast(substring);
+                back(s, i + 1);
+                deque.pollLast();
+            }
+        }
+
+        private boolean isHui(String s, int i, int j) {
+            while (i < j) {
+                if (s.charAt(i) != s.charAt(j)) {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+    }
+}
