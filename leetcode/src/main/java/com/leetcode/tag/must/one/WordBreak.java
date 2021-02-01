33a9146a5f39c6d68186fc5b6ff2bfdafa86bb04
xuweng <2028403305@qq.com>
2021-02-01 21:23:20 +0800

    方法一：动态规划
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/one/WordBreak.java
+++ leetcode/src/main/java/com/leetcode/tag/must/one/WordBreak.java
@@ -33,4 +33,28 @@ public class WordBreak {
             return dp[s.length() - 1];
         }
     }
+
+    /**
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/word-break/solution/dan-ci-chai-fen-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public boolean wordBreak(String s, List<String> wordDict) {
+            Set<String> wordDictSet = new HashSet<>(wordDict);
+            boolean[] dp = new boolean[s.length() + 1];
+            dp[0] = true;
+            for (int i = 1; i <= s.length(); i++) {
+                for (int j = 0; j < i; j++) {
+                    if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+            return dp[s.length()];
+        }
+    }
+
 }
