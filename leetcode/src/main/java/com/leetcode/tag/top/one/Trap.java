c8ec5a6348ac0ef1505e14dc9f4fbefe05cf0d23
xuweng <2028403305@qq.com>
2021-02-02 21:44:57 +0800

    单调递增栈
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/top/one/Trap.java
+++ leetcode/src/main/java/com/leetcode/tag/top/one/Trap.java
@@ -121,6 +121,8 @@ public class Trap {
     class Solution3 {
         public int trap(int[] height) {
             int ans = 0, current = 0;
+            // 单调递增栈
+            // 使用栈来存储条形块的索引下标
             Deque<Integer> stack = new LinkedList<>();
             while (current < height.length) {
                 while (!stack.isEmpty() && height[current] > height[stack.peek()]) {
