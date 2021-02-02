17de7c330e55dfe2aedbe45849760cec3aedd729
xuweng <2028403305@qq.com>
2021-02-02 21:40:02 +0800

    方法 2：动态编程
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/top/one/Trap.java
+++ leetcode/src/main/java/com/leetcode/tag/top/one/Trap.java
@@ -69,4 +69,41 @@ public class Trap {
             return ans;
         }
     }
+
+    /**
+     * 方法 2：动态编程
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/trapping-rain-water/solution/jie-yu-shui-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public int trap(int[] height) {
+            if (height == null || height.length == 0) {
+                return 0;
+            }
+            int ans = 0;
+            int size = height.length;
+            // 左侧递增
+            int[] leftMax = new int[size];
+            leftMax[0] = height[0];
+            for (int i = 1; i < size; i++) {
+                leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+            }
+
+            // 右侧递增
+            int[] rightMax = new int[size];
+            rightMax[size - 1] = height[size - 1];
+            for (int i = size - 2; i >= 0; i--) {
+                rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+            }
+
+            for (int i = 1; i < size - 1; i++) {
+                ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+            }
+            return ans;
+        }
+
+    }
 }
