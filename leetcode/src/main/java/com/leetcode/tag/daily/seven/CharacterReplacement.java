f2ccd67036d6892b7f76229c0a63e3f6e17c29c7
xuweng <2028403305@qq.com>
2021-02-02 20:38:39 +0800

    424. 替换后的最长重复字符
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/seven/CharacterReplacement.java
@@ -0,0 +1,36 @@
+package com.leetcode.tag.daily.seven;
+
+/**
+ * 424. 替换后的最长重复字符
+ */
+public class CharacterReplacement {
+    /**
+     * 方法一：双指针
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/longest-repeating-character-replacement/solution/ti-huan-hou-de-zui-chang-zhong-fu-zi-fu-n6aza/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution {
+        public int characterReplacement(String s, int k) {
+            int[] num = new int[26];
+            int n = s.length();
+            int maxn = 0;
+            int left = 0, right = 0;
+            // 枚举字符串中的每一个位置作为右端点
+            // 找到其最远的左端点的位置，满足该区间内除了出现次数最多的那一类字符之外，剩余的字符（即非最长重复字符）数量不超过 k 个
+            while (right < n) {
+                num[s.charAt(right) - 'A']++;
+                maxn = Math.max(maxn, num[s.charAt(right) - 'A']);
+                if (right - left + 1 - maxn > k) {
+                    num[s.charAt(left) - 'A']--;
+                    left++;
+                }
+                right++;
+            }
+            return right - left;
+        }
+    }
+
+}
