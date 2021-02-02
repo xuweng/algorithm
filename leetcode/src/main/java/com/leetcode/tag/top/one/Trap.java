d14519769b3c0ac4f407ce4f284554fd77a87c8e
xuweng <2028403305@qq.com>
2021-02-02 21:32:02 +0800

    方法 1：暴力
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/top/one/Trap.java
+++ leetcode/src/main/java/com/leetcode/tag/top/one/Trap.java
@@ -37,4 +37,36 @@ public class Trap {
             return result;
         }
     }
+
+    /**
+     * 方法 1：暴力
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/trapping-rain-water/solution/jie-yu-shui-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        /**
+         * 对于数组中的每个元素，我们找出下雨后水能达到的最高位置，等于两边最大高度的较小值减去当前高度的值
+         *
+         * @param height
+         * @return
+         */
+        public int trap(int[] height) {
+            int ans = 0;
+            int size = height.length;
+            for (int i = 1; i < size - 1; i++) {
+                int maxLeft = 0, maxRight = 0;
+                for (int j = i; j >= 0; j--) { //Search the left part for max bar size
+                    maxLeft = Math.max(maxLeft, height[j]);
+                }
+                for (int j = i; j < size; j++) { //Search the right part for max bar size
+                    maxRight = Math.max(maxRight, height[j]);
+                }
+                ans += Math.min(maxLeft, maxRight) - height[i];
+            }
+            return ans;
+        }
+    }
 }
