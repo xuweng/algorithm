dcb9173e3ad84d66986a4301465088aface27b40
xuweng <2028403305@qq.com>
2021-02-02 21:01:29 +0800

    滑动窗口
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/seven/CharacterReplacement.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/seven/CharacterReplacement.java
@@ -37,4 +37,25 @@ public class CharacterReplacement {
         }
     }
 
+    class Solution1 {
+        public int characterReplacement(String s, int k) {
+            int[] record = new int[26];
+            int len = s.length();
+            char[] arr = s.toCharArray();
+            int left = 0;
+            int right = 0;
+            int historyMax = 0;
+            while (right < len) {
+                record[arr[right] - 'A']++;
+                historyMax = Math.max(historyMax, record[arr[right] - 'A']);
+                right++;
+                while (right - left > historyMax + k) {
+                    record[arr[left] - 'A']--;
+                    left++;
+                }
+            }
+            return right - left;
+        }
+    }
+
 }
