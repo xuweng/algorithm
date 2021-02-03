ffe1288a09d8b289d9271a90aeaf2ccbd9027557
xuweng <2028403305@qq.com>
2021-02-03 20:22:01 +0800

    最大堆 最小堆
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/seven/MedianFinder.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/seven/MedianFinder.java
@@ -16,6 +16,10 @@ public class MedianFinder {
      * 当前大顶堆和小顶堆的元素个数之和
      */
     private int count;
+    /**
+     * 最大堆的堆顶元素，小于或者等于最小堆的堆顶元素
+     * 最大堆的元素个数或者与最小堆的元素个数相等，或者多 1 相等就是偶数，多1就是奇数
+     */
     private PriorityQueue<Integer> maxheap;
     private PriorityQueue<Integer> minheap;
 
