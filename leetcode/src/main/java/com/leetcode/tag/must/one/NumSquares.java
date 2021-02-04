e815930e4da066cb17395c5255d2d168d6697a0b
xuweng <2028403305@qq.com>
2021-02-04 22:01:13 +0800

    方法二：动态规划
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/one/NumSquares.java
+++ leetcode/src/main/java/com/leetcode/tag/must/one/NumSquares.java
@@ -27,7 +27,7 @@ public class NumSquares {
         public int numSquares(int n) {
             int[] dp = new int[n + 1];
             Arrays.fill(dp, Integer.MAX_VALUE);
-
+            dp[0] = 0;
             for (int i = 1; i * i <= n; i++) {
                 for (int j = i * i; j <= n; j++) {
                     if (dp[j - i * i] == Integer.MAX_VALUE) {
