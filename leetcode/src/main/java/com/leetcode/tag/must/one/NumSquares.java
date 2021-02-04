1b95fea747e5dc1b9b5e900827bda6f00ef46a1c
xuweng <2028403305@qq.com>
2021-02-04 21:34:22 +0800

    方法二：动态规划
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/one/NumSquares.java
+++ leetcode/src/main/java/com/leetcode/tag/must/one/NumSquares.java
@@ -1,5 +1,7 @@
 package com.leetcode.tag.must.one;
 
+import java.util.Arrays;
+
 /**
  * 279. 完全平方数
  */
@@ -19,4 +21,39 @@ public class NumSquares {
             return dp[n];
         }
     }
+
+    /**
+     * 方法二：动态规划
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/perfect-squares/solution/wan-quan-ping-fang-shu-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int numSquares(int n) {
+            int[] dp = new int[n + 1];
+            Arrays.fill(dp, Integer.MAX_VALUE);
+            // bottom case
+            dp[0] = 0;
+
+            // pre-calculate the square numbers.
+            int maxSquareIndex = (int) Math.sqrt(n) + 1;
+            int squareNums[] = new int[maxSquareIndex];
+            for (int i = 1; i < maxSquareIndex; ++i) {
+                squareNums[i] = i * i;
+            }
+
+            for (int i = 1; i <= n; ++i) {
+                for (int s = 1; s < maxSquareIndex; ++s) {
+                    if (i < squareNums[s]) {
+                        break;
+                    }
+                    dp[i] = Math.min(dp[i], dp[i - squareNums[s]] + 1);
+                }
+            }
+            return dp[n];
+        }
+    }
+
 }
