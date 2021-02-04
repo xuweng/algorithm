8ffec1e27685f46fe60f709cd53bd430c40ff867
xuweng <2028403305@qq.com>
2021-02-04 22:26:43 +0800

    贪心算法
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/one/Jump.java
+++ leetcode/src/main/java/com/leetcode/tag/must/one/Jump.java
@@ -34,6 +34,19 @@ public class Jump {
      * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
      */
     class Solution1 {
+        /**
+         * 如果有多个位置通过跳跃都能够到达最后一个位置，那么我们应该如何进行选择呢？直观上来看，
+         * <p>
+         * 我们可以「贪心」地选择距离最后一个位置最远的那个位置，也就是对应下标最小的那个位置
+         * <p>
+         * 作者：LeetCode-Solution
+         * 链接：https://leetcode-cn.com/problems/jump-game-ii/solution/tiao-yue-you-xi-ii-by-leetcode-solution/
+         * 来源：力扣（LeetCode）
+         * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+         *
+         * @param nums
+         * @return
+         */
         public int jump(int[] nums) {
             int position = nums.length - 1;
             int steps = 0;
