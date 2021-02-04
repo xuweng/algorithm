fb09545f0fce17f9373329d3d0f0db7c2a7a6cce
xuweng <2028403305@qq.com>
2021-02-04 20:49:50 +0800

    643. 子数组最大平均数 I
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/seven/FindMaxAverage.java
@@ -0,0 +1,22 @@
+package com.leetcode.tag.daily.seven;
+
+/**
+ * 643. 子数组最大平均数 I
+ */
+public class FindMaxAverage {
+    class Solution {
+        public double findMaxAverage(int[] nums, int k) {
+            int sum = 0;
+            int result = Integer.MIN_VALUE;
+            for (int i = 0; i < nums.length; i++) {
+                sum += i >= k ? nums[i] - nums[i - k] : nums[i];
+
+                if (i >= k - 1) {
+                    result = Math.max(result, sum / k);
+                }
+            }
+
+            return result;
+        }
+    }
+}
