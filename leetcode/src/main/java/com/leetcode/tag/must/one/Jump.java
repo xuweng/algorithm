23e09763cc5011f905f3c5aa203ea4d40cd66cb8
xuweng <2028403305@qq.com>
2021-02-04 22:24:54 +0800

    贪心算法
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/one/Jump.java
+++ leetcode/src/main/java/com/leetcode/tag/must/one/Jump.java
@@ -22,4 +22,32 @@ public class Jump {
             return dp[nums.length - 1];
         }
     }
+
+    /**
+     * 贪心算法
+     * <p>
+     * 方法一：反向查找出发位置
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/jump-game-ii/solution/tiao-yue-you-xi-ii-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int jump(int[] nums) {
+            int position = nums.length - 1;
+            int steps = 0;
+            while (position > 0) {
+                for (int i = 0; i < position; i++) {
+                    if (i + nums[i] >= position) {
+                        position = i;
+                        steps++;
+                        break;
+                    }
+                }
+            }
+            return steps;
+        }
+    }
+
 }
