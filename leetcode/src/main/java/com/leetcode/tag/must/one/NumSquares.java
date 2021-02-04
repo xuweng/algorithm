50859116b608c137f56fc64026a5f0b4b40e2581
xuweng <2028403305@qq.com>
2021-02-04 21:28:07 +0800

    279. 完全平方数
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/one/NumSquares.java
+++ leetcode/src/main/java/com/leetcode/tag/must/one/NumSquares.java
@@ -7,12 +7,11 @@ public class NumSquares {
     class Solution {
         public int numSquares(int n) {
             int[] dp = new int[n + 1];
-            dp[0] = 1;
             for (int i = 1; i <= n; i++) {
-                dp[i] = 1;
+                dp[i] = i;
                 for (int j = 1; j < i; j++) {
                     if (j * j <= i) {
-                        dp[i] = Math.min(dp[i], dp[i - j * j]);
+                        dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
                     }
                 }
             }
