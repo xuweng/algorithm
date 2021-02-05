d533172db115a296ba48e376f62ac16945b9d11e
xuweng <2028403305@qq.com>
2021-02-05 22:28:03 +0800

    403. 青蛙过河
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/one/CanCross.java
+++ leetcode/src/main/java/com/leetcode/tag/must/one/CanCross.java
@@ -18,6 +18,7 @@ public class CanCross {
      */
     class Solution {
         public boolean canCross(int[] stones) {
+            // key是石头 value是跳到石头的步数
             HashMap<Integer, Set<Integer>> map = new HashMap<>();
             for (int stone : stones) {
                 map.put(stone, new HashSet<>());
@@ -27,6 +28,8 @@ public class CanCross {
                 for (int k : map.get(stone)) {
                     for (int step = k - 1; step <= k + 1; step++) {
                         if (step > 0 && map.containsKey(stone + step)) {
+                            // 存在stone + step这个石头
+                            // 跳到stone + step这个石头的步数是step
                             map.get(stone + step).add(step);
                         }
                     }
