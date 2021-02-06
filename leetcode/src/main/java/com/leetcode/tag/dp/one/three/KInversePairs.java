4e80b783f231a5d41b41b52a93dc80a19d13b495
xuweng <2028403305@qq.com>
2021-02-06 18:16:41 +0800

    用 f(i, j) 表示数字 [1 .. i] 的排列中恰好包含 j 个逆序对的个数
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/one/three/KInversePairs.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/one/three/KInversePairs.java
@@ -20,9 +20,11 @@ public class KInversePairs {
      */
     class Solution {
         public int kInversePairs(int n, int k) {
+            //用 f(i, j) 表示数字 [1 .. i] 的排列中恰好包含 j 个逆序对的个数
             int[][] dp = new int[n + 1][k + 1];
             int M = 1000000007;
             for (int i = 1; i <= n; i++) {
+                //因为 i 是 [1 .. i] 中的最大值，因此将它放置在 [1 .. i - 1] 的排列中的任意一个位置，它都会与在它之后的那些数形成逆序对
                 for (int j = 0; j <= k && j <= i * (i - 1) / 2; j++) {
                     if (i == 1 && j == 0) {
                         dp[i][j] = 1;
