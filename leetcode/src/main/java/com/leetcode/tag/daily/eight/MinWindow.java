85ed5be89213229fc2205e24767c43ba4b0bc5eb
xuweng <2028403305@qq.com>
2021-02-18 17:12:08 +0800

    容易理解 框架
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/eight/MinWindow.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/eight/MinWindow.java
@@ -111,34 +111,31 @@ public class MinWindow {
             int left = 0, right = 0;
             int valid = 0;
             // 记录最小覆盖字串的起始索引及长度
-            int start = 0, len = Integer.MAX_VALUE;
+            int start = 0;
+            // 记录最小覆盖字串长度
+            int len = Integer.MAX_VALUE;
             while (right < s.length()) {
                 char c = s.charAt(right);
                 right++;
-                // 判断取出的字符是否在字串中
-                if (need.containsKey(c)) {
-                    window.put(c, window.getOrDefault(c, 0) + 1);
-                    if (window.get(c).equals(need.get(c))) {
-                        valid++;
-                    }
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
                 }
 
                 // 判断是否需要收缩（已经找到合适的覆盖串）
                 while (valid == need.size()) {
                     if (right - left < len) {
+                        // 更新最下长度
                         start = left;
                         len = right - left;
                     }
 
                     char c1 = s.charAt(left);
                     left++;
-                    if (need.containsKey(c1)) {
-                        if (window.get(c1).equals(need.get(c1))) {
-                            valid--;
-                        }
-                        window.put(c1, window.getOrDefault(c1, 0) - 1);
+                    if (window.get(c1).equals(need.get(c1))) {
+                        valid--;
                     }
-
+                    window.put(c1, window.getOrDefault(c1, 0) - 1);
                 }
             }
 
