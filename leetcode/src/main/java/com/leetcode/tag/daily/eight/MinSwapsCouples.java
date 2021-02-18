3159aeddee7a456a4f3beb3083287643d2dea5db
xuweng <2028403305@qq.com>
2021-02-18 23:02:03 +0800

    方法二：广度优先搜索
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/eight/MinSwapsCouples.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/eight/MinSwapsCouples.java
@@ -1,7 +1,6 @@
 package com.leetcode.tag.daily.eight;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 765. 情侣牵手
@@ -57,6 +56,59 @@ public class MinSwapsCouples {
             int fx = getf(f, x);
             int fy = getf(f, y);
             f[fx] = fy;
+        }
+    }
+
+    /**
+     * 方法二：广度优先搜索
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/couples-holding-hands/solution/qing-lu-qian-shou-by-leetcode-solution-bvzr/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int minSwapsCouples(int[] row) {
+            int n = row.length;
+            int tot = n / 2;
+
+            List<Integer>[] graph = new List[tot];
+            for (int i = 0; i < tot; i++) {
+                graph[i] = new ArrayList<>();
+            }
+            for (int i = 0; i < n; i += 2) {
+                int l = row[i] / 2;
+                int r = row[i + 1] / 2;
+                if (l != r) {
+                    graph[l].add(r);
+                    graph[r].add(l);
+                }
+            }
+            boolean[] visited = new boolean[tot];
+            int ret = 0;
+            for (int i = 0; i < tot; i++) {
+                if (visited[i]) {
+                    continue;
+                }
+                Queue<Integer> queue = new LinkedList<>();
+                visited[i] = true;
+                queue.offer(i);
+                int cnt = 0;
+
+                while (!queue.isEmpty()) {
+                    int x = queue.poll();
+                    cnt += 1;
+
+                    for (int y : graph[x]) {
+                        if (!visited[y]) {
+                            visited[y] = true;
+                            queue.offer(y);
+                        }
+                    }
+                }
+                ret += cnt - 1;
+            }
+            return ret;
         }
     }
 
