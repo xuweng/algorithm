43749d8b45a112544741575ec1bfff704f9a3426
xuweng <2028403305@qq.com>
2021-02-18 22:48:23 +0800

    思路
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/eight/FindDisappearedNumbers.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/eight/FindDisappearedNumbers.java
@@ -18,6 +18,21 @@ public class FindDisappearedNumbers {
      * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
      */
     class Solution {
+        /**
+         * 思路
+         * <p>
+         * 遍历 nums，每遇到一个数 x，就让 nums[x−1] 增加 n。由于 nums 中所有数均在 [1,n] 中，增加以后，这些数必然大于 n。
+         * <p>
+         * 最后我们遍历 nums，若 nums[i] 未大于 n，就说明没有遇到过数 i+1。这样我们就找到了缺失的数字
+         * <p>
+         * 作者：LeetCode-Solution
+         * 链接：https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array/solution/zhao-dao-suo-you-shu-zu-zhong-xiao-shi-d-mabl/
+         * 来源：力扣（LeetCode）
+         * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+         *
+         * @param nums
+         * @return
+         */
         public List<Integer> findDisappearedNumbers(int[] nums) {
             int n = nums.length;
             for (int num : nums) {
