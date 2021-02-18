745e915d1c8252e0a342fd29d5127787877e9834
xuweng <2028403305@qq.com>
2021-02-18 22:38:04 +0800

    方法二：线性递推
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/eight/GetRow.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/eight/GetRow.java
@@ -93,4 +93,23 @@ public class GetRow {
         }
     }
 
+    /**
+     * 方法二：线性递推
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/pascals-triangle-ii/solution/yang-hui-san-jiao-ii-by-leetcode-solutio-shuk/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution3 {
+        public List<Integer> getRow(int rowIndex) {
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+            for (int i = 1; i <= rowIndex; ++i) {
+                row.add((int) ((long) row.get(i - 1) * (rowIndex - i + 1) / i));
+            }
+            return row;
+        }
+    }
+
 }
