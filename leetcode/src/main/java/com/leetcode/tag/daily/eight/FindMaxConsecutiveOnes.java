f849ebdcd20a0adaa5eb98edc2fbaaeffdbdb850
xuweng <2028403305@qq.com>
2021-02-18 23:19:23 +0800

    方法一：一次遍历
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/eight/FindMaxConsecutiveOnes.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/eight/FindMaxConsecutiveOnes.java
@@ -19,7 +19,6 @@ public class FindMaxConsecutiveOnes {
     class Solution {
         public int findMaxConsecutiveOnes(int[] nums) {
             int maxCount = 0, count = 0;
-            int n = nums.length;
             for (int num : nums) {
                 if (num == 1) {
                     count++;
