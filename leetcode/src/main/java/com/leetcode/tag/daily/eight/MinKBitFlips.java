876631e9dff0dedbe3d69131277d0c4829118943
xuweng <2028403305@qq.com>
2021-02-18 13:05:46 +0800

    而对于一个数若其经历了奇数次翻转，则其改变，若经历了偶数次翻转则其值未变
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/eight/MinKBitFlips.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/eight/MinKBitFlips.java
@@ -45,6 +45,24 @@ public class MinKBitFlips {
     /**
      * 方法一：差分数组
      * <p>
+     * 推理 推理
+     * <p>
+     * 而对于一个数若其经历了奇数次翻转，则其改变，若经历了偶数次翻转则其值未变，即
+     * <p>
+     * 0翻转偶数次后，数值仍然是0，需要被翻转
+     * 0翻转奇数次后，数值变为1，无需被翻转
+     * 1翻转偶数次后，数值任然是1，无需翻转
+     * 1翻转奇数次后，数值变为0，需要被翻转
+     * 则可使用(sumReA&1)^A[i]，判断是否需要翻转
+     * <p>
+     * 作者：yxiaojian
+     * 链接：https://leetcode-cn.com/problems/minimum-number-of-k-consecutive-bit-flips/solution/k-lian-xu-wei-de-zui-xiao-fan-zhuan-ci-s-dseq/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * <p>
+     * https://leetcode-cn.com/problems/minimum-number-of-k-consecutive-bit-flips/solution/k-lian-xu-wei-de-zui-xiao-fan-zhuan-ci-s-dseq/
+     * <p>
      * 每次都模拟了以i开始长度为K的翻转，但这样效率便受限于K值了，而很明显，一次翻转是对【i,i+K-1】区间的数都进行了改变，
      * <p>
      * 而对于区间操作，有一种常见的方法——差分（前缀和的逆运用，对区间两端进行操作以代替区间内操作）
