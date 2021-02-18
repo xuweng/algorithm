110edbe21d3b5f2ef7f23d4fe0d9f07e4bf7f8bd
xuweng <2028403305@qq.com>
2021-02-18 22:32:32 +0800

    滚动数组
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/eight/GetRow.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/eight/GetRow.java
@@ -39,4 +39,36 @@ public class GetRow {
         }
     }
 
+    /**
+     * 优化
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/pascals-triangle-ii/solution/yang-hui-san-jiao-ii-by-leetcode-solutio-shuk/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        /**
+         * 第 i+1 行的计算仅用到了第 i 行的数据，因此可以使用滚动数组的思想优化空间复杂度
+         *
+         * @param rowIndex
+         * @return
+         */
+        public List<Integer> getRow(int rowIndex) {
+            List<Integer> pre = new ArrayList<>();
+            for (int i = 0; i <= rowIndex; ++i) {
+                List<Integer> cur = new ArrayList<>();
+                for (int j = 0; j <= i; ++j) {
+                    if (j == 0 || j == i) {
+                        cur.add(1);
+                    } else {
+                        cur.add(pre.get(j - 1) + pre.get(j));
+                    }
+                }
+                pre = cur;
+            }
+            return pre;
+        }
+    }
+
 }
