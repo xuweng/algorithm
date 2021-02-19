5c7c6a3adf3b5a63cc385774eb9f7a23561ee42f
xuweng <2028403305@qq.com>
2021-02-19 20:54:25 +0800

    题意转换
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/eight/LongestOnes.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/eight/LongestOnes.java
@@ -75,4 +75,38 @@ public class LongestOnes {
         }
     }
 
+    /**
+     * 重点：题意转换。把「最多可以把 K 个 0 变成 1，求仅包含 1 的最长子数组的长度」转换为
+     * <p>
+     * 「找出一个最长的子数组，该子数组内最多允许有 K 个 0 」。
+     * <p>
+     * 作者：fuxuemingzhu
+     * 链接：https://leetcode-cn.com/problems/max-consecutive-ones-iii/solution/fen-xiang-hua-dong-chuang-kou-mo-ban-mia-f76z/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public int longestOnes(int[] A, int K) {
+            int N = A.length;
+            int res = 0;
+            int left = 0, right = 0;
+            int zeros = 0;
+            while (right < N) {
+                if (A[right] == 0) {
+                    zeros++;
+                }
+                // 扩容
+                right++;
+                while (zeros > K) {
+                    // 收缩
+                    if (A[left++] == 0) {
+                        zeros--;
+                    }
+                }
+                res = Math.max(res, right - left + 1);
+            }
+            return res;
+        }
+    }
+
 }
