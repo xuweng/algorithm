7b29124918a7fd2badf6e705a21a1f35d9fd5057
xuweng <2028403305@qq.com>
2021-02-19 20:48:14 +0800

    方法二：滑动窗口
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/eight/LongestOnes.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/eight/LongestOnes.java
@@ -44,4 +44,31 @@ public class LongestOnes {
         }
     }
 
+    /**
+     * 方法二：滑动窗口
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/max-consecutive-ones-iii/solution/zui-da-lian-xu-1de-ge-shu-iii-by-leetcod-hw12/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int longestOnes(int[] A, int K) {
+            int n = A.length;
+            int left = 0;
+            //用两个变量 lsum 和 rsum 记录 left 和 right 分别对应的前缀和即可
+            int lsum = 0, rsum = 0;
+            int ans = 0;
+            for (int right = 0; right < n; ++right) {
+                rsum += 1 - A[right];
+                while (lsum < rsum - K) {
+                    lsum += 1 - A[left];
+                    ++left;
+                }
+                ans = Math.max(ans, right - left + 1);
+            }
+            return ans;
+        }
+    }
+
 }
