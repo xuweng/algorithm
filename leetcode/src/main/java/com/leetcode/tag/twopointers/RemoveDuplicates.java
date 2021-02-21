21cb4152d944bc2c8572f878dc6a343ac79a3fe6
xuweng <2028403305@qq.com>
2021-02-21 22:12:18 +0800

    26. 删除排序数组中的重复项
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/twopointers/RemoveDuplicates.java
@@ -0,0 +1,22 @@
+package com.leetcode.tag.twopointers;
+
+/**
+ * 26. 删除排序数组中的重复项
+ */
+public class RemoveDuplicates {
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return 0;
+            }
+            int left = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[left] != nums[i]) {
+                    nums[++left] = nums[i];
+                }
+            }
+
+            return left;
+        }
+    }
+}
