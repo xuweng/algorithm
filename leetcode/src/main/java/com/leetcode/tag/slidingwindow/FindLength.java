d539625cae2ec2159ebfe56281c2556e1c717a51
xuweng <2028403305@qq.com>
2021-02-22 21:43:56 +0800

    方法二：滑动窗口
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/slidingwindow/FindLength.java
+++ leetcode/src/main/java/com/leetcode/tag/slidingwindow/FindLength.java
@@ -62,4 +62,43 @@ public class FindLength {
         }
     }
 
+    /**
+     * 方法二：滑动窗口
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/maximum-length-of-repeated-subarray/solution/zui-chang-zhong-fu-zi-shu-zu-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public int findLength(int[] A, int[] B) {
+            int n = A.length, m = B.length;
+            int ret = 0;
+            for (int i = 0; i < n; i++) {
+                int len = Math.min(m, n - i);
+                int maxlen = maxLength(A, B, i, 0, len);
+                ret = Math.max(ret, maxlen);
+            }
+            for (int i = 0; i < m; i++) {
+                int len = Math.min(n, m - i);
+                int maxlen = maxLength(A, B, 0, i, len);
+                ret = Math.max(ret, maxlen);
+            }
+            return ret;
+        }
+
+        public int maxLength(int[] A, int[] B, int addA, int addB, int len) {
+            int ret = 0, k = 0;
+            for (int i = 0; i < len; i++) {
+                if (A[addA + i] == B[addB + i]) {
+                    k++;
+                } else {
+                    k = 0;
+                }
+                ret = Math.max(ret, k);
+            }
+            return ret;
+        }
+    }
+
 }
