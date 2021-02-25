41425c201c875b5d6a915ae0e7cee9066340c4cd
xuweng <2028403305@qq.com>
2021-02-25 22:19:19 +0800

    32. 最长有效括号
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/one/LongestValidParentheses.java
+++ leetcode/src/main/java/com/leetcode/tag/must/one/LongestValidParentheses.java
@@ -13,7 +13,7 @@ public class LongestValidParentheses {
             for (int i = 1; i < s.length(); i++) {
                 if (s.charAt(i) == ')') {
                     if (s.charAt(i - 1) == '(') {
-                        dp[i] = (i >= 2) ? dp[i - 2] : 2;
+                        dp[i] = ((i >= 2) ? dp[i - 2] : 0) + 2;
                     } else if ((i - dp[i - 1] - 1) >= 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
                         dp[i] = ((i - dp[i - 1] - 2) >= 0 ? dp[i - dp[i - 1] - 2] : 0) + dp[i - 1] + 2;
                     }
