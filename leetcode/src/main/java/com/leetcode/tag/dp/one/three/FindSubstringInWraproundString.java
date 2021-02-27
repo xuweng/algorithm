d9cada6c1585f86586a741fd9a4d19db455b1a3c
xuweng <2028403305@qq.com>
2021-02-27 22:58:21 +0800

    467. 环绕字符串中唯一的子字符串
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dp/one/three/FindSubstringInWraproundString.java
@@ -0,0 +1,46 @@
+package com.leetcode.tag.dp.one.three;
+
+/**
+ * 467. 环绕字符串中唯一的子字符串
+ * <p>
+ * 滑动窗口适合在题目要求连续的情况下使用， 而前缀和也是如此
+ */
+public class FindSubstringInWraproundString {
+    /**
+     * 解法：动态规划
+     * <p>
+     * 作者：zyxwmj
+     * 链接：https://leetcode-cn.com/problems/unique-substrings-in-wraparound-string/solution/dong-tai-gui-hua-java-by-zyxwmj-dn12/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution {
+        public int findSubstringInWraproundString(String p) {
+            // 记录 p 中以每个字符结尾的最长连续子串的长度
+            int[] dp = new int[26];
+            char[] array = p.toCharArray();
+            // 记录当前连续子串的长度
+            int count = 0;
+            // 遍历 p 中的所有字符
+            for (int i = 0; i < array.length; i++) {
+                // 判断字符是否连续
+                if (i > 0 && (array[i] - array[i - 1] - 1) % 26 == 0) {
+                    // 连续则自加
+                    count++;
+                } else {
+                    // 不连续则刷新
+                    count = 1;
+                }
+                // 只存储最长的连续长度
+                dp[array[i] - 'a'] = Math.max(dp[array[i] - 'a'], count);
+            }
+            int result = 0;
+            // 统计所有以每个字符结尾的最长连续子串的长度，就是唯一相等子串的个数
+            for (int i : dp) {
+                result += i;
+            }
+            return result;
+        }
+    }
+
+}
