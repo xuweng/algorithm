78e52f34dd79f5aefc687092c05296c2376aa565
xuweng <2028403305@qq.com>
2021-03-02 14:38:26 +0800

    方法一：一维前缀和
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/eight/NumMatrix.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/eight/NumMatrix.java
@@ -23,4 +23,40 @@ public class NumMatrix {
 
         return sum;
     }
+
+    /**
+     * 方法一：一维前缀和
+     * <p>
+     * 前缀和统计一行
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/range-sum-query-2d-immutable/solution/er-wei-qu-yu-he-jian-suo-ju-zhen-bu-ke-b-2z5n/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class NumMatrix1 {
+        int[][] sums;
+
+        public NumMatrix1(int[][] matrix) {
+            int m = matrix.length;
+            if (m > 0) {
+                int n = matrix[0].length;
+                sums = new int[m][n + 1];
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < n; j++) {
+                        sums[i][j + 1] = sums[i][j] + matrix[i][j];
+                    }
+                }
+            }
+        }
+
+        public int sumRegion(int row1, int col1, int row2, int col2) {
+            int sum = 0;
+            for (int i = row1; i <= row2; i++) {
+                sum += sums[i][col2 + 1] - sums[i][col1];
+            }
+            return sum;
+        }
+    }
+
 }
