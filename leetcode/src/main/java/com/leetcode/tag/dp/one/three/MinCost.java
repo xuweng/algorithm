dacea33957bdef0141b12a54e881f3d1ed92de3d
xuweng <2028403305@qq.com>
2021-03-02 22:42:08 +0800

    dfs
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/one/three/MinCost.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/one/three/MinCost.java
@@ -80,4 +80,60 @@ public class MinCost {
         }
     }
 
+    class Solution1 {
+        /**
+         * 我们用 f[i][j] 表示在当前待切割的木棍的左端点为 cuts[i−1]，右端点为 cuts[j+1] 时，将木棍全部切开的最小总成本。
+         * <p>
+         * 如果第一刀的位置为 cuts[k]，其中 k∈[i,j]，那么我们会将待切割的木棍分成两部分，左侧部分的木棍为 cuts[i−1..k]，
+         * <p>
+         * 对应的可以继续切割的位置为 cuts[i..k−1]；右侧部分的木棍为 cuts[k..j+1]，对应的可以继续切割的位置为 cuts[k+1..j]
+         * <p>
+         * 即我们无论在哪里切第一刀，这一刀的成本都是木棍的长度 cuts[j+1]−cuts[i−1]。
+         * <p>
+         * 作者：LeetCode-Solution
+         * 链接：https://leetcode-cn.com/problems/minimum-cost-to-cut-a-stick/solution/qie-gun-zi-de-zui-xiao-cheng-ben-by-leetcode-solut/
+         * 来源：力扣（LeetCode）
+         * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+         *
+         * @param n
+         * @param cuts
+         * @return
+         */
+        public int minCost(int n, int[] cuts) {
+            int m = cuts.length;
+            Arrays.sort(cuts);
+            //在左侧添加 0，右侧添加 n
+            int[] newCuts = new int[m + 2];
+            newCuts[0] = 0;
+            for (int i = 1; i <= m; ++i) {
+                newCuts[i] = cuts[i - 1];
+            }
+            newCuts[m + 1] = n;
+
+            return dfs(newCuts, 0, newCuts.length - 1);
+        }
+
+        /**
+         * dfs
+         *
+         * @param cuts 新数组
+         * @param i    左端点
+         * @param j    右端点
+         * @return
+         */
+        private int dfs(int[] cuts, int i, int j) {
+            if (j - i == 1) {
+                return 0;
+            }
+            if (j - i == 2) {
+                return cuts[j] - cuts[i];
+            }
+            int min = Integer.MAX_VALUE;
+            for (int k = i + 1; k < j; k++) {
+                min = Math.min(min, Math.min(dfs(cuts, i, k), dfs(cuts, k, j)));
+            }
+            min += cuts[j] - cuts[i];
+            return min;
+        }
+    }
 }
