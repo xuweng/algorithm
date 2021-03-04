81032f961a2796acd3575fc9a009dc0de816127d
xuweng <2028403305@qq.com>
2021-03-04 22:32:50 +0800

    方法 2：线性解法
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/stack/NextGreaterElement1.java
+++ leetcode/src/main/java/com/leetcode/tag/stack/NextGreaterElement1.java
@@ -55,4 +55,57 @@ public class NextGreaterElement1 {
         }
     }
 
+    /**
+     * 方法 2：线性解法
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/next-greater-element-iii/solution/xia-yi-ge-geng-da-yuan-su-iii-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int nextGreaterElement(int n) {
+            char[] a = ("" + n).toCharArray();
+            int i = a.length - 2;
+            while (i >= 0 && a[i + 1] <= a[i]) {
+                i--;
+            }
+            if (i < 0) {
+                return -1;
+            }
+            int j = a.length - 1;
+            while (j >= 0 && a[j] <= a[i]) {
+                j--;
+            }
+            swap(a, i, j);
+            reverse(a, i + 1);
+            try {
+                return Integer.parseInt(new String(a));
+            } catch (Exception e) {
+                return -1;
+            }
+        }
+
+        /**
+         * 反转
+         *
+         * @param a
+         * @param start
+         */
+        private void reverse(char[] a, int start) {
+            int i = start, j = a.length - 1;
+            while (i < j) {
+                swap(a, i, j);
+                i++;
+                j--;
+            }
+        }
+
+        private void swap(char[] a, int i, int j) {
+            char temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+
 }
