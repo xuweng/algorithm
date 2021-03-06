96a0db302de85a49780a2a53f29c18445e95ad48
xuweng <2028403305@qq.com>
2021-03-06 23:41:04 +0800

    1262. 可被三整除的最大和
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/two/MaxSumDivThree.java
+++ leetcode/src/main/java/com/leetcode/tag/must/two/MaxSumDivThree.java
@@ -17,15 +17,15 @@ public class MaxSumDivThree {
                 if (nums[i - 1] % 3 == 0) {
                     dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][0] + nums[i - 1]);
                     dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][1] + nums[i - 1]);
-                    dp[i][2] = Math.max(dp[i - 1][2], dp[i - 2][2] + nums[i - 1]);
+                    dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][2] + nums[i - 1]);
                 } else if (nums[i - 1] % 3 == 1) {
                     dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2] + nums[i - 1]);
                     dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + nums[i - 1]);
-                    dp[i][2] = Math.max(dp[i - 1][2], dp[i - 2][1] + nums[i - 1]);
+                    dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] + nums[i - 1]);
                 } else if (nums[i - 1] % 3 == 2) {
                     dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + nums[i - 1]);
                     dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][2] + nums[i - 1]);
-                    dp[i][2] = Math.max(dp[i - 1][2], dp[i - 2][0] + nums[i - 1]);
+                    dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][0] + nums[i - 1]);
                 }
             }
 
