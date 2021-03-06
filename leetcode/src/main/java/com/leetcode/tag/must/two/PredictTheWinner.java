a27da8ead8195bc0e375de1721dd0d515e30cc03
xuweng <2028403305@qq.com>
2021-03-06 23:57:54 +0800

    486. 预测赢家
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/two/PredictTheWinner.java
@@ -0,0 +1,26 @@
+package com.leetcode.tag.must.two;
+
+/**
+ * 486. 预测赢家
+ */
+public class PredictTheWinner {
+    class Solution {
+        public boolean PredictTheWinner(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return false;
+            }
+            int[][] dp = new int[nums.length][nums.length];
+            for (int i = nums.length - 1; i >= 0; i--) {
+                for (int j = i; j < nums.length; j++) {
+                    if (i == j) {
+                        dp[i][j] = nums[i];
+                    } else {
+                        dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+                    }
+                }
+            }
+
+            return dp[0][nums.length - 1] >= 0;
+        }
+    }
+}
