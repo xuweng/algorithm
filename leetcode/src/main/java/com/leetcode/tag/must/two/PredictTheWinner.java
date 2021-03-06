1943d5ebfcd640de2a35fefb3fa1e57a7b435ce1
xuweng <2028403305@qq.com>
2021-03-07 00:00:24 +0800

    只依赖两个状态 二维 一维
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/two/PredictTheWinner.java
+++ leetcode/src/main/java/com/leetcode/tag/must/two/PredictTheWinner.java
@@ -23,4 +23,27 @@ public class PredictTheWinner {
             return dp[0][nums.length - 1] >= 0;
         }
     }
+
+    /**
+     * 只依赖两个状态 二维 一维
+     */
+    class Solution1 {
+        public boolean PredictTheWinner(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return false;
+            }
+            int[] dp = new int[nums.length];
+            for (int i = nums.length - 1; i >= 0; i--) {
+                for (int j = i; j < nums.length; j++) {
+                    if (i == j) {
+                        dp[i] = nums[i];
+                    } else {
+                        dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+                    }
+                }
+            }
+
+            return dp[nums.length - 1] >= 0;
+        }
+    }
 }
