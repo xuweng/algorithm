05faa73355409f31880cb4c119af5bedb58960c0
xuweng <2028403305@qq.com>
2021-03-07 10:56:07 +0800

    5698. 构成特定和需要添加的最少元素
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/two/MinElements.java
@@ -0,0 +1,41 @@
+package com.leetcode.tag.contest.two;
+
+import java.util.Arrays;
+
+/**
+ * 5698. 构成特定和需要添加的最少元素
+ */
+public class MinElements {
+    class Solution {
+        public int minElements(int[] nums, int limit, int goal) {
+            int sum = Arrays.stream(nums).sum();
+            int i = goal - sum;
+            if (i == 0) {
+                return 0;
+            } else if (i < 0) {
+                int sum1 = 0;
+                int count = 0;
+                for (int j = -limit; j < 0; j++) {
+                    count++;
+                    sum1 += j;
+                    if (sum1 == i) {
+                        return count;
+                    }
+                }
+            } else {
+                int sum1 = 0;
+                int count = 0;
+                for (int j = limit; j > 0; j--) {
+                    count++;
+                    sum1 += j;
+
+                    if (sum1 == i) {
+                        return count;
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
