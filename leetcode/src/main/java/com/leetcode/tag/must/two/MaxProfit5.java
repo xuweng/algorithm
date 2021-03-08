8c98d3cd3000c2611537af91e4202cc411785374
xuweng <2028403305@qq.com>
2021-03-08 21:31:07 +0800

    309. 最佳买卖股票时机含冷冻期
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/two/MaxProfit5.java
+++ leetcode/src/main/java/com/leetcode/tag/must/two/MaxProfit5.java
@@ -15,8 +15,11 @@ public class MaxProfit5 {
             dp[0][1][0] = -prices[0];
 
             for (int i = 1; i < prices.length; i++) {
+                // i是冷冻期
                 dp[i][0][1] = dp[i - 1][1][0] + prices[i];
+                // i不是冷冻期 有股票
                 dp[i][1][0] = Math.max(dp[i - 1][1][0], dp[i][0][0] - prices[i]);
+                // i不是冷冻期 没有股票
                 dp[i][0][0] = Math.max(dp[i - 1][0][0], dp[i - 1][0][1]);
             }
 
