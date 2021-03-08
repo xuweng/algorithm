bdba7ed78499665c066158af4c61035d760885c4
xuweng <2028403305@qq.com>
2021-03-08 20:23:46 +0800

    132. 分割回文串 II
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/two/MinCut.java
@@ -0,0 +1,37 @@
+package com.leetcode.tag.must.two;
+
+/**
+ * 132. 分割回文串 II
+ */
+public class MinCut {
+    class Solution {
+        public int minCut(String s) {
+            if (s == null || s.isEmpty()) {
+                return 0;
+            }
+            int[] dp = new int[s.length()];
+            for (int i = 0; i < s.length(); i++) {
+                if (is(s, 0, i)) {
+                    continue;
+                }
+                for (int j = 0; j < i; j++) {
+                    if (is(s, j, i)) {
+                        dp[i] = Math.min(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+
+            return dp[s.length() - 1];
+        }
+
+        private boolean is(String s, int i, int j) {
+            for (int k = i; k <= j; k++) {
+                if (s.charAt(i) != s.charAt(j)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
