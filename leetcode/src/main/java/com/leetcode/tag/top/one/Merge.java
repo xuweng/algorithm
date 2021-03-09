cf281a1c2745c7421165fbc0d77eebf6b91f90fb
xuweng <2028403305@qq.com>
2021-03-09 23:00:49 +0800

    56. 合并区间
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/top/one/Merge.java
@@ -0,0 +1,31 @@
+package com.leetcode.tag.top.one;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 56. 合并区间
+ */
+public class Merge {
+    class Solution {
+        public int[][] merge(int[][] intervals) {
+            if (intervals == null || intervals.length == 0) {
+                return intervals;
+            }
+            Arrays.sort(intervals, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]);
+            List<int[]> list = new ArrayList<>();
+            list.add(intervals[0]);
+            for (int i = 1; i < intervals.length; i++) {
+                int[] ints = list.get(list.size() - 1);
+                if (intervals[i][0] <= ints[1]) {
+                    ints[1] = intervals[i][1];
+                } else {
+                    list.add(intervals[i]);
+                }
+            }
+
+            return list.toArray(new int[0][]);
+        }
+    }
+}
