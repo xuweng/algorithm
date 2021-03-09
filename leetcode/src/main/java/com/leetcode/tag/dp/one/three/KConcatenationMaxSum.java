9ca99c3a5cabedbb0b74eed62b6f742a120a9544
xuweng <2028403305@qq.com>
2021-03-09 22:11:12 +0800

    1191. K 次串联后最大子数组之和
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dp/one/three/KConcatenationMaxSum.java
@@ -0,0 +1,32 @@
+package com.leetcode.tag.dp.one.three;
+
+import java.util.Arrays;
+
+/**
+ * 1191. K 次串联后最大子数组之和
+ */
+public class KConcatenationMaxSum {
+    class Solution {
+        public int kConcatenationMaxSum(int[] arr, int k) {
+            if (arr == null || arr.length == 0) {
+                return 0;
+            }
+            int result = 0;
+            int max = Math.max(arr[0], 0);
+            int index = 2 * arr.length;
+            for (int i = 1; i < index; i++) {
+                int val = arr[i % arr.length];
+
+                max = Math.max(max + val, val);
+                result = Math.max(result, max);
+            }
+            int sum = Arrays.stream(arr).sum();
+            while (k > 2 && sum > 0) {
+                result = (result + sum) % 1000000007;
+                k--;
+            }
+
+            return result;
+        }
+    }
+}
