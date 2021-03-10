c9a8b3a8a5e9d01f5d4ea4e3cc29357e4e6c9a84
xuweng <2028403305@qq.com>
2021-03-10 23:24:34 +0800

    图解相交链表
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/hot/GetIntersectionNode.java
+++ leetcode/src/main/java/com/leetcode/tag/hot/GetIntersectionNode.java
@@ -50,6 +50,28 @@ public class GetIntersectionNode {
         }
     }
 
+    /**
+     * 图解相交链表
+     * <p>
+     * 作者：reals
+     * 链接：https://leetcode-cn.com/problems/intersection-of-two-linked-lists/solution/tu-jie-xiang-jiao-lian-biao-by-user7208t/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            if (headA == null || headB == null) {
+                return null;
+            }
+            ListNode pA = headA, pB = headB;
+            while (pA != pB) {
+                pA = pA == null ? headB : pA.next;
+                pB = pB == null ? headA : pB.next;
+            }
+            return pA;
+        }
+    }
+
     class ListNode {
         int val;
         ListNode next;
