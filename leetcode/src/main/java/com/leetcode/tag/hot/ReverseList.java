2b5077eb46f2543ae7c064fd7a4bc79266676c60
xuweng <2028403305@qq.com>
2021-03-13 21:46:55 +0800

    方法一：迭代
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/hot/ReverseList.java
+++ leetcode/src/main/java/com/leetcode/tag/hot/ReverseList.java
@@ -68,4 +68,28 @@ public class ReverseList {
             this.next = next;
         }
     }
+
+    /**
+     * 方法一：迭代
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/reverse-linked-list/solution/fan-zhuan-lian-biao-by-leetcode-solution-d1k2/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public ListNode reverseList(ListNode head) {
+            ListNode prev = null;
+            ListNode curr = head;
+            while (curr != null) {
+                ListNode next = curr.next;
+                curr.next = prev;
+
+                prev = curr;
+                curr = next;
+            }
+            return prev;
+        }
+    }
+
 }
