bb3dc9467bbd8d45a33d3e7dbe6bffab82817882
xuweng <2028403305@qq.com>
2021-03-13 21:51:40 +0800

    方法一：迭代
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/hot/ReverseList.java
+++ leetcode/src/main/java/com/leetcode/tag/hot/ReverseList.java
@@ -72,6 +72,8 @@ public class ReverseList {
     /**
      * 方法一：迭代
      * <p>
+     * 三指针
+     * <p>
      * 作者：LeetCode-Solution
      * 链接：https://leetcode-cn.com/problems/reverse-linked-list/solution/fan-zhuan-lian-biao-by-leetcode-solution-d1k2/
      * 来源：力扣（LeetCode）
@@ -89,6 +91,24 @@ public class ReverseList {
                 curr = next;
             }
             return prev;
+        }
+    }
+
+    /**
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/reverse-linked-list/solution/fan-zhuan-lian-biao-by-leetcode-solution-d1k2/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution3 {
+        public ListNode reverseList(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode newHead = reverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+            return newHead;
         }
     }
 
