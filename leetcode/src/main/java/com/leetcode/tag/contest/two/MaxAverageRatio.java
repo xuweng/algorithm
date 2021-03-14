4ab9e97d84b20afef68d722aa291fc131bb6da6d
xuweng <2028403305@qq.com>
2021-03-14 11:22:02 +0800

    5704. 好子数组的最大分数
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/two/MaxAverageRatio.java
@@ -0,0 +1,12 @@
+package com.leetcode.tag.contest.two;
+
+/**
+ * 5703. 最大平均通过率
+ */
+public class MaxAverageRatio {
+    class Solution {
+        public double maxAverageRatio(int[][] classes, int extraStudents) {
+            return 0;
+        }
+    }
+}
---
+++ leetcode/src/main/java/com/leetcode/tag/contest/two/MaximumScore.java
@@ -0,0 +1,12 @@
+package com.leetcode.tag.contest.two;
+
+/**
+ * 5704. 好子数组的最大分数
+ */
+public class MaximumScore {
+    class Solution {
+        public int maximumScore(int[] nums, int k) {
+            return 0;
+        }
+    }
+}
