3d76c427fd3a6f82bf16aa829d66e874f9efa9d9
xuweng <2028403305@qq.com>
2021-03-14 12:01:19 +0800

    461. 汉明距离
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/hot/HammingDistance.java
+++ leetcode/src/main/java/com/leetcode/tag/hot/HammingDistance.java
@@ -25,4 +25,26 @@ public class HammingDistance {
         }
     }
 
+    /**
+     * 方法二：移位
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/hamming-distance/solution/yi-ming-ju-chi-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int hammingDistance(int x, int y) {
+            int xor = x ^ y;
+            int distance = 0;
+            while (xor != 0) {
+                if (xor % 2 == 1) {
+                    distance += 1;
+                }
+                xor = xor >> 1;
+            }
+            return distance;
+        }
+    }
+
 }
