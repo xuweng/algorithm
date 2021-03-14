0c97d44e9266b5be8412a41b26a32fdc8fa9420d
xuweng <2028403305@qq.com>
2021-03-14 12:05:52 +0800

    方法三：布赖恩·克尼根算法
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/hot/HammingDistance.java
+++ leetcode/src/main/java/com/leetcode/tag/hot/HammingDistance.java
@@ -63,4 +63,26 @@ public class HammingDistance {
         }
     }
 
+    /**
+     * 方法三：布赖恩·克尼根算法
+     * <p>
+     * 作者：LeetCode
+     * 链接：https://leetcode-cn.com/problems/hamming-distance/solution/yi-ming-ju-chi-by-leetcode/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution2 {
+        public int hammingDistance(int x, int y) {
+            int xor = x ^ y;
+            int distance = 0;
+            while (xor != 0) {
+                distance += 1;
+                // remove the rightmost bit of '1'
+                // 原数字 number 的最右边等于 1 的比特会被移除。
+                xor = xor & (xor - 1);
+            }
+            return distance;
+        }
+    }
+
 }
