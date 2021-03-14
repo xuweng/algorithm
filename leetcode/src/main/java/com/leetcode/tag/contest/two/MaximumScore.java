9f1a4618a25359c39406156b6508b8047cc0048a
xuweng <2028403305@qq.com>
2021-03-14 11:32:41 +0800

    5704. 好子数组的最大分数
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/contest/two/MaximumScore.java
+++ leetcode/src/main/java/com/leetcode/tag/contest/two/MaximumScore.java
@@ -1,12 +1,27 @@
 package com.leetcode.tag.contest.two;
 
+import java.util.PriorityQueue;
+
 /**
  * 5704. 好子数组的最大分数
  */
 public class MaximumScore {
     class Solution {
         public int maximumScore(int[] nums, int k) {
-            return 0;
+            if (nums == null || nums.length == 0) {
+                return 0;
+            }
+            int result = 0;
+            for (int i = 0; i < nums.length; i++) {
+                PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+                for (int j = i; j < nums.length; j++) {
+                    priorityQueue.offer(nums[j]);
+
+                    result = Math.max(result, priorityQueue.peek() * (j - i + 1));
+                }
+            }
+
+            return result;
         }
     }
 }
