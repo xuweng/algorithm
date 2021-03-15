3f0999a7bd3eb2ff60d3a46076fc158a5e15762d
xuweng <2028403305@qq.com>
2021-03-15 20:31:06 +0800

    clone
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/hot/FindUnsortedSubarray.java
+++ leetcode/src/main/java/com/leetcode/tag/hot/FindUnsortedSubarray.java
@@ -86,4 +86,22 @@ public class FindUnsortedSubarray {
             return min == Integer.MAX_VALUE ? 0 : max - min + 1;
         }
     }
+
+    class Solution2 {
+        public int findUnsortedSubarray(int[] nums) {
+            int[] snums = nums.clone();
+
+            Arrays.sort(snums);
+
+            int start = snums.length, end = 0;
+            for (int i = 0; i < snums.length; i++) {
+                // 比较 nums  和 snums 的元素来决定最左边和最右边不匹配的元素
+                if (snums[i] != nums[i]) {
+                    start = Math.min(start, i);
+                    end = Math.max(end, i);
+                }
+            }
+            return (end - start >= 0 ? end - start + 1 : 0);
+        }
+    }
 }
