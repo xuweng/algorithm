ffa2ae23afc0beb482d906fb3428054b9268cc81
xuweng <2028403305@qq.com>
2021-03-17 21:07:18 +0800

    7. 整数反转
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/some/Reverse.java
@@ -0,0 +1,22 @@
+package com.leetcode.tag.some;
+
+/**
+ * 7. 整数反转
+ */
+public class Reverse {
+    class Solution {
+        public int reverse(int x) {
+            String s = String.valueOf(x);
+            int length = s.length() - 1;
+            while (length >= 0 && s.charAt(length) == '0') {
+                length--;
+            }
+            StringBuilder temp = new StringBuilder();
+            for (int i = length; i >= 0; i--) {
+                temp.append(s.charAt(i));
+            }
+
+            return Integer.parseInt(temp.toString());
+        }
+    }
+}
