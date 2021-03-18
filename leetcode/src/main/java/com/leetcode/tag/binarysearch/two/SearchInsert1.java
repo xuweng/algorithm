7ec9226013d43ecd0cfba62f53d8f5c766c93521
xuweng <2028403305@qq.com>
2021-03-18 22:39:45 +0800

    35. 搜索插入位置
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/binarysearch/two/SearchInsert1.java
+++ leetcode/src/main/java/com/leetcode/tag/binarysearch/two/SearchInsert1.java
@@ -26,4 +26,27 @@ public class SearchInsert1 {
             return left;
         }
     }
+
+    class Solution1 {
+        public int searchInsert(int[] nums, int target) {
+            if (nums == null || nums.length == 0) {
+                return -1;
+            }
+            int left = 0;
+            int right = nums.length;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                }
+                if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+            return left;
+        }
+    }
 }
