19fc7c7f7126e3e72ac092752078130030ceb550
xuweng <2028403305@qq.com>
2021-03-18 22:13:17 +0800

    81. 搜索旋转排序数组 II
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/binarysearch/two/Search6.java
@@ -0,0 +1,39 @@
+package com.leetcode.tag.binarysearch.two;
+
+/**
+ * 81. 搜索旋转排序数组 II
+ */
+public class Search6 {
+    class Solution {
+        public boolean search(int[] nums, int target) {
+            if (nums == null || nums.length == 0) {
+                return false;
+            }
+            int left = 0;
+            int right = nums.length - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    return true;
+                }
+                if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                    right--;
+                } else if (nums[left] <= nums[mid]) {
+                    if (nums[left] <= target && target < nums[mid]) {
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1;
+                    }
+                } else {
+                    if (nums[mid] < target && target <= nums[right]) {
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
