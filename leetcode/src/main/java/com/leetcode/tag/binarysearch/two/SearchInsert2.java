2ab97852026f81388de2579faa03cd1b63458c20
xuweng <2028403305@qq.com>
2021-03-19 22:36:17 +0800

    区间 [left,mid)
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/binarysearch/two/SearchInsert2.java
+++ leetcode/src/main/java/com/leetcode/tag/binarysearch/two/SearchInsert2.java
@@ -28,4 +28,28 @@ public class SearchInsert2 {
             return left;
         }
     }
+
+    class Solution1 {
+        public int searchInsert(int[] nums, int target) {
+            if (nums == null || nums.length == 0) {
+                return -1;
+            }
+            int left = 0;
+            int right = nums.length;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                }
+                if (nums[mid] > target) {
+                    // 区间 [left,mid)
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            return left;
+        }
+    }
 }
