0e617b33b4dedeb295487b3b041efd1b012164aa
xuweng <2028403305@qq.com>
2021-03-19 22:30:28 +0800

    115. 不同的子序列
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dp/one/three/NumDistinct2.java
@@ -0,0 +1,25 @@
+package com.leetcode.tag.dp.one.three;
+
+/**
+ * 115. 不同的子序列115. 不同的子序列
+ */
+public class NumDistinct2 {
+    class Solution {
+        public int numDistinct(String s, String t) {
+            if (s == null || s.isEmpty() || t == null || t.isEmpty()) {
+                return 0;
+            }
+            int[] dp = new int[t.length() + 1];
+            dp[0] = 1;
+            for (int i = 1; i <= s.length(); i++) {
+                for (int j = t.length(); j >= 1; j--) {
+                    if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                        dp[j] += dp[j - 1];
+                    }
+                }
+            }
+
+            return dp[t.length()];
+        }
+    }
+}
