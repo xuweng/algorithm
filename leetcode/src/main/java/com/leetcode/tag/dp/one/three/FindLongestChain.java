041a1b566a0f0ff34e0c1f2869f1d752875a7215
xuweng <2028403305@qq.com>
2021-03-20 21:52:25 +0800

    646. 最长数对链
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dp/one/three/FindLongestChain.java
@@ -0,0 +1,33 @@
+package com.leetcode.tag.dp.one.three;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 646. 最长数对链
+ */
+public class FindLongestChain {
+    class Solution {
+        public int findLongestChain(int[][] pairs) {
+            if (pairs == null || pairs.length == 0) {
+                return 0;
+            }
+            Arrays.sort(pairs, Comparator.comparingInt(arr -> arr[0]));
+
+            int[] dp = new int[pairs.length];
+            int max = 1;
+            for (int i = 0; i < pairs.length; i++) {
+                dp[i] = 1;
+                for (int j = 0; j < i; j++) {
+                    if (pairs[j][1] < pairs[i][0]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                }
+
+                max = Math.max(max, dp[i]);
+            }
+
+            return max;
+        }
+    }
+}
