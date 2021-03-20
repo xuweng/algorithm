ea7207a6e144da6ca816ab0f36204f5b2237b7c8
xuweng <2028403305@qq.com>
2021-03-20 21:41:12 +0800

    334. 递增的三元子序列
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/one/three/IncreasingTriplet.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/one/three/IncreasingTriplet.java
@@ -25,4 +25,25 @@ public class IncreasingTriplet {
             return false;
         }
     }
+
+    class Solution1 {
+        public boolean increasingTriplet(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return false;
+            }
+            int min1 = Integer.MAX_VALUE;
+            int min2 = Integer.MAX_VALUE;
+            for (int num : nums) {
+                if (min1 > num) {
+                    min1 = num;
+                } else if (min2 > num) {
+                    min2 = num;
+                } else {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
 }
