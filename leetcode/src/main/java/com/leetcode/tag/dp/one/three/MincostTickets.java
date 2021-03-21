27caa0b59736941005bb6e0c4801dbd17040f58f
xuweng <2028403305@qq.com>
2021-03-21 22:24:23 +0800

    爬楼梯 跳石头
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/one/three/MincostTickets.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/one/three/MincostTickets.java
@@ -111,7 +111,7 @@ public class MincostTickets {
             int index = 0;
             for (int i = 1; i <= lastDay; i++) {
                 if (i == days[index]) {
-                    dp[i] = Math.min((i >= 1 ? dp[i - 1] : 0) + costs[0],
+                    dp[i] = Math.min(dp[i - 1] + costs[0],
                             Math.min((i >= 7 ? dp[i - 7] : 0) + costs[1],
                                     (i >= 30 ? dp[i - 30] : 0) + costs[2]));
                     index++;
