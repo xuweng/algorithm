6026a9947c219ea930f4e9827075b54e69b9dbcf
xuweng <2028403305@qq.com>
2021-03-21 11:55:18 +0800

    983. 最低票价
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dp/one/three/MincostTickets.java
@@ -0,0 +1,14 @@
+package com.leetcode.tag.dp.one.three;
+
+/**
+ * 983. 最低票价
+ * <p>
+ * 模型 模型 模型 模型
+ */
+public class MincostTickets {
+    class Solution {
+        public int mincostTickets(int[] days, int[] costs) {
+            return 0;
+        }
+    }
+}
