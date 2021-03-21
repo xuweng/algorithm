94416eca58e43ca01d36e32c0e13f509cce709fc
xuweng <2028403305@qq.com>
2021-03-21 22:06:10 +0800

    983. 最低票价
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/one/three/MincostTickets.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/one/three/MincostTickets.java
@@ -60,4 +60,28 @@ public class MincostTickets {
         }
     }
 
+    class Solution1 {
+        public int mincostTickets(int[] days, int[] costs) {
+            // 子问题：dp[i] 表示到第 i 天结束时的最低消费
+            // 转移方程：
+            // 如果第 i 天需要通行证，dp[i] = min(dp[i - 1] + cost[0], dp[i - 7] + cost[1], dp[i - 30] + cost[2])
+            // 如果第 i 天不需要通行证，dp[i] = dp[i - 1]
+
+            int lastDay = days[days.length - 1];
+            int[] dp = new int[lastDay + 1];
+
+            int index = 0;
+            for (int i = 1; i <= lastDay; i++) {
+                if (i == days[index]) {
+                    // 包含 1 7 30 3种选择
+                    dp[i] = Math.min(costs[0], Math.min((i >= 7 ? dp[i - 7] : 0) + costs[1], (i >= 30 ? dp[i - 30] : 0) + costs[2]));
+                    index++;
+                } else {
+                    // 不包含 不用买票
+                    dp[i] = dp[i - 1];
+                }
+            }
+            return dp[lastDay];
+        }
+    }
 }
