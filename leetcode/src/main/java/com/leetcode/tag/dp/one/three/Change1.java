4b24ea418f1d4b43b1e4418cc530b3e142b6feb2
xuweng <2028403305@qq.com>
2021-03-21 21:36:59 +0800

    518. 零钱兑换 II
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dp/one/three/Change1.java
@@ -0,0 +1,24 @@
+package com.leetcode.tag.dp.one.three;
+
+/**
+ * 518. 零钱兑换 II
+ */
+public class Change1 {
+    class Solution {
+        public int change(int amount, int[] coins) {
+            if (coins == null) {
+                return 0;
+            }
+            int[] dp = new int[amount + 1];
+            // 初始化
+            dp[0] = 1;
+            for (int coin : coins) {
+                for (int i = coin; i <= amount; i++) {
+                    dp[i] += dp[i - coin];
+                }
+            }
+
+            return dp[amount];
+        }
+    }
+}
