4de6772183930e93c034c94591d0073d3b9e1809
xuweng <2028403305@qq.com>
2021-03-21 09:54:27 +0800

    73. 矩阵置零
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/eight/SetZeroes.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/eight/SetZeroes.java
@@ -12,13 +12,20 @@ public class SetZeroes {
             int m = matrix.length;
             int n = matrix[0].length;
 
+            boolean[][] flag = new boolean[m][n];
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (matrix[i][j] == 0) {
+                    if (matrix[i][j] == 0 && !flag[i][j]) {
                         for (int k = 0; k < n; k++) {
+                            if (matrix[i][k] != 0) {
+                                flag[i][k] = true;
+                            }
                             matrix[i][k] = 0;
                         }
                         for (int k = 0; k < m; k++) {
+                            if (matrix[k][j] != 0) {
+                                flag[k][j] = true;
+                            }
                             matrix[k][j] = 0;
                         }
                     }
