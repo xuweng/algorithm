41e1df5d086fc15fd96bd6fbd477a0a60a786ec5
xuweng <2028403305@qq.com>
2021-03-22 20:55:25 +0800

    初始化 正序 倒序
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/dp/one/four/NumDistinct.java
+++ leetcode/src/main/java/com/leetcode/tag/dp/one/four/NumDistinct.java
@@ -28,7 +28,7 @@ public class NumDistinct {
             int[] dp = new int[t.length() + 1];
             dp[0] = 1;
             for (int i = 1; i <= s.length(); i++) {
-                for (int j = 1; j <= t.length(); j++) {
+                for (int j = t.length(); j >= 1; j--) {
                     if (s.charAt(i - 1) == t.charAt(j - 1)) {
                         dp[j] += dp[j - 1];
                     }
