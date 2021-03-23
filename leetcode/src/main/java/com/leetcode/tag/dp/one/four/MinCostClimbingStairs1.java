83f5b854b5a1ef15098e5ae0cbc087951e74f291
xuweng <2028403305@qq.com>
2021-03-23 21:50:59 +0800

    746. 使用最小花费爬楼梯
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/dp/one/four/MinCostClimbingStairs1.java
@@ -0,0 +1,23 @@
+package com.leetcode.tag.dp.one.four;
+
+/**
+ * 746. 使用最小花费爬楼梯
+ */
+public class MinCostClimbingStairs1 {
+    class Solution {
+        public int minCostClimbingStairs(int[] cost) {
+            if (cost == null || cost.length == 0) {
+                return 0;
+            }
+            int[] dp = new int[cost.length];
+            dp[0] = cost[0];
+            dp[1] = cost[1];
+
+            for (int i = 2; i < cost.length; i++) {
+                dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+            }
+
+            return Math.min(dp[dp.length - 1], dp[dp.length - 2]);
+        }
+    }
+}
