aa7218bcff1f974f4eed063f76115ba3fdd841a9
xuweng <2028403305@qq.com>
2021-03-27 21:40:01 +0800

    189. 旋转数组
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/nine/Rotate.java
@@ -0,0 +1,20 @@
+package com.leetcode.tag.daily.nine;
+
+/**
+ * 189. 旋转数组
+ */
+public class Rotate {
+    class Solution {
+        public void rotate(int[] nums, int k) {
+            if (nums == null) {
+                return;
+            }
+            int[] result = new int[nums.length];
+            for (int i = 0; i < nums.length; i++) {
+                result[(i + k) % nums.length] = nums[i];
+            }
+
+            nums = result;
+        }
+    }
+}
