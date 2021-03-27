ed9b3ce15a03c2006d0ff80bfc12e64aba60b972
xuweng <2028403305@qq.com>
2021-03-27 22:00:52 +0800

    只能判断p为空
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/nine/IsMatch.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/nine/IsMatch.java
@@ -4,12 +4,18 @@ package com.leetcode.tag.daily.nine;
  * 44. 通配符匹配
  */
 public class IsMatch {
+    /**
+     * aaaaaaaaaaaaaaa
+     * a*
+     */
     class Solution {
         public boolean isMatch(String s, String p) {
-            if (s.isEmpty()) {
-                return p.isEmpty();
+            // s为空,p不为空
+            // 只能判断p为空
+            if (p.isEmpty()) {
+                return s.isEmpty();
             }
-            boolean first = !p.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
+            boolean first = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
             boolean next = p.length() >= 2 && p.charAt(1) == '*';
             if (next) {
                 return isMatch(s, p.substring(2)) || (first && isMatch(s.substring(1), p));
