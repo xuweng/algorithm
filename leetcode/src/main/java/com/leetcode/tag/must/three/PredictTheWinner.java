43a9cba38a3f3cab613c86b562dba853c34922b2
xuweng <2028403305@qq.com>
2021-03-28 12:19:11 +0800

    正序遍历
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/three/PredictTheWinner.java
+++ leetcode/src/main/java/com/leetcode/tag/must/three/PredictTheWinner.java
@@ -22,4 +22,24 @@ public class PredictTheWinner {
             return dp[0][dp.length - 1] >= 0;
         }
     }
+
+    class Solution1 {
+        public boolean PredictTheWinner(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return false;
+            }
+            int[] dp = new int[nums.length];
+            for (int i = 0; i < nums.length; i++) {
+                dp[i] = nums[i];
+            }
+            for (int i = nums.length - 2; i >= 0; i--) {
+                // 正序遍历
+                for (int j = i + 1; j < nums.length; j++) {
+                    dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+                }
+            }
+
+            return dp[dp.length - 1] >= 0;
+        }
+    }
 }
