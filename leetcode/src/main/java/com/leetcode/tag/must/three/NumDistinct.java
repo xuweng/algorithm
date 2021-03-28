11552d7ebb0420ecce6813f1a7da530e6755352a
xuweng <2028403305@qq.com>
2021-03-28 12:11:13 +0800

    初始化 初始化溢出
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/three/NumDistinct.java
+++ leetcode/src/main/java/com/leetcode/tag/must/three/NumDistinct.java
@@ -25,4 +25,24 @@ public class NumDistinct {
             return dp[s.length()][t.length()];
         }
     }
+
+    class Solution1 {
+        public int numDistinct(String s, String t) {
+            if (s == null || t == null) {
+                return 0;
+            }
+            int[] dp = new int[t.length() + 1];
+            // 初始化 初始化溢出
+            dp[0] = 1;
+            for (int i = 1; i <= s.length(); i++) {
+                for (int j = 1; j <= t.length(); j++) {
+                    if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                        dp[j] += dp[j - 1];
+                    }
+                }
+            }
+
+            return dp[t.length()];
+        }
+    }
 }
