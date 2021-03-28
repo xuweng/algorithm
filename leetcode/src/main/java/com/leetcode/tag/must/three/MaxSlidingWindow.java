f00864b3f9415b2bfd8cf1adbecd5c1af65e91a6
xuweng <2028403305@qq.com>
2021-03-28 21:41:49 +0800

    239. 滑动窗口最大值
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/three/MaxSlidingWindow.java
+++ leetcode/src/main/java/com/leetcode/tag/must/three/MaxSlidingWindow.java
@@ -20,6 +20,7 @@ public class MaxSlidingWindow {
                 while (i - deque.peekFirst() >= k) {
                     deque.pollFirst();
                 }
+                // 够一个窗口
                 if (i >= k - 1) {
                     result[i - k + 1] = nums[deque.peekFirst()];
                 }
