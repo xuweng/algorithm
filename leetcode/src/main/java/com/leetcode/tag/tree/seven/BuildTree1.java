bf6690ef39244870029c4518328aef3603c359ed
xuweng <2028403305@qq.com>
2021-03-28 22:27:18 +0800

    105. 从前序与中序遍历序列构造二叉树
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/seven/BuildTree1.java
@@ -0,0 +1,53 @@
+package com.leetcode.tag.tree.seven;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 105. 从前序与中序遍历序列构造二叉树
+ */
+public class BuildTree1 {
+    class Solution {
+        int preRootIndex;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        public TreeNode buildTree(int[] preorder, int[] inorder) {
+            for (int i = 0; i < inorder.length; i++) {
+                map.put(inorder[i], i);
+            }
+
+            return dfs(preorder, inorder, 0, preorder.length - 1);
+        }
+
+        private TreeNode dfs(int[] preorder, int[] inorder, int low, int high) {
+            if (low > high) {
+                return null;
+            }
+            TreeNode root = new TreeNode(preorder[preRootIndex]);
+            Integer inRoot = map.get(inorder[preRootIndex++]);
+            root.left = dfs(preorder, inorder, low, inRoot - 1);
+            root.right = dfs(preorder, inorder, inRoot + 1, high);
+
+            return root;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
