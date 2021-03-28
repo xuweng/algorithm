5433d274bf93b7c8746fb0f5f969493ad1b2620f
xuweng <2028403305@qq.com>
2021-03-28 09:33:30 +0800

    173. 二叉搜索树迭代器
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/daily/nine/BSTIterator.java
@@ -0,0 +1,54 @@
+package com.leetcode.tag.daily.nine;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * 173. 二叉搜索树迭代器
+ */
+public class BSTIterator {
+    List<Integer> list;
+    Iterator<Integer> iterator;
+
+    public BSTIterator(TreeNode root) {
+        zhong(root);
+        iterator = list.iterator();
+    }
+
+    public int next() {
+        return iterator.next();
+    }
+
+    public boolean hasNext() {
+        return iterator.hasNext();
+    }
+
+    private void zhong(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        zhong(root.left);
+        list.add(root.val);
+        zhong(root.right);
+    }
+
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
