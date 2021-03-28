9105e91cda6689e184f7343cfdcc10c8d11a24a9
xuweng <2028403305@qq.com>
2021-03-28 21:55:06 +0800

    剑指 Offer 55 - I. 二叉树的深度
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/tree/seven/MaxDepth.java
@@ -0,0 +1,27 @@
+package com.leetcode.tag.tree.seven;
+
+/**
+ * 剑指 Offer 55 - I. 二叉树的深度
+ */
+public class MaxDepth {
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            if (root == null) {
+                return 1;
+            }
+
+            return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
