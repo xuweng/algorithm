ea286b363656f74d4fe8548783ebd8dc43d8b643
xuweng <2028403305@qq.com>
2021-03-29 21:56:08 +0800

    698. 划分为k个相等的子集
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/four/CanPartitionKSubsets.java
@@ -0,0 +1,48 @@
+package com.leetcode.tag.must.four;
+
+import java.util.Arrays;
+
+/**
+ * 698. 划分为k个相等的子集
+ */
+public class CanPartitionKSubsets {
+    class Solution {
+        public boolean canPartitionKSubsets(int[] nums, int k) {
+            if (nums == null || nums.length == 0) {
+                return false;
+            }
+            int sum = Arrays.stream(nums).sum();
+            if (sum % k != 0) {
+                return false;
+            }
+            int target = sum / k;
+            boolean[] used = new boolean[nums.length];
+
+            return dfs(nums, k, 0, target, 0, used);
+        }
+
+        private boolean dfs(int[] nums, int k, int s, int target, int index, boolean[] used) {
+            if (k == 0) {
+                return true;
+            }
+            if (s == target) {
+                return dfs(nums, k - 1, 0, target, 0, used);
+            }
+            for (int i = index; i < nums.length; i++) {
+                if (used[i]) {
+                    continue;
+                }
+                if (s + nums[i] > target) {
+                    continue;
+                }
+                used[i] = true;
+                if (dfs(nums, k, s + nums[i], target, i + 1, used)) {
+                    return true;
+                }
+                used[i] = false;
+            }
+
+            return false;
+        }
+    }
+}
