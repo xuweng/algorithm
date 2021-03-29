5a27741529c0ab68692b4b925548238a0e3764f4
xuweng <2028403305@qq.com>
2021-03-29 21:28:08 +0800

    154. 寻找旋转排序数组中的最小值 II
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/four/FindMin1.java
+++ leetcode/src/main/java/com/leetcode/tag/must/four/FindMin1.java
@@ -29,4 +29,29 @@ public class FindMin1 {
             return nums[left];
         }
     }
+
+    /**
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii/solution/xun-zhao-xuan-zhuan-pai-xu-shu-zu-zhong-de-zui--16/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public int findMin(int[] nums) {
+            int low = 0;
+            int high = nums.length - 1;
+            while (low < high) {
+                int pivot = low + (high - low) / 2;
+                if (nums[pivot] < nums[high]) {
+                    high = pivot;
+                } else if (nums[pivot] > nums[high]) {
+                    low = pivot + 1;
+                } else {
+                    high -= 1;
+                }
+            }
+            return nums[low];
+        }
+    }
+
 }
