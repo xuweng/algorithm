8e51052d90865947da53e4e6a1c7c357e7491664
xuweng <2028403305@qq.com>
2021-04-03 16:31:50 +0800

    239. 滑动窗口最大值
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/seven/MaxSlidingWindow.java
@@ -0,0 +1,33 @@
+package com.leetcode.tag.must.seven;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 239. 滑动窗口最大值
+ */
+public class MaxSlidingWindow {
+    class Solution {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            if (nums == null) {
+                return nums;
+            }
+            int[] result = new int[nums.length - k + 1];
+            Deque<Integer> deque = new LinkedList<>();
+            for (int i = 0; i < nums.length; i++) {
+                while (!deque.isEmpty() && nums[deque.peekLast()] <= nums[i]) {
+                    deque.pollLast();
+                }
+                deque.offerLast(i);
+                while (i - deque.peekFirst() >= k) {
+                    deque.pollFirst();
+                }
+                if (i >= k - 1) {
+                    result[i - k + 1] = nums[deque.peekFirst()];
+                }
+            }
+
+            return result;
+        }
+    }
+}
