92f1f0ed5aca0ca80397cb34f5bb247d7ed6701d
xuweng <2028403305@qq.com>
2021-04-03 23:01:27 +0800

    int right = nums.length 和 int right = nums.length-1的区别;
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/seven/SearchRange.java
+++ leetcode/src/main/java/com/leetcode/tag/must/seven/SearchRange.java
@@ -284,14 +284,10 @@ public class SearchRange {
          * @return
          */
         private int findLastPosition(int[] nums, int target) {
-            if (nums.length == 1) {
-                // 防止进入循环
-                return 0;
-            }
             int left = 0;
-            int right = nums.length;
+            int right = nums.length - 1;
             while (left < right) {
-                int mid = left + (right - left) / 2;
+                int mid = left + (right + 1 - left) / 2;
                 if (nums[mid] == target) {
                     left = mid;
                 } else if (nums[mid] < target) {
