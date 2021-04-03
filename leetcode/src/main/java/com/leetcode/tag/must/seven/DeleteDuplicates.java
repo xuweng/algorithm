02ac901bc16073574e5a026746ba7dc74f932a8e
xuweng <2028403305@qq.com>
2021-04-03 16:16:05 +0800

    83. 删除排序链表中的重复元素
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/seven/DeleteDuplicates.java
@@ -0,0 +1,43 @@
+package com.leetcode.tag.must.seven;
+
+/**
+ * 83. 删除排序链表中的重复元素
+ */
+public class DeleteDuplicates {
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            if (head == null) {
+                return null;
+            }
+            ListNode cur = head;
+            while (cur.next != null) {
+                if (cur.val == cur.next.val) {
+                    ListNode next = cur.next;
+                    cur.next = next.next;
+                    next = null;
+                } else {
+                    cur = cur.next;
+                }
+            }
+
+            return cur;
+        }
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
