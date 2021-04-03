435434a20f70f9db2c7061c434d75420991cc806
xuweng <2028403305@qq.com>
2021-04-03 17:02:56 +0800

    718. 最长重复子数组
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/seven/FindLength.java
+++ leetcode/src/main/java/com/leetcode/tag/must/seven/FindLength.java
@@ -10,15 +10,18 @@ public class FindLength {
                 return 0;
             }
             int[][] dp = new int[A.length + 1][B.length + 1];
+            int max = 0;
             for (int i = 1; i <= A.length; i++) {
                 for (int j = 1; j <= B.length; j++) {
                     if (A[i - 1] == B[j - 1]) {
                         dp[i][j] = dp[i - 1][j - 1] + 1;
+
+                        max = Math.max(max, dp[i][j]);
                     }
                 }
             }
 
-            return dp[A.length][B.length];
+            return max;
         }
     }
 }
