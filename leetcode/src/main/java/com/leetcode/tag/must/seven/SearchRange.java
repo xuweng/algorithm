3196b703859de44db91de6fa42d3f42dcf0b91df
xuweng <2028403305@qq.com>
2021-04-03 21:38:21 +0800

    两次二分
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/seven/SearchRange.java
+++ leetcode/src/main/java/com/leetcode/tag/must/seven/SearchRange.java
@@ -97,4 +97,60 @@ public class SearchRange {
             return left;
         }
     }
+
+    class Solution2 {
+        public int[] searchRange(int[] nums, int target) {
+            int len = nums.length;
+            if (len == 0) {
+                return new int[]{-1, -1};
+            }
+
+            int firstPosition = findFirstPosition(nums, target);
+            if (firstPosition == -1) {
+                return new int[]{-1, -1};
+            }
+
+            int lastPosition = findLastPosition(nums, target, firstPosition);
+            return new int[]{firstPosition, lastPosition};
+        }
+
+        private int findFirstPosition(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    right = mid;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+            if (nums[left] == target) {
+                return left;
+            }
+            return -1;
+        }
+
+        private int findLastPosition(int[] nums, int target, int firstPosition) {
+            int left = firstPosition + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    left = mid;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            if (nums[left] == target) {
+                return left;
+            }
+            return firstPosition;
+        }
+    }
 }
