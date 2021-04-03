bc6528e61ff7d50458e7005e68e83d5d71e9883c
xuweng <2028403305@qq.com>
2021-04-03 22:43:51 +0800

    只有两个数时,选择mid=0或者mid=1
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/seven/SearchRange.java
+++ leetcode/src/main/java/com/leetcode/tag/must/seven/SearchRange.java
@@ -275,9 +275,9 @@ public class SearchRange {
          */
         private int findLastPosition(int[] nums, int target) {
             int left = 0;
-            int right = nums.length;
+            int right = nums.length - 1;
             while (left < right) {
-                int mid = left + (right - left) / 2;
+                int mid = left + (right + 1 - left) / 2;
                 if (nums[mid] == target) {
                     left = mid;
                 } else if (nums[mid] < target) {
