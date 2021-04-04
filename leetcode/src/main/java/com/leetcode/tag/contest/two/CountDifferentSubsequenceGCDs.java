0cac8d2ba06f8fd88239ee8a2e7810f6641406aa
xuweng <2028403305@qq.com>
2021-04-04 11:38:14 +0800

    5725. 序列中不同最大公约数的数目
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/two/CountDifferentSubsequenceGCDs.java
@@ -0,0 +1,12 @@
+package com.leetcode.tag.contest.two;
+
+/**
+ * 5725. 序列中不同最大公约数的数目
+ */
+public class CountDifferentSubsequenceGCDs {
+    class Solution {
+        public int countDifferentSubsequenceGCDs(int[] nums) {
+            return 0;
+        }
+    }
+}
