ed28779756b9bc8e3e92bb0018ef15a93602d1a4
xuweng <2028403305@qq.com>
2021-04-04 22:51:24 +0800

    35. 搜索插入位置
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/eight/SearchInsert.java
@@ -0,0 +1,29 @@
+package com.leetcode.tag.must.eight;
+
+/**
+ * 35. 搜索插入位置
+ */
+public class SearchInsert {
+    class Solution {
+        public int searchInsert(int[] nums, int target) {
+            if (nums == null) {
+                return -1;
+            }
+            int left = 0;
+            int right = nums.length - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                }
+                if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+            return left;
+        }
+    }
+}
