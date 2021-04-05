4cc0436f347c6f0c1394d096d17e8de775613a42
xuweng <2028403305@qq.com>
2021-04-05 23:07:22 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/nine/SearchRange.java
+++ leetcode/src/main/java/com/leetcode/tag/must/nine/SearchRange.java
@@ -38,7 +38,7 @@ public class SearchRange {
             int right = nums.length - 1;
             while (left < right) {
                 int mid = left + (right - left) / 2;
-                if (nums[mid] <= target) {
+                if (nums[mid] >= target) {
                     right = mid;
                 } else {
                     left = mid + 1;
