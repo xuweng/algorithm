426367400e05d51a5ee925c6a2a5e712c632a5f1
xuweng <2028403305@qq.com>
2021-04-05 23:20:26 +0800

    方法二：双指针
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/daily/nine/Merge.java
+++ leetcode/src/main/java/com/leetcode/tag/daily/nine/Merge.java
@@ -22,4 +22,32 @@ public class Merge {
             Arrays.sort(nums1);
         }
     }
+
+    /**
+     * 方法二：双指针
+     * <p>
+     * 已经被排序的性质
+     */
+    class Solution1 {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int p1 = 0, p2 = 0;
+            int[] sorted = new int[m + n];
+            int cur;
+            while (p1 < m || p2 < n) {
+                if (p1 == m) {
+                    cur = nums2[p2++];
+                } else if (p2 == n) {
+                    cur = nums1[p1++];
+                } else if (nums1[p1] < nums2[p2]) {
+                    cur = nums1[p1++];
+                } else {
+                    cur = nums2[p2++];
+                }
+                sorted[p1 + p2 - 1] = cur;
+            }
+            for (int i = 0; i != m + n; ++i) {
+                nums1[i] = sorted[i];
+            }
+        }
+    }
 }
