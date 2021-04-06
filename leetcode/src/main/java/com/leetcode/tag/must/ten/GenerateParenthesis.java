8323d38f51dfd7fb6f22883869b71fd637a50eb1
xuweng <2028403305@qq.com>
2021-04-06 22:42:00 +0800

    22. 括号生成
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/ten/GenerateParenthesis.java
@@ -0,0 +1,31 @@
+package com.leetcode.tag.must.ten;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 22. 括号生成
+ */
+public class GenerateParenthesis {
+    class Solution {
+        List<String> result = new ArrayList<>();
+
+        public List<String> generateParenthesis(int n) {
+            dfs(n, n, "");
+
+            return result;
+        }
+
+        private void dfs(int left, int right, String temp) {
+            if (left < 0 || right < 0 || left > right) {
+                return;
+            }
+            if (left == 0 && right == 0) {
+                result.add(temp);
+                return;
+            }
+            dfs(left - 1, right, temp + "(");
+            dfs(left, right - 1, temp + ")");
+        }
+    }
+}
