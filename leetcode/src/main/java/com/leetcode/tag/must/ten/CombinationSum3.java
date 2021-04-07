649dacbf01c210c2d5a928ce7322e296d53d72d9
xuweng <2028403305@qq.com>
2021-04-07 21:49:14 +0800

    216. 组合总和 III
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/ten/CombinationSum3.java
@@ -0,0 +1,37 @@
+package com.leetcode.tag.must.ten;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 216. 组合总和 III
+ */
+public class CombinationSum3 {
+    class Solution {
+        List<List<Integer>> result = new ArrayList<>();
+        Deque<Integer> deque = new LinkedList<>();
+
+        public List<List<Integer>> combinationSum3(int k, int n) {
+            dfs(k, n, 1);
+
+            return result;
+        }
+
+        private void dfs(int k, int n, int index) {
+            if (deque.size() > k || n < 0) {
+                return;
+            }
+            if (deque.size() == k && n == 0) {
+                result.add(new ArrayList<>(deque));
+                return;
+            }
+            for (int i = index; i <= 9; i++) {
+                deque.push(i);
+                dfs(k, n - i, i + 1);
+                deque.pop();
+            }
+        }
+    }
+}
