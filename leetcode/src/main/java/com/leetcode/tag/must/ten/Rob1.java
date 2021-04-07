65d2545035e50fad6600b116d6219d1b949bc86b
xuweng <2028403305@qq.com>
2021-04-07 22:52:37 +0800

    337. 打家劫舍 III
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/ten/Rob1.java
@@ -0,0 +1,47 @@
+package com.leetcode.tag.must.ten;
+
+/**
+ * 337. 打家劫舍 III
+ */
+public class Rob1 {
+    class Solution {
+        public int rob(TreeNode root) {
+            int[] ints = myRob(root);
+
+            return Math.max(ints[0], ints[1]);
+        }
+
+        private int[] myRob(TreeNode root) {
+            if (root == null) {
+                return new int[]{};
+            }
+            int[] left = myRob(root.left);
+            int[] right = myRob(root.right);
+
+            int[] dp = new int[2];
+            dp[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+            dp[1] = left[0] + right[0];
+
+            return dp;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
