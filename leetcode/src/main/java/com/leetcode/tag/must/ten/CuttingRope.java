92c2fc2c6d6bd5872ab45fcde3549286f9563cea
xuweng <2028403305@qq.com>
2021-04-07 22:21:36 +0800

    动态规划：大数取余
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/ten/CuttingRope.java
+++ leetcode/src/main/java/com/leetcode/tag/must/ten/CuttingRope.java
@@ -1,5 +1,8 @@
 package com.leetcode.tag.must.ten;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+
 /**
  * 剑指 Offer 14- II. 剪绳子 II
  */
@@ -34,6 +37,23 @@ public class CuttingRope {
                 n -= 3;
             }
             return (int) (res * n % 1000000007);
+        }
+    }
+
+    /**
+     * 动态规划：大数取余
+     */
+    class Solution2 {
+        public int cuttingRope(int n) {
+            BigInteger[] dp = new BigInteger[n + 1];
+            Arrays.fill(dp, BigInteger.valueOf(1));
+            // dp[1] = BigInteger.valueOf(1);
+            for (int i = 3; i < n + 1; i++) {
+                for (int j = 1; j < i; j++) {
+                    dp[i] = dp[i].max(BigInteger.valueOf(j * (i - j))).max(dp[i - j].multiply(BigInteger.valueOf(j)));
+                }
+            }
+            return dp[n].mod(BigInteger.valueOf(1000000007)).intValue();
         }
     }
 }
