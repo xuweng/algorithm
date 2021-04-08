56a43d0f1938ee9bd52d0ecca498b7e611162513
xuweng <2028403305@qq.com>
2021-04-08 21:54:54 +0800

    重复选择 排列 正序 外循环
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/two/CombinationSum4.java
+++ leetcode/src/main/java/com/leetcode/tag/must/two/CombinationSum4.java
@@ -14,6 +14,8 @@ public class CombinationSum4 {
                 return 0;
             }
             int[] dp = new int[target + 1];
+            // 初始化
+            dp[0] = 1;
             for (int i = 1; i <= target; i++) {
                 for (int num : nums) {
                     if (i < num) {
