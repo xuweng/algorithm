65190bd54bad02d2b39611ca1177514852430eeb
xuweng <2028403305@qq.com>
2021-04-08 21:53:17 +0800

    377. 组合总和 Ⅳ
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/two/CombinationSum4.java
@@ -0,0 +1,27 @@
+package com.leetcode.tag.must.two;
+
+/**
+ * 377. 组合总和 Ⅳ
+ * <p>
+ * 重复选择 排列
+ */
+public class CombinationSum4 {
+    class Solution {
+        public int combinationSum4(int[] nums, int target) {
+            if (nums == null) {
+                return 0;
+            }
+            int[] dp = new int[target + 1];
+            for (int i = 1; i <= target; i++) {
+                for (int num : nums) {
+                    if (i < num) {
+                        continue;
+                    }
+                    dp[i] += dp[i - num];
+                }
+            }
+
+            return dp[target];
+        }
+    }
+}
