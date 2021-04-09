ade21c6b5835215980c740d0dd6346e7081c22e4
xuweng <2028403305@qq.com>
2021-04-09 20:53:55 +0800

    241. 为运算表达式设计优先级
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/three/DiffWaysToCompute.java
@@ -0,0 +1,42 @@
+package com.leetcode.tag.must.three;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 241. 为运算表达式设计优先级
+ */
+public class DiffWaysToCompute {
+    class Solution {
+        public List<Integer> diffWaysToCompute(String expression) {
+            List<Integer> result = new ArrayList<>();
+            if (expression == null || expression.isEmpty()) {
+                return result;
+            }
+            for (int i = 0; i < expression.length(); i++) {
+                if (Character.isDigit(expression.charAt(i))) {
+                    continue;
+                }
+                List<Integer> left = diffWaysToCompute(expression.substring(0, i));
+                List<Integer> right = diffWaysToCompute(expression.substring(i + 1));
+                for (Integer l : left) {
+                    for (Integer r : right) {
+                        if (expression.charAt(i) == '+') {
+                            result.add(l + r);
+                        } else if (expression.charAt(i) == '-') {
+                            result.add(l - r);
+                        } else if (expression.charAt(i) == '*') {
+                            result.add(l * r);
+                        }
+                    }
+                }
+            }
+
+            if (result.isEmpty()) {
+                result.add(Integer.valueOf(expression));
+            }
+
+            return result;
+        }
+    }
+}
