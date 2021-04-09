4e23aa26ea7d5ddc036754e4efbda1fb514dfe06
xuweng <2028403305@qq.com>
2021-04-09 21:57:52 +0800

    230. 二叉搜索树中第K小的元素
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/three/KthSmallest.java
@@ -0,0 +1,53 @@
+package com.leetcode.tag.must.three;
+
+/**
+ * 230. 二叉搜索树中第K小的元素
+ * <p>
+ * 排列去重 排列去重 排列去重
+ * <p>
+ * 正序 倒序 外循环
+ */
+public class KthSmallest {
+    class Solution {
+        int count;
+        int result;
+
+        public int kthSmallest(TreeNode root, int k) {
+            dfs(root, k);
+
+            return result;
+        }
+
+        private void dfs(TreeNode root, int k) {
+            if (root == null) {
+                return;
+            }
+            dfs(root.left, k);
+            if (count == k) {
+                result = root.val;
+                return;
+            }
+            count++;
+            dfs(root.right, k);
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
