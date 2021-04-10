7c713786016b1633b2ab12b6a662b24ce48c0ff3
xuweng <2028403305@qq.com>
2021-04-10 22:58:14 +0800

    v2,v3,v5三者可能相等 这3个只要=dp[i]都要移动
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/four/NthUglyNumber.java
+++ leetcode/src/main/java/com/leetcode/tag/must/four/NthUglyNumber.java
@@ -10,6 +10,7 @@ public class NthUglyNumber {
             dp[1] = 1;
             int p2 = 1, p3 = 1, p5 = 1;
             for (int i = 2; i <= n; i++) {
+                // v2,v3,v5三者可能相等 这3个只要=dp[i]都要移动
                 int v2 = dp[p2] * 2, v3 = dp[p3] * 3, v5 = dp[p5] * 5;
                 dp[i] = Math.min(v2, Math.min(v3, v5));
                 if (dp[i] == v2) {
