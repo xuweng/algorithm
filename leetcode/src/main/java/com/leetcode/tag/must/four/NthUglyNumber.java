c07e1732623659d03a7a5c9eeed20100637cc2e5
xuweng <2028403305@qq.com>
2021-04-10 22:35:51 +0800

    264. 丑数 II
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/four/NthUglyNumber.java
@@ -0,0 +1,27 @@
+package com.leetcode.tag.must.four;
+
+/**
+ * 264. 丑数 II
+ */
+public class NthUglyNumber {
+    class Solution {
+        public int nthUglyNumber(int n) {
+            int[] dp = new int[n + 1];
+            dp[1] = 1;
+            int p2 = 1, p3 = 1, p5 = 1;
+            for (int i = 2; i <= n; i++) {
+                int v2 = dp[p2] * 2, v3 = dp[p3] * 3, v5 = dp[p5] * 5;
+                dp[i] = Math.min(v2, Math.min(v3, v5));
+                if (dp[i] == v2) {
+                    p2++;
+                } else if (dp[i] == v3) {
+                    p3++;
+                } else {
+                    p5++;
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
