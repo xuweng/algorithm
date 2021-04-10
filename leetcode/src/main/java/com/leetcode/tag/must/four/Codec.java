2afdd05ca41e938507a784b01c4c8e726ef65fa6
xuweng <2028403305@qq.com>
2021-04-10 22:16:46 +0800

    297. 二叉树的序列化与反序列化
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/four/Codec.java
@@ -0,0 +1,48 @@
+package com.leetcode.tag.must.four;
+
+/**
+ * 297. 二叉树的序列化与反序列化
+ */
+public class Codec {
+    int index;
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return "#";
+        }
+        String left = serialize(root.left);
+        String right = serialize(root.right);
+
+        return root.val + "," + left + "," + right;
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] split = data.split(",");
+
+        return dfs(split);
+    }
+
+    private TreeNode dfs(String[] split) {
+        if ("#".equals(split[index])) {
+            index++;
+            return null;
+        }
+        TreeNode root = new TreeNode(Integer.parseInt(split[index++]));
+        root.left = dfs(split);
+        root.right = dfs(split);
+
+        return root;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
