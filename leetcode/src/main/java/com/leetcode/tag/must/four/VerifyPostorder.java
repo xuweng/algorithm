399f24f3d4f0adaf30b9b140d71a4304b76a9f6e
xuweng <2028403305@qq.com>
2021-04-10 20:58:32 +0800

    剑指 Offer 33. 二叉搜索树的后序遍历序列
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/four/VerifyPostorder.java
+++ leetcode/src/main/java/com/leetcode/tag/must/four/VerifyPostorder.java
@@ -32,4 +32,34 @@ public class VerifyPostorder {
             return dfs(postorder, i + 1, high - 1) && dfs(postorder, low, i);
         }
     }
+
+    /**
+     * 方法一：递归分治
+     * <p>
+     * 作者：jyd
+     * 链接：https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-hou-xu-bian-li-xu-lie-lcof/solution/mian-shi-ti-33-er-cha-sou-suo-shu-de-hou-xu-bian-6/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public boolean verifyPostorder(int[] postorder) {
+            return recur(postorder, 0, postorder.length - 1);
+        }
+
+        boolean recur(int[] postorder, int i, int j) {
+            if (i >= j) {
+                return true;
+            }
+            int p = i;
+            while (postorder[p] < postorder[j]) {
+                p++;
+            }
+            int m = p;
+            while (postorder[p] > postorder[j]) {
+                p++;
+            }
+            return p == j && recur(postorder, i, m - 1) && recur(postorder, m, j - 1);
+        }
+    }
+
 }
