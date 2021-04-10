ac074de84ea085437f404143470abc4a0e71859b
xuweng <2028403305@qq.com>
2021-04-10 20:38:49 +0800

    剑指 Offer 33. 二叉搜索树的后序遍历序列
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/four/VerifyPostorder.java
@@ -0,0 +1,31 @@
+package com.leetcode.tag.must.four;
+
+/**
+ * 剑指 Offer 33. 二叉搜索树的后序遍历序列
+ */
+public class VerifyPostorder {
+    class Solution {
+        public boolean verifyPostorder(int[] postorder) {
+            if (postorder == null || postorder.length == 0) {
+                return false;
+            }
+
+            return dfs(postorder, 0, postorder.length - 1);
+        }
+
+        private boolean dfs(int[] postorder, int low, int high) {
+            if (low >= high) {
+                return true;
+            }
+            int i = high;
+            while (i >= low && postorder[i] >= postorder[high]) {
+                i--;
+            }
+            if (i == high) {
+                return false;
+            }
+
+            return dfs(postorder, i + 1, high) && dfs(postorder, low, i);
+        }
+    }
+}
