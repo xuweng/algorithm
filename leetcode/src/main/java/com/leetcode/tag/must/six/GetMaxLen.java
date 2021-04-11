f35aef7486a607d06217c9ad160e1b4540a5148a
xuweng <2028403305@qq.com>
2021-04-11 21:30:51 +0800

    1567. 乘积为正数的最长子数组长度
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/six/GetMaxLen.java
@@ -0,0 +1,36 @@
+package com.leetcode.tag.must.six;
+
+/**
+ * 1567. 乘积为正数的最长子数组长度
+ */
+public class GetMaxLen {
+    class Solution {
+        public int getMaxLen(int[] nums) {
+            int length = nums.length;
+            // positive[i] 表示以下标 i 结尾的乘积为正数的最长子数组长度
+            int[] positive = new int[length];
+            // negative[i] 表示乘积为负数的最长子数组长度
+            int[] negative = new int[length];
+            if (nums[0] > 0) {
+                positive[0] = 1;
+            } else if (nums[0] < 0) {
+                negative[0] = 1;
+            }
+            int maxLength = positive[0];
+            for (int i = 1; i < length; i++) {
+                if (nums[i] > 0) {
+                    positive[i] = positive[i - 1] + 1;
+                    negative[i] = negative[i - 1] > 0 ? negative[i - 1] + 1 : 0;
+                } else if (nums[i] < 0) {
+                    positive[i] = negative[i - 1] > 0 ? negative[i - 1] + 1 : 0;
+                    negative[i] = positive[i - 1] + 1;
+                } else {
+                    positive[i] = 0;
+                    negative[i] = 0;
+                }
+                maxLength = Math.max(maxLength, positive[i]);
+            }
+            return maxLength;
+        }
+    }
+}
