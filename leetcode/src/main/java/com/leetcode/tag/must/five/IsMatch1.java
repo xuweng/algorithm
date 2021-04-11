3c3fc9f5c6565fd14d8e575eadd5a03240d9d6d0
xuweng <2028403305@qq.com>
2021-04-11 15:08:48 +0800

    10. 正则表达式匹配
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/five/IsMatch1.java
@@ -0,0 +1,36 @@
+package com.leetcode.tag.must.five;
+
+/**
+ * 10. 正则表达式匹配
+ * <p>
+ * 模型 模型 模型 模型
+ */
+public class IsMatch1 {
+    class Solution {
+        public boolean isMatch(String s, String p) {
+            if (s == null || p == null) {
+                return false;
+            }
+            boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+            // 空初始化
+            dp[0][0] = true;
+            for (int i = 2; i <= p.length(); i++) {
+                dp[0][i] = dp[0][i - 2];
+            }
+            for (int i = 1; i <= s.length(); i++) {
+                for (int j = 1; j <= p.length(); j++) {
+                    if (p.charAt(j - 1) == '*') {
+                        dp[i][j] = dp[i][j - 2];
+                        if (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') {
+                            dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
+                        }
+                    } else if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                }
+            }
+
+            return dp[s.length()][p.length()];
+        }
+    }
+}
