950fdd5373a69680edcc3e482a3ab255616e6a04
xuweng <2028403305@qq.com>
2021-04-11 21:05:36 +0800

    5727. 找出游戏的获胜者
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/six/FindTheWinner.java
@@ -0,0 +1,26 @@
+package com.leetcode.tag.must.six;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 5727. 找出游戏的获胜者
+ */
+public class FindTheWinner {
+    class Solution {
+        public int findTheWinner(int n, int k) {
+            List<Integer> list = new ArrayList<>();
+            for (int i = 1; i <= n; i++) {
+                list.add(i);
+            }
+            int start = 0;
+            while (list.size() > 1) {
+                int id = (start + k - 1) % n;
+                list.remove(id);
+                start = id;
+            }
+
+            return list.get(0);
+        }
+    }
+}
