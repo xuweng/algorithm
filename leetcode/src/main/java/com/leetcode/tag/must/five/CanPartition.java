ac7c802c53e0e269c1e8705195d30733e1b78137
xuweng <2028403305@qq.com>
2021-04-11 16:24:40 +0800

    416. 分割等和子集
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/five/CanPartition.java
@@ -0,0 +1,29 @@
+package com.leetcode.tag.must.five;
+
+import java.util.Arrays;
+
+/**
+ * 416. 分割等和子集
+ */
+public class CanPartition {
+    class Solution {
+        public boolean canPartition(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return false;
+            }
+            int sum = Arrays.stream(nums).sum();
+            if (sum % 2 != 0) {
+                return false;
+            }
+            int target = sum / 2;
+            boolean[] dp = new boolean[target + 1];
+            for (int num : nums) {
+                for (int i = target; i >= num; i--) {
+                    dp[i] = dp[i] || dp[i - num];
+                }
+            }
+
+            return dp[target];
+        }
+    }
+}
