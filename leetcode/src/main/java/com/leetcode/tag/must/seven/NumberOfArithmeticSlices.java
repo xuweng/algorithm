560959cdcf97b7e1464490376ccadef7db683174
xuweng <2028403305@qq.com>
2021-04-13 21:03:15 +0800

    413. 等差数列划分
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/seven/NumberOfArithmeticSlices.java
@@ -0,0 +1,24 @@
+package com.leetcode.tag.must.seven;
+
+/**
+ * 413. 等差数列划分
+ */
+public class NumberOfArithmeticSlices {
+    class Solution {
+        public int numberOfArithmeticSlices(int[] nums) {
+            if (nums == null) {
+                return 0;
+            }
+            int[] dp = new int[nums.length];
+            int sum = 0;
+            for (int i = 2; i < nums.length; i++) {
+                if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+                    dp[i] = dp[i - 1] + 1;
+                }
+                sum += dp[i];
+            }
+
+            return sum;
+        }
+    }
+}
