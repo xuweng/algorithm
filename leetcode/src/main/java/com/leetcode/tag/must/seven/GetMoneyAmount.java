61139966706ffba8bb63dadfa519b270497e471a
xuweng <2028403305@qq.com>
2021-04-13 21:20:33 +0800

    375. 猜数字大小 II
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/seven/GetMoneyAmount.java
+++ leetcode/src/main/java/com/leetcode/tag/must/seven/GetMoneyAmount.java
@@ -7,6 +7,7 @@ public class GetMoneyAmount {
     public class Solution {
         public int getMoneyAmount(int n) {
             // dp(i, j) 代表在 (i, j) 中最坏情况下最小开销的代价
+            // cost(i,j)=pivot+max(cost(i,pivot−1),cost(pivot+1,n))
             int[][] dp = new int[n + 1][n + 1];
             for (int len = 2; len <= n; len++) {
                 for (int start = 1; start <= n - len + 1; start++) {
