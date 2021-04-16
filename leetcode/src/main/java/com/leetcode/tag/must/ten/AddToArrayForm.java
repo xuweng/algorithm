cdff44f8cd0d390f3016f4b35db23ba582e949a4
xuweng <2028403305@qq.com>
2021-04-16 21:44:06 +0800

    989. 数组形式的整数加法
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must/ten/AddToArrayForm.java
@@ -0,0 +1,35 @@
+package com.leetcode.tag.must.ten;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 989. 数组形式的整数加法
+ */
+public class AddToArrayForm {
+    /**
+     * 方法一：逐位相加 末尾相加
+     */
+    class Solution {
+        public List<Integer> addToArrayForm(int[] A, int K) {
+            List<Integer> res = new ArrayList<>();
+            int n = A.length;
+            int jin = 0;
+            for (int i = n - 1; i >= 0; --i) {
+                //末尾相加
+                int sum = A[i] + K % 10 + jin;
+                K /= 10;
+                res.add(sum % 10);
+                jin = sum / 10;
+            }
+            // k有剩余
+            while (K > 0) {
+                res.add(K % 10);
+                K /= 10;
+            }
+            Collections.reverse(res);
+            return res;
+        }
+    }
+}
