da6bab381c42335910e9cd5472c00ef9e48b4e8a
xuweng <2028403305@qq.com>
2021-04-16 21:21:50 +0800

    43. 字符串相乘
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must/ten/Multiply.java
+++ leetcode/src/main/java/com/leetcode/tag/must/ten/Multiply.java
@@ -4,6 +4,9 @@ package com.leetcode.tag.must.ten;
  * 43. 字符串相乘
  */
 public class Multiply {
+    /**
+     * 方法二：做乘法
+     */
     class Solution {
         public String multiply(String num1, String num2) {
             if ("0".equals(num1) || "0".equals(num2)) {
@@ -28,6 +31,56 @@ public class Multiply {
                 ans.append(ansArr[index]);
                 index++;
             }
+            return ans.toString();
+        }
+    }
+
+    /**
+     * 方法一：做加法
+     */
+    class Solution1 {
+        public String multiply(String num1, String num2) {
+            if ("0".equals(num1) || "0".equals(num2)) {
+                return "0";
+            }
+            String ans = "0";
+            int m = num1.length(), n = num2.length();
+            for (int i = n - 1; i >= 0; i--) {
+                StringBuilder curr = new StringBuilder();
+                int add = 0;
+                for (int j = n - 1; j > i; j--) {
+                    curr.append(0);
+                }
+                int y = num2.charAt(i) - '0';
+                for (int j = m - 1; j >= 0; j--) {
+                    int x = num1.charAt(j) - '0';
+                    int product = x * y + add;
+                    // 取末尾
+                    curr.append(product % 10);
+                    // 进位
+                    add = product / 10;
+                }
+                if (add != 0) {
+                    curr.append(add % 10);
+                }
+                ans = addStrings(ans, curr.reverse().toString());
+            }
+            return ans;
+        }
+
+        public String addStrings(String num1, String num2) {
+            int i = num1.length() - 1, j = num2.length() - 1, add = 0;
+            StringBuffer ans = new StringBuffer();
+            while (i >= 0 || j >= 0 || add != 0) {
+                int x = i >= 0 ? num1.charAt(i) - '0' : 0;
+                int y = j >= 0 ? num2.charAt(j) - '0' : 0;
+                int result = x + y + add;
+                ans.append(result % 10);
+                add = result / 10;
+                i--;
+                j--;
+            }
+            ans.reverse();
             return ans.toString();
         }
     }
