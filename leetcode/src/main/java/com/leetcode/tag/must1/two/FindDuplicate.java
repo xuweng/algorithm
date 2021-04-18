820b906bbc9789c23753c7d30dae0ca7446013ff
xuweng <2028403305@qq.com>
2021-04-18 12:07:05 +0800

    287. 寻找重复数
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must1/two/FindDuplicate.java
+++ leetcode/src/main/java/com/leetcode/tag/must1/two/FindDuplicate.java
@@ -5,6 +5,8 @@ import java.util.Set;
 
 /**
  * 287. 寻找重复数
+ * <p>
+ * 移动后比较 if开始
  */
 public class FindDuplicate {
     class Solution {
@@ -20,6 +22,32 @@ public class FindDuplicate {
             }
 
             return 0;
+        }
+    }
+
+    class Solution1 {
+        public int findDuplicate(int[] nums) {
+            if (nums == null) {
+                return 0;
+            }
+            int left = 1, right = nums.length - 1;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                int count = 0;
+                for (int num : nums) {
+                    if (num <= mid) {
+                        count++;
+                    }
+                }
+                if (count > mid) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+
+            }
+
+            return left;
         }
     }
 }
