5fdae4ca9be6983c79ce1f8b03198003268dc502
xuweng <2028403305@qq.com>
2021-04-18 10:05:11 +0800

    203. 移除链表元素
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must1/two/RemoveElements.java
@@ -0,0 +1,42 @@
+package com.leetcode.tag.must1.two;
+
+/**
+ * 203. 移除链表元素
+ */
+public class RemoveElements {
+    class Solution {
+        public ListNode removeElements(ListNode head, int val) {
+            if (head == null) {
+                return null;
+            }
+            ListNode ya = new ListNode(-1, head);
+            ListNode cur = ya;
+            while (cur.next != null) {
+                if (cur.next.val == val) {
+                    cur.next = cur.next.next;
+                } else {
+                    cur = cur.next;
+                }
+            }
+
+            return ya.next;
+        }
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
