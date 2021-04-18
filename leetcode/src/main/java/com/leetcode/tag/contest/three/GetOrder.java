0071d51ba48baba08adda7387e989cb49e0d1f6d
xuweng <2028403305@qq.com>
2021-04-18 10:59:37 +0800

    5736. 单线程 CPU
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/three/GetOrder.java
@@ -0,0 +1,12 @@
+package com.leetcode.tag.contest.three;
+
+/**
+ * 5736. 单线程 CPU
+ */
+public class GetOrder {
+    class Solution {
+        public int[] getOrder(int[][] tasks) {
+            return null;
+        }
+    }
+}
