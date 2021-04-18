b7461e6733f41c59c0acfe9cedd6cc472e495400
xuweng <2028403305@qq.com>
2021-04-18 09:37:39 +0800

    27. 移除元素
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must1/two/RemoveElement.java
@@ -0,0 +1,24 @@
+package com.leetcode.tag.must1.two;
+
+/**
+ * 27. 移除元素
+ */
+public class RemoveElement {
+    class Solution {
+        public int removeElement(int[] nums, int val) {
+            if (nums == null) {
+                return 0;
+            }
+            int i = 0, j = 0;
+            while (j < nums.length) {
+                while (nums[j] != val) {
+                    // 复制
+                    nums[i++] = nums[j];
+                }
+                j++;
+            }
+
+            return i;
+        }
+    }
+}
