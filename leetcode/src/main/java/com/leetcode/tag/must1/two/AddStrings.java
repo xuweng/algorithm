dc78a1b6d5594bffe81e1456eb2a786b102edfc4
xuweng <2028403305@qq.com>
2021-04-18 17:49:02 +0800

    415. 字符串相加
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must1/two/AddStrings.java
@@ -0,0 +1,33 @@
+package com.leetcode.tag.must1.two;
+
+/**
+ * 415. 字符串相加
+ * <p>
+ * 移动后比较
+ */
+public class AddStrings {
+    class Solution {
+        public String addStrings(String num1, String num2) {
+            if (num1 == null || num2 == null) {
+                return null;
+            }
+            int i = num1.length() - 1, j = num2.length() - 1;
+            int jin = 0;
+            StringBuilder stringBuilder = new StringBuilder();
+            while (i < num1.length() || j < num2.length()) {
+                int v1 = i >= 0 ? num1.charAt(i) - '0' : 0;
+                int v2 = j >= 0 ? num2.charAt(j) - '0' : 0;
+                int sum = v1 + v2 + jin;
+                stringBuilder.append(sum % 10);
+                jin = sum / 10;
+
+                i--;
+                j--;
+            }
+            if (jin > 0) {
+                stringBuilder.append(jin);
+            }
+            return stringBuilder.reverse().toString();
+        }
+    }
+}
