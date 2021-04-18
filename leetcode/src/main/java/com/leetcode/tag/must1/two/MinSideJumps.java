984ceb68ce226627804c01d77990b408044a1fae
xuweng <2028403305@qq.com>
2021-04-18 22:06:11 +0800

    1824. 最少侧跳次数
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must1/two/MinSideJumps.java
+++ leetcode/src/main/java/com/leetcode/tag/must1/two/MinSideJumps.java
@@ -23,8 +23,7 @@ public class MinSideJumps {
                     dp[i][2] = Integer.MAX_VALUE - 1;
                     dp[i][1] = Math.min(dp[i - 1][1], dp[i - 1][3] + 1);
                     dp[i][3] = Math.min(dp[i - 1][3], dp[i - 1][1] + 1);
-                }
-                if (obstacles[i] == 3) {
+                } else if (obstacles[i] == 3) {
                     dp[i][3] = Integer.MAX_VALUE - 1;
                     dp[i][1] = Math.min(dp[i - 1][1], dp[i - 1][2] + 1);
                     dp[i][2] = Math.min(dp[i - 1][2], dp[i - 1][1] + 1);
