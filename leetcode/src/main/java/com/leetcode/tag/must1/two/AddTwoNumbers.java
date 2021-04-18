fbfe715574305163156e5191ecdf312863779347
xuweng <2028403305@qq.com>
2021-04-18 17:56:10 +0800

    2. 两数相加
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must1/two/AddTwoNumbers.java
@@ -0,0 +1,53 @@
+package com.leetcode.tag.must1.two;
+
+/**
+ * 2. 两数相加
+ */
+public class AddTwoNumbers {
+    class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode head = null, tail = null;
+            int jin = 0;
+            while (l1 != null || l2 != null) {
+                int v1 = l1 == null ? 0 : l1.val;
+                int v2 = l2 == null ? 0 : l2.val;
+                int sum = v1 + v2 + jin;
+                jin = sum / 10;
+                if (head == null) {
+                    head = tail = new ListNode(sum % 10);
+                } else {
+                    tail.next = new ListNode(sum % 10);
+                    tail = tail.next;
+                }
+                if (l1 != null) {
+                    l1 = l1.next;
+                }
+                if (l2 != null) {
+                    l2 = l2.next;
+                }
+            }
+            if (jin > 0) {
+                tail.next = new ListNode(jin);
+            }
+
+            return head;
+        }
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
