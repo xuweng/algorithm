c143c7cd0247b9242625f80245331fd15107dd5e
xuweng <2028403305@qq.com>
2021-04-18 16:47:50 +0800

    268. 丢失的数字
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must1/two/MissingNumber.java
@@ -0,0 +1,29 @@
+package com.leetcode.tag.must1.two;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 268. 丢失的数字
+ */
+public class MissingNumber {
+    class Solution {
+        public int missingNumber(int[] nums) {
+            if (nums == null) {
+                return 0;
+            }
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                set.add(num);
+            }
+            int n = nums.length;
+            for (int i = 0; i <= n; i++) {
+                if (!set.contains(i)) {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
