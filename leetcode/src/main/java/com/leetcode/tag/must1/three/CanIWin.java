83f7967ce05373a8499bdf35a9e88214ae0d7a28
xuweng <2028403305@qq.com>
2021-04-19 21:20:13 +0800

    先手能赢就选最大值 先手不赢就选最小值 后手能赢就选最大值 后手不赢就选最小值
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must1/three/CanIWin.java
+++ leetcode/src/main/java/com/leetcode/tag/must1/three/CanIWin.java
@@ -166,4 +166,43 @@ public class CanIWin {
             return false;
         }
     }
+
+    /**
+     * 先手能赢就选最大值 先手不赢就选最小值
+     * <p>
+     * 后手能赢就选最大值 后手不赢就选最小值
+     */
+    class Solution3 {
+        Boolean[] dp = null; //用Boolean比较方便判断是否记忆化
+
+        public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+            //小于最大值先手可以直接拿
+            if (desiredTotal <= maxChoosableInteger) {
+                return true;
+            }
+            //前N项和还不够desiredTotal
+            if ((1 + maxChoosableInteger) * (maxChoosableInteger) / 2 < desiredTotal) {
+                return false;
+            }
+            //20位二进制 1<<21
+            dp = new Boolean[1 << 21];
+            return dfs(maxChoosableInteger, desiredTotal, 0);
+        }
+
+        public boolean dfs(int max, int total, int state) {
+            if (total <= 0) {
+                return false; //前人已经拿完了
+            }
+            if (dp[state] != null) {
+                return dp[state];
+            }
+            for (int i = max; i >= 1; i--) {
+                //参数传递的，就不用回溯了，代码变的简洁多了
+                if ((state >> i & 1) == 0 && !dfs(max, total - i, state | (1 << i))) {
+                    return dp[state] = true;
+                }
+            }
+            return dp[state] = false;
+        }
+    }
 }
