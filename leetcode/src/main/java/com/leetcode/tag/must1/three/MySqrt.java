3ae2c179478a84a7293296951b4b47f6b1d97f3f
xuweng <2028403305@qq.com>
2021-04-20 22:26:06 +0800

    69. x 的平方根
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must1/three/MySqrt.java
+++ leetcode/src/main/java/com/leetcode/tag/must1/three/MySqrt.java
@@ -9,7 +9,7 @@ public class MySqrt {
             if (x < 0) {
                 return 1 / mySqrt(-x);
             }
-            int left = 1;
+            int left = 0;
             int right = x;
             while (left <= right) {
                 int mid = left + (right - left) / 2;
@@ -17,9 +17,9 @@ public class MySqrt {
                     return mid;
                 }
                 if (x > mid * mid) {
-                    right = mid - 1;
-                } else {
                     left = mid + 1;
+                } else {
+                    right = mid - 1;
                 }
             }
 
