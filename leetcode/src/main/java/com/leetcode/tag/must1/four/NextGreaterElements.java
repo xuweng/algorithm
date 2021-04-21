c57f2fc8eabf68227c084b580be7bef685dc2683
xuweng <2028403305@qq.com>
2021-04-21 21:38:30 +0800

    503. 下一个更大元素 II
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must1/four/NextGreaterElements.java
@@ -0,0 +1,26 @@
+package com.leetcode.tag.must1.four;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 503. 下一个更大元素 II
+ */
+public class NextGreaterElements {
+    class Solution {
+        public int[] nextGreaterElements(int[] nums) {
+            Deque<Integer> deque = new LinkedList<>();
+            int[] result = new int[nums.length];
+            for (int i = 2 * nums.length - 1; i >= 0; i--) {
+                int index = i % nums.length;
+                while (!deque.isEmpty() && deque.peekLast() <= nums[index]) {
+                    deque.pollLast();
+                }
+                result[index] = deque.isEmpty() ? -1 : deque.peekLast();
+                deque.offerLast(nums[index]);
+            }
+
+            return result;
+        }
+    }
+}
