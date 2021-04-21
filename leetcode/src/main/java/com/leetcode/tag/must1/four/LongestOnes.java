3a3e2f65b0645c998c9bffd56d16d411658a81f6
xuweng <2028403305@qq.com>
2021-04-21 21:50:22 +0800

    1004. 最大连续1的个数 III
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must1/four/LongestOnes.java
@@ -0,0 +1,26 @@
+package com.leetcode.tag.must1.four;
+
+/**
+ * 1004. 最大连续1的个数 III
+ */
+public class LongestOnes {
+    class Solution {
+        public int longestOnes(int[] nums, int k) {
+            int left = 0;
+            int right = 0;
+            int sum = 0;
+            int result = 0;
+
+            while (right < nums.length) {
+                sum += nums[right];
+                while (right - left + 1 > sum + k) {
+                    sum -= nums[left++];
+                }
+                result = Math.max(result, right - left + 1);
+                right++;
+            }
+
+            return result;
+        }
+    }
+}
