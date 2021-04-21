cfd222d85b9cd7a5ab1004e86185ae4202e22090
xuweng <2028403305@qq.com>
2021-04-21 22:15:27 +0800

    633. 平方数之和
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must1/four/JudgeSquareSum.java
@@ -0,0 +1,27 @@
+package com.leetcode.tag.must1.four;
+
+/**
+ * 633. 平方数之和
+ */
+public class JudgeSquareSum {
+    class Solution {
+        public boolean judgeSquareSum(int c) {
+            int left = 1;
+            int right = (int) Math.sqrt(c);
+
+            while (left <= right) {
+                int sum = left * left + right * right;
+                if (sum == c) {
+                    return true;
+                }
+                if (sum < c) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+
+            return false;
+        }
+    }
+}
