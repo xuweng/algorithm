36f4df630a7c4f8dbd8a9a618631e4c8e2279882
xuweng <2028403305@qq.com>
2021-04-22 22:58:22 +0800

    剑指 Offer 22. 链表中倒数第k个节点
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must1/five/GetKthFromEnd.java
@@ -0,0 +1,32 @@
+package com.leetcode.tag.must1.five;
+
+/**
+ * 剑指 Offer 22. 链表中倒数第k个节点
+ */
+public class GetKthFromEnd {
+    class Solution {
+        public ListNode getKthFromEnd(ListNode head, int k) {
+            int count = 1;
+            ListNode p = head;
+            while (p != null && count < k) {
+                count++;
+                p = p.next;
+            }
+            while (p != null) {
+                p = p.next;
+                head = head.next;
+            }
+
+            return head;
+        }
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
