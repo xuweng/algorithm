e27553e1570a398496ee569b704cb40e228f70de
xuweng <2028403305@qq.com>
2021-04-22 22:11:17 +0800

    303. 区域和检索 - 数组不可变
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must1/five/NumArray.java
@@ -0,0 +1,21 @@
+package com.leetcode.tag.must1.five;
+
+/**
+ * 303. 区域和检索 - 数组不可变
+ * <p>
+ * 大数溢出
+ */
+public class NumArray {
+    int[] sum;
+
+    public NumArray(int[] nums) {
+        sum = new int[nums.length + 1];
+        for (int i = 1; i <= nums.length; i++) {
+            sum[i] = sum[i - 1] + nums[i - 1];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return sum[right + 1] - sum[left];
+    }
+}
