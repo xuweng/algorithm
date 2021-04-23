bede95ed982e3104abcd6dc0841f4958c73cf439
xuweng <2028403305@qq.com>
2021-04-23 22:57:10 +0800

    剑指 Offer 24. 反转链表
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must1/six/ReverseList.java
@@ -0,0 +1,40 @@
+package com.leetcode.tag.must1.six;
+
+/**
+ * 剑指 Offer 24. 反转链表
+ */
+public class ReverseList {
+    class Solution {
+        ListNode listNode;
+
+        public ListNode reverseList(ListNode head) {
+            if (head == null) {
+                return null;
+            }
+            dfs(head);
+
+            return listNode;
+        }
+
+        private ListNode dfs(ListNode head) {
+            if (head.next == null) {
+                listNode = head;
+                return head;
+            }
+            ListNode dfs = dfs(head.next);
+            dfs.next = head;
+            head.next = null;
+
+            return head;
+        }
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
