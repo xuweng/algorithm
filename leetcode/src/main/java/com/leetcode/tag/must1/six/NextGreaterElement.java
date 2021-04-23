9474a0d1c223e6e03c7a8268a7d2ccd96eda7d8e
xuweng <2028403305@qq.com>
2021-04-23 22:53:31 +0800

    496. 下一个更大元素 I
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must1/six/NextGreaterElement.java
@@ -0,0 +1,33 @@
+package com.leetcode.tag.must1.six;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * 496. 下一个更大元素 I
+ */
+public class NextGreaterElement {
+    class Solution {
+        public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+            Deque<Integer> deque = new LinkedList<>();
+            Map<Integer, Integer> map = new HashMap<>();
+
+            for (int i = nums2.length - 1; i >= 0; i--) {
+                while (!deque.isEmpty() && deque.peekLast() <= nums2[i]) {
+                    deque.pollLast();
+                }
+                map.put(nums2[i], deque.isEmpty() ? -1 : deque.peekLast());
+                deque.offerLast(nums2[i]);
+            }
+
+            int[] result = new int[nums1.length];
+            for (int i = 0; i < nums1.length; i++) {
+                result[i] = map.get(nums1[i]);
+            }
+
+            return result;
+        }
+    }
+}
