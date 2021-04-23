432be906a367374d04a83529d8165191b1804b28
xuweng <2028403305@qq.com>
2021-04-23 21:35:59 +0800

    剑指 Offer 21. 调整数组顺序使奇数位于偶数前面
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must1/six/Exchange.java
@@ -0,0 +1,28 @@
+package com.leetcode.tag.must1.six;
+
+/**
+ * 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面
+ */
+public class Exchange {
+    class Solution {
+        public int[] exchange(int[] nums) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left < right) {
+                while (left < right && nums[left] % 2 != 0) {
+                    left++;
+                }
+                while (left < right && nums[right] % 2 == 0) {
+                    right--;
+                }
+                if (left < right) {
+                    int tem = nums[left];
+                    nums[left] = nums[right];
+                    nums[right] = tem;
+                }
+            }
+
+            return nums;
+        }
+    }
+}
