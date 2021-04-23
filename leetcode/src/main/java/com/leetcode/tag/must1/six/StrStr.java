9cde5c305029a187f5f92e0e2213bb8f525a733d
xuweng <2028403305@qq.com>
2021-04-23 22:01:02 +0800

    28. 实现 strStr()
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must1/six/StrStr.java
@@ -0,0 +1,45 @@
+package com.leetcode.tag.must1.six;
+
+/**
+ * 28. 实现 strStr()
+ */
+public class StrStr {
+    class Solution {
+        public int strStr(String haystack, String needle) {
+            if (needle == null || needle.isEmpty()) {
+                return 0;
+            }
+            int[] next = getNext(needle);
+            int j = 0;
+            for (int i = 0; i < haystack.length(); i++) {
+                while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
+                    j = next[j - 1];
+                }
+                if (haystack.charAt(i) == needle.charAt(j)) {
+                    j++;
+                }
+                if (j == needle.length()) {
+                    return i - needle.length() + 1;
+                }
+            }
+
+            return -1;
+        }
+
+        private int[] getNext(String needle) {
+            int[] next = new int[needle.length()];
+            int j = 0;
+            for (int i = 1; i < needle.length(); i++) {
+                while (j > 0 && needle.charAt(i) != needle.charAt(j)) {
+                    j = next[j - 1];
+                }
+                if (needle.charAt(i) == needle.charAt(j)) {
+                    j++;
+                }
+                next[i] = j;
+            }
+
+            return next;
+        }
+    }
+}
