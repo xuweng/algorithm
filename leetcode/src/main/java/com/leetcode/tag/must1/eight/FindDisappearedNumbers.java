46cbf07a4938357809e6979796a0f2a8639bf1f2
xuweng <2028403305@qq.com>
2021-04-25 22:29:04 +0800

    448. 找到所有数组中消失的数字
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must1/eight/FindDisappearedNumbers.java
@@ -0,0 +1,34 @@
+package com.leetcode.tag.must1.eight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 448. 找到所有数组中消失的数字
+ */
+public class FindDisappearedNumbers {
+    class Solution {
+        public List<Integer> findDisappearedNumbers(int[] nums) {
+            for (int i = 0; i < nums.length; i++) {
+                while (nums[i] != i + 1 && nums[nums[i] - 1] != nums[i]) {
+                    swap(nums, i, nums[i] - 1);
+                }
+            }
+
+            List<Integer> result = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != i + 1) {
+                    result.add(nums[i]);
+                }
+            }
+
+            return result;
+        }
+
+        private void swap(int[] nums, int i, int j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
