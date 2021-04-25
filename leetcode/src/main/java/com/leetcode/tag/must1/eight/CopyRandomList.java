d16156c0f71f6832b5790350fa6dae23e4238da7
xuweng <2028403305@qq.com>
2021-04-25 22:59:02 +0800

    剑指 Offer 35. 复杂链表的复制
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must1/eight/CopyRandomList.java
@@ -0,0 +1,62 @@
+package com.leetcode.tag.must1.eight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 剑指 Offer 35. 复杂链表的复制
+ */
+public class CopyRandomList {
+    class Solution {
+        public Node copyRandomList(Node head) {
+            Map<Node, Node> map = new HashMap<>();
+            Node ya = new Node(-1);
+            Node cur = ya;
+            while (head != null) {
+                cur.next = new Node(head.val);
+                map.put(head, head.random);
+
+                cur = cur.next;
+                head = head.next;
+            }
+
+            return head;
+        }
+    }
+
+    class Solution1 {
+        public Node copyRandomList(Node head) {
+            if (head == null) {
+                return null;
+            }
+            Node cur = head;
+            Map<Node, Node> map = new HashMap<>();
+            // 3. 复制各节点，并建立 “原节点 -> 新节点” 的 Map 映射
+            while (cur != null) {
+                map.put(cur, new Node(cur.val));
+                cur = cur.next;
+            }
+            cur = head;
+            // 4. 构建新链表的 next 和 random 指向
+            while (cur != null) {
+                map.get(cur).next = map.get(cur.next);
+                map.get(cur).random = map.get(cur.random);
+                cur = cur.next;
+            }
+            // 5. 返回新链表的头节点
+            return map.get(head);
+        }
+    }
+
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+}
