5835d6489055974c00a731aeca3a5b229319fbcc
xuweng <2028403305@qq.com>
2021-04-26 23:17:39 +0800

    没有初始化0行 初始化最大值
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must1/nine/MinimumTotal.java
+++ leetcode/src/main/java/com/leetcode/tag/must1/nine/MinimumTotal.java
@@ -34,8 +34,6 @@ public class MinimumTotal {
         public int minimumTotal(List<List<Integer>> triangle) {
             int m = triangle.size();
             int[] dp = new int[m];
-            // 没有初始化0行 初始化最大值
-            Arrays.fill(dp, Integer.MAX_VALUE);
             dp[0] = triangle.get(0).get(0);
 
             for (int i = 1; i < triangle.size(); i++) {
