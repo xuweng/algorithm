d01cd167dc6f9373ee1719f97b7e674871d19392
xuweng <2028403305@qq.com>
2021-04-26 21:19:57 +0800

    62. 不同路径
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must1/nine/UniquePaths.java
+++ leetcode/src/main/java/com/leetcode/tag/must1/nine/UniquePaths.java
@@ -7,6 +7,7 @@ public class UniquePaths {
     class Solution {
         public int uniquePaths(int m, int n) {
             int[][] dp = new int[m][n];
+            dp[0][0] = 1;
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
                     if (i > 0 && j > 0) {
