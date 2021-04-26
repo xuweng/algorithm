0b85c8f1124aa95055eb5bebfafe9e1fb75bb324
xuweng <2028403305@qq.com>
2021-04-26 22:26:58 +0800

    64. 最小路径和
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must1/nine/MinPathSum.java
+++ leetcode/src/main/java/com/leetcode/tag/must1/nine/MinPathSum.java
@@ -14,6 +14,7 @@ public class MinPathSum {
             int n = grid[0].length;
 
             int[][] dp = new int[m][n];
+            dp[0][0] = grid[0][0];
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
                     if (i > 0 && j > 0) {
@@ -21,7 +22,7 @@ public class MinPathSum {
                     } else if (i > 0) {
                         dp[i][j] = dp[i - 1][j] + grid[i][j];
                     } else if (j > 0) {
-                        dp[i][j] = dp[i][j - 1];
+                        dp[i][j] = dp[i][j - 1] + grid[i][j];
                     }
                 }
             }
