5cfe68eff8ae95fd2e9a1739b77d2ca9be54713c
xuweng <2028403305@qq.com>
2021-04-26 20:37:11 +0800

    方法一：二分查找转化为判定问题
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must1/nine/ShipWithinDays.java
+++ leetcode/src/main/java/com/leetcode/tag/must1/nine/ShipWithinDays.java
@@ -6,11 +6,18 @@ import java.util.Arrays;
  * 1011. 在 D 天内送达包裹的能力
  */
 public class ShipWithinDays {
+    /**
+     * 方法一：二分查找转化为判定问题
+     */
     class Solution {
         public int shipWithinDays(int[] weights, int D) {
             // 确定二分查找左右边界
+            // 左边界而言，由于我们不能「拆分」一个包裹，因此船的运载能力不能小于所有包裹中最重的那个的重量，
+            // 即左边界为数组 weights 中元素的最大值
             int left = Arrays.stream(weights).max().getAsInt();
+            // 右边界而言，船的运载能力也不会大于所有包裹的重量之和，即右边界为数组  weights 中元素的和
             int right = Arrays.stream(weights).sum();
+            // 从上述左右边界开始进行二分查找，就可以保证找到最终的答案
             while (left < right) {
                 int mid = (left + right) / 2;
                 // need 为需要运送的天数
