3a04b948369d105c20ff159590ea3ceac5620492
xuweng <2028403305@qq.com>
2021-04-27 20:48:02 +0800

    938. 二叉搜索树的范围和
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/RangeSumBST.java
@@ -0,0 +1,41 @@
+package com.leetcode.tag.must2;
+
+/**
+ * 938. 二叉搜索树的范围和
+ */
+public class RangeSumBST {
+    class Solution {
+        public int rangeSumBST(TreeNode root, int low, int high) {
+            if (root == null) {
+                return 0;
+            }
+            if (high < root.val) {
+                return rangeSumBST(root.left, low, high);
+            }
+            if (low > root.val) {
+                return rangeSumBST(root.right, low, high);
+            }
+
+            return root.val + rangeSumBST(root.left, low, root.val) + rangeSumBST(root.right, root.val, high);
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
