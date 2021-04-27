a003e78b919e97a3feeebf1bbaa0307018315296
xuweng <2028403305@qq.com>
2021-04-27 21:05:19 +0800

    931. 下降路径最小和
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/MinFallingPathSum.java
@@ -0,0 +1,34 @@
+package com.leetcode.tag.must2;
+
+import java.util.Arrays;
+
+/**
+ * 931. 下降路径最小和
+ */
+public class MinFallingPathSum {
+    class Solution {
+        public int minFallingPathSum(int[][] matrix) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+
+            int[][] dp = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i == 0) {
+                        dp[i][j] = matrix[i][j];
+                        continue;
+                    }
+                    if (j > 0 && j < n - 1) {
+                        dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i - 1][j + 1])) + matrix[i][j];
+                    } else if (j > 0) {
+                        dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + matrix[i][j];
+                    } else if (j < n - 1) {
+                        dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j + 1]) + matrix[i][j];
+                    }
+                }
+            }
+
+            return Arrays.stream(dp[m - 1]).min().getAsInt();
+        }
+    }
+}
