ffbe23938ae5252d19da754fb7b9e61920363a6e
xuweng <2028403305@qq.com>
2021-04-29 21:55:19 +0800

    402. 移掉K位数字
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/three/RemoveKdigits.java
@@ -0,0 +1,37 @@
+package com.leetcode.tag.must2.three;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 402. 移掉K位数字
+ */
+public class RemoveKdigits {
+    class Solution {
+        public String removeKdigits(String num, int k) {
+            Deque<Character> deque = new LinkedList<>();
+            for (int i = 0; i < num.length(); i++) {
+                while (!deque.isEmpty() && k > 0 && deque.peekLast() >= num.charAt(i)) {
+                    deque.pollLast();
+                    k--;
+                }
+                if (deque.isEmpty() && num.charAt(i) == '0') {
+                    // 不能有任何前导零
+                    continue;
+                }
+                deque.offerLast(num.charAt(i));
+            }
+            while (k > 0) {
+                // 移出剩余
+                deque.pollLast();
+                k--;
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            while (!deque.isEmpty()) {
+                stringBuilder.append(deque.pollFirst());
+            }
+
+            return stringBuilder.toString();
+        }
+    }
+}
