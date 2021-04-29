6a14274b89d6760c12e32cf6b8068d230916a8c6
xuweng <2028403305@qq.com>
2021-04-29 22:29:06 +0800

    120. 三角形最小路径和
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must2/three/MinimumTotal.java
+++ leetcode/src/main/java/com/leetcode/tag/must2/three/MinimumTotal.java
@@ -11,7 +11,7 @@ public class MinimumTotal {
             int m = triangle.size();
             int[] dp = new int[m + 1];
 
-            for (int i = m - 1; i > 0; i--) {
+            for (int i = m; i > 0; i--) {
                 for (int j = 0; j < i; j++) {
                     dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i - 1).get(j);
                 }
