254232567d192b997bff5281419d507f1073c917
xuweng <2028403305@qq.com>
2021-04-30 22:30:47 +0800

    368. 最大整除子集
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/four/LargestDivisibleSubset.java
@@ -0,0 +1,47 @@
+package com.leetcode.tag.must2.four;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 368. 最大整除子集
+ * <p>
+ * 枚举依赖 枚举依赖 枚举依赖
+ * <p>
+ * 可变参数 可变参数 可变参数
+ */
+public class LargestDivisibleSubset {
+    class Solution {
+        public List<Integer> largestDivisibleSubset(int[] nums) {
+            int m = nums.length;
+            int[] dp = new int[m];
+            int[] back = new int[m];
+
+            int max = 1;
+            int index = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                dp[i] = 1;
+                for (int j = 0; j < i; j++) {
+                    if (nums[i] % nums[j] == 0) {
+                        if (dp[i] < dp[j] + 1) {
+                            dp[i] = dp[j] + 1;
+                            back[i] = j;
+                        }
+                    }
+                }
+                if (max < dp[i]) {
+                    max = dp[i];
+                    index = i;
+                }
+            }
+            List<Integer> result = new ArrayList<>();
+            while (result.size() != max) {
+                result.add(nums[index]);
+                index = back[index];
+            }
+
+            return result;
+        }
+    }
+}
