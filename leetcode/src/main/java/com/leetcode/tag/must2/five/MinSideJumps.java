0092441a5e7cf6c435983ecd2e38bd52abfdb60c
xuweng <2028403305@qq.com>
2021-05-01 11:18:19 +0800

    1824. 最少侧跳次数
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/five/MinSideJumps.java
@@ -0,0 +1,38 @@
+package com.leetcode.tag.must2.five;
+
+/**
+ * 1824. 最少侧跳次数
+ */
+public class MinSideJumps {
+    class Solution {
+        public int minSideJumps(int[] obstacles) {
+            int[][] dp = new int[obstacles.length][4];
+            // 0-->1
+            dp[0][1] = 1;
+            // 0->2
+            dp[0][2] = 1;
+
+            for (int i = 1; i < obstacles.length; i++) {
+                if (obstacles[i] == 1) {
+                    dp[i][1] = Integer.MAX_VALUE - 1;
+                    dp[i][2] = Math.min(dp[i - 1][2], dp[i - 1][3] + 1);
+                    dp[i][3] = Math.min(dp[i - 1][3], dp[i - 1][2] + 1);
+                } else if (obstacles[i] == 2) {
+                    dp[i][2] = Integer.MAX_VALUE - 1;
+                    dp[i][1] = Math.min(dp[i - 1][1], dp[i - 1][3] + 1);
+                    dp[i][3] = Math.min(dp[i - 1][3], dp[i - 1][1] + 1);
+                } else if (obstacles[i] == 3) {
+                    dp[i][3] = Integer.MAX_VALUE - 1;
+                    dp[i][1] = Math.min(dp[i - 1][1], dp[i - 1][2] + 1);
+                    dp[i][2] = Math.min(dp[i - 1][2], dp[i - 1][1] + 1);
+                } else {
+                    dp[i][1] = Math.min(dp[i - 1][1], Math.min(dp[i - 1][2], dp[i - 1][3]) + 1);
+                    dp[i][2] = Math.min(dp[i - 1][2], Math.min(dp[i - 1][1], dp[i - 1][3]) + 1);
+                    dp[i][3] = Math.min(dp[i - 1][3], Math.min(dp[i - 1][1], dp[i - 1][2]) + 1);
+                }
+            }
+
+            return Math.min(dp[dp.length - 1][1], Math.min(dp[dp.length - 1][2], dp[dp.length - 1][3]));
+        }
+    }
+}
