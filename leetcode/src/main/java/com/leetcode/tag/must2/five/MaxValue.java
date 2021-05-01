8257b69ec2067d754cead5e386dc9c06f01a4020
xuweng <2028403305@qq.com>
2021-05-01 11:24:44 +0800

    LCP 34. 二叉树染色
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/five/MaxValue.java
@@ -0,0 +1,48 @@
+package com.leetcode.tag.must2.five;
+
+import java.util.Arrays;
+
+/**
+ * LCP 34. 二叉树染色
+ */
+public class MaxValue {
+    class Solution {
+        public int maxValue(TreeNode root, int k) {
+            int[] dfs = dfs(root, k);
+
+            return Arrays.stream(dfs).max().getAsInt();
+        }
+
+        private int[] dfs(TreeNode root, int k) {
+            if (root == null) {
+                return new int[k + 1];
+            }
+            int[] left = dfs(root.left, k);
+            int[] right = dfs(root.right, k);
+            int[] dp = new int[k + 1];
+            int lMax = 0;
+            int rMax = 0;
+            for (int i = 0; i <= k; i++) {
+                lMax = Math.max(lMax, left[i]);
+                rMax = Math.max(rMax, right[i]);
+            }
+            for (int i = 1; i <= k; i++) {
+                for (int j = 0; j < i; j++) {
+                    dp[i] = Math.max(dp[i], dp[j] + dp[i - j - 1] + root.val);
+                }
+            }
+
+            return dp;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
