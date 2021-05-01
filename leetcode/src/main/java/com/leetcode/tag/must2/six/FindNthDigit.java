160b65f45fb4202f8aa5d63a59babcacc261af58
xuweng <2028403305@qq.com>
2021-05-01 21:09:58 +0800

    剑指 Offer 44. 数字序列中某一位的数字
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/six/FindNthDigit.java
@@ -0,0 +1,22 @@
+package com.leetcode.tag.must2.six;
+
+/**
+ * 剑指 Offer 44. 数字序列中某一位的数字
+ */
+public class FindNthDigit {
+    class Solution {
+        public int findNthDigit(int n) {
+            int digit = 1;
+            long start = 1;
+            long count = 9;
+            while (n > count) { // 1.
+                n -= count;
+                digit += 1;
+                start *= 10;
+                count = digit * start * 9;
+            }
+            long num = start + (n - 1) / digit; // 2.
+            return Long.toString(num).charAt((n - 1) % digit) - '0'; // 3.
+        }
+    }
+}
