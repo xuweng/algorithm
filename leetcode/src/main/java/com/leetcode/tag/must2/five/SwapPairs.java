e57d69647eff9c80c172791fb2b95892d32fb1c7
xuweng <2028403305@qq.com>
2021-05-01 15:08:44 +0800

    24. 两两交换链表中的节点
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/five/SwapPairs.java
@@ -0,0 +1,37 @@
+package com.leetcode.tag.must2.five;
+
+/**
+ * 24. 两两交换链表中的节点
+ */
+public class SwapPairs {
+    class Solution {
+        public ListNode swapPairs(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode listNode = swapPairs(head.next.next);
+            ListNode next = head.next;
+            next.next = head;
+            head.next = listNode;
+
+            return next;
+        }
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
