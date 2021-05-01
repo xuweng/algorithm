d8a4db17e04ce482d698862b9926444908470f86
xuweng <2028403305@qq.com>
2021-05-01 15:54:44 +0800

    304. 二维区域和检索 - 矩阵不可变
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/five/NumMatrix.java
@@ -0,0 +1,24 @@
+package com.leetcode.tag.must2.five;
+
+/**
+ * 304. 二维区域和检索 - 矩阵不可变
+ */
+public class NumMatrix {
+    int[][] sum;
+
+    public NumMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        sum = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                sum[i][j] = sum[i][j - 1] + sum[i - 1][j] - sum[i - 1][j - 1] + matrix[i - 1][j - 1];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sum[row2 + 1][col2 + 1] - sum[row2 + 1][col1] - sum[row1][col2 + 1] + sum[row1][col1];
+    }
+}
