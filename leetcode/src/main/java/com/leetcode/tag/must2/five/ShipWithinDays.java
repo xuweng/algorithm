86bd0a4f347bf260a5f02f9b4bf2a0d43fe89c18
xuweng <2028403305@qq.com>
2021-05-01 09:47:57 +0800

    1011. 在 D 天内送达包裹的能力
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/five/ShipWithinDays.java
@@ -0,0 +1,35 @@
+package com.leetcode.tag.must2.five;
+
+import java.util.Arrays;
+
+/**
+ * 1011. 在 D 天内送达包裹的能力
+ */
+public class ShipWithinDays {
+    class Solution {
+        public int shipWithinDays(int[] weights, int D) {
+            int left = Arrays.stream(weights).max().getAsInt();
+            int right = Arrays.stream(weights).sum();
+
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                int need = 1;
+                int sum = 0;
+                for (int weight : weights) {
+                    sum += weight;
+                    if (sum > mid) {
+                        need++;
+                        sum = weight;
+                    }
+                }
+                if (need <= D) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            return right;
+        }
+    }
+}
