c49ef299536284358d5bae04bb5a6b7876478372
xuweng <2028403305@qq.com>
2021-05-01 21:38:54 +0800

    剑指 Offer 51. 数组中的逆序对
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must2/six/ReversePairs.java
+++ leetcode/src/main/java/com/leetcode/tag/must2/six/ReversePairs.java
@@ -6,6 +6,9 @@ import java.util.Arrays;
  * 剑指 Offer 51. 数组中的逆序对
  */
 public class ReversePairs {
+    /**
+     * 方法一：归并排序
+     */
     public class Solution {
         public int reversePairs(int[] nums) {
             int len = nums.length;
@@ -120,6 +123,58 @@ public class ReversePairs {
                 ++tree[x];
                 x += lowbit(x);
             }
+        }
+    }
+
+    class Solution2 {
+        public int reversePairs(int[] nums) {
+            int cnt = 0;
+            int len = nums.length;
+            for (int i = 0; i < len - 1; i++) {
+                for (int j = i + 1; j < len; j++) {
+                    if (nums[i] > nums[j]) {
+                        cnt++;
+                    }
+                }
+            }
+            return cnt;
+        }
+    }
+
+    class Solution3 {
+        int[] nums, tmp;
+
+        public int reversePairs(int[] nums) {
+            this.nums = nums;
+            tmp = new int[nums.length];
+
+            return mergeSort(0, nums.length - 1);
+        }
+
+        private int mergeSort(int l, int r) {
+            // 终止条件
+            if (l >= r) {
+                return 0;
+            }
+            // 递归划分
+            int m = (l + r) / 2;
+            int res = mergeSort(l, m) + mergeSort(m + 1, r);
+            // 合并阶段
+            int i = l, j = m + 1;
+            for (int k = l; k <= r; k++) {
+                tmp[k] = nums[k];
+            }
+            for (int k = l; k <= r; k++) {
+                if (i == m + 1) {
+                    nums[k] = tmp[j++];
+                } else if (j == r + 1 || tmp[i] <= tmp[j]) {
+                    nums[k] = tmp[i++];
+                } else {
+                    nums[k] = tmp[j++];
+                    res += m - i + 1; // 统计逆序对
+                }
+            }
+            return res;
         }
     }
 
