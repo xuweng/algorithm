b6045ad349fa8a79e2a025050a16613d0fec8c11
xuweng <2028403305@qq.com>
2021-05-01 15:18:33 +0800

    1312. 让字符串成为回文串的最少插入次数
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/five/MinInsertions.java
@@ -0,0 +1,23 @@
+package com.leetcode.tag.must2.five;
+
+/**
+ * 1312. 让字符串成为回文串的最少插入次数
+ */
+public class MinInsertions {
+    class Solution {
+        public int minInsertions(String s) {
+            int[][] dp = new int[s.length()][s.length()];
+            for (int i = s.length() - 2; i >= 0; i--) {
+                for (int j = i + 1; j < s.length(); j++) {
+                    if (s.charAt(i) == s.charAt(j)) {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    } else {
+                        dp[i][j] = Math.min(dp[i + 1][j], dp[i][j - 1]) + 1;
+                    }
+                }
+            }
+
+            return dp[0][s.length() - 1];
+        }
+    }
+}
