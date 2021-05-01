4369ab64a97c734e3e3640466caa540400e5604d
xuweng <2028403305@qq.com>
2021-05-01 16:11:21 +0800

    739. 每日温度
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/five/DailyTemperatures.java
@@ -0,0 +1,26 @@
+package com.leetcode.tag.must2.five;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 739. 每日温度
+ */
+public class DailyTemperatures {
+    class Solution {
+        public int[] dailyTemperatures(int[] T) {
+            Deque<Integer> deque = new LinkedList<>();
+            int[] result = new int[T.length];
+
+            for (int i = T.length - 1; i >= 0; i--) {
+                while (!deque.isEmpty() && T[deque.peekLast()] < T[i]) {
+                    deque.pollLast();
+                }
+                result[i] = deque.isEmpty() ? 0 : deque.peekLast() - i;
+                deque.offerLast(i);
+            }
+
+            return result;
+        }
+    }
+}
