6f29cda426ec2bec4842019e0a719f19ac2db08f
xuweng <2028403305@qq.com>
2021-05-01 12:01:22 +0800

    446. 等差数列划分 II - 子序列
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/five/NumberOfArithmeticSlices1.java
@@ -0,0 +1,38 @@
+package com.leetcode.tag.must2.five;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 446. 等差数列划分 II - 子序列
+ */
+public class NumberOfArithmeticSlices1 {
+    class Solution {
+        public int numberOfArithmeticSlices(int[] nums) {
+            Map<Long, List<Integer>> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                map.computeIfAbsent((long) nums[i], v -> new ArrayList<>()).add(i);
+            }
+            int sum = 0;
+            int[][] dp = new int[nums.length][nums.length];
+            for (int i = 2; i < nums.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    long key = 2 * (long) nums[j] - (long) nums[i];
+                    if (!map.containsKey(key)) {
+                        continue;
+                    }
+                    List<Integer> list = map.get(key);
+                    for (Integer k : list) {
+                        // k<j<i
+                        dp[i][j] += dp[j][k];
+                    }
+                    sum += dp[i][j];
+                }
+            }
+
+            return sum;
+        }
+    }
+}
