d6f271aa95cb4d8527c8d1e19f886b5bd4940366
xuweng <2028403305@qq.com>
2021-05-01 21:30:04 +0800

    剑指 Offer 51. 数组中的逆序对
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must2/six/ReversePairs.java
+++ leetcode/src/main/java/com/leetcode/tag/must2/six/ReversePairs.java
@@ -1,5 +1,7 @@
 package com.leetcode.tag.must2.six;
 
+import java.util.Arrays;
+
 /**
  * 剑指 Offer 51. 数组中的逆序对
  */
@@ -66,4 +68,59 @@ public class ReversePairs {
             return count;
         }
     }
+
+    /**
+     * 方法二：离散化树状数组
+     */
+    class Solution1 {
+        public int reversePairs(int[] nums) {
+            int n = nums.length;
+            int[] tmp = new int[n];
+            System.arraycopy(nums, 0, tmp, 0, n);
+            // 离散化
+            Arrays.sort(tmp);
+            for (int i = 0; i < n; ++i) {
+                nums[i] = Arrays.binarySearch(tmp, nums[i]) + 1;
+            }
+            // 树状数组统计逆序对
+            BIT bit = new BIT(n);
+            int ans = 0;
+            for (int i = n - 1; i >= 0; --i) {
+                ans += bit.query(nums[i] - 1);
+                bit.update(nums[i]);
+            }
+            return ans;
+        }
+    }
+
+    class BIT {
+        private int[] tree;
+        private int n;
+
+        public BIT(int n) {
+            this.n = n;
+            this.tree = new int[n + 1];
+        }
+
+        public int lowbit(int x) {
+            return x & (-x);
+        }
+
+        public int query(int x) {
+            int ret = 0;
+            while (x != 0) {
+                ret += tree[x];
+                x -= lowbit(x);
+            }
+            return ret;
+        }
+
+        public void update(int x) {
+            while (x <= n) {
+                ++tree[x];
+                x += lowbit(x);
+            }
+        }
+    }
+
 }
