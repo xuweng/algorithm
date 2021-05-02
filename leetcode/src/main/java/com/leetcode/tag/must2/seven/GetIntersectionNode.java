bfad47f9ce87e96a2116a7dacf73b4667295fd5b
xuweng <2028403305@qq.com>
2021-05-02 11:22:50 +0800

    lengthA + [headB,node] = lengthB + [headA,node]
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must2/seven/GetIntersectionNode.java
+++ leetcode/src/main/java/com/leetcode/tag/must2/seven/GetIntersectionNode.java
@@ -12,7 +12,30 @@ package com.leetcode.tag.must2.seven;
  * right right right right
  */
 public class GetIntersectionNode {
+    /**
+     * 设「第一个公共节点」为 node ，「链表 headA」的节点数量为 a ，「链表 headB」的节点数量为 b ，「两链表的公共尾部」的节点数量为 c ，则有：
+     * <p>
+     * 头节点 headA 到 node 前，共有 a - c 个节点；
+     * 头节点 headB 到 node 前，共有 b - c 个节点；
+     * <p>
+     * 指针 A 先遍历完链表 headA ，再开始遍历链表 headB ，当走到 node 时，共走步数为：
+     * a + (b - c)
+     * 指针 B 先遍历完链表 headB ，再开始遍历链表 headA ，当走到 node 时，共走步数为：
+     * b + (a - c)
+     * 如下式所示，此时指针 A , B 重合，并有两种情况：
+     * <p>
+     * a + (b - c) = b + (a - c)
+     */
     public class Solution {
+        /**
+         * node是第一个公共结点
+         * <p>
+         * lengthA + [headB,node] = lengthB + [headA,node]
+         *
+         * @param headA
+         * @param headB
+         * @return
+         */
         public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
             ListNode A = headA, B = headB;
             while (A != B) {
