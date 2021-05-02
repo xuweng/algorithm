add5695b4fa54632b0c60623a2926905ecb97e73
xuweng <2028403305@qq.com>
2021-05-02 18:04:51 +0800

    剑指 Offer 48. 最长不含重复字符的子字符串
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must2/seven/LengthOfLongestSubstring.java
+++ leetcode/src/main/java/com/leetcode/tag/must2/seven/LengthOfLongestSubstring.java
@@ -1,5 +1,8 @@
 package com.leetcode.tag.must2.seven;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 剑指 Offer 48. 最长不含重复字符的子字符串
  * <p>
@@ -11,12 +14,12 @@ public class LengthOfLongestSubstring {
             int left = 0;
             int right = 0;
             int result = 0;
-            int[] ints = new int[26];
+            Map<Character, Integer> map = new HashMap<>();
 
             while (right < s.length()) {
-                ints[s.charAt(right) - 'a']++;
-                while (ints[s.charAt(right) - 'a'] > 1) {
-                    ints[s.charAt(left) - 'a']--;
+                map.put(s.charAt(right), map.getOrDefault(s.charAt(right), 0) + 1);
+                while (map.get(s.charAt(left)) > 1) {
+                    map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
                     left++;
                 }
                 result = Math.max(result, right - left + 1);
