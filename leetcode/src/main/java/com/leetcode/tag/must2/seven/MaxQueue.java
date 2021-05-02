5e2e14eb890c601484df444936cf7f8f979f06ae
xuweng <2028403305@qq.com>
2021-05-02 19:11:26 +0800

    方法一：暴力
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must2/seven/MaxQueue.java
+++ leetcode/src/main/java/com/leetcode/tag/must2/seven/MaxQueue.java
@@ -39,4 +39,35 @@ public class MaxQueue {
         }
         return deque.pollFirst();
     }
+
+    /**
+     * 方法一：暴力
+     */
+    class MaxQueue1 {
+        int[] q = new int[20000];
+        int begin = 0, end = 0;
+
+        public MaxQueue1() {
+
+        }
+
+        public int max_value() {
+            int ans = -1;
+            for (int i = begin; i != end; ++i) {
+                ans = Math.max(ans, q[i]);
+            }
+            return ans;
+        }
+
+        public void push_back(int value) {
+            q[end++] = value;
+        }
+
+        public int pop_front() {
+            if (begin == end) {
+                return -1;
+            }
+            return q[begin++];
+        }
+    }
 }
