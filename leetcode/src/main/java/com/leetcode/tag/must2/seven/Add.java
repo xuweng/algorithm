d11fe981dbaeaf904ff3b1441f6d2a35658230db
xuweng <2028403305@qq.com>
2021-05-02 18:38:47 +0800

    使用位运算实现加法
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must2/seven/Add.java
+++ leetcode/src/main/java/com/leetcode/tag/must2/seven/Add.java
@@ -9,4 +9,18 @@ public class Add {
             return a + b;
         }
     }
+
+    /**
+     * 本题考察对位运算的灵活使用，即使用位运算实现加法
+     */
+    class Solution1 {
+        public int add(int a, int b) {
+            while (b != 0) { // 当进位为 0 时跳出
+                int c = (a & b) << 1;  // c = 进位
+                a ^= b; // a = 非进位和
+                b = c; // b = 进位
+            }
+            return a;
+        }
+    }
 }
