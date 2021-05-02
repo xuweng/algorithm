af877f6a6e0cfb4f79e55becaa64b876cefcbeef
xuweng <2028403305@qq.com>
2021-05-02 18:34:56 +0800

    剑指 Offer 65. 不用加减乘除做加法
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/seven/Add.java
@@ -0,0 +1,12 @@
+package com.leetcode.tag.must2.seven;
+
+/**
+ * 剑指 Offer 65. 不用加减乘除做加法
+ */
+public class Add {
+    class Solution {
+        public int add(int a, int b) {
+            return a + b;
+        }
+    }
+}
