dae1b1d4f96c985a44679b5dd8e891a2711ff398
xuweng <2028403305@qq.com>
2021-05-02 17:27:08 +0800

    剑指 Offer 52. 两个链表的第一个公共节点
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/seven/GetIntersectionNode1.java
@@ -0,0 +1,28 @@
+package com.leetcode.tag.must2.seven;
+
+/**
+ * 剑指 Offer 52. 两个链表的第一个公共节点
+ */
+public class GetIntersectionNode1 {
+    public class Solution {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            ListNode a = headA, b = headB;
+            while (a != b) {
+                a = (a == null) ? b : a.next;
+                b = (b == null) ? a : b.next;
+            }
+
+            return a;
+        }
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
