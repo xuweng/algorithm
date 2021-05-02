2c017084f0e09bfdae2cdca560b83b85ba006b11
xuweng <2028403305@qq.com>
2021-05-02 18:26:43 +0800

    58. 最后一个单词的长度
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/seven/LengthOfLastWord.java
@@ -0,0 +1,37 @@
+package com.leetcode.tag.must2.seven;
+
+/**
+ * 58. 最后一个单词的长度
+ */
+public class LengthOfLastWord {
+    class Solution {
+        public int lengthOfLastWord(String s) {
+            int a = 0;
+            for (int i = s.length() - 1; i >= 0; i--) {
+                if (s.charAt(i) != ' ') {
+                    a++;
+                } else if (a != 0) {
+                    return a;
+                }
+
+            }
+            return a;
+        }
+    }
+
+    class Solution1 {
+        public int lengthOfLastWord(String s) {
+            if (s == null || s.length() == 0) {
+                return 0;
+            }
+            int count = 0;
+            for (int i = s.length() - 1; i >= 0; i--) {
+                if (s.charAt(i) == ' ') {
+                    return count;
+                }
+                count++;
+            }
+            return count;
+        }
+    }
+}
