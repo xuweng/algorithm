a821ac2f6e3ff640ff006ae8c0103261d86a18ae
xuweng <2028403305@qq.com>
2021-05-02 12:48:27 +0800

    遍历
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must2/seven/ReverseWords.java
+++ leetcode/src/main/java/com/leetcode/tag/must2/seven/ReverseWords.java
@@ -159,4 +159,23 @@ public class ReverseWords {
             return reverseWords + " " + s.substring(start, end);
         }
     }
+
+    class Solution3 {
+        public String reverseWords(String s) {
+            // 删除首尾空格，分割字符串
+            String[] strs = s.trim().split(" ");
+            StringBuilder res = new StringBuilder();
+            // 倒序遍历单词列表
+            for (int i = strs.length - 1; i >= 0; i--) {
+                if ("".equals(strs[i])) {
+                    // 遇到空单词则跳过
+                    continue;
+                }
+                // 将单词拼接至 StringBuilder
+                res.append(strs[i] + " ");
+            }
+            // 转化为字符串，删除尾部空格，并返回
+            return res.toString().trim();
+        }
+    }
 }
