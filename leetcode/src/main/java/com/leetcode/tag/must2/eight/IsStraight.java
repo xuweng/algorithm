0760ddb63f47d3e3595e6efc7d5b7ebfbd04f59e
xuweng <2028403305@qq.com>
2021-05-03 10:59:15 +0800

    判断重复
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must2/eight/IsStraight.java
+++ leetcode/src/main/java/com/leetcode/tag/must2/eight/IsStraight.java
@@ -10,8 +10,13 @@ import java.util.Set;
  * 溢出：最大值 / 10, 最值 / 10
  * <p>
  * 负数取模 负数取模 负数取模
+ * <p>
+ * 判断重复
  */
 public class IsStraight {
+    /**
+     * 方法一： 集合 Set + 遍历
+     */
     class Solution {
         public boolean isStraight(int[] nums) {
             Set<Integer> repeat = new HashSet<>();
@@ -38,6 +43,9 @@ public class IsStraight {
         }
     }
 
+    /**
+     * 方法二：排序 + 遍历
+     */
     class Solution1 {
         public boolean isStraight(int[] nums) {
             int joker = 0;
@@ -54,6 +62,41 @@ public class IsStraight {
                 }
             }
             return nums[4] - nums[joker] < 5; // 最大牌 - 最小牌 < 5 则可构成顺子
+        }
+    }
+
+    /**
+     * 1.假如没有大小王的情况下，只需要这5个数字不能有重复的，并且这5个数字中的最大值减去最小值等于4就行了。类似于[a,a+1,a+2,a+3,a+4]。
+     * <p>
+     * 2.假如有大小王的情况下，无论有一个还是有两个，我们只需要让大小王替换上面数组[a,a+1,a+2,a+3,a+4]中的任意元素，
+     * <p>
+     * 也能构成顺子。比如替换a,类似于[0，a+1,a+2,a+3,a+4]，那么这个数组中的最大值减去最小值就是3了。
+     * <p>
+     * 所以我们可以得出结论
+     * <p>
+     * 只要数组中的最大值减去最小值小于等于4
+     * 数组中的元素除了大小王不能有重复的
+     */
+    class Solution2 {
+        public boolean isStraight(int[] nums) {
+            //先对数组进行排序
+            Arrays.sort(nums);
+            //记录大小王的数量
+            int zero = 0;
+            for (int i = 0; i < 4; i++) {
+                //统计大小王的数量
+                if (nums[i] == 0) {
+                    zero++;
+                    continue;
+                }
+                //如果不是大小王，不能有重复的
+                if (nums[i] == nums[i + 1]) {
+                    return false;
+                }
+            }
+            //最大牌和最小牌的差值小于5（这里zero是大小王是数量，
+            // nums[zero]表示排序后第一个非大小王的牌）
+            return nums[nums.length - 1] - nums[zero] < 5;
         }
     }
 }
