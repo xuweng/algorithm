d4add84df02c25b4660daa200d4c1035fbef886c
xuweng <2028403305@qq.com>
2021-05-03 23:13:32 +0800

    剑指 Offer 04. 二维数组中的查找
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/eight/FindNumberIn2DArray.java
@@ -0,0 +1,30 @@
+package com.leetcode.tag.must2.eight;
+
+/**
+ * 剑指 Offer 04. 二维数组中的查找
+ * <p>
+ * 滚动数组 滚动数组 滚动数组
+ * <p>
+ * 不合法状态 不合法状态 不合法状态
+ */
+public class FindNumberIn2DArray {
+    class Solution {
+        public boolean findNumberIn2DArray(int[][] matrix, int target) {
+            return fen(matrix, target, 0, matrix[0].length);
+        }
+
+        private boolean fen(int[][] matrix, int target, int i, int j) {
+            // 越界
+            if (i >= matrix.length || j < 0) {
+                return false;
+            }
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            if (matrix[i][j] > target) {
+                return fen(matrix, target, i, j - 1);
+            }
+            return fen(matrix, target, i + 1, j);
+        }
+    }
+}
