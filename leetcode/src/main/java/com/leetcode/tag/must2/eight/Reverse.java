421c158c88a43053b41ade9cb1a24795976748ce
xuweng <2028403305@qq.com>
2021-05-03 10:35:17 +0800

    负数取模结果是负数
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must2/eight/Reverse.java
+++ leetcode/src/main/java/com/leetcode/tag/must2/eight/Reverse.java
@@ -4,7 +4,7 @@ package com.leetcode.tag.must2.eight;
  * 7. 整数反转
  */
 public class Reverse {
-    class Solution {
+    static class Solution {
         /**
          * x <= 1463847412
          *
@@ -22,6 +22,10 @@ public class Reverse {
                 if (rev < Integer.MIN_VALUE / 10 || rev > Integer.MAX_VALUE / 10) {
                     return 0;
                 }
+                // 负数取模结果是负数
+                // -123 % 10 = -3  0*10-3 =-3
+                // -12 % 10= -2   -3*10-2 =-32
+                // -1 % 10 = -1   -32*10-1 =-321
                 // 末尾数字 digit
                 int digit = x % 10;
                 rev = rev * 10 + digit;
@@ -29,5 +33,10 @@ public class Reverse {
             }
             return rev;
         }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.reverse(-123);
     }
 }
