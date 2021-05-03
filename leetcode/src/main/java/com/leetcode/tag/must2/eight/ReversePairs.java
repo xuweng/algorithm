60e1b5545890714832354ebd60353ad860e1a96c
xuweng <2028403305@qq.com>
2021-05-03 23:07:32 +0800

    剑指 Offer 51. 数组中的逆序对
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/eight/ReversePairs.java
@@ -0,0 +1,54 @@
+package com.leetcode.tag.must2.eight;
+
+/**
+ * 剑指 Offer 51. 数组中的逆序对
+ */
+public class ReversePairs {
+    class Solution {
+        int count;
+
+        public int reversePairs(int[] nums) {
+            if (nums.length < 2) {
+                return 0;
+            }
+
+            merge(nums, 0, nums.length - 1);
+
+            return count;
+        }
+
+        private int[] merge(int[] nums, int left, int right) {
+            if (left >= right) {
+                return new int[]{nums[left]};
+            }
+            int mid = left + (right - left) / 2;
+            int[] mergeLeft = merge(nums, left, mid);
+            int[] mergeRight = merge(nums, mid + 1, right);
+            int[] result = new int[mergeLeft.length + mergeRight.length];
+
+            // 新数组 下标从0开始
+            int i = 0;
+            int j = 0;
+            int r = 0;
+
+            while (i < mergeLeft.length && j < mergeRight.length) {
+                if (mergeLeft[i] <= mergeRight[j]) {
+                    result[r++] = mergeLeft[i++];
+                } else {
+                    // i和i的后面都比j大
+                    count += mergeLeft.length - i;
+                    result[r++] = mergeRight[j++];
+                }
+            }
+
+            while (i < mergeLeft.length) {
+                result[r++] = mergeLeft[i++];
+            }
+            while (j < mergeRight.length) {
+                result[r++] = mergeRight[j++];
+            }
+
+            return result;
+        }
+    }
+}
