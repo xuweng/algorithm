8ece0d8032e80b97ff8c1c21223a27e823cd75e1
xuweng <2028403305@qq.com>
2021-05-03 22:56:44 +0800

    滚动数组
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must2/eight/MaxProfit5.java
+++ leetcode/src/main/java/com/leetcode/tag/must2/eight/MaxProfit5.java
@@ -21,4 +21,35 @@ public class MaxProfit5 {
             return Math.max(dp[prices.length - 1][0], dp[prices.length - 1][2]);
         }
     }
+
+    /**
+     * 滚动数组
+     */
+    class Solution1 {
+        public int maxProfit(int[] prices) {
+            if (prices.length == 0) {
+                return 0;
+            }
+
+            int n = prices.length;
+            int f0 = 0;
+            // 有股票
+            int f1 = -prices[0];
+            int f2 = 0;
+            for (int i = 1; i < n; ++i) {
+                // 冷冻期 没有股票
+                int newf0 = f1 + prices[i];
+                // 不是冷冻期 有股票
+                int newf1 = Math.max(f1, f2 - prices[i]);
+                // 不是冷冻期 没有股票
+                int newf2 = Math.max(f2, f0);
+
+                f0 = newf0;
+                f1 = newf1;
+                f2 = newf2;
+            }
+
+            return Math.max(f0, f2);
+        }
+    }
 }
