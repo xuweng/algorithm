5a6f95abfc15a5b451387e8f8113171fdabc35e3
xuweng <2028403305@qq.com>
2021-05-03 21:49:30 +0800

    方法二：贪心
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must2/eight/MaxProfit1.java
+++ leetcode/src/main/java/com/leetcode/tag/must2/eight/MaxProfit1.java
@@ -43,4 +43,18 @@ public class MaxProfit1 {
             return dp0;
         }
     }
+
+    /**
+     * 方法二：贪心
+     */
+    class Solution2 {
+        public int maxProfit(int[] prices) {
+            int ans = 0;
+            int n = prices.length;
+            for (int i = 1; i < n; ++i) {
+                ans += Math.max(0, prices[i] - prices[i - 1]);
+            }
+            return ans;
+        }
+    }
 }
