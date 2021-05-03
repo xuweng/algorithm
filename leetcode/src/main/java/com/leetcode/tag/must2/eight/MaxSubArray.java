8c047a87976d3c630b3f288bc527fd1bbe8634c5
xuweng <2028403305@qq.com>
2021-05-03 11:23:14 +0800

    53. 最大子序和
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/eight/MaxSubArray.java
@@ -0,0 +1,21 @@
+package com.leetcode.tag.must2.eight;
+
+/**
+ * 53. 最大子序和
+ */
+public class MaxSubArray {
+    class Solution {
+        public int maxSubArray(int[] nums) {
+            int[] dp = new int[nums.length];
+            dp[0] = nums[0];
+            int max = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                dp[i] = Math.max(dp[i - 1], 0) + nums[i];
+
+                max = Math.max(max, dp[i]);
+            }
+
+            return max;
+        }
+    }
+}
