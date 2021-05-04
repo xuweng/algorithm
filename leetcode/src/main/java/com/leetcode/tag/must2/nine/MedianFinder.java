c70ad5a4600a78d48ef0b003afc2d0c7b394f9a7
xuweng <2028403305@qq.com>
2021-05-04 22:02:19 +0800

    剑指 Offer 41. 数据流中的中位数
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/nine/MedianFinder.java
@@ -0,0 +1,52 @@
+package com.leetcode.tag.must2.nine;
+
+import java.util.PriorityQueue;
+
+/**
+ * 剑指 Offer 41. 数据流中的中位数
+ * <p>
+ * 单调 单调 单调
+ * <p>
+ * 长度 长度 长度
+ * <p>
+ * 结尾 结尾 结尾
+ */
+public class MedianFinder {
+    /**
+     * 当前大顶堆和小顶堆的元素个数之和
+     */
+    private int count;
+    /**
+     * 最大堆的堆顶元素，小于或者等于最小堆的堆顶元素
+     * 最大堆的元素个数或者与最小堆的元素个数相等，或者多 1 相等就是偶数，多1就是奇数
+     */
+    private final PriorityQueue<Integer> maxheap;
+    private final PriorityQueue<Integer> minheap;
+
+    /**
+     * initialize your data structure here.
+     */
+    public MedianFinder() {
+        count = 0;
+        maxheap = new PriorityQueue<>((x, y) -> y - x);
+        minheap = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        count++;
+        maxheap.offer(num);
+        minheap.offer(maxheap.poll());
+
+        if (count % 2 != 0) {
+            maxheap.offer(minheap.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (count % 2 == 0) {
+            return (double) (maxheap.peek() + minheap.peek()) / 2;
+        }
+
+        return maxheap.peek();
+    }
+}
