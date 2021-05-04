5dc2f0f8067377b0ecf68684549ee6af97422847
xuweng <2028403305@qq.com>
2021-05-04 20:57:03 +0800

    1473. 粉刷房子 III
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must2/nine/MinCost.java
+++ leetcode/src/main/java/com/leetcode/tag/must2/nine/MinCost.java
@@ -18,11 +18,16 @@ public class MinCost {
                 for (int j = 1; j <= n; j++) {
                     // 枚举街区
                     for (int k = 1; k <= target; k++) {
+                        // 形成分区数量大于房子数量，状态无效 必须
+                        if (k > i) {
+                            dp[i][j][k] = Integer.MAX_VALUE / 2;
+                            continue;
+                        }
                         // 房子没有上色
                         if (houses[i - 1] == 0) {
                             // 新街区
                             int temp = Integer.MAX_VALUE / 2;
-                            for (int i1 = 0; i1 < n; i1++) {
+                            for (int i1 = 1; i1 <= n; i1++) {
                                 if (i1 != j) {
                                     temp = Math.min(temp, dp[i - 1][i1][k - 1]);
                                 }
@@ -32,7 +37,7 @@ public class MinCost {
                             // 房子已经上色
                             if (j == houses[i - 1]) {
                                 int temp = Integer.MAX_VALUE / 2;
-                                for (int i1 = 0; i1 < n; i1++) {
+                                for (int i1 = 0; i1 <= n; i1++) {
                                     if (i1 != j) {
                                         temp = Math.min(temp, dp[i - 1][i1][k - 1]);
                                     }
