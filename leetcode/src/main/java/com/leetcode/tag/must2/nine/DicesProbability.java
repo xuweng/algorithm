cc823af1414cc7a82068fd9376343db08eae4eb0
xuweng <2028403305@qq.com>
2021-05-04 21:16:50 +0800

    初始化 枚举范围
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must2/nine/DicesProbability.java
+++ leetcode/src/main/java/com/leetcode/tag/must2/nine/DicesProbability.java
@@ -11,6 +11,7 @@ public class DicesProbability {
     class Solution {
         public double[] dicesProbability(int n) {
             int[][] dp = new int[n + 1][6 * n + 1];
+            // 初始化
             // 投掷完 第1 枚骰子后，它的可能点数分别为 1, 2, 3, ... , 6，并且每个点数出现的次数都是 1
             for (int i = 1; i <= 6; i++) {
                 dp[1][i] = 1;
@@ -28,6 +29,7 @@ public class DicesProbability {
             // P(k) = k出现的次数 / 总次数
             // 投掷 n 个骰子，所有点数出现的总次数是 6^n ，因为一共有 n 枚骰子，每枚骰子的点数都有 6 种可能出现的情况。
             double[] ans = new double[6 * n - n + 1];
+            // 枚举范围
             for (int i = n; i <= 6 * n; i++) {
                 ans[i - n] = ((double) dp[n][i]) / (Math.pow(6, n));
             }
