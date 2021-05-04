85c515c92d55b72db8799abf8cfc3fed48290145
xuweng <2028403305@qq.com>
2021-05-04 20:51:30 +0800

    1473. 粉刷房子 III
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must2/nine/MinCost.java
@@ -0,0 +1,57 @@
+package com.leetcode.tag.must2.nine;
+
+/**
+ * 1473. 粉刷房子 III
+ */
+public class MinCost {
+    class Solution {
+        public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
+            int[][][] dp = new int[m + 1][n + 1][target + 1];
+            for (int i = 0; i <= m; i++) {
+                for (int j = 0; j <= n; j++) {
+                    dp[i][j][0] = Integer.MAX_VALUE / 2;
+                }
+            }
+            // 枚举房子
+            for (int i = 1; i <= m; i++) {
+                // 枚举颜色
+                for (int j = 1; j <= n; j++) {
+                    // 枚举街区
+                    for (int k = 1; k <= target; k++) {
+                        // 房子没有上色
+                        if (houses[i - 1] == 0) {
+                            // 新街区
+                            int temp = Integer.MAX_VALUE / 2;
+                            for (int i1 = 0; i1 < n; i1++) {
+                                if (i1 != j) {
+                                    temp = Math.min(temp, dp[i - 1][i1][k - 1]);
+                                }
+                            }
+                            dp[i][j][k] = Math.min(dp[i - 1][j][k], temp) + cost[i - 1][j - 1];
+                        } else {
+                            // 房子已经上色
+                            if (j == houses[i - 1]) {
+                                int temp = Integer.MAX_VALUE / 2;
+                                for (int i1 = 0; i1 < n; i1++) {
+                                    if (i1 != j) {
+                                        temp = Math.min(temp, dp[i - 1][i1][k - 1]);
+                                    }
+                                }
+                                dp[i][j][k] = Math.min(dp[i - 1][j][k], temp);
+                            } else {
+                                dp[i][j][k] = Integer.MAX_VALUE / 2;
+                            }
+                        }
+                    }
+                }
+            }
+
+            int result = Integer.MAX_VALUE / 2;
+            for (int i = 1; i <= n; i++) {
+                result = Math.min(result, dp[m][i][target]);
+            }
+
+            return result;
+        }
+    }
+}
