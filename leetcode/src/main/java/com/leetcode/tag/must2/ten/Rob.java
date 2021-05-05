1c0758b72b15948ff2d7fb230c48e71b3bcf84fd
xuweng <2028403305@qq.com>
2021-05-05 21:28:50 +0800

    198. 打家劫舍
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must2/ten/Rob.java
+++ leetcode/src/main/java/com/leetcode/tag/must2/ten/Rob.java
@@ -27,4 +27,24 @@ public class Rob {
             return dp[nums.length - 1];
         }
     }
+
+    class Solution1 {
+        public int rob(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return 0;
+            }
+            int length = nums.length;
+            if (length == 1) {
+                return nums[0];
+            }
+            int first = nums[0];
+            int second = Math.max(nums[0], nums[1]);
+            for (int i = 2; i < length; i++) {
+                int temp = second;
+                second = Math.max(first + nums[i], second);
+                first = temp;
+            }
+            return second;
+        }
+    }
 }
