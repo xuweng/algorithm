ac0a308d2aa784c968bb1286f3835bfa06375858
xuweng <2028403305@qq.com>
2021-05-06 21:52:10 +0800

    740. 删除并获得点数
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must3/DeleteAndEarn.java
@@ -0,0 +1,28 @@
+package com.leetcode.tag.must3;
+
+import java.util.Arrays;
+
+/**
+ * 740. 删除并获得点数
+ */
+public class DeleteAndEarn {
+    class Solution {
+        public int deleteAndEarn(int[] nums) {
+            int max = Arrays.stream(nums).max().getAsInt();
+            // 计数
+            int[] count = new int[max + 1];
+            for (int num : nums) {
+                count[num]++;
+            }
+            int[] dp = new int[max + 1];
+            dp[1] = count[1];
+
+            for (int i = 2; i <= max; i++) {
+                // 相邻不能同时选择
+                dp[i] = Math.max(dp[i - 1], dp[i - 2] + i * count[i]);
+            }
+
+            return dp[max];
+        }
+    }
+}
