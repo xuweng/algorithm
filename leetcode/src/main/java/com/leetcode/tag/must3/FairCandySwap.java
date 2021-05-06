35ab7e1a804701b69f37fe9a83647212c578d647
xuweng <2028403305@qq.com>
2021-05-06 21:34:21 +0800

    888. 公平的糖果棒交换
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must3/FairCandySwap.java
@@ -0,0 +1,31 @@
+package com.leetcode.tag.must3;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * 888. 公平的糖果棒交换
+ */
+public class FairCandySwap {
+    class Solution {
+        public int[] fairCandySwap(int[] A, int[] B) {
+            // sumA-a+b=sumB-b+a
+            // sumA-sumB=2a-2b
+            // (sumA-sumB)/2=a-b
+            int sumA = Arrays.stream(A).sum();
+            int sumB = Arrays.stream(B).sum();
+            int cha = (sumA - sumB) / 2;
+            Set<Integer> set = Arrays.stream(A).boxed().collect(Collectors.toSet());
+
+            for (int b : B) {
+                int a = b + cha;
+                if (set.contains(a)) {
+                    return new int[]{a, b};
+                }
+            }
+
+            return A;
+        }
+    }
+}
