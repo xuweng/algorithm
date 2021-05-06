bc19dbe0836c0659f51442741e97d5c5cbf9ca0b
xuweng <2028403305@qq.com>
2021-05-06 21:17:46 +0800

    不需要新建结点
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must3/MergeKLists.java
+++ leetcode/src/main/java/com/leetcode/tag/must3/MergeKLists.java
@@ -51,6 +51,40 @@ public class MergeKLists {
         }
     }
 
+    class Solution1 {
+        public ListNode mergeKLists(ListNode[] lists) {
+            if (lists == null || lists.length == 0) {
+                return null;
+            }
+            return merge(lists, 0, lists.length - 1);
+        }
+
+        private ListNode merge(ListNode[] lists, int left, int right) {
+            if (left >= right) {
+                return lists[left];
+            }
+            int mid = left + (right - left) / 2;
+
+            return merge(merge(lists, left, mid), merge(lists, mid + 1, right));
+        }
+
+        private ListNode merge(ListNode l1, ListNode l2) {
+            if (l1 == null) {
+                return l2;
+            }
+            if (l2 == null) {
+                return l1;
+            }
+            // 不需要新建结点
+            if (l1.val <= l2.val) {
+                l1.next = merge(l1.next, l2);
+                return l1;
+            }
+            l2.next = merge(l1, l2.next);
+            return l2;
+        }
+    }
+
     class ListNode {
         int val;
         ListNode next;
