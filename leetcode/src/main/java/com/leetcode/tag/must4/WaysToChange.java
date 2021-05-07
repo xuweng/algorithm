80f9b8be9dbe89b92638c792e34b15ac7ba1c472
xuweng <2028403305@qq.com>
2021-05-07 22:09:13 +0800

    面试题 08.11. 硬币
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must4/WaysToChange.java
@@ -0,0 +1,26 @@
+package com.leetcode.tag.must4;
+
+/**
+ * 面试题 08.11. 硬币
+ * <p>
+ * 移动指针 移动指针 移动指针
+ * <p>
+ * 2数和 2数和 2数和
+ */
+public class WaysToChange {
+    class Solution {
+        public int waysToChange(int n) {
+            int[] coins = new int[]{25, 10, 5, 1};
+            int[] dp = new int[n + 1];
+            dp[0] = 1;
+
+            for (int coin : coins) {
+                for (int i = coin; i <= n; i++) {
+                    dp[i] += dp[i - coin];
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
