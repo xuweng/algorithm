f3a08252b6884c9d92e5f29fc266c07f054e822a
xuweng <2028403305@qq.com>
2021-05-09 16:33:49 +0800

    剑指 Offer 05. 替换空格
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/three/ReplaceSpace.java
@@ -0,0 +1,33 @@
+package com.leetcode.tag.contest.three;
+
+/**
+ * 剑指 Offer 05. 替换空格
+ */
+public class ReplaceSpace {
+    class Solution {
+        public String replaceSpace(String s) {
+            char[] chars = s.toCharArray();
+            int count = 0;
+            for (char aChar : chars) {
+                if (aChar == ' ') {
+                    count++;
+                }
+            }
+
+            int len = chars.length + 3 * count;
+            char[] result = new char[len];
+            int index = len - 1;
+            for (int i = chars.length - 1; i >= 0; i--) {
+                if (chars[i] == ' ') {
+                    result[index--] = '0';
+                    result[index--] = '2';
+                    result[index--] = '%';
+                } else {
+                    result[index--] = chars[i];
+                }
+            }
+
+            return new String(result);
+        }
+    }
+}
