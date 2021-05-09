3b0a88ee0053c68f4e57f3516a1b85bc3242ede8
xuweng <2028403305@qq.com>
2021-05-09 11:10:29 +0800

    5751. 下标对中的最大距离
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/contest/three/MaxDistance.java
+++ leetcode/src/main/java/com/leetcode/tag/contest/three/MaxDistance.java
@@ -1,5 +1,10 @@
 package com.leetcode.tag.contest.three;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 5751. 下标对中的最大距离
  */
@@ -13,6 +18,24 @@ public class MaxDistance {
                         result = Math.max(result, j - i);
                     }
                 }
+            }
+
+            return result;
+        }
+    }
+
+    class Solution1 {
+        public int maxDistance(int[] nums1, int[] nums2) {
+            int result = 0;
+            Map<Integer, List<Integer>> map = new HashMap<>();
+            Map<Integer, List<Integer>> map1 = new HashMap<>();
+
+            for (int i = 0; i < nums1.length; i++) {
+                map.computeIfAbsent(nums1[i], v -> new ArrayList<>()).add(i);
+            }
+
+            for (int i = 0; i < nums2.length; i++) {
+                map1.computeIfAbsent(nums2[i], v -> new ArrayList<>()).add(i);
             }
 
             return result;
