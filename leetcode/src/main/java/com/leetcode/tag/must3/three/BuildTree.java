7dbe549fd5e66a7a34e06ad005e51390db545569
xuweng <2028403305@qq.com>
2021-05-09 11:35:24 +0800

    剑指 Offer 07. 重建二叉树
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must3/three/BuildTree.java
@@ -0,0 +1,44 @@
+package com.leetcode.tag.must3.three;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 剑指 Offer 07. 重建二叉树
+ */
+public class BuildTree {
+    class Solution {
+        int index;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        public TreeNode buildTree(int[] preorder, int[] inorder) {
+            for (int i = 0; i < inorder.length; i++) {
+                map.put(inorder[i], i);
+            }
+
+            return dfs(preorder, 0, preorder.length - 1);
+        }
+
+        private TreeNode dfs(int[] preorder, int i, int j) {
+            if (i > j) {
+                return null;
+            }
+            TreeNode root = new TreeNode(preorder[index]);
+            Integer integer = map.get(index++);
+            root.left = dfs(preorder, i, integer - 1);
+            root.right = dfs(preorder, integer + 1, j);
+
+            return root;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
