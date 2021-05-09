fc5a51fe8605001861f87a915405fceb3c6a0a42
xuweng <2028403305@qq.com>
2021-05-09 22:17:15 +0800

    剑指 Offer 37. 序列化二叉树
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must3/three/Codec.java
@@ -0,0 +1,55 @@
+package com.leetcode.tag.must3.three;
+
+/**
+ * 剑指 Offer 37. 序列化二叉树
+ * <p>
+ * 配匹完 配匹完 配匹完 二分 二分 二分
+ * <p>
+ * 二分 二分 二分
+ * <p>
+ * 归并 归并 归并
+ * <p>
+ * 快排 快排 快排
+ * <p>
+ * head head head
+ */
+public class Codec {
+    int root;
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return "#";
+        }
+
+        return root.val + "," + serialize(root.left) + "," + serialize(root.right);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] split = data.split(",");
+
+        return dfs(split);
+    }
+
+    private TreeNode dfs(String[] split) {
+        if ("#".equals(split[root])) {
+            return null;
+        }
+        TreeNode node = new TreeNode(Integer.parseInt(split[root++]));
+        node.left = dfs(split);
+        node.right = dfs(split);
+
+        return node;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
