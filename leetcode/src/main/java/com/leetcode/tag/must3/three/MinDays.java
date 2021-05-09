59723c651eddeb7d6fc01dd9d1c253a80d573a7a
xuweng <2028403305@qq.com>
2021-05-09 09:23:44 +0800

    方法一：二分查找
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must3/three/MinDays.java
+++ leetcode/src/main/java/com/leetcode/tag/must3/three/MinDays.java
@@ -8,12 +8,16 @@ package com.leetcode.tag.must3.three;
 public class MinDays {
     /**
      * 方法一：二分查找
+     * <p>
+     * 制作花朵最少的时间必然是 bloomDay 数组中开花所用天数最少的那朵花 min(bloomDay)
+     * 制作花朵最多的时间也只能是 bloomDay 数组中开花所需天数最多的那朵花 max(bloomDay)
+     * 寻找制作花束的最少天数必然落在上面所说的区间里 [min(bloomDay),max(bloomDay)], 连续的一个正整数区间, 因此可以通过二分查找来提升查找效率!
      */
     class Solution {
         public int minDays(int[] bloomDay, int m, int k) {
             // 每束花需要 k 朵花，需要制作 m 束花，因此一共需要 k×m 朵花
             if (k * m > bloomDay.length) {
-                // 花园中的花朵数量少于nk×m，即数组 bloomDay 的长度小于 k×m，则无法制作出指定数量的花束
+                // 花园中的花朵数量少于 k×m，即数组 bloomDay 的长度小于 k×m，则无法制作出指定数量的花束
                 return -1;
             }
             int low = 1, high = 1;
