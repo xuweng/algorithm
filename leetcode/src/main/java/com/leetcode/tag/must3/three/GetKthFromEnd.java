14e3cfa88b36f57eb5b4ac0085452e0621f5346a
xuweng <2028403305@qq.com>
2021-05-09 17:00:42 +0800

    剑指 Offer 22. 链表中倒数第k个节点
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must3/three/GetKthFromEnd.java
@@ -0,0 +1,30 @@
+package com.leetcode.tag.must3.three;
+
+/**
+ * 剑指 Offer 22. 链表中倒数第k个节点
+ */
+public class GetKthFromEnd {
+    class Solution {
+        public ListNode getKthFromEnd(ListNode head, int k) {
+            ListNode fast = head;
+            for (int i = 0; i < k; i++) {
+                fast = fast.next;
+            }
+            while (fast != null) {
+                head = head.next;
+                fast = fast.next;
+            }
+
+            return head;
+        }
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
