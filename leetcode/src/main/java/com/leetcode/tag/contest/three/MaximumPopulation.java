453533b3b90e8461ce695883b4b9e5f66c48792d
xuweng <2028403305@qq.com>
2021-05-09 10:49:41 +0800

    5750. 人口最多的年份
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/three/MaximumPopulation.java
@@ -0,0 +1,31 @@
+package com.leetcode.tag.contest.three;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 5750. 人口最多的年份
+ */
+public class MaximumPopulation {
+    class Solution {
+        public int maximumPopulation(int[][] logs) {
+            Arrays.stream(logs).sorted(Comparator.comparing(l -> l[0]));
+
+            int max = 1;
+            int max1 = 1;
+            int result = logs[0][0];
+            int last = 0;
+            for (int i = 1; i < logs.length; i++) {
+                if (logs[i][0] < logs[last][1]) {
+                    max++;
+                } else {
+                    max = 1;
+                    last = i;
+                }
+                result = max1 < max ? logs[last][0] : logs[i][0];
+            }
+
+            return result;
+        }
+    }
+}
