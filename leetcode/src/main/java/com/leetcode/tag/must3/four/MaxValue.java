327e7fde63a6e0e4919c41faa43b9c03392ec0a5
xuweng <2028403305@qq.com>
2021-05-10 22:56:33 +0800

    剑指 Offer 47. 礼物的最大价值
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must3/four/MaxValue.java
@@ -0,0 +1,31 @@
+package com.leetcode.tag.must3.four;
+
+/**
+ * 剑指 Offer 47. 礼物的最大价值
+ */
+public class MaxValue {
+    class Solution {
+        public int maxValue(int[][] grid) {
+            int m = grid.length;
+            int n = grid[0].length;
+
+            int[] dp = new int[n];
+            dp[0] = grid[0][0];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i > 0 && j > 0) {
+                        dp[j] = Math.max(dp[j - 1], dp[j]) + grid[i][j];
+                    } else if (i > 0) {
+                        // 上一行
+                        dp[j] = dp[j] + grid[i][j];
+                    } else if (j > 0) {
+                        // 左一列
+                        dp[j] = dp[j - 1] + grid[i][j];
+                    }
+                }
+            }
+
+            return dp[n - 1];
+        }
+    }
+}
