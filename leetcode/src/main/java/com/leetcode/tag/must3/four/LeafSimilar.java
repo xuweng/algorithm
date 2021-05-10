58a31545cc38a1c56e082ab5c1058f3b7d633a6c
xuweng <2028403305@qq.com>
2021-05-10 21:37:51 +0800

    872. 叶子相似的树
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must3/four/LeafSimilar.java
@@ -0,0 +1,50 @@
+package com.leetcode.tag.must3.four;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 872. 叶子相似的树
+ */
+public class LeafSimilar {
+    class Solution {
+        public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+            List<Integer> list = new ArrayList<>();
+            List<Integer> list1 = new ArrayList<>();
+
+            dfs(root1, list);
+            dfs(root2, list1);
+
+            return list.equals(list1);
+        }
+
+        private void dfs(TreeNode root, List<Integer> list) {
+            if (root == null) {
+                return;
+            }
+            list.add(root.val);
+
+            dfs(root.left, list);
+            dfs(root.right, list);
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
