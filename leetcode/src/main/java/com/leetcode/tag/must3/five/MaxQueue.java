559d5ae50bd681cb0262b41853d1a6a0f7340e0e
xuweng <2028403305@qq.com>
2021-05-12 21:26:18 +0800

    剑指 Offer 59 - II. 队列的最大值
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must3/five/MaxQueue.java
@@ -0,0 +1,47 @@
+package com.leetcode.tag.must3.five;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 剑指 Offer 59 - II. 队列的最大值
+ */
+public class MaxQueue {
+    Deque<Integer> deque;
+    Deque<Integer> max;
+
+    public MaxQueue() {
+        deque = new LinkedList<>();
+        max = new LinkedList<>();
+    }
+
+    public int max_value() {
+        if (max.isEmpty()) {
+            return -1;
+        }
+
+        return max.peekFirst();
+    }
+
+    public void push_back(int value) {
+        deque.offerLast(value);
+        // 单调递减队列
+        while (!max.isEmpty() && max.peekLast() < value) {
+            max.pollLast();
+        }
+        max.offerLast(value);
+    }
+
+    public int pop_front() {
+        if (deque.isEmpty()) {
+            return -1;
+        }
+        Integer integer = deque.pollFirst();
+        if (integer.equals(max.peekFirst())) {
+            max.pollFirst();
+        }
+
+        return integer;
+    }
+}
+
