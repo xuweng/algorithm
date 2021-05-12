2685aab6d990a6e4b4c32940cad37aa7ef2b549b
xuweng <2028403305@qq.com>
2021-05-12 21:11:37 +0800

    剑指 Offer 32 - I. 从上到下打印二叉树
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must3/five/LevelOrder.java
@@ -0,0 +1,48 @@
+package com.leetcode.tag.must3.five;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 剑指 Offer 32 - I. 从上到下打印二叉树
+ */
+public class LevelOrder {
+    class Solution {
+        public int[] levelOrder(TreeNode root) {
+            if (root == null) {
+                return new int[0];
+            }
+            Deque<TreeNode> deque = new LinkedList<>();
+            deque.offerLast(root);
+
+            List<Integer> result = new ArrayList<>();
+            while (!deque.isEmpty()) {
+                for (int i = 0; i < deque.size(); i++) {
+                    TreeNode treeNode = deque.pollFirst();
+                    result.add(treeNode.val);
+                    if (treeNode.left != null) {
+                        deque.offerLast(treeNode.left);
+                    }
+                    if (treeNode.right != null) {
+                        deque.offerLast(treeNode.right);
+                    }
+                }
+            }
+
+            return result.stream().mapToInt(Integer::intValue).toArray();
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+}
