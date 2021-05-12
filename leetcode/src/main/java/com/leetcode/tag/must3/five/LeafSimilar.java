e4a9d62ee2a805b05797d85d5c8f0ed02512eabf
xuweng <2028403305@qq.com>
2021-05-12 22:28:52 +0800

    872. 叶子相似的树
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must3/five/LeafSimilar.java
+++ leetcode/src/main/java/com/leetcode/tag/must3/five/LeafSimilar.java
@@ -1,6 +1,8 @@
 package com.leetcode.tag.must3.five;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -22,9 +24,40 @@ public class LeafSimilar {
             if (root == null) {
                 return;
             }
-            list.add(root.val);
+            if (root.left == null && root.right == null) {
+                list.add(root.val);
+            }
             dfs(root.left, list);
             dfs(root.right, list);
+        }
+    }
+
+    class Solution1 {
+        public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+            List<Integer> list1 = new ArrayList<>();
+            List<Integer> list2 = new ArrayList<>();
+
+            dfs(root1, list1);
+            dfs(root2, list2);
+
+            return list1.equals(list2);
+        }
+
+        private void dfs(TreeNode root, List<Integer> list) {
+            Deque<TreeNode> deque = new LinkedList<>();
+            while (root != null || !deque.isEmpty()) {
+                while (root != null) {
+                    // left全部入栈
+                    deque.push(root);
+                    root = root.left;
+                }
+                TreeNode pop = deque.pop();
+                if (pop.left == null && pop.right == null) {
+                    list.add(pop.val);
+                }
+                // 切换到right
+                root = pop.right;
+            }
         }
     }
 
