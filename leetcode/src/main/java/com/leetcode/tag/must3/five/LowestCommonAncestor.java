1728f0d96aa04d327822c0f004bf0f631949b410
xuweng <2028403305@qq.com>
2021-05-12 20:54:27 +0800

    剑指 Offer 68 - II. 二叉树的最近公共祖先
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must3/five/LowestCommonAncestor.java
+++ leetcode/src/main/java/com/leetcode/tag/must3/five/LowestCommonAncestor.java
@@ -18,6 +18,24 @@ public class LowestCommonAncestor {
         }
     }
 
+    class Solution1 {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null || root == p || root == q) {
+                return root;
+            }
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            if (left == null) {
+                return lowestCommonAncestor(root.right, p, q);
+            }
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+            if (right == null) {
+                return left;
+            }
+
+            return root;
+        }
+    }
+
     class TreeNode {
         int val;
         TreeNode left;
