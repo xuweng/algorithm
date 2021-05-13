53084a42765191dfcd1ed4b7caeba7cca43d19a3
xuweng <2028403305@qq.com>
2021-05-13 20:48:37 +0800

    50. Pow(x, n)
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must3/six/MyPow.java
@@ -0,0 +1,26 @@
+package com.leetcode.tag.must3.six;
+
+/**
+ * 50. Pow(x, n)
+ */
+public class MyPow {
+    class Solution {
+        public double myPow(double x, int n) {
+            long N = n;
+
+            return n >= 0 ? pow(x, N) : 1 / pow(x, -N);
+        }
+
+        private double pow(double x, long n) {
+            if (x == 1 || n == 0) {
+                return 1.0;
+            }
+            if (n == 1) {
+                return x;
+            }
+            double mid = pow(x, n / 2);
+
+            return n % 2 == 0 ? mid * mid : mid * mid * x;
+        }
+    }
+}
