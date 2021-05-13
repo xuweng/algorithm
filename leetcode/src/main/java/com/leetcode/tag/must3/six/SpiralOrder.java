6c4c07d6008a0325dbb589e854e1f4aef3b3c52b
xuweng <2028403305@qq.com>
2021-05-13 20:38:36 +0800

    剑指 Offer 29. 顺时针打印矩阵
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must3/six/SpiralOrder.java
@@ -0,0 +1,46 @@
+package com.leetcode.tag.must3.six;
+
+/**
+ * 剑指 Offer 29. 顺时针打印矩阵
+ */
+public class SpiralOrder {
+    class Solution {
+        public int[] spiralOrder(int[][] matrix) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+            int top = 0;
+            int bu = m - 1;
+            int left = 0;
+            int right = n - 1;
+
+            int[] result = new int[m * n];
+            int start = 0;
+            int end = m * n;
+
+            while (start < end) {
+                // 左到右
+                for (int i = left; i <= right && start < end; i++) {
+                    result[start++] = matrix[top][i];
+                }
+                top++;
+                // 上到下
+                for (int i = top; i <= bu && start < end; i++) {
+                    result[start++] = matrix[i][right];
+                }
+                right--;
+                // 右到左
+                for (int i = right; i >= left && start < end; i--) {
+                    result[start++] = matrix[bu][i];
+                }
+                bu--;
+                // 下到上
+                for (int i = bu; i >= top && start < end; i--) {
+                    result[start++] = matrix[i][left];
+                }
+                left++;
+            }
+
+            return result;
+        }
+    }
+}
