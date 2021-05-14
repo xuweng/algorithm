2d161fb89b195ba52516b888edb231bfec34c7a8
xuweng <2028403305@qq.com>
2021-05-14 21:44:22 +0800

    45. 跳跃游戏 II
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must3/seven/Jump.java
+++ leetcode/src/main/java/com/leetcode/tag/must3/seven/Jump.java
@@ -43,7 +43,7 @@ public class Jump {
             }
             int[] dp = new int[nums.length];
             Arrays.fill(dp, Integer.MAX_VALUE - 1);
-            dp[0] = 1;
+            dp[0] = 0;
 
             for (int i = 1; i < nums.length; i++) {
                 for (int j = 0; j < i; j++) {
