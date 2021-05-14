3b60d8dc9e1ec8045c251803c535cf29d9e4fb4e
xuweng <2028403305@qq.com>
2021-05-14 21:07:56 +0800

    1277. 统计全为 1 的正方形子矩阵
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must3/seven/CountSquares.java
@@ -0,0 +1,31 @@
+package com.leetcode.tag.must3.seven;
+
+/**
+ * 1277. 统计全为 1 的正方形子矩阵
+ */
+public class CountSquares {
+    class Solution {
+        public int countSquares(int[][] matrix) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+            int[][] dp = new int[m][n];
+
+            int count = 0;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][j] == 0) {
+                        continue;
+                    }
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(dp[i][j - 1], Math.min(dp[i - 1][j], dp[i - 1][j - 1])) + 1;
+                    }
+                    count += dp[i][j];
+                }
+            }
+
+            return count;
+        }
+    }
+}
