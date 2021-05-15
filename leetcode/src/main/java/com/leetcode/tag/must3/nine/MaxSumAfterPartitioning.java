7bb9a79e0f42cbb0ea19a689fbb0f0b8a4aaf72b
xuweng <2028403305@qq.com>
2021-05-15 22:28:49 +0800

    倒序
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must3/nine/MaxSumAfterPartitioning.java
+++ leetcode/src/main/java/com/leetcode/tag/must3/nine/MaxSumAfterPartitioning.java
@@ -11,7 +11,24 @@ public class MaxSumAfterPartitioning {
             int[] dp = new int[n + 1];
             for (int i = 0; i <= n; i++) {
                 int max = dp[i];
+                // 倒序
                 for (int j = i - 1; j >= 0 && (i - j) <= K; j--) {
+                    max = Math.max(max, A[j]);
+                    dp[i] = Math.max(dp[i], dp[j] + (i - j) * max);
+                }
+            }
+            return dp[n];
+        }
+    }
+
+    class Solution1 {
+        public int maxSumAfterPartitioning(int[] A, int K) {
+            int n = A.length;
+            // dp[i]：数组的前i个数即nums[0,1...i-1],被切了Y-刀，分割成Y个数组
+            int[] dp = new int[n + 1];
+            for (int i = 0; i <= n; i++) {
+                int max = dp[i];
+                for (int j = 0; j < i && (i - j) <= K; j++) {
                     max = Math.max(max, A[j]);
                     dp[i] = Math.max(dp[i], dp[j] + (i - j) * max);
                 }
