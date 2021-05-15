74579b10563d45a18c71acfc52adcce677e3fa97
xuweng <2028403305@qq.com>
2021-05-15 23:13:34 +0800

    dp[j] 长度为j,下一个下标就是j,[j,i-1]
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must3/nine/MaxSumAfterPartitioning.java
+++ leetcode/src/main/java/com/leetcode/tag/must3/nine/MaxSumAfterPartitioning.java
@@ -8,8 +8,9 @@ public class MaxSumAfterPartitioning {
         public int maxSumAfterPartitioning(int[] A, int K) {
             int n = A.length;
             // dp[i]：数组的前i个数即nums[0,1...i-1],被切了Y-刀，分割成Y个数组
+            // 长度
             int[] dp = new int[n + 1];
-            for (int i = 0; i <= n; i++) {
+            for (int i = 1; i <= n; i++) {
                 int max = 0;
                 // 倒序
                 // 每个数组的个数最大值不超过K
