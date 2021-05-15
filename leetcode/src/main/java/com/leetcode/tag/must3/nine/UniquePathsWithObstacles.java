8014359f93308fe722960892475c30a665259cd2
xuweng <2028403305@qq.com>
2021-05-15 22:00:19 +0800

    63. 不同路径 II
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must3/nine/UniquePathsWithObstacles.java
@@ -0,0 +1,30 @@
+package com.leetcode.tag.must3.nine;
+
+/**
+ * 63. 不同路径 II
+ */
+public class UniquePathsWithObstacles {
+    class Solution {
+        public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+            int m = obstacleGrid.length;
+            int n = obstacleGrid[0].length;
+
+            int[] dp = new int[n];
+            dp[0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (obstacleGrid[i][j] == 1) {
+                        // 置0
+                        dp[j] = 0;
+                        continue;
+                    }
+                    if (j > 0) {
+                        dp[j] += dp[j - 1];
+                    }
+                }
+            }
+
+            return dp[n - 1];
+        }
+    }
+}
