58437508b5ffec340f5309c187a4ec8ee4c1a8eb
xuweng <2028403305@qq.com>
2021-05-15 22:58:31 +0800

    每个数组的个数最大值不超过K
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must3/nine/MaxSumAfterPartitioning.java
+++ leetcode/src/main/java/com/leetcode/tag/must3/nine/MaxSumAfterPartitioning.java
@@ -24,6 +24,26 @@ public class MaxSumAfterPartitioning {
         }
     }
 
+    class Solution3 {
+        public int maxSumAfterPartitioning(int[] A, int K) {
+            int n = A.length;
+            // dp[i]：数组的前i个数即nums[0,1...i-1],被切了Y-刀，分割成Y个数组
+            int[] dp = new int[n];
+            for (int i = 0; i < n; i++) {
+                int max = 0;
+                // 倒序
+                // 每个数组的个数最大值不超过K
+                for (int j = i; j >= 0 && j >= i - K + 1; j--) {
+                    // MAX是 nums[j,i−1] 范围内的局部最大值
+                    max = Math.max(max, A[j]);
+                    // (i - j)个max
+                    dp[i] = Math.max(dp[i], dp[j] + (i - j + 1) * max);
+                }
+            }
+            return dp[n - 1];
+        }
+    }
+
     class Solution1 {
         public int maxSumAfterPartitioning(int[] A, int K) {
             int n = A.length;
