3d11cce5227e5d00b79091610a3a2a56346625d3
xuweng <2028403305@qq.com>
2021-05-16 11:24:25 +0800

    5762. 恰有 K 根木棍可以看到的排列数目
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/four/RearrangeSticks.java
@@ -0,0 +1,12 @@
+package com.leetcode.tag.contest.four;
+
+/**
+ * 5762. 恰有 K 根木棍可以看到的排列数目
+ */
+public class RearrangeSticks {
+    class Solution {
+        public int rearrangeSticks(int n, int k) {
+            return 0;
+        }
+    }
+}
