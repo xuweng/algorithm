2caf9e0633a5986e3b46bbec93666e64d7f24c69
xuweng <2028403305@qq.com>
2021-05-16 12:09:34 +0800

    1423. 可获得的最大点数
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must3/ten/MaxScore.java
@@ -0,0 +1,34 @@
+package com.leetcode.tag.must3.ten;
+
+import java.util.Arrays;
+
+/**
+ * 1423. 可获得的最大点数
+ * <p>
+ * 模型 模型 模型
+ */
+public class MaxScore {
+    class Solution {
+        public int maxScore(int[] cardPoints, int k) {
+            if (cardPoints.length == k) {
+                return Arrays.stream(cardPoints).sum();
+            }
+            int no = cardPoints.length - k;
+            int left = 0;
+            int right = 0;
+            int sum = 0;
+            int min = Integer.MAX_VALUE;
+
+            while (right < cardPoints.length) {
+                sum += cardPoints[right];
+                if (right >= no - 1) {
+                    min = Math.min(min, sum);
+                    sum -= cardPoints[left];
+                    left++;
+                }
+                right++;
+            }
+            return Arrays.stream(cardPoints).sum() - min;
+        }
+    }
+}
