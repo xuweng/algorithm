d5e1b56c555e5a40765d639ea6708bb00dfcc140
xuweng <2028403305@qq.com>
2021-05-16 11:59:02 +0800

    221. 最大正方形
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must3/ten/MaximalSquare.java
+++ leetcode/src/main/java/com/leetcode/tag/must3/ten/MaximalSquare.java
@@ -10,7 +10,7 @@ public class MaximalSquare {
             int n = matrix[0].length;
 
             int[][] dp = new int[m][n];
-            int sum = 0;
+            int max = 0;
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
                     if (matrix[i][j] == 0) {
@@ -23,11 +23,11 @@ public class MaximalSquare {
                         // 三取小
                         dp[i][j] = Math.min(dp[i][j - 1], Math.min(dp[i - 1][j], dp[i - 1][j - 1])) + 1;
                     }
-                    sum += dp[i][j];
+                    max = Math.max(max, dp[i][j]);
                 }
             }
 
-            return sum;
+            return max * max;
         }
     }
 }
