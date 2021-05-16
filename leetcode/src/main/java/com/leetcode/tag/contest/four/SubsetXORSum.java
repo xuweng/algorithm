15fcd1ea9bf244469478a688069248314b56ab51
xuweng <2028403305@qq.com>
2021-05-16 10:44:56 +0800

    5759. 找出所有子集的异或总和再求和
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/contest/four/SubsetXORSum.java
+++ leetcode/src/main/java/com/leetcode/tag/contest/four/SubsetXORSum.java
@@ -1,20 +1,43 @@
 package com.leetcode.tag.contest.four;
 
+import java.util.*;
+
 /**
  * 5759. 找出所有子集的异或总和再求和
  */
 public class SubsetXORSum {
     class Solution {
+        List<List<Integer>> list = new ArrayList<>();
+        Deque<Integer> deque = new LinkedList<>();
+
         public int subsetXORSum(int[] nums) {
-            int sum = 0;
-            for (int i = 0; i < nums.length; i++) {
-                sum += nums[i];
-                for (int j = i + 1; j < nums.length; j++) {
-                    sum += nums[i] ^ nums[j];
+            int sum = Arrays.stream(nums).sum();
+
+            dfs(nums, 0);
+
+            for (List<Integer> integers : list) {
+                if (integers.size() < 2) {
+                    continue;
                 }
+                int i = 0;
+                for (Integer integer : integers) {
+                    i = i ^ integer;
+                }
+                sum += i;
             }
 
             return sum;
+        }
+
+        private void dfs(int[] nums, int index) {
+            list.add(new ArrayList<>(deque));
+            for (int i = index; i < nums.length; i++) {
+                deque.push(nums[i]);
+
+                dfs(nums, i + 1);
+
+                deque.pop();
+            }
         }
     }
 }
