6f6e646a1de5e2927044e260b442bc68b4abaea7
xuweng <2028403305@qq.com>
2021-05-16 09:10:23 +0800

    1043. 分隔数组以得到最大和
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must3/ten/MaxSumAfterPartitioning.java
@@ -0,0 +1,25 @@
+package com.leetcode.tag.must3.ten;
+
+/**
+ * 1043. 分隔数组以得到最大和
+ */
+public class MaxSumAfterPartitioning {
+    class Solution {
+        public int maxSumAfterPartitioning(int[] arr, int k) {
+            // 长度 长度
+            int[] dp = new int[arr.length + 1];
+            // 长度
+            for (int i = 1; i <= arr.length; i++) {
+                int max = 0;
+                // 索引
+                // 长度j 下标j
+                for (int j = i - 1; j >= 0 && (i - j) <= k; j--) {
+                    max = Math.max(max, arr[j]);
+                    dp[i] = Math.max(dp[i], dp[j] + (i - j) * max);
+                }
+            }
+
+            return dp[arr.length];
+        }
+    }
+}
