5a6fdb6a8bc4e8abef326c151f02abac38e1aea2
xuweng <2028403305@qq.com>
2021-05-16 11:12:29 +0800

    5761. 找出和为指定值的下标对
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/contest/four/FindSumPairs.java
+++ leetcode/src/main/java/com/leetcode/tag/contest/four/FindSumPairs.java
@@ -1,8 +1,7 @@
 package com.leetcode.tag.contest.four;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 5761. 找出和为指定值的下标对
@@ -10,22 +9,28 @@ import java.util.stream.Collectors;
 public class FindSumPairs {
     int[] nums1;
     int[] nums2;
+    Map<Integer, Integer> map;
 
     public FindSumPairs(int[] nums1, int[] nums2) {
         this.nums1 = nums1;
         this.nums2 = nums2;
+        map = new HashMap<>();
+        for (int i : nums2) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
     }
 
     public void add(int index, int val) {
+        map.put(nums2[index], map.getOrDefault(nums2[index], 1) - 1);
         nums2[index] += val;
+        map.put(nums2[index], map.getOrDefault(nums2[index], 0) + 1);
     }
 
     public int count(int tot) {
-        Set<Integer> set2 = Arrays.stream(nums2).boxed().collect(Collectors.toSet());
         int count = 0;
         for (int i : nums1) {
-            if (set2.contains(tot - i)) {
-                count++;
+            if (map.containsKey(tot - i)) {
+                count += map.get(tot - i);
             }
         }
 
