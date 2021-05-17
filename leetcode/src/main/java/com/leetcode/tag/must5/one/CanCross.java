82ff8f973478f416cd17f18e0431b4d53f1043e0
xuweng <2028403305@qq.com>
2021-05-17 21:51:38 +0800

    403. 青蛙过河
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must5/one/CanCross.java
@@ -0,0 +1,53 @@
+package com.leetcode.tag.must5.one;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 403. 青蛙过河
+ */
+public class CanCross {
+    class Solution {
+        Map<Integer, Integer> map = new HashMap<>();
+        Map<String, Boolean> meno = new HashMap<>();
+
+        public boolean canCross(int[] stones) {
+            if (stones[1] != 1) {
+                return false;
+            }
+            if (stones.length == 2) {
+                return true;
+            }
+
+            // 0到1 1步
+            return dfs(stones, 1, 1);
+        }
+
+        private boolean dfs(int[] stones, int index, int step) {
+            String key = index + "_" + step;
+            if (meno.containsKey(key)) {
+                return meno.get(key);
+            }
+            if (index == stones.length - 1) {
+                return true;
+            }
+            for (int i = step - 1; i <= step + 1; i++) {
+                if (i == 0) {
+                    continue;
+                }
+                int next = stones[index] + i;
+                if (!map.containsKey(next)) {
+                    continue;
+                }
+                Integer nextIndex = map.get(next);
+                boolean is = dfs(stones, nextIndex, i);
+                if (is) {
+                    meno.put(key, true);
+                    return true;
+                }
+            }
+            meno.put(key, false);
+            return false;
+        }
+    }
+}
