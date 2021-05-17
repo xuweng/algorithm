d0280cbb49f0e86d675a4458b9682d840e1d78e1
xuweng <2028403305@qq.com>
2021-05-17 21:06:29 +0800

    剑指 Offer 26. 树的子结构
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must5/one/IsSubStructure.java
@@ -0,0 +1,42 @@
+package com.leetcode.tag.must5.one;
+
+/**
+ * 剑指 Offer 26. 树的子结构
+ */
+public class IsSubStructure {
+    class Solution {
+        public boolean isSubStructure(TreeNode A, TreeNode B) {
+            if (A == null || B == null) {
+                return false;
+            }
+
+            return is(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B);
+        }
+
+        private boolean is(TreeNode a, TreeNode b) {
+            if (b == null) {
+                // b已经配匹完
+                return true;
+            }
+            if (a == null) {
+                // b没有配匹完 a已经配匹完
+                return false;
+            }
+            if (a.val != b.val) {
+                return false;
+            }
+
+            return is(a.left, b.left) && is(a.right, b.right);
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
