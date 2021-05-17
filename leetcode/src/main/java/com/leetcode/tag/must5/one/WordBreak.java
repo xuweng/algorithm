0aea82ace5988986d8e5383046407c526770e036
xuweng <2028403305@qq.com>
2021-05-17 21:04:13 +0800

    139. 单词拆分
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must5/one/WordBreak.java
+++ leetcode/src/main/java/com/leetcode/tag/must5/one/WordBreak.java
@@ -12,6 +12,7 @@ public class WordBreak {
         public boolean wordBreak(String s, List<String> wordDict) {
             Set<String> set = new HashSet<>(wordDict);
             boolean[] dp = new boolean[s.length() + 1];
+            dp[0] = true;
 
             for (int i = 1; i <= s.length(); i++) {
                 for (int j = 0; j < i; j++) {
