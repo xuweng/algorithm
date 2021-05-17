08b1d8e19e442af0a470f2f8493f242dcd7b812c
xuweng <2028403305@qq.com>
2021-05-17 21:35:19 +0800

    剑指 Offer 36. 二叉搜索树与双向链表
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must5/one/TreeToDoublyList.java
@@ -0,0 +1,60 @@
+package com.leetcode.tag.must5.one;
+
+/**
+ * 剑指 Offer 36. 二叉搜索树与双向链表
+ */
+public class TreeToDoublyList {
+    class Solution {
+        Node pre;
+        Node head;
+
+        public Node treeToDoublyList(Node root) {
+            if (root == null) {
+                return null;
+            }
+
+            dfs(root);
+
+            pre.right = head;
+            head.left = pre;
+
+            return head;
+        }
+
+        private void dfs(Node root) {
+            if (root == null) {
+                return;
+            }
+            dfs(root.left);
+            if (pre != null) {
+                pre.right = root;
+            } else {
+                // 记录head
+                head = root;
+            }
+            root.left = pre;
+            pre = root;
+
+            dfs(root.right);
+        }
+    }
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+    }
+}
