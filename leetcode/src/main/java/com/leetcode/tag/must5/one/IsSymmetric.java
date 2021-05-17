8213700d0eb721bc08391d5fae12508bac3cc77f
xuweng <2028403305@qq.com>
2021-05-17 21:09:06 +0800

    剑指 Offer 28. 对称的二叉树
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must5/one/IsSymmetric.java
@@ -0,0 +1,37 @@
+package com.leetcode.tag.must5.one;
+
+/**
+ * 剑指 Offer 28. 对称的二叉树
+ */
+public class IsSymmetric {
+    class Solution {
+        public boolean isSymmetric(TreeNode root) {
+            return dfs(root, root);
+        }
+
+        private boolean dfs(TreeNode root, TreeNode root1) {
+            if (root == null) {
+                // 完全配匹
+                return root1 == null;
+            }
+            if (root1 == null) {
+                return false;
+            }
+            if (root.val != root1.val) {
+                return false;
+            }
+
+            return dfs(root.left, root1.right) && dfs(root.right, root1.left);
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
