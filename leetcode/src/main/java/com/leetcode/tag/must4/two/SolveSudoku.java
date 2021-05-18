439b3269f60b80f63666d54542e1cedf935a7c01
xuweng <2028403305@qq.com>
2021-05-18 21:08:17 +0800

    37. 解数独
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must4/two/SolveSudoku.java
+++ leetcode/src/main/java/com/leetcode/tag/must4/two/SolveSudoku.java
@@ -71,4 +71,71 @@ public class SolveSudoku {
             }
         }
     }
+
+    class Solution1 {
+        //行
+        private boolean[][] line = new boolean[9][10];
+        //列
+        private boolean[][] column = new boolean[9][10];
+        //小宫格.3*3.3行3列.
+        private boolean[][][] block = new boolean[3][3][10];
+        //保存.的坐标.候选集
+        private List<int[]> spaces = new ArrayList<>();
+
+        public void solveSudoku(char[][] board) {
+            for (int i = 0; i < 9; ++i) {
+                for (int j = 0; j < 9; ++j) {
+                    if (board[i][j] == '.') {
+                        spaces.add(new int[]{i, j});
+                    } else {
+                        //如果是当前元素是数字,初始化标记数组.和八皇后不一样
+                        //digit就是当前坐标的数字.厉害
+                        int digit = board[i][j] - '0';
+                        line[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = true;
+                    }
+                }
+            }
+
+            dfs(board, 0);
+        }
+
+        /**
+         * 只需要在空格填就可以
+         * <p>
+         * 用 line[2][3]=True 表示数字 4 在第 2 行已经出现过
+         * <p>
+         * 可以填写的数字范围为 [1, 9]
+         *
+         * @param board
+         * @param pos
+         */
+        public boolean dfs(char[][] board, int pos) {
+            if (pos == spaces.size()) {
+                return true;
+            }
+
+            int[] space = spaces.get(pos);
+            //行
+            int i = space[0];
+            //列
+            int j = space[1];
+            for (int digit = 1; digit <= 9; ++digit) {
+                if (line[i][digit] || column[j][digit] || block[i / 3][j / 3][digit]) {
+                    continue;
+                }
+                //一行搞定
+                line[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = true;
+                //digit是表示要填的数字
+                board[i][j] = (char) (digit + '0');
+                if (dfs(board, pos + 1)) {
+                    return true;
+                }
+                //一行搞定
+                line[i][digit] = column[j][digit] = block[i / 3][j / 3][digit] = false;
+                //board[i][j]不需要重置.标记数组重置就可以.
+            }
+
+            return false;
+        }
+    }
 }
