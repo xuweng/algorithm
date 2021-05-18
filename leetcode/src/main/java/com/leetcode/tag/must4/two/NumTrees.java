79c32b4723567022bafceeb5e3f214aecc1b948f
xuweng <2028403305@qq.com>
2021-05-18 21:44:38 +0800

    96. 不同的二叉搜索树
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must4/two/NumTrees.java
+++ leetcode/src/main/java/com/leetcode/tag/must4/two/NumTrees.java
@@ -7,6 +7,7 @@ public class NumTrees {
     class Solution {
         public int numTrees(int n) {
             int[] dp = new int[n + 1];
+            dp[0] = 1;
             dp[1] = 1;
 
             for (int i = 2; i <= n; i++) {
