93714c737f90df9a06313a36c81e6a8de819b679
xuweng <2028403305@qq.com>
2021-05-18 22:13:34 +0800

    比较 dp[i][j - side + 1]的上边， dp[i - side + 1][j]的左边,4条边都比较
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must4/two/Largest1BorderedSquare.java
+++ leetcode/src/main/java/com/leetcode/tag/must4/two/Largest1BorderedSquare.java
@@ -51,6 +51,7 @@ public class Largest1BorderedSquare {
             // dp[i][j][0]: i,j左边连续的1的个数
             // dp[i][j][1]: i,j上边连续的1的个数
             int[][][] dp = new int[m + 1][n + 1][2];
+            // 枚举长度
             for (int i = 1; i <= m; i++) {
                 for (int j = 1; j <= n; j++) {
                     if (grid[i - 1][j - 1] == 1) {
@@ -63,6 +64,8 @@ public class Largest1BorderedSquare {
             for (int i = 1; i <= m; i++) {
                 for (int j = 1; j <= n; j++) {
                     //最短的那条边不一定是合法的边长，如果该边长不合法就需要缩减边长，直到找到合法的
+                    // 最大边 Math.min(dp[i][j][0], dp[i][j][1])
+                    // 比较 dp[i][j - side + 1]的上边， dp[i - side + 1][j]的左边,4条边都比较
                     for (int side = Math.min(dp[i][j][0], dp[i][j][1]); side >= 1; side--) {
                         if (dp[i][j - side + 1][1] >= side && dp[i - side + 1][j][0] >= side) {
                             res = Math.max(res, side);
