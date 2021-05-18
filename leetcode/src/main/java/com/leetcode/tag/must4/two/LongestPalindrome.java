1437262ecc8409c8017e8efa1a6edcf7d449f77f
xuweng <2028403305@qq.com>
2021-05-18 21:23:41 +0800

    5. 最长回文子串
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must4/two/LongestPalindrome.java
@@ -0,0 +1,35 @@
+package com.leetcode.tag.must4.two;
+
+/**
+ * 5. 最长回文子串
+ * <p>
+ * 预处理 候选集
+ * <p>
+ * 行 列 3*3 索引
+ * <p>
+ * 重置 覆盖
+ */
+public class LongestPalindrome {
+    class Solution {
+        public String longestPalindrome(String s) {
+            int[][] dp = new int[s.length()][s.length()];
+            int left = 0;
+            int right = 0;
+            for (int i = s.length() - 2; i >= 0; i--) {
+                for (int j = i + 1; j < s.length(); j++) {
+                    if (s.charAt(i) == s.charAt(j)) {
+                        dp[i][j] = dp[i + 1][j - 1] + 2;
+                    } else {
+                        dp[i][j] = Math.max(dp[i][j - 1], dp[i + 1][j]);
+                    }
+                    if (dp[i][j] > right - left) {
+                        left = i;
+                        right = j;
+                    }
+                }
+            }
+
+            return s.substring(left, right + 1);
+        }
+    }
+}
