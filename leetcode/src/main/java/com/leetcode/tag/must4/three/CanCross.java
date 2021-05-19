ae4dd5e49aa6a376f497719d96a4ec92bef34f73
xuweng <2028403305@qq.com>
2021-05-19 21:41:38 +0800

    403. 青蛙过河
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must4/three/CanCross.java
@@ -0,0 +1,57 @@
+package com.leetcode.tag.must4.three;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 403. 青蛙过河
+ */
+public class CanCross {
+    class Solution {
+        Map<Integer, Integer> map = new HashMap<>();
+        Map<String, Boolean> meno = new HashMap<>();
+
+        public boolean canCross(int[] stones) {
+            if (stones[1] != 1) {
+                return false;
+            }
+            if (stones.length == 2) {
+                return true;
+            }
+            for (int i = 0; i < stones.length; i++) {
+                map.put(stones[i], i);
+            }
+
+            // 0到1 1步
+            return dfs(stones, 1, 1);
+        }
+
+        private boolean dfs(int[] stones, int index, int k) {
+            String key = index + "_" + k;
+            if (meno.containsKey(key)) {
+                return meno.get(key);
+            }
+            if (index == stones.length - 1) {
+                meno.put(key, true);
+                return true;
+            }
+            for (int i = k - 1; i <= k + 1; i++) {
+                if (i == 0) {
+                    continue;
+                }
+                int next = stones[index] + i;
+                if (!map.containsKey(next)) {
+                    continue;
+                }
+                Integer nextIndex = map.get(next);
+                boolean is = dfs(stones, nextIndex, i);
+                if (is) {
+                    meno.put(key, true);
+                    return true;
+                }
+            }
+            meno.put(key, false);
+            return false;
+        }
+    }
+}
