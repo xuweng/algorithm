53b4352bd8a55bdfc54f8c413e028dc6719d3a05
xuweng <2028403305@qq.com>
2021-05-19 20:40:59 +0800

    91. 解码方法
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must4/three/NumDecodings.java
+++ leetcode/src/main/java/com/leetcode/tag/must4/three/NumDecodings.java
@@ -13,12 +13,34 @@ public class NumDecodings {
                 if (s.charAt(i - 1) == '0') {
                     dp[i] = dp[i - 1];
                 }
-                if (i >= 2 && s.charAt(i - 2) != 0 && ((s.charAt(i - 2) - '0') * 10 + s.charAt(i - 1) - '0' <= 26)) {
+                if (i >= 2 && s.charAt(i - 2) != '0' && ((s.charAt(i - 2) - '0') * 10 + s.charAt(i - 1) - '0' <= 26)) {
                     dp[i] += dp[i - 2];
                 }
             }
 
             return dp[s.length()];
+        }
+    }
+
+    class Solution1 {
+        public int numDecodings(String s) {
+            int a = 0;
+            int b = 1;
+            int c = 0;
+
+            for (int i = 1; i <= s.length(); i++) {
+                c = 0;
+                if (s.charAt(i - 1) == '0') {
+                    c = b;
+                }
+                if (i >= 2 && s.charAt(i - 2) != '0' && ((s.charAt(i - 2) - '0') * 10 + s.charAt(i - 1) - '0' <= 26)) {
+                    c += a;
+                }
+                a = b;
+                b = c;
+            }
+
+            return c;
         }
     }
 }
