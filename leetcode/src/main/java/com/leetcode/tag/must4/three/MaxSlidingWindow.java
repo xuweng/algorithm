d6baa93aca06624e82c32f6bbbdf70ddc016dbb5
xuweng <2028403305@qq.com>
2021-05-19 22:16:07 +0800

    剑指 Offer 59 - I. 滑动窗口的最大值
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must4/three/MaxSlidingWindow.java
@@ -0,0 +1,35 @@
+package com.leetcode.tag.must4.three;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 剑指 Offer 59 - I. 滑动窗口的最大值
+ */
+public class MaxSlidingWindow {
+    class Solution {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            if (nums == null || nums.length == 0) {
+                return new int[0];
+            }
+
+            Deque<Integer> deque = new LinkedList<>();
+            int[] result = new int[nums.length - k + 1];
+
+            for (int i = 0; i < nums.length; i++) {
+                while (!deque.isEmpty() && nums[deque.peekLast()] <= nums[i]) {
+                    deque.pollLast();
+                }
+                deque.offerLast(i);
+                if (i - deque.peekFirst() >= k) {
+                    deque.pollFirst();
+                }
+                if (i >= k - 1) {
+                    result[i - k + 1] = nums[deque.peekFirst()];
+                }
+            }
+
+            return result;
+        }
+    }
+}
