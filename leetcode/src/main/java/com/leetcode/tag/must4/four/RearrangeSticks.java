76a212f5de1c547235aa30c3ad3b6c61008e2dec
xuweng <2028403305@qq.com>
2021-05-20 21:43:54 +0800

    62. 不同路径
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must4/four/RearrangeSticks.java
+++ leetcode/src/main/java/com/leetcode/tag/must4/four/RearrangeSticks.java
@@ -9,6 +9,7 @@ public class RearrangeSticks {
 
         public int rearrangeSticks(int n, int k) {
             int[][] dp = new int[n + 1][k + 1];
+            dp[0][0] = 1;
             for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= k; j++) {
                     // 前i-1个看到j-1个
