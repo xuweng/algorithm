c9cecaeb6c6681ea248a61fff7603c140fdde2ad
xuweng <2028403305@qq.com>
2021-05-22 10:19:06 +0800

    剑指 Offer 14- I. 剪绳子
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must4/six/CuttingRope.java
@@ -0,0 +1,23 @@
+package com.leetcode.tag.must4.six;
+
+/**
+ * 剑指 Offer 14- I. 剪绳子
+ * <p>
+ * i j 模型
+ */
+public class CuttingRope {
+    class Solution {
+        public int cuttingRope(int n) {
+            int[] dp = new int[n + 1];
+            dp[2] = 1;
+
+            for (int i = 3; i <= n; i++) {
+                for (int j = 2; j < i; j++) {
+                    dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
