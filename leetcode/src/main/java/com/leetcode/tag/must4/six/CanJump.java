2266a50b5e5849da7e3c85002bacac2a0c8a4545
xuweng <2028403305@qq.com>
2021-05-22 10:36:52 +0800

    55. 跳跃游戏
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must4/six/CanJump.java
@@ -0,0 +1,24 @@
+package com.leetcode.tag.must4.six;
+
+/**
+ * 55. 跳跃游戏
+ */
+public class CanJump {
+    class Solution {
+        public boolean canJump(int[] nums) {
+            boolean[] dp = new boolean[nums.length];
+            dp[0] = true;
+
+            for (int i = 1; i < nums.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (dp[j] && nums[j] > i - j) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            return dp[nums.length - 1];
+        }
+    }
+}
