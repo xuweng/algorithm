6c93a9c17417cee838c446d24949d9dc80fe5ca9
xuweng <2028403305@qq.com>
2021-05-22 08:42:17 +0800

    优先分配给「空闲工人」
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must4/six/MinimumTimeRequired.java
+++ leetcode/src/main/java/com/leetcode/tag/must4/six/MinimumTimeRequired.java
@@ -46,4 +46,57 @@ public class MinimumTimeRequired {
             }
         }
     }
+
+    class Solution1 {
+        int[] jobs;
+        int n, k;
+        int ans = 0x3f3f3f3f;
+
+        public int minimumTimeRequired(int[] _jobs, int _k) {
+            jobs = _jobs;
+            n = jobs.length;
+            k = _k;
+            int[] sum = new int[k];
+
+            dfs(0, 0, sum, 0);
+
+            return ans;
+        }
+
+        /**
+         * 【补充说明】不理解可以看看下面的「我猜你问」的 Q5 哦 ~
+         * <p>
+         * u     : 当前处理到那个 job
+         * used  : 当前分配给了多少个工人了
+         * sum   : 工人的分配情况          例如：sum[0] = x 代表 0 号工人工作量为 x
+         * max   : 当前的「最大工作时间」
+         */
+        void dfs(int u, int used, int[] sum, int max) {
+            if (max >= ans) {
+                return;
+            }
+            if (u == n) {
+                ans = max;
+                return;
+            }
+            // 优先分配给「空闲工人」
+            if (used < k) {
+                // 工作u分配工人used
+                sum[used] = jobs[u];
+
+                // 下一份工作u+1分配给工人used+1
+                dfs(u + 1, used + 1, sum, Math.max(sum[used], max));
+
+                sum[used] = 0;
+            }
+            for (int i = 0; i < used; i++) {
+                // 当前工作u分配给工人i
+                sum[i] += jobs[u];
+
+                dfs(u + 1, used, sum, Math.max(sum[i], max));
+
+                sum[i] -= jobs[u];
+            }
+        }
+    }
 }
