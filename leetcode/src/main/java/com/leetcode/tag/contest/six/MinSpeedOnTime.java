9b63edad6d0f4320ba20da5beaf597156700a32f
xuweng <2028403305@qq.com>
2021-05-23 12:03:39 +0800

    1269. 停在原地的方案数
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/six/MinSpeedOnTime.java
@@ -0,0 +1,12 @@
+package com.leetcode.tag.contest.six;
+
+/**
+ * 5764. 准时到达的列车最小时速
+ */
+public class MinSpeedOnTime {
+    class Solution {
+        public int minSpeedOnTime(int[] dist, double hour) {
+            return 0;
+        }
+    }
+}
