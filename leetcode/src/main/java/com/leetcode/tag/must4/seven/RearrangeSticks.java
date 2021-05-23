3091bdbfe0ba3c3b2aa7ef9fc40f1cfbf203154b
xuweng <2028403305@qq.com>
2021-05-23 10:26:33 +0800

    1866. 恰有 K 根木棍可以看到的排列数目
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must4/seven/RearrangeSticks.java
+++ leetcode/src/main/java/com/leetcode/tag/must4/seven/RearrangeSticks.java
@@ -8,7 +8,8 @@ public class RearrangeSticks {
         int mod = (int) (1e9 + 7);
 
         public int rearrangeSticks(int n, int k) {
-            int[][] dp = new int[n + 1][k + 1];
+            long[][] dp = new long[n + 1][k + 1];
+            dp[0][0] = 1;
             for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= k; j++) {
                     // 前i-1看到j-1
@@ -18,7 +19,7 @@ public class RearrangeSticks {
                 }
             }
 
-            return dp[n][k];
+            return (int) dp[n][k];
         }
     }
 }
