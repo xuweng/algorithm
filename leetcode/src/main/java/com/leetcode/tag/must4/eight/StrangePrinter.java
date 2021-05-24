1998e29a6f568ca854e532478e01189aa93a99ca
xuweng <2028403305@qq.com>
2021-05-24 20:58:40 +0800

    [i,j] 区间分割
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must4/eight/StrangePrinter.java
+++ leetcode/src/main/java/com/leetcode/tag/must4/eight/StrangePrinter.java
@@ -36,6 +36,20 @@ public class StrangePrinter {
     }
 
     class Solution1 {
+        /**
+         * 当区间 [i, j] 两端点的字符相同时（'aba'）：此时它的打印次数可以从它的更小一层的子区间的打印次数而来（'ab'），
+         * <p>
+         * 此时状态转移方程：dp[i][j] = dp[i][j - 1]；
+         * <p>
+         * 当区间 [i, j] 两端点的字符不同时（'abab'）：此时要遍历所有可能的组合，将该区间中的字符串进行拆分，取需要打印次数最小的解。
+         * 示例：s = 'abab'
+         * 先打印 'a' 再打印 'bab'，此时需要 1 + 2 = 3 次打印；
+         * 先打印 'ab' 再打印 'ab'，此时需要 2 + 2 = 4 次打印；
+         * 先打印 'aba' 再打印 'b'，此时需要 2 + 1 = 3 次打印，因此得到 ' abab ' 至少需要三次打印。
+         *
+         * @param s
+         * @return
+         */
         public int strangePrinter(String s) {
             // f[i][j] 表示打印完成区间 [i,j] 的最少操作数
             int[][] dp = new int[s.length()][s.length()];
@@ -89,6 +103,39 @@ public class StrangePrinter {
                 }
             }
             return f[0][n - 1];
+        }
+    }
+
+    class Solution3 {
+        public int strangePrinter(String s) {
+            int n = s.length();
+            if (n == 0) {
+                return 0;
+            }
+
+            int[][] dp = new int[n][n];
+            // 对于一个元素时，只打印一次即可。
+            for (int i = 0; i < n; i++) {
+                dp[i][i] = 1;
+            }
+
+            for (int i = n - 1; i >= 0; i--) {
+                // 从 i + 1 开始，往右端点(n - 1)移动，有 i < j。
+                for (int j = i + 1; j < n; j++) {
+                    if (s.charAt(j) == s.charAt(i)) {
+                        dp[i][j] = dp[i][j - 1];
+                    } else {
+                        int val = Integer.MAX_VALUE;
+                        // 从 i 开始向 j 移动
+                        // [i,j] 区间分割
+                        for (int k = i; k < j; k++) {
+                            val = Math.min(val, dp[i][k] + dp[k + 1][j]);
+                        }
+                        dp[i][j] = val;
+                    }
+                }
+            }
+            return dp[0][n - 1];
         }
     }
 }
