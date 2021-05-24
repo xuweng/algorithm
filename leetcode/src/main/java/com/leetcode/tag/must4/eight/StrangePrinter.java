a1447c4480fd1b9a7adc1e848e1f25c6811c8519
xuweng <2028403305@qq.com>
2021-05-24 20:38:05 +0800

    664. 奇怪的打印机
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must4/eight/StrangePrinter.java
+++ leetcode/src/main/java/com/leetcode/tag/must4/eight/StrangePrinter.java
@@ -17,10 +17,14 @@ public class StrangePrinter {
                 f[i][i] = 1;
                 for (int j = i + 1; j < n; j++) {
                     if (s.charAt(i) == s.charAt(j)) {
+                        // 打印左侧字符 s[i] 时，可以顺便打印右侧字符 s[j]，这样我们即可忽略右侧字符对该区间的影响，
+                        // 只需要考虑如何尽快打印完区间 [i,j-1]
                         f[i][j] = f[i][j - 1];
                     } else {
                         int minn = Integer.MAX_VALUE;
                         for (int k = i; k < j; k++) {
+                            // 分别完成该区间的左右两部分的打印
+                            // 两部分分别为区间 [i,k] 和区间 [k+1,j]
                             minn = Math.min(minn, f[i][k] + f[k + 1][j]);
                         }
                         f[i][j] = minn;
@@ -28,6 +32,34 @@ public class StrangePrinter {
                 }
             }
             return f[0][n - 1];
+        }
+    }
+
+    class Solution1 {
+        public int strangePrinter(String s) {
+            // f[i][j] 表示打印完成区间 [i,j] 的最少操作数
+            int[][] dp = new int[s.length()][s.length()];
+            for (int i = 0; i < s.length(); i++) {
+                dp[i][i] = 1;
+            }
+            for (int i = s.length() - 2; i >= 0; i--) {
+                for (int j = i + 1; j < s.length(); j++) {
+                    if (s.charAt(i) == s.charAt(j)) {
+                        // 打印左侧字符 s[i] 时，可以顺便打印右侧字符 s[j]，这样我们即可忽略右侧字符对该区间的影响，
+                        // 只需要考虑如何尽快打印完区间 [i,j-1]
+                        dp[i][j] = dp[i][j - 1];
+                    } else {
+                        int minn = Integer.MAX_VALUE;
+                        for (int k = i + 1; k < j; k++) {
+                            // 分别完成该区间的左右两部分的打印
+                            // 两部分分别为区间 [i,k] 和区间 [k+1,j]
+                            minn = Math.min(minn, dp[i][k] + dp[k + 1][j]);
+                        }
+                        dp[i][j] = minn;
+                    }
+                }
+            }
+            return dp[0][s.length() - 1];
         }
     }
 }
