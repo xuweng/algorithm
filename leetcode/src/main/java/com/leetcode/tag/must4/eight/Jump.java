177eff38ec393447ced245a68fa4e8ec8f0ce400
xuweng <2028403305@qq.com>
2021-05-24 22:33:31 +0800

    45. 跳跃游戏 II
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must4/eight/Jump.java
@@ -0,0 +1,26 @@
+package com.leetcode.tag.must4.eight;
+
+import java.util.Arrays;
+
+/**
+ * 45. 跳跃游戏 II
+ */
+public class Jump {
+    class Solution {
+        public int jump(int[] nums) {
+            int[] dp = new int[nums.length];
+            Arrays.fill(dp, Integer.MAX_VALUE - 1);
+            dp[0] = 0;
+
+            for (int i = 1; i < nums.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (nums[j] >= i - j) {
+                        dp[i] = Math.min(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+
+            return dp[nums.length - 1];
+        }
+    }
+}
