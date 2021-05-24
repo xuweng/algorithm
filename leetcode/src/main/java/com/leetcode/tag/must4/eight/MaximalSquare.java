b7b9d55d99c753293a8f0e242db66666d29d4b1e
xuweng <2028403305@qq.com>
2021-05-24 21:26:10 +0800

    221. 最大正方形
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must4/eight/MaximalSquare.java
@@ -0,0 +1,41 @@
+package com.leetcode.tag.must4.eight;
+
+/**
+ * 221. 最大正方形
+ * <p>
+ * 区间 区间 区间
+ * <p>
+ * 相等 相等 相等
+ * <p>
+ * 区间 区间 区间
+ * <p>
+ * 长度 left 确定right 分割点
+ */
+public class MaximalSquare {
+    class Solution {
+        public int maximalSquare(char[][] matrix) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+
+            int max = 0;
+            int[][] dp = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][j] == '0') {
+                        continue;
+                    }
+                    if (i == 0 || j == 0) {
+                        // 边界
+                        dp[i][j] = 1;
+                    } else {
+                        // 三取小
+                        dp[i][j] = Math.min(dp[i][j - 1], Math.min(dp[i - 1][j], dp[i - 1][j - 1])) + 1;
+                    }
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+
+            return max * max;
+        }
+    }
+}
