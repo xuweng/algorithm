bcf19b70ba48dfe17f578b51fc3dad453d1bbd21
xuweng <2028403305@qq.com>
2021-05-26 23:25:44 +0800

    1190. 反转每对括号间的子串
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must4/ten/ReverseParentheses.java
+++ leetcode/src/main/java/com/leetcode/tag/must4/ten/ReverseParentheses.java
@@ -1,5 +1,8 @@
 package com.leetcode.tag.must4.ten;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * 1190. 反转每对括号间的子串
  */
@@ -47,6 +50,26 @@ public class ReverseParentheses {
             }
 
             return new String(chars);
+        }
+    }
+
+    class Solution1 {
+        public String reverseParentheses(String s) {
+            Deque<String> stack = new LinkedList<>();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.length(); i++) {
+                char ch = s.charAt(i);
+                if (ch == '(') {
+                    stack.push(sb.toString());
+                    sb.setLength(0);
+                } else if (ch == ')') {
+                    sb.reverse();
+                    sb.insert(0, stack.pop());
+                } else {
+                    sb.append(ch);
+                }
+            }
+            return sb.toString();
         }
     }
 }
