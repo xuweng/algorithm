7c7eb9beeedbf4ff7b3032dd54bb12b4c30dbc29
xuweng <2028403305@qq.com>
2021-05-27 21:55:32 +0800

    1035. 不相交的线
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must4/ten/MaxUncrossedLines.java
+++ leetcode/src/main/java/com/leetcode/tag/must4/ten/MaxUncrossedLines.java
@@ -10,7 +10,7 @@ public class MaxUncrossedLines {
             for (int i = 1; i <= nums1.length; i++) {
                 for (int j = 1; j <= nums2.length; j++) {
                     if (nums1[i - 1] == nums2[j - 1]) {
-                        dp[i][j] = dp[i - 1][j - 1];
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
                     } else {
                         dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
                     }
@@ -18,6 +18,26 @@ public class MaxUncrossedLines {
             }
 
             return dp[nums1.length][nums2.length];
+        }
+    }
+
+    class Solution1 {
+        public int maxUncrossedLines(int[] nums1, int[] nums2) {
+            int[] dp = new int[nums2.length + 1];
+            for (int i = 1; i <= nums1.length; i++) {
+                int pre = dp[0];
+                for (int j = 1; j <= nums2.length; j++) {
+                    int cur = dp[j];
+                    if (nums1[i - 1] == nums2[j - 1]) {
+                        dp[j] = pre + 1;
+                    } else {
+                        dp[j] = Math.max(dp[j - 1], dp[j]);
+                    }
+                    pre = cur;
+                }
+            }
+
+            return dp[nums2.length];
         }
     }
 }
