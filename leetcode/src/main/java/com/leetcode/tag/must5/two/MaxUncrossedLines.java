a1d9f6824812949093169c676547bc337492edce
xuweng <2028403305@qq.com>
2021-05-28 23:01:49 +0800

    1035. 不相交的线
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must5/two/MaxUncrossedLines.java
+++ leetcode/src/main/java/com/leetcode/tag/must5/two/MaxUncrossedLines.java
@@ -11,7 +11,7 @@ public class MaxUncrossedLines {
             int[][] dp = new int[nums1.length + 1][nums2.length + 1];
             for (int i = 1; i <= nums1.length; i++) {
                 for (int j = 1; j <= nums2.length; j++) {
-                    if (nums1[i - 1] == nums2[i - 1]) {
+                    if (nums1[i - 1] == nums2[j - 1]) {
                         dp[i][j] = dp[i - 1][j - 1] + 1;
                     } else {
                         dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
