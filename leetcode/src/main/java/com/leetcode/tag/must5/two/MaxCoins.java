106a94fc96880333206540e668e453c24f0c4485
xuweng <2028403305@qq.com>
2021-05-28 22:18:23 +0800

    312. 戳气球
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must5/two/MaxCoins.java
@@ -0,0 +1,29 @@
+package com.leetcode.tag.must5.two;
+
+/**
+ * 312. 戳气球
+ */
+public class MaxCoins {
+    class Solution {
+        public int maxCoins(int[] nums) {
+            int[] n = new int[nums.length + 2];
+            n[0] = 1;
+            n[n.length - 1] = 1;
+
+            for (int i = 1; i < n.length - 1; i++) {
+                n[i] = nums[i - 1];
+            }
+
+            int[][] dp = new int[n.length][n.length];
+            for (int i = n.length - 1; i >= 0; i--) {
+                for (int j = i + 1; j < n.length; j++) {
+                    for (int k = i + 1; k < j; k++) {
+                        dp[i][j] = Math.max(dp[i][j], dp[i][k] + dp[k][j] + n[i] * n[k] * n[j]);
+                    }
+                }
+            }
+
+            return dp[0][n.length - 1];
+        }
+    }
+}
