8afe1334c27fcdbb47dc445a69f7dc76d0e5a087
xuweng <2028403305@qq.com>
2021-05-28 21:46:54 +0800

    477. 汉明距离总和
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must5/two/TotalHammingDistance.java
+++ leetcode/src/main/java/com/leetcode/tag/must5/two/TotalHammingDistance.java
@@ -20,4 +20,44 @@ public class TotalHammingDistance {
             return ans;
         }
     }
+
+    class Solution1 {
+        public int totalHammingDistance(int[] nums) {
+            int res = 0;
+            for (int i = 0; i < nums.length - 1; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    res += getHamm(nums[i], nums[j]);
+                }
+            }
+            return res;
+        }
+
+        private int getHamm(int x, int y) {
+            return Integer.bitCount(x ^ y);
+        }
+    }
+
+    class Solution2 {
+        public int totalHammingDistance(int[] nums) {
+            int sum = 0;
+            int n = nums.length;
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    sum += CountHammingDistance(nums[i], nums[j]);
+                }
+            }
+            System.out.println(sum);
+            return sum;
+        }
+
+        public int CountHammingDistance(int a, int b) {
+            int res = (a ^ b);
+            int count = 0;
+            while (res != 0) {
+                count++;
+                res = res & (res - 1);
+            }
+            return count;
+        }
+    }
 }
