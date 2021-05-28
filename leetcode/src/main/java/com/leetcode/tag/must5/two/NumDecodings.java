906cc0b374bc0da2b1ba0a2eda1002388dda09e3
xuweng <2028403305@qq.com>
2021-05-28 22:07:06 +0800

    91. 解码方法
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must5/two/NumDecodings.java
@@ -0,0 +1,26 @@
+package com.leetcode.tag.must5.two;
+
+/**
+ * 91. 解码方法
+ * <p>
+ * 乘法原理 乘法原理 乘法原理
+ */
+public class NumDecodings {
+    class Solution {
+        public int numDecodings(String s) {
+            int[] dp = new int[s.length() + 1];
+            dp[0] = 1;
+
+            for (int i = 1; i <= s.length(); i++) {
+                if (s.charAt(i - 1) != '0') {
+                    dp[i] = dp[i - 1];
+                }
+                if (i >= 2 && s.charAt(i - 2) != '0' && (s.charAt(i - 2) - '0') * 10 + s.charAt(i - 1) - '0' <= 26) {
+                    dp[i] += dp[i - 2];
+                }
+            }
+
+            return dp[s.length()];
+        }
+    }
+}
