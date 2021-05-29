720fea83a4f0351f8ad44d9437be4f78a397e241
xuweng <2028403305@qq.com>
2021-05-29 22:29:48 +0800

    714. 买卖股票的最佳时机含手续费
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must5/three/MaxProfit1.java
@@ -0,0 +1,23 @@
+package com.leetcode.tag.must5.three;
+
+/**
+ * 714. 买卖股票的最佳时机含手续费
+ */
+public class MaxProfit1 {
+    class Solution {
+        public int maxProfit(int[] prices, int fee) {
+            int buy = -prices[0];
+            int sell = 0;
+
+            for (int i = 1; i < prices.length; i++) {
+                int b = Math.max(buy, sell - prices[i]);
+                int s = Math.max(sell, buy + prices[i] - fee);
+
+                buy = b;
+                sell = s;
+            }
+
+            return sell;
+        }
+    }
+}
