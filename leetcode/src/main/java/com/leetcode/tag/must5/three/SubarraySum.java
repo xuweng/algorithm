65008abfd4b2c247350b04b76aea99076fed8cf9
xuweng <2028403305@qq.com>
2021-05-29 21:38:35 +0800

    560. 和为K的子数组
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must5/three/SubarraySum.java
@@ -0,0 +1,29 @@
+package com.leetcode.tag.must5.three;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 560. 和为K的子数组
+ */
+public class SubarraySum {
+    class Solution {
+        public int subarraySum(int[] nums, int k) {
+            Map<Integer, Integer> map = new HashMap<>();
+            map.put(0, 1);
+
+            int count = 0;
+            int sum = 0;
+            for (int num : nums) {
+                sum += num;
+
+                if (map.containsKey(sum - k)) {
+                    count += map.get(sum - k);
+                }
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+
+            return count;
+        }
+    }
+}
