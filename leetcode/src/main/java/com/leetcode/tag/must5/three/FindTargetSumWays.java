d968c06b5fc8c372bc6df68ff1df80c3a038d30a
xuweng <2028403305@qq.com>
2021-05-29 22:23:44 +0800

    494. 目标和
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must5/three/FindTargetSumWays.java
@@ -0,0 +1,35 @@
+package com.leetcode.tag.must5.three;
+
+import java.util.Arrays;
+
+/**
+ * 494. 目标和
+ */
+public class FindTargetSumWays {
+    class Solution {
+        public int findTargetSumWays(int[] nums, int target) {
+            // sum=s1+s2
+            // target=s1-s2
+            // sum+target=2*s1
+            int sum = Arrays.stream(nums).sum();
+            if (target > sum) {
+                return 0;
+            }
+            if ((sum + target) % 2 != 0) {
+                return 0;
+            }
+            int s = (sum + target) / 2;
+
+            int[] dp = new int[s + 1];
+            dp[0] = 1;
+
+            for (int num : nums) {
+                for (int i = s; i >= num; i--) {
+                    dp[i] += dp[i - num];
+                }
+            }
+
+            return dp[s];
+        }
+    }
+}
