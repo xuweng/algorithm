f14648a26a19b8b1fb1fae664e9af4daba209370
xuweng <2028403305@qq.com>
2021-05-31 22:01:45 +0800

    1872. 石子游戏 VIII
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must5/six/StoneGameVIII.java
+++ leetcode/src/main/java/com/leetcode/tag/must5/six/StoneGameVIII.java
@@ -82,4 +82,22 @@ public class StoneGameVIII {
             return res;
         }
     }
+
+    class Solution3 {
+        public int stoneGameVIII(int[] stones) {
+            int n = stones.length;
+            int[] sum = new int[n + 1];
+            for (int i = 0; i < n; i++) {
+                sum[i + 1] = sum[i] + stones[i];
+            }
+
+            int[] dp = new int[n + 1];
+            dp[n] = sum[n];
+
+            for (int i = n - 1; i >= 2; i--) {
+                dp[i] = Math.max(dp[i + 1], sum[i] - dp[i + 1]);
+            }
+            return dp[2];
+        }
+    }
 }
