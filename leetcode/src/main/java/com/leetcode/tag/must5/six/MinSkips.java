cd39b1d9e42e726c211aeec9ff7bf33724398afb
xuweng <2028403305@qq.com>
2021-05-31 20:50:56 +0800

    1883. 准时抵达会议现场的最小跳过休息次数
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must5/six/MinSkips.java
@@ -0,0 +1,37 @@
+package com.leetcode.tag.must5.six;
+
+import java.util.Arrays;
+
+/**
+ * 1883. 准时抵达会议现场的最小跳过休息次数
+ */
+public class MinSkips {
+    class Solution {
+        public int minSkips(int[] dist, int speed, int m) {
+            int n = dist.length;
+            double eps = 1e-8;
+            double[][] f = new double[n + 1][n + 1];
+            for (double[] doubles : f) {
+                Arrays.fill(doubles, Double.MAX_VALUE);
+            }
+
+            for (int i = 1; i <= n; i++) {
+                double t = (double) dist[i - 1] / speed;
+                for (int j = 0; j <= i; j++) {
+                    if (j <= i - 1) {
+                        f[i][j] = Math.ceil(f[i - 1][j] + t - eps);
+                    }
+                    if (j > 0) {
+                        f[i][j] = Math.min(f[i][j], f[i - 1][j - 1] + t);
+                    }
+                }
+            }
+            for (int i = 0; i <= n; i++) {
+                if (f[n][i] <= m) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
