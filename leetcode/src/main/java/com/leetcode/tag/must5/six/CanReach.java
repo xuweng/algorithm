7779fc990cab17edb5ba37a1a6f2a1b9b42e7ea1
xuweng <2028403305@qq.com>
2021-05-31 21:25:58 +0800

    1871. 跳跃游戏 VII
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must5/six/CanReach.java
+++ leetcode/src/main/java/com/leetcode/tag/must5/six/CanReach.java
@@ -73,4 +73,26 @@ public class CanReach {
             return dp[n - 1];
         }
     }
+
+    class Solution2 {
+        public boolean canReach(String s, int minJump, int maxJump) {
+            int n = s.length();
+            boolean[] f = new boolean[n];
+            int[] pre = new int[n];
+            f[0] = true;
+            // 由于我们从 i=minJump 开始动态规划，因此需要将 [0,minJump) 这部分的前缀和预处理出来
+            for (int i = 0; i < minJump; ++i) {
+                pre[i] = 1;
+            }
+            for (int i = minJump; i < n; ++i) {
+                int left = i - maxJump, right = i - minJump;
+                if (s.charAt(i) == '0') {
+                    int total = pre[right] - (left <= 0 ? 0 : pre[left - 1]);
+                    f[i] = (total != 0);
+                }
+                pre[i] = pre[i - 1] + (f[i] ? 1 : 0);
+            }
+            return f[n - 1];
+        }
+    }
 }
