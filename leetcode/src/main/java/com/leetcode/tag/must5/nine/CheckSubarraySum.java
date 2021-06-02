897f87e5956d7d9bf08e9cf591f628ca24bc7257
xuweng <2028403305@qq.com>
2021-06-02 21:42:08 +0800

    523. 连续的子数组和
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must5/nine/CheckSubarraySum.java
+++ leetcode/src/main/java/com/leetcode/tag/must5/nine/CheckSubarraySum.java
@@ -1,7 +1,9 @@
 package com.leetcode.tag.must5.nine;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * 523. 连续的子数组和
@@ -37,4 +39,33 @@ public class CheckSubarraySum {
             return false;
         }
     }
+
+    /**
+     * 作者：AC_OIer
+     * 链接：https://leetcode-cn.com/problems/continuous-subarray-sum/solution/gong-shui-san-xie-tuo-zhan-wei-qiu-fang-1juse/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    class Solution1 {
+        public boolean checkSubarraySum(int[] nums, int k) {
+            int n = nums.length;
+            // 前缀和
+            int[] sum = new int[n + 1];
+            for (int i = 1; i <= n; i++) {
+                sum[i] = sum[i - 1] + nums[i - 1];
+            }
+            Set<Integer> set = new HashSet<>();
+            // 从 2 开始枚举右端点（根据题目要求，子数组长度至少为 2）
+            for (int i = 2; i <= n; i++) {
+                // 左端点
+                set.add(sum[i - 2] % k);
+                if (set.contains(sum[i] % k)) {
+                    // 枚举某个右端点 j 时发现存在某个左端点 i 符合要求
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
 }
