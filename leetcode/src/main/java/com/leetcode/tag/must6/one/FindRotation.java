5a775488e82cd8a725f81a520cba69b57e576727
xuweng <2028403305@qq.com>
2021-06-06 18:08:10 +0800

    5776. 判断矩阵经轮转后是否一致
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must6/one/FindRotation.java
@@ -0,0 +1,30 @@
+package com.leetcode.tag.must6.one;
+
+/**
+ * 5776. 判断矩阵经轮转后是否一致
+ */
+public class FindRotation {
+    int temp;
+
+    public boolean findRotation(int[][] mat, int[][] target) {
+        int n = mat.length;
+        // 最多旋转 4 次
+        for (int k = 0; k < 4; ++k) {
+            // 旋转操作
+            for (int i = 0; i < n / 2; ++i) {
+                for (int j = 0; j < (n + 1) / 2; ++j) {
+                    temp = mat[i][j];
+                    mat[i][j] = mat[n - 1 - j][i];
+                    mat[n - 1 - j][i] = mat[n - 1 - i][n - 1 - j];
+                    mat[n - 1 - i][n - 1 - j] = mat[j][n - 1 - i];
+                    mat[j][n - 1 - i] = temp;
+                }
+            }
+
+            if (mat == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
