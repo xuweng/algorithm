bf6b896650f58aea43d12589009e62be2b9fdbfa
xuweng <2028403305@qq.com>
2021-06-11 21:46:06 +0800

    1027. 最长等差数列
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must6/four/LongestArithSeqLength.java
@@ -0,0 +1,30 @@
+package com.leetcode.tag.must6.four;
+
+import java.util.Arrays;
+
+/**
+ * 1027. 最长等差数列
+ */
+public class LongestArithSeqLength {
+    class Solution {
+        public int longestArithSeqLength(int[] nums) {
+            int offset = 10000;
+            int[][] dp = new int[nums.length][2 * offset];
+
+            for (int[] ints : dp) {
+                Arrays.fill(ints, 1);
+            }
+            int max = 1;
+            for (int i = 1; i < nums.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    int cha = nums[i] - nums[j];
+                    dp[i][cha + offset] = Math.max(dp[i][cha + offset], dp[j][cha + offset] + 1);
+
+                    max = Math.max(max, dp[i][cha + offset]);
+                }
+            }
+
+            return max;
+        }
+    }
+}
