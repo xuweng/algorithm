0beab2d2b4ec99ed74140c09f689888a4c315c24
xuweng <2028403305@qq.com>
2021-06-11 21:20:41 +0800

    300. 最长递增子序列
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must6/four/LengthOfLIS.java
@@ -0,0 +1,25 @@
+package com.leetcode.tag.must6.four;
+
+/**
+ * 300. 最长递增子序列
+ */
+public class LengthOfLIS {
+    class Solution {
+        public int lengthOfLIS(int[] nums) {
+            int[] dp = new int[nums.length];
+            int max = 1;
+
+            for (int i = 0; i < nums.length; i++) {
+                dp[i] = 1;
+                for (int j = 0; j < i; j++) {
+                    if (nums[j] < nums[i]) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                }
+                max = Math.max(max, dp[i]);
+            }
+
+            return max;
+        }
+    }
+}
