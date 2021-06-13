eb4a7ea2ba11b377e41865576aed01d213eee719
xuweng <2028403305@qq.com>
2021-06-13 10:36:41 +0800

    5784. 重新分配字符使所有字符串都相等
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/eight/MakeEqual.java
@@ -0,0 +1,12 @@
+package com.leetcode.tag.contest.eight;
+
+/**
+ * 5784. 重新分配字符使所有字符串都相等
+ */
+public class MakeEqual {
+    class Solution {
+        public boolean makeEqual(String[] words) {
+            return true;
+        }
+    }
+}
