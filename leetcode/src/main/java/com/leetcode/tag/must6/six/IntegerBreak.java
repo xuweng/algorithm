173523474960e05746d7aaf9f5ac71de7c952210
xuweng <2028403305@qq.com>
2021-06-16 21:20:54 +0800

    343. 整数拆分
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must6/six/IntegerBreak.java
@@ -0,0 +1,21 @@
+package com.leetcode.tag.must6.six;
+
+/**
+ * 343. 整数拆分
+ */
+public class IntegerBreak {
+    class Solution {
+        public int integerBreak(int n) {
+            int[] dp = new int[n + 1];
+            dp[0] = 1;
+
+            for (int i = 1; i <= n; i++) {
+                for (int j = 0; j < i; j++) {
+                    dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
