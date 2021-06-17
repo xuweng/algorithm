122fac8aab3e27636d82635d9e2ec924f176038f
xuweng <2028403305@qq.com>
2021-06-17 21:47:59 +0800

    879. 盈利计划
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must6/seven/ProfitableSchemes.java
@@ -0,0 +1,31 @@
+package com.leetcode.tag.must6.seven;
+
+/**
+ * 879. 盈利计划
+ */
+public class ProfitableSchemes {
+    class Solution {
+        int mod = (int) (1e9 + 7);
+
+        public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
+            int[][] dp = new int[n + 1][minProfit + 1];
+            for (int i = 0; i <= n; i++) {
+                dp[i][0] = 1;
+            }
+            for (int i = 0; i < group.length; i++) {
+                int g = group[i];
+                int p = profit[i];
+                // 枚举员工
+                for (int j = n; j >= g; j--) {
+                    // 枚举利润
+                    for (int k = minProfit; k >= 0; k--) {
+                        dp[j][k] += dp[j - g][Math.max(0, k - p)];
+                        dp[j][k] %= mod;
+                    }
+                }
+            }
+
+            return dp[n][minProfit];
+        }
+    }
+}
