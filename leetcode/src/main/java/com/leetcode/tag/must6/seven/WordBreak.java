361056736c299200d99bac57d46cbe1482ed96d1
xuweng <2028403305@qq.com>
2021-06-17 22:14:53 +0800

    139. 单词拆分
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must6/seven/WordBreak.java
@@ -0,0 +1,30 @@
+package com.leetcode.tag.must6.seven;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 139. 单词拆分
+ */
+public class WordBreak {
+    class Solution {
+        public boolean wordBreak(String s, List<String> wordDict) {
+            Set<String> set = new HashSet<>(wordDict);
+            boolean[] dp = new boolean[s.length() + 1];
+            dp[0] = true;
+
+            for (int i = 1; i <= s.length(); i++) {
+                for (int j = 0; j < i; j++) {
+                    // [0,j-1] [j,i-1]
+                    if (dp[j] && set.contains(s.substring(j, i))) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            return dp[s.length()];
+        }
+    }
+}
