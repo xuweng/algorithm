18ff09ec25c7bc2dc6a1da4297c468e72673a6fa
xuweng <2028403305@qq.com>
2021-06-20 11:41:48 +0800

    852. 山脉数组的峰顶索引
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must6/eight/PeakIndexInMountainArray.java
@@ -0,0 +1,27 @@
+package com.leetcode.tag.must6.eight;
+
+/**
+ * 852. 山脉数组的峰顶索引
+ */
+public class PeakIndexInMountainArray {
+    class Solution {
+        public int peakIndexInMountainArray(int[] arr) {
+            if (arr == null || arr.length == 0) {
+                return 0;
+            }
+            int left = 0;
+            int right = arr.length - 1;
+
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (arr[mid + 1] > arr[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+
+            return left;
+        }
+    }
+}
