9d5138fb84791ebddc98dd15ba811948284732e5
xuweng <2028403305@qq.com>
2021-06-20 11:28:10 +0800

    877. 石子游戏
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must6/eight/StoneGame.java
@@ -0,0 +1,22 @@
+package com.leetcode.tag.must6.eight;
+
+/**
+ * 877. 石子游戏
+ */
+public class StoneGame {
+    class Solution {
+        public boolean stoneGame(int[] piles) {
+            int[][] dp = new int[piles.length][piles.length];
+            for (int i = 0; i < piles.length; i++) {
+                dp[i][i] = piles[i];
+            }
+            for (int i = piles.length - 2; i >= 0; i--) {
+                for (int j = i + 1; j < piles.length; j++) {
+                    dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+                }
+            }
+
+            return dp[0][piles.length - 1] > 0;
+        }
+    }
+}
