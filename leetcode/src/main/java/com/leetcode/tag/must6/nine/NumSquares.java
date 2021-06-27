2704811e174b7549ac7f4e377f3c2b071b26ad42
xuweng <2028403305@qq.com>
2021-06-27 18:44:52 +0800

    279. 完全平方数
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must6/nine/NumSquares.java
@@ -0,0 +1,29 @@
+package com.leetcode.tag.must6.nine;
+
+import java.util.Arrays;
+
+/**
+ * 279. 完全平方数
+ */
+public class NumSquares {
+    class Solution {
+        public int numSquares(int n) {
+            int max = (int) Math.sqrt(n);
+            int[] coins = new int[max + 1];
+            for (int i = 1; i <= max; i++) {
+                coins[i] = i * i;
+            }
+            int[] dp = new int[n + 1];
+            Arrays.fill(dp, Integer.MAX_VALUE);
+            dp[0] = 0;
+
+            for (int coin : coins) {
+                for (int i = coin; i <= n; i++) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
