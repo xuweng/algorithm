7742029a082e90b517645eb237573866708a748b
xuweng <2028403305@qq.com>
2021-06-27 10:55:23 +0800

    5799. 最美子字符串的数目
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/contest/must/one/WonderfulSubstrings.java
@@ -0,0 +1,43 @@
+package com.leetcode.tag.contest.must.one;
+
+/**
+ * 5799. 最美子字符串的数目
+ */
+public class WonderfulSubstrings {
+    class Solution {
+        public long wonderfulSubstrings(String word) {
+            if (word == null || word.isEmpty()) {
+                return 0;
+            }
+            int count = 0;
+
+            for (int i = 0; i < word.length(); i++) {
+                for (int j = 0; j < word.length(); j++) {
+                    if (is(word.substring(i, j + 1))) {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        private boolean is(String s) {
+            int[] count = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                count[s.charAt(i) - '0']++;
+            }
+            int n = 0;
+            for (int i : count) {
+                if (i % 2 != 0) {
+                    n++;
+                }
+                if (n > 1) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
