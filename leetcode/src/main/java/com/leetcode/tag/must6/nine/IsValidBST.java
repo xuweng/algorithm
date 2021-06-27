c909815c96f9043e3b9cef090f5599dffd871c56
xuweng <2028403305@qq.com>
2021-06-27 19:23:24 +0800

    98. 验证二叉搜索树
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must6/nine/IsValidBST.java
@@ -0,0 +1,42 @@
+package com.leetcode.tag.must6.nine;
+
+/**
+ * 98. 验证二叉搜索树
+ */
+public class IsValidBST {
+    class Solution {
+        public boolean isValidBST(TreeNode root) {
+            return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        }
+
+        private boolean dfs(TreeNode root, long min, long max) {
+            if (root == null) {
+                return true;
+            }
+            if (root.val <= min || root.val >= max) {
+                return false;
+            }
+
+            return dfs(root.left, min, root.val) && dfs(root.right, root.val, max);
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
