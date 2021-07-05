44576b12aef6df4670718603a89eaf2b023dd360
xuweng <2028403305@qq.com>
2021-07-05 22:06:03 +0800

    647. 回文子串
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must6/ten/CountSubstrings.java
@@ -0,0 +1,32 @@
+package com.leetcode.tag.must6.ten;
+
+/**
+ * 647. 回文子串
+ */
+public class CountSubstrings {
+    class Solution {
+        public int countSubstrings(String s) {
+            if (s == null || s.isEmpty()) {
+                return 0;
+            }
+            boolean[][] dp = new boolean[s.length()][s.length()];
+            for (int i = 0; i < s.length(); i++) {
+                dp[i][i] = true;
+            }
+            int count = s.length();
+            for (int i = s.length() - 2; i >= 0; i--) {
+                for (int j = i + 1; j < s.length(); j++) {
+                    dp[i][j] = s.charAt(i) == s.charAt(j);
+                    if (j > i + 1) {
+                        dp[i][j] = dp[i][j] && dp[i + 1][j - 1];
+                    }
+                    if (dp[i][j]) {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
