14b4ac7b1c8853b487da9fc6329a9f78dbbe1695
xuweng <2028403305@qq.com>
2021-07-05 21:37:57 +0800

    394. 字符串解码
    Signed-off-by: xuweng <2028403305@qq.com>

---
+++ leetcode/src/main/java/com/leetcode/tag/must6/ten/DecodeString.java
@@ -0,0 +1,60 @@
+package com.leetcode.tag.must6.ten;
+
+/**
+ * 394. 字符串解码
+ */
+public class DecodeString {
+    class Solution {
+        int index;
+        String str;
+
+        public String decodeString(String s) {
+            if (s == null || s.isEmpty()) {
+                return s;
+            }
+            str = s;
+
+            return get();
+        }
+
+        private String get() {
+            if (index >= str.length() || str.charAt(index) == ']') {
+                // 终止条件
+                return "";
+            }
+            StringBuilder result = new StringBuilder();
+            char c = str.charAt(index);
+            if (Character.isDigit(c)) {
+                // 处理数字
+                // 23[abc]8[bd]
+                int d = getDigit();
+                // 过滤左括号 [
+                index++;
+                // 获取括号内
+                String s = get();
+                for (int i = 0; i < d; i++) {
+                    result.append(s);
+                }
+            } else if (Character.isLetter(c)) {
+                // 处理字母
+                // abc
+                result.append(c);
+            }
+            // 过滤右括号 ]
+            // 下一个
+            index++;
+
+            return result + get();
+        }
+
+        private int getDigit() {
+            int result = 0;
+            while (index < str.length() && Character.isDigit(str.charAt(index))) {
+                result = result * 10 + (str.charAt(index) - '0');
+                index++;
+            }
+
+            return result;
+        }
+    }
+}
