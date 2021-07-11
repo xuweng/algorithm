17e2b617de0ae9aedec04a25c5f31ec7d4caa2e3
xuweng <2028403305@qq.com>
2021-07-11 11:26:36 +0800

    模拟 + 二分
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must7/two/HIndex.java
+++ leetcode/src/main/java/com/leetcode/tag/must7/two/HIndex.java
@@ -46,4 +46,33 @@ public class HIndex {
             return 0;
         }
     }
+
+    /**
+     * 模拟 + 二分
+     */
+    class Solution2 {
+        public int hIndex(int[] cs) {
+            int n = cs.length;
+            int l = 0, r = n;
+            while (l < r) {
+                int mid = l + r + 1 >> 1;
+                if (check(cs, mid)) {
+                    l = mid;
+                } else {
+                    r = mid - 1;
+                }
+            }
+            return r;
+        }
+
+        boolean check(int[] cs, int mid) {
+            int ans = 0;
+            for (int i : cs) {
+                if (i >= mid) {
+                    ans++;
+                }
+            }
+            return ans >= mid;
+        }
+    }
 }
