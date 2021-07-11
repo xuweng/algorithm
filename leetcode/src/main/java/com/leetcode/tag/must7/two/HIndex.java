9a743286f26f7239e784847925d057f82cafcb7d
xuweng <2028403305@qq.com>
2021-07-11 11:24:31 +0800

    方法二：计数排序
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must7/two/HIndex.java
+++ leetcode/src/main/java/com/leetcode/tag/must7/two/HIndex.java
@@ -21,4 +21,29 @@ public class HIndex {
             return h;
         }
     }
+
+    /**
+     * 方法二：计数排序
+     */
+    class Solution1 {
+        public int hIndex(int[] citations) {
+            int n = citations.length;
+            int[] counter = new int[n + 1];
+            for (int citation : citations) {
+                if (citation >= n) {
+                    counter[n]++;
+                } else {
+                    counter[citation]++;
+                }
+            }
+            int tot = 0;
+            for (int i = n; i >= 0; i--) {
+                tot += counter[i];
+                if (tot >= i) {
+                    return i;
+                }
+            }
+            return 0;
+        }
+    }
 }
