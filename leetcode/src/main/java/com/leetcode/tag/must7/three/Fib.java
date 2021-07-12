29fb4ac4e8396967d934f151565b732eed7315ae
xuweng <2028403305@qq.com>
2021-07-12 20:49:57 +0800

    509. 斐波那契数
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must7/three/Fib.java
+++ leetcode/src/main/java/com/leetcode/tag/must7/three/Fib.java
@@ -16,4 +16,27 @@ public class Fib {
             return fib(n - 1) + fib(n - 2);
         }
     }
+
+    class Solution1 {
+        public int fib(int n) {
+            if (n == 0) {
+                return 0;
+            }
+            if (n == 1) {
+                return 1;
+            }
+
+            int p1 = 0;
+            int p2 = 1;
+            int p3 = 1;
+            for (int i = 2; i <= n; i++) {
+                p3 = p1 + p2;
+
+                p1 = p2;
+                p2 = p3;
+            }
+
+            return p3;
+        }
+    }
 }
