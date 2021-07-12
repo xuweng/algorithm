1d79757bc00a95e1182a50287daf2db8d3e75a5f
xuweng <2028403305@qq.com>
2021-07-12 20:15:16 +0800

    704. 二分查找
    Signed-off-by: xuweng <2028403305@qq.com>

--- leetcode/src/main/java/com/leetcode/tag/must7/three/Search.java
+++ leetcode/src/main/java/com/leetcode/tag/must7/three/Search.java
@@ -26,4 +26,27 @@ public class Search {
             return -1;
         }
     }
+
+    class Solution1 {
+        public int search(int[] nums, int target) {
+            if (nums == null || nums.length == 0) {
+                return 0;
+            }
+            int left = 0;
+            int right = nums.length;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                }
+                if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+
+            return -1;
+        }
+    }
 }
